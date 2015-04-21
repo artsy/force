1af1eb559f886b516c434cfccb03ffa743c16089
Kana Abe <kanakoabe5@gmail.com>
2015-04-21 17:24:41 -0400

    Moving to a more sensible location

--- analytics/main_layout.js
+++ analytics/main_layout.js
@@ -2,21 +2,3 @@
 // Analytics for the main layout. This includes buttons in the header, footer
 // or any other actions that occur on each page.
 //
-
-$('.js--post-split-test').click(function(e) {
-  e.preventDefault();
-  analytics.track("Clicked posts link", {
-    label: sd.POSTS_SECTION_NAME
-  })
-  location.assign($(e.target).attr('href'))
-})
-
-if (location.pathname.match('/articles')) {
-  var start = Date.now();
-  window.onbeforeunload = function(){
-    analytics.track("Spent time on articles page" , {
-      label: sd.POSTS_SECTION_NAME,
-      timeOnPage: Date.now() - start
-    })
-  };
-}
