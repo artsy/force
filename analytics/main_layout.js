f13e15a07b9e22e0f072da5ecc66e2b859c4b1bd
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-05-12 18:15:44 -0400

    closer

--- analytics/main_layout.js
+++ analytics/main_layout.js
@@ -8,12 +8,12 @@ analytics.page();
 
 // Track 15 second bounce rate
 setTimeout(function() {
-  analytics.track('time on page more than 15 seconds', { nonInteraction: 1 });
+  analytics.track('time on page more than 15 seconds', { category: '15 Seconds' });
 }, 15000);
 
 // Track 3 Minute bounce rate
 setTimeout(function() {
-  analytics.track('time on page more than 3 minutes', { nonInteraction: 1 });
+  analytics.track('time on page more than 3 minutes', { category: '3 Minutes' });
 }, 180000);
 
 // Tracking Snowplow page view. TODO: Remove when we phase out Snowplow.
