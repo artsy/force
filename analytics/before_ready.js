6eb5117cb404b360a3f2368b369a8dc22a551f63
James O'Toole <james.otoole@artsymail.com>
2015-07-28 18:05:46 -0400

    Remove debugger :/

--- analytics/before_ready.js
+++ analytics/before_ready.js
@@ -1,6 +1,5 @@
 /* Analytics code that needs to run before page load and analytics.ready */
 
 if (!location.pathname.match(/^\/article/) && !sd.VERTICAL){
-  debugger;
   PARSELY = {autotrack: false};
 } /* Disable Parsely firing on non-article/vertical pages */
