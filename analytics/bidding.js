32f0e7ebe1aa58e1e1a2be5bb83d6985068506bf
Craig Spaeth <craigspaeth@Craigs-iMac.local>
2015-10-05 14:51:49 -0400

    tracking for bidding

--- analytics/bidding.js
+++ analytics/bidding.js
@@ -3,16 +3,35 @@
 //
 
 // Clicked “Bid” button on artwork item from auction feature page
-
+$('.auction-artworks .aga-bid-button').click(function() {
+  analytics.track('Clicked “Bid” button on artwork item from auction feature page');
+});
 
 // Clicked “Register to bid” on the auction feature page
+$('.auction-header-register-button').click(function() {
+  analytics.track('Clicked “Register to bid” on the auction feature page');
+});
 
 // Clicked “Bid” on the artwork page
+$('.artwork-bid-form .abf-button').click(function() {
+  analytics.track('Clicked “Bid” on the artwork page');
+});
 
 // Clicked “Confirm Bid” on bid page
+$('.registration-form-content .avant-garde-button-black').click(function() {
+  analytics.track('Clicked “Confirm Bid” on bid page');
+
+  // Confirmed bid on bid page
+  $(document).one('ajaxSuccess', function(e) {
+    analytics.track('Confirmed bid on bid page');
+  });
+
+  // Error placing your bid
+  $(document).one('ajaxError', function(e) {
+    analytics.track('Error placing your bid');
+  });
+});
 
 // Showed ‘Confirm bid on artwork page’
-
-// Confirmed bid on bid page
-
-// Error placing your bid
+if (location.pathname.match(/artwork\/.*\/confirm-bid/))
+  analytics.track("Showed 'Confirm bid on artwork page'", { nonInteraction: 1 });
