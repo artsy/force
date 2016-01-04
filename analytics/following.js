327ee9b490b9f4e6a4080011d5db1812e086f580
Charles Broskoski <broskoski@gmail.com>
2016-01-04 18:08:34 -0500

    moves home tracking to segment

--- analytics/following.js
+++ analytics/following.js
@@ -1,7 +1,6 @@
 (function() {
   'use strict';
-  if (location.pathname.match(new RegExp('/favorites')) ||
-      location.pathname.match(new RegExp('/following/.*'))) {
+  if (location.pathname.match(new RegExp('/favorites')) || location.pathname.match(new RegExp('/following/.*'))) {
     analytics.page('Favorites/Follows page');
   }
 })();
--- analytics/home.js
+++ analytics/home.js
@@ -1,3 +1,16 @@
 $(function(){
   if(location.pathname == '/') analytics.track('Home page', { nonInteraction: 1 });
+
+  $('#home-featured-artworks .grid-item').click(function(e){
+    analytics.track('Clicked homepage artwork');
+  })
+
+  $('.is-via-personalized').click(function(e){
+    analytics.track('Clicked personalized homepage artwork');
+  })
+
+  $('is-via-featured').click(function(e){
+    analytics.track('Clicked featured homepage artwork');
+  })
+
 });
