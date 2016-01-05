b558d54b4d0c8b45b5e2a5d8dcc7cba35ccf5de7
Charles Broskoski <broskoski@gmail.com>
2016-01-05 14:08:52 -0500

    moves following analytics

--- analytics/following.js
+++ analytics/following.js
@@ -3,4 +3,9 @@
   if (location.pathname.match(new RegExp('/favorites')) || location.pathname.match(new RegExp('/following/.*'))) {
     analytics.page('Favorites/Follows page');
   }
+
+  analyticsHooks.on('followable:unfollowed followable:followed', function(options) {
+    analytics.track(options.message, { label: options.label } );
+  });
+
 })();
