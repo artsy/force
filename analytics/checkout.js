d243643afec76210ed895ea63dcaad5f659f4aa1
Charles Broskoski <broskoski@gmail.com>
2016-01-06 11:52:48 -0500

    moves analytics helpers to analytics folder

--- analytics/checkout.js
+++ analytics/checkout.js
@@ -1,16 +1,20 @@
 (function() {
   'use strict';
 
-  analyticsHooks.on('order:submitted', function(options) {
-    analytics.track("Order submitted", options);
+  analyticsHooks.on('order:submitted', function(data) {
+    analytics.track("Order submitted", data);
   });
 
-  analyticsHooks.on('order:validated', function(options) {
-    analytics.track("Order card validated", options);
+  analyticsHooks.on('order:item-added', function(data) {
+    analytics.track('Order - item added', data)
   });
 
-  analyticsHooks.on('order:submit-shipping', function(options) {
-    analytics.track("Order submit shipping", options);
+  analyticsHooks.on('order:validated', function(data) {
+    analytics.track("Order card validated", data);
+  });
+
+  analyticsHooks.on('order:submit-shipping', function(data) {
+    analytics.track("Order submit shipping", data);
   });
 
 })();
--- lib/analytics_helpers.coffee
+++
@@ -1,6 +0,0 @@
-_ = require 'underscore'
-_s = require 'underscore.string'
-
-module.exports.modelNameAndIdToLabel = (modelName, id) ->
-  throw new Error('Requires modelName and id') unless modelName? and id?
-  "#{_s.capitalize(modelName)}:#{id}"
