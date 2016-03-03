36b4a5ee24740bbe6896462fd94f8d3abdefafc6
Laura Brown <laurabrown1113@gmail.com>
2016-03-03 16:46:17 +0100

    artist sticky header

--- analytics/artist_page.js
+++ analytics/artist_page.js
@@ -1,7 +1,7 @@
 if (location.pathname.match('/artist/.*') && sd.ARTIST ) {
   analytics.track('Artist page', { id: sd.ARTIST.id, nonInteraction: 1 });
 
-  $('#artist-nav-list a, #artist-tabs a').click(function(e){
+  $('#artist-nav-list a, .artist-tabs a').click(function(e){
     tab = $(e.target).text();
     analytics.track('Clicked artist page tab', { tab: tab });
   });
