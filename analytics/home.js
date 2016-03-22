b9f13be9c12a2f0c9f08ac64f57b5c47abff79a0
Artsy <craigspaeth@gmail.com>
2016-03-22 17:31:40 -0400

    Added tracking to right arrow

--- analytics/home.js
+++ analytics/home.js
@@ -1,8 +1,14 @@
-$(function(){
-  if(location.pathname == '/') analytics.track('Home page', { nonInteraction: 1 });
+if(location.pathname == '/') analytics.track('Home page', { nonInteraction: 1 });
 
-  analytics.trackLink($('#home-featured-artworks .grid-item'), 'Clicked homepage artwork');
-  analytics.trackLink($('.is-via-personalized'), 'Clicked personalized homepage artwork');
-  analytics.trackLink($('.is-via-featured'), 'Clicked featured homepage artwork');
+analytics.trackLink($('#home-featured-artworks .grid-item'), 'Clicked homepage artwork');
+analytics.trackLink($('.is-via-personalized'), 'Clicked personalized homepage artwork');
+analytics.trackLink($('.is-via-featured'), 'Clicked featured homepage artwork');
 
-});
+
+$('#home-hero-units-right-arrow').click(function() {
+  analytics.track('Clicked right arrow on homepage', {
+    first_hero_unit: sd.HERO_UNITS[0].id,
+    auction_id: sd.AUCTION._id,
+    auction_slug: sd.AUCTION.id,
+  })
+})
