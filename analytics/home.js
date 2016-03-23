b9ee6ea6ef62991f5493341dbfb2aeb8a391e33d
zackgow <zackgow@gmail.com>
2016-03-23 12:50:59 -0400

    update tracking to feature links on the homepage

--- analytics/home.js
+++ analytics/home.js
@@ -7,9 +7,10 @@ analytics.trackLink($('.is-via-featured'), 'Clicked featured homepage artwork');
 
 
 $('.home-top-feature-link').click(function () {
-	var href = $(this).find('.htfl-image-link').attr('href')
-	var context_type = $(this).find('.htfl-details h3').text()
-	analytics.track('Clicked homepage featured link', {
-		featured_link_path: href, context_type
-	})
+  var href = $(this).find('.htfl-image-link').attr('href')
+  var context_type = $(this).find('.htfl-details h3').text()
+  analytics.track('Clicked homepage featured link', {
+  	featured_link_path: href
+	context_type: context_type
+  })
 })
