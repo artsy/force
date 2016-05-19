4ed681071497c6bfda1b8996690deab669752d42
Nicholas Sewitz <nicholas@artsymail.com>
2016-05-19 10:01:43 -0400

    fixed first committo only fire on active image and added part two for galleries banner

--- analytics/galleries.js
+++ analytics/galleries.js
@@ -1,4 +1,12 @@
-$(".galleries-institutions-primary-carousel .gpc-carousel .gpc-carousel-figure").click(function() {
+$(".galleries-institutions-primary-carousel .gpc-carousel [class*='is-selected'] .gpc-carousel-figure").click(function() {
+  var slug = $(this).attr("href").split("/")[2]
+
+  analytics.track('Clicked Galleries Banner', {
+    show_slug: slug
+  })
+})
+
+$(".galleries-institutions-primary-carousel .gpc-overlays [style*='block'] [href*='/show']").click(function() {
   var slug = $(this).attr("href").split("/")[2]
 
   analytics.track('Clicked Galleries Banner', {
