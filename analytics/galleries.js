692acdf01f1ca28e7b392983adf6b5afe923a960
nicholassewitz <nicholas@artsymail.com>
2016-05-23 15:38:56 -0400

    fixed click tracking on /galleries page

--- analytics/galleries.js
+++ analytics/galleries.js
@@ -1,23 +1,17 @@
-$(".galleries-institutions-primary-carousel .gpc-carousel [class*='is-selected'] .gpc-carousel-figure").click(function() {
-  var slug = $(this).attr("href").split("/")[2]
+$(".galleries-institutions-primary-carousel .gpc-carousel [class*='is-selected']").click(function() {
+  var slug = $(this).find(".gpc-carousel-figure").attr("href").split("/")[2]
 
   analytics.track('Clicked Galleries Banner', {
     show_slug: slug
   })
 })
 
-$(".galleries-institutions-primary-carousel .gpc-overlays [style*='block'] [href*='/show']").click(function() {
-  var slug = $(this).attr("href").split("/")[2]
+$(".galleries-institutions-primary-carousel .gpc-overlay").click(function() {
+  var partner_slug = $(this).find(".gpc-headline").attr("href").split("/")[1];
+  var show_slug = $(this).find("[href*='/show']").attr("href").split("/")[2]
 
   analytics.track('Clicked Galleries Banner', {
-    show_slug: slug
-  })
-})
-
-$(".galleries-institutions-primary-carousel .gpc-overlays .gpc-headline").click(function() {
-  var slug = $(this).attr("href").split("/")[1]
-
-  analytics.track('Clicked Galleries Banner', {
-    partner_slug: slug
+    show_slug: show_slug,
+    partner_slug: partner_slug
   })
 })
