07328766d5636406f3bbf4a8d540278b5a10c3a9
dzucconi <mail@damonzucconi.com>
2016-06-29 16:14:14 +0200

    Fixup artwork analytics

--- analytics/artwork.js
+++ analytics/artwork.js
@@ -14,6 +14,12 @@
     .on('click', '.analytics-artwork-share', function() {
       analytics.track('Viewed sharing_is_caring form');
     })
+    .on('click', '.analytics-artwork-zoom', function() {
+      analytics.track('Clicked to zoom in on artwork');
+    })
+    .on('click', '.analytics-artwork-view-in-room', function() {
+      analytics.track("Entered 'View In Room'");
+    })
     .on('click', '.analytics-artwork-acquire', function() {
       analytics.track('Clicked "Buy" on the artwork page');
     })
@@ -29,12 +35,6 @@
     });
 
   analyticsHooks
-    .on('artwork:zoom', function() {
-      analytics.track('Clicked to zoom in on artwork');
-    })
-    .on('artwork:view-in-room', function(){
-      analytics.track("Entered 'View In Room'");
-    })
     .on('artwork:contact-gallery', function(context) {
       analytics.track("Clicked 'Contact Gallery'", {
         artwork_id: context.artwork_id,
@@ -46,9 +46,6 @@
     })
     .on('artwork:confirm-registration', function() {
       analytics.track("Showed 'Confirm registration on artwork page'", { nonInteraction: 1 });
-    })
-    .on('artwork:view-in-room', function() {
-      //
     });
 })();
 
