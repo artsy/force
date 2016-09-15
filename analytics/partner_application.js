4f5308f913f4aab18da82b017ec03ab650c7da8b
Craig Spaeth <craigspaeth@gmail.com>
2016-09-15 18:05:19 -0400

    remove tracking that was conflicting

--- analytics/partner_application.js
+++ analytics/partner_application.js
@@ -2,15 +2,6 @@ if (location.pathname.match('/gallery-partnerships')) {
 
   analytics.page('Gallery Partnerships');
 
-  analytics.trackForm(
-    $('#mktoForm_1238')[0],
-    '/gallery-partnerships CTA',
-    {
-      session_id: sd.SESSION_ID,
-      user_id: sd.CURRENT_USER && sd.CURRENT_USER.id
-    }
-  );
-
   $('.partnerships-nav-link.internal').click(function(e){
     var section = $(e.currentTarget).attr('data-section')
     analytics.track('Clicked nav link on gallery partnerships',
