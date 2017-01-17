d22077f9b1823b68140920fc7118eed3d5d4acbe
xtina-starr <c.thompsonms@gmail.com>
2017-01-17 13:04:22 -0500

    pull gallery id from data attr

--- analytics/partner.js
+++ analytics/partner.js
@@ -9,15 +9,15 @@
     analytics.track('Non-claimed partner page', _.extend(options, {nonInteraction: 1}))
   })
 
-  $('#partner-contact .email-gallery').click(function () {
+  $('#partner-contact .email-gallery').click(function (e) {
     analytics.track('Clicked Contact Gallery Via Email', {
-      gallery_id: sd.CURRENT_PATH.split('/')[1] // gallery id
+      gallery_id: $(e.currentTarget).data('id')
     })
   })
 
-  $('#partner-contact .partner-website').click(function () {
+  $('#partner-contact .partner-website').click(function (e) {
     analytics.track('Clicked Gallery Website', {
-      gallery_id: sd.CURRENT_PATH.split('/')[1] // gallery id
+      gallery_id: $(e.currentTarget).data('id')
     })
   })
 })()
