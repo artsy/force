f6dc32d3664a55f14b1f3efc8b47d234efd71b95
xtina-starr <c.thompsonms@gmail.com>
2017-01-17 12:00:13 -0500

    add analytics to gallery contact

--- analytics/partner.js
+++ analytics/partner.js
@@ -8,4 +8,16 @@
   analyticsHooks.on('partner:non-claimed', function (options) {
     analytics.track('Non-claimed partner page', _.extend(options, {nonInteraction: 1}))
   })
+
+  $('#partner-contact .email-gallery').click(function () {
+    analytics.track('Clicked Contact Gallery Via Email', {
+      gallery_id: sd.CURRENT_PATH.split('/')[1] // gallery id
+    })
+  })
+
+  $('#partner-contact .partner-website').click(function () {
+    analytics.track('Clicked Gallery Website', {
+      gallery_id: sd.CURRENT_PATH.split('/')[1] // gallery id
+    })
+  })
 })()
