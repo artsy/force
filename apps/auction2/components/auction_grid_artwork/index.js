95676f3994df4cb29843abb06f57358255867140
sweir27 <sweir27@gmail.com>
2017-01-24 13:58:35 -0500

    namespacing auction2 page

--- apps/auction2/components/auction_grid_artwork/index.js
+++ apps/auction2/components/auction_grid_artwork/index.js
@@ -6,25 +6,25 @@ export default function AuctionGridArtwork({ artwork }, _) {
   const artistDisplay = artists && artists.length > 0 ? artists.map((aa) => aa.name).join(', ') : null
 
   return (
-    <div className='auction-grid-artwork' key={artwork._id}>
-      <div className='auction-grid-artwork__image-container'>
+    <div className='auction2-grid-artwork' key={artwork._id}>
+      <div className='auction2-grid-artwork__image-container'>
         <div className='vam-outer'>
           <div className='vam-inner'>
-            <a className='auction-grid-artwork__image' href={artwork.href()}>
+            <a className='auction2-grid-artwork__image' href={artwork.href()}>
               <img src={artwork.defaultImage().get('image_url')} alt={artwork.title}></img>
             </a>
           </div>
         </div>
       </div>
-      <div className='auction-grid-artwork__primary-information'>
-        <div className='auction-grid-artwork__lot-number'>
+      <div className='auction2-grid-artwork__primary-information'>
+        <div className='auction2-grid-artwork__lot-number'>
           Lot {artwork.get('sale_artwork').lot_number}
         </div>
-        <div className='auction-grid-artwork__artists'>
+        <div className='auction2-grid-artwork__artists'>
           {artistDisplay}
         </div>
-        <div className='auction-grid-artwork__title' dangerouslySetInnerHTML={{ __html: artwork.titleAndYear() }}></div>
-        <div className='auction-grid-artwork__sale'>
+        <div className='auction2-grid-artwork__title' dangerouslySetInnerHTML={{ __html: artwork.titleAndYear() }}></div>
+        <div className='auction2-grid-artwork__sale'>
           { artwork.get('sale') && artwork.get('sale').name }
         </div>
       </div>
