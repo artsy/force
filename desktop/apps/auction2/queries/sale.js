c5bb6d885462192d53c70b1803b45a05b39217ed
sweir27 <sweir27@gmail.com>
2017-03-30 16:38:14 -0400

    add associated sale to the auction2 page

--- desktop/apps/auction2/queries/sale.js
+++ desktop/apps/auction2/queries/sale.js
@@ -2,6 +2,22 @@ export default function SaleQuery(id) {
   return `{
     sale(id: "${id}") {
       _id
+      associated_sale {
+        cover_image {
+          cropped(width: 260, height: 110) {
+            url
+          }
+        }
+        end_at
+        href
+        id
+        is_closed
+        is_live_open
+        is_preview
+        live_start_at
+        name
+        start_at
+      }
       auction_state
       cover_image {
         cropped(width: 1800 height: 600 version: "wide") {
--- desktop/apps/auction2/routes.js
+++ desktop/apps/auction2/routes.js
@@ -3,6 +3,7 @@ import Auction from '../../models/auction.coffee'
 import SaleQuery from './queries/sale'
 import MeQuery from './queries/me'
 import { PREDICTION_URL } from '../../config'
+import upcomingLabel from '../../components/current_auctions/utils/upcoming_label'
 
 export const index = async (req, res) => {
   const { me } = await metaphysics({ query: MeQuery(req.params.id), req: req })
@@ -10,7 +11,7 @@ export const index = async (req, res) => {
 
   res.locals.sd.AUCTION = sale
   const newAuction = new Auction(sale)
-  res.render('index', { auction: newAuction, me: me })
+  res.render('index', { auction: newAuction, me: me, auctionUpcomingLabel: upcomingLabel })
 }
 
 export const redirectLive = async (req, res, next) => {
