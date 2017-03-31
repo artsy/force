4de40594c2120dbda5231fe2b126a564dc69dc90
sweir27 <sweir27@gmail.com>
2017-03-31 14:04:30 -0400

    related article footer on auction2 page

--- desktop/apps/auction2/routes.js
+++ desktop/apps/auction2/routes.js
@@ -1,5 +1,9 @@
 import metaphysics from '../../../lib/metaphysics'
+import ArticlesQuery from './queries/articles'
+import Articles from '../../collections/articles.coffee'
 import Auction from '../../models/auction.coffee'
+import footerItems from './footer_items'
+import positronql from '../../lib/positronql.coffee'
 import SaleQuery from './queries/sale'
 import MeQuery from './queries/me'
 import { PREDICTION_URL } from '../../config'
@@ -7,10 +11,17 @@ import { PREDICTION_URL } from '../../config'
 export const index = async (req, res) => {
   const { me } = await metaphysics({ query: MeQuery(req.params.id), req: req })
   const { sale } = await metaphysics({ query: SaleQuery(req.params.id) })
+  const { articles } = await metaphysics({ query: ArticlesQuery(sale._id) })
 
   res.locals.sd.AUCTION = sale
   const newAuction = new Auction(sale)
-  res.render('index', { auction: newAuction, me: me })
+  const auctionArticles = new Articles(articles)
+  res.render('index', {
+    articles: auctionArticles,
+    auction: newAuction,
+    footerItems: footerItems,
+    me: me
+  })
 }
 
 export const redirectLive = async (req, res, next) => {
