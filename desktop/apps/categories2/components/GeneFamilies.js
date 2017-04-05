b3a61fc135c890e278424382cf1a26b11fb073f6
Lance W <lwakeling@gmail.com>
2017-04-05 11:27:12 -0400

    Refactor components to be stateless and update tests

--- desktop/apps/categories2/components/GeneFamilies.js
+++ desktop/apps/categories2/components/GeneFamilies.js
@@ -4,20 +4,23 @@ const GeneFamily = (props) => {
   return (
     <div className="categories2-gene-family">
       <h3>{props.data.name}</h3>
-      <div className="categories2-gene-family-description">{props.data.description}</div>
+      <div className="categories2-gene-family-description">
+        {props.data.description}
+      </div>
     </div>
   )
 }
 
-export default class GeneFamilies extends React.Component {
-  render(){
-    const geneFamilies = this.props.data.map((value, index) => {
-      return <GeneFamily key={`gene-family-${index}`} data={value} />
-    })
-    return (
-      <div className="categories2-gene-families">
-        {geneFamilies}
-      </div>
-    )
-  }
+const GeneFamilies = (props) => {
+  return (
+    <div className="categories2-gene-families">
+      {
+        props.data.map((value, index) => {
+          return <GeneFamily key={value.id} data={value} />
+        })
+      }
+    </div>
+  )
 }
+
+export default GeneFamilies
--- desktop/apps/categories2/components/Sidebar.js
+++ desktop/apps/categories2/components/Sidebar.js
@@ -1,16 +1,21 @@
 import React from 'react'
 
-export default class Sidebar extends React.Component {
-  render(){
-    const { data } = this.props
-    return (
-      <div className="categories2-sidebar">
-        <ul>{ 
-          data.map(
-            (value) => <li key={`gene-${value.id}`}>{value.name}</li>
-          )
-        }</ul>
-      </div>
-    )
-  }
+const Sidebar = (props) => {
+  return (
+    <div className="categories2-sidebar">
+      <ul>
+        {
+          props.data.map((value) => {
+            return (
+              <li key={`gene-${value.id}`}>
+                {value.name}
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
+  )
 }
+
+export default Sidebar
