9f76f7884a395a15407e83d8391dfc368d17d3a5
sweir27 <sweir27@gmail.com>
2017-04-11 14:17:12 -0400

    rename auction2 --> auction across the board

---
+++ desktop/apps/auction/components/sidebar/index.js
@@ -0,0 +1,28 @@
+import ArtistFilter from '../artist_filter'
+import BasicCheckbox from '../basic_checkbox'
+import { connect } from 'react-redux'
+import MediumFilter from '../medium_filter'
+import RangeSlider from '../range_slider'
+import React from 'react'
+
+function Sidebar({ isOpen }) {
+  return (
+    <div className='auction-artworks-sidebar'>
+      <div className='auction-artworks-sidebar__artist-filter'>
+        { isOpen && <RangeSlider /> }
+        <MediumFilter />
+        <ArtistFilter />
+      </div>
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    isOpen: state.auctionArtworks.isOpen
+  }
+}
+
+export default connect(
+  mapStateToProps,
+)(Sidebar)
