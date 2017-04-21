946ba2619795426483cc265e0cf96918cd51caee
Lance W <lwakeling@gmail.com>
2017-04-21 13:34:15 -0400

    Un-update .env.test path

--- scripts/acceptance.sh
+++ scripts/acceptance.sh
@@ -7,4 +7,4 @@ mocha \
   --compilers coffee:coffee-script/register,js:babel-core/register \
   -r dotenv/config \
   -r should \
-  -t 300000 dotenv_config_path=../.env.test test/acceptance/*.js
+  -t 300000 dotenv_config_path=.env.test test/acceptance/*.js
