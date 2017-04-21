9751e2f4d11ae18288d92cdf1f006bdeacf18865
Lance W <lwakeling@gmail.com>
2017-04-21 14:25:37 -0400

    Remove the `&& \`s

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,12 +5,12 @@ set -e -x
 yarn acceptance
 yarn mocha test/lib/*
 yarn mocha $(find desktop/test -name '*.coffee')
-yarn mocha $(find desktop/components/*/test -name '*.coffee') && \
-yarn mocha $(find desktop/components/**/*/test -name '*.coffee') && \
-yarn mocha $(find desktop/apps/*/test -name '*.coffee' -or -name '*.js') && \
-yarn mocha $(find desktop/apps/*/**/*/test -name '*.coffee') && \
-yarn mocha $(find mobile/test -name '*.coffee') && \
-yarn mocha $(find mobile/components/*/test -name '*.coffee') && \
-yarn mocha $(find mobile/components/**/*/test -name '*.coffee') && \
-yarn mocha $(find mobile/apps/*/test -name '*.coffee') && \
+yarn mocha $(find desktop/components/*/test -name '*.coffee')
+yarn mocha $(find desktop/components/**/*/test -name '*.coffee')
+yarn mocha $(find desktop/apps/*/test -name '*.coffee' -or -name '*.js')
+yarn mocha $(find desktop/apps/*/**/*/test -name '*.coffee')
+yarn mocha $(find mobile/test -name '*.coffee')
+yarn mocha $(find mobile/components/*/test -name '*.coffee')
+yarn mocha $(find mobile/components/**/*/test -name '*.coffee')
+yarn mocha $(find mobile/apps/*/test -name '*.coffee')
 yarn mocha $(find mobile/apps/*/**/*/test -name '*.coffee')
