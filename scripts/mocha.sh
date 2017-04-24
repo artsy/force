22e21d3497fcfb2f46fef3b5186ddf568c1660ef
Lance W <lwakeling@gmail.com>
2017-04-24 12:15:42 -0400

    Use source-map-support with mocha.sh

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -5,6 +5,7 @@ set -e -x
 for file in "$@"
   do
     mocha \
+      --require source-map-support/register \
       -r should \
       --compilers coffee:coffee-script/register,js:babel-core/register \
       -t 30000 $file
