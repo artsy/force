833b3e2948fb45645e512b8d82a1008a1098a04f
Craig Spaeth <craigspaeth@gmail.com>
2017-04-28 17:54:48 -0400

    Pass all files into mocha command to leverage caching for faster tests

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -2,11 +2,8 @@
 
 set -e -x
 
-for file in "$@"
-  do
-    mocha \
-      --require source-map-support/register \
-      -r should \
-      --compilers coffee:coffee-script/register,js:babel-core/register \
-      -t 30000 $file
-  done
+mocha \
+  --require source-map-support/register \
+  -r should \
+  --compilers coffee:coffee-script/register,js:babel-core/register \
+  -t 30000 $@
