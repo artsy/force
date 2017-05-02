f3b4b369221589a6407bd58d77ef6ca6ba6e0884
Charles Broskoski <broskoski@gmail.com>
2017-05-02 16:50:10 -0400

    Add Artsy.ContextProvider

--- desktop/apps/tag/client.js
+++ desktop/apps/tag/client.js
@@ -4,7 +4,9 @@ import * as Relay from 'react-relay'
 import { data as sd } from 'sharify'
 
 import components from '../../../node_modules/@artsy/reaction-force/dist/components/index'
+import * as Artsy from '../../../node_modules/@artsy/reaction-force/dist/components/artsy'
 import FilterArtworksQueryConfig from '../../../node_modules/@artsy/reaction-force/dist/relay/queries/filter_artworks'
+
 
 function artsyNetworkLayer (user) {
   return new Relay.DefaultNetworkLayer(sd.METAPHYSICS_ENDPOINT, {
@@ -18,10 +20,12 @@ function artsyNetworkLayer (user) {
 function init () {
   Relay.injectNetworkLayer(artsyNetworkLayer())
   ReactDOM.render(
-    <Relay.RootContainer
-      Component={components.ArtworkFilter}
-      route={new FilterArtworksQueryConfig({ tag_id: sd.TAG.id })}
-    />, document.getElementById('tag-filter')
+    <Artsy.ContextProvider currentUser={sd.CURRENT_USER}>
+      <Relay.RootContainer
+        Component={components.ArtworkFilter}
+        route={new FilterArtworksQueryConfig({ tag_id: sd.TAG.id })}
+      />
+    </Artsy.ContextProvider>, document.getElementById('tag-filter')
   )
 }
 
