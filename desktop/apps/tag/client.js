08501c29664baa02a8db951a6a419ba5ca65d237
Charles Broskoski <broskoski@gmail.com>
2017-05-02 10:18:11 -0400

    Adds client file

---
+++ desktop/apps/tag/client.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import * as Relay from 'react-relay'
+import { data as sd } from 'sharify'
+
+import components from '../../../node_modules/@artsy/reaction-force/dist/components/index'
+import FilterArtworksQueryConfig from '../../../node_modules/@artsy/reaction-force/dist/relay/queries/filter_artworks'
+
+function artsyNetworkLayer (user) {
+  return new Relay.DefaultNetworkLayer(sd.METAPHYSICS_ENDPOINT, {
+    headers: !sd.CURRENT_USER ? {
+      'X-USER-ID': sd.CURRENT_USER.id,
+      'X-ACCESS-TOKEN': sd.CURRENT_USER.accessToken
+    } : {}
+  })
+}
+
+function init () {
+  Relay.injectNetworkLayer(artsyNetworkLayer())
+  ReactDOM.render(
+    <Relay.RootContainer
+      Component={components.ArtworkFilter}
+      route={new FilterArtworksQueryConfig({ tag_id: sd.TAG.id })}
+    />, document.getElementById('tag-filter')
+  )
+}
+
+export default { init }
