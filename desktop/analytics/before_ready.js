97c6d2aae41cc3f776e4a81d9bba69243fe4d514
kanaabe <kanakoabe5@gmail.com>
2017-05-03 15:55:40 -0400

    Adds more dynamic metadata and image

--- desktop/analytics/before_ready.js
+++ desktop/analytics/before_ready.js
@@ -1,4 +1,4 @@
 /* Analytics code that needs to run before page load and analytics.ready */
-if (!location.pathname.match(/^\/article|^\/2016-year-in-art/)) {
+if (!location.pathname.match(/^\/article|^\/2016-year-in-art|^\/venice-biennale/)) {
   window.PARSELY = {autotrack: false}
 } /* Disable Parsely firing on non-article/section pages */
