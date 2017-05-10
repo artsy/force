4b2f37996d3fa159878bd53aa80813a732d0a4dd
Charles Broskoski <broskoski@gmail.com>
2017-05-10 18:14:49 -0400

    Use reaction-force network layer and import from module

--- desktop/apps/tag/client.js
+++ desktop/apps/tag/client.js
@@ -3,19 +3,10 @@ import ReactDOM from 'react-dom'
 import * as Relay from 'react-relay'
 import { data as sd } from 'sharify'
 
-import components from '../../../node_modules/@artsy/reaction-force/dist/components/index'
-import * as Artsy from '../../../node_modules/@artsy/reaction-force/dist/components/artsy'
-import FilterArtworksQueryConfig from '../../../node_modules/@artsy/reaction-force/dist/relay/queries/filter_artworks'
-
-
-function artsyNetworkLayer (user) {
-  return new Relay.DefaultNetworkLayer(sd.METAPHYSICS_ENDPOINT, {
-    headers: sd.CURRENT_USER ? {
-      'X-USER-ID': sd.CURRENT_USER.id,
-      'X-ACCESS-TOKEN': sd.CURRENT_USER.accessToken
-    } : {}
-  })
-}
+import { artsyNetworkLayer } from '@artsy/reaction-force/dist/relay/config'
+import components from '@artsy/reaction-force/dist/components/index'
+import * as Artsy from '@artsy/reaction-force/dist/components/artsy'
+import FilterArtworksQueryConfig from '@artsy/reaction-force/dist/relay/queries/filter_artworks'
 
 function init () {
   Relay.injectNetworkLayer(artsyNetworkLayer(sd.CURRENT_USER))
