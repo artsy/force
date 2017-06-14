539d819a06298b0441e43ae4307499e6a3bcdc42
sweir27 <sweir27@gmail.com>
2017-06-14 17:18:47 -0400

    revert hack for creative time

--- desktop/apps/auction/components/sidebar/index.js
+++ desktop/apps/auction/components/sidebar/index.js
@@ -4,15 +4,12 @@ import PropTypes from 'prop-types'
 import RangeSlider from '../range_slider'
 import React from 'react'
 import { connect } from 'react-redux'
-import { data as sd } from 'sharify'
 
 function Sidebar ({ isClosed }) {
-  const isCreativeTimeAuction = sd.AUCTION && sd.AUCTION._id === '593c3fac8b0c147c16a59381'
-
   return (
     <div className='auction-artworks-sidebar'>
       <div className='auction-artworks-sidebar__artist-filter'>
-        { !isClosed && !isCreativeTimeAuction && <RangeSlider /> }
+        { !isClosed && <RangeSlider /> }
         <MediumFilter />
         <ArtistFilter />
       </div>
