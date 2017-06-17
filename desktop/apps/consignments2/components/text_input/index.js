abd4fd183666d906ae26c09e228fab91af837686
sweir27 <sweir27@gmail.com>
2017-06-16 23:01:55 -0400

    little more obvious validation

--- desktop/apps/consignments2/components/text_input/index.js
+++ desktop/apps/consignments2/components/text_input/index.js
@@ -2,19 +2,20 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import block from 'bem-cn'
 
-export const renderTextInput = ({ input: { onChange, value }, meta: { warning }, ...custom }) => (
+export const renderTextInput = ({ input: { onChange, value }, meta: { error, touched, warning }, ...custom }) => (
   <TextInput
     {...custom}
     value={value}
     onChange={onChange}
     warning={warning}
+    touched={touched}
+    error={error}
   />
 )
 
 function TextInput (props) {
-  const { autofocus, item, label, instructions, onChange, type, warning, value, ...rest } = props
+  const { autofocus, error, item, label, instructions, onChange, touched, type, warning, value, ...rest } = props
   const b = block('consignments2-submission-text-input')
-
   return (
     <div className={b()}>
       { label && <div className={b('label')}>{ label }</div> }
@@ -27,17 +28,24 @@ function TextInput (props) {
         onKeyUp={(e) => onChange(e.target.value)}
         defaultValue={value}
       />
-      {warning && <div className={b('warning')}>{warning}</div>}
+      {
+        touched && (
+          (warning && <div className={b('warning')}>{warning}</div>) ||
+          (error && <div className={b('error')}>{error}</div>)
+        )
+      }
     </div>
   )
 }
 
 TextInput.propTypes = {
   autofocus: PropTypes.bool,
+  error: PropTypes.bool,
   item: PropTypes.string.isRequired,
   label: PropTypes.string,
   instructions: PropTypes.string,
   onChange: PropTypes.func,
+  touched: PropTypes.bool,
   type: PropTypes.string,
   warning: PropTypes.string,
   value: PropTypes.oneOfType([
