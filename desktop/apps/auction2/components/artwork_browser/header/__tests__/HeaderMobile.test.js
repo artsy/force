1e3e7b71ac6d4825fd9e78a6aa96eb7b03fd3b96
Christopher Pappas <damassi.pappas@gmail.com>
2017-07-18 18:45:57 -0400

    Add HeaderDesktop tests

--- desktop/apps/auction2/components/artwork_browser/header/__tests__/HeaderMobile.test.js
+++ desktop/apps/auction2/components/artwork_browser/header/__tests__/HeaderMobile.test.js
@@ -1,5 +1,62 @@
-describe('auction/components/artwork_browser/header/HeaderMobile.test', () => {
-  it('works', () => {
+import renderTestComponent from 'desktop/apps/auction2/__tests__/utils/renderTestComponent'
+import HeaderMobile from 'desktop/apps/auction2/components/artwork_browser/header/HeaderMobile'
+import sinon from 'sinon'
+import { __RewireAPI__ as RoutesRewireApi } from 'desktop/apps/auction2/actions/filter'
 
+describe('auction/components/artwork_browser/header/HeaderMobile.test', () => {
+  describe('<HeaderMobile />', () => {
+    afterEach(() => {
+      RoutesRewireApi.__ResetDependency__('metaphysics')
+    })
+
+    it('default sort is Default', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderMobile
+      })
+
+      wrapper.find('select').find('option').first().html().should.containEql('Default')
+    })
+
+    it('calls updateSort on select change', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderMobile
+      })
+
+      const { store } = wrapper.props()
+      const updateSort = 'bidder_positions_count'
+
+      const auctionQueries = {
+        filter_sale_artworks: {
+          aggregations: [
+            {
+              slice: 'ARTIST',
+              counts: 10
+            },
+            {
+              slice: 'MEDIUM',
+              counts: 13
+            }
+          ],
+          counts: 10,
+          hits: [
+            {
+              artwork: {
+                _id: 'foo'
+              }
+            }
+          ]
+        }
+      }
+
+      RoutesRewireApi.__Rewire__('metaphysics', sinon.stub().returns(Promise.resolve(auctionQueries)))
+
+      wrapper.find('select').simulate('change', {
+        target: {
+          value: updateSort
+        }
+      })
+
+      store.getState().auctionArtworks.filterParams.sort.should.eql(updateSort)
+    })
   })
 })
