9dfd2621e78cf845d595062a34b42c35a66c75ff
Christopher Pappas <damassi.pappas@gmail.com>
2017-07-18 18:11:46 -0400

    Add HeaderDesktop tests

--- desktop/apps/auction2/components/artwork_browser/header/__tests__/HeaderDesktop.test.js
+++ desktop/apps/auction2/components/artwork_browser/header/__tests__/HeaderDesktop.test.js
@@ -1,5 +1,57 @@
-describe('auction/components/artwork_browser/header/HeaderDesktop.test', () => {
-  it('works', () => {
+import renderTestComponent from 'desktop/apps/auction2/__tests__/utils/renderTestComponent'
+import FilterSort from 'desktop/apps/auction2/components/artwork_browser/header/FilterSort'
+import HeaderDesktop from 'desktop/apps/auction2/components/artwork_browser/header/HeaderDesktop'
 
+describe('auction/components/artwork_browser/header/HeaderDesktop.test', () => {
+  describe('<HeaderDesktop />', () => {
+    it('renders a <FilterSort /> component', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderDesktop
+      })
+
+      wrapper.find(FilterSort).length.should.eql(1)
+    })
+
+    it('renders a grid icon', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderDesktop
+      })
+
+      wrapper.find('.auction2-artworks-header-desktop__grid').length.should.eql(1)
+    })
+
+    it('renders a default grid icon', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderDesktop
+      })
+
+      wrapper.find('.auction2-artworks-header-desktop__grid .active').length.should.eql(1)
+    })
+
+    it('renders a list icon', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderDesktop
+      })
+
+      wrapper.find('.auction2-artworks-header-desktop__list').length.should.eql(1)
+    })
+
+    it('toggles a sort on click', () => {
+      const { wrapper } = renderTestComponent({
+        Component: HeaderDesktop
+      })
+
+      const { store } = wrapper.props()
+
+      wrapper.find('.auction2-artworks-header-desktop__list').simulate('click')
+      store.getState().auctionArtworks.isListView.should.eql(true)
+      wrapper.find('.auction2-artworks-header-desktop__list .active').length.should.eql(1)
+      wrapper.find('.auction2-artworks-header-desktop__grid .active').length.should.eql(0)
+
+      wrapper.find('.auction2-artworks-header-desktop__grid').simulate('click')
+      store.getState().auctionArtworks.isListView.should.eql(false)
+      wrapper.find('.auction2-artworks-header-desktop__list .active').length.should.eql(0)
+      wrapper.find('.auction2-artworks-header-desktop__grid .active').length.should.eql(1)
+    })
   })
 })
