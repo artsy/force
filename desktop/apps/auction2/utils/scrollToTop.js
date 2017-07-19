bc0a222a296e49970fd5a130e34ca52d3857083e
Christopher Pappas <damassi.pappas@gmail.com>
2017-07-19 18:36:01 -0400

    Fix selectors

--- desktop/apps/auction2/utils/scrollToTop.js
+++ desktop/apps/auction2/utils/scrollToTop.js
@@ -3,6 +3,6 @@ import $ from 'jquery'
 const DURATION_MS = 400
 
 export default function scrollToTop () {
-  const scrollTop = $('.auction2-artworks-header').offset().top - $('.mlh-navbar').height()
+  const scrollTop = $('.auction2-artworks-header-desktop').offset().top - $('.mlh-navbar').height()
   $('html,body').animate({ scrollTop }, DURATION_MS)
 }
