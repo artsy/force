a413499fbeaeb75095f3a65be46fff639de23d17
Christopher Pappas <damassi.pappas@gmail.com>
2017-08-17 13:13:26 -0700

    Finish updating subapps

--- desktop/apps/auction/index.js
+++ desktop/apps/auction/index.js
@@ -1,16 +1,14 @@
 import 'babel-core/register'
+import * as routes from './routes'
 import express from 'express'
-import reloadable, { isDevelopment } from 'desktop/lib/reloadable'
 
 const app = module.exports = express()
 
 app.set('view engine', 'jade')
 app.set('views', `${__dirname}/components/layout`)
 
-if (isDevelopment) {
-  reloadable(app, (req, res, next) => {
-    require('./server')(req, res, next)
-  })
-} else {
-  app.use(require('./server'))
-}
+app.get('/sale/:id', routes.index)
+app.get('/sale/:id/confirm-registration', routes.index)
+
+app.get('/auction/:id', routes.index)
+app.get('/auction/:id/confirm-registration', routes.redirectLive, routes.index)
