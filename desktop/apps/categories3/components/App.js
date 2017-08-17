93174b0ddc2d0fcc1abbe5e034d236fcbd9f953e
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-08-16 21:40:56 -0400

    Rough in the gene family data

--- desktop/apps/categories3/components/App.js
+++ desktop/apps/categories3/components/App.js
@@ -1,16 +1,74 @@
 import React, { Component } from 'react'
 
+const GeneFamilyNav = ({ geneFamilies }) => {
+  return (
+    <ul>
+      {geneFamilies.map(geneFamily => <li key={geneFamily.id}>TK</li>)}
+    </ul>
+  )
+}
+
+const TAGPContent = ({ geneFamilies }) => {
+  return (
+    <div>
+      <TAGPIntro />
+      <GeneFamilies geneFamilies={geneFamilies} />
+    </div>
+  )
+}
+
+const TAGPIntro = () => {
+  return (
+    <div>
+      <h1>The Art Genome Project</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit, quidem
+        facere doloribus quisquam ducimus alias, consequuntur officia modi
+        dolores ipsam distinctio harum facilis, illum fugit ea? Quo illum esse
+        et!
+      </p>
+    </div>
+  )
+}
+
+const GeneFamilies = ({ geneFamilies }) => {
+  return (
+    <div>
+      {geneFamilies.map(geneFamily =>
+        <GeneFamily key={geneFamily.id} {...geneFamily} />
+      )}
+    </div>
+  )
+}
+
+const GeneFamily = ({ id, name, genes }) => {
+  return (
+    <div>
+      <h2>
+        {name}
+      </h2>
+      <ul>
+        {genes.map(gene => <Gene key={gene.id} {...gene} />)}
+      </ul>
+    </div>
+  )
+}
+
+const Gene = ({ id, name }) => {
+  return (
+    <li>
+      {name}
+    </li>
+  )
+}
+
 class App extends Component {
   render() {
-    const geneFamilies = this.props.geneFamilies
-    const geneCount = geneFamilies.reduce(
-      (count, family) => count + family.genes.length,
-      0
-    )
+    const { geneFamilies } = this.props
     return (
       <div>
-        I am going to be the new categories page, with {geneFamilies.length}{' '}
-        gene families and {geneCount} genes
+        <GeneFamilyNav geneFamilies={geneFamilies} />
+        <TAGPContent geneFamilies={geneFamilies} />
       </div>
     )
   }
