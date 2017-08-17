2b1da8e0d900638e64a1b071b3bc284ccc6dda14
Christopher Pappas <damassi.pappas@gmail.com>
2017-08-17 13:08:51 -0700

    Update /article2

--- desktop/apps/article2/server.js
+++
@@ -1,8 +0,0 @@
-import * as routes from './routes'
-import adminOnly from 'desktop/lib/admin_only.coffee'
-import express from 'express'
-
-const app = module.exports = express.Router()
-
-app.get('/article2/:slug/amp', adminOnly, routes.amp)
-app.get('/article2/:slug', adminOnly, routes.index)
