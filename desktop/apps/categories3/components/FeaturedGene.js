a2ac7fecf1285d755f6e0236b085f23244ec98bc
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-08-17 15:44:27 -0400

    Style the featured genes

--- desktop/apps/categories3/components/FeaturedGene.js
+++ desktop/apps/categories3/components/FeaturedGene.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import { primary } from '@artsy/reaction-force/dist/assets/fonts'
 
 const propTypes = {
   title: PropTypes.string,
@@ -7,14 +9,33 @@ const propTypes = {
   image: PropTypes.object
 }
 
+const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+`
+
+const GeneLink = styled.a`
+  position: absolute;
+  left: 1em;
+  bottom: 1em;
+  text-decoration: none;
+
+  color: white;
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.7);
+
+  ${primary.style} font-size: 13px;
+  line-height: 1.33em;
+  font-weight: bold;
+`
+
+const GeneImage = styled.img`width: 90%;`
+
 const FeaturedGene = ({ title, href, image: { url: imageSrc } }) => {
   return (
-    <div>
-      <a href={href}>
-        {title}
-      </a>
-      <img src={imageSrc} />
-    </div>
+    <Container>
+      <GeneLink href={href}>{title}</GeneLink>
+      <GeneImage src={imageSrc} />
+    </Container>
   )
 }
 
--- desktop/apps/categories3/components/FeaturedGenes.js
+++ desktop/apps/categories3/components/FeaturedGenes.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 import FeaturedGene from './FeaturedGene'
 
@@ -7,20 +8,27 @@ const propTypes = {
   featuredGenes: PropTypes.object
 }
 
+const Layout = styled.div`
+  padding-top: 1em;
+  column-count: 3;
+  column-gap: 2em;
+`
+
 const FeaturedGenes = ({ featuredGenes }) => {
   return (
-    <div>
+    <Layout>
       {featuredGenes
         ? featuredGenes.genes.length > 0
           ? featuredGenes.genes
               .map(featuredGene => <FeaturedGene key={featuredGene.id} {...featuredGene} />)
+              .slice(0, 3)
           : <p style={{ color: 'orange' }}>
               missing Featured Links?<br />(No featuredGenes.genes list)
             </p>
         : <p style={{ color: 'red' }}>
             missing Set?<br />(No featuredGenes object)
           </p>}
-    </div>
+    </Layout>
   )
 }
 
