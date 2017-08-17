a8dec068d8168a2ea861762a51c94182715b449c
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-08-16 22:17:18 -0400

    Introduced styling via Styled Components
    Currently this throws a console error:
    
    Warning: React attempted to reuse markup in a container but the checksum was invalid. This generally means that you are using server rendering and the markup generated on the server was not what the client was expecting. React injected new markup to compensate which works but you have lost many of the benefits of server rendering. Instead, figure out why the markup being generated is different on the client or server:
    
    (client) ="20"><li class="sc-bdVaJa dfQcku" data-
    (server) ="20"><li class="sc-htpNat ghdkrB" data-

--- desktop/apps/categories3/components/Gene.js
+++ desktop/apps/categories3/components/Gene.js
@@ -1,16 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import styled from 'styled-components'
 
 const propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired
 }
 
+const GeneLI = styled.li`
+  line-height: 2em;
+`
+const GeneA = styled.a`
+  text-decoration: none;
+
+  &:hover {
+    color: purple;
+  }
+`
 const Gene = ({ id, name }) => {
   return (
-    <li>
-      {name}
-    </li>
+    <GeneLI>
+      <GeneA href='#'>
+        {name}
+      </GeneA>
+    </GeneLI>
   )
 }
 
