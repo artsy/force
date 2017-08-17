2f67bfb4239ec96b57a71ffdb48d7cb3240f58c3
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-08-17 09:17:40 -0400

    Styled the sidebar menu
    Holding off on any refactoring of reusable styles just yet.

--- desktop/apps/categories3/components/GeneFamilyNav.js
+++ desktop/apps/categories3/components/GeneFamilyNav.js
@@ -7,7 +7,6 @@ const propTypes = {
 }
 
 const ResponsiveSidebar = styled.aside`
-  background: pink;
   display: none;
 
   @media (min-width: 768px) {
@@ -21,15 +20,37 @@ const GeneFamilyList = styled.ul`
   position: fixed;
   width: inherit;
   padding-right: 2em;
+
+  font-family: 'ITC Avant Garde Gothic W04', 'AvantGardeGothicITCW01D 731075',
+    'AvantGardeGothicITCW01Dm', 'Helvetica', 'sans-serif';
+  font-smoothing: antialiased;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+
+  font-size: 13px;
+  line-height: 1.33em;
 `
+
+const GeneFamilyItem = styled.li`margin-bottom: 1em;`
+
+const GeneFamilyLink = styled.a`
+  transition: color 0.125s;
+
+  &:hover {
+    color: #6e1fff;
+  }
+`
+
 const GeneFamilyNav = ({ geneFamilies }) => {
   return (
     <ResponsiveSidebar>
       <GeneFamilyList>
         {geneFamilies.map(geneFamily =>
-          <li key={geneFamily.id}>
-            {geneFamily.name}
-          </li>
+          <GeneFamilyItem key={geneFamily.id}>
+            <GeneFamilyLink>
+              {geneFamily.name}
+            </GeneFamilyLink>
+          </GeneFamilyItem>
         )}
       </GeneFamilyList>
     </ResponsiveSidebar>
