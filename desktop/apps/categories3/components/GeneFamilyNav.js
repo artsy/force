12f1a4d918e354f1d4dd09556508c63cb7a28b4f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-08-31 00:33:45 -0400

    Add scrollspying to gene family menu

--- desktop/apps/categories3/components/GeneFamilyNav.js
+++ desktop/apps/categories3/components/GeneFamilyNav.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
+import Scrollspy from 'react-scrollspy'
 
 import colors from '@artsy/reaction-force/dist/assets/colors'
 import { primary } from '@artsy/reaction-force/dist/assets/fonts'
@@ -21,17 +22,22 @@ const ResponsiveSidebar = styled.aside`
   }
 `
 
-const GeneFamilyList = styled.ul`
+const GeneFamilyList = styled(Scrollspy)`
   position: fixed;
   width: inherit;
   padding-right: 2em;
 
-  ${primary.style}
+  ${primary.style};
   font-size: 13px;
   line-height: 1.33em;
 `
 
-const GeneFamilyItem = styled.li`margin-bottom: 1em;`
+const GeneFamilyItem = styled.li`
+  margin-bottom: 1em;
+  &.is-current a {
+    color: ${colors.purpleRegular};
+  }
+`
 
 const GeneFamilyLink = styled.a`
   transition: color 0.125s;
@@ -70,7 +76,11 @@ class GeneFamilyNav extends React.Component {
     const { geneFamilies } = this.props
     return (
       <ResponsiveSidebar>
-        <GeneFamilyList>
+        <GeneFamilyList
+          items={geneFamilies.map(f => f.id)}
+          currentClassName='is-current'
+          offset={-90}
+        >
           {geneFamilies.map(geneFamily => (
             <GeneFamilyItem key={geneFamily.id}>
               <GeneFamilyLink
