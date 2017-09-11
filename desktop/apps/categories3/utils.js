5501438b99e8b8b69d22e2923f02bd6b77154f1e
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-11 17:31:05 -0400

    Refactor the frame animator
    Changing it from a higher-order function into a plain old class.
    This simplifies the API for calling code:
    
    const myAnimator = new FrameAnimator( value => { doSomethingWith(value) } )
    myAnimator.start()

--- desktop/apps/categories3/utils.js
+++ desktop/apps/categories3/utils.js
@@ -10,19 +10,33 @@ export const featuredGenesForFamily = (familyName, featuredGenesList) => {
   )
 }
 
-export const frameAnimator = (options, callback) => {
-  const { duration = 500, startValue = 0, endValue = 1 } = options
-  const startedAt = window.performance.now()
-
-  const animator = timestamp => {
-    const elapsed = timestamp - startedAt
-    if (elapsed <= duration) {
-      const t = elapsed / duration
-      const currentEasing = cubicInOut(t)
-      const currentValue = startValue + currentEasing * (endValue - startValue)
-      callback(currentValue)
-      window.requestAnimationFrame(animator)
-    }
+export class FrameAnimator {
+  constructor (callback, options = {}) {
+    const { duration = 500, startValue = 0, endValue = 1 } = options
+    this._animatorFunction = this._createAnimatorFunction(callback, {
+      duration,
+      startValue,
+      endValue
+    })
   }
-  return animator
+
+  _createAnimatorFunction (callback, { duration, startValue, endValue }) {
+    const startedAt = window.performance.now()
+    const animatorFunction = timestamp => {
+      const elapsed = timestamp - startedAt
+      if (elapsed <= duration) {
+        const t = elapsed / duration
+        const currentEasing = cubicInOut(t)
+        const currentValue =
+          startValue + currentEasing * (endValue - startValue)
+        callback(currentValue)
+        window.requestAnimationFrame(animatorFunction)
+      }
+    }
+    return animatorFunction
+  }
+
+  start () {
+    window.requestAnimationFrame(this._animatorFunction)
+  }
 }
--- desktop/apps/categories3/utils.test.js
+++ desktop/apps/categories3/utils.test.js
@@ -1,8 +1,8 @@
 import 'jsdom-global/register'
 import sinon from 'sinon'
-import { frameAnimator } from './utils'
+import { FrameAnimator } from './utils'
 
-describe('frameAnimator', () => {
+describe('FrameAnimator', () => {
   beforeEach(() => {
     window.requestAnimationFrame = () => {}
     window.performance = {
@@ -11,108 +11,100 @@ describe('frameAnimator', () => {
     window.performance.now.onCall(0).returns(0)
   })
 
-  it('returns an animator function', () => {
-    const animator = frameAnimator({}, () => {})
-    const returnedType = typeof animator
-    returnedType.should.equal('function')
+  it('fires the callback within the requested duration', () => {
+    const duration = 100
+    const timestampWithinDuration = 99
+    const callback = sinon.spy()
+
+    const animator = new FrameAnimator(callback, { duration })
+    animator._animatorFunction(timestampWithinDuration)
+
+    callback.callCount.should.equal(1)
   })
 
-  describe('the returned animator function', () => {
-    it('fires the callback within the requested duration', () => {
-      const duration = 100
-      const timestampWithinDuration = 99
-      const callback = sinon.spy()
+  it('does not fire the callback after the duration is up', () => {
+    const duration = 100
+    const timestampAfterDuration = 101
+    const callback = sinon.spy()
 
-      const animator = frameAnimator({ duration }, callback)
-      animator(timestampWithinDuration)
+    const animator = new FrameAnimator(callback, { duration })
+    animator._animatorFunction(timestampAfterDuration)
 
-      callback.callCount.should.equal(1)
+    callback.callCount.should.equal(0)
+  })
+
+  it('animates from the starting value', () => {
+    const duration = 100
+    const startValue = 42
+    const endValue = 420
+    const timestamp = 0
+    const callback = sinon.spy()
+
+    const animator = new FrameAnimator(callback, {
+      duration,
+      startValue,
+      endValue
+    })
+    animator._animatorFunction(timestamp)
+
+    callback.callCount.should.equal(1)
+    callback.getCall(0).args[0].should.equal(42)
+  })
+
+  it('animates to the ending value', () => {
+    const duration = 100
+    const startValue = 42
+    const endValue = 420
+    const timestamp = 100
+    const callback = sinon.spy()
+
+    const animator = new FrameAnimator(callback, {
+      duration,
+      startValue,
+      endValue
+    })
+    animator._animatorFunction(timestamp)
+
+    callback.callCount.should.equal(1)
+    callback.getCall(0).args[0].should.equal(420)
+  })
+
+  it('queues itself again with requestAnimationFrame', () => {
+    const duration = 100
+    const startValue = 42
+    const endValue = 420
+    const callback = sinon.spy()
+
+    // stub a series of hi-res timestamp calls for requestAnimationFrame
+    window.performance.now.onCall(1).returns(30) // re-queues
+    window.performance.now.onCall(2).returns(60) // re-queues
+    window.performance.now.onCall(3).returns(90) // re-queues
+    window.performance.now.onCall(4).returns(120) // finishes
+
+    // stub requestAnimationFrame itself
+    sinon.stub(window, 'requestAnimationFrame', fn => {
+      const currentTimestamp = window.performance.now()
+      fn(currentTimestamp)
     })
 
-    it('does not fire the callback after the duration is up', () => {
-      const duration = 100
-      const timestampAfterDuration = 101
-      const callback = sinon.spy()
-
-      const animator = frameAnimator({ duration }, callback)
-      animator(timestampAfterDuration)
-
-      callback.callCount.should.equal(0)
+    const animator = new FrameAnimator(callback, {
+      duration,
+      startValue,
+      endValue
     })
+    animator.start()
 
-    it('animates from the starting value', () => {
-      const duration = 100
-      const startValue = 42
-      const endValue = 420
-      const timestamp = 0
-      const callback = sinon.spy()
+    callback.callCount.should.equal(3)
+  })
 
-      const animator = frameAnimator(
-        { duration, startValue, endValue },
-        callback
-      )
-      animator(timestamp)
+  it('uses sensible defaults', () => {
+    const callback = sinon.spy()
+    const animator = new FrameAnimator(callback)
 
-      callback.callCount.should.equal(1)
-      callback.getCall(0).args[0].should.equal(42)
-    })
+    // animate from 0 to 1 in 500ms
+    animator._animatorFunction(0)
+    animator._animatorFunction(500)
 
-    it('animates to the ending value', () => {
-      const duration = 100
-      const startValue = 42
-      const endValue = 420
-      const timestamp = 100
-      const callback = sinon.spy()
-
-      const animator = frameAnimator(
-        { duration, startValue, endValue },
-        callback
-      )
-      animator(timestamp)
-
-      callback.callCount.should.equal(1)
-      callback.getCall(0).args[0].should.equal(420)
-    })
-
-    it('queues itself again with requestAnimationFrame', () => {
-      const duration = 100
-      const startValue = 42
-      const endValue = 420
-      const callback = sinon.spy()
-
-      // stub a series of hi-res timestamp calls for requestAnimationFrame
-      window.performance.now.onCall(1).returns(30) // re-queues
-      window.performance.now.onCall(2).returns(60) // re-queues
-      window.performance.now.onCall(3).returns(90) // re-queues
-      window.performance.now.onCall(4).returns(120) // finishes
-
-      // stub requestAnimationFrame itself
-      sinon.stub(window, 'requestAnimationFrame', fn => {
-        const currentTimestamp = window.performance.now()
-        fn(currentTimestamp)
-      })
-
-      const animator = frameAnimator(
-        { duration, startValue, endValue },
-        callback
-      )
-      window.requestAnimationFrame(animator)
-
-      callback.callCount.should.equal(3)
-    })
-
-    it('uses sensible defaults', () => {
-      const callback = sinon.spy()
-      const animator = frameAnimator(
-        {},
-        callback
-      )
-
-      // animate from 0 to 1 in 500ms
-      animator(0)
-      animator(500)
-
-      callback.args.should.match([[0], [1]])
-    })
+    callback.args.should.match([[0], [1]])
   })
 })
