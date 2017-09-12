be0e9f9104f0a62f1c1c9d812d1a83aa75770c9d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-09-12 10:46:49 -0400

    Move description from inline jsdoc to README, with screencaps

--- desktop/components/frame_animator/index.js
+++ desktop/components/frame_animator/index.js
@@ -1,37 +1,5 @@
 import * as easingFunctions from './easing'
 
-/**
- * Class representing an animatable value and a callback that acts on it.
- *
- * Can be used to transition any value over any duration, but particularly
- * useful in animating UI elements that can't be handled via CSS transitions.
- *
- * Uses the Window.requestAnimationFrame API in order to be as smooth and
- * CPU-friendly as possible.
- *
- * Example 1: Log out values from 0 to 1, over a half-second interval.
- *
- * const myLogger = new FrameAnimator(value => console.log(value))
- * myLogger.start()
- *
- * Example 2: Animate the window scroll position over 3/4 of a second,
- * easing out with a sine function.
- *
- * const startingScrollPosition = 0
- * const endingScrollPosition = 1000
- * const durationInMilliseconds = 750
- * const easingFunction = 'sinOut'
- *
- * const myAnimator = new FrameAnimator(value => window.scrollTo(0,value), {
- *   startValue: startingScrollPosition,
- *   endValue: endingScrollPosition,
- *   duration: durationInMilliseconds,
- *   easing: easingFunction
- * })
- *
- * myAnimator.start()
- *
- */
 export default class FrameAnimator {
   /**
    * Create a frame animator
