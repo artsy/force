9bd04b2dcab47aecd4aec8b44a7a992b0653c569
Christopher Pappas <damassi.pappas@gmail.com>
2017-09-20 19:54:00 -0700

    Add NODE_ENV=production compilation config

--- desktop/webpack/components/App.js
+++ desktop/webpack/components/App.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        Hi!
+        hi
       </div>
     )
   }
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
+const ProgressBarPlugin = require('progress-bar-webpack-plugin')
+const WebpackNotifierPlugin = require('webpack-notifier')
+const path = require('path')
 const webpack = require('webpack')
 
-module.exports = {
+const { NODE_ENV } = process.env
+
+const config = {
   devtool: 'eval',
   entry: {
     app: [
-      'webpack-hot-middleware/client',
       './desktop/webpack/index.js'
     ]
   },
   output: {
     filename: '[name].js',
-    publicPath: '/public/assets'
+    path: path.resolve(__dirname, 'public/assets'),
+    publicPath: '/public/assets',
+    sourceMapFilename: '[name].js.map'
   },
   module: {
     rules: [
@@ -22,7 +29,16 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new FriendlyErrorsWebpackPlugin(),
+    new ProgressBarPlugin(),
+    new WebpackNotifierPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify(NODE_ENV)
+      }
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('commons.chunk')
   ],
   resolve: {
     extensions: ['.js', '.jsx'],
@@ -31,3 +47,28 @@ module.exports = {
     ]
   }
 }
+
+const isDevelopment = NODE_ENV === 'development'
+const isStaging = NODE_ENV === 'staging'
+const isProduction = NODE_ENV === 'production'
+const isDeploy = isStaging || isProduction
+
+if (isDevelopment) {
+  config.entry.app.push('webpack-hot-middleware/client')
+  config.plugins.push(new webpack.HotModuleReplacementPlugin())
+
+  // Staging
+} else if (isDeploy) {
+  config.devtool = '#source-map'
+
+  // Prod
+  if (isProduction) {
+    config.plugins.push(
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: true
+      })
+    )
+  }
+}
+
+module.exports = config
