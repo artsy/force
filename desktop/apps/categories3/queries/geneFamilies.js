0540d29fd328953727b0c3a5694c8b04b62081ae
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-10-06 21:28:17 -0400

    Prefer display name over name

--- desktop/apps/categories3/queries/geneFamilies.js
+++ desktop/apps/categories3/queries/geneFamilies.js
@@ -7,6 +7,7 @@ export default function GeneFamiliesQuery() {
       genes {
         id
         name
+        display_name
       }
     }
   }
--- desktop/apps/categories3/utils.js
+++ desktop/apps/categories3/utils.js
@@ -1,6 +1,6 @@
 import _ from 'underscore'
 
-export const alphabetizeGenes = genes => _.sortBy(genes, gene => gene.name)
+export const alphabetizeGenes = genes => _.sortBy(genes, gene => (gene.display_name || gene.name))
 
 export const featuredGenesForFamily = (familyName, featuredGenesList) => {
   return _.find(
