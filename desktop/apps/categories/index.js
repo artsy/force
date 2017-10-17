4079691cd4e4b94ccd44fea8c2adfb5304e4a510
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2017-10-17 17:28:44 -0400

    Remove references to categories3

--- desktop/apps/categories/index.js
+++ desktop/apps/categories/index.js
@@ -8,4 +8,4 @@ const app = module.exports = express()
 app.set('view engine', 'jade')
 app.set('views', `${__dirname}/components`)
 
-app.get('/categories3', adminOnly, routes.index)
+app.get('/categories', adminOnly, routes.index)
