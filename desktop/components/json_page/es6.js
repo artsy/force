02877106005cc98d06c394db21fdb32268fcc76e
Yuki Nishijima <yk.nishijima@gmail.com>
2017-10-19 16:02:03 -0400

    Set up landing page

---
+++ desktop/components/json_page/es6.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import JsonPage from './index'
+import routes from './routes'
+
+export default class JSONPage {
+  constructor(options) {
+    // TODO: Convert this class to TypeScript so you can take advantage of its type system
+    if (this.registerRoutes === undefined) {
+      throw new TypeError("registerRoutes() must be implemented.")
+    }
+
+    this.jsonPage = new JsonPage(options)
+    this.routes = routes(this.jsonPage)
+    this.app = express()
+    this.registerRoutes()
+  }
+
+  get data() {
+    return this.routes.data
+  }
+
+  get edit() {
+    return this.routes.edit
+  }
+
+  get upload() {
+    return this.routes.upload
+  }
+}
