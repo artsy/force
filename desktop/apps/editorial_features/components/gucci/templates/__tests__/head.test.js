4f4c2f9afbcbabb2f9bcb21ded78895bfa66a8c8
kanaabe <kanakoabe5@gmail.com>
2017-11-09 14:58:48 -0500

    Adds test

---
+++ desktop/apps/editorial_features/components/gucci/templates/__tests__/head.test.js
@@ -0,0 +1,75 @@
+import * as _ from 'underscore'
+import path from 'path'
+import jade from 'jade'
+import fs from 'fs'
+import Curation from 'desktop/models/curation.coffee'
+
+const render = (templateName) => {
+  const filename = path.resolve(__dirname, `../${templateName}.jade`)
+  return jade.compile(fs.readFileSync(filename), { filename })
+}
+
+describe('Meta template', () => {
+  it('contains basic meta tags on the landing page', () => {
+    const curation = {
+      name: 'Artists for Gender Equality',
+      type: 'editorial-feature',
+      sections: [],
+      social_title: 'What Happened in the Past?',
+      social_description: 'A series of films optimized for social media',
+      email_title: 'Good Morning, What Happened?',
+      email_author: 'Artsy Editors',
+      email_tags: 'magazine,article,gucci',
+      keywords: 'women,gender,equality',
+      thumbnail_image: 'http://thumbnail.jpg',
+      email_image: 'http://emailimage.jpg',
+      social_image: 'http://socialimage.jpg'
+    }
+    const html = render('head')({
+      crop: url => url,
+      sd: {
+        SUB_PATH: '',
+        CURATION: curation,
+        APP_URL: 'https://artsy.net'
+      }
+    })
+    html.should.containEql('<title>Artists for Gender Equality</title>')
+    html.should.containEql('<meta name="description" content="A series of films optimized for social media"/>')
+    html.should.containEql('<meta name="keywords" content="women,gender,equality"/>')
+    html.should.containEql('<meta name="sailthru.title" content="Good Morning, What Happened?"/><meta name="sailthru.image.full" content="http://emailimage.jpg"/><meta name="sailthru.image.thumb" content="http://emailimage.jpg"/><meta name="sailthru.author" content="Artsy Editors"/><meta name="sailthru.tags" content="magazine,article,gucci"/>')
+  })
+
+  it('contains basic meta tags on a nested route', () => {
+    const curation = new Curation({
+      name: 'Artists for Gender Equality',
+      type: 'editorial-feature',
+      sections: [{
+        title: 'I. PAST',
+        social_title: 'What Happened in the Past?',
+        social_description: 'A series of films optimized for social media',
+        email_title: 'Good Morning, What Happened?',
+        email_author: 'Artsy Editors',
+        email_tags: 'magazine,article,gucci',
+        keywords: 'women,gender,equality',
+        thumbnail_image: 'http://thumbnail.jpg',
+        email_image: 'http://emailimage.jpg',
+        social_image: 'http://socialimage.jpg'
+      }]
+    })
+    const html = render('head')({
+      curation,
+      crop: url => url,
+      sd: {
+        SUB_PATH: '/past',
+        CURATION: curation.toJSON(),
+        APP_URL: 'https://artsy.net',
+        VIDEO_INDEX: 0
+      }
+    })
+    html.should.containEql('<title>I. PAST</title>')
+    html.should.containEql('<link rel="canonical" href="https://artsy.net/gender-equality/past"/>')
+    html.should.containEql('<meta name="description" content="A series of films optimized for social media"/>')
+    html.should.containEql('<meta name="keywords" content="women,gender,equality"/>')
+    html.should.containEql('<meta name="sailthru.title" content="Good Morning, What Happened?"/><meta name="sailthru.image.full" content="http://emailimage.jpg"/><meta name="sailthru.image.thumb" content="http://emailimage.jpg"/><meta name="sailthru.author" content="Artsy Editors"/><meta name="sailthru.tags" content="magazine,article,gucci"/>')
+  })
+})
