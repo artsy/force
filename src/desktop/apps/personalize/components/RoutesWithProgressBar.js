115267eccf6931cfcc7c77a93589102b60c5ff7f
Christopher Pappas <damassi.pappas@gmail.com>
2018-02-20 14:50:28 -0800

    Cleanup rebase

--- src/desktop/apps/personalize/components/RoutesWithProgressBar.js
+++ src/desktop/apps/personalize/components/RoutesWithProgressBar.js
@@ -11,7 +11,7 @@ const STEPS = [
   `/personalize/${CollectorIntent.slug}`,
   `/personalize/${Artists.slug}`,
   `/personalize/${Genes.slug}`,
-  `/personalize/${Budget.slug}`,
+  `/personalize/${Budget.slug}`
 ]
 
 export class RoutesWithProgressBar extends React.Component {
@@ -26,75 +26,32 @@ export class RoutesWithProgressBar extends React.Component {
     history.push(STEPS[STEPS.indexOf(location.pathname) + increaseBy])
   }
 
-  onFinish = (redirectTo) => {
+  onFinish = redirectTo => {
     this.setState({ finished: true })
-    setTimeout(() => (window.location.href = redirectTo || '/'), 500)
+    setTimeout(() => (window.location.href = redirectTo || "/"), 500)
   }
 
   render() {
     return (
       <div>
-        <Route
-          path="/personalize/*"
-          render={() => (
-            <ProgressIndicator
-              percentComplete={
-                this.state.finished
-                  ? 1
-                  : STEPS.indexOf(location.pathname) / STEPS.length
-              }
-            />
-          )}
-        />
+        <Route path='/personalize/*' render={() =>
+          <ProgressIndicator percentComplete={ this.state.finished ? 1 : STEPS.indexOf(location.pathname) / STEPS.length } />
+        } />
 
-        <Route
-          path={`/personalize/${CollectorIntent.slug}`}
-          render={(props) => (
-            <CollectorIntent
-              {...props}
-              onNextButtonPressed={(increaseBy = 1) =>
-                this.onNextButtonPressed(increaseBy, props.history)
-              }
-            />
-          )}
-        />
-        <Route
-          path={`/personalize/${Artists.slug}`}
-          render={(props) => (
-            <Artists
-              {...props}
-              onNextButtonPressed={(increaseBy = 1) =>
-                this.onNextButtonPressed(increaseBy, props.history)
-              }
-            />
-          )}
-        />
-        <Route
-          path={`/personalize/${Genes.slug}`}
-          render={(props) => (
-            <Genes
-              {...props}
-              onNextButtonPressed={(increaseBy = 1) =>
-                this.onNextButtonPressed(increaseBy, props.history)
-              }
-            />
-          )}
-        />
-        <Route
-          path={`/personalize/${Budget.slug}`}
-          render={(props) => (
-            <Budget
-              {...props}
-              onNextButtonPressed={(increaseBy = 1) =>
-                this.onFinish(props.redirectTo, props.history)
-              }
-            />
-          )}
-        />
+        <Route path={`/personalize/${CollectorIntent.slug}`} render={props =>
+          <CollectorIntent {...props} onNextButtonPressed={(increaseBy = 1) => this.onNextButtonPressed(increaseBy, props.history)} />
+        } />
+        <Route path={`/personalize/${Artists.slug}`} render={props =>
+          <Artists {...props} onNextButtonPressed={(increaseBy = 1) => this.onNextButtonPressed(increaseBy, props.history)} />
+        } />
+        <Route path={`/personalize/${Genes.slug}`} render={props =>
+          <Genes {...props} onNextButtonPressed={(increaseBy = 1) => this.onNextButtonPressed(increaseBy, props.history)} />
+        } />
+        <Route path={`/personalize/${Budget.slug}`} render={props =>
+          <Budget {...props} onNextButtonPressed={(increaseBy = 1) => this.onFinish(props.redirectTo, props.history)} />
+        } />
 
-        {new RegExp('/personalize(/*)$').exec(location.pathname) && (
-          <Redirect to={`/personalize/${CollectorIntent.slug}`} />
-        )}
+        {new RegExp("/personalize(/*)$").exec(location.pathname) && <Redirect to={`/personalize/${CollectorIntent.slug}`} />}
       </div>
     )
   }
