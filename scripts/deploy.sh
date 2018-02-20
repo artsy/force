a231e4e3d1a2024c414bb3f84f8be1d5baf81fc2
Christopher Pappas <damassi.pappas@gmail.com>
2018-02-20 14:59:21 -0800

    Fix path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,8 @@
 set -e -x
 
 yarn assets
-gzip -S .cgz $(find src/public/assets -name '*.css')
-gzip -S .jgz $(find src/public/assets -name '*.js')
+gzip -S .cgz $(find public/assets -name '*.css')
+gzip -S .jgz $(find public/assets -name '*.js')
 bucket-assets --bucket artsy-force-$DEPLOY_ENV
 heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=force-$DEPLOY_ENV
 if [ -z "$CIRCLE_SHA1" ]; then
