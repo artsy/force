27a05b2a9abf064873f9274deb914b63b4bcf121
kanaabe <kanakoabe5@gmail.com>
2018-02-21 16:02:05 -0500

    Use destination cookie in new onboarding

--- src/desktop/apps/personalize/client/onboarding.js
+++ src/desktop/apps/personalize/client/onboarding.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from 'react-router'
 import createHistory from 'history/createBrowserHistory'
+import Cookies from 'cookies-js'
 
 import { ContextProvider } from '@artsy/reaction/dist/Components/Artsy'
 import { Wizard } from '@artsy/reaction/dist/Components/Onboarding/Wizard'
@@ -9,11 +10,17 @@ import { Wizard } from '@artsy/reaction/dist/Components/Onboarding/Wizard'
 export const init = () => {
   const bootstrapData = window.__BOOTSTRAP__
 
+  // Get the path we should redirect to after onboarding
+  const destination = Cookies.get('destination')
+  if (destination) {
+    Cookies.expire('destination')
+  }
+
   // Start app
   ReactDOM.hydrate(
     <Router history={createHistory()}>
       <ContextProvider {...bootstrapData}>
-        <Wizard />
+        <Wizard redirectTo={destination} />
       </ContextProvider>
     </Router>,
     document.getElementById('react-root')
