61e72374c39a0d8474a3a8522dca6dd25a97b8b9
eriks <erik@artsymail.com>
2018-02-28 18:07:04 -0500

    create segment key for server events

--- src/desktop/analytics/account_login.js
+++ src/desktop/analytics/account_login.js
@@ -3,21 +3,21 @@
 //
 
 // Viewed login form
-var trackViewLogin = function () {
+var trackViewLogin = function() {
   analytics.track('Viewed login form')
 }
-analyticsHooks.on('mediator:open:auth', function (options) {
+analyticsHooks.on('mediator:open:auth', function(options) {
   if (options.mode === 'login') trackViewLogin()
 })
 $('#auth-footer [href*=log_in]').click(trackViewLogin)
 
 // Clicked login via the header
-$('.mlh-login').click(function () {
+$('.mlh-login').click(function() {
   analytics.track('Clicked login via the header')
 })
 
 // Login: The password you entered is incorrect.
-analyticsHooks.on('mediator:auth:error', function (message) {
+analyticsHooks.on('mediator:auth:error', function(message) {
   if (message === 'invalid email or password') {
     analytics.track('Login: The password you entered is incorrect.')
   }
