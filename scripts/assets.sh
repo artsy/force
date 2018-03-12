79c51aecdfad7c38a9409c4e8f4093b42c558d06
Christopher Pappas <damassi.pappas@gmail.com>
2018-03-12 14:27:42 -0700

    Import $ in main_layout

--- scripts/assets.sh
+++ scripts/assets.sh
@@ -5,7 +5,8 @@ set -e -x
 rm -rf public
 mkdir public
 mkdir public/assets
-NODE_ENV=production webpack
+# NODE_ENV=production webpack
+NODE_ENV=development webpack
 stylus \
   $(find src/desktop/assets src/mobile/assets -name '*.styl') \
   --compress \
--- src/desktop/assets/main_layout.coffee
+++ src/desktop/assets/main_layout.coffee
@@ -1,3 +1,4 @@
+$ = require('jquery')
 CurrentUser = require '../models/current_user.coffee'
 
 $ ->
