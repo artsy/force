87eb0e304b56a9ca3fd90f0bd981558d05be2a36
Sarah Weir <sweir27@gmail.com>
2018-03-12 10:26:05 -0400

    adds tracking for pageload speed

--- src/desktop/analytics/main_layout.js
+++ src/desktop/analytics/main_layout.js
@@ -3,27 +3,66 @@
 // or any other actions that occur on each page.
 //
 
+import { data as sd } from 'sharify'
+
 // Track pageview
-analytics.page({path: location.pathname}, {integrations: {'Marketo': false}})
+analytics.page(
+  { path: location.pathname },
+  { integrations: { Marketo: false } }
+)
+
+// Track pageload speed
+if (
+  window.performance &&
+  window.performance.timing &&
+  sd.TRACK_PAGELOAD_PATHS
+) {
+  window.addEventListener('load', function() {
+    _.each(sd.TRACK_PAGELOAD_PATHS.split('|'), (path) => {
+      if (window.location.pathname.split('/')[1] === path) {
+        window.setTimeout(function() {
+          const {
+            requestStart,
+            loadEventEnd,
+            domComplete,
+          } = window.performance.timing
+
+          analytics.track('Page load time', {
+            requestStart,
+            loadEventEnd,
+            domComplete,
+          })
+        }, 0)
+      }
+    })
+  })
+}
 
 // Track 15 second bounce rate
-setTimeout(function () {
-  analytics.track('time on page more than 15 seconds', { category: '15 Seconds', message: sd.CURRENT_PATH })
+setTimeout(function() {
+  analytics.track('time on page more than 15 seconds', {
+    category: '15 Seconds',
+    message: sd.CURRENT_PATH,
+  })
 }, 15000)
 
 // Track 3 Minute bounce rate
-setTimeout(function () {
-  analytics.track('time on page more than 3 minutes', { category: '3 Minutes', message: sd.CURRENT_PATH })
+setTimeout(function() {
+  analytics.track('time on page more than 3 minutes', {
+    category: '3 Minutes',
+    message: sd.CURRENT_PATH,
+  })
 }, 180000)
 
 // debug tracking calls in development
 if (sd.NODE_ENV !== 'production') {
-  analytics.on('track', function () {
+  analytics.on('track', function() {
     console.info('TRACKED: ', arguments[0], JSON.stringify(arguments[1]))
   })
 }
+
 if (sd.NODE_ENV === 'development') {
-  analyticsHooks.on('all', function (name, data) {
+  analyticsHooks.on('all', function(name, data) {
     console.info('ANALYTICS HOOK: ', name, data)
   })
 }
--- src/mobile/analytics/global.js
+++ src/mobile/analytics/global.js
@@ -2,12 +2,13 @@
 // Analytics that occur globaly on every page. Think if there's a better place
 // before you add to this file.
 //
-analyticsHooks.on('track', function (message, options) {
+
+analyticsHooks.on('track', function(message, options) {
   analytics.track(message, options)
 })
 
 // Track 15 second bounce rate
-setTimeout(function () {
+setTimeout(function() {
   analytics.track('time on page more than 15 seconds', {
     category: '15 Seconds',
     message: sd.CURRENT_PATH,
@@ -15,7 +16,7 @@ setTimeout(function () {
 }, 15000)
 
 // Track 30 second bounce rate
-setTimeout(function () {
+setTimeout(function() {
   analytics.track('time on page more than 30 seconds', {
     category: '30 Seconds',
     message: sd.CURRENT_PATH,
@@ -23,13 +24,13 @@ setTimeout(function () {
 }, 180000)
 
 // Debug tracking calls in development
-if (sd.NODE_ENV != 'production') {
-  analytics.on('track', function () {
+if (sd.NODE_ENV !== 'production') {
+  analytics.on('track', function() {
     console.debug('TRACKED: ', arguments[0], JSON.stringify(arguments[1]))
   })
 }
-if (sd.NODE_ENV == 'development') {
-  analyticsHooks.on('all', function (name, data) {
+if (sd.NODE_ENV === 'development') {
+  analyticsHooks.on('all', function(name, data) {
     console.info('ANALYTICS HOOK: ', name, data)
   })
 }
