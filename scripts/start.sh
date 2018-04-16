4fd784010a6850d91d07e3fe25ec21771fabcd27
Stephen Olsen <steve.olsen@gmail.com>
2018-04-16 13:10:13 -0400

    drop forever

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,9 +7,3 @@ if [ ! -f "./.env" ]; then
 fi
 
 export $(cat .env | grep NODE_ENV | xargs)
-
-if [ "$NODE_ENV" = "development" ]; then
-  node -r dotenv/config --max_old_space_size=1024 ./src
-else
-  forever -c 'node -r dotenv/config --max_old_space_size=1024' ./src --colors
-fi
