e899ef49bec3825121c97f3fc922008ad04ac189
Eve Essex <eve.essex@gmail.com>
2018-04-24 15:57:39 -0400

    use q for promises

--- src/desktop/apps/rss/routes.js
+++ src/desktop/apps/rss/routes.js
@@ -2,6 +2,7 @@ import request from 'superagent'
 import { data as sd } from 'sharify'
 import Articles from '../../collections/articles'
 const PAGE_SIZE = 50
+import Q from 'bluebird-q'
 
 export const news = (req, res, next) =>
   new Articles().fetch({
@@ -13,9 +14,9 @@ export const news = (req, res, next) =>
       limit: PAGE_SIZE,
     },
     error: res.backboneError,
-    success: async (articles) => {
+    success: async articles => {
       try {
-        const newArticles = findArticlesWithEmbeds(articles.models)
+        const newArticles = await findArticlesWithEmbeds(articles.models)
         res.set('Content-Type', 'application/rss+xml')
         return res.render('news', { articles: newArticles, pretty: true })
       } catch (err) {
@@ -33,48 +34,58 @@ export const partnerUpdates = (req, res, next) =>
       limit: PAGE_SIZE,
     },
     error: res.backboneError,
-    success: (articles) => {
+    success: articles => {
       res.set('Content-Type', 'application/rss+xml')
       return res.render('partner_updates', { articles, pretty: true })
     },
   })
 
-const findArticlesWithEmbeds = (articles) => {
-  const newArticles = articles.map((article, i) => {
-    const newSections = findSocialEmbeds(article)
-    article.set('sections', newSections)
-    return article
+const findArticlesWithEmbeds = articles => {
+  return Q.all(
+    articles.map(async (article, i) => {
+      const newSections = await findSocialEmbeds(article)
+      article.set('sections', newSections)
+      return article
+    })
+  ).then(res => {
+    return new Articles(res)
   })
-  return new Articles(newArticles)
 }
 
-const findSocialEmbeds = (article) => {
-  return article.get('sections').map(async (section, index) => {
-    console.log(section.type)
-    if (section.type !== 'social_embed') {
-      return section
-    } else {
+const findSocialEmbeds = article => {
+  return Q.all(
+    article.get('sections').map(async (section, index) => {
       try {
-        const newSection = await fetchSocialEmbed(section)
+        const newSection = await maybeFetchSocialEmbed(section)
         return newSection
       } catch (err) {
         console.error(err)
       }
-    }
+    })
+  ).then(res => {
+    return res
   })
 }
 
-const fetchSocialEmbed = (section) => {
-  const { url } = section
-  const service = url.includes('twitter') ? 'publish.twitter' : 'api.instagram'
+const maybeFetchSocialEmbed = section => {
+  return new Promise((resolve, reject) => {
+    if (section.type !== 'social_embed') {
+      return resolve(section)
+    } else {
+      const { url } = section
+      const service = url.includes('twitter')
+        ? 'publish.twitter'
+        : 'api.instagram'
 
-  request
-    .get(`https://${service}.com/oembed?url=${url}`)
-    .end(function(err, res) {
-      if (err) {
-        console.log(err)
-      }
-      section.url = res.body.html
-      return section
-    })
+      request
+        .get(`https://${service}.com/oembed?url=${url}`)
+        .end(function(err, res) {
+          if (err) {
+            reject(err)
+          }
+          section.url = res.body.html
+          return reject(section)
+        })
+    }
+  })
 }
