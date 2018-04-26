56ca3ba7931093037fcbf98bfe8797e71fc26fa2
Stephen Olsen <steve.olsen@gmail.com>
2018-04-26 13:01:12 -0400

    new workflow

--- scripts/test.sh
+++ scripts/test.sh
@@ -10,9 +10,6 @@ run () {
     yarn assets
     ;;
   1)
-    yarn acceptance src/test/acceptance/*.js
-    ;;
-  2)
     yarn mocha src/test/lib/*
     yarn mocha $(find src/desktop/test -name '*.coffee')
     yarn mocha $(find src/desktop/components/*/test -name '*.coffee')
@@ -21,13 +18,13 @@ run () {
     yarn mocha $(find src/desktop/components/**/*/test -name '*.js')
     yarn mocha $(find src/desktop/components -name '*.test.js')
     ;;
-  3)
+  2)
     yarn mocha $(find src/desktop/apps/*/test -name '*.coffee')
     yarn mocha $(find src/desktop/apps/*/test -name '*.js')
     yarn mocha $(find src/desktop/apps/*/**/*/test -name '*.coffee')
     yarn mocha $(find src/desktop/apps -name '*.test.js')
     ;;
-  4)
+  3)
     yarn mocha $(find src/mobile/test -name '*.coffee')
     yarn mocha $(find src/mobile/components/*/test -name '*.coffee')
     yarn mocha $(find src/mobile/components/**/*/test -name '*.coffee')
@@ -43,7 +40,6 @@ if [ -z "$CIRCLE_NODE_INDEX" ]; then
   CIRCLE_NODE_INDEX=1 run
   CIRCLE_NODE_INDEX=2 run
   CIRCLE_NODE_INDEX=3 run
-  CIRCLE_NODE_INDEX=4 run
 else
   run
 fi
