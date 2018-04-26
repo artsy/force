569e84ba0fb9eb8cabe89719bab75ef1d52429da
Stephen Olsen <steve.olsen@gmail.com>
2018-04-26 12:12:19 -0400

    still not a working script

--- scripts/hokusai_test.sh
+++ scripts/hokusai_test.sh
@@ -2,8 +2,11 @@
 
 set -e -x
 
-install_packages libnss3 libgconf-2-4 xvfb xauth
+# install java 8
+install_packages libnss3 libgconf-2-4 xvfb xauth openjdk-8-jre openjdk-8-jre-headless openjdk-8-jdk openjdk-8-jdk-headless ca-certificates-java
 
+## install phantomjs
+#
 PHANTOMJS_URL="https://circle-downloads.s3.amazonaws.com/circleci-images/cache/linux-amd64/phantomjs-latest.tar.bz2" \
   && apt-get install libfontconfig \
   && curl --silent --show-error --location --fail --retry 3 --output /tmp/phantomjs.tar.bz2 ${PHANTOMJS_URL} \
@@ -12,12 +15,27 @@ PHANTOMJS_URL="https://circle-downloads.s3.amazonaws.com/circleci-images/cache/l
   && rm -rf /tmp/phantomjs.tar.bz2 /tmp/phantomjs-* \
   && phantomjs --version
 
-curl --silent --show-error --location --fail --retry 3 --output /tmp/google-chrome-stable_current_amd64.deb https://dl.google.com/linux/chrome/deb/pool/main/g/google-chrome-stable/google-chrome-stable_64.0.3282.186-1_amd64.deb \
-      && (dpkg -i /tmp/google-chrome-stable_current_amd64.deb || apt-get -fy install)  \
-      && rm -rf /tmp/google-chrome-stable_current_amd64.deb \
-      && sed -i 's|HERE/chrome"|HERE/chrome" --disable-setuid-sandbox --no-sandbox|g' \
-           "/opt/google/chrome/google-chrome" \
-      && google-chrome --version
+# install firefox
+
+# If you are upgrading to any version newer than 47.0.1, you must check the compatibility with
+# selenium. See https://github.com/SeleniumHQ/selenium/issues/2559#issuecomment-237079591
+install_packages libgtk3.0-cil-dev libasound2 libasound2 libdbus-glib-1-2 libdbus-1-3
+
+FIREFOX_URL="https://s3.amazonaws.com/circle-downloads/firefox-mozilla-build_47.0.1-0ubuntu1_amd64.deb" \
+  && curl --silent --show-error --location --fail --retry 3 --output /tmp/firefox.deb $FIREFOX_URL \
+  && echo 'ef016febe5ec4eaf7d455a34579834bcde7703cb0818c80044f4d148df8473bb  /tmp/firefox.deb' | sha256sum -c \
+  && dpkg -i /tmp/firefox.deb \
+  && rm -rf /tmp/firefox.deb \
+  && firefox --version
+
+# install chrome
+# Note: Revert back to https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+# after browser testing works in google-chrome-stable 65.0.x.y
+curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - \
+  && echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list \
+  && install_packages libnss3 libgconf-2-4 google-chrome-stable \
+  && sed -i 's|HERE/chrome"|HERE/chrome" --disable-setuid-sandbox --no-sandbox|g' "/opt/google/chrome/google-chrome" \
+  && google-chrome --version
 
 export CHROMEDRIVER_RELEASE=$(curl --location --fail --retry 3 http://chromedriver.storage.googleapis.com/LATEST_RELEASE) \
       && curl --silent --show-error --location --fail --retry 3 --output /tmp/chromedriver_linux64.zip "http://chromedriver.storage.googleapis.com/$CHROMEDRIVER_RELEASE/chromedriver_linux64.zip" \
@@ -31,4 +49,5 @@ export CHROMEDRIVER_RELEASE=$(curl --location --fail --retry 3 http://chromedriv
 cd /app
 
 export DISPLAY=:99
-Xvfb :99 -screen 0 1280x1024x24 & exec sh scripts/test.sh
+Xvfb :99 -screen 0 1280x1024x24 &
+exec bash scripts/test.sh
