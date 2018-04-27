82ae700eb9bdbdad3dbd9513f5dddbad72cc21f6
Christopher Pappas <damassi.pappas@gmail.com>
2018-04-26 19:12:31 -0700

    WIP: Spike on isomorphic relay

--- src/desktop/apps/react_example/client.js
+++ src/desktop/apps/react_example/client.js
@@ -1,12 +1,15 @@
+import { data as sd } from 'sharify'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import { TestComponent } from 'reaction/Components/TestComponent'
 
 export default () => {
   const bootstrapData = window.__BOOTSTRAP__
 
-  ReactDOM.hydrate(
-    <App {...bootstrapData} />,
-    document.getElementById('react-root')
-  )
+  // ReactDOM.hydrate(
+  //   <TestComponent {...sd.relay} />,
+  //   // <App {...bootstrapData} />,
+  //   document.getElementById('react-root')
+  // )
 }
--- src/desktop/apps/react_example/routes.js
+++ src/desktop/apps/react_example/routes.js
@@ -1,14 +1,36 @@
+import React from 'react'
+import { createEnvironment } from 'reaction/Relay/createEnvironment'
+import { TestComponent, query } from 'reaction/Components/TestComponent'
 import App from 'desktop/apps/react_example/components/App'
 import { renderLayout } from '@artsy/stitch'
+import { fetchQuery } from 'react-relay'
 
 export async function index(req, res, next) {
   try {
+    const relayEnvironment = createEnvironment({
+      endpoint: process.env.METAPHYSICS_ENDPOINT,
+    })
+
+    const variables = {
+      artistID: 'andy-warhol',
+    }
+
+    const data = await fetchQuery(relayEnvironment, query, variables)
+    res.locals.sharify.data.relay = data
+
     const layout = await renderLayout({
       basePath: req.app.get('views'),
       layout: '../../../components/main_layout/templates/react_index.jade',
       blocks: {
         head: 'meta.jade',
-        body: App,
+        body: props => {
+          return <TestComponent relayEnvironment={relayEnvironment} {...data} />
+          // return (
+          //   <ContextProvider variables={>
+          //     <App {...props} />,
+          //   </ContextProvider>
+          // )
+        },
       },
       locals: {
         ...res.locals,
@@ -18,9 +40,9 @@ export async function index(req, res, next) {
         name: 'Leif',
         description: 'hello hi how are you',
       },
-      templates: {
-        MyJadeView: 'myJadeView.jade',
-      },
+      // templates: {
+      //   MyJadeView: 'myJadeView.jade',
+      // },
     })
 
     res.send(layout)
