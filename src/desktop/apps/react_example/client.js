a64b31a6d28b9082bd5d1a8d064184eb3a06f738
Christopher Pappas <damassi.pappas@gmail.com>
2018-04-26 23:31:51 -0700

    wip

--- src/desktop/apps/react_example/client.js
+++ src/desktop/apps/react_example/client.js
@@ -2,14 +2,22 @@ import { data as sd } from 'sharify'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
+import { getRelayEnvironment } from 'reaction/Relay/createEnvironment'
 import { TestComponent } from 'reaction/Components/TestComponent'
 
 export default () => {
   const bootstrapData = window.__BOOTSTRAP__
+  const relayEnvironment = getRelayEnvironment(
+    sd.relay.records,
+    sd.CURRENT_USER
+  )
 
-  // ReactDOM.hydrate(
-  //   <TestComponent {...sd.relay} />,
-  //   // <App {...bootstrapData} />,
-  //   document.getElementById('react-root')
-  // )
+  ReactDOM.hydrate(
+    <TestComponent
+      relayEnvironment={relayEnvironment}
+      currentUser={sd.CURRENT_USER}
+    />,
+    // <App {...bootstrapData} />,
+    document.getElementById('react-root')
+  )
 }
--- src/desktop/apps/react_example/routes.js
+++ src/desktop/apps/react_example/routes.js
@@ -1,14 +1,16 @@
 import React from 'react'
+import App from 'desktop/apps/react_example/components/App'
+import CurrentUser from 'lib/current_user.coffee'
 import { createEnvironment } from 'reaction/Relay/createEnvironment'
 import { TestComponent, query } from 'reaction/Components/TestComponent'
-import App from 'desktop/apps/react_example/components/App'
 import { renderLayout } from '@artsy/stitch'
 import { fetchQuery } from 'react-relay'
 
 export async function index(req, res, next) {
   try {
+    const user = req.user.toJSON()
     const relayEnvironment = createEnvironment({
-      endpoint: process.env.METAPHYSICS_ENDPOINT,
+      user,
     })
 
     const variables = {
@@ -16,15 +18,27 @@ export async function index(req, res, next) {
     }
 
     const data = await fetchQuery(relayEnvironment, query, variables)
-    res.locals.sharify.data.relay = data
+
+    res.locals.sharify.data.relay = {
+      records: relayEnvironment.getStore().getSource(),
+    }
 
     const layout = await renderLayout({
       basePath: req.app.get('views'),
       layout: '../../../components/main_layout/templates/react_index.jade',
+      config: {
+        styledComponents: true,
+      },
       blocks: {
         head: 'meta.jade',
         body: props => {
-          return <TestComponent relayEnvironment={relayEnvironment} {...data} />
+          return (
+            <TestComponent
+              relayEnvironment={relayEnvironment}
+              currentUser={user}
+              {...data}
+            />
+          )
           // return (
           //   <ContextProvider variables={>
           //     <App {...props} />,
