e913f2ad3bb9c0383285aff15be27e958f4f574a
kanaabe <kanakoabe5@gmail.com>
2018-04-30 18:44:44 -0400

    Adds more work

--- src/desktop/apps/auth2/routes.js
+++ src/desktop/apps/auth2/routes.js
@@ -2,7 +2,22 @@ import { renderLayout } from '@artsy/stitch'
 import { App } from './components/App.tsx'
 
 export const index = async (req, res, next) => {
-  const mode = req.path.includes('login') ? 'login' : 'register'
+  let mode
+  switch (req.path) {
+    case '/login':
+      mode = 'login'
+      break
+    case '/signup':
+      mode = 'register'
+      break
+    case '/reset_password':
+      mode = 'reset_password'
+      break
+    default:
+      mode = 'login'
+      break
+  }
+
   try {
     const layout = await renderLayout({
       basePath: __dirname,
