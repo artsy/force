cf7531211563b13f53f227413b21917bf96c7eb5
Matthew Zikherman <matt@bestvendor.com>
2018-05-04 15:28:39 -0400

    Add some analytics on /gallery-partnerships links

--- src/desktop/analytics/gallery_partnerships.js
+++ src/desktop/analytics/gallery_partnerships.js
@@ -1,15 +1,28 @@
-(function () {
+;(function() {
   'use strict'
 
-  $(document).on('click', '.gallery-partnerships2-white-signup-button, .gallery-partnerships2-black-signup-button', function (e) {
-    var $button = $(e.currentTarget)
+  $(document).on(
+    'click',
+    '.gallery-partnerships2-white-signup-button, .gallery-partnerships2-black-signup-button',
+    function(e) {
+      var $button = $(e.currentTarget)
 
+      analytics.track('Click', {
+        type: 'button',
+        label: $button.data('label'),
+        flow: 'partnership application',
+        context_module: $button.data('section'),
+        destination_path: null,
+      })
+    }
+  )
+
+  $(document).on('click', '.gallery-partnership-link', function(e) {
     analytics.track('Click', {
-      type: 'button',
-      label: $button.data('label'),
-      flow: 'partnership application',
-      context_module: $button.data('section'),
-      destination_path: null
+      type: 'link',
+      flow: 'gallery application',
+      destination_path: 'https://www.artsy.net/gallery-partnerships',
+      context_module: $(e.currentTarget).data('context'),
     })
   })
 })()
