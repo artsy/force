60ba3ec9aa20bc8bf9bdf4f4e7afce905db662df
Matthew Zikherman <matt@bestvendor.com>
2018-05-09 16:27:52 -0400

    Bundle + upload assets during heroku Circle 2.0 deploys, and source maps too

--- .circleci/heroku_deploy.sh
+++ .circleci/heroku_deploy.sh
@@ -1,5 +1,6 @@
 #! /bin/bash -ex
 
+DEPLOY_ENV=staging yarn deploy
 if git remote | grep heroku > /dev/null; then
   git fetch heroku
   git push --force heroku $CIRCLE_SHA1:master
@@ -8,3 +9,4 @@ else
   echo "Heroku is not set up :("
   exit 1
 fi 
+DEPLOY_ENV=staging yarn sentry
