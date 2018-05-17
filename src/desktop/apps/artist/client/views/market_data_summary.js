643801ece35655a271e771fa6b1d31d86bb239c0
oxaudo <oksana@artsymail.com>
2018-05-17 14:12:51 -0400

    move rendering of marketData summary for the artist to happen earlier in page load

--- src/desktop/apps/artist/client/views/market_data_summary.js
+++ src/desktop/apps/artist/client/views/market_data_summary.js
@@ -1,9 +1,11 @@
+import $ from 'jquery'
+
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Contents } from 'reaction/Components/Artist/MarketDataSummary'
 import { ContextProvider } from 'reaction/Components/Artsy'
 
-function renderArtistMarketDataSummary(artistID) {
+function renderArtistMarketDataSummaryFor(artistID) {
   const elementID = 'market-data-summary-container-' + artistID
   const mountPoint = document.getElementById(elementID)
 
@@ -17,4 +19,10 @@ function renderArtistMarketDataSummary(artistID) {
   }
 }
 
-export default { renderArtistMarketDataSummary }
+function setupArtistMarketDataSummary() {
+  $('.market-data-summary[data-artist-id]').each((i, el) => {
+    renderArtistMarketDataSummaryFor($(el).data('artist-id'))
+  })
+}
+
+export default { setupArtistMarketDataSummary }
