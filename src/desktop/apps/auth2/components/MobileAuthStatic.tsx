308d3209a36d8fdcfcaf7957310d48c51093466d
xtina-starr <c.thompsonms@gmail.com>
2018-06-14 18:24:48 -0400

    wire up submit functionality for mobile

--- src/desktop/apps/auth2/components/MobileAuthStatic.tsx
+++ src/desktop/apps/auth2/components/MobileAuthStatic.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import Colors from '@artsy/reaction/dist/Assets/Colors'
 import { FormSwitcher } from '@artsy/reaction/dist/Components/Authentication/Mobile/FormSwitcher'
+import { handleSubmit } from '../helpers'
 import { ModalType } from '@artsy/reaction/dist/Components/Authentication/Types'
-// import { MobileSignUpForm } from '@artsy/reaction/dist/Components/Authentication/Mobile/SignUpForm'
 
 interface Props {
   type: string
@@ -14,20 +14,11 @@ export class MobileAuthStatic extends React.Component<Props> {
   render() {
     return (
       <AuthFormContainer>
-        {/* <MobileContainer>
-          <MobileSignUpForm
-            values={{}}
-            handleSubmit={() => null}
-            handleTypeChange={() => mode => null}
-          />
-        </MobileContainer> */}
         <MobileContainer>
           <FormSwitcher
             {...this.props}
             type={this.props.type as ModalType}
-            handleSubmit={() => {
-              console.log('here.')
-            }}
+            handleSubmit={handleSubmit.bind(this, this.props.type)}
           />
         </MobileContainer>
       </AuthFormContainer>
