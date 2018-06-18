3e9aea6aa99698c0620ee4b9e3b594a1e4fc3832
Eve Essex <eve.essex@gmail.com>
2018-06-18 15:12:11 -0400

    remove commented code

--- src/desktop/apps/auth2/index.ts
+++ src/desktop/apps/auth2/index.ts
@@ -1,6 +1,5 @@
 import express from 'express'
 import { index } from './routes'
-// const { twitterLastStepPath } = require('@artsy/passport').options
 const app = (module.exports = express())
 
 app.set('view engine', 'jade')
@@ -9,4 +8,3 @@ app.set('views', `${__dirname}/templates`)
 app.get('/login', index)
 app.get('/signup', index)
 app.get('/forgot', index)
-// app.get(twitterLastStepPath, routes.twitterLastStep)
