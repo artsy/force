6ab65aa11e0f41e9b94b2e233ed853cae444a5ba
Eve Essex <eve.essex@gmail.com>
2018-06-19 14:18:24 -0400

    tests, move modal container into its own component, move setCookies to helpers

--- src/desktop/apps/auth2/client/index.tsx
+++ src/desktop/apps/auth2/client/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Cookies from 'cookies-js'
-import { AuthStatic } from 'desktop/apps/auth2/components/AuthStatic'
-import { MobileAuthStatic } from 'desktop/apps/auth2/components/MobileAuthStatic'
+import { AuthStatic } from '../components/AuthStatic'
+import { MobileAuthStatic } from '../components/MobileAuthStatic'
+import { ModalContainer } from '../components/ModalContainer'
 import { ModalManager } from '@artsy/reaction/dist/Components/Authentication/Desktop/ModalManager'
-import { handleSubmit } from '../helpers'
+import { handleSubmit, setCookies } from '../helpers'
 import { data as sd } from 'sharify'
 
 const mediator = require('../../../lib/mediator.coffee')
@@ -26,50 +27,7 @@ export const init = () => {
 export const initModalManager = () => {
   const el = document.getElementById('react-modal-container')
 
-  const Container: React.SFC<any> = () => {
-    let manager: ModalManager | null
-
-    mediator.on('open:auth', options => {
-      setCookies(options)
-
-      // TODO: remember to swap 'register' with 'signup' in triggers
-      if (options.mode === 'register') {
-        options.mode = 'signup'
-      }
-
-      if (manager) {
-        manager.openModal(options)
-      }
-    })
-
-    return (
-      <ModalManager
-        ref={ref => (manager = ref)}
-        submitUrls={{
-          login: sd.AP.loginPagePath,
-          signup: sd.AP.signupPagePath,
-        }}
-        csrf={sd.CSRF_TOKEN}
-        handleSubmit={handleSubmit}
-      />
-    )
-  }
-
   if (el) {
-    ReactDOM.render(<Container />, el)
-  }
-}
-
-export const setCookies = options => {
-  const { afterSignUpAction, destination } = options
-
-  if (afterSignUpAction) {
-    Cookies.set('afterSignUpAction', JSON.stringify(afterSignUpAction))
-  }
-
-  if (destination) {
-    Cookies.set('destination', destination, {
-      expires: 60 * 60 * 24,
-    })
+    ReactDOM.render(<ModalContainer />, el)
   }
 }
