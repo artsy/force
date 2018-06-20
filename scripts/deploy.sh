65d95af7b504f7f64a3a51720d59c4ad4435ba84
kanaabe <kanakoabe5@gmail.com>
2018-06-20 10:51:39 -0400

    Restore deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ yarn assets
 gzip -S .cgz $(find public/assets -name '*.css')
 gzip -S .jgz $(find public/assets -name '*.js')
 bucket-assets --bucket artsy-force-$DEPLOY_ENV
-heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$HEROKU_APP
+heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=force-$DEPLOY_ENV
 if [ -z "$CIRCLE_SHA1" ]; then
-  git push --force git@heroku.com:$HEROKU_APP.git master
+  git push --force heroku master
 else
   git push --force heroku $CIRCLE_SHA1:refs/heads/master
 fi
