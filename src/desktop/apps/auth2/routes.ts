6ad2d182056985d1b4c498a91e4b3da51e98b749
Eve Essex <eve.essex@gmail.com>
2018-06-20 14:34:43 -0400

    component tests

--- src/desktop/apps/auth2/routes.ts
+++ src/desktop/apps/auth2/routes.ts
@@ -1,7 +1,7 @@
 import { renderLayout } from '@artsy/stitch'
 import { AuthStatic } from './components/AuthStatic'
 import { ModalType } from '@artsy/reaction/dist/Components/Authentication/Types'
-import { AuthenticationMeta } from './meta'
+import { AuthenticationMeta } from './components/meta'
 import { MobileAuthStatic } from './components/MobileAuthStatic'
 
 export const index = async (req, res, next) => {
