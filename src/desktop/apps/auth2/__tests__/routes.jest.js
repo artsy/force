e8aa8af526e0e4367367019acc04d9c9832b0bb3
Eve Essex <eve.essex@gmail.com>
2018-06-20 13:44:58 -0400

    tests routes resetPassword

--- src/desktop/apps/auth2/__tests__/routes.jest.js
+++ src/desktop/apps/auth2/__tests__/routes.jest.js
@@ -12,24 +12,35 @@ describe('Routes', () => {
   let res
   let next
 
-  beforeEach(() => {
-    req = {
-      path: '/',
-      query: {},
-    }
-    res = {
-      locals: {
-        sd: {
-          IS_MOBILE: false,
-        },
-      },
-      send: jest.fn(),
-    }
-    next = jest.fn()
-    renderLayout.mockReset()
-  })
-
   describe('#index', () => {
+    beforeEach(() => {
+      req = {
+        path: '/',
+        query: {},
+      }
+      res = {
+        locals: {
+          sd: {
+            IS_MOBILE: false,
+          },
+        },
+        send: jest.fn(),
+      }
+      next = jest.fn()
+      renderLayout.mockReset()
+    })
+
+    it('calls next if #renderLayout returns an error', done => {
+      renderLayout.mockImplementationOnce(() => {
+        throw new Error('A new error')
+      })
+
+      index(req, res, next).then(() => {
+        expect(next).toHaveBeenCalled()
+        done()
+      })
+    })
+
     describe('Component', () => {
       it('Returns AuthStatic component if UA is desktop', done => {
         index(req, res, next).then(() => {
@@ -114,6 +125,7 @@ describe('Routes', () => {
           signupIntent: 'follow artist',
           signupReferer: 'referrer',
         }
+
         index(req, res, next).then(() => {
           const {
             afterSignUpAction,
@@ -135,8 +147,72 @@ describe('Routes', () => {
   })
 
   describe('#resetPassword', () => {
-    it('does a thing', () => {
-      // resetPassword(req, res)
+    beforeEach(() => {
+      res = {
+        locals: {
+          sd: {
+            IS_MOBILE: false,
+          },
+        },
+        redirect: jest.fn(),
+        render: jest.fn(),
+      }
+      req = {
+        query: {},
+        session: {},
+      }
+    })
+
+    describe('Has reset_password_token', () => {
+      beforeEach(() => {
+        req.query = {
+          reset_password_token: 'foobar',
+          reset_password_redirect_to: '/articles',
+          set_password: 'set password',
+        }
+      })
+
+      it('matches session params to query params', () => {
+        const {
+          reset_password_token,
+          reset_password_redirect_to,
+          set_password,
+        } = req.query
+        resetPassword(req, res)
+
+        expect(req.session.reset_password_token).toBe(reset_password_token)
+        expect(req.session.reset_password_redirect_to).toBe(
+          reset_password_redirect_to
+        )
+        expect(req.session.set_password).toBe(set_password)
+      })
+
+      it('redirects to /reset_password', () => {
+        resetPassword(req, res)
+        expect(res.redirect.mock.calls[0][0]).toBe('/reset_password')
+      })
+    })
+
+    describe('Without reset_password_token', () => {
+      beforeEach(() => {
+        req.session = {
+          reset_password_token: 'foobar',
+          reset_password_redirect_to: '/articles',
+          set_password: 'set password',
+        }
+      })
+
+      it('sets sd.RESET_PASWORD_REDIRECT_TO', () => {
+        resetPassword(req, res)
+        expect(res.locals.sd.RESET_PASWORD_REDIRECT_TO).toBe('/articles')
+      })
+
+      it('renders reset_password with expected args', () => {
+        resetPassword(req, res)
+        expect(res.render.mock.calls[0][0]).toBe('reset_password')
+        expect(res.render.mock.calls[0][1].reset_password_token).toBe('foobar')
+        expect(res.render.mock.calls[0][1].set_password).toBe('set password')
+      })
     })
   })
 })
