25310a26ade3cda5d7d4793781ed467b42a64d99
kanaabe <kanakoabe5@gmail.com>
2018-06-20 10:45:05 -0400

    Bump reaction

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,9 +6,9 @@ yarn assets
 gzip -S .cgz $(find public/assets -name '*.css')
 gzip -S .jgz $(find public/assets -name '*.js')
 bucket-assets --bucket artsy-force-$DEPLOY_ENV
-heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=force-$DEPLOY_ENV
+heroku config:set ASSET_MANIFEST=$(cat manifest.json) --app=$HEROKU_APP
 if [ -z "$CIRCLE_SHA1" ]; then
-  git push --force heroku master
+  git push --force git@heroku.com:$HEROKU_APP.git master
 else
   git push --force heroku $CIRCLE_SHA1:refs/heads/master
 fi
