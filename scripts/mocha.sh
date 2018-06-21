76a481629f640e9408f4d182baf1bab015f5656e
Chung-Yi Chi <cychi1210@gmail.com>
2018-06-21 16:04:29 -0400

    Fix scripts not exiting properly in CI; refactor (#2638)
    * Fix incorrect shebangs in shell scripts
    
    * Use consistent `/bin/bash` in header for running scripts
    
    While using `/usr/bin/env bash` might be more portable, I would prefer
    not take security risks. If we run scripts as executables, not found
    /bin/bash will break things early and we can fix.
    
    * Use consistent shell set builtin in scripts
    
    It seems like the -e and -x are portable in other shells. Let's move it
    out of the the header so it will still be set when the scripts are run
    by a shell command.
    
    * Always run shell scripts as executables
    
    * Fix syntax error in deploy script

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -1,6 +1,6 @@
-# !/usr/bin/bash
+#! /bin/bash
 
-set -e -x
+set -ex
 
 #trap "exit" INT
 
--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
-# !/usr/bin/bash
+#! /bin/bash
 
-set -e -x
+set -ex
 
 if [ ! -f "./.env" ]; then
   echo -e "\033[1;31m WARNING: Missing .env file, see CONTRIBUTING.md. \033[0m"
@@ -8,8 +8,8 @@ fi
 
 export $(cat .env | grep NODE_ENV | xargs)
 
-if [ "$NODE_ENV" = "development" ]; then	
-  node -r dotenv/config --max_old_space_size=1024 ./src	
-else	
+if [ "$NODE_ENV" = "development" ]; then
+  node -r dotenv/config --max_old_space_size=1024 ./src
+else
   node --max_old_space_size=1024 ./src
 fi
