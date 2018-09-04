64158ae2ae06395c2c5f09da7d12c99737014ce5
Eve Essex <eve.essex@gmail.com>
2018-09-04 13:09:37 -0400

    add makesOffer object

--- src/desktop/apps/artist2/components/__tests__/Meta.jest.js
+++ src/desktop/apps/artist2/components/__tests__/Meta.jest.js
@@ -17,6 +17,31 @@ describe('Meta', () => {
       large:
         'https://d32dm0rphc51dk.cloudfront.net/6q6LeyKvA_vpT5YzHRSNUA/large.jpg',
     },
+    artworks_connection: {
+      edges: [
+        {
+          node: {
+            title:
+              "'25 Years Studio',  1993, SIGNED by the BIG-8 Contemporary Artists, Gemini G.E.L.",
+            availability: 'for sale',
+            description: null,
+            category: 'Drawing, Collage or other Work on Paper',
+            price_currency: 'USD',
+            is_price_range: false,
+            href:
+              '/artwork/robert-rauschenberg-25-years-studio-1993-signed-by-the-big-8-contemporary-artists-gemini-gel',
+            image: {
+              small:
+                'https://d32dm0rphc51dk.cloudfront.net/PmBrn30fGmg9dGwk2Nf51w/small.jpg',
+            },
+            partner: {
+              name: 'VINCE fine arts/ephemera',
+              href: '/vince-fine-arts-slash-ephemera',
+            },
+          },
+        },
+      ],
+    },
   }
 
   describe('#toJSONLD', () => {
@@ -40,6 +65,36 @@ describe('Meta', () => {
           '@type': 'Country',
           name: 'Swedish',
         },
+        makesOffer: [
+          {
+            '@type': 'Offer',
+            availability: 'InStock',
+            itemOffered: {
+              '@type': 'Product',
+              additionalType: 'Drawing, Collage or other Work on Paper',
+              brand: {
+                '@type': 'Person',
+                name: 'Claes Oldenburg',
+              },
+              image: {
+                '@type': 'ImageObject',
+                thumbnailUrl:
+                  'https://d32dm0rphc51dk.cloudfront.net/PmBrn30fGmg9dGwk2Nf51w/small.jpg',
+              },
+              name:
+                "'25 Years Studio',  1993, SIGNED by the BIG-8 Contemporary Artists, Gemini G.E.L.",
+              url:
+                'https://artsy.net/artwork/robert-rauschenberg-25-years-studio-1993-signed-by-the-big-8-contemporary-artists-gemini-gel',
+            },
+            priceCurrency: 'USD',
+            seller: {
+              '@context': 'http://schema.org',
+              '@type': 'ArtGallery',
+              name: 'VINCE fine arts/ephemera',
+              url: 'https://artsy.net/vince-fine-arts-slash-ephemera',
+            },
+          },
+        ],
       })
     })
 
