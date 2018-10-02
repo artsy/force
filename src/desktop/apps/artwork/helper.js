7df8a1f5cbe860c15dcbe6e96877308fe3f3206b
xtina-starr <c.thompsonms@gmail.com>
2018-10-02 12:03:20 -0400

    [Structured Data] Add missing properties

--- src/desktop/apps/artwork/helper.js
+++ src/desktop/apps/artwork/helper.js
@@ -1,14 +1,41 @@
-import { pickBy, identity } from "lodash"
+import { identity, map, pickBy, trim } from "lodash"
+import { toSentence } from "underscore.string"
 const { APP_URL } = require("sharify").data
+
+const AVAILABILITY = {
+  "for sale": "https://schema.org/InStock",
+  sold: "https://schema.org/OutOfStock",
+}
 
 export const compactObject = o => {
   return pickBy(o, identity)
+}
+
+const displayPrice = artwork => {
+  const { is_price_hidden, is_price_range, price, sale_message } = artwork
+
+  if (is_price_range && !is_price_hidden) {
+    const minAndMaxPrice = price.split("-")
+    return {
+      minPrice: trim(minAndMaxPrice[0]).replace("$", ""),
+      maxPrice: trim(minAndMaxPrice[1]),
+    }
+  } else {
+    return {
+      price: sale_message,
+    }
+  }
 }
 
 export const convertArtworkToJSONLD = artwork => {
   let jsonLD
   const imageUrl =
     artwork.images && artwork.images.length ? artwork.images[0].url : ""
+
+  const artistsName =
+    artwork.artists.length && toSentence(map(artwork.artists, "name"))
+
+  const partnerName = artwork.partner && artwork.partner.name
 
   const artworkMetaData = {
     name: artwork.title,
@@ -18,6 +45,10 @@ export const convertArtworkToJSONLD = artwork => {
     depth: artwork.depth,
     width: artwork.width,
     height: artwork.height,
+    brand: {
+      "@type": "Person",
+      name: artistsName,
+    },
   }
 
   if (artwork.partner.type === "Institution") {
@@ -35,10 +66,13 @@ export const convertArtworkToJSONLD = artwork => {
       productionDate: artwork.date,
       offers: compactObject({
         "@type": "Offer",
-        price: artwork.price,
+        ...displayPrice(artwork),
         priceCurrency: artwork.price_currency, // not available in mp
-        availability: artwork.availability,
-        seller: artwork.partner.name,
+        availability: AVAILABILITY[artwork.availability],
+        seller: {
+          "@type": "ArtGallery",
+          name: partnerName,
+        },
       }),
     })
   }
