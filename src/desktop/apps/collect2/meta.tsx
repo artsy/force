2fd8b4527c4723f2bdaaca4091263f754470514b
xtina-starr <c.thompsonms@gmail.com>
2018-10-11 15:00:14 -0400

    add custom collect metadata

--- src/desktop/apps/collect2/meta.tsx
+++ src/desktop/apps/collect2/meta.tsx
@@ -1,15 +1,20 @@
 import React, { Fragment } from "react"
+import { capitalize } from "underscore.string"
 
 interface Props {
   appUrl: string
+  medium?: string
 }
 
 export const Meta = (props: Props) => {
-  const { appUrl } = props
+  const { appUrl, medium } = props
+
   // TODO: custom page title / description based on params
-  const title = "Collect | Artsy"
-  const description =
-    "Find artworks by subject matter, style/technique, movement, price, and gallery/institution."
+  const title = medium
+    ? `${capitalize(medium)} for Sale | Collect on Artsy`
+    : "Collect | Artsy"
+  const description = `Buy, bid, and inquire on ${medium ||
+    "artworks"} on Artsy, the world’s largest online marketplace for art and design.`
 
   return (
     <Fragment>
