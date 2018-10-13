bef6938dd2f7baa60d3557be472513944b1ba2c2
Christopher Pappas <damassi.pappas@gmail.com>
2018-10-13 14:42:30 -0700

    [Mobile Artwork] Stitch in ArtistInfo

--- src/mobile/lib/global_react_modules.js
+++ src/mobile/lib/global_react_modules.js
@@ -1,2 +1,7 @@
-import { ReactionArtworkDetails } from "../../desktop/components/react/stitch_components/ReactionArtworkDetails"
-export const ArtworkDetails = ReactionArtworkDetails
+export {
+  ReactionArtworkDetails as ArtworkDetails,
+} from "desktop/components/react/stitch_components/ReactionArtworkDetails"
+
+export {
+  ReactionArtworkArtistInfo as ArtworkArtistInfo,
+} from "desktop/components/react/stitch_components/ReactionArtworkArtistInfo"
