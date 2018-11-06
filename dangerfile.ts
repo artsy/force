f75e9a1ac322dca4c6f479a1e237b8f66f82e1a6
Orta Therox <orta.therox@gmail.com>
2018-11-06 18:50:53 -0500

    Test the danger integration with prod

--- dangerfile.ts
+++ dangerfile.ts
@@ -14,29 +14,29 @@ export default async () => {
   }
 
   // Breaking change check for Metaphysics production when deploying
-  if (danger.github.pr.base.ref === "release") {
-    const breakingChanges = await getBreakingChanges()
-    const bc = breakingChanges
-    const typesMissing = bc.filter(b => b.type === "TYPE_REMOVED")
-    const fieldsMissing = bc.filter(b => b.type === "FIELD_REMOVED")
-    const unionOptionsMissing = bc.filter(
-      b => b.type === "TYPE_REMOVED_FROM_UNION"
-    )
-    const fieldChanged = bc.filter(b => b.type === "FIELD_CHANGED_KIND")
+  // if (danger.github.pr.base.ref === "release") {
+  const breakingChanges = await getBreakingChanges()
+  const bc = breakingChanges
+  const typesMissing = bc.filter(b => b.type === "TYPE_REMOVED")
+  const fieldsMissing = bc.filter(b => b.type === "FIELD_REMOVED")
+  const unionOptionsMissing = bc.filter(
+    b => b.type === "TYPE_REMOVED_FROM_UNION"
+  )
+  const fieldChanged = bc.filter(b => b.type === "FIELD_CHANGED_KIND")
 
-    const descriptions = {
-      "Missing types": typesMissing,
-      "Fields missing": fieldsMissing,
-      "Fields changed": fieldChanged,
-      "Union types-mismatch": unionOptionsMissing,
-    }
-
-    Object.keys(descriptions).forEach(key => {
-      const breakingChanges: BreakingChange[] = descriptions[key]
-      if (breakingChanges.length) {
-        const fields = breakingChanges.map(b => b.description.split(" ")[0])
-        fail(`${key}: ${danger.utils.sentence(fields)}`)
-      }
-    })
+  const descriptions = {
+    "Missing types": typesMissing,
+    "Fields missing": fieldsMissing,
+    "Fields changed": fieldChanged,
+    "Union types-mismatch": unionOptionsMissing,
   }
+
+  Object.keys(descriptions).forEach(key => {
+    const breakingChanges: BreakingChange[] = descriptions[key]
+    if (breakingChanges.length) {
+      const fields = breakingChanges.map(b => b.description.split(" ")[0])
+      fail(`${key}: ${danger.utils.sentence(fields)}`)
+    }
+  })
+  // }
 }
