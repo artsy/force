f2cab46bee7e63d905087bf5e0ee37412c9a1f21
Orta Therox <orta.therox@gmail.com>
2018-11-06 21:21:44 -0500

    Adds travis for Danger

--- dangerfile.ts
+++ dangerfile.ts
@@ -5,7 +5,9 @@ import { BreakingChange } from "graphql"
 // tslint:disable-next-line:no-default-export
 export default async () => {
   // Warn about creating new JS files
-  const jsFiles = danger.git.created_files.filter(f => f.endsWith(".js"))
+  const jsFiles = danger.git.created_files.filter(
+    f => f.includes("src") && f.endsWith(".js")
+  )
   if (jsFiles.length) {
     const files = danger.github.utils.fileLinks(jsFiles)
     warn(
@@ -14,29 +16,37 @@ export default async () => {
   }
 
   // Breaking change check for Metaphysics production when deploying
-  // if (danger.github.pr.base.ref === "release") {
-  const breakingChanges = await getBreakingChanges()
-  const bc = breakingChanges
-  const typesMissing = bc.filter(b => b.type === "TYPE_REMOVED")
-  const fieldsMissing = bc.filter(b => b.type === "FIELD_REMOVED")
-  const unionOptionsMissing = bc.filter(
-    b => b.type === "TYPE_REMOVED_FROM_UNION"
-  )
-  const fieldChanged = bc.filter(b => b.type === "FIELD_CHANGED_KIND")
+  if (danger.github.pr.base.ref === "release") {
+    const breakingChanges = await getBreakingChanges()
+    const bc = breakingChanges
+    const typesMissing = bc.filter(b => b.type === "TYPE_REMOVED")
+    const fieldsMissing = bc.filter(b => b.type === "FIELD_REMOVED")
+    const unionOptionsMissing = bc.filter(
+      b => b.type === "TYPE_REMOVED_FROM_UNION"
+    )
+    const fieldChanged = bc.filter(b => b.type === "FIELD_CHANGED_KIND")
 
-  const descriptions = {
-    "Missing types": typesMissing,
-    "Fields missing": fieldsMissing,
-    "Fields changed": fieldChanged,
-    "Union types-mismatch": unionOptionsMissing,
-  }
-
-  Object.keys(descriptions).forEach(key => {
-    const breakingChanges: BreakingChange[] = descriptions[key]
-    if (breakingChanges.length) {
-      const fields = breakingChanges.map(b => b.description.split(" ")[0])
-      fail(`${key}: ${danger.utils.sentence(fields)}`)
+    const descriptions = {
+      "Missing types": typesMissing,
+      "Fields missing": fieldsMissing,
+      "Fields changed": fieldChanged,
+      "Union types-mismatch": unionOptionsMissing,
     }
-  })
-  // }
+
+    if (bc.length) {
+      fail(
+        "Metaphysics production does not have a compatible schema for force."
+      )
+    }
+
+    Object.keys(descriptions).forEach(key => {
+      const breakingChanges: BreakingChange[] = descriptions[key]
+      if (breakingChanges.length) {
+        const fields = breakingChanges.map(
+          b => "`" + b.description.split(" ")[0] + "`"
+        )
+        fail(`${key}: ${danger.utils.sentence(fields)}`)
+      }
+    })
+  }
 }
--- scripts/validate_schemas.js
+++ scripts/validate_schemas.js
@@ -2,6 +2,10 @@
 
 // Grab the schema for reaction out of the node_modules
 // to see if it is a subset of the production schema
+//
+// Used both by Danger during the deploy PR, and also
+// before the deployment on circle
+//
 const { readFileSync } = require("fs")
 const {
   introspectionQuery,
@@ -39,10 +43,31 @@ const downloadSchema = async endpoint => {
   return printSchema(buildClientSchema(data), { commentDescriptions: true })
 }
 
-export const getBreakingChanges = async () => {
+const getBreakingChanges = async () => {
   const metaphyiscSchema = await downloadSchema(metaphysicsProd)
   return findBreakingChanges(
     buildSchema(metaphyiscSchema),
     buildSchema(reactionSchema)
   )
+}
+
+module.exports = {
+  getBreakingChanges,
+}
+
+// @ts-ignore
+if (require.main === module) {
+  // When this is being called as a script via `node scripts/validate_schemas.js`
+  getBreakingChanges().then(changes => {
+    if (changes.length) {
+      process.exitCode = 1
+      console.error(
+        "Failing due to breaking changes between Force and Metaphysics Production\n\n"
+      )
+      console.error(changes)
+      console.error(
+        "\n\nYou should deploy metaphysics production, and re-deploy force"
+      )
+    }
+  })
 }
