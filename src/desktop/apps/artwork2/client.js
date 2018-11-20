325b9dcd096e06079cb28ba79d4f9c0e5d21a2a7
oxaudo <oksana@artsymail.com>
2018-11-20 16:41:48 -0500

    add mediator events for asking auction specialist and buy now specialist for artwork2

--- src/desktop/apps/artwork2/client.js
+++ src/desktop/apps/artwork2/client.js
@@ -2,6 +2,48 @@ import { buildClientApp } from "reaction/Artsy/Router/client"
 import { routes } from "reaction/Apps/Artwork/routes"
 import React from "react"
 import ReactDOM from "react-dom"
+import mediator from "desktop/lib/mediator.coffee"
+import User from "desktop/models/user.coffee"
+import Artwork from "desktop/models/artwork.coffee"
+import ArtworkInquiry from "desktop/models/artwork_inquiry.coffee"
+import openInquiryQuestionnaireFor from "desktop/components/inquiry_questionnaire/index.coffee"
+
+mediator.on("openBuyNowAskSpecialistModal", options => {
+  const artworkId = options.artworkId
+  if (artworkId) {
+    const user = User.instantiate()
+    const inquiry = new ArtworkInquiry({ notification_delay: 600 })
+    const artwork = new Artwork({ id: artworkId })
+
+    artwork.fetch().then(() => {
+      openInquiryQuestionnaireFor({
+        user,
+        artwork,
+        inquiry,
+        ask_specialist: true,
+      })
+    })
+  }
+})
+
+mediator.on("openAuctionAskSpecialistModal", options => {
+  const artworkId = options.artworkId
+  if (artworkId) {
+    const user = User.instantiate()
+    const inquiry = new ArtworkInquiry({ notification_delay: 600 })
+    const artwork = new Artwork({ id: artworkId })
+
+    artwork.fetch().then(() => {
+      artwork.set("is_in_auction", true)
+      openInquiryQuestionnaireFor({
+        user,
+        artwork,
+        inquiry,
+        ask_specialist: true,
+      })
+    })
+  }
+})
 
 buildClientApp({ routes })
   .then(({ ClientApp }) => {
