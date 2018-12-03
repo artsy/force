1fac3689aa645d2a8ab5ff9d816e31bf839e0278
Christopher Pappas <damassi.pappas@gmail.com>
2018-12-03 12:03:16 -0800

    Fix mobile stitch issue

--- src/mobile/lib/global_react_modules.js
+++ src/mobile/lib/global_react_modules.js
@@ -1,7 +1,1 @@
-export {
-  ArtworkDetailsQueryRenderer as ArtworkDetails,
-} from "reaction/Apps/Artwork/Components/ArtworkDetails"
-
-export {
-  ReactionArtworkArtistInfo as ArtworkArtistInfo,
-} from "desktop/components/react/stitch_components/ReactionArtworkArtistInfo"
+export * from "desktop/components/react/stitch_components"
