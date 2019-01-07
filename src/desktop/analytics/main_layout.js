1c3d160135632804d5a7428d73091b8c07770449
Matthew Zikherman <mzikherman@gmail.com>
2019-01-07 18:13:08 -0500

    Update a bunch of analytics tracking for the new artwork page

--- src/desktop/analytics/main_layout.js
+++ src/desktop/analytics/main_layout.js
@@ -23,9 +23,9 @@ if (pageType == "artwork") {
   }
 }
 
-// We exclude 'orders' routes from analytics.page calls because they're already
+// We exclude these routes from analytics.page calls because they're already
 // taken care of in another place.
-const excludedRoutes = ["orders"]
+const excludedRoutes = ["orders", "new-artwork"]
 if (!excludedRoutes.includes(pageType)) {
   analytics.page(properties, { integrations: { Marketo: false } })
 }
