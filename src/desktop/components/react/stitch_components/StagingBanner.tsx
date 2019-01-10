2c23472368a4bddd9063fcb17fce72d5ed0d0e20
Kieran Gillen <kgillen@gmail.com>
2019-01-10 15:56:13 -0500

    Removes brackets from message string

--- src/desktop/components/react/stitch_components/StagingBanner.tsx
+++ src/desktop/components/react/stitch_components/StagingBanner.tsx
@@ -2,5 +2,5 @@ import React from "react"
 import { Banner } from "@artsy/palette"
 
 export const StagingBanner = () => (
-  <Banner backgroundColor="purple100" message={"staging.artsy.net"} />
+  <Banner backgroundColor="purple100" message="staging.artsy.net" />
 )
