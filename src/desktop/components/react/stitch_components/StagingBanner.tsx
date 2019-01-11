2ea2e7b698d31ca05ae0e6a84ddb082e74604144
Kieran Gillen <kgillen@gmail.com>
2019-01-11 14:50:39 -0500

    Adds URL from sd

--- src/desktop/components/react/stitch_components/StagingBanner.tsx
+++ src/desktop/components/react/stitch_components/StagingBanner.tsx
@@ -1,6 +1,12 @@
 import React from "react"
 import { Banner } from "@artsy/palette"
+import { data as sd } from "sharify"
 
-export const StagingBanner = () => (
-  <Banner backgroundColor="purple100" message="staging.artsy.net" />
-)
+export const StagingBanner = () => {
+  return (
+    <Banner
+      backgroundColor="purple100"
+      message={sd.APP_URL.replace("https://", "").replace("http://", "")}
+    />
+  )
+}
