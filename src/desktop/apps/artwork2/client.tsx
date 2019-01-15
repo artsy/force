5da63c5e72e139fc30a37779c1ea2a666b61555b
Christopher Pappas <damassi.pappas@gmail.com>
2019-01-14 19:59:14 -0800

    Remove container component

--- src/desktop/apps/artwork2/client.tsx
+++ src/desktop/apps/artwork2/client.tsx
@@ -3,7 +3,6 @@ import { routes } from "reaction/Apps/Artwork/routes"
 import { data as sd } from "sharify"
 import React from "react"
 import ReactDOM from "react-dom"
-import styled from "styled-components"
 
 const mediator = require("desktop/lib/mediator.coffee")
 const User = require("desktop/models/user.coffee")
@@ -11,13 +10,6 @@ const Artwork = require("desktop/models/artwork.coffee")
 const ArtworkInquiry = require("desktop/models/artwork_inquiry.coffee")
 const openInquiryQuestionnaireFor = require("desktop/components/inquiry_questionnaire/index.coffee")
 const openAuctionBuyerPremium = require("desktop/apps/artwork/components/auction/components/buyers_premium/index.coffee")
-
-// FIXME: Move this to Reaction
-const Container = styled.div`
-  width: 100%;
-  max-width: 1192px;
-  margin: auto;
-`
 
 buildClientApp({
   routes,
@@ -27,12 +19,7 @@ buildClientApp({
   },
 })
   .then(({ ClientApp }) => {
-    ReactDOM.hydrate(
-      <Container>
-        <ClientApp />
-      </Container>,
-      document.getElementById("react-root")
-    )
+    ReactDOM.hydrate(<ClientApp />, document.getElementById("react-root"))
   })
   .catch(error => {
     console.error(error)
