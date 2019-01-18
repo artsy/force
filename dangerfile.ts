3f38ac039bb7c77fb0001a9bf92d8f6e7ea54f2b
Isac Petruzzi <isac.petruzzi@gmail.com>
2019-01-18 17:39:39 +0100

    remove breaking changes checks from dangerfile.ts

--- dangerfile.ts
+++ dangerfile.ts
@@ -13,41 +13,4 @@ export default async () => {
       `Please don't include .js files, we want to be using TypeScript found: ${files}.`
     )
   }
-
-  // Breaking change check for Metaphysics production when deploying
-  // also locked against Peril having access to the file
-  if (!peril && danger.github.pr.base.ref === "release") {
-    const { getBreakingChanges } = require("./scripts/validate_schemas")
-    const breakingChanges = await getBreakingChanges()
-    const bc = breakingChanges
-    const typesMissing = bc.filter(b => b.type === "TYPE_REMOVED")
-    const fieldsMissing = bc.filter(b => b.type === "FIELD_REMOVED")
-    const unionOptionsMissing = bc.filter(
-      b => b.type === "TYPE_REMOVED_FROM_UNION"
-    )
-    const fieldChanged = bc.filter(b => b.type === "FIELD_CHANGED_KIND")
-
-    const descriptions = {
-      "Missing types": typesMissing,
-      "Fields missing": fieldsMissing,
-      "Fields changed": fieldChanged,
-      "Union types-mismatch": unionOptionsMissing,
-    }
-
-    if (bc.length) {
-      warn(
-        `Metaphysics production does not have a compatible schema for force's GraphQL usage, please deploy metaphysics to production and re-run Travis CI.`
-      )
-    }
-
-    Object.keys(descriptions).forEach(key => {
-      const breakingChanges: BreakingChange[] = descriptions[key]
-      if (breakingChanges.length) {
-        const fields = breakingChanges.map(
-          b => "`" + b.description.split(" ")[0] + "`"
-        )
-        warn(`${key}: ${danger.utils.sentence(fields)}`)
-      }
-    })
-  }
 }
