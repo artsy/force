64d99c2e9666d252b73df1424f6ab478a8a0a8dd
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-07 18:26:20 -0800

    Update sentry

--- scripts/sentry.sh
+++ scripts/sentry.sh
@@ -3,4 +3,10 @@
 set -ex
 
 VERSION=$(node_modules/.bin/sentry-cli releases propose-version)
+
+# See: https://docs.sentry.io/workflow/releases
+
+# Create a release
 node_modules/.bin/sentry-cli releases -o artsynet -p force-$DEPLOY_ENV files $VERSION upload-sourcemaps ./public/assets
+# Associate commits
+node_modules/.bin/sentry-cli releases set-commits --auto $VERSION
