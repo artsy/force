6b45d16fc7aa5a24cc7728f563cda57a888d724b
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-07 21:04:13 -0800

    Fix deploy env

--- scripts/sentry.sh
+++ scripts/sentry.sh
@@ -2,11 +2,9 @@
 
 set -ex
 
-VERSION=$(node_modules/.bin/sentry-cli releases propose-version)
-
 # See: https://docs.sentry.io/workflow/releases
 
-# Create a release
-node_modules/.bin/sentry-cli releases -o artsynet -p force-$DEPLOY_ENV files $VERSION upload-sourcemaps ./public/assets
-# Associate commits
+VERSION=$(node_modules/.bin/sentry-cli releases propose-version)
+
+node_modules/.bin/sentry-cli releases -o artsynet -p force-$DEPLOY_ENV files $VERSION upload-sourcemaps ./public/assets --no-rewrite
 node_modules/.bin/sentry-cli releases set-commits --auto $VERSION
