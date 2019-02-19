b72ec3d3e60c2d8e76c06d7ec6308aa28e1f791c
Matthew Zikherman <mzikherman@gmail.com>
2019-02-19 08:07:27 -0500

    [Autosuggest] Bring in Reaction component

--- src/desktop/components/react/stitch_components/SearchBar.tsx
+++ src/desktop/components/react/stitch_components/SearchBar.tsx
@@ -1,3 +1,16 @@
 import React from "react"
+import { Box } from "@artsy/palette"
+import { SearchBarQueryRenderer } from "reaction/Components/Search/SearchBar"
 
-export const SearchBar = () => <h1>New Search Bar</h1>
+export const SearchBar = () => (
+  <Box
+    style={{
+      position: "fixed",
+      zIndex: 100,
+      backgroundColor: "white",
+      top: "2px",
+    }}
+  >
+    <SearchBarQueryRenderer />
+  </Box>
+)
