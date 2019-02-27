3c350a51cc15f98d3344b143c82334e136e6c031
Justin Bennett <zephraph@gmail.com>
2019-02-26 19:32:32 -0500

    Ensure webpack bundle analyzer works

--- webpack/envs/ci.js
+++ webpack/envs/ci.js
@@ -1,10 +1,36 @@
 // @ts-check
 
-import { NODE_ENV } from "../../src/lib/environment"
-import { plugins } from "./debug"
+import { NODE_ENV, basePath } from "../../src/lib/environment"
+
+import { promises as fs } from "fs"
+import { DuplicatesPlugin } from "inspectpack/plugin"
+import path from "path"
+import stripAnsi from "strip-ansi"
 
 export const ciConfig = {
   mode: NODE_ENV,
   devtool: false,
-  plugins: [plugins.duplicates],
+  plugins: [
+    new DuplicatesPlugin({
+      verbose: true,
+      emitHandler(report) {
+        const artifactsPath = path.join(basePath, ".artifacts")
+        fs
+          .mkdir(artifactsPath)
+          .catch(() => Promise.resolve()) // .artifact directory exists, continue...
+          .then(() =>
+            fs.writeFile(
+              path.join(artifactsPath, "duplicates-report"),
+              stripAnsi(report)
+            )
+          )
+          .catch(err => {
+            console.error(
+              "[DuplicatesPlugin] Could not write duplicates report",
+              err
+            )
+          })
+      },
+    }),
+  ],
 }
