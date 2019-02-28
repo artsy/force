f1ef03acca6e39c436b2718a87c7b9d20a2405a6
Christopher Pappas <damassi.pappas@gmail.com>
2019-02-27 17:20:31 -0800

    Remove old isomorphic-test-app

--- src/desktop/index.js
+++ src/desktop/index.js
@@ -84,7 +84,6 @@ app.use(require("./apps/user"))
 
 // Examples
 app.use(require("./apps/react_example").app)
-app.use(require("./apps/isomorphic-relay-example/server").app)
 
 // FIXME: Remove
 app.use(require("./apps/ssr-experiments/server").app)
