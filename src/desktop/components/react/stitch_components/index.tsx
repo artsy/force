4551ff4f6b1eb0f7ed5afc8f2220ac8b50ce2913
Christopher Pappas <damassi.pappas@gmail.com>
2019-03-07 16:27:23 -0800

    [NavBar] Stitch in new <NavBar />

--- src/desktop/components/react/stitch_components/index.tsx
+++ src/desktop/components/react/stitch_components/index.tsx
@@ -1,8 +1,9 @@
 export { SearchBar } from "./SearchBar"
+export { NavBar } from './NavBar'
+
 export { ReactionArtworkArtistInfo as ArtworkArtistInfo } from "./ReactionArtworkArtistInfo"
 export { ArtworkDetailsQueryRenderer as ArtworkDetails } from "reaction/Apps/Artwork/Components/ArtworkDetails"
 export { ReactionTooltipQuestion as TooltipQuestion } from "./ReactionTooltipQuestion"
 export { UserSettingsPaymentsQueryRenderer as UserSettingsPayments } from "reaction/Components/Payment/UserSettingsPayments"
 export { StitchWrapper } from './StitchWrapper'
 export { StagingBanner } from './StagingBanner'
-
