87e24fb313faccb8a2954b7710be3afeaae197b3
Eve Essex <eve.essex@gmail.com>
2019-04-09 11:59:14 -0400

    send coffee flag to nyc, update codecov script permissions

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -6,7 +6,7 @@ set -ex
 
 # node --inspect-brk \
 # ./node_modules/.bin/mocha \
-nyc mocha \
+nyc --extension .coffee --reporter=lcov --reporter=text-summary mocha \
   -c \
   --require test.config.js \
   --require mocha.config.js \
