2d706a7c509753aea80bc252837fbab1e032b09e
Eve Essex <eve.essex@gmail.com>
2019-04-09 15:45:59 -0400

    mocha tests are read correctly

--- scripts/mocha.sh
+++ scripts/mocha.sh
@@ -6,7 +6,7 @@ set -ex
 
 # node --inspect-brk \
 # ./node_modules/.bin/mocha \
-nyc --extension .coffee --reporter=lcov --reporter=text-summary mocha \
+nyc --extension .coffee mocha \
   -c \
   --require test.config.js \
   --require mocha.config.js \
