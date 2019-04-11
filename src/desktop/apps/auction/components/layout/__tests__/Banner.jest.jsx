50c50b7b0c816a06e5db5ed422d30c6e49fedda7
Eve Essex <eve.essex@gmail.com>
2019-04-11 12:34:23 -0400

    convert auction banner to jest

--- src/desktop/apps/auction/components/layout/__tests__/Banner.jest.jsx
+++ src/desktop/apps/auction/components/layout/__tests__/Banner.jest.jsx
@@ -1,23 +1,24 @@
 import React from "react"
 import renderTestComponent from "desktop/apps/auction/__tests__/utils/renderTestComponent"
 import moment from "moment"
-import sinon from "sinon"
+import { test as Banner } from "../Banner"
 
-const rewire = require("rewire")("../Banner")
-const { Banner } = rewire.test
+jest.mock("desktop/components/clock/react", () => ({
+  __esModule: true,
+  namedExport: jest.fn(),
+  default: jest.fn().mockImplementation(() => ({
+    render: () => <div className="auction-clock" />,
+  })),
+}))
 
 describe("auction/components/layout/Banner.test", () => {
-  beforeEach(() => {
-    rewire.__set__("ClockView", () => <div className="auction-clock" />)
-  })
-
   describe("<Banner />", () => {
     it('tracks a click on the "Enter Live Auction" button', () => {
       const liveAuctionUrl = "live.artsy.net/some-auction-url"
-      const mockTrack = sinon.spy()
+      const mockTrack = jest.fn()
       window.analytics = { track: mockTrack }
       const { wrapper } = renderTestComponent({
-        Component: Banner,
+        Component: Banner.Banner,
         props: {
           hasEndTime: true,
           isAuction: true,
@@ -32,12 +33,18 @@ describe("auction/components/layout/Banner.test", () => {
       })
 
       wrapper.find("a").simulate("click")
-      mockTrack.calledWithMatch("click").should.be.true()
+      expect(mockTrack).toBeCalledWith("click", {
+        context_module: "auction banner",
+        destination_path: "live.artsy.net/some-auction-url",
+        flow: "auctions",
+        label: "enter live auction",
+        type: "button",
+      })
     })
 
     it("renders a clock if the auction has yet to open for live bidding", () => {
       const { wrapper } = renderTestComponent({
-        Component: Banner,
+        Component: Banner.Banner,
         props: {
           auction: { live_start_at: moment().add(2, "days") },
           isAuction: true,
@@ -50,12 +57,12 @@ describe("auction/components/layout/Banner.test", () => {
         },
       })
 
-      wrapper.find(".auction-clock").length.should.eql(1)
+      expect(wrapper.find(".auction-clock").length).toBe(1)
     })
 
     it("renders a clock if the auction hasn't started yet", () => {
       const { wrapper } = renderTestComponent({
-        Component: Banner,
+        Component: Banner.Banner,
         props: {
           auction: {
             live_start_at: moment().add(4, "days"),
@@ -71,14 +78,14 @@ describe("auction/components/layout/Banner.test", () => {
         },
       })
 
-      wrapper.find(".auction-clock").length.should.eql(1)
+      expect(wrapper.find(".auction-clock").length).toBe(1)
     })
 
     it("renders an Enter Live Auction banner if isLiveOpen", () => {
       const liveAuctionUrl = "live.artsy.net/some-auction-url"
 
       const { wrapper } = renderTestComponent({
-        Component: Banner,
+        Component: Banner.Banner,
         props: {
           auction: {},
           isAuction: true,
@@ -92,16 +99,13 @@ describe("auction/components/layout/Banner.test", () => {
         },
       })
 
-      wrapper.text().should.containEql("Live Bidding Now Open")
-      wrapper
-        .find("a")
-        .html()
-        .should.containEql(liveAuctionUrl)
+      expect(wrapper.text()).toMatch("Live Bidding Now Open")
+      expect(wrapper.find("a").html()).toMatch(liveAuctionUrl)
     })
 
     it("renders a normal banner if not live", () => {
       const { wrapper } = renderTestComponent({
-        Component: Banner,
+        Component: Banner.Banner,
         props: {
           auction: {},
           isAuction: true,
@@ -117,12 +121,12 @@ describe("auction/components/layout/Banner.test", () => {
         },
       })
 
-      wrapper.find(".auction-clock").length.should.eql(1)
+      expect(wrapper.find(".auction-clock").length).toBe(1)
     })
 
     it("hides clock if no end_at", () => {
       const { wrapper } = renderTestComponent({
-        Component: Banner,
+        Component: Banner.Banner,
         props: {
           auction: {},
           isAuction: false,
@@ -138,7 +142,7 @@ describe("auction/components/layout/Banner.test", () => {
         },
       })
 
-      wrapper.find(".auction-clock").length.should.eql(0)
+      expect(wrapper.find(".auction-clock").length).toBe(0)
     })
   })
 })
