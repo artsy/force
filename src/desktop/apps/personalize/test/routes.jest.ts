04796f9b0d54da10b521cf1bd9b576c95aed52cd
Eve Essex <eve.essex@gmail.com>
2019-05-16 16:45:44 -0400

    Address problematic tests (#4053)
    * make jest searching rules less restrictive
    * fix personalize routes test and convert to jest
    * x or fix broken promise tests
    
    on-behalf-of: @eessex

---
+++ src/desktop/apps/personalize/test/routes.jest.ts
@@ -0,0 +1,43 @@
+import { index } from "../routes"
+const CurrentUser = require("desktop/models/current_user.coffee")
+
+jest.mock("@artsy/stitch", () => ({
+  stitch: jest.fn(),
+}))
+const stichMock = require("@artsy/stitch").stitch as jest.Mock
+
+describe("Personalize routes", () => {
+  let req
+  let res
+  let next
+
+  beforeEach(() => {
+    req = {
+      body: {},
+      params: { slug: "interests" },
+      redirect: jest.fn(),
+      user: new CurrentUser({
+        name: "user",
+      }),
+      app: { get: jest.fn() },
+      query: {
+        redirectTo: "",
+      },
+    }
+    res = {
+      send: jest.fn(),
+      locals: {
+        sd: {},
+      },
+    }
+    next = jest.fn()
+  })
+
+  it("renders the personalize app", done => {
+    index(req, res, next).then(() => {
+      expect(stichMock.mock.calls[0][0].data.title).toBe("Personalize | Artsy")
+      expect(stichMock.mock.calls[0][0].locals.assetPackage).toBe("onboarding")
+      done()
+    })
+  })
+})
