be911929860c801e938d457452ea5e0d34f175fc
Luc Succes <l2succes@gmail.com>
2019-05-21 13:42:37 -0400

    Remove htl test

--- src/desktop/apps/article/templates/__tests__/meta.jest.js
+++ src/desktop/apps/article/templates/__tests__/meta.jest.js
@@ -3,7 +3,6 @@ import path from "path"
 import jade from "jade"
 import fs from "fs"
 import Article from "desktop/models/article.coffee"
-import fixtures from "desktop/test/helpers/fixtures.coffee"
 import { StandardArticle } from "@artsy/reaction/dist/Components/Publishing/Fixtures/Articles"
 
 const render = templateName => {
@@ -12,28 +11,6 @@ const render = templateName => {
     filename,
   })
 }
-
-describe("Script tag", () => {
-  it("contains ad server script tag", () => {
-    const article = _.extend({}, StandardArticle, {
-      slug: "artsy-editorial-slug",
-      indexable: true,
-      description: "Artsy Editorial is an editorial channel.",
-      social_description: "Artsy Editorial is socially friendly.",
-      social_image: "http://kitten-social.com",
-    })
-    const html = render("meta")({
-      article,
-      crop: url => url,
-      sd: {
-        APP_URL: "https://artsy.net",
-        CURRENT_PATH: "/article/artsy-editorial-slug",
-      },
-    })
-
-    expect(html).toMatch('<script src="//htlbid.com/v2/artsy.min.js">')
-  })
-})
 
 describe("Meta template", () => {
   it("contains basic meta tags", () => {
