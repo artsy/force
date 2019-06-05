8cec30fcb1f88007cedf6d2a3517258a516b18a8
Christopher Pappas <damassi.pappas@gmail.com>
2019-06-04 21:45:27 -0700

    Add toggle to hide staging banner

--- src/desktop/components/react/stitch_components/StagingBanner.tsx
+++ src/desktop/components/react/stitch_components/StagingBanner.tsx
@@ -1,11 +1,17 @@
-import React from "react"
+import React, { useState } from "react"
 import styled from "styled-components"
 import { Banner, Box } from "@artsy/palette"
 import { data as sd } from "sharify"
 
 export const StagingBanner = () => {
+  const [show, toggleVisible] = useState(true)
+
+  if (!show) {
+    return null
+  }
+
   return (
-    <Container>
+    <Container onClick={() => toggleVisible(false)}>
       <Banner
         backgroundColor="purple100"
         message={sd.APP_URL.replace("https://", "").replace("http://", "")}
