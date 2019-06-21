bd4a76e24961b493aba6b834fdead6feb18db09a
Ashley Jelks <arjelks@gmail.com>
2019-06-21 15:26:45 -0400

    enable ads on staging for all users

--- src/desktop/apps/article/helpers.tsx
+++ src/desktop/apps/article/helpers.tsx
@@ -2,27 +2,12 @@ import { get } from "lodash"
 /**
  * For now ads, served by Hashtag Labs are only to viewable in the following instances:
  *  1) When the environment is NOT Production (HASHTAG_LAB_ADS_ENABLED set to false)
- *  2) When a user is an Admin
- *  3) When a user has been added to the allowlist set via HASHTAG_LAB_ADS_ALLOWLIST
  */
 
 export const areThirdPartyAdsEnabled = (sd: {}): boolean => {
   const areThirdPartyAdsEnabled = get(sd, "HASHTAG_LAB_ADS_ENABLED", false)
-  const currentUser = get(sd, "CURRENT_USER.email", "")
-  const allowList = get(sd, "HASHTAG_LAB_ADS_ALLOWLIST", "")
-  const isAllowedUser = allowList
-    .split(",")
-    .filter(Boolean)
-    .includes(currentUser)
-  const isAdminUser = get(sd, "CURRENT_USER.type") === "Admin"
 
-  if (!areThirdPartyAdsEnabled) {
-    return false
-  }
-  if (isAllowedUser || isAdminUser) {
-    return true
-  }
-  return false
+  return areThirdPartyAdsEnabled
 }
 
 // Helper method to determine how frequently ads should be rendered in Article components
