0d71f72a82dd0ad7a4ab34925a461999d545e2e2
xtina-starr <c.thompsonms@gmail.com>
2019-07-01 15:54:30 -0400

    triggers authentication modal with image on artist page

--- src/desktop/apps/artist/components/cta.js
+++ src/desktop/apps/artist/components/cta.js
@@ -1,8 +1,10 @@
-import $ from "jquery"
+// import $ from "jquery"
 import metaphysics from "lib/metaphysics.coffee"
-import Artist from "desktop/models/artist.coffee"
+// import Artist from "desktop/models/artist.coffee"
+import { once } from "lodash"
 import { data as sd } from "sharify"
-import ArtistPageCTAView from "desktop/components/artist_page_cta/view.coffee"
+const mediator = require("../../../lib/mediator.coffee")
+// import ArtistPageCTAView from "desktop/components/artist_page_cta/view.coffee"
 
 const query = `
 query ArtistCTAQuery($artistID: String!) {
@@ -22,6 +24,13 @@ query ArtistCTAQuery($artistID: String!) {
       }
       name
     }
+    artworks(size: 1) {
+      image {
+        cropped(width: 390, height: 644) {
+          url
+        }
+      }
+    }
   }
 }
 `
@@ -33,12 +42,38 @@ const send = {
 
 if (sd.ARTIST_PAGE_CTA_ENABLED && sd.ARTIST_PAGE_CTA_ARTIST_ID) {
   metaphysics(send).then(({ artist: artistData }) => {
-    const artist = new Artist(artistData)
-    const view = new ArtistPageCTAView({ artist })
-    $("body").append(view.render().$el)
-    view.initializeMailcheck()
-    setTimeout(() => {
-      view.$el.removeClass("initial")
-    }, 500)
+    const image =
+      artistData.artworks &&
+      artistData.artworks[0] &&
+      artistData.artworks[0].image &&
+      artistData.artworks[0].image.cropped &&
+      artistData.artworks[0].image.cropped.url
+
+    // const artist = new Artist(artistData)
+    // const view = new ArtistPageCTAView({ artist })
+    // $("body").append(view.render().$el)
+    // view.initializeMailcheck()
+    // setTimeout(() => {
+    //   view.$el.removeClass("initial")
+    // }, 500)
+
+    if (!sd.CURRENT_USER && !sd.IS_MOBILE) {
+      window.addEventListener(
+        "scroll",
+        once(() => {
+          setTimeout(() => {
+            mediator.trigger("open:auth", {
+              mode: "signup",
+              intent: "signup",
+              signupIntent: "signup",
+              trigger: "click",
+              destination: location.href,
+              image,
+            })
+          }, 2000)
+        }),
+        { once: true }
+      )
+    }
   })
 }
