9b5cf6b1bfb876072b0c4b009d1ada3e9d2af78a
xtina-starr <c.thompsonms@gmail.com>
2019-07-01 17:18:03 -0400

    setup up artist page signup modal a/b test for QA

--- src/desktop/apps/artist/components/cta.js
+++ src/desktop/apps/artist/components/cta.js
@@ -1,10 +1,10 @@
-// import $ from "jquery"
+import ArtistPageCTAView from "desktop/components/artist_page_cta/view.coffee"
+import $ from "jquery"
 import metaphysics from "lib/metaphysics.coffee"
-// import Artist from "desktop/models/artist.coffee"
+import Artist from "desktop/models/artist.coffee"
 import { once } from "lodash"
 import { data as sd } from "sharify"
 const mediator = require("../../../lib/mediator.coffee")
-// import ArtistPageCTAView from "desktop/components/artist_page_cta/view.coffee"
 
 const query = `
 query ArtistCTAQuery($artistID: String!) {
@@ -49,31 +49,37 @@ if (sd.ARTIST_PAGE_CTA_ENABLED && sd.ARTIST_PAGE_CTA_ARTIST_ID) {
       artistData.artworks[0].image.cropped &&
       artistData.artworks[0].image.cropped.url
 
-    // const artist = new Artist(artistData)
-    // const view = new ArtistPageCTAView({ artist })
-    // $("body").append(view.render().$el)
-    // view.initializeMailcheck()
-    // setTimeout(() => {
-    //   view.$el.removeClass("initial")
-    // }, 500)
-
-    if (!sd.CURRENT_USER && !sd.IS_MOBILE) {
-      window.addEventListener(
-        "scroll",
-        once(() => {
-          setTimeout(() => {
-            mediator.trigger("open:auth", {
-              mode: "signup",
-              intent: "signup",
-              signupIntent: "signup",
-              trigger: "click",
-              destination: location.href,
-              image,
-            })
-          }, 2000)
-        }),
-        { once: true }
-      )
+    if (sd.ARTIST_PAGE_SIGNUP_MODAL_QA === "experiment") {
+      if (!sd.CURRENT_USER && !sd.IS_MOBILE) {
+        window.addEventListener(
+          "scroll",
+          once(() => {
+            setTimeout(() => {
+              mediator.trigger("open:auth", {
+                copy: `Join Artsy to discover new works by ${
+                  artistData.name
+                } and more artists you love`,
+                mode: "signup",
+                intent: "signup",
+                signupIntent: "signup",
+                trigger: "scroll",
+                triggerSeconds: 2,
+                destination: location.href,
+                image,
+              })
+            }, 2000)
+          }),
+          { once: true }
+        )
+      }
+    } else {
+      const artist = new Artist(artistData)
+      const view = new ArtistPageCTAView({ artist })
+      $("body").append(view.render().$el)
+      view.initializeMailcheck()
+      setTimeout(() => {
+        view.$el.removeClass("initial")
+      }, 500)
     }
   })
 }
