eab0e9727bf1ad3789deb6d1efbebe5adc06b240
xtina-starr <c.thompsonms@gmail.com>
2019-07-09 16:50:54 -0400

    add tests

---
+++ src/desktop/apps/artist/components/__tests__/cta.jest.ts
@@ -0,0 +1,73 @@
+import { init, query } from "../cta"
+
+jest.mock("lib/metaphysics.coffee", () =>
+  jest.fn().mockReturnValue(Promise.resolve({}))
+)
+
+const mockMetaphysics = require("lib/metaphysics.coffee")
+
+jest.mock("sharify", () => ({
+  data: {
+    ARTIST_PAGE_CTA_ARTIST_ID: "claes-oldenburg",
+    ARTIST_PAGE_CTA_ENABLED: true,
+    ARTIST_PAGE_SIGNUP_MODAL_QA: "experiment",
+  },
+}))
+
+describe("CTA", () => {
+  const artist = {
+    id: "claes-oldenburg",
+    name: "Claes Oldenburg",
+    href: "/artist/claes-oldenburg",
+    image: {
+      thumb: {
+        resized: {
+          url:
+            "https://d32dm0rphc51dk.cloudfront.net/6q6LeyKvA_vpT5YzHRSNUA/large.jpg",
+        },
+      },
+    },
+    artists: [
+      {
+        image: {
+          thumb: {
+            resized: {
+              url:
+                "https://d32dm0rphc51dk.cloudfront.net/6q6LeyKvA_vpT5YzHRSNUA",
+            },
+          },
+        },
+      },
+    ],
+    artworks: [
+      {
+        image: {
+          cropped: {
+            url: "https://d32dm0rphc51dk.cloudfront.net/6q6LeyKvA_vpT5YzHRSNUA",
+          },
+        },
+      },
+    ],
+  }
+
+  it("should get artist data when artist cta is enabled and there is an artist id", async () => {
+    const options = {
+      method: "post",
+      query: query,
+      variables: { artistID: artist.id },
+    }
+    await init()
+
+    expect(mockMetaphysics).toBeCalledWith(options)
+  })
+
+  it("should set up a scroll event listener when in the experiment group", async () => {
+    const spy = jest.spyOn(window, "addEventListener")
+    mockMetaphysics.mockReturnValue(Promise.resolve({ artist }))
+    await init()
+
+    expect(spy).toHaveBeenCalled()
+    expect(spy.mock.calls[0][0]).toBe("scroll")
+    expect(spy.mock.calls[0][2]).toEqual({ once: true })
+  })
+})
