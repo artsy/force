76db5bf11f0ceabf5d6a93629d51afd89f1adc9d
Justin Bennett <zephraph@gmail.com>
2019-09-04 10:31:13 -0400

    Add a bit more logging for the linking process

--- scripts/quicklink.sh
+++ scripts/quicklink.sh
@@ -1,9 +1,13 @@
 
 
 # Add package as a "linked" package to force
+echo ""
+echo "Linking $1"
 npx yalc add $1
 
 # Try to install force's dependencies (retry if it fails)
+echo ""
+echo "Installing dependencies"
 n=0
 until [ $n -ge 3 ]
 do
@@ -15,6 +19,8 @@ do
   sleep 1
 done
 
+echo ""
+echo "Beginning service"
 if [ -z "$ENABLE_DEBUGGER" ]; then
   # Start force without debugger
   TERM_PROGRAM=other yarn start
