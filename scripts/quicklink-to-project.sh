aec6604ec67fd1a454b180f4f1de5a3cf92eadc1
Justin Bennett <zephraph@gmail.com>
2019-09-05 18:08:17 -0400

    Allow reaction linking from force

---
+++ scripts/quicklink-to-project.sh
@@ -0,0 +1,22 @@
+if [ -z "$1" ] && [ -z "$PROJECT_PATH" ]; then
+  echo "Must supply a project name or set the PROJECT_PATH env"
+fi
+
+PROJECT=${PROJECT_PATH:-../$1}
+
+if [ ! -d "$PROJECT" ]; then
+  echo "$(realpath $PROJECT) doesn't exist, ensure project name is correct or PROJECT_PATH is pointing to a valid directory"
+  exit 1
+fi
+
+cd $PROJECT
+
+# Does package have integrate command?
+cat package.json | jq -e .scripts.integrate &>2
+
+if [ $? -ne 0 ]; then
+  echo "$PROJECT doesn't have integrate npm script, ensure it's valid"
+  exit 1
+fi
+
+yarn integrate force
