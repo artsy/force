ada42e6a71eddbc3c35f5fc5409a952e08d7227f
Erik Stockmeier <erik@artsymail.com>
2019-09-05 17:30:06 -0400

    use if/else to ensure route handler halts for next/redirect

--- src/desktop/apps/auction_reaction/routes.jest.ts
+++ src/desktop/apps/auction_reaction/routes.jest.ts
@@ -11,14 +11,16 @@ jest.mock("@artsy/stitch", () => ({
 
 describe("Reaction Auction app routes", () => {
   describe("bidderRegistration", () => {
+    let req
+    let res
     const mockSend = jest.fn()
     const mockRedirect = jest.fn()
     const mockNext = jest.fn()
     const mockStitch = stitch as jest.Mock
-    let req
-    let res
+    const layout = "<marquee>Welcome to Artsy</marquee>"
 
     beforeEach(() => {
+      jest.resetAllMocks()
       req = {
         query: {},
         header: jest.fn(),
@@ -29,7 +31,7 @@ describe("Reaction Auction app routes", () => {
         redirect: mockRedirect,
         status: () => ({ send: mockSend }),
       }
-      jest.resetAllMocks()
+      mockStitch.mockResolvedValue(layout)
     })
 
     describe("?accepted-conditions=true query param is present", () => {
@@ -37,9 +39,10 @@ describe("Reaction Auction app routes", () => {
         req.query["accepted-conditions"] = "true"
       })
 
-      it("defers handling using next() if the accepted-conditions=true query param is present", () => {
-        bidderRegistration(req, res, mockNext)
+      it("defers handling using next() if the accepted-conditions=true query param is present", async () => {
+        await bidderRegistration(req, res, mockNext)
         expect(mockNext).toHaveBeenCalledWith()
+        expect(mockSend).not.toHaveBeenCalled()
       })
     })
 
@@ -48,20 +51,18 @@ describe("Reaction Auction app routes", () => {
         req.query = {}
       })
 
-      it("redirects if user is not present", () => {
+      it("redirects if user is not present", async () => {
         delete res.locals.sd.CURRENT_USER
-        bidderRegistration(req, res, mockNext)
+        await bidderRegistration(req, res, mockNext)
         expect(mockRedirect).toHaveBeenCalledWith(
           "/login?redirectTo=testurl.artsy.net%2Fauction-registration%2Fdeluxe-art-sale"
         )
+        expect(mockSend).not.toHaveBeenCalled()
       })
 
       it("does not defer handling", async () => {
-        const layout = "<marquee>Welcome to Artsy</marquee>"
-        const next = jest.fn()
-        mockStitch.mockResolvedValue(layout)
-        await bidderRegistration(req, res, next)
-        expect(next).not.toHaveBeenCalled()
+        await bidderRegistration(req, res, mockNext)
+        expect(mockNext).not.toHaveBeenCalled()
         expect(mockSend).toHaveBeenCalledWith(layout)
       })
     })
