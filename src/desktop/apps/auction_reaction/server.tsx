917e6acc7d905f0109ff3b8a1770370fdfe45e1a
Devon Blandin <dblandin@gmail.com>
2019-09-05 10:51:11 -0400

    Revert "Promote new reaction-based registration form"
    This reverts commit ec81f4069301d4afe26cd20b46aeefde473b76a9.

--- src/desktop/apps/auction_reaction/server.tsx
+++ src/desktop/apps/auction_reaction/server.tsx
@@ -3,5 +3,4 @@ import { bidderRegistration } from "./routes"
 
 export const app = express()
 
-app.get("/auction-registration/:auctionID*", bidderRegistration)
 app.get("/auction-registration2/:auctionID*", bidderRegistration)
