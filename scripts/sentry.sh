14856f97e30905742a6ba705885ce4c8757f7eec
Erik Stockmeier <erik@artsymail.com>
2019-12-12 16:15:38 -0500

    wip - setting up sourcemaps

--- scripts/sentry.sh
+++ scripts/sentry.sh
@@ -3,4 +3,6 @@
 set -ex
 
 VERSION=$(node_modules/.bin/sentry-cli releases propose-version)
-node_modules/.bin/sentry-cli releases -o artsynet -p force-$DEPLOY_ENV files $VERSION upload-sourcemaps ./public/assets
+
+node_modules/.bin/sentry-cli releases -o artsynet -p \
+  force-$DEPLOY_ENV files $VERSION upload-sourcemaps ./public/assets
