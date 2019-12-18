1d25fb17dff765e7c4f1cf5d2a4dc52c517ff864
Eve Essex <eve.essex@gmail.com>
2019-12-18 14:55:59 -0500

    upload coverage reports

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,22 +2,5 @@
 
 set -ex
 
-#trap "exit" INT
-
-run () {
-  case $CIRCLE_NODE_INDEX in
-  0)
-    yarn mocha $(find src -name '*.test.*')
-    ;;
-  1)
-    yarn jest
-    ;;
-  esac
-}
-
-if [ -z "$CIRCLE_NODE_INDEX" ]; then
-  CIRCLE_NODE_INDEX=0 run
-  CIRCLE_NODE_INDEX=1 run
-else
-  run
-fi
+yarn mocha $(find src -name '*.test.*')
+yarn jest
