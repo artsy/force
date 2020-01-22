da2f41b4299d97768b6545b116474f62f41d28be
Eve Essex <eve.essex@gmail.com>
2020-01-22 18:13:40 -0500

    convert system/up nightmare tests to cypress

---
+++ cypress/integration/systemUp.spec.js
@@ -0,0 +1,21 @@
+describe("/system/up", () => {
+  it("should not render a page for /system/up healthcheck endpoint", () => {
+    cy.request("/system/up")
+      .its("headers")
+      .its("content-type")
+      .should("include", "application/json")
+  })
+
+  context("mobile", () => {
+    before(() => {
+      cy.viewport("iphone-x")
+    })
+
+    it("should not render a page for /system/up healthcheck endpoint", () => {
+      cy.request("/system/up")
+        .its("headers")
+        .its("content-type")
+        .should("include", "application/json")
+    })
+  })
+})
