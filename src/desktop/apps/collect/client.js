979a871195783d79806c554de58ebc788a532ff7
Christopher Pappas <damassi.pappas@gmail.com>
2020-02-13 22:44:19 -0800

    Bump reaction

--- src/desktop/apps/collect/client.js
+++ src/desktop/apps/collect/client.js
@@ -4,6 +4,7 @@ import { collectRoutes } from "reaction/Apps/Collect2/collectRoutes"
 import mediator from "desktop/lib/mediator.coffee"
 import React from "react"
 import ReactDOM from "react-dom"
+import { loadableReady } from "@loadable/component"
 
 buildClientApp({
   routes: collectRoutes,
@@ -13,7 +14,9 @@ buildClientApp({
   },
 })
   .then(({ ClientApp }) => {
-    ReactDOM.hydrate(<ClientApp />, document.getElementById("react-root"))
+    loadableReady(() => {
+      ReactDOM.hydrate(<ClientApp />, document.getElementById("react-root"))
+    })
   })
   .catch(error => {
     console.error(error)
