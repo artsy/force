7c20d1e6c76c006e774a6742d06528c6543ec68e
Christopher Pappas <damassi.pappas@gmail.com>
2020-02-23 02:55:28 -0800

    Test 4: Only set webpack CDN public path on client

--- src/desktop/lib/global_client_setup.tsx
+++ src/desktop/lib/global_client_setup.tsx
@@ -1,3 +1,15 @@
+/**
+ * Set webpack public-path asset lookup to CDN in production, but only on
+ * the client, as we use the assetMiddleware helper to map URLs on the server.
+ * @see https://github.com/damassi/force/blob/master/src/lib/middleware/assetMiddleware.ts
+ */
+if (process.env.NODE_ENV === "production") {
+  __webpack_public_path__ = "https://d1rmpw1xlv9rxa.cloudfront.net/assets/"
+
+  // @ts-ignore
+  window._logAssetPath = () => process.env.CDN_URL + "/assets/"
+}
+
 import $ from "jquery"
 import Backbone from "backbone"
 import _ from "underscore"
