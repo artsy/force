43d41780cff6f5d02385f4a0eb0ca43d7ea42e82
Erik Stockmeier <erik.d.stock@gmail.com>
2020-03-02 16:37:04 -0500

    Remove skipRouteIfClientSideRoutingEnabled middleware for now

--- src/desktop/apps/identity_verification/server.js
+++ src/desktop/apps/identity_verification/server.js
@@ -1,11 +1,6 @@
 import express from "express"
-import { skipIfClientSideRoutingEnabled } from "desktop/components/split_test/skipIfClientSideRoutingEnabled"
 import { identityVerification } from "desktop/apps/identity_verification/routes"
 
 export const app = express()
 
-app.get(
-  "/identity-verification/:id",
-  skipIfClientSideRoutingEnabled,
-  identityVerification
-)
+app.get("/identity-verification/:id", identityVerification)
