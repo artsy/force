00fc75468081a8f2944a4fe498fae729c67babef
brainbicycle <brian.beckerle@artsymail.com>
2020-04-29 14:52:53 -0400

    Title casing for web home rails #trivial
    Title casing for web home rails #trivial
    
    Update cypress test to check for title case

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,7 +1,7 @@
 describe("/ (home)", () => {
   it("renders metadata and page content", () => {
     cy.visit("/")
-    cy.get("h2").should("contain", "Featured categories")
+    cy.get("h2").should("contain", "Featured Categories")
     cy.title().should("eq", "Artsy - Discover & Buy Art")
   })
 })
