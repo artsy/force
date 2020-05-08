eb78579a68d3a53350a54d972011df88b02b7779
Sarah Weir <sweir27@gmail.com>
2020-05-08 16:37:30 -0400

    Adds routing for new feature page

---
+++ src/desktop/apps/feature-reaction/client.tsx
@@ -0,0 +1,28 @@
+import { buildClientApp } from "reaction/Artsy/Router/client"
+import { data as sd } from "sharify"
+import { routes as featureRoutes } from "reaction/Apps/Feature/routes"
+import React from "react"
+import ReactDOM from "react-dom"
+import { loadableReady } from "@loadable/component"
+
+const mediator = require("desktop/lib/mediator.coffee")
+
+buildClientApp({
+  routes: featureRoutes,
+  context: {
+    user: sd.CURRENT_USER,
+    mediator,
+  } as any,
+})
+  .then(({ ClientApp }) => {
+    loadableReady(() => {
+      ReactDOM.hydrate(<ClientApp />, document.getElementById("react-root"))
+    })
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
+if (module.hot) {
+  module.hot.accept()
+}
---
+++ src/desktop/assets/feature-reaction.tsx
@@ -0,0 +1,1 @@
+import "desktop/apps/feature-reaction/client"
