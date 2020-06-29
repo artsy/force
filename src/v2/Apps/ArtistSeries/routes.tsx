b674ada8bca18bc2ba0e545d73f79459cc27dd88
Will Doenlen <wdoenlen@gmail.com>
2020-06-29 12:21:22 -0400

    Remove leftover commented out code

--- src/v2/Apps/ArtistSeries/routes.tsx
+++ src/v2/Apps/ArtistSeries/routes.tsx
@@ -1,11 +1,8 @@
 import loadable from "@loadable/component"
 import { graphql } from "react-relay"
 import { RouteConfig } from "found"
-// import React from "react"
 
 const ArtistSeriesApp = loadable(() => import("./ArtistSeriesApp"))
-
-// const ArtistSeriesApp = props => <h1>{props.artistSeries.internalID}</h1>
 
 export const routes: RouteConfig[] = [
   {
