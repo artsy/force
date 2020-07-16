705322c6fc75713a9c723d492b7c73a2b79b447f
oxaudo <oksana@artsymail.com>
2020-07-16 15:17:01 -0700

    just removing temporarily unrelated code to illustrate the problem more clear

--- src/v2/Apps/ViewingRoom/Components/ViewingRoomsLatestGrid.tsx
+++ src/v2/Apps/ViewingRoom/Components/ViewingRoomsLatestGrid.tsx
@@ -1,12 +1,5 @@
 import React from "react"
-import {
-  Box,
-  // CSSGrid,
-  // CardTagProps,
-  // Link,
-  // Sans,
-  // SmallCard,
-} from "@artsy/palette"
+import { Box } from "@artsy/palette"
 import {
   RelayPaginationProp,
   createPaginationContainer,
@@ -20,115 +13,11 @@ export interface ViewingRoomsLatestGridProps {
   viewingRooms: ViewingRoomsLatestGrid_viewingRooms
 }
 
-// const getTagProps = (
-//   status: string,
-//   distanceToOpen: string | null,
-//   distanceToClose: string | null
-// ): CardTagProps | null => {
-//   switch (status) {
-//     case "closed":
-//       return {
-//         text: "Closed",
-//         textColor: "white100",
-//         color: "black100",
-//         borderColor: "black100",
-//       }
-//     case "live":
-//       return distanceToClose
-//         ? {
-//             text: `${distanceToClose} left`,
-//             textColor: "black60",
-//             color: "white100",
-//             borderColor: "black5",
-//           }
-//         : null
-//     case "scheduled":
-//       return distanceToOpen
-//         ? {
-//             text: "Opening soon",
-//             textColor: "white100",
-//             color: "black100",
-//             borderColor: "black100",
-//           }
-//         : null
-//   }
-// }
-
 export const ViewingRoomsLatestGrid: React.FC<ViewingRoomsLatestGridProps> = props => {
-  // const viewingRooms = props.viewingRooms
   console.log("!!!!!!!!!!!!!!!!!")
   console.log(props)
   console.log("!!!!!!!!!!!!!!!!!")
   return <Box>Hello World</Box>
-
-  // if (!viewingRooms?.edges?.length) {
-  //   return null
-  // }
-
-  // const viewingRoomsForLatestGrid = viewingRooms.edges
-  //   .map(vr => {
-  //     if (!vr.node) {
-  //       return null
-  //     }
-
-  //     if (vr.node.status != "scheduled" && vr.node.status != "live") {
-  //       return null
-  //     }
-
-  //     return {
-  //       ...vr.node,
-  //     }
-  //   })
-  //   .filter(Boolean)
-  // const count = viewingRoomsForLatestGrid.length
-
-  // return (
-  //   <Box>
-  //     <Sans size="5">Latest</Sans>
-  //     <Box>
-  //       <CSSGrid
-  //         mt={2}
-  //         mb={6}
-  //         gridTemplateColumns={[
-  //           "repeat(1fr)",
-  //           `repeat(${Math.min(count, 2)}, 1fr)`,
-  //           `repeat(${Math.min(count, 3)}, 1fr)`,
-  //         ]}
-  //         gridColumnGap={2}
-  //         gridRowGap={6}
-  //       >
-  //         {viewingRoomsForLatestGrid.map(vr => {
-  //           const {
-  //             slug,
-  //             title,
-  //             status,
-  //             heroImageURL,
-  //             partner,
-  //             distanceToOpen,
-  //             distanceToClose,
-  //             artworksConnection,
-  //           } = vr
-  //           const artworksCount = artworksConnection.totalCount
-  //           const artworkImages = artworksConnection.edges.map(({ node }) =>
-  //             artworksCount < 2 ? node.image.regular : node.image.square
-  //           )
-  //           const tag = getTagProps(status, distanceToOpen, distanceToClose)
-
-  //           return (
-  //             <Link href={`/viewing-room/${slug}`} key={slug} noUnderline>
-  //               <SmallCard
-  //                 title={title}
-  //                 subtitle={partner.name}
-  //                 images={[heroImageURL].concat(artworkImages)}
-  //                 tag={tag}
-  //               />
-  //             </Link>
-  //           )
-  //         })}
-  //       </CSSGrid>
-  //     </Box>
-  //   </Box>
-  // )
 }
 
 export const ViewingRoomsLatestGridFragmentContainer = createPaginationContainer(
