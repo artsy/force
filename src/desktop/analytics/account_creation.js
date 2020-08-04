d81247a3a888027f243aa51611b0e2d8c5b1f326
Eve Essex <eve.essex@gmail.com>
2020-08-04 18:19:49 -0400

    deprecate unused facebook events

--- src/desktop/analytics/account_creation.js
+++ src/desktop/analytics/account_creation.js
@@ -20,23 +20,6 @@ const getUrlParameter = name => {
 const getAcquisitionInitiative = () =>
   getUrlParameter("m-id") || getUrlParameter("acquisition_initiative")
 
-// Created account (via social)
-
-// 1. Upon clicking the social signup button
-$(document).on("click", ".auth-signup-facebook", e => {
-  // 2. Store some data in cookies before being redirected everywhere
-  mediator.trigger("auth:sign_up:fb", {
-    service: "facebook",
-    acquisition_initiative: getAcquisitionInitiative(),
-    context: $(e.currentTarget).data("context"),
-  })
-})
-
 analyticsHooks.on("auth:login", (options = {}) => {
   analytics.track("Successfully logged in", options)
-})
-
-analyticsHooks.on("mediator:open:auth", (options = {}) => {
-  analytics.trackLink($(".auth-signup-facebook")[0], "Created account")
-  analytics.trackLink($(".auth-signup-twitter")[0], "Created account")
 })
