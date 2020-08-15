c69fca75d6970f259fc0777c27a288d3aced93a1
Christopher Pappas <damassi.pappas@gmail.com>
2020-08-14 17:49:58 -0700

    Fix tests

--- src/v2/DevTools/MockBoot.tsx
+++ src/v2/DevTools/MockBoot.tsx
@@ -1,6 +1,7 @@
 import { Boot } from "v2/Artsy/Router"
 import React from "react"
 import { Breakpoint } from "v2/Utils/Responsive"
+import { Environment, Network, RecordSource, Store } from "relay-runtime"
 
 export const MockBoot: React.SFC<{
   breakpoint?: Breakpoint
@@ -14,16 +15,20 @@ export const MockBoot: React.SFC<{
   user = null,
   context = null,
 }) => {
-    return (
-      <Boot
-        onlyMatchMediaQueries={[breakpoint]}
-        headTags={headTags}
-        context={context}
-        user={user}
-        relayEnvironment={null as any}
-        routes={null as any}
-      >
-        {children}
-      </Boot>
-    )
-  }
+  const mockRelayEnvironment = new Environment({
+    network: Network.create(x => x as any),
+    store: new Store(new RecordSource()),
+  })
+  return (
+    <Boot
+      onlyMatchMediaQueries={[breakpoint]}
+      headTags={headTags}
+      context={context}
+      user={user}
+      relayEnvironment={mockRelayEnvironment}
+      routes={null as any}
+    >
+      {children}
+    </Boot>
+  )
+}
