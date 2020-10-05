b2bd9fdcd5836e66f2c86a604a7de9494ce2c2aa
Christopher Pappas <damassi.pappas@gmail.com>
2020-10-05 16:22:04 -0700

    chore: Add react-test-utils

--- src/v2/Apps/Artist/Components/__tests__/ArtistConsignButton.jest.tsx
+++ src/v2/Apps/Artist/Components/__tests__/ArtistConsignButton.jest.tsx
@@ -1,11 +1,14 @@
 import { Breakpoint } from "@artsy/palette"
-import { ArtistConsignButtonQueryRawResponse } from "v2/__generated__/ArtistConsignButtonQuery.graphql"
+import { ArtistConsignButton_Test_Query } from "v2/__generated__/ArtistConsignButton_Test_Query.graphql"
+
 import { useTracking } from "v2/Artsy/Analytics/useTracking"
-import { MockBoot, renderRelayTree } from "v2/DevTools"
+import { MockBoot } from "v2/DevTools"
 import { cloneDeep } from "lodash"
 import React from "react"
-import { graphql } from "react-relay"
+import { QueryRenderer, graphql } from "react-relay"
 import { ArtistConsignButtonFragmentContainer } from "../ArtistConsignButton"
+import { MockPayloadGenerator, createMockEnvironment } from "relay-test-utils"
+import { mount } from "enzyme"
 
 jest.unmock("react-relay")
 jest.mock("v2/Artsy/Analytics/useTracking")
@@ -13,37 +16,46 @@ jest.mock("v2/Artsy/Analytics/useTracking")
 describe("ArtistConsignButton", () => {
   const trackEvent = jest.fn()
 
-  const getWrapper = async ({
-    breakpoint = "xs",
-    response,
-  }: {
-    breakpoint: Breakpoint
-    response: ArtistConsignButtonQueryRawResponse
-  }) => {
-    return await renderRelayTree({
-      Component: ({ artist }) => {
-        return (
-          <MockBoot breakpoint={breakpoint}>
-            <ArtistConsignButtonFragmentContainer artist={artist} />
-          </MockBoot>
-        )
-      },
-      query: graphql`
-        query ArtistConsignButton_Test_Query($artistID: String!)
-          @raw_response_type {
-          artist(id: $artistID) {
-            ...ArtistConsignButton_artist
+  let env = createMockEnvironment() as ReturnType<typeof createMockEnvironment>
+
+  const getWrapper = ({ breakpoint = "xs", response }) => {
+    const TestRenderer = () => (
+      <QueryRenderer<ArtistConsignButton_Test_Query>
+        environment={env}
+        query={graphql`
+          query ArtistConsignButton_Test_Query {
+            artist(id: "alex-katz") {
+              ...ArtistConsignButton_artist
+            }
           }
-        }
-      `,
-      variables: {
-        artistID: "alex-katz",
-      },
-      mockData: response,
-    })
+        `}
+        variables={{}}
+        render={({ props, error }) => {
+          if (props?.artist) {
+            return (
+              <MockBoot breakpoint={breakpoint as Breakpoint}>
+                <ArtistConsignButtonFragmentContainer artist={props.artist} />
+              </MockBoot>
+            )
+          } else if (error) {
+            console.error(error)
+          }
+        }}
+      />
+    )
+
+    const wrapper = mount(<TestRenderer />)
+    env.mock.resolveMostRecentOperation(operation =>
+      MockPayloadGenerator.generate(operation, {
+        Artist: () => response,
+      })
+    )
+    wrapper.update()
+    return wrapper
   }
 
   beforeEach(() => {
+    env = createMockEnvironment()
     const mockTracking = useTracking as jest.Mock
     mockTracking.mockImplementation(() => {
       return {
@@ -58,38 +70,40 @@ describe("ArtistConsignButton", () => {
 
   describe("Top 20 (Microfunnel) and Target Supply Button", () => {
     const response = {
-      artist: {
-        targetSupply: {
-          isInMicrofunnel: true,
-          isTargetSupply: true,
-        },
-        internalID: "fooBarBaz",
-        slug: "alex-katz",
-        name: "Alex Katz",
-        href: "/artist/alex-katz",
-        image: {
-          cropped: {
-            url:
-              "https://d7hftxdivxxvm.cloudfront.net?resize_to=fill&width=75&height=66&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FbrHdWfNxoereaVk2VOneuw%2Flarge.jpg",
-          },
-        },
-        id: "QXJ0aXN0OjRkOGQxMjBjODc2YzY5N2FlMTAwMDA0Ng==",
+      targetSupply: {
+        isInMicrofunnel: true,
+        isTargetSupply: true,
       },
+      internalID: "fooBarBaz",
+      slug: "alex-katz",
+      name: "Alex Katz",
+      href: "/artist/alex-katz",
+      image: {
+        cropped: {
+          url:
+            "https://d7hftxdivxxvm.cloudfront.net?resize_to=fill&width=75&height=66&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FbrHdWfNxoereaVk2VOneuw%2Flarge.jpg",
+        },
+      },
+      id: "QXJ0aXN0OjRkOGQxMjBjODc2YzY5N2FlMTAwMDA0Ng==",
     }
 
     const analyticsEvent = {
       action_type: "Click",
       context_page: "Artist",
-      context_page_owner_id: response.artist.internalID,
-      context_page_owner_slug: response.artist.slug,
+      context_page_owner_id: response.internalID,
+      context_page_owner_slug: response.slug,
       context_page_owner_type: "Artist",
       context_module: "ArtistConsignment",
       subject: "Get Started",
     }
 
     describe("desktop", () => {
-      it("renders properly when in microfunnel", async () => {
-        const wrapper = await getWrapper({ breakpoint: "md", response })
+      it("renders properly when in microfunnel", () => {
+        const wrapper = getWrapper({
+          breakpoint: "md",
+          response,
+        })
+
         expect(wrapper.find("Image").length).toEqual(1)
         expect(wrapper.text()).toContain("Sell your Alex Katz")
         expect(wrapper.find("RouterLink").html()).toContain(
@@ -99,8 +113,8 @@ describe("ArtistConsignButton", () => {
 
       it("renders properly when target supply", async () => {
         const targetSupplyResponse = cloneDeep(response)
-        targetSupplyResponse.artist.targetSupply.isInMicrofunnel = false
-        targetSupplyResponse.artist.targetSupply.isTargetSupply = true
+        targetSupplyResponse.targetSupply.isInMicrofunnel = false
+        targetSupplyResponse.targetSupply.isTargetSupply = true
         const wrapper = await getWrapper({
           breakpoint: "md",
           response: targetSupplyResponse,
@@ -110,18 +124,18 @@ describe("ArtistConsignButton", () => {
         expect(wrapper.find("RouterLink").html()).toContain(`href="/consign"`)
       })
 
-      it("guards against missing imageURL", async () => {
+      it("guards against missing imageURL", () => {
         const responseWithoutImage = cloneDeep(response)
-        responseWithoutImage.artist.image = null
-        const wrapper = await getWrapper({
+        responseWithoutImage.image = null
+        const wrapper = getWrapper({
           breakpoint: "md",
           response: responseWithoutImage,
         })
         expect(wrapper.find("Image").length).toEqual(0)
       })
 
-      it("tracks clicks", async () => {
-        const wrapper = await getWrapper({
+      it("tracks clicks", () => {
+        const wrapper = getWrapper({
           breakpoint: "md",
           response,
         })
@@ -134,8 +148,8 @@ describe("ArtistConsignButton", () => {
     })
 
     describe("mobile", () => {
-      it("renders properly when in microfunnel", async () => {
-        const wrapper = await getWrapper({ breakpoint: "xs", response })
+      it("renders properly when in microfunnel", () => {
+        const wrapper = getWrapper({ breakpoint: "xs", response })
         expect(wrapper.find("Image").length).toEqual(1)
         expect(wrapper.text()).toContain("Sell your Alex Katz")
         expect(wrapper.find("RouterLink").html()).toContain(
@@ -143,11 +157,11 @@ describe("ArtistConsignButton", () => {
         )
       })
 
-      it("renders properly when target supply", async () => {
+      it("renders properly when target supply", () => {
         const targetSupplyResponse = cloneDeep(response)
-        targetSupplyResponse.artist.targetSupply.isInMicrofunnel = false
-        targetSupplyResponse.artist.targetSupply.isTargetSupply = true
-        const wrapper = await getWrapper({
+        targetSupplyResponse.targetSupply.isInMicrofunnel = false
+        targetSupplyResponse.targetSupply.isTargetSupply = true
+        const wrapper = getWrapper({
           breakpoint: "xs",
           response: targetSupplyResponse,
         })
@@ -156,18 +170,18 @@ describe("ArtistConsignButton", () => {
         expect(wrapper.find("RouterLink").html()).toContain(`href="/consign"`)
       })
 
-      it("guards against missing imageURL", async () => {
+      it("guards against missing imageURL", () => {
         const responseWithoutImage = cloneDeep(response)
-        responseWithoutImage.artist.image = null
-        const wrapper = await getWrapper({
+        responseWithoutImage.image = null
+        const wrapper = getWrapper({
           breakpoint: "md",
           response: responseWithoutImage,
         })
         expect(wrapper.find("Image").length).toEqual(0)
       })
 
-      it("tracks clicks", async () => {
-        const wrapper = await getWrapper({
+      it("tracks clicks", () => {
+        const wrapper = getWrapper({
           breakpoint: "xs",
           response,
         })
@@ -182,45 +196,43 @@ describe("ArtistConsignButton", () => {
 
   describe("Default Button", () => {
     const response = {
-      artist: {
-        targetSupply: {
-          isInMicrofunnel: false,
-          isTargetSupply: false,
-        },
-        internalID: "fooBarBaz",
-        slug: "alex-katz",
-        name: "Andy Warhol",
-        href: "/artist/andy-warhol",
-        image: {
-          cropped: {
-            url:
-              "https://d7hftxdivxxvm.cloudfront.net?resize_to=fill&width=75&height=66&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FbrHdWfNxoereaVk2VOneuw%2Flarge.jpg",
-          },
-        },
-        id: "QXJ0aXN0OjRkOGQxMjBjODc2YzY5N2FlMTAwMDA0Ng==",
+      targetSupply: {
+        isInMicrofunnel: false,
+        isTargetSupply: false,
       },
+      internalID: "fooBarBaz",
+      slug: "alex-katz",
+      name: "Andy Warhol",
+      href: "/artist/andy-warhol",
+      image: {
+        cropped: {
+          url:
+            "https://d7hftxdivxxvm.cloudfront.net?resize_to=fill&width=75&height=66&quality=80&src=https%3A%2F%2Fd32dm0rphc51dk.cloudfront.net%2FbrHdWfNxoereaVk2VOneuw%2Flarge.jpg",
+        },
+      },
+      id: "QXJ0aXN0OjRkOGQxMjBjODc2YzY5N2FlMTAwMDA0Ng==",
     }
 
     const analyticsEvent = {
       action_type: "Click",
       context_page: "Artist",
-      context_page_owner_id: response.artist.internalID,
-      context_page_owner_slug: response.artist.slug,
+      context_page_owner_id: response.internalID,
+      context_page_owner_slug: response.slug,
       context_page_owner_type: "Artist",
       context_module: "ArtistConsignment",
       subject: "Get Started",
     }
 
     describe("desktop", () => {
-      it("renders properly", async () => {
-        const wrapper = await getWrapper({ breakpoint: "md", response })
+      it("renders properly", () => {
+        const wrapper = getWrapper({ breakpoint: "md", response })
         expect(wrapper.find("Image").length).toEqual(0)
         expect(wrapper.text()).toContain("Sell art from your collection")
         expect(wrapper.find("RouterLink").html()).toContain(`href="/consign"`)
       })
 
-      it("tracks clicks", async () => {
-        const wrapper = await getWrapper({
+      it("tracks clicks", () => {
+        const wrapper = getWrapper({
           breakpoint: "md",
           response,
         })
@@ -233,15 +245,15 @@ describe("ArtistConsignButton", () => {
     })
 
     describe("mobile", () => {
-      it("renders properly", async () => {
-        const wrapper = await getWrapper({ breakpoint: "xs", response })
+      it("renders properly", () => {
+        const wrapper = getWrapper({ breakpoint: "xs", response })
         expect(wrapper.find("Image").length).toEqual(0)
         expect(wrapper.text()).toContain("Sell art from your collection")
         expect(wrapper.find("RouterLink").html()).toContain(`href="/consign"`)
       })
 
-      it("tracks clicks", async () => {
-        const wrapper = await getWrapper({
+      it("tracks clicks", () => {
+        const wrapper = getWrapper({
           breakpoint: "xs",
           response,
         })
