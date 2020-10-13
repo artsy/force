90dcfbd840018322e2158095c0b81765630351f6
Damon <mail@damonzucconi.com>
2020-10-13 15:24:33 -0400

    [FX-2289] Fixes collection rail text overflow (#6469)
    * Bumps palette
    
    * Fixes collection rail cell truncation

--- src/v2/Apps/Collect/Routes/Collection/Components/CollectionsHubRails/FeaturedCollectionsRails/index.tsx
+++ src/v2/Apps/Collect/Routes/Collection/Components/CollectionsHubRails/FeaturedCollectionsRails/index.tsx
@@ -1,15 +1,21 @@
-import { Box, Flex, ResponsiveImage, Spacer, Text, color } from "@artsy/palette"
+import {
+  Box,
+  Flex,
+  ReadMore,
+  ResponsiveImage,
+  Spacer,
+  Text,
+  color,
+} from "@artsy/palette"
 import { FeaturedCollectionsRails_collectionGroup } from "v2/__generated__/FeaturedCollectionsRails_collectionGroup.graphql"
 import { useTracking } from "v2/Artsy/Analytics/useTracking"
 import { RouterLink } from "v2/Artsy/Router/RouterLink"
 import { Carousel } from "v2/Components/Carousel"
-import { Truncator } from "v2/Components/Truncator"
 import currency from "currency.js"
 import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import styled from "styled-components"
 import { resize } from "v2/Utils/resizer"
-import { Media } from "v2/Utils/Responsive"
 import { ContextModule, clickedCollectionGroup } from "@artsy/cohesion"
 import { useAnalyticsContext } from "v2/Artsy/Analytics/AnalyticsContext"
 
@@ -79,28 +85,6 @@ export const FeaturedCollectionEntity: React.FC<FeaturedCollectionEntityProps> =
     )
   }
 
-  const getTruncatedDescription = (lines: number) => {
-    return (
-      <Truncator
-        maxLineCount={lines}
-        ReadMoreLink={() => {
-          return (
-            <>
-              {`... `}
-              <ReadMoreLink display="inline">Read more</ReadMoreLink>
-            </>
-          )
-        }}
-      >
-        <>
-          {description && (
-            <span dangerouslySetInnerHTML={{ __html: description }} />
-          )}
-        </>
-      </Truncator>
-    )
-  }
-
   return (
     <Container p={2} width={["261px", "261px", "355px", "355px"]}>
       <StyledLink to={`/collection/${slug}`} onClick={handleClick}>
@@ -114,21 +98,25 @@ export const FeaturedCollectionEntity: React.FC<FeaturedCollectionEntityProps> =
             })}
           />
         </Flex>
-        <Text variant="subtitle" mt={1} maxWidth={["246px", "100%"]}>
-          <Truncator maxLineCount={1}>{title}</Truncator>
+
+        <Text
+          variant="subtitle"
+          mt={1}
+          maxWidth={["246px", "100%"]}
+          overflowEllipsis
+        >
+          {title}
         </Text>
+
         {price_guidance && (
           <Text variant="small" color="black60">
             {`From $${formattedPrice}`}
           </Text>
         )}
-        {/* TODO: fix this truncation/overflow issue properly, rather than this overflow:hidden Box */}
-        <Box style={{ overflow: "hidden" }}>
-          <ExtendedText mt={1}>
-            <Media lessThan="md">{getTruncatedDescription(4)}</Media>
-            <Media greaterThan="sm">{getTruncatedDescription(3)}</Media>
-          </ExtendedText>
-        </Box>
+
+        <Text variant="text" mt={1}>
+          <ReadMore maxChars={100} content={description} disabled />
+        </Text>
       </StyledLink>
     </Container>
   )
@@ -168,17 +156,6 @@ const FeaturedCollectionsContainer = styled(Box)`
   border-top: 1px solid ${color("black10")};
 `
 
-const ExtendedText = styled(Text)`
-  div span {
-    span p {
-      display: inline;
-    }
-
-    div p {
-      display: inline;
-    }
-  }
-`
 export const FeaturedImage = styled(ResponsiveImage)`
   background-position: top;
 `
@@ -190,8 +167,4 @@ export const StyledLink = styled(RouterLink)`
   &:hover {
     text-decoration: none;
   }
-`
-
-const ReadMoreLink = styled(Text).attrs({ variant: "mediumText" })`
-  text-decoration: underline;
 `
