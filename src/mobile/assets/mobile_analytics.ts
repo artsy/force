6bd438c05c96b3cb2483d5c01708e6e25f13caa1
Eve Essex <eve.essex@gmail.com>
2020-10-13 13:11:04 -0400

    address comments

--- src/mobile/assets/mobile_analytics.ts
+++ src/mobile/assets/mobile_analytics.ts
@@ -8,7 +8,7 @@ const Events = require("../../v2/Utils/Events").default
 // Send Reaction events to Segment
 Events.onEvent(trackEvent)
 
-require("../analytics/before_ready.ts")
+require("../analytics/before_ready")
 
 $(() =>
   window.analytics.ready(function () {
