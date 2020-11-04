8bf3d55aff60bf42d9ce00baf0ffc6f1808701db
dzucconi <mail@damonzucconi.com>
2020-11-04 17:36:02 -0500

    Implements artwork filter empty states

---
+++ src/v2/Apps/Show/Components/ShowArtworksEmptyState.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import { Message } from "@artsy/palette"
+import { createFragmentContainer, graphql } from "react-relay"
+import { ShowArtworksEmptyState_show } from "v2/__generated__/ShowArtworksEmptyState_show.graphql"
+
+interface ShowArtworksEmptyStateProps {
+  show: ShowArtworksEmptyState_show
+}
+
+export const ShowArtworksEmptyState: React.FC<ShowArtworksEmptyStateProps> = ({
+  show,
+}) => {
+  const message = [
+    `This ${
+      show.isFairBooth ? "fair booth" : "show"
+    } is currently unavailable.`,
+
+    ...(show.status !== "closed"
+      ? [
+          `Please check back closer to the ${
+            show.isFairBooth ? "fair" : "show"
+          } for artworks.`,
+        ]
+      : []),
+  ].join(" ")
+
+  return <Message>{message}</Message>
+}
+
+export const ShowArtworksEmptyStateFragmentContainer = createFragmentContainer(
+  ShowArtworksEmptyState,
+  {
+    show: graphql`
+      fragment ShowArtworksEmptyState_show on Show {
+        isFairBooth
+        status
+      }
+    `,
+  }
+)
---
+++ src/v2/Apps/Show/__tests__/ShowArtworksEmptyState.jest.tsx
@@ -0,0 +1,65 @@
+import { graphql } from "react-relay"
+import { ShowArtworksEmptyStateFragmentContainer } from "../Components/ShowArtworksEmptyState"
+import { ShowArtworksEmptyState_Test_Query } from "v2/__generated__/ShowArtworksEmptyState_Test_Query.graphql"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
+
+jest.unmock("react-relay")
+
+const { getWrapper } = setupTestWrapper<ShowArtworksEmptyState_Test_Query>({
+  Component: ShowArtworksEmptyStateFragmentContainer,
+  query: graphql`
+    query ShowArtworksEmptyState_Test_Query {
+      show(id: "example-show-id") {
+        ...ShowArtworksEmptyState_show
+      }
+    }
+  `,
+})
+
+describe("ShowArtworksEmptyState", () => {
+  describe("fair booth", () => {
+    it("renders the correct message for non-closed fair booths", () => {
+      const wrapper = getWrapper({ Show: () => ({ isFairBooth: true }) })
+      const html = wrapper.html()
+
+      expect(html).toContain(
+        "This fair booth is currently unavailable. Please check back closer to the fair for artworks."
+      )
+    })
+
+    it("renders the correct message for closed fair booths", () => {
+      const wrapper = getWrapper({
+        Show: () => ({ isFairBooth: true, status: "closed" }),
+      })
+      const html = wrapper.html()
+
+      expect(html).toContain("This fair booth is currently unavailable.")
+      expect(html).not.toContain(
+        "Please check back closer to the fair for artworks."
+      )
+    })
+  })
+
+  describe("show", () => {
+    it("renders the correct message for non-closed shows", () => {
+      const wrapper = getWrapper({ Show: () => ({ isFairBooth: false }) })
+      const html = wrapper.html()
+
+      expect(html).toContain(
+        "This show is currently unavailable. Please check back closer to the show for artworks."
+      )
+    })
+
+    it("renders the correct message for closed shows", () => {
+      const wrapper = getWrapper({
+        Show: () => ({ isFairBooth: false, status: "closed" }),
+      })
+      const html = wrapper.html()
+
+      expect(html).toContain("This show is currently unavailable.")
+      expect(html).not.toContain(
+        "Please check back closer to the show for artworks."
+      )
+    })
+  })
+})
