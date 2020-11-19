d122dd9bcdde35b6caee0b0bfd17f4d22cd41999
sepans <sepans@sepans.com>
2020-11-19 14:55:07 -0500

    Update TrustSignal tests to use new relay test framework

--- src/v2/Apps/Artwork/Components/TrustSignals/__tests__/VerifiedSeller.jest.tsx
+++ src/v2/Apps/Artwork/Components/TrustSignals/__tests__/VerifiedSeller.jest.tsx
@@ -1,79 +1,70 @@
-import {
-  VerifiedSellerTestQueryRawResponse,
-  VerifiedSellerTestQueryResponse,
-} from "v2/__generated__/VerifiedSellerTestQuery.graphql"
-import { renderRelayTree } from "v2/DevTools"
-import React from "react"
 import { graphql } from "react-relay"
-import { ExtractProps } from "v2/Utils/ExtractProps"
 import { VerifiedSellerFragmentContainer } from "../VerifiedSeller"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
 
 jest.unmock("react-relay")
 
-const render = (
-  artwork: VerifiedSellerTestQueryRawResponse["artwork"],
-  extraProps?: Partial<ExtractProps<typeof VerifiedSellerFragmentContainer>>
-) =>
-  renderRelayTree({
-    Component: (props: VerifiedSellerTestQueryResponse) => (
-      <VerifiedSellerFragmentContainer
-        artwork={{
-          ...artwork,
-        }}
-        {...props}
-        {...extraProps}
-      />
-    ),
-    mockData: {
-      artwork,
-    } as VerifiedSellerTestQueryRawResponse,
-    query: graphql`
-      query VerifiedSellerTestQuery @raw_response_type {
-        artwork(id: "whatevs") {
-          ...VerifiedSeller_artwork
-        }
+const { getWrapper } = setupTestWrapper({
+  Component: VerifiedSellerFragmentContainer,
+  query: graphql`
+    query VerifiedSellerTestQuery {
+      artwork(id: "whatevs") {
+        ...VerifiedSeller_artwork
       }
-    `,
-  })
+    }
+  `,
+})
 
 const partnerName = "partner-name"
 
 describe("VerifiedSeller", () => {
   it("Doesn't render when the partner is a verified seller", async () => {
-    const component = await render({
-      id: "opaque-seller-id",
-      partner: {
-        id: "opaque-partner-id",
-        name: partnerName,
-        isVerifiedSeller: false,
+    const component = getWrapper({
+      Artwork: () => {
+        return {
+          id: "opaque-seller-id",
+          is_biddable: false,
+          partner: {
+            id: "opaque-partner-id",
+            isVerifiedSeller: false,
+            name: partnerName,
+          },
+        }
       },
-      is_biddable: false,
     })
     expect(component.find("TrustSignal").length).toBe(0)
   })
 
   it("Doesn't render when the artwork is biddable", async () => {
-    const component = await render({
-      id: "opaque-seller-id",
-      partner: {
-        id: "opaque-partner-id",
-        name: partnerName,
-        isVerifiedSeller: true,
+    const component = getWrapper({
+      Artwork: () => {
+        return {
+          id: "opaque-seller-id",
+          is_biddable: true,
+          partner: {
+            id: "opaque-partner-id",
+            isVerifiedSeller: true,
+            name: partnerName,
+          },
+        }
       },
-      is_biddable: true,
     })
     expect(component.find("TrustSignal").length).toBe(0)
   })
 
   it("Renders when the partner is a verified seller, but the work is not biddable", async () => {
-    const component = await render({
-      id: "opaque-seller-id",
-      partner: {
-        id: "opaque-partner-id",
-        name: partnerName,
-        isVerifiedSeller: true,
+    const component = getWrapper({
+      Artwork: () => {
+        return {
+          id: "opaque-seller-id",
+          is_biddable: false,
+          partner: {
+            id: "opaque-partner-id",
+            isVerifiedSeller: true,
+            name: partnerName,
+          },
+        }
       },
-      is_biddable: false,
     })
     expect(component.find("TrustSignal").length).toBe(1)
     expect(component.text()).toContain(partnerName)
