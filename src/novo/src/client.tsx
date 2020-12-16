c61513d183576e85c5ad1cd81233e84590c41100
Cameron Rollheiser <cameron.rollheiser@artsymail.com>
2020-12-16 16:30:43 -0500

    feature: Third Party Scripts
    Adds the following third party scripts:
    
    - Stripe
    - Sailthru

--- src/novo/src/client.tsx
+++ src/novo/src/client.tsx
@@ -7,37 +7,52 @@ import { loadableReady } from "@loadable/component"
 import { logoutEventHandler } from "desktop/lib/logoutHandler"
 import { mediator } from "lib/mediator"
 import { beforeAnalyticsReady, onAnalyticsReady } from "lib/analytics/helpers"
+import { loadScript } from "lib/scriptLoader"
+import sharify from "sharify"
 
-import("v2/Artsy/Router/client")
-  .then(({ buildClientApp }) => {
-    return buildClientApp({
+async function loadStripe() {
+  if (sharify.data.THIRD_PARTIES_DISABLED) {
+    return
+  }
+  return loadScript("https://js.stripe.com/v3/")
+}
+
+;(async () => {
+  try {
+    // Generate client
+    const { buildClientApp } = await import("v2/Artsy/Router/client")
+    const { ClientApp } = await buildClientApp({
       routes: getAppNovoRoutes(),
     })
-  })
-  .then(({ ClientApp }) => {
-    loadableReady(() => {
+
+    // Rehydrate
+    await loadableReady(() => {
       ReactDOM.hydrate(<ClientApp />, document.getElementById("react-root"))
     })
-  })
-  .then(() => {
+
+    // Attach analytics
     beforeAnalyticsReady()
     window.analytics.ready(() => {
       onAnalyticsReady()
     })
-  })
-  .then(() => {
-    return import("desktop/apps/authentication/client/initModalManager")
-  })
-  .then(({ initModalManager }) => {
-    initModalManager()
-  })
-  .then(() => {
-    mediator.on("auth:logout", logoutEventHandler)
-  })
-  .catch(error => {
-    console.error(error)
-  })
 
+    // Wire up auth modal
+    const { initModalManager } = await import(
+      "desktop/apps/authentication/client/initModalManager"
+    )
+    initModalManager()
+
+    // Logout handler
+    mediator.on("auth:logout", logoutEventHandler)
+
+    // Third party analytics
+    await loadStripe()
+  } catch (error) {
+    console.error(error)
+  }
+})()
+
+// Enable hot-reloading if available.
 if (module.hot) {
   module.hot.accept()
 }
