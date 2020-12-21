2f069d2829ff27626951e121c46621c073ec1cae
dzucconi <mail@damonzucconi.com>
2020-12-21 12:29:38 -0500

    chore: exports analytics context from index

--- src/v2/Artsy/Analytics/index.ts
+++ src/v2/Artsy/Analytics/index.ts
@@ -3,4 +3,5 @@ import * as Schema from "./Schema"
 export * from "./Schema"
 export * from "./track"
 export * from "./useTracking"
+export * from "./AnalyticsContext"
 export const AnalyticsSchema = Schema
