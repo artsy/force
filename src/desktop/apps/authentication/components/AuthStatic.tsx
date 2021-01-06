d637bffa04f67331e9e54167e6df6f2513b828ab
Will Doenlen <wdoenlen@gmail.com>
2021-01-06 14:42:55 -0500

    Update mobile sign up to use desktop signup flow

--- src/desktop/apps/authentication/components/AuthStatic.tsx
+++ src/desktop/apps/authentication/components/AuthStatic.tsx
@@ -3,43 +3,47 @@ import { FormSwitcher } from "v2/Components/Authentication/FormSwitcher"
 import { ModalOptions, ModalType } from "v2/Components/Authentication/Types"
 import { ModalHeader } from "v2/Components/Modal/ModalHeader"
 import { handleSubmit } from "../helpers"
-import { Box, Flex } from "@artsy/palette"
+import { Box, Flex, FlexProps } from "@artsy/palette"
 
-interface Props {
-  type: string
+interface Props extends FlexProps {
+  type: ModalType
   meta: {
     title?: string
   }
   options: ModalOptions
 }
 
-export class AuthStatic extends React.Component<Props> {
-  render() {
-    const {
-      type,
-      meta: { title },
-      options,
-    } = this.props
-    return (
-      <Flex alignItems="center" justifyContent="center" height="90vh">
-        <Box py={2} maxWidth={400}>
-          <ModalHeader title={title} hasLogo />
-          <FormSwitcher
-            {...this.props}
-            type={type as ModalType}
-            isStatic
-            handleSubmit={handleSubmit.bind(this, type, options)}
-            submitUrls={{
-              login: "/log_in",
-              forgot: "/forgot_password",
-              signup: "/sign_up",
-              apple: "/users/auth/apple",
-              facebook: "/users/auth/facebook",
-            }}
-            showRecaptchaDisclaimer={true}
-          />
-        </Box>
-      </Flex>
-    )
-  }
+export const AuthStatic: React.FC<Props> = props => {
+  const {
+    type,
+    meta: { title },
+    options,
+    ...flexProps
+  } = props
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="center"
+      height="90vh"
+      {...flexProps}
+    >
+      <Box py={2} maxWidth={400}>
+        <ModalHeader title={title} hasLogo />
+        <FormSwitcher
+          {...props}
+          type={type}
+          isStatic
+          handleSubmit={handleSubmit.bind(this, type, options)}
+          submitUrls={{
+            apple: "/users/auth/apple",
+            facebook: "/users/auth/facebook",
+            forgot: "/forgot_password",
+            login: "/log_in",
+            signup: "/sign_up",
+          }}
+          showRecaptchaDisclaimer={true}
+        />
+      </Box>
+    </Flex>
+  )
 }
---
+++ src/desktop/apps/authentication/components/FullPageAuthStatic.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import { AuthStatic } from "./AuthStatic"
+
+export const FullPageAuthStatic: React.FC = (props: any) => {
+  return <AuthStatic px={3} {...props} />
+}
