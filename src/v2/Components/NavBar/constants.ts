343ad812235c91d4ba45545713a16e938b93e2a3
Will Doenlen <wdoenlen@gmail.com>
2021-02-02 15:09:51 -0500

    Adjust NAV_BAR_HEIGHT'

--- src/v2/Components/NavBar/constants.ts
+++ src/v2/Components/NavBar/constants.ts
@@ -1,6 +1,7 @@
 import { space } from "@artsy/palette"
 
 export const NAV_BAR_BORDER_OFFSET = 1
-export const NAV_BAR_HEIGHT = space(6) - NAV_BAR_BORDER_OFFSET
 export const NAV_BAR_TOP_TIER_HEIGHT = space(6) - NAV_BAR_BORDER_OFFSET
 export const NAV_BAR_BOTTOM_TIER_HEIGHT = 44
+export const NAV_BAR_HEIGHT =
+  NAV_BAR_TOP_TIER_HEIGHT + NAV_BAR_BOTTOM_TIER_HEIGHT
