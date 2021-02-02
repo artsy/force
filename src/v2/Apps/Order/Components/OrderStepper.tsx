65a8500b504ebfbea014c0a53c9593da28204638
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2021-02-02 13:39:25 -0800

    Conditional rendering styled OrderStepper

--- src/v2/Apps/Order/Components/OrderStepper.tsx
+++ src/v2/Apps/Order/Components/OrderStepper.tsx
@@ -1,4 +1,6 @@
+import { Media } from "v2/Utils/Responsive"
 import { Step, Stepper } from "@artsy/palette"
+import styled from "styled-components"
 import React from "react"
 
 function typedArray<T extends string>(...elems: T[]): T[] {
@@ -14,6 +16,10 @@ export const offerFlowSteps = typedArray(
 export const buyNowFlowSteps = typedArray("Shipping", "Payment", "Review")
 export const counterofferFlowSteps = typedArray("Respond", "Review")
 
+const StyledContainer = styled.div`
+  padding-left: 20px;
+`
+
 export function OrderStepper<Steps extends string[]>({
   currentStep,
   steps,
@@ -23,15 +29,33 @@ export function OrderStepper<Steps extends string[]>({
 }) {
   const stepIndex = steps.indexOf(currentStep)
   return (
-    <Stepper
-      initialTabIndex={stepIndex}
-      currentStepIndex={stepIndex}
-      disableNavigation
-      autoScroll
-    >
-      {steps.map(step => (
-        <Step name={step} key={step} />
-      ))}
-    </Stepper>
+    <>
+      <Media between={["xs", "md"]}>
+        <StyledContainer>
+          <Stepper
+            initialTabIndex={stepIndex}
+            currentStepIndex={stepIndex}
+            disableNavigation
+            autoScroll
+          >
+            {steps.map(step => (
+              <Step name={step} key={step} />
+            ))}
+          </Stepper>
+        </StyledContainer>
+      </Media>
+      <Media greaterThan="sm">
+        <Stepper
+          initialTabIndex={stepIndex}
+          currentStepIndex={stepIndex}
+          disableNavigation
+          autoScroll
+        >
+          {steps.map(step => (
+            <Step name={step} key={step} />
+          ))}
+        </Stepper>
+      </Media>
+    </>
   )
 }
