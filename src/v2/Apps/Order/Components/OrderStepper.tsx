03fcd5db39ce15d17a786910ee9337f830eb4fb3
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2021-02-04 14:04:06 -0800

    Added padding on the right mobile view as well per PR suggestions

--- src/v2/Apps/Order/Components/OrderStepper.tsx
+++ src/v2/Apps/Order/Components/OrderStepper.tsx
@@ -26,7 +26,7 @@ export function OrderStepper<Steps extends string[]>({
   return (
     <>
       <Media between={["xs", "md"]}>
-        <Box pl={2}>
+        <Box pl={2} pr={2}>
           <Stepper
             initialTabIndex={stepIndex}
             currentStepIndex={stepIndex}
