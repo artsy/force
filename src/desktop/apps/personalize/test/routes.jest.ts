3dc68358b9c09fdb7b2d2bc893bcfd2c517ac379
Jon Allured <jon.allured@gmail.com>
2021-02-12 09:04:12 -0600

    Add tests for redirecting signed out users

--- src/desktop/apps/personalize/test/routes.jest.ts
+++ src/desktop/apps/personalize/test/routes.jest.ts
@@ -1,4 +1,4 @@
-import { index } from "../routes"
+import { index, ensureLoggedInUser } from "../routes"
 const CurrentUser = require("desktop/models/current_user")
 
 jest.mock("@artsy/stitch", () => ({
@@ -32,9 +32,44 @@ describe("Personalize routes", () => {
     }
   })
 
-  it("renders the personalize app", () => {
-    index(req, res, next)
-    expect(stichMock.mock.calls[0][0].data.title).toBe("Personalize | Artsy")
-    expect(stichMock.mock.calls[0][0].locals.assetPackage).toBe("onboarding")
+  describe("index route", () => {
+    it("renders the personalize app", () => {
+      index(req, res, next)
+      expect(stichMock.mock.calls[0][0].data.title).toBe("Personalize | Artsy")
+      expect(stichMock.mock.calls[0][0].locals.assetPackage).toBe("onboarding")
+    })
+  })
+
+  describe("ensureLoggedInUser route", () => {
+    const mockNext = jest.fn()
+    const mockRedirect = jest.fn()
+
+    const request = {}
+    const response = {
+      locals: { sd: { CURRENT_USER: undefined } },
+      redirect: mockRedirect,
+    }
+
+    beforeEach(() => {
+      jest.clearAllMocks()
+    })
+
+    it("redirects when there is no current user", () => {
+      response.locals.sd.CURRENT_USER = undefined
+
+      ensureLoggedInUser(request, response, mockNext)
+
+      expect(mockRedirect).toBeCalledWith("/personalize")
+      expect(mockNext).not.toHaveBeenCalled()
+    })
+
+    it("calls next when there is a user", () => {
+      response.locals.sd.CURRENT_USER = {}
+
+      ensureLoggedInUser(request, response, mockNext)
+
+      expect(mockRedirect).not.toHaveBeenCalled()
+      expect(mockNext).toHaveBeenCalled()
+    })
   })
 })
