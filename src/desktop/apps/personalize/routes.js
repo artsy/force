dc30f2cc2f5ca71432c62d69758b459cf333d761
Jon Allured <jon.allured@gmail.com>
2021-02-12 09:12:44 -0600

    Extract func to compute stitch options

--- src/desktop/apps/personalize/routes.js
+++ src/desktop/apps/personalize/routes.js
@@ -2,31 +2,37 @@ import * as _ from "underscore"
 import { stitch } from "@artsy/stitch"
 import { App } from "desktop/apps/personalize/components/App"
 
+const computeStitchOptions = (request, response) => {
+  const options = {
+    basePath: request.app.get("views"),
+    config: {
+      styledComponents: true,
+    },
+    layout: "../../components/main_layout/templates/react_blank_index.jade",
+    blocks: {
+      head: "./meta.jade",
+      body: App,
+    },
+    locals: {
+      ...response.locals,
+      assetPackage: "onboarding",
+    },
+    data: {
+      title: "Personalize | Artsy",
+      currentUser: response.locals.sd.CURRENT_USER,
+      redirectTo: request.query.redirectTo,
+      forceStep: request.params.slug,
+    },
+  }
+
+  return options
+}
+
 export const index = async (req, res, next) => {
   try {
-    const options = {
-      basePath: req.app.get("views"),
-      config: {
-        styledComponents: true,
-      },
-      layout: "../../components/main_layout/templates/react_blank_index.jade",
-      blocks: {
-        head: "./meta.jade",
-        body: App,
-      },
-      locals: {
-        ...res.locals,
-        assetPackage: "onboarding",
-      },
-      data: {
-        title: "Personalize | Artsy",
-        currentUser: res.locals.sd.CURRENT_USER,
-        redirectTo: req.query.redirectTo,
-        forceStep: req.params.slug,
-      },
-    }
-
+    const options = computeStitchOptions(req, res)
     const layout = await stitch(options)
+
     res.send(layout)
   } catch (error) {
     next(error)
