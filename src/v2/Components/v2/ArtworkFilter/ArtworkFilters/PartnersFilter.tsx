e34fa4e275aca5cb7ad5bd8578db1ff4807c95cf
Matthew Zikherman <mzikherman@gmail.com>
2021-02-23 18:03:23 -0500

    chore: update location and partner filter to use ShowMore component

--- src/v2/Components/v2/ArtworkFilter/ArtworkFilters/PartnersFilter.tsx
+++ src/v2/Components/v2/ArtworkFilter/ArtworkFilters/PartnersFilter.tsx
@@ -1,97 +1,61 @@
-import { Checkbox, Flex, Text, Toggle } from "@artsy/palette"
+import { Checkbox, Flex, Toggle } from "@artsy/palette"
 import { sortBy } from "lodash"
-import React, { FC, useState } from "react"
-import styled from "styled-components"
+import React, { FC } from "react"
 
 import { useArtworkFilterContext } from "../ArtworkFilterContext"
 import { OptionText } from "./OptionText"
 import { PartnerAutosuggest } from "./PartnerAutosuggest"
-
-const ToggleLink = styled(Text)`
-  cursor: pointer;
-  text-decoration: underline;
-`
+import { ShowMore } from "./ShowMore"
 
 const INITIAL_PARTNERS_TO_SHOW = 6
 
-export const PartnersFilter: FC = () => {
-  const { aggregations, ...filterContext } = useArtworkFilterContext()
-  const partners = aggregations.find(agg => agg.slice === "PARTNER")
+const PartnerOption: React.FC<{ name: string }> = ({ name }) => {
+  const { currentlySelectedFilters, setFilter } = useArtworkFilterContext()
 
-  const [expanded, setExpanded] = useState(
-    filterContext.currentlySelectedFilters().partnerIDs.length > 0
+  const toggleLocationSelection = (selected, slug) => {
+    let locations = currentlySelectedFilters().locationCities.slice()
+    if (selected) {
+      locations.push(slug)
+    } else {
+      locations = locations.filter(item => item !== slug)
+    }
+    setFilter("partnerIDs", locations)
+  }
+  const selected = currentlySelectedFilters().partnerIDs.includes(name)
+  const props = {
+    onSelect: selected => {
+      toggleLocationSelection(selected, name)
+    },
+    selected,
+    key: name,
+  }
+
+  return (
+    <Checkbox {...props}>
+      <OptionText>{name}</OptionText>
+    </Checkbox>
   )
+}
+
+export const PartnersFilter: FC = () => {
+  const { aggregations } = useArtworkFilterContext()
+  const partners = aggregations.find(agg => agg.slice === "PARTNER")
 
   if (!(partners && partners.counts)) {
     return null
   }
 
   const partnersSorted = sortBy(partners.counts, ["count"]).reverse()
-  const initialPartnersGroup = partnersSorted.slice(0, INITIAL_PARTNERS_TO_SHOW)
-  const remainingPartnersGroup =
-    partnersSorted.length > INITIAL_PARTNERS_TO_SHOW
-      ? partnersSorted.slice(INITIAL_PARTNERS_TO_SHOW - partnersSorted.length)
-      : []
-
-  const toggle = () => setExpanded(!expanded)
-
-  const togglePartnerSelection = (selected, slug) => {
-    let partnerIDs = filterContext.currentlySelectedFilters().partnerIDs.slice()
-    if (selected) {
-      partnerIDs.push(slug)
-    } else {
-      partnerIDs = partnerIDs.filter(item => item !== slug)
-    }
-    filterContext.setFilter("partnerIDs", partnerIDs)
-  }
-
-  const ExpandControl = () => (
-    <ToggleLink mt={1} onClick={toggle}>
-      Show {remainingPartnersGroup.length} more
-    </ToggleLink>
-  )
-
-  const HideControl = () => (
-    <ToggleLink mt={1} onClick={toggle}>
-      Hide list
-    </ToggleLink>
-  )
-
-  const renderPartnerGroup = artistGroup => {
-    return artistGroup.map(({ value: slug, name }, index) => {
-      const selected = filterContext
-        .currentlySelectedFilters()
-        .partnerIDs.includes(slug)
-      const props = {
-        key: index,
-        onSelect: selected => {
-          togglePartnerSelection(selected, slug)
-        },
-        selected,
-      }
-
-      return (
-        <Checkbox {...props}>
-          <OptionText>{name}</OptionText>
-        </Checkbox>
-      )
-    })
-  }
 
   return (
     <Toggle label="Galleries and institutions" expanded>
       <Flex flexDirection="column">
         <PartnerAutosuggest partners={partners.counts} />
-        {renderPartnerGroup(initialPartnersGroup)}
-
-        {!expanded && remainingPartnersGroup.length && <ExpandControl />}
-
-        {expanded && (
-          <>
-            {renderPartnerGroup(remainingPartnersGroup)}
-            <HideControl />
-          </>
-        )}
+        <ShowMore initial={INITIAL_PARTNERS_TO_SHOW}>
+          {partnersSorted.map(({ name }) => {
+            return <PartnerOption key={name} name={name} />
+          })}
+        </ShowMore>
       </Flex>
     </Toggle>
   )
--- src/v2/Components/v2/ArtworkFilter/ArtworkFilters/__tests__/PartnersFilter.jest.tsx
+++ src/v2/Components/v2/ArtworkFilter/ArtworkFilters/__tests__/PartnersFilter.jest.tsx
@@ -24,58 +24,5 @@ describe("PartnersFilter", () => {
       })
       expect(wrapper.find("Checkbox").first().text()).toContain("Percy Z")
     })
-
-    describe("show more", () => {
-      const wrapper = getWrapper({
-        counts: { followedArtists: 0 },
-        aggregations: [
-          {
-            slice: "PARTNER",
-            counts: [
-              { name: "Percy A", count: 10, value: "percy-a" },
-              { name: "Percy B", count: 9, value: "percy-b" },
-              { name: "Percy C", count: 8, value: "percy-c" },
-              { name: "Percy D", count: 7, value: "percy-d" },
-              { name: "Percy E", count: 6, value: "percy-e" },
-              { name: "Percy F", count: 5, value: "percy-f" },
-              { name: "Percy G", count: 4, value: "percy-g" },
-            ],
-          },
-        ],
-      })
-
-      it("renders the first 6 and includes a show more expand link", () => {
-        expect(wrapper.text()).toContain("Percy A")
-        expect(wrapper.text()).toContain("Percy B")
-        expect(wrapper.text()).toContain("Percy C")
-        expect(wrapper.text()).toContain("Percy D")
-        expect(wrapper.text()).toContain("Percy E")
-        expect(wrapper.text()).toContain("Percy F")
-        expect(wrapper.text()).toContain("Show 1 more")
-        expect(wrapper.text()).not.toContain("Percy G")
-      })
-
-      it("reveals the rest of the list when 'Show more' is clicked, and can then hide the list again", done => {
-        wrapper
-          .findWhere(t => t.text() === "Show 1 more")
-          .first()
-          .simulate("click")
-
-        setTimeout(() => {
-          expect(wrapper.text()).toContain("Percy G")
-          expect(wrapper.text()).toContain("Hide list")
-          wrapper
-            .findWhere(t => t.text() === "Hide list")
-            .first()
-            .simulate("click")
-
-          setTimeout(() => {
-            expect(wrapper.text()).toContain("Show 1 more")
-            expect(wrapper.text()).not.toContain("Percy G")
-            done()
-          }, 0)
-        }, 0)
-      })
-    })
   })
 })
