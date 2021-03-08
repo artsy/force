9aa7cb890a873f72b665c30c8330e5cd2ae0d006
Jon Allured <jon.allured@gmail.com>
2021-03-08 16:22:39 -0600

    Add missing landing pages

--- src/desktop/apps/sitemaps/helpers.ts
+++ src/desktop/apps/sitemaps/helpers.ts
@@ -2,9 +2,23 @@ export const computeLandingPages = appUrl => {
   const urls = [
     { loc: appUrl, priority: 1 },
     { loc: appUrl + "/about", priority: 1 },
+    { loc: appUrl + "/all-cities" },
+    { loc: appUrl + "/art-fairs" },
+    { loc: appUrl + "/articles" },
     { loc: appUrl + "/artists", priority: 1 },
+    { loc: appUrl + "/artsy-education" },
+    { loc: appUrl + "/auction-info" },
+    { loc: appUrl + "/auction-partnerships" },
+    { loc: appUrl + "/auctions" },
+    { loc: appUrl + "/buying-with-artsy" },
     { loc: appUrl + "/categories" },
     { loc: appUrl + "/collect", priority: 1 },
+    { loc: appUrl + "/conditions-of-sale" },
+    { loc: appUrl + "/consign" },
+    { loc: appUrl + "/contact" },
+    { loc: appUrl + "/institution-partnerships" },
+    { loc: appUrl + "/institutions" },
+    { loc: appUrl + "/jobs" },
     { loc: appUrl + "/login" },
     { loc: appUrl + "/press/in-the-media" },
     { loc: appUrl + "/press/press-releases" },
@@ -13,6 +27,7 @@ export const computeLandingPages = appUrl => {
     { loc: appUrl + "/shows", priority: 1 },
     { loc: appUrl + "/signup" },
     { loc: appUrl + "/terms" },
+    { loc: appUrl + "/viewing-rooms" },
   ]
 
   return urls
