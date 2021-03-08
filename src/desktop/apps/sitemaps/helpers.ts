8e630711e622c436bd8362fe375d603bb8d8ef07
Jon Allured <jon.allured@gmail.com>
2021-03-08 16:22:32 -0600

    Use newer login/signup routes

--- src/desktop/apps/sitemaps/helpers.ts
+++ src/desktop/apps/sitemaps/helpers.ts
@@ -5,13 +5,13 @@ export const computeLandingPages = appUrl => {
     { loc: appUrl + "/artists", priority: 1 },
     { loc: appUrl + "/categories" },
     { loc: appUrl + "/collect", priority: 1 },
-    { loc: appUrl + "/log_in" },
+    { loc: appUrl + "/login" },
     { loc: appUrl + "/press/in-the-media" },
     { loc: appUrl + "/press/press-releases" },
     { loc: appUrl + "/privacy" },
     { loc: appUrl + "/security" },
     { loc: appUrl + "/shows", priority: 1 },
-    { loc: appUrl + "/sign_up" },
+    { loc: appUrl + "/signup" },
     { loc: appUrl + "/terms" },
   ]
 
