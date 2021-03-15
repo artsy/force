087752cdead333fc2aa5f5aa80687b0cfaf25893
dzucconi <mail@damonzucconi.com>
2021-03-15 12:24:44 -0400

    fix(userouter): guards against cases without context (specs)

--- src/v2/Artsy/Router/useRouter.tsx
+++ src/v2/Artsy/Router/useRouter.tsx
@@ -1,21 +1,21 @@
 import { useContext } from "react"
-
 import { Match, Router, RouterContext } from "found"
 
 export function useRouter(): {
   match: Match
   router: Router
 } {
-  const { match, router } = useContext(RouterContext)
-  return {
-    match,
-    router,
-  }
+  const { match, router } = useContext(RouterContext) ?? {}
+  return { match, router }
 }
 
 export function useIsRouteActive(to, options = { exact: true }): boolean {
   const { match, router } = useRouter()
+
+  if (match === undefined && router === undefined) return false
+
   const toLocation = router.createLocation(to)
   const isActive = router.isActive(match, toLocation, options)
+
   return isActive
 }
