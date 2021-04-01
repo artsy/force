81f1c4e8f1806216807d367156b0768c8f64cf45
Ole <ole.richter91@gmail.com>
2021-04-01 17:05:40 +0200

    bring back modal

--- src/v2/Apps/Order/OrderApp.tsx
+++ src/v2/Apps/Order/OrderApp.tsx
@@ -44,6 +44,11 @@ class OrderApp extends React.Component<OrderAppProps, {}> {
     if (window.zEmbed) {
       window.zEmbed.show()
     }
+
+    this.mediator.on(
+      "openOrdersContactArtsyModal",
+      this.openAskSpecialistInquireableModal
+    )
   }
 
   componentWillUnmount() {
@@ -57,6 +62,40 @@ class OrderApp extends React.Component<OrderAppProps, {}> {
     if (window.zEmbed) {
       window.zEmbed.hide()
     }
+
+    this.mediator.off("openOrdersContactArtsyModal")
+  }
+
+  openAskSpecialistInquireableModal = () => {
+    const User = require("desktop/models/user.coffee")
+    const Artwork = require("desktop/models/artwork.coffee")
+    const ArtworkInquiry = require("desktop/models/artwork_inquiry.coffee")
+    const openInquiryQuestionnaireFor = require("desktop/components/inquiry_questionnaire/index.coffee")
+    const $ = require("jquery")
+
+    const user = User.instantiate()
+    const inquiry = new ArtworkInquiry({ notification_delay: 600 })
+
+    const artworkId = get(
+      this.props,
+      props => props.order.lineItems.edges[0].node.artwork.slug
+    )
+    const artwork = new Artwork({
+      id: artworkId,
+    })
+
+    // Set token to be able to load user's collector profile later
+    $.ajaxSettings.headers = {
+      "X-ACCESS-TOKEN":
+        sd.CURRENT_USER != null ? sd.CURRENT_USER.accessToken : undefined,
+      "X-XAPP-TOKEN": sd.ARTSY_XAPP_TOKEN,
+    }
+    openInquiryQuestionnaireFor({
+      artwork,
+      ask_specialist: true,
+      inquiry,
+      user,
+    })
   }
 
   preventHardReload = event => {
