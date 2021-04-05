f89df65002734a54e50fdb779898b92de054adad
Ivan Badyulya <ivan.badyulya@artsymail.com>
2021-04-05 19:30:28 +0300

    Apply pagination with createRefetchContainer

--- src/v2/Apps/Partner/Routes/Articles/index.tsx
+++ src/v2/Apps/Partner/Routes/Articles/index.tsx
@@ -1,59 +1,117 @@
 import React from "react"
-import { Column, GridColumns, Box } from "@artsy/palette"
-import {
-  createPaginationContainer,
-  graphql,
-  RelayPaginationProp,
-} from "react-relay"
+import { Column, GridColumns, Box, Col, Row } from "@artsy/palette"
+import { createRefetchContainer, graphql, RelayRefetchProp } from "react-relay"
 import { Articles_partner } from "v2/__generated__/Articles_partner.graphql"
-import { ArticleCardFragmentContainer } from "../../Components/PartnerArticles/ArticleCard"
+import { ArticleCardFragmentContainer as ArticleCard } from "../../Components/PartnerArticles/ArticleCard"
+import { PaginationFragmentContainer as Pagination } from "v2/Components/Pagination"
 
 interface ArticlesProps {
   partner: Articles_partner
-  relay: RelayPaginationProp
+  relay: RelayRefetchProp
 }
 
-const Articles: React.FC<ArticlesProps> = ({ partner, relay }) => {
+const Articles: React.FC<ArticlesProps> = ({ partner, relay, ...rest }) => {
+  if (!partner.articlesConnection) {
+    return null
+  }
+
   const {
-    articlesConnection: { edges: articles },
+    articlesConnection: {
+      edges: articles,
+      pageInfo: { hasNextPage, endCursor },
+      pageCursors,
+    },
+    slug,
   } = partner
+
+  const scrollToTop = () => {
+    window.scrollTo(0, 0)
+  }
+
+  const loadAfter = cursor => {
+    scrollToTop()
+
+    relay.refetch(
+      {
+        first: 18,
+        after: cursor,
+        partnerID: slug,
+        before: null,
+        last: null,
+      },
+      null,
+      error => {
+        if (error) {
+          console.error(error)
+        }
+      }
+    )
+  }
+
+  const loadNext = () => {
+    scrollToTop()
+
+    if (hasNextPage) {
+      loadAfter(endCursor)
+    }
+  }
 
   return (
     <Box mt={6}>
+      <span id="jumpto-ArticlesPageTop" />
+
       <GridColumns gridRowGap={[2, 4]}>
         {articles.map(({ node: article }) => {
           return (
             <Column key={article.internalID} span={4}>
-              <ArticleCardFragmentContainer article={article} />
+              <ArticleCard article={article} />
             </Column>
           )
         })}
       </GridColumns>
+      <Row>
+        <Col>
+          <Box>
+            <Pagination
+              getHref={() => ""}
+              hasNextPage={hasNextPage}
+              pageCursors={pageCursors}
+              onClick={loadAfter}
+              onNext={loadNext}
+              scrollTo="#jumpto-ArticlesPageTop"
+            />
+          </Box>
+        </Col>
+      </Row>
     </Box>
   )
 }
 
-export const ARTICLES_QUERY = graphql`
-  query ArticlesQuery($partnerId: String!, $first: Int!, $after: String) {
-    partner(id: $partnerId) {
-      ...Articles_partner @arguments(first: $first, after: $after)
-    }
-  }
-`
-
-export const ArticlesPaginationContainer = createPaginationContainer(
+export const ArticlesPaginationContainer = createRefetchContainer(
   Articles,
   {
     partner: graphql`
       fragment Articles_partner on Partner
         @argumentDefinitions(
           first: { type: "Int", defaultValue: 18 }
+          last: { type: "Int" }
           after: { type: "String" }
+          before: { type: "String" }
         ) {
         slug
-        articlesConnection(first: $first, after: $after)
-          @connection(key: "ArticlesQuery_articlesConnection") {
-          totalCount
+        articlesConnection(
+          first: $first
+          last: $last
+          after: $after
+          before: $before
+        ) @connection(key: "ArticlesQuery_articlesConnection") {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          pageCursors {
+            ...Pagination_pageCursors
+          }
           edges {
             node {
               internalID
@@ -64,11 +122,18 @@ export const ArticlesPaginationContainer = createPaginationContainer(
       }
     `,
   },
-  {
-    query: ARTICLES_QUERY,
-    direction: "forward",
-    getVariables({ partner: { slug: id } }, { cursor: after }, { first }) {
-      return { after, first, id }
-    },
-  }
+  graphql`
+    query ArticlesQuery(
+      $partnerId: String!
+      $first: Int
+      $last: Int
+      $after: String
+      $before: String
+    ) {
+      partner(id: $partnerId) {
+        ...Articles_partner
+          @arguments(first: $first, last: $last, after: $after, before: $before)
+      }
+    }
+  `
 )
