b331d54b6a06a13ba7d5391120cdf0c6fa22d1c9
Ivan Badyulya <ivan.badyulya@artsymail.com>
2021-04-08 20:29:41 +0300

    Add artwork grid on works page; Made basic layout without pagination, sort and filter

--- src/v2/Apps/Partner/Routes/Works/index.tsx
+++ src/v2/Apps/Partner/Routes/Works/index.tsx
@@ -1,5 +1,48 @@
 import React from "react"
+import { Box, GridColumns, Column } from "@artsy/palette"
+import ArtworkGrid from "v2/Components/ArtworkGrid"
+import { createFragmentContainer } from "react-relay"
+import { graphql } from "relay-runtime"
+import { Works_partner } from "v2/__generated__/Works_partner.graphql"
 
-export const WorksRoute: React.FC = () => {
-  return <div>Works</div>
+interface ArtworksProps {
+  partner: Works_partner
 }
+
+export const Works: React.FC<ArtworksProps> = ({ partner }) => {
+  const { artworksConnection } = partner
+  return (
+    <Box mt={126}>
+      <GridColumns mt={6} gridRowGap={[2, 4]}>
+        <Column span={3}>Filters would be here</Column>
+        <Column span={9}>
+          <ArtworkGrid
+            artworks={artworksConnection}
+            columnCount={[2, 2, 3, 3]}
+            preloadImageCount={0}
+          />
+        </Column>
+      </GridColumns>
+    </Box>
+  )
+}
+
+export const WorksFragmentContainer = createFragmentContainer(Works, {
+  partner: graphql`
+    fragment Works_partner on Partner
+      @argumentDefinitions(first: { type: "Int", defaultValue: 15 }) {
+      slug
+      artworksConnection(first: $first)
+        @connection(key: "ArtworksQuery_artworksConnection") {
+        ...ArtworkGrid_artworks
+
+        # So we know whether or not to render the grid.
+        edges {
+          node {
+            id
+          }
+        }
+      }
+    }
+  `,
+})
