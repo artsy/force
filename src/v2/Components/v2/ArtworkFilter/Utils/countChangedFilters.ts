eb36f0e6831185ac05e47be66de90b9acb232e95
dzucconi <mail@damonzucconi.com>
2021-04-08 17:25:49 -0400

    fix(filters): correctly counts boolean filters

--- src/v2/Components/v2/ArtworkFilter/Utils/countChangedFilters.ts
+++ src/v2/Components/v2/ArtworkFilter/Utils/countChangedFilters.ts
@@ -9,16 +9,17 @@ export const countChangedFilters = (
   let key
 
   // simple keys
-  for (key of [
-    "medium",
-    "priceRange",
-    "offerable",
-    "acquireable",
-    "atAuction",
-    "inquireableOnly",
-    "color",
-  ]) {
+  for (key of ["medium", "priceRange", "color"]) {
     if (filtersAfter[key] != filtersBefore[key]) {
+      changedCount++
+    }
+  }
+
+  // boolean-valued keys
+  for (key of ["offerable", "acquireable", "atAuction", "inquireableOnly"]) {
+    // `filtersBefore[key]` will always be `undefined`
+    // Just uses the `true` or `false` value to determine count
+    if (filtersAfter[key]) {
       changedCount++
     }
   }
