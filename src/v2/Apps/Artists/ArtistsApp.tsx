e7742b1f4c503b8f5f85f25f32ca44cd39e42e55
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2021-04-14 14:58:19 -0700

    Removed Footer from ArtistsApp component

--- src/v2/Apps/Artists/ArtistsApp.tsx
+++ src/v2/Apps/Artists/ArtistsApp.tsx
@@ -1,18 +1,13 @@
 import React from "react"
 import { AppContainer } from "v2/Apps/Components/AppContainer"
 import { HorizontalPadding } from "v2/Apps/Components/HorizontalPadding"
-import { Footer } from "v2/Components/Footer"
 
 interface ArtistsAppProps {}
 
 export const ArtistsApp: React.FC<ArtistsAppProps> = ({ children }) => {
   return (
     <AppContainer>
-      <HorizontalPadding>
-        {children}
-
-        <Footer />
-      </HorizontalPadding>
+      <HorizontalPadding>{children}</HorizontalPadding>
     </AppContainer>
   )
 }
