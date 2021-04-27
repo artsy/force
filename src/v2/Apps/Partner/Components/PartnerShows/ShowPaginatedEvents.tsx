8d740bf19526fbfa9c77f30af8f1a6d552589ef1
Ivan Badyulya <ivan.badyulya@artsymail.com>
2021-04-27 13:16:07 +0300

    Create a grid  container which renders a list of shows with pagination

---
+++ src/v2/Apps/Partner/Components/PartnerShows/ShowPaginatedEvents.tsx
@@ -0,0 +1,191 @@
+import { Box } from "@artsy/palette"
+import React from "react"
+import {
+  createRefetchContainer,
+  graphql,
+  QueryRenderer,
+  RelayRefetchProp,
+} from "react-relay"
+import { PaginationFragmentContainer } from "v2/Components/Pagination"
+
+import { useSystemContext } from "v2/Artsy"
+import { useRouter } from "v2/Artsy/Router/useRouter"
+import { ShowPaginatedEventsQuery } from "v2/__generated__/ShowPaginatedEventsQuery.graphql"
+import { ShowPaginatedEvents_partner } from "v2/__generated__/ShowPaginatedEvents_partner.graphql"
+import { ShowEventsFragmentContainer } from "v2/Apps/Partner/Components/PartnerShows/ShowEvents"
+
+interface ShowEventsProps {
+  relay: RelayRefetchProp
+  partner: ShowPaginatedEvents_partner
+  scrollTo: string
+  eventTitle: string
+  offset: number
+}
+
+const ShowPaginatedEvents: React.FC<ShowEventsProps> = ({
+  eventTitle,
+  relay,
+  partner,
+  scrollTo,
+  offset,
+}): JSX.Element => {
+  const {
+    match: { location },
+    router,
+  } = useRouter()
+
+  if (!partner.showsList) {
+    return null
+  }
+
+  const {
+    showsList: {
+      edges: shows,
+      pageInfo: { hasNextPage, endCursor },
+      pageCursors,
+    },
+    slug,
+  } = partner
+
+  const handleClick = (cursor: string, page: number) => {
+    relay.refetch(
+      {
+        first: 24,
+        after: cursor,
+        partnerID: slug,
+        before: null,
+        last: null,
+      },
+      null,
+      error => {
+        if (error) {
+          console.error(error)
+        }
+      }
+    )
+
+    router.push({
+      pathname: location.pathname,
+      query: { ...location.query, page },
+    })
+  }
+
+  const handleNext = (page: number) => {
+    handleClick(endCursor, page)
+  }
+
+  return (
+    <Box id={scrollTo.substring(1)}>
+      <ShowEventsFragmentContainer edges={shows} eventTitle={eventTitle} />
+      <Box mt={9}>
+        <PaginationFragmentContainer
+          hasNextPage={hasNextPage}
+          pageCursors={pageCursors}
+          onClick={handleClick}
+          onNext={handleNext}
+          scrollTo={scrollTo}
+          offset={offset}
+        />
+      </Box>
+    </Box>
+  )
+}
+
+export const ShowEventsRefetchContainer = createRefetchContainer(
+  ShowPaginatedEvents,
+  {
+    partner: graphql`
+      fragment ShowPaginatedEvents_partner on Partner
+        @argumentDefinitions(
+          first: { type: "Int", defaultValue: 24 }
+          last: { type: "Int" }
+          after: { type: "String" }
+          before: { type: "String" }
+          status: { type: "EventStatus", defaultValue: CLOSED }
+        ) {
+        slug
+        showsList: showsConnection(
+          first: $first
+          last: $last
+          after: $after
+          before: $before
+          status: $status
+        ) {
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+          pageCursors {
+            ...Pagination_pageCursors
+          }
+          edges {
+            ...ShowEvents_edges
+          }
+        }
+      }
+    `,
+  },
+  graphql`
+    query ShowPaginatedEventsQuery(
+      $partnerId: String!
+      $first: Int
+      $last: Int
+      $after: String
+      $before: String
+      $status: EventStatus
+    ) {
+      partner(id: $partnerId) @principalField {
+        ...ShowPaginatedEvents_partner
+          @arguments(
+            first: $first
+            last: $last
+            after: $after
+            before: $before
+            status: $status
+          )
+      }
+    }
+  `
+)
+
+interface ShowPaginatedEventsRendererProps {
+  partnerId: string
+  first: number
+  status: string
+  eventTitle: string
+  scrollTo: string
+  offset: number
+}
+
+export const ShowPaginatedEventsRenderer: React.FC<ShowPaginatedEventsRendererProps> = ({
+  partnerId,
+  first,
+  status,
+  ...rest
+}) => {
+  const { relayEnvironment } = useSystemContext()
+
+  return (
+    <QueryRenderer<ShowPaginatedEventsQuery>
+      environment={relayEnvironment}
+      query={graphql`
+        query ShowPaginatedEventsRendererQuery(
+          $partnerId: String!
+          $first: Int
+          $status: EventStatus
+        ) {
+          partner(id: $partnerId) @principalField {
+            ...ShowPaginatedEvents_partner
+              @arguments(first: $first, status: $status)
+          }
+        }
+      `}
+      variables={{ partnerId, first, status }}
+      render={({ error, props }) => {
+        if (error || !props) return null
+
+        return <ShowEventsRefetchContainer {...rest} {...props} />
+      }}
+    />
+  )
+}
