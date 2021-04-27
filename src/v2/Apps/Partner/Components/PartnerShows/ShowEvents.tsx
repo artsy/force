772d3c2ec9af4ac8398207d5beb9dddeaaa886ac
Ivan Badyulya <ivan.badyulya@artsymail.com>
2021-04-27 13:15:39 +0300

    Create a grid  container which renders a list of shows

---
+++ src/v2/Apps/Partner/Components/PartnerShows/ShowEvents.tsx
@@ -0,0 +1,44 @@
+import { Column, GridColumns, Text } from "@artsy/palette"
+import React from "react"
+import { createFragmentContainer, graphql } from "react-relay"
+import { ShowCardFragmentContainer } from "./ShowCard"
+import { ShowEvents_edges } from "v2/__generated__/ShowEvents_edges.graphql"
+
+interface ShowEventsProps {
+  edges: ShowEvents_edges
+  eventTitle: string
+}
+
+const ShowEvents: React.FC<ShowEventsProps> = ({
+  edges,
+  eventTitle,
+}): JSX.Element => {
+  return (
+    <>
+      <Text color="black" variant="title" mb={6}>
+        {eventTitle}
+      </Text>
+
+      <GridColumns mb={6} gridRowGap={[2, 4]}>
+        {edges.map(({ node: show }) => {
+          return (
+            <Column key={show.internalID} span={[6, 6, 3, 3]}>
+              <ShowCardFragmentContainer show={show} />
+            </Column>
+          )
+        })}
+      </GridColumns>
+    </>
+  )
+}
+
+export const ShowEventsFragmentContainer = createFragmentContainer(ShowEvents, {
+  edges: graphql`
+    fragment ShowEvents_edges on ShowEdge @relay(plural: true) {
+      node {
+        internalID
+        ...ShowCard_show
+      }
+    }
+  `,
+})
