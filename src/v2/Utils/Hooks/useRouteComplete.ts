20e9569856e28c11521cbc17879eac852ec1783d
Damon <mail@damonzucconi.com>
2021-04-28 15:28:17 -0400

    fix(fx-2869): reset context to prevent stale filter state (#7429)
    * chore(types): adds types
    
    * refactor: moves and renames route complete hook
    
    * feat: adds hook for getting changed pathname after route complete
    
    * fix(filters): uses pathname as key to reset state of filter context
    
    * chore: adds comment

--- src/v2/Utils/Hooks/useRouteComplete.ts
+++ src/v2/Utils/Hooks/useRouteComplete.ts
@@ -3,25 +3,27 @@ import { useEffect, useState } from "react"
 import { usePrevious } from "v2/Utils/Hooks/usePrevious"
 
 /**
- * Checks to see if a route was previously fetching and has completed, in order
- * to track at the end of execution.
+ * Checks to see if a route was previously fetching and has completed.
  */
-export function useRouteTracking() {
-  const [shouldTrack, setShouldTrack] = useState(false)
+export const useRouteComplete = ({
+  onComplete,
+}: { onComplete?(): void } = {}) => {
+  const [isComplete, setIsComplete] = useState(false)
   const { isFetching } = useSystemContext()
   const prevFetching = usePrevious(isFetching)
 
   useEffect(() => {
     if (prevFetching && !isFetching) {
-      setShouldTrack(true)
+      setIsComplete(true)
+      onComplete && onComplete()
 
       // Wait till after the next tick to set to false, to give react tree
       // ability to execute related tracking handlers
       setImmediate(() => {
-        setShouldTrack(false)
+        setIsComplete(false)
       })
     }
-  }, [isFetching, prevFetching])
+  }, [isFetching, onComplete, prevFetching])
 
-  return shouldTrack
+  return { isComplete }
 }
