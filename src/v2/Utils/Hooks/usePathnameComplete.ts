761f7df700eee92637723a2d46636bc7b5394ac5
dzucconi <mail@damonzucconi.com>
2021-04-28 15:15:02 -0400

    chore: adds comment

--- src/v2/Utils/Hooks/usePathnameComplete.ts
+++ src/v2/Utils/Hooks/usePathnameComplete.ts
@@ -2,6 +2,12 @@ import { useState } from "react"
 import { useRouter } from "v2/Artsy/Router/useRouter"
 import { useRouteComplete } from "./useRouteComplete"
 
+/**
+ * Will return the active pathname *after*  the route completes.
+ * When a route change happens the route will immediately update even though
+ * the new route is still loading and hasn't been rendered. This waits until
+ * the render is complete and returns the next pathname.
+ */
 export const usePathnameComplete = () => {
   const { match } = useRouter()
   const [pathname, setPathname] = useState(match?.location?.pathname)
