7e0e404a49c3cf3f3a5f3d4e3b1867238794d658
Justin Bennett <zephraph@gmail.com>
2021-05-12 11:21:41 -0400

    Remove betterer, add strictness-migration script from eigen

--- .betterer.ts
+++
@@ -1,8 +0,0 @@
-import { regexp } from "@betterer/regexp"
-
-export default {
-  // Add tests here ☀️
-  "strictNullCheck migration": regexp(
-    /@ts-expect-error\s+STRICT_NULL_CHECK/
-  ).include("**/*.ts(x)?"),
-}
