df12faa0f1abd9c38c882c06e6a3d78f50e5ac23
Justin Bennett <zephraph@gmail.com>
2021-05-12 16:12:12 -0400

    Fix more null failures

--- src/v2/Apps/Components/AppShell.tsx
+++ src/v2/Apps/Components/AppShell.tsx
@@ -27,7 +27,9 @@ export const AppShell: React.FC<AppShellProps> = props => {
   const { children, match } = props
   const routeConfig = findCurrentRoute(match)
   const { isEigen } = useSystemContext()
+  // @ts-expect-error STRICT_NULL_CHECK
   const showFooter = !isEigen && !routeConfig.hideFooter
+  // @ts-expect-error STRICT_NULL_CHECK
   const appContainerMaxWidth = routeConfig.displayFullPage ? "100%" : null
 
   /**
@@ -36,8 +38,10 @@ export const AppShell: React.FC<AppShellProps> = props => {
    * in the background.
    */
   useEffect(() => {
+    // @ts-expect-error STRICT_NULL_CHECK
     if (isFunction(routeConfig.prepare)) {
       try {
+        // @ts-expect-error STRICT_NULL_CHECK
         routeConfig.prepare()
       } catch (error) {
         logger.error(error)
@@ -59,6 +63,7 @@ export const AppShell: React.FC<AppShellProps> = props => {
    * will cause the styles to update out of sync with the page change. Here we
    * wait for the route to finish rendering before setting the next theme.
    */
+  // @ts-expect-error STRICT_NULL_CHECK
   const nextTheme = routeConfig.theme ?? "v2"
   const [theme, setTheme] = useState<"v2" | "v3">(nextTheme)
   useRouteComplete({ onComplete: () => setTheme(nextTheme) })
