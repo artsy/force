826f4f0f2e762af9bbe211d883de675cfad0ea3d
dzucconi <mail@damonzucconi.com>
2021-05-12 18:04:28 -0400

    refactor(fair): uses full bleed header

--- src/v2/Apps/Fair/Components/FairHeader/FairHeaderImage.tsx
+++ src/v2/Apps/Fair/Components/FairHeader/FairHeaderImage.tsx
@@ -1,20 +1,7 @@
 import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { FairHeaderImage_fair } from "v2/__generated__/FairHeaderImage_fair.graphql"
-import styled from "styled-components"
-import { FullBleed } from "@artsy/palette"
-
-const Container = styled(FullBleed)`
-  position: relative;
-  overflow: hidden;
-`
-
-const Image = styled.img`
-  display: block;
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-`
+import { FullBleedHeader } from "v2/Components/FullBleedHeader"
 
 interface FairHeaderImageProps {
   fair: FairHeaderImage_fair
@@ -25,16 +12,7 @@ export const FairHeaderImage: React.FC<FairHeaderImageProps> = ({
 }) => {
   if (!image) return null
 
-  return (
-    <Container bg="black10" height={600}>
-      <picture>
-        <source srcSet={image.lg.srcSet} media="(min-width: 1200px)" />
-        <source srcSet={image.md.srcSet} media="(min-width: 700px)" />
-        <source srcSet={image.sm.srcSet} media="(max-width: 700px)" />
-        <Image src={image.sm.src} alt="" loading="lazy" />
-      </picture>
-    </Container>
-  )
+  return <FullBleedHeader src={image.url} />
 }
 
 export const FairHeaderImageFragmentContainer = createFragmentContainer(
@@ -43,18 +21,7 @@ export const FairHeaderImageFragmentContainer = createFragmentContainer(
     fair: graphql`
       fragment FairHeaderImage_fair on Fair {
         image {
-          sm: cropped(width: 480, height: 600, version: "wide") {
-            src
-            srcSet
-          }
-          md: cropped(width: 900, height: 600, version: "wide") {
-            src
-            srcSet
-          }
-          lg: cropped(width: 1600, height: 600, version: "wide") {
-            src
-            srcSet
-          }
+          url(version: "wide")
         }
       }
     `,
