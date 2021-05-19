1dd778b27e1ad92682085d9f8f2a5796fd7d6130
nastassia <nastassia@wsa-231-72b.local>
2021-05-19 18:21:01 +0300

    Updated tests

---
+++ src/v2/Apps/Consign/Routes/MarketingLanding/Components/__tests__/BecomePartner.jest.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { mount } from "enzyme"
+import { BecomePartner } from "../BecomePartner"
+import { MockBoot } from "v2/DevTools"
+import { Breakpoint } from "@artsy/palette"
+
+jest.mock("react-tracking")
+
+describe("BecomePartner", () => {
+  const getWrapper = (breakpoint = "md") => {
+    return mount(
+      <MockBoot breakpoint={breakpoint as Breakpoint}>
+        <BecomePartner />
+      </MockBoot>
+    )
+  }
+
+  it("has correct link", () => {
+    const wrapper = getWrapper()
+    expect(wrapper.find("Link").props().href).toBe(
+      "https://partners.artsy.net/gallery-partnerships"
+    )
+    const wrapperMobile = getWrapper("xs")
+    expect(wrapperMobile.find("Link").props().href).toBe(
+      "https://partners.artsy.net/gallery-partnerships"
+    )
+  })
+})
