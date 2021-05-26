632d320dce6ffecc56fcf41a8e7a83823c638d24
Jon Allured <jon.allured@gmail.com>
2021-05-26 14:21:45 -0500

    Provide fallback for request ip

--- src/lib/middleware/bootstrapSharifyAndContextLocalsMiddleware.ts
+++ src/lib/middleware/bootstrapSharifyAndContextLocalsMiddleware.ts
@@ -60,7 +60,8 @@ export function bootstrapSharifyAndContextLocalsMiddleware(
 
   updateSharifyAndContext(res, "IS_GOOGLEBOT", Boolean(ua.match(/Googlebot/i)))
 
-  updateSharifyAndContext(res, "IP_ADDRESS", req.ip)
+  const reqIp = req.ip || ""
+  updateSharifyAndContext(res, "IP_ADDRESS", reqIp)
 
   // Required to know the hashed dll name.try
   updateSharifyAndContext(
