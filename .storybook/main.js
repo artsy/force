dd8b948709fec4875b7a7e57e79de1e93df71c7e
Christopher Pappas <damassi.pappas@gmail.com>
2021-05-28 09:22:01 -0700

    fix: [Storybooks] Fix deploy issue due to ts 2.3.x incompat

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,13 @@
 module.exports = {
-  stories: ["../src/**/*.story.tsx"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  stories: ["../src/**/*.story.tsx"],
+
+  typescript: {
+    // Disable docgeneration due to TypeScript 2.3.x incompatability.
+    // TODO: Fix this once https://github.com/styleguidist/react-docgen-typescript/issues/356
+    // is addressed
+    reactDocgen: "none",
+  },
 
   webpackFinal: async config => {
     config.stats = "errors-only"
