c55f360276ac7b92cc8f5cb2739805d746829ce9
nastassia <nastassia@wsa-231-72b.local>
2021-05-28 15:01:37 +0300

    Updated tests for DownloadAppBadge

--- src/v2/Components/__tests__/DownloadAppBadge.jest.tsx
+++ src/v2/Components/__tests__/DownloadAppBadge.jest.tsx
@@ -2,11 +2,22 @@ import React from "react"
 import { mount } from "enzyme"
 import { useTracking } from "react-tracking"
 import { Link } from "@artsy/palette"
-import { DownloadAppBadge } from "v2/Components/DownloadAppBadge"
+import { Device, DownloadAppBadge } from "v2/Components/DownloadAppBadge"
 import { ContextModule } from "@artsy/cohesion"
 
 describe("DownloadAppBadge", () => {
   const trackEvent = jest.fn()
+  const event = {
+    context_module: "footer",
+    subject: "Download on the App Store",
+    destination_path:
+      "https://apps.apple.com/us/app/artsy-buy-sell-original-art/id703796080",
+  }
+  const props = {
+    contextModule: ContextModule.footer,
+    device: Device.iPhone,
+  }
+
   beforeEach(() => {
     const mockTracking = useTracking as jest.Mock
     mockTracking.mockImplementation(() => {
@@ -20,19 +31,24 @@ describe("DownloadAppBadge", () => {
     jest.resetAllMocks()
   })
 
-  it("tracks clicks on the app download badge", () => {
-    const badge = mount(
-      <DownloadAppBadge contextModule={ContextModule.footer} />
-    )
+  it("tracks clicks on the iPhone app download badge", () => {
+    const badge = mount(<DownloadAppBadge {...props} />)
+    const downloadLink = badge.find(Link)
+    // @ts-expect-error STRICT_NULL_CHECK
+    downloadLink.props().onClick({} as any)
+    expect(trackEvent).toHaveBeenCalledWith(expect.objectContaining(event))
+  })
+
+  it("tracks clicks on the Android app download badge", () => {
+    const badge = mount(<DownloadAppBadge {...props} device={Device.Android} />)
     const downloadLink = badge.find(Link)
     // @ts-expect-error STRICT_NULL_CHECK
     downloadLink.props().onClick({} as any)
     expect(trackEvent).toHaveBeenCalledWith(
       expect.objectContaining({
-        context_module: "footer",
+        ...event,
         destination_path:
-          "https://apps.apple.com/us/app/artsy-buy-sell-original-art/id703796080",
-        subject: "Download on the App Store",
+          "https://play.google.com/store/apps/details?id=net.artsy.app",
       })
     )
   })
