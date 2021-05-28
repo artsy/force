78882e4074fc9dca02ffce868e1e12c508b25b56
Kirill Zubarau <kiry.zubarau@artsymail.com>
2021-05-28 17:03:41 +0300

    Scroll to artist after the device type is known

--- src/v2/Apps/Partner/Routes/Artists/ArtistsRoute.tsx
+++ src/v2/Apps/Partner/Routes/Artists/ArtistsRoute.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect } from "react"
 import { Box, Text, themeProps } from "@artsy/palette"
 import { Match } from "found"
 import {
@@ -12,7 +12,7 @@ import { graphql } from "lib/graphql"
 import { createFragmentContainer } from "react-relay"
 import { Media } from "v2/Utils/Responsive"
 import { MOBILE_NAV_HEIGHT, NAV_BAR_HEIGHT } from "v2/Components/NavBar"
-import { PartnerArtistsLoadingContextProvider } from "../../Utils/PartnerArtistsLoadingContext"
+import { usePartnerArtistsLoadingContext } from "../../Utils/PartnerArtistsLoadingContext"
 import { scrollIntoView } from "v2/Utils/scrollHelpers"
 import { useMatchMedia } from "v2/Utils/Hooks/useMatchMedia"
 
@@ -26,6 +26,13 @@ export const ArtistsRoute: React.FC<ArtistsRouteProps> = ({
   match,
 }) => {
   const isMobile = useMatchMedia(themeProps.mediaQueries.xs)
+  const { isLoaded } = usePartnerArtistsLoadingContext()
+
+  useEffect(() => {
+    if (match.params.artistId && isLoaded && isMobile !== null) {
+      scrollIntoArtistDetails()
+    }
+  }, [isLoaded, isMobile])
 
   const scrollIntoArtistDetails = () => {
     const offset =
@@ -39,49 +46,39 @@ export const ArtistsRoute: React.FC<ArtistsRouteProps> = ({
     })
   }
 
-  const handleArtistsLoaded = () => {
-    if (match.params.artistId) {
-      scrollIntoArtistDetails()
-    }
-  }
-
   return (
-    <PartnerArtistsLoadingContextProvider onArtistsLoaded={handleArtistsLoaded}>
-      <Box mt={4}>
-        <Text variant="title" mb={6}>
-          Artists
-        </Text>
-        <Media greaterThan="xs">
-          <PartnerArtistsFragmentContainer
-            scrollTo={{
-              selector: "#jump--PartnerArtistDetails",
-              offset: PARTNER_NAV_BAR_HEIGHT + NAV_BAR_HEIGHT + 20,
-            }}
-            partner={partner}
-          />
-        </Media>
-        <Media at="xs">
-          <PartnerArtistsFragmentContainer
-            scrollTo={{
-              selector: "#jump--PartnerArtistDetails",
-              offset: PARTNER_NAV_BAR_HEIGHT + MOBILE_NAV_HEIGHT + 20,
-            }}
-            partner={partner}
-          />
-        </Media>
+    <Box mt={4}>
+      <Text variant="title" mb={6}>
+        Artists
+      </Text>
+      <Media greaterThan="xs">
+        <PartnerArtistsFragmentContainer
+          scrollTo={{
+            selector: "#jump--PartnerArtistDetails",
+            offset: PARTNER_NAV_BAR_HEIGHT + NAV_BAR_HEIGHT + 20,
+          }}
+          partner={partner}
+        />
+      </Media>
+      <Media at="xs">
+        <PartnerArtistsFragmentContainer
+          scrollTo={{
+            selector: "#jump--PartnerArtistDetails",
+            offset: PARTNER_NAV_BAR_HEIGHT + MOBILE_NAV_HEIGHT + 20,
+          }}
+          partner={partner}
+        />
+      </Media>
 
-        {match.params.artistId ? (
-          <PartnerArtistDetailsRenderer
-            partnerId={match.params.partnerId}
-            artistId={match.params.artistId}
-          />
-        ) : (
-          <PartnerArtistDetailsListRenderer
-            partnerId={match.params.partnerId}
-          />
-        )}
-      </Box>
-    </PartnerArtistsLoadingContextProvider>
+      {match.params.artistId ? (
+        <PartnerArtistDetailsRenderer
+          partnerId={match.params.partnerId}
+          artistId={match.params.artistId}
+        />
+      ) : (
+        <PartnerArtistDetailsListRenderer partnerId={match.params.partnerId} />
+      )}
+    </Box>
   )
 }
 
