27663459e33e0e2b58fe52b37c79c48d297514d1
alexj105 <aliaksandr.savelyeu@artsymail.com>
2021-06-03 15:43:25 +0300

    Remove login/signup gate on viewing rooms

--- src/v2/Apps/ViewingRoom/ViewingRoomApp.tsx
+++ src/v2/Apps/ViewingRoom/ViewingRoomApp.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react"
+import React, { useContext } from "react"
 import { Banner, Box, ClosedEyeIcon, Text } from "@artsy/palette"
 import { ViewingRoomHeaderFragmentContainer as ViewingRoomHeader } from "./Components/ViewingRoomHeader"
 import { ViewingRoomContentNotAccessibleFragmentContainer as ViewingRoomContentNotAccessible } from "./Components/ViewingRoomContentNotAccessible"
@@ -8,11 +8,7 @@ import { createFragmentContainer, graphql } from "react-relay"
 import { ViewingRoomApp_viewingRoom } from "v2/__generated__/ViewingRoomApp_viewingRoom.graphql"
 import { ViewingRoomMetaFragmentContainer as ViewingRoomMeta } from "./Components/ViewingRoomMeta"
 import { ErrorPage } from "v2/Components/ErrorPage"
-import { openAuthModal } from "v2/Utils/openAuthModal"
 import { SystemContext } from "v2/Artsy"
-import { ModalType } from "v2/Components/Authentication/Types"
-import { ContextModule, Intent } from "@artsy/cohesion"
-import { useRouter } from "v2/Artsy/Router/useRouter"
 import { userHasAccessToPartner } from "v2/Utils/user"
 
 interface ViewingRoomAppProps {
@@ -24,23 +20,7 @@ const ViewingRoomApp: React.FC<ViewingRoomAppProps> = ({
   children,
   viewingRoom,
 }) => {
-  const { mediator, user } = useContext(SystemContext)
-  const { router } = useRouter()
-  useEffect(() => {
-    if (user && user.id) return
-    // @ts-expect-error STRICT_NULL_CHECK
-    openAuthModal(mediator, {
-      afterClose: () => {
-        router.push("/viewing-rooms")
-      },
-      contextModule: ContextModule.viewingRoom,
-      copy: "Sign up to enter viewing rooms",
-      disableCloseOnBackgroundClick: true,
-      intent: Intent.viewViewingRoom,
-      mode: ModalType.signup,
-      redirectTo: window.location.href,
-    })
-  }, [user, router, mediator])
+  const { user } = useContext(SystemContext)
 
   if (!viewingRoom) {
     return <ErrorPage code={404} />
