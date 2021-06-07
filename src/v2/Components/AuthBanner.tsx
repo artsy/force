2c27504b7f4bbf6d7025ab5773996e1143341a76
dzucconi <mail@damonzucconi.com>
2021-06-07 19:05:17 -0400

    fix(navbar): prevents doubled auth buttons

--- src/v2/Components/AuthBanner.tsx
+++ src/v2/Components/AuthBanner.tsx
@@ -2,19 +2,28 @@ import React from "react"
 import { Flex, Button, Separator, Spacer } from "@artsy/palette"
 import { RouterLink } from "v2/Artsy/Router/RouterLink"
 
-export const NavBarAuthBanner: React.FC = () => {
+export const AuthBanner: React.FC = () => {
   return (
     <>
       <Flex bg="white100" p={1}>
-        {/* @ts-ignore */}
-        <Button as={RouterLink} to="/signup" flex={1}>
+        <Button
+          // @ts-ignore
+          as={RouterLink}
+          to="/signup"
+          variant="secondaryOutline"
+          flex={1}
+        >
           Sign up
         </Button>
 
         <Spacer ml={1} />
 
-        {/* @ts-ignore */}
-        <Button as={RouterLink} to="/login" flex={1}>
+        <Button
+          // @ts-ignore
+          as={RouterLink}
+          to="/login"
+          flex={1}
+        >
           Log in
         </Button>
       </Flex>
