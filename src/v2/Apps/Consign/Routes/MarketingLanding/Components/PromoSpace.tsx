ef80d350e5f7befec186bb0311da0d68c95e21d7
Jon Allured <jon.allured@gmail.com>
2021-06-09 16:34:09 -0500

    Reduce promo copy at small size

--- src/v2/Apps/Consign/Routes/MarketingLanding/Components/PromoSpace.tsx
+++ src/v2/Apps/Consign/Routes/MarketingLanding/Components/PromoSpace.tsx
@@ -1,15 +1,24 @@
 import React from "react"
 import { Link, Text } from "@artsy/palette"
 import { SectionContainer } from "./SectionContainer"
+import { Media } from "v2/Utils/Responsive"
 
 export const PromoSpace: React.FC = () => {
   return (
     <SectionContainer bg="black5" py={3}>
-      <Text variant="subtitle" color="black100" textAlign="center">
-        Gallerist or art dealer?{" "}
-        <Link href="https://partners.artsy.net">Become a partner</Link> to
-        access the largest global online art marketplace
-      </Text>
+      <Media greaterThanOrEqual="sm">
+        <Text variant="subtitle" color="black100" textAlign="center">
+          Gallerist or art dealer?{" "}
+          <Link href="https://partners.artsy.net">Become a partner</Link> to
+          access the largest global online art marketplace
+        </Text>
+      </Media>
+      <Media lessThan="sm">
+        <Text variant="subtitle" color="black100" textAlign="center">
+          Gallerist or art dealer?{" "}
+          <Link href="https://partners.artsy.net">Become a partner</Link>
+        </Text>
+      </Media>
     </SectionContainer>
   )
 }
