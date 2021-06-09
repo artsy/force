7246cfaf23447ee2803a8d7f4679bbf4413a96a2
Jon Allured <jon.allured@gmail.com>
2021-06-09 16:28:43 -0500

    Update copy for new promo

--- src/v2/Apps/Consign/Routes/MarketingLanding/Components/PromoSpace.tsx
+++ src/v2/Apps/Consign/Routes/MarketingLanding/Components/PromoSpace.tsx
@@ -1,13 +1,14 @@
 import React from "react"
-import { Text } from "@artsy/palette"
+import { Link, Text } from "@artsy/palette"
 import { SectionContainer } from "./SectionContainer"
 
 export const PromoSpace: React.FC = () => {
   return (
     <SectionContainer bg="black5" py={3}>
       <Text variant="subtitle" color="black100" textAlign="center">
-        Now through December 31, 2020 — we are offering 0% commission on any
-        work sold with Artsy
+        Gallerist or art dealer?{" "}
+        <Link href="https://partners.artsy.net">Become a partner</Link> to
+        access the largest global online art marketplace
       </Text>
     </SectionContainer>
   )
