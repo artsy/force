4b8a0bf2d7aae6b6f6df484c18f771718d01d8e1
Christopher Pappas <damassi.pappas@gmail.com>
2021-06-11 15:47:36 -0700

    ci: Update circle config and add danger job

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,7 +1,6 @@
 /* eslint-disable jest/no-jasmine-globals */
-import { warn, danger, markdown } from "danger"
+import { danger } from "danger"
 import * as fs from "fs"
-import { getBreakingChanges } from "./scripts/validateSchemas"
 
 /**
  * Helpers
@@ -13,18 +12,9 @@ const filesOnly = (file: string) =>
 const getCreatedFiles = (createdFiles: string[]) =>
   createdFiles.filter(filesOnly)
 
-function preventNewJSFilesFromBeingCreated() {
-  // Warn about creating new JS files
-  const jsFiles = danger.git.created_files.filter(
-    f => f.includes("src") && f.endsWith(".js")
-  )
-  if (jsFiles.length) {
-    const files = danger.github.utils.fileLinks(jsFiles)
-    warn(
-      `Please don't include .js files, we want to be using TypeScript found: ${files}.`
-    )
-  }
-}
+/**
+ * Rules
+ */
 
 function preventDefaultQueryRenderImport() {
   const newQueryRendererImports = getCreatedFiles(
@@ -34,38 +24,11 @@ function preventDefaultQueryRenderImport() {
     return content.includes("<QueryRenderer")
   })
   if (newQueryRendererImports.length > 0) {
-    fail(`Please use <SystemQueryRenderer /> instead of <QueryRender />. This prevents double fetching during the server-side render pass. See: ${newQueryRendererImports
-      .map(filename => `- \`${filename}\``)
-      .join("\n")}
-  }`)
-  }
-}
-
-async function checkIfMetaphysicsSchemaIsInSync() {
-  const isReleasePR = danger.github.pr.base.ref === "release"
-  const versionToCheck = isReleasePR ? "production" : "staging"
-  const changes: string[] = await getBreakingChanges(versionToCheck, 2)
-
-  // There are breaking changes with the schema
-  if (changes.length) {
-    if (isReleasePR) {
-      const deployURL = "https://github.com/artsy/metaphysics#deployment"
-      fail(
-        `You need to promote Metaphysics from staging to production. The schema used in Reaction, is further ahead than the version in Metaphyisc production. This could cause your queries to fail. You can see how to [deploy here](${deployURL}).`
-      )
-    } else {
-      warn(
-        "The staging version of Metaphysics has breaking changes, this might not affect your build, but could mean your queries don't work the way you expect. See below for breaking changes:"
-      )
-    }
-
-    // Show a list of the changes
-    markdown(changes.map(change => `- :warning: ${change}`).join("\n"))
+    fail(`Please use \`<SystemQueryRenderer />\` instead of \`<QueryRender />\`. This prevents double fetching during the server-side render pass. See:
+> ${newQueryRendererImports.map(filename => `\`${filename}\``).join("\n")}`)
   }
 }
 
 ;(async function () {
-  preventNewJSFilesFromBeingCreated()
   preventDefaultQueryRenderImport()
-  await checkIfMetaphysicsSchemaIsInSync()
 })()
--- src/v2/test.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import { QueryRenderer } from "react-relay"
-
-export const Foo = props => {
-  // @ts-ignore
-  return <QueryRenderer />
-}
