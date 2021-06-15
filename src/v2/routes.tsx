17c52976acaaa2c86464bf842c38bf9560fc67b0
dzucconi <mail@damonzucconi.com>
2021-06-15 12:37:53 -0400

    refactor: sorts routes

--- src/v2/routes.tsx
+++ src/v2/routes.tsx
@@ -1,3 +1,4 @@
+import { AppRouteConfig } from "v2/System/Router/Route"
 import { artistRoutes } from "v2/Apps/Artist/artistRoutes"
 import { artistSeriesRoutes } from "v2/Apps/ArtistSeries/artistSeriesRoutes"
 import { artistsRoutes } from "v2/Apps/Artists/artistsRoutes"
@@ -19,91 +20,40 @@ import { orderRoutes } from "v2/Apps/Order/orderRoutes"
 import { partnerRoutes } from "v2/Apps/Partner/partnerRoutes"
 import { paymentRoutes } from "v2/Apps/Payment/paymentRoutes"
 import { purchaseRoutes } from "v2/Apps/Purchase/purchaseRoutes"
-import { AppRouteConfig } from "v2/System/Router/Route"
 import { searchRoutes } from "v2/Apps/Search/searchRoutes"
 import { showRoutes } from "v2/Apps/Show/showRoutes"
-import { viewingRoomRoutes } from "v2/Apps/ViewingRoom/viewingRoomRoutes"
 import { tagRoutes } from "./Apps/Tag/tagRoutes"
 import { unsubscribeRoutes } from "./Apps/Unsubscribe/unsubscribeRoutes"
+import { viewingRoomRoutes } from "v2/Apps/ViewingRoom/viewingRoomRoutes"
 
 export function getAppRoutes(): AppRouteConfig[] {
   return buildAppRoutes([
-    {
-      routes: artistRoutes,
-    },
-    {
-      routes: artistsRoutes,
-    },
-    {
-      routes: artistSeriesRoutes,
-    },
-    {
-      routes: artworkRoutes,
-    },
-    {
-      routes: auctionsRoutes,
-    },
-    {
-      routes: buyerGuaranteeRoutes,
-    },
-    {
-      routes: collectRoutes,
-    },
-    {
-      routes: consignRoutes,
-    },
-    {
-      routes: conversationRoutes,
-    },
-    {
-      routes: exampleRoutes,
-    },
-    {
-      routes: fairRoutes,
-    },
-    {
-      routes: fairsRoutes,
-    },
-    {
-      routes: featureRoutes,
-    },
-    {
-      routes: geneRoutes,
-    },
-    {
-      routes: identityVerificationRoutes,
-    },
-    {
-      routes: orderRoutes,
-    },
-    {
-      routes: paymentRoutes,
-    },
-    {
-      routes: partnerRoutes,
-    },
-    {
-      routes: purchaseRoutes,
-    },
-    {
-      routes: searchRoutes,
-    },
-    {
-      routes: showRoutes,
-    },
-    {
-      routes: tagRoutes,
-    },
-    {
-      routes: unsubscribeRoutes,
-    },
-    {
-      routes: viewingRoomRoutes,
-    },
+    { routes: artistRoutes },
+    { routes: artistSeriesRoutes },
+    { routes: artistsRoutes },
+    { routes: artworkRoutes },
+    { routes: auctionsRoutes },
+    { routes: buyerGuaranteeRoutes },
+    { routes: collectRoutes },
+    { routes: consignRoutes },
+    { routes: conversationRoutes },
+    { routes: exampleRoutes },
+    { routes: fairRoutes },
+    { routes: fairsRoutes },
+    { routes: featureRoutes },
+    { routes: geneRoutes },
+    { routes: identityVerificationRoutes },
+    { routes: orderRoutes },
+    { routes: partnerRoutes },
+    { routes: paymentRoutes },
+    { routes: purchaseRoutes },
+    { routes: searchRoutes },
+    { routes: showRoutes },
+    { routes: tagRoutes },
+    { routes: unsubscribeRoutes },
+    { routes: viewingRoomRoutes },
 
     // For debugging baseline app shell stuff
-    {
-      routes: debugRoutes,
-    },
+    { routes: debugRoutes },
   ])
 }
