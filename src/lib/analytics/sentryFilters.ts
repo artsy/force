ef9ad25b0def21c530b72166bbd22c95925520bf
Christopher Pappas <damassi.pappas@gmail.com>
2021-06-16 14:25:46 -0700

    feat: Notify devs compilation complete

--- src/lib/analytics/sentryFilters.ts
+++ src/lib/analytics/sentryFilters.ts
@@ -4,7 +4,6 @@ export const IGNORED_ERRORS = [
   "AbortError: The operation was aborted",
   'Blocked a frame with origin "https://www.artsy.net" from accessing a cross-origin frame',
   "cancelled",
-  "Cannot set headers after they are sent to the client",
   "Failed to fetch",
   "Origin is not allowed by Access-Control-Allow-Origin",
   "pktAnnotationHighlighter", // Pocket ios app errors on opening articles
@@ -13,11 +12,13 @@ export const IGNORED_ERRORS = [
   "Response timeout",
   "SecurityError: Blocked a frame with origin", // Facebook scripts error on opening articles
   "TypeError Qg.m(gpt/pubads_impl_2021052601)",
-  "undefined is not an object (evaluating 'g.toString')",
 ]
 
 export const DENIED_URLS = [
   /t\.dca0\.com/i,
   /api\.sail-personalize\.com/i,
   /securepubads\.g\.doubleclick\.net/i,
+  /googletagservices\.com/i,
+  /t\.getletterpress\.com/i,
+  /sdk\.iad-06\.braze\.com/i,
 ]
