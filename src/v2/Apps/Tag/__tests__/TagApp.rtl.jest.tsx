bd983e11691acb87b59f03398a1eee6fdf3a42f4
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2021-06-22 13:00:17 -0400

    test(RTL): add new RTL spec for a sample Relay-based component

---
+++ src/v2/Apps/Tag/__tests__/TagApp.rtl.jest.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { TagAppFragmentContainer } from "../TagApp"
+import { setupTestWrapperTL } from "v2/DevTools/setupTestWrapper"
+import { graphql } from "react-relay"
+import { TagApp_Test_RTL_Query } from "v2/__generated__/TagApp_Test_RTL_Query.graphql"
+import { MockBoot } from "v2/DevTools"
+import { screen } from "@testing-library/react"
+
+jest.unmock("react-relay")
+jest.mock("../Components/TagArtworkFilter", () => ({
+  TagArtworkFilterRefetchContainer: () => <div />,
+}))
+
+const { renderWithRelay } = setupTestWrapperTL<TagApp_Test_RTL_Query>({
+  Component: props => {
+    return (
+      <MockBoot>
+        {/* @ts-expect-error STRICT_NULL_CHECK */}
+        <TagAppFragmentContainer {...props} />
+      </MockBoot>
+    )
+  },
+  query: graphql`
+    query TagApp_Test_RTL_Query {
+      tag(id: "example") {
+        ...TagApp_tag
+      }
+    }
+  `,
+})
+
+describe("TagApp", () => {
+  it("renders correctly", () => {
+    renderWithRelay({
+      Tag: () => ({
+        name: "Example Tag",
+      }),
+    })
+
+    expect(screen.getByText("Example Tag")).toBeInTheDocument()
+  })
+})
