7cefbe71a4f213cfd31d7b7c32fde181a51b1989
nastassia <nastassia@wsa-231-72b.local>
2021-06-28 14:39:15 +0300

    Updated tests

---
+++ src/v2/__generated__/FairEditorialItem_Test_Query.graphql.ts
@@ -0,0 +1,225 @@
+/* tslint:disable */
+/* eslint-disable */
+
+import { ConcreteRequest } from "relay-runtime";
+import { FragmentRefs } from "relay-runtime";
+export type FairEditorialItem_Test_QueryVariables = {};
+export type FairEditorialItem_Test_QueryResponse = {
+    readonly article: {
+        readonly " $fragmentRefs": FragmentRefs<"FairEditorialItem_article">;
+    } | null;
+};
+export type FairEditorialItem_Test_Query = {
+    readonly response: FairEditorialItem_Test_QueryResponse;
+    readonly variables: FairEditorialItem_Test_QueryVariables;
+};
+
+
+
+/*
+query FairEditorialItem_Test_Query {
+  article(id: "test") {
+    ...FairEditorialItem_article
+    id
+  }
+}
+
+fragment FairEditorialItem_article on Article {
+  id
+  internalID
+  slug
+  title
+  href
+  publishedAt(format: "MMMM D, YYYY")
+  thumbnailTitle
+  thumbnailImage {
+    cropped(width: 325, height: 240) {
+      width
+      height
+      src
+      srcSet
+    }
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "id",
+    "value": "test"
+  }
+];
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "FairEditorialItem_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Article",
+        "kind": "LinkedField",
+        "name": "article",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "FairEditorialItem_article"
+          }
+        ],
+        "storageKey": "article(id:\"test\")"
+      }
+    ],
+    "type": "Query"
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "FairEditorialItem_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Article",
+        "kind": "LinkedField",
+        "name": "article",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "id",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "internalID",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "slug",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "title",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "href",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": [
+              {
+                "kind": "Literal",
+                "name": "format",
+                "value": "MMMM D, YYYY"
+              }
+            ],
+            "kind": "ScalarField",
+            "name": "publishedAt",
+            "storageKey": "publishedAt(format:\"MMMM D, YYYY\")"
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "thumbnailTitle",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Image",
+            "kind": "LinkedField",
+            "name": "thumbnailImage",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": [
+                  {
+                    "kind": "Literal",
+                    "name": "height",
+                    "value": 240
+                  },
+                  {
+                    "kind": "Literal",
+                    "name": "width",
+                    "value": 325
+                  }
+                ],
+                "concreteType": "CroppedImageUrl",
+                "kind": "LinkedField",
+                "name": "cropped",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "width",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "height",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "src",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "srcSet",
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": "cropped(height:240,width:325)"
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "article(id:\"test\")"
+      }
+    ]
+  },
+  "params": {
+    "id": null,
+    "metadata": {},
+    "name": "FairEditorialItem_Test_Query",
+    "operationKind": "query",
+    "text": "query FairEditorialItem_Test_Query {\n  article(id: \"test\") {\n    ...FairEditorialItem_article\n    id\n  }\n}\n\nfragment FairEditorialItem_article on Article {\n  id\n  internalID\n  slug\n  title\n  href\n  publishedAt(format: \"MMMM D, YYYY\")\n  thumbnailTitle\n  thumbnailImage {\n    cropped(width: 325, height: 240) {\n      width\n      height\n      src\n      srcSet\n    }\n  }\n}\n"
+  }
+};
+})();
+(node as any).hash = 'a9d036c4e6d42e9e7a216ca1d722fa8b';
+export default node;
