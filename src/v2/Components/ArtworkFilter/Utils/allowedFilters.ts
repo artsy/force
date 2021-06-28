835d46b1a9737871124fbd7b2c99e0f08fca103f
dzucconi <mail@damonzucconi.com>
2021-06-28 13:25:56 -0400

    fix: sanitizes inputs

--- src/v2/Components/ArtworkFilter/Utils/allowedFilters.ts
+++ src/v2/Components/ArtworkFilter/Utils/allowedFilters.ts
@@ -1,16 +1,49 @@
-export const allowedFilters = filterParams => {
-  return Object.keys(filterParams)
-    .filter(key => supportedInputArgs.includes(key))
-    .reduce((obj, key) => {
-      obj[key] = filterParams[key]
+export const allowedFilters = (
+  filterParams: Record<string, any> = {}
+): Record<string, any> => {
+  return Object.keys(filterParams).reduce((obj, key) => {
+    // Filter out unsupported arguments
+    if (!SUPPORTED_INPUT_ARGS.includes(key)) {
       return obj
-    }, {})
+    }
+
+    // Coerce integers
+    if (INTEGER_INPUT_ARGS.includes(key)) {
+      obj[key] = parseInt(filterParams[key], 10) || 1
+      return obj
+    }
+
+    // Coerce booleans
+    if (BOOLEAN_INPUT_ARGS.includes(key)) {
+      const value = filterParams[key]
+      obj[key] = value === "true" || value === true
+      return obj
+    }
+
+    obj[key] = filterParams[key]
+    return obj
+  }, {})
 }
 
-// This corresponds to all the allowed inputs to the
-// filtered artworks connection. This can be used as an
-// allow-list when passing filter state from a URL.
-const supportedInputArgs = [
+const INTEGER_INPUT_ARGS = ["first", "last", "page", "size"]
+
+const BOOLEAN_INPUT_ARGS = [
+  "acquireable",
+  "atAuction",
+  "forSale",
+  "includeArtworksByFollowedArtists",
+  "includeMediumFilterInAggregation",
+  "inquireableOnly",
+  "keywordMatchExact",
+  "marketable",
+  "offerable",
+]
+
+/**
+ * This corresponds to all the allowed inputs to the filtered artworks connection.
+ * This can be used as an allow-list when passing filter state from a URL.
+ */
+const SUPPORTED_INPUT_ARGS = [
   "acquireable",
   "additionalGeneIDs",
   "after",
