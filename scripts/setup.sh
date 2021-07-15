da8f5bdd467a4329054ed7c7459452dd6274cc4f
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2021-07-15 18:42:19 -0400

    add setup script, minimal example env

---
+++ scripts/setup.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This assumes you have general prerequisites installed as by:
+# https://github.com/artsy/potential/blob/master/scripts/setup
+
+# Exit if any subcommand fails
+set -e
+
+echo "Brew update and bundle install..."
+brew update
+brew bundle --file=- <<EOF
+brew 'yarn'
+EOF
+
+if [ ! -z $NVM_DIR ]; then # skip if nvm is not available
+  echo "Installing Node..."
+  source ~/.nvm/nvm.sh
+  nvm install
+fi
+
+echo "Installing javascript dependencies"
+yarn install
+
+# Download the shared .env
+echo "Download .env.shared (for common local dev config)..."
+aws s3 cp s3://artsy-citadel/dev/.env.force .env.shared
+
+if [ ! -e ".env" ]; then
+  echo "Initialize .env from from .env.example (for any custom configuration)..."
+  cat .env.example > .env
+fi
+
+echo "Setup complete! Remember to execute 'nvm use' in your shell."
