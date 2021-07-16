7e56cd7dddc9cca929eb53b8de0e5dd612cc980f
dzucconi <mail@damonzucconi.com>
2021-07-16 13:35:34 -0400

    feat(artwork): rebuilds artwork lightbox

--- src/v2/Apps/Artwork/Components/ArtworkImageBrowser/ArtworkImageBrowser.tsx
+++ src/v2/Apps/Artwork/Components/ArtworkImageBrowser/ArtworkImageBrowser.tsx
@@ -1,197 +1,70 @@
 import { ArtworkImageBrowser_artwork } from "v2/__generated__/ArtworkImageBrowser_artwork.graphql"
-import { Lightbox } from "v2/Components/Lightbox"
 import React, { useState } from "react"
+import { createFragmentContainer, graphql } from "react-relay"
+import { ArtworkActionsFragmentContainer as ArtworkActions } from "./ArtworkActions"
+import { Box, Spacer } from "@artsy/palette"
+import { ContextModule } from "@artsy/cohesion"
+import { ArtworkImageBrowserLargeFragmentContainer } from "./ArtworkImageBrowserLarge"
+import { ArtworkImageBrowserSmallFragmentContainer } from "./ArtworkImageBrowserSmall"
 import { Media } from "v2/Utils/Responsive"
-import {
-  Box,
-  ChevronIcon,
-  Clickable,
-  ProgressDots,
-  Swiper,
-  SwiperCell,
-  SwiperCellProps,
-  SwiperRail,
-  SwiperRailProps,
-  VisuallyHidden,
-} from "@artsy/palette"
-import { useCursor } from "use-cursor"
-import styled from "styled-components"
-import { themeGet } from "@styled-system/theme-get"
-import { compact } from "lodash"
 
-interface ArtworkBrowserProps {
-  imageAlt?: string | null
-  images: ArtworkImageBrowser_artwork["images"]
+export interface ArtworkImageBrowserProps {
+  artwork: ArtworkImageBrowser_artwork
 }
 
-export const ArtworkImageBrowser = (props: ArtworkBrowserProps) => {
-  return (
-    <>
-      <Media at="xs">
-        <SmallArtworkImageBrowser {...props} />
-      </Media>
-
-      <Media greaterThan="xs">
-        <LargeArtworkImageBrowser {...props} />
-      </Media>
-    </>
-  )
-}
-
-export const LargeArtworkImageBrowser: React.FC<ArtworkBrowserProps> = ({
-  imageAlt,
-  images: _images,
+export const ArtworkImageBrowser: React.FC<ArtworkImageBrowserProps> = ({
+  artwork,
 }) => {
-  const images = compact(_images)
+  const { images } = artwork
 
-  const { index, handleNext, handlePrev } = useCursor({ max: images.length })
+  // Rather than lift state up to reset the carousel to the first slide, just
+  // re-render it to reset the state.
+  const [key, setKey] = useState(0)
+  const handleSelectDefaultSlide = () => {
+    return setKey(prevKey => prevKey + 1)
+  }
 
-  if (images.length === 0) {
+  if ((images ?? []).length === 0) {
     return null
   }
 
-  const image = images[index]
-
   return (
-    <Box position="relative">
-      {images.length > 1 && (
-        <nav>
-          <NextPrevious
-            onClick={handlePrev}
-            aria-label="Previous image"
-            left={0}
-            p={2}
-          >
-            <ChevronIcon
-              direction="left"
-              // @ts-ignore
-              fill="currentColor"
-              width={30}
-              height={30}
-              aria-hidden
-            />
-          </NextPrevious>
-
-          <NextPrevious
-            onClick={handleNext}
-            aria-label="Next image"
-            right={0}
-            p={2}
-          >
-            <ChevronIcon
-              direction="right"
-              // @ts-ignore
-              fill="currentColor"
-              width={30}
-              height={30}
-              aria-hidden
-            />
-          </NextPrevious>
-        </nav>
-      )}
-
-      <Box p={2}>
-        <Lightbox
-          imageAlt={imageAlt}
-          deepZoom={image.deepZoom}
-          enabled={image.is_zoomable}
-          isDefault={image.is_default}
-          src={image.uri}
-          maxHeight="2000px"
-          initialHeight="60vh"
+    <Box data-test={ContextModule.artworkImage}>
+      <Media at="xs">
+        <ArtworkImageBrowserSmallFragmentContainer
+          key={key}
+          artwork={artwork}
         />
-      </Box>
+      </Media>
 
-      {images.length > 1 && (
-        <>
-          <VisuallyHidden aria-live="polite" aria-atomic="true">
-            Page {index + 1} of {images.length}
-          </VisuallyHidden>
+      <Media greaterThan="xs">
+        <ArtworkImageBrowserLargeFragmentContainer
+          key={key}
+          artwork={artwork}
+        />
+      </Media>
 
-          <ProgressDots
-            activeIndex={index}
-            amount={images.length}
-            variant="dash"
-          />
-        </>
-      )}
+      <Spacer mt={2} />
+
+      <ArtworkActions
+        artwork={artwork}
+        selectDefaultSlide={handleSelectDefaultSlide}
+      />
     </Box>
   )
 }
 
-const NextPrevious = styled(Clickable)`
-  position: absolute;
-  top: 0;
-  height: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: ${themeGet("colors.black30")};
-  transition: color 250ms;
-
-  &:hover {
-    color: ${themeGet("colors.black100")};
-  }
-`
-
-const SmallArtworkImageBrowser: React.FC<ArtworkBrowserProps> = ({
-  imageAlt,
-  images: _images,
-}) => {
-  const [index, setIndex] = useState(0)
-  const images = compact(_images)
-
-  if (images.length === 0) {
-    return null
-  }
-
-  return (
-    <>
-      <Swiper snap="center" onChange={setIndex} Cell={Cell} Rail={Rail}>
-        {images.map((image, i) => {
-          return (
-            <Lightbox
-              key={i}
-              imageAlt={imageAlt}
-              deepZoom={image.deepZoom}
-              enabled={image.is_zoomable}
-              isDefault={image.is_default}
-              src={image.uri}
-              maxHeight="2000px"
-              initialHeight="45vh"
-            />
-          )
-        })}
-      </Swiper>
-
-      {images.length > 1 && (
-        <ProgressDots
-          variant="dash"
-          amount={images.length}
-          activeIndex={index}
-        />
-      )}
-    </>
-  )
-}
-
-const Cell: React.ForwardRefExoticComponent<SwiperCellProps> = React.forwardRef(
-  (props, ref) => {
-    return (
-      <SwiperCell
-        {...props}
-        ref={ref as any}
-        display="inline-flex"
-        width="100%"
-        verticalAlign="top"
-        justifyContent="center"
-        pr={0}
-        p={2}
-      />
-    )
-  }
-)
-
-const Rail: React.FC<SwiperRailProps> = props => {
-  return <SwiperRail {...props} display="block" />
-}
+export const ArtworkImageBrowserFragmentContainer = createFragmentContainer<
+  ArtworkImageBrowserProps
+>(ArtworkImageBrowser, {
+  artwork: graphql`
+    fragment ArtworkImageBrowser_artwork on Artwork {
+      ...ArtworkActions_artwork
+      ...ArtworkImageBrowserSmall_artwork
+      ...ArtworkImageBrowserLarge_artwork
+      images {
+        internalID
+      }
+    }
+  `,
+})
---
+++ src/v2/Apps/Artwork/Components/ArtworkImageBrowser/__tests__/ArtworkImageBrowserLarge.jest.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import { graphql } from "relay-runtime"
+import { MockBoot } from "v2/DevTools"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
+import { ArtworkImageBrowserLargeFragmentContainer } from "../ArtworkImageBrowserLarge"
+
+jest.unmock("react-relay")
+
+const { getWrapper } = setupTestWrapper({
+  Component: props => {
+    return (
+      <MockBoot>
+        <ArtworkImageBrowserLargeFragmentContainer {...(props as any)} />
+      </MockBoot>
+    )
+  },
+  query: graphql`
+    query ArtworkImageBrowserLarge_Test_Query {
+      artwork(id: "example") {
+        ...ArtworkImageBrowserLarge_artwork
+      }
+    }
+  `,
+})
+
+describe("ArtworkImageBrowserLarge", () => {
+  it("renders correctly", () => {
+    const wrapper = getWrapper({
+      Image: () => ({ isDefault: true }),
+      ResizedImageUrl: () => ({
+        width: 800,
+        height: 600,
+        src: "example.jpg",
+        srcSet: "example.jpg 1x",
+      }),
+    })
+
+    expect(wrapper.html()).toContain("max-width: 800px")
+    expect(wrapper.html()).toContain('src="example.jpg"')
+    expect(wrapper.html()).toContain('srcset="example.jpg 1x"')
+  })
+})
