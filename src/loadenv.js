aa9dc77a5a83136e0a0585b5576760d46e12b4f3
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2021-07-19 18:36:54 -0400

    edit src/loadenv

--- src/loadenv.js
+++ src/loadenv.js
@@ -3,16 +3,17 @@
 import { readFileSync, accessSync, constants } from "fs"
 import { parse } from "dotenv"
 
-// parse .shared.env if it exists
-const sharedConfig = checkFileExistsSync(".env.shared")
+const shared = checkFileExistsSync(".env.shared")
   ? parse(readFileSync(".env.shared"))
   : {}
-// parse .env
-const commonConfig = parse(readFileSync(".env"))
-// combine env files and update process.env
-const envConfig = { ...sharedConfig, ...commonConfig }
-for (const k in envConfig) {
-  process.env[k] = envConfig[k]
+const env = checkFileExistsSync(".env") ? parse(readFileSync(".env")) : {}
+
+// merge env files and update process.env
+let mergedEnv = { ...shared, ...env }
+if (Object.keys(mergedEnv).length !== 0) {
+  for (const k in mergedEnv) {
+    process.env[k] = mergedEnv[k]
+  }
 }
 
 function checkFileExistsSync(filepath) {
