638f3dc03887eb727a3e2010923e3f35dd75c06c
Aliaksand Savelyeu <37010996+alexj105@users.noreply.github.com>
2021-07-19 14:34:45 +0300

    fix: correct fields order for add new adress modal [PURCHASE-2772] (#7991)
    * correct fields order for add new adress modal
    
    * fixed test

--- src/v2/Components/Address/AddressModalFields.tsx
+++ src/v2/Components/Address/AddressModalFields.tsx
@@ -1,14 +1,5 @@
 import React from "react"
-import {
-  Box,
-  Column,
-  Flex,
-  GridColumns,
-  Input,
-  Join,
-  Spacer,
-  Text,
-} from "@artsy/palette"
+import { Box, Column, Flex, GridColumns, Input, Text } from "@artsy/palette"
 import { useFormikContext } from "formik"
 import { SavedAddressType } from "v2/Apps/Order/Utils/shippingUtils"
 import { CountrySelect } from "../CountrySelect"
@@ -22,6 +13,7 @@ export const AddressModalFields: React.FC = props => {
     errors,
     setFieldValue,
   } = useFormikContext<SavedAddressType>()
+
   return (
     <>
       <Flex flexDirection="column">
@@ -37,75 +29,78 @@ export const AddressModalFields: React.FC = props => {
           value={values?.name || undefined}
         />
       </Flex>
-      <GridColumns mt={2}>
+      <GridColumns mt={[1, 2]}>
         <Column span={6}>
-          <Flex flexDirection="column">
-            <Join separator={<Spacer mb={2} />}>
-              <Box>
-                <Text mb={0.5}>Country</Text>
-                <CountrySelect
-                  selected={values?.country}
-                  onSelect={countryCode => {
-                    setFieldValue("country", countryCode)
-                  }}
-                  error={
-                    touched.country && errors.country ? errors.country : ""
-                  }
-                />
-              </Box>
-              <Input
-                title="Address Line 1"
-                placeholder="Add address"
-                name="addressLine1"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.addressLine1 && errors.addressLine1}
-                value={values?.addressLine1}
-              />
-              <Input
-                title="City"
-                placeholder="Enter city"
-                name="city"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.city && errors.city}
-                value={values?.city}
-              />
-            </Join>
-          </Flex>
+          <Box>
+            <Text mb={0.5}>Country</Text>
+            <CountrySelect
+              selected={values?.country}
+              onSelect={countryCode => {
+                setFieldValue("country", countryCode)
+              }}
+              error={touched.country && errors.country ? errors.country : ""}
+            />
+          </Box>
         </Column>
         <Column span={6}>
-          <Flex flexDirection="column">
-            <Join separator={<Spacer mb={2} />}>
-              <Input
-                title="Postal Code"
-                placeholder="Add postal code"
-                name="postalCode"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.postalCode && errors.postalCode}
-                value={values?.postalCode || ""}
-              />
-              <Input
-                title="Address Line 2 (optional)"
-                placeholder="Add address line 2"
-                name="addressLine2"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.addressLine2 && errors.addressLine2}
-                value={values?.addressLine2 || ""}
-              />
-              <Input
-                title="State, province, or region"
-                placeholder="Add state, province, or region"
-                name="region"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                error={touched.region && errors.region}
-                value={values?.region || ""}
-              />
-            </Join>
-          </Flex>
+          <Input
+            title="Postal Code"
+            placeholder="Add postal code"
+            name="postalCode"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.postalCode && errors.postalCode}
+            value={values?.postalCode || ""}
+          />
+        </Column>
+      </GridColumns>
+
+      <GridColumns mt={[1, 2]}>
+        <Column span={6}>
+          <Input
+            title="Address Line 1"
+            placeholder="Add address"
+            name="addressLine1"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.addressLine1 && errors.addressLine1}
+            value={values?.addressLine1}
+          />
+        </Column>
+        <Column span={6}>
+          <Input
+            title="Address Line 2 (optional)"
+            placeholder="Add address line 2"
+            name="addressLine2"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.addressLine2 && errors.addressLine2}
+            value={values?.addressLine2 || ""}
+          />
+        </Column>
+      </GridColumns>
+      <GridColumns mt={[1, 2]}>
+        <Column span={6}>
+          <Input
+            title="City"
+            placeholder="Enter city"
+            name="city"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.city && errors.city}
+            value={values?.city}
+          />
+        </Column>
+        <Column span={6}>
+          <Input
+            title="State, province, or region"
+            placeholder="Add state, province, or region"
+            name="region"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            error={touched.region && errors.region}
+            value={values?.region || ""}
+          />
         </Column>
       </GridColumns>
     </>
