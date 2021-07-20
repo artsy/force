58fb916042feebea2f108d769ecdf069589d4a74
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2021-07-20 15:04:41 +0300

    feat(FX-3064): base filter alignment + artist series (#8013)
    * chore: update base filters order
    
    * test: add aggregation fixtures
    
    * test: artist series artworks

---
+++ src/v2/Apps/ArtistSeries/__tests__/ArtistSeriesArtworksFilter.jest.tsx
@@ -0,0 +1,120 @@
+import { MockBoot } from "v2/DevTools"
+import React from "react"
+import { ArtistSeriesArtworksFilterRefetchContainer } from "../Components/ArtistSeriesArtworksFilter"
+import { graphql } from "react-relay"
+import { ArtistSeriesArtworksFilter_Query } from "v2/__generated__/ArtistSeriesArtworksFilter_Query.graphql"
+import { useTracking } from "v2/System/Analytics/useTracking"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
+import {
+  locationCityAggregation,
+  materialsTermsAggregation,
+  mediumAggregation,
+  partnerAggregation,
+} from "test/fixtures/aggregations"
+
+jest.unmock("react-relay")
+jest.mock("v2/System/Router/useRouter", () => ({
+  useRouter: () => ({
+    match: {
+      location: { query: {} },
+    },
+  }),
+}))
+jest.mock("v2/System/Analytics/useTracking")
+
+const { getWrapper } = setupTestWrapper<ArtistSeriesArtworksFilter_Query>({
+  Component: ({ artistSeries }) => (
+    <MockBoot user={{ id: "percy-z" }}>
+      <ArtistSeriesArtworksFilterRefetchContainer
+        aggregations={[
+          partnerAggregation,
+          locationCityAggregation,
+          mediumAggregation,
+          materialsTermsAggregation,
+        ]}
+        artistSeries={artistSeries!}
+      />
+    </MockBoot>
+  ),
+  query: graphql`
+    query ArtistSeriesArtworksFilter_Query($slug: ID!) {
+      artistSeries(id: $slug) {
+        ...ArtistSeriesArtworksFilter_artistSeries
+      }
+    }
+  `,
+  variables: { slug: "kaws-spongebob" },
+})
+
+describe("ArtistSeriesArtworksFilter", () => {
+  const trackEvent = jest.fn()
+
+  beforeEach(() => {
+    ;(useTracking as jest.Mock).mockImplementation(() => {
+      return {
+        trackEvent,
+      }
+    })
+  })
+
+  it("renders correctly", () => {
+    const wrapper = getWrapper()
+    expect(wrapper.find("ArtworkFilterArtworkGrid").length).toBe(1)
+    expect(wrapper.find("ArtworkGridItem").length).toBe(1)
+  })
+
+  it("renders filters in correct order", () => {
+    const wrapper = getWrapper({
+      FilterArtworksConnection: () => ({
+        counts: {
+          followedArtists: 10,
+        },
+      }),
+    })
+    const filterWrappers = wrapper.find("FilterExpandable")
+    const filters = [
+      {
+        label: "Rarity",
+        expanded: true,
+      },
+      {
+        label: "Medium",
+        expanded: true,
+      },
+      {
+        label: "Price",
+        expanded: true,
+      },
+      {
+        label: "Size",
+        expanded: true,
+      },
+      {
+        label: "Ways to Buy",
+        expanded: true,
+      },
+      {
+        label: "Material",
+      },
+      {
+        label: "Artwork Location",
+      },
+      {
+        label: "Time Period",
+      },
+      {
+        label: "Color",
+      },
+      {
+        label: "Galleries and Institutions",
+      },
+    ]
+
+    filters.forEach((filter, filterIndex) => {
+      const { label, expanded } = filter
+
+      expect(filterWrappers.at(filterIndex).prop("label")).toEqual(label)
+      expect(filterWrappers.at(filterIndex).prop("expanded")).toEqual(expanded)
+    })
+  })
+})
