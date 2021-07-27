b5bdef3629a79be26eb3015664632b48735ffc87
nastassia <nastassia@wsb-073-72b.local>
2021-07-27 14:00:56 +0300

    FX-3129 Added cypress test

--- src/v2/Apps/FairOrginizer/FairOrganizerApp.tsx
+++ src/v2/Apps/FairOrginizer/FairOrganizerApp.tsx
@@ -1,10 +1,11 @@
 import React from "react"
-import { Text } from "@artsy/palette"
+import { Text, Title } from "@artsy/palette"
 
 export const FairOrganizerApp: React.FC = () => {
   return (
     <>
-      <Text variant="lg" pt={4}>
+      <Title>Fair Organizer | Artsy</Title>
+      <Text as="h1" variant="lg" pt={4}>
         Fair Organizer
       </Text>
     </>
