a2b57fe50cc47a51c072412b60f9acebf7b64ec4
Cameron Rollheiser <icirellik@gmail.com>
2021-07-29 09:27:56 -0400

    feature: Webpack Stats from Env (#8092)
    Add support for loading the webpack datadog configuration from the
    shared environment file.

--- webpack/plugins/datadog.js
+++ webpack/plugins/datadog.js
@@ -5,26 +5,32 @@ import merge from "webpack-merge"
 import { env } from "../utils/env"
 
 export function metrics(config, build) {
-  if (!env.enableWebpackDatadog || !env.datadogKey) {
+  if (!env.enableWebpackDatadog) {
     return config
+  }
+
+  if (!env.datadogKey) {
+    return merge.smart(config, {
+      plugins: [new BuildPlugin()],
+    })
   }
 
   return merge.smart(config, {
     plugins: [
-      new BuildPlugin(
-        env.datadogKey
-          ? {
-              datadog: {
-                apiKey: env.datadogKey,
-                prefix: "webpack.force",
-                tags: [
-                  `build:${build}`,
-                  `env:${process.env.NODE_ENV || "development"}`,
-                ],
-              },
-            }
-          : {}
-      ),
+      new BuildPlugin({
+        datadog: {
+          apiKey: env.datadogKey,
+          prefix: "webpack.force",
+          tags: [
+            `build:${build}`,
+            `cpuLimit:${env.webpackCiCpuLimit}`,
+            `cpus:${env.machineCpus}`,
+            `env:${process.env.NODE_ENV || "development"}`,
+            `heapSize:${env.machineHeapSize}`,
+            `machine:${env.machineNameHash}`,
+          ],
+        },
+      }),
     ],
   })
 }
