cd39b564d78cd086bd3899651584c0e33e990ef7
dzucconi <mail@damonzucconi.com>
2021-07-30 15:13:03 -0400

    refactor: extracts wait

--- src/v2/Components/ViewInRoom/util.ts
+++ src/v2/Components/ViewInRoom/util.ts
@@ -3,6 +3,3 @@ import { EIGHT_FEET_CM, EIGHT_FEET_PX } from "./ViewInRoomScale"
 
 export const cmToPx = (cm: number) =>
   remapValue(cm, { min: 0, max: EIGHT_FEET_CM }, { min: 0, max: EIGHT_FEET_PX })
-
-export const wait = (ms: number) =>
-  new Promise(resolve => setTimeout(resolve, ms))
