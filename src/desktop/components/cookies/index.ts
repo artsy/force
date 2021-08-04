6864f4e38dea6fdda49b43cee9435763a108d8a7
Cameron Rollheiser <icirellik@gmail.com>
2021-08-04 11:11:05 -0400

    fix: Cookie Export (#8134)
    Renames `expires` to `expire`

--- src/desktop/components/cookies/index.ts
+++ src/desktop/components/cookies/index.ts
@@ -14,6 +14,6 @@ const TEST_STUB = ({
 
 export const get = IS_TEST_ENV ? TEST_STUB.get : cookiejs.get
 export const set = IS_TEST_ENV ? TEST_STUB.set : cookiejs.set
-export const expires = IS_TEST_ENV ? TEST_STUB.expire : cookiejs.expire
+export const expire = IS_TEST_ENV ? TEST_STUB.expire : cookiejs.expire
 
 export default IS_TEST_ENV ? TEST_STUB : cookiejs
