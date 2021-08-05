ab99720e1fd0514da0e50aeda6e74111b478b6fe
Jon Allured <jon.allured@gmail.com>
2021-08-05 10:10:09 -0500

    Migrate artwork to extracted component
    Note that an artwork that's not for sale uses CreativeWork and one that
    is commercial uses Product.

---
+++ src/v2/Components/Seo/__tests__/Product.jest.tsx
@@ -0,0 +1,27 @@
+import { mount } from "enzyme"
+import React from "react"
+import { Product } from "../Product"
+import { HeadProvider } from "react-head"
+
+describe("Product", () => {
+  const defaultProps = { data: {} }
+
+  const getWrapper = (props = {}) => {
+    const wrapper = mount(
+      <HeadProvider>
+        <Product {...defaultProps} {...props} />
+      </HeadProvider>
+    )
+
+    return wrapper
+  }
+
+  it("sets the schema type", () => {
+    const data = { title: "Pretty Painting" }
+    const props = { data }
+    const wrapper = getWrapper(props)
+    const script = wrapper.find("script")
+    expect(script.text()).toMatch('"@type": "Product"')
+    expect(script.text()).toMatch('"title": "Pretty Painting"')
+  })
+})
