84055bf0913a448590eefdc11cd01acc8fd9484c
Jon Allured <jon.allured@gmail.com>
2021-08-05 11:39:12 -0500

    Improve compute func to convert all items

--- src/v2/Components/Seo/__tests__/BreadCrumbList.jest.tsx
+++ src/v2/Components/Seo/__tests__/BreadCrumbList.jest.tsx
@@ -25,12 +25,24 @@ describe("BreadCrumbList", () => {
 
 describe("computeListItems", () => {
   it("prepends a root item", () => {
-    const listItems = computeListItems("artsy.net")
+    const items = []
+    const listItems = computeListItems(items, "artsy.net")
     expect(listItems.length).toEqual(1)
     const rootItem = listItems[0]
     expect(rootItem["@type"]).toEqual("ListItem")
     expect(rootItem.position).toEqual(1)
     expect(rootItem.item["@id"]).toEqual("artsy.net")
     expect(rootItem.item.name).toEqual("Artsy")
+  })
+
+  it("converts additional items to schema spec", () => {
+    const items = [{ path: "/artworks", name: "Artworks" }]
+    const listItems = computeListItems(items, "artsy.net")
+    expect(listItems.length).toEqual(2)
+    const artworksItem = listItems[1]
+    expect(artworksItem["@type"]).toEqual("ListItem")
+    expect(artworksItem.position).toEqual(2)
+    expect(artworksItem.item["@id"]).toEqual("artsy.net/artworks")
+    expect(artworksItem.item.name).toEqual("Artworks")
   })
 })
