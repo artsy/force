f93333277616e4a557aeba952ceb3d6639bfcdbb
nastassia <chizhikpyzhik2015@gmail.com>
2021-08-09 18:00:12 +0300

    FX-3135 Added pagination

--- src/v2/Apps/FairOrginizer/Routes/FairOrganizerDedicatedArticles.tsx
+++ src/v2/Apps/FairOrginizer/Routes/FairOrganizerDedicatedArticles.tsx
@@ -1,36 +1,94 @@
 import { Box, Column, GridColumns, Spacer, Text } from "@artsy/palette"
-import React from "react"
-import { createRefetchContainer, graphql } from "react-relay"
+import React, { useEffect, useState } from "react"
+import { createRefetchContainer, graphql, RelayRefetchProp } from "react-relay"
 import { FairEditorialItemFragmentContainer as FairEditorialItem } from "v2/Apps/Fair/Components/FairEditorial/FairEditorialItem"
+import { PaginationFragmentContainer as Pagination } from "v2/Components/Pagination"
 import { extractNodes } from "v2/Utils/extractNodes"
 import { FairOrganizerDedicatedArticlesQuery } from "./FairOrganizerDedicatedArticlesQuery"
 import { FairOrganizerDedicatedArticles_fairOrganizer } from "v2/__generated__/FairOrganizerDedicatedArticles_fairOrganizer.graphql"
+import createLogger from "v2/Utils/logger"
+import { updateUrl } from "v2/Components/ArtworkFilter/Utils/urlBuilder"
+import { LoadingArea } from "v2/Components/LoadingArea"
+
+const PAGE_SIZE = 16
 
 interface FairOrganizerDedicatedArticlesProps {
   fairOrganizer: FairOrganizerDedicatedArticles_fairOrganizer
+  relay: RelayRefetchProp
 }
 
 export const FairOrganizerDedicatedArticles: React.FC<FairOrganizerDedicatedArticlesProps> = ({
   fairOrganizer,
+  relay,
 }) => {
-  const { name, articlesConnection } = fairOrganizer
+  const { name, slug, articlesConnection } = fairOrganizer
+  const {
+    pageInfo: { hasNextPage },
+    pageCursors,
+  } = articlesConnection!
+
   const articles = extractNodes(articlesConnection)
 
+  const [page, setPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+  const logger = createLogger("FairOrganizerDedicatedArticles.tsx")
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    const relayRefetchParams = {
+      id: slug,
+      first: PAGE_SIZE,
+      page,
+    }
+
+    relay.refetch(relayRefetchParams, null, error => {
+      if (error) {
+        logger.error(error)
+      }
+
+      setIsLoading(false)
+    })
+  }, [page])
+
+  const loadNext = () => {
+    const currentPage = page ?? 0
+    const nextPage = currentPage + 1
+    if (hasNextPage) {
+      loadPage(nextPage)
+    }
+  }
+
+  const loadPage = newPage => {
+    setPage(newPage)
+    updateUrl({ page: newPage })
+  }
+
   return (
-    <Box>
+    <Box id="jump--FairOrganizerDedicatedArticles">
       <Text as="h1" variant="lg" mt={6}>
         All Articles for Explore {name} on Artsy
       </Text>
 
       <Spacer mt={4} />
 
-      <GridColumns>
-        {articles.map(article => (
-          <Column key={article.id} span={[12, 6, 3, 3]}>
-            <FairEditorialItem article={article as any} />
-          </Column>
-        ))}
-      </GridColumns>
+      <LoadingArea isLoading={isLoading}>
+        <GridColumns>
+          {articles.map(article => (
+            <Column key={article.id} span={[12, 6, 3, 3]}>
+              <FairEditorialItem article={article as any} />
+            </Column>
+          ))}
+        </GridColumns>
+      </LoadingArea>
+
+      <Pagination
+        hasNextPage={hasNextPage}
+        pageCursors={pageCursors}
+        onClick={(_cursor, page) => loadPage(page)}
+        onNext={loadNext}
+        scrollTo="#jump--FairOrganizerDedicatedArticles"
+      />
     </Box>
   )
 }
