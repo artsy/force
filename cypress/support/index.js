3eb21ffa539f46e03ff46a1eb5b9e869ee80608d
Joey Aghion <joey@aghion.com>
2021-08-11 13:24:35 -0400

    Just for giggles (and screenshots), fail on uncaught exceptions

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,11 +18,3 @@ import "./commands"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-before(() => {
-  Cypress.on("uncaught:exception", (err, runnable) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    return false
-  })
-})
