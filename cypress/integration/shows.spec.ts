6886dbcc9bd6c6ad776fe5bfccc329c5846a279e
Joey Aghion <joey@aghion.com>
2021-08-11 17:46:58 -0400

    Add cypress test of /shows route

--- cypress/integration/shows.spec.ts
+++ cypress/integration/shows.spec.ts
@@ -3,6 +3,10 @@ import { visitWithStatusRetries } from "../helpers/visitWithStatusRetries"
 
 describe("Shows", () => {
   it("/shows", () => {
+    visitWithStatusRetries("shows")
+    cy.get("h1").should("contain", "Featured Shows")
+    cy.title().should("eq", "Art Gallery Shows and Museum Exhibitions | Artsy")
+
     visitWithStatusRetries("shows2")
     cy.get("h1").should("contain", "Featured Shows")
     cy.title().should("eq", "Art Gallery Shows and Museum Exhibitions | Artsy")
