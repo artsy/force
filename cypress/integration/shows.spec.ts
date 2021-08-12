a0f1aab048462a96861697013097eff47ba0b473
Joey Aghion <joey@aghion.com>
2021-08-12 11:44:48 -0400

    Add assertion about visiting individual show page

--- cypress/integration/shows.spec.ts
+++ cypress/integration/shows.spec.ts
@@ -10,5 +10,11 @@ describe("Shows", () => {
     visitWithStatusRetries("shows2")
     cy.get("h1").should("contain", "Featured Shows")
     cy.title().should("eq", "Art Gallery Shows and Museum Exhibitions | Artsy")
+
+    // follow link to individual show
+    const showLink = cy.get('a[href*="/show/"]:first')
+    showLink.click()
+    cy.url().should("contain", "/show/")
+    cy.contains("Presented by")
   })
 })
