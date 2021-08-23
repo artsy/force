6bb0d787d7c1a94fa8ae442bf3ce24bfd58d3fb0
nastassia <chizhikpyzhik2015@gmail.com>
2021-08-23 16:17:53 +0300

    FX-3158 Fixed indents

--- src/v2/Apps/Fair/Components/ExhibitorsLetterNav.tsx
+++ src/v2/Apps/Fair/Components/ExhibitorsLetterNav.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import styled from "styled-components"
-import { Text, Flex, Swiper } from "@artsy/palette"
+import { Text, Flex, Swiper, themeProps } from "@artsy/palette"
 import { Media } from "v2/Utils/Responsive"
 import { scrollIntoView } from "v2/Utils/scrollHelpers"
 import { ExhibitorsLetterNav_fair } from "v2/__generated__/ExhibitorsLetterNav_fair.graphql"
+import { useMatchMedia } from "v2/Utils/Hooks/useMatchMedia"
+import { MOBILE_NAV_HEIGHT, DESKTOP_NAV_BAR_HEIGHT } from "v2/Components/NavBar"
 
 const LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("").concat(["0-9"])
 
@@ -17,11 +19,14 @@ export const ExhibitorsLetterNav: React.FC<ExhibitorsLetterNavProps> = ({
 }) => {
   const letters = fair?.exhibitorsGroupedByName?.map(group => group?.letter)
 
-  const Letters = props => {
+  const isMobile = useMatchMedia(themeProps.mediaQueries.xs)
+
+  const Letters = ({ withSwiper = false }) => {
     return (
-      <Flex justifyContent="space-between" {...props}>
-        {LETTERS.map(letter => {
+      <Flex justifyContent="space-between" pl={withSwiper ? 2 : 0}>
+        {LETTERS.map((letter, i) => {
           const isEnabled = letters?.includes(letter)
+          const isLast = i === LETTERS.length - 1
           return (
             <Letter
               key={letter}
@@ -29,11 +34,15 @@ export const ExhibitorsLetterNav: React.FC<ExhibitorsLetterNavProps> = ({
               title={
                 isEnabled ? `View exhibitors starting with “${letter}”` : ""
               }
+              mr={!withSwiper || isLast ? 0 : 4}
               onClick={() => {
                 if (isEnabled) {
+                  const offset = isMobile
+                    ? MOBILE_NAV_HEIGHT
+                    : DESKTOP_NAV_BAR_HEIGHT + 30
                   scrollIntoView({
                     selector: `#jump--letter${letter}`,
-                    offset: 120,
+                    offset,
                     behavior: "smooth",
                   })
                 }
@@ -51,7 +60,7 @@ export const ExhibitorsLetterNav: React.FC<ExhibitorsLetterNavProps> = ({
     <>
       <Media lessThan="md">
         <Swiper snap="start">
-          <Letters width={1300} />
+          <Letters withSwiper />
         </Swiper>
       </Media>
 
@@ -62,7 +71,7 @@ export const ExhibitorsLetterNav: React.FC<ExhibitorsLetterNavProps> = ({
   )
 }
 
-const Letter = styled(Text).attrs({ p: 1, variant: "md" })`
+const Letter = styled(Text).attrs({ variant: "md" })`
   display: inline-block;
   cursor: pointer;
   white-space: nowrap;
