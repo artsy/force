e0900445ab215a441c27d312cc68ab83d4079a37
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2021-08-23 09:56:48 -0700

    Bug Fixed regarding spacing issues for empty Auction Rails

--- src/v2/Apps/Auctions/Routes/UpcomingAuctions.tsx
+++ src/v2/Apps/Auctions/Routes/UpcomingAuctions.tsx
@@ -49,7 +49,16 @@ const UpcomingAuctions: React.FC<UpcomingAuctionsProps> = ({
   if (nodes.length === 0) {
     return (
       <Box>
-        <Text>No upcoming auctions.</Text>
+        <Text
+          as="h3"
+          color="black60"
+          mb={12}
+          mt={6}
+          textAlign="center"
+          variant="mediumText"
+        >
+          No upcoming auctions.
+        </Text>
       </Box>
     )
   }
