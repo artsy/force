0ea92141bb1612bd0ce54bab112b7992229f815a
dzucconi <mail@damonzucconi.com>
2021-08-24 13:57:33 -0400

    refactor(recaptcha): utilize hooks

--- src/v2/Utils/ReCaptchaContainer.tsx
+++ src/v2/Utils/ReCaptchaContainer.tsx
@@ -1,29 +1,18 @@
 import React from "react"
-import sharify from "sharify"
-const sd = sharify.data
+import { data as sd } from "sharify"
+import { useAppendStylesheet } from "./Hooks/useAppendStylesheet"
+import { useLoadScript } from "./Hooks/useLoadScript"
 
-export class ReCaptchaContainer extends React.Component {
-  componentDidMount() {
-    if (sd.EIGEN || !sd.RECAPTCHA_KEY) {
-      return
-    }
+export const ReCaptchaContainer: React.FC = () => {
+  useAppendStylesheet({
+    id: "google-recaptcha-css",
+    body: ".grecaptcha-badge { visibility: hidden; }",
+  })
 
-    if (document.getElementById("google-recaptcha")) {
-      return
-    }
-    const css = document.createElement("style")
-    css.setAttribute("type", "text/css")
-    css.textContent = ".grecaptcha-badge { visibility: hidden; }"
-    document.body.appendChild(css)
+  useLoadScript({
+    id: "google-recaptcha-js",
+    src: `https://www.google.com/recaptcha/api.js?render=${sd.RECAPTCHA_KEY}`,
+  })
 
-    const script = document.createElement("script")
-    script.id = "google-recaptcha"
-    script.async = true
-    script.src = `https://www.google.com/recaptcha/api.js?render=${sd.RECAPTCHA_KEY}`
-    document.body.appendChild(script)
-  }
-
-  render() {
-    return null
-  }
+  return null
 }
