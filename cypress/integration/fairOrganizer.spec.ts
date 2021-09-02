0c472bba570560b20691305a9f3be04c8d9122fc
nastassia <chizhikpyzhik2015@gmail.com>
2021-09-02 15:39:27 +0300

    Revert "Updated cypress"
    This reverts commit 194d478db18a959c61e0ce2b504deafbb3f8908b.

--- cypress/integration/fairOrganizer.spec.ts
+++ cypress/integration/fairOrganizer.spec.ts
@@ -2,14 +2,14 @@ import { visitWithStatusRetries } from "../helpers/visitWithStatusRetries"
 
 describe("FairOrganizer", () => {
   it("/fair-organizer/:slug", () => {
-    visitWithStatusRetries("fair-organizer/dallas-art-fair")
-    cy.contains("Explore Dallas Art Fair on Artsy").should("exist")
-    cy.title().should("eq", "Dallas Art Fair | Artsy")
+    visitWithStatusRetries("fair-organizer/art-paris")
+    cy.contains("Explore Art Paris on Artsy").should("exist")
+    cy.title().should("eq", "Art Paris | Artsy")
   })
 
   it("/fair-organizer/:slug/articles", () => {
-    visitWithStatusRetries("fair-organizer/dallas-art-fair/articles")
-    cy.contains("All Articles for Dallas Art Fair on Artsy").should("exist")
-    cy.title().should("eq", "Dallas Art Fair | Artsy")
+    visitWithStatusRetries("fair-organizer/art-paris/articles")
+    cy.contains("All Articles for Art Paris on Artsy").should("exist")
+    cy.title().should("eq", "Art Paris | Artsy")
   })
 })
