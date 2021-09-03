1b14495d6433bf9a67d84503a1d1bd6eafb09bd9
nastassia <chizhikpyzhik2015@gmail.com>
2021-09-03 14:31:12 +0300

    FX-3209 Updated tests

---
+++ src/v2/Apps/Fair/Components/FairEditorial/__tests__/FairEditorial.jest.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { graphql } from "relay-runtime"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
+import { FairEditorialFragmentContainer } from ".."
+
+jest.unmock("react-relay")
+
+describe("FairEditorial", () => {
+  const { getWrapper } = setupTestWrapper({
+    Component: (props: any) => {
+      return <FairEditorialFragmentContainer fair={props.fair} />
+    },
+    query: graphql`
+      query FairEditorial_Test_Query {
+        fair(id: "test") {
+          ...FairEditorial_fair
+        }
+      }
+    `,
+  })
+
+  it("renders title correctly", () => {
+    const wrapper = getWrapper({})
+    expect(wrapper.text()).toContain("Explore Further")
+  })
+
+  it("renders articles rail", () => {
+    const wrapper = getWrapper({})
+    expect(wrapper.find("FairEditorialRailArticles").length).toBe(1)
+  })
+})
