7889ae06dbb6e1558e41604b96682184b5032137
dzucconi <mail@damonzucconi.com>
2021-09-09 17:55:21 -0400

    chore(sticky): adds faq

--- src/v2/Components/Sticky/Sticky.tsx
+++ src/v2/Components/Sticky/Sticky.tsx
@@ -4,6 +4,31 @@ import React, { useEffect, useRef, useState } from "react"
 import { useSticky } from "./StickyProvider"
 import { useNavBarHeight } from "../NavBar/useNavBarHeight"
 
+/**
+ * Wrap a component to have it stick below the main nav.
+ * Use render props `{({ stuck }) => {}` to swap styles.
+ * See the stories for examples.
+ *
+ * FAQ:
+ * - Q: Why can't I simply use `position: sticky`?
+ * - A: We use `overflow-x: hidden` on the main layout container so that we can
+ *      utilize the `FullBleed` component. When you set `overflow: hidden` on
+ *      any ancestor of your sticky element, this element becomes the scrolling
+ *      container for your sticky element.
+ *
+ *      See: https://github.com/w3c/csswg-drafts/issues/865 for more detail.
+ *      TLDR: `overflow: clip` solves this and support is growing.
+ *
+ * - Q: How can I maintain the original horizontal position of the element I
+ *      want to be sticky?
+ * - A: Currently this implementation does not inherit any horizontal positioning.
+ *      Everything that is stuck is the full width of the screen. See the
+ *      `GridExample` story for an example that works around this by re-wrapping
+ *      everything in an AppContainer + HorizontalPadding. (This is non-ideal)
+ *
+ * - Q: How do I unstick an element once it reaches the end of its parent container?
+ * - A: This is also not currently supported.
+ */
 export const Sticky: React.FC = ({ children }) => {
   const sentinelRef = useRef<HTMLDivElement | null>(null)
   const containerRef = useRef<HTMLDivElement | null>(null)
