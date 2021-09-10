f5d05d46869954810b84d71790f545c3086994a2
dzucconi <mail@damonzucconi.com>
2021-09-10 11:02:30 -0400

    refactor: uses existing hook

--- src/v2/Components/Inquiry/Components/InquiryBackdrop.tsx
+++ src/v2/Components/Inquiry/Components/InquiryBackdrop.tsx
@@ -1,13 +1,10 @@
 import { ModalBase, ModalBaseProps } from "@artsy/palette"
-import React, { useEffect, useState } from "react"
+import React from "react"
 import styled from "styled-components"
+import { useDidMount } from "v2/Utils/Hooks/useDidMount"
 
 export const InquiryBackdrop: React.FC<ModalBaseProps> = props => {
-  const [isMounted, setIsMounted] = useState(false)
-
-  useEffect(() => {
-    setIsMounted(true)
-  }, [])
+  const isMounted = useDidMount()
 
   return (
     <Modal bg={isMounted ? "rgba(0, 0, 0, 0.8)" : "transparent"} {...props} />
