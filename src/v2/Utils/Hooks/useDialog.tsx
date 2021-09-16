0c27789d5adc32cea093f4631a042e2ec7abda75
dzucconi <mail@damonzucconi.com>
2021-09-16 12:32:55 -0400

    refactor: extracts dialog hook pattern

---
+++ src/v2/Utils/Hooks/useDialog.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import { useState } from "react"
+
+export interface UseDialogProps {
+  Dialog: React.ComponentType<any>
+  onShow?(): void
+  onHide?(): void
+}
+
+export const useDialog = ({ Dialog, onShow, onHide }: UseDialogProps) => {
+  const [isVisible, setIsVisible] = useState(false)
+
+  const showDialog = () => {
+    setIsVisible(true)
+    onShow?.()
+  }
+
+  const hideDialog = () => {
+    setIsVisible(false)
+    onHide?.()
+  }
+
+  const dialogComponent = <>{isVisible && <Dialog />}</>
+
+  return { isVisible, showDialog, hideDialog, dialogComponent }
+}
