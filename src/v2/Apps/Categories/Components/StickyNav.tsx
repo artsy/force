3b41d132e18818de204dda4d5b435bb0696f28fa
Matt Dole <mdole7@gmail.com>
2021-09-23 14:15:24 +0200

    chore: use HorizontalOverflow instead of Swiper

--- src/v2/Apps/Categories/Components/StickyNav.tsx
+++ src/v2/Apps/Categories/Components/StickyNav.tsx
@@ -1,13 +1,4 @@
-import {
-  Pill,
-  Spacer,
-  Swiper,
-  SwiperCell,
-  SwiperRail,
-  SwiperCellProps,
-  SwiperRailProps,
-  Flex,
-} from "@artsy/palette"
+import { Pill, Spacer, Flex, HorizontalOverflow } from "@artsy/palette"
 import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { AppContainer } from "v2/Apps/Components/AppContainer"
@@ -35,46 +26,26 @@ const StickyNav: React.FC<StickyNavProps> = props => {
   }
 
   return (
-    <Swiper Cell={Cell} Rail={Rail}>
-      <Spacer pr={[2, 4]} />
-      {geneFamilies.map((geneFamily, i) => {
-        return (
-          <Flex key={geneFamily.slug} flexDirection="row">
-            <a href={`#jump--${geneFamily.slug}`} onClick={handleClick}>
-              <Pill variant="filter">{geneFamily.name}</Pill>
-            </a>
-            {i !== geneFamilies.length - 1 ? <Spacer pr={1} /> : null}
-          </Flex>
-        )
-      })}
-      <Spacer pr={[2, 4]} />
-    </Swiper>
-  )
-}
-
-const Cell: React.ForwardRefExoticComponent<SwiperCellProps> = React.forwardRef(
-  (props, ref) => {
-    return (
-      <SwiperCell
-        {...props}
-        ref={ref as any}
-        display="inline-flex"
-        verticalAlign="top"
-        pr={0}
-      />
-    )
-  }
-)
-
-const Rail: React.FC<SwiperRailProps> = props => {
-  return (
-    // This looks weird considering we wrap this whole component in AppContainer
-    // in the parent. It's necessary to get the rail to align with the rest of
-    // the content on the page on first load, but then go all the way to
-    // the edge of the screen as the user swipes/scrolls
-    <AppContainer>
-      <SwiperRail {...props} display="block" />
-    </AppContainer>
+    <HorizontalOverflow>
+      {/* This AppContainer looks weird considering we wrap StickyNav in an
+      AppConainer in the parent. It's necessary to get the rail to align with 
+      the rest of the content on the page on first load, but then go all the 
+      way to the edge of the screen as the user swipes/scrolls */}
+      <AppContainer display="flex">
+        <Spacer pr={[2, 4]} />
+        {geneFamilies.map((geneFamily, i) => {
+          return (
+            <Flex key={geneFamily.slug} flexDirection="row">
+              <a href={`#jump--${geneFamily.slug}`} onClick={handleClick}>
+                <Pill variant="filter">{geneFamily.name}</Pill>
+              </a>
+              {i !== geneFamilies.length - 1 ? <Spacer pr={1} /> : null}
+            </Flex>
+          )
+        })}
+        <Spacer pr={[2, 4]} />
+      </AppContainer>
+    </HorizontalOverflow>
   )
 }
 
