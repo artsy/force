79fdbb0182773eb6dbe7e5db9bc8aea17e0cbc40
Ole <ole.richter91@gmail.com>
2021-09-23 17:41:52 +0200

    fix specs

--- src/v2/__generated__/CreateUserAddressMutation.graphql.ts
+++ src/v2/__generated__/CreateUserAddressMutation.graphql.ts
@@ -14,7 +14,6 @@ export type UserAddressAttributes = {
     country: string;
     name: string;
     phoneNumber?: string | null;
-    phoneNumberCountryCode?: string | null;
     postalCode?: string | null;
     region?: string | null;
 };
