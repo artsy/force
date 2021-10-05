5e6a9826b8824c3470faa19db54aa3147aa3e6a2
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-10-05 13:52:37 +0300

    added form to ArtworkDetails

--- src/v2/Apps/Consign/Routes/SubmissionFlow/ArtworkDetails/ArtworkDetails.tsx
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/ArtworkDetails/ArtworkDetails.tsx
@@ -1,12 +1,41 @@
 import React, { FC } from "react"
-import { Box } from "@artsy/palette"
+import { Button, Clickable, Spacer, Text } from "@artsy/palette"
 import { SubmissionStepper } from "v2/Apps/Consign/Components/SubmissionStepper"
+import { Formik } from "formik"
+import { ArtworkDetailsForm } from "./Components/ArtworkDetailsForm"
 
 export const ArtworkDetails: FC = () => {
   return (
     <>
+      <Clickable onClick={() => {}}>
+        <Text variant="sm">{"< Back..."}</Text>
+      </Clickable>
+      <Spacer mt={6} />
       <SubmissionStepper currentStep="Artwork Details" />
-      <Box mt={50}>Artwork Details step</Box>
+      <Spacer mb={4} />
+      <Text variant="lg">Tell us about your artwork</Text>
+      <Spacer mb={1} />
+      <Text variant="sm" color="black60">
+        All fields are required to submit a work.
+      </Text>
+      <Spacer mb={6} />
+      <Formik initialValues={{}} onSubmit={() => {}}>
+        {({ isSubmitting }) => (
+          <>
+            <ArtworkDetailsForm />
+            <Spacer mt={6} />
+            <Button
+              type="submit"
+              size="medium"
+              variant="primaryBlack"
+              loading={isSubmitting}
+              mt={2}
+            >
+              Save and Continue
+            </Button>
+          </>
+        )}
+      </Formik>
     </>
   )
 }
