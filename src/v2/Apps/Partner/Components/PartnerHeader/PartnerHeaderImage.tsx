b49aa3096566666fe89e81a1462ea3828a41e9db
dzucconi <mail@damonzucconi.com>
2021-10-13 16:26:07 -0400

    refactor(partner): removes previous fixed header implementation

--- src/v2/Apps/Partner/Components/PartnerHeader/PartnerHeaderImage.tsx
+++ src/v2/Apps/Partner/Components/PartnerHeader/PartnerHeaderImage.tsx
@@ -1,12 +1,7 @@
 import React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { PartnerHeaderImage_profile } from "v2/__generated__/PartnerHeaderImage_profile.graphql"
-import { Box } from "@artsy/palette"
-import {
-  FullBleedHeader,
-  useFullBleedHeaderHeight,
-} from "v2/Components/FullBleedHeader"
-import { useNavBarHeight } from "v2/Components/NavBar/useNavBarHeight"
+import { FullBleedHeader } from "v2/Components/FullBleedHeader"
 
 interface PartnerHeaderImageProps {
   profile: PartnerHeaderImage_profile
@@ -15,25 +10,8 @@ interface PartnerHeaderImageProps {
 export const PartnerHeaderImage: React.FC<PartnerHeaderImageProps> = ({
   profile,
 }) => {
-  const { mobile, desktop } = useNavBarHeight()
-  const height = useFullBleedHeaderHeight()
-
-  if (!profile || !profile.image) return null
-  const { image } = profile
-
-  return (
-    <>
-      <Box height={height} />
-
-      <FullBleedHeader
-        top={[mobile, desktop]}
-        position="fixed"
-        zIndex={-1}
-        // @ts-expect-error STRICT_NULL_CHECK
-        src={image.url}
-      />
-    </>
-  )
+  if (!profile || !profile.image?.url) return null
+  return <FullBleedHeader src={profile.image.url} />
 }
 
 export const PartnerHeaderImageFragmentContainer = createFragmentContainer(
