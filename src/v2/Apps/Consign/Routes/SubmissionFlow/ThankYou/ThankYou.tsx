3c38d136686ce77b62a02756b37bc0f775be9ea0
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-10-15 13:47:54 +0300

    Thank you page

--- src/v2/Apps/Consign/Routes/SubmissionFlow/ThankYou/ThankYou.tsx
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/ThankYou/ThankYou.tsx
@@ -1,6 +1,63 @@
 import React, { FC } from "react"
-import { Box } from "@artsy/palette"
+import {
+  Button,
+  Clickable,
+  Flex,
+  Text,
+  Join,
+  Spacer,
+  Box,
+} from "@artsy/palette"
+import { FAQ } from "../../MarketingLanding/Components/FAQ"
+import { SoldRecentlyQueryRenderer } from "../../MarketingLanding/Components/SoldRecently"
+import { useRouter } from "v2/System/Router/useRouter"
 
 export const ThankYou: FC = () => {
-  return <Box mt={50}>Thank You step</Box>
+  const { router } = useRouter()
+
+  const submitAnotherWork = () => {
+    router.push({ pathname: "/consign/submission2" })
+  }
+
+  const goToArtsyHomePage = () => {
+    router.push({ pathname: "/" })
+  }
+
+  return (
+    <>
+      <Text variant="xxl" mt={4}>
+        Thank you for submitting a work
+      </Text>
+      <Box maxWidth="720px" mt={4}>
+        <Text variant="lg">
+          We’ll email you within 1–3 business days to let you know the status of
+          your submission.
+        </Text>
+        <Text variant="lg" mt={2}>
+          In the meantime, feel free to submit another work—and benefit from
+          Artsy’s low fees, informed pricing, and multiple selling options.
+        </Text>
+      </Box>
+
+      <Flex py={[2, 4]} mt={4}>
+        <Button
+          size="medium"
+          variant="primaryBlack"
+          onClick={submitAnotherWork}
+        >
+          Submit Another Work
+        </Button>
+        <Clickable ml={150} onClick={goToArtsyHomePage}>
+          <Text variant="xs">
+            <u>Back to Artsy Homepage</u>
+          </Text>
+        </Clickable>
+      </Flex>
+
+      <Join separator={<Spacer mt={6} />}>
+        <SoldRecentlyQueryRenderer />
+        <FAQ />
+      </Join>
+    </>
+  )
 }
