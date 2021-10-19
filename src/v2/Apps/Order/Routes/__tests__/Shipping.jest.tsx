4d7214da8bf74540d2d43f575200b8dbb67c9def
Lily Pace <lilyfpace@gmail.com>
2021-10-19 14:28:12 -0400

    removed flushpromisequeue

--- src/v2/Apps/Order/Routes/__tests__/Shipping.jest.tsx
+++ src/v2/Apps/Order/Routes/__tests__/Shipping.jest.tsx
@@ -33,6 +33,7 @@ import {
 } from "../__fixtures__/MutationResults/saveAddress"
 import { useTracking } from "v2/System"
 import { flushPromiseQueue } from "v2/DevTools"
+import { waitFor } from "@testing-library/react"
 
 jest.unmock("react-relay")
 jest.mock("v2/System/Analytics/useTracking")
@@ -648,8 +649,8 @@ describe("Shipping", () => {
           fillAddressForm(page.root, address)
           await page.clickSubmit()
 
-          await flushPromiseQueue()
-          page.update()
+          // await flushPromiseQueue()
+          // page.update()
 
           const input = page
             .find(Input)
@@ -678,17 +679,19 @@ describe("Shipping", () => {
           expect(cityInput.props().error).toBeFalsy()
         })
 
-        it("after submit, shows all validation errors on inputs that have not been touched", async () => {
+        fit("after submit, shows all validation errors on inputs that have not been touched", async () => {
           fillIn(page.root, { title: "Full name", value: "Erik David" })
 
           await page.clickSubmit()
 
-          await flushPromiseQueue()
-          page.update()
-
           const cityInput = page
             .find(Input)
             .filterWhere(wrapper => wrapper.props().title === "City")
+
+          // await waitFor(() =>
+          //   /* eslint-disable testing-library/no-node-access */
+          //   expect(cityInput.props().error).toBeTruthy()
+          // )
 
           expect(cityInput.props().error).toBeTruthy()
         })
@@ -723,8 +726,8 @@ describe("Shipping", () => {
           fillAddressForm(page.root, address)
           await page.clickSubmit()
 
-          await flushPromiseQueue()
-          page.update()
+          // await flushPromiseQueue()
+          // page.update()
 
           expect(mutations.mockFetch).toBeCalled()
         })
