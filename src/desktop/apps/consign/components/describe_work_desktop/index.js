c0422e9de5f5ba080812d25847cf186ab3449909
Gon4ar5 <stas.gon4ar@rambler.ru>
2021-10-19 18:09:50 +0300

    [SwA-43] Migrate the edition size field in Convection to a string

--- src/desktop/apps/consign/components/describe_work_desktop/index.js
+++ src/desktop/apps/consign/components/describe_work_desktop/index.js
@@ -141,7 +141,6 @@ export function makeDescribeWorkDesktop(initialValues = {}) {
                   component={renderTextInput}
                   item={"edition_size"}
                   label={"Size of Edition*"}
-                  warn={numberWarning}
                 />
               </div>
             </div>
