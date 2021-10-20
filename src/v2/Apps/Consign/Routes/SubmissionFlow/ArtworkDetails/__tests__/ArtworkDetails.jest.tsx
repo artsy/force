4f57bf21aa0a1abd3b868f05a4d8ceaccc0ce4ed
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-10-20 12:09:27 +0300

    Save and Continue button tests

--- src/v2/Apps/Consign/Routes/SubmissionFlow/ArtworkDetails/__tests__/ArtworkDetails.jest.tsx
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/ArtworkDetails/__tests__/ArtworkDetails.jest.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { mount } from "enzyme"
-import { ArtworkDetails } from "../ArtworkDetails"
+import { ArtworkDetails, initialValues } from "../ArtworkDetails"
 import { ArtworkDetailsForm } from "../Components/ArtworkDetailsForm"
 import {
   submissionFlowSteps,
@@ -9,19 +9,43 @@ import {
 } from "v2/Apps/Consign/Components/SubmissionStepper"
 import { MockBoot } from "v2/DevTools"
 import { Breakpoint } from "v2/Utils/Responsive"
+import { flushPromiseQueue } from "v2/DevTools"
 
+const validForm = {
+  artistId: "artistId",
+  artistName: "Banksy",
+  year: "2021",
+  title: "Some title",
+  medium: "PAINTING",
+  rarity: "limited edition",
+  editionNumber: "1",
+  editionSize: "2",
+  height: "3",
+  width: "4",
+  depth: "5",
+  units: "cm",
+}
+
+const mockRouterPush = jest.fn()
 jest.mock("v2/System/Router/useRouter", () => {
   return {
     useRouter: jest.fn(() => {
       return {
-        match: {
-          params: {
-            id: "1",
-          },
-        },
+        router: { push: mockRouterPush },
+        match: { params: { id: "1" } },
       }
     }),
   }
+})
+
+let sessionStore = { "submission-1": JSON.stringify({ artistId: "artistId" }) }
+Object.defineProperty(window, "sessionStorage", {
+  value: {
+    getItem(key) {
+      return sessionStore[key] || null
+    },
+    setItem: jest.fn(),
+  },
 })
 
 const getWrapper = (breakpoint: Breakpoint = "lg") =>
@@ -32,8 +56,16 @@ const getWrapper = (breakpoint: Breakpoint = "lg") =>
   )
 
 describe("ArtworkDetails", () => {
-  it("renders correctly", () => {
+  it("renders correctly", async () => {
+    sessionStore = {
+      "submission-1": JSON.stringify({
+        artworkDetailsForm: { ...initialValues },
+      }),
+    }
     const wrapper = getWrapper()
+    await flushPromiseQueue()
+    wrapper.update()
+
     const text = wrapper.text()
 
     const artworkCurrentStep = wrapper
@@ -52,8 +84,16 @@ describe("ArtworkDetails", () => {
   })
 
   describe("Correct steps", () => {
-    it("on mobile", () => {
+    it("on mobile", async () => {
+      sessionStore = {
+        "submission-1": JSON.stringify({
+          artworkDetailsForm: { ...initialValues },
+        }),
+      }
       const wrapper = getWrapper("xs")
+      await flushPromiseQueue()
+      wrapper.update()
+
       const steps = wrapper
         .find("button")
         .filterWhere(n => n.prop("aria-selected") !== undefined)
@@ -63,8 +103,16 @@ describe("ArtworkDetails", () => {
       })
     })
 
-    it("on desktop", () => {
+    it("on desktop", async () => {
+      sessionStore = {
+        "submission-1": JSON.stringify({
+          artworkDetailsForm: { ...initialValues },
+        }),
+      }
       const wrapper = getWrapper("lg")
+      await flushPromiseQueue()
+      wrapper.update()
+
       const steps = wrapper
         .find("button")
         .filterWhere(n => n.prop("aria-selected") !== undefined)
@@ -72,6 +120,35 @@ describe("ArtworkDetails", () => {
       steps.forEach((n, idx) => {
         expect(n.text()).toBe(submissionFlowSteps[idx])
       })
+    })
+  })
+
+  describe("Save and Continue button", () => {
+    it("is disabled if form isn't valid", async () => {
+      sessionStore = {
+        "submission-1": JSON.stringify({
+          artworkDetailsForm: { ...initialValues },
+        }),
+      }
+      const wrapper = getWrapper()
+      await flushPromiseQueue()
+      wrapper.update()
+      const button = wrapper.find("button[data-test-id='save-button']")
+
+      expect(button.prop("disabled")).toBe(true)
+    })
+
+    it("is enabled if form is valid", async () => {
+      sessionStore = {
+        "submission-1": JSON.stringify({
+          artworkDetailsForm: { ...validForm },
+        }),
+      }
+      const wrapper = getWrapper()
+      await flushPromiseQueue()
+      const button = wrapper.find("button[data-test-id='save-button']")
+
+      expect(button.prop("disabled")).toBe(false)
     })
   })
 })
