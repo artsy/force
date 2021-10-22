d8c7bdac9bc9e69d64da4605afbe7d07a6045213
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-10-22 18:29:59 +0300

    Update artwork classifications modal copy

--- src/v2/Apps/Artwork/Components/__tests__/ArtworkSidebarClassificationsModal.jest.tsx
+++ src/v2/Apps/Artwork/Components/__tests__/ArtworkSidebarClassificationsModal.jest.tsx
@@ -13,13 +13,21 @@ jest.mock("@artsy/palette", () => {
 
 jest.unmock("react-relay")
 
-const { getWrapper } = setupTestWrapper({
-  Component: ArtworkSidebarClassificationsModalFragmentContainer,
-  query: ARTWORK_SIDEBAR_CLASSIFICATIONS_MODAL_QUERY,
-})
+const getWrapperWithResponsibilityMessage = (showDisclaimer?: boolean) =>
+  setupTestWrapper({
+    Component: (props: any) => {
+      return (
+        <ArtworkSidebarClassificationsModalFragmentContainer
+          {...{ ...props, showDisclaimer }}
+        />
+      )
+    },
+    query: ARTWORK_SIDEBAR_CLASSIFICATIONS_MODAL_QUERY,
+  })
 
 describe("ArtworkSidebarClassificationsModal", () => {
-  it("renders the classifications", () => {
+  it("renders the classifications with disclaimer", () => {
+    const { getWrapper } = getWrapperWithResponsibilityMessage(true)
     const wrapper = getWrapper({
       AttributionClass: () => ({
         name: "Unique",
@@ -32,6 +40,24 @@ describe("ArtworkSidebarClassificationsModal", () => {
     expect(html).toContain("Unique")
     expect(html).toContain("One of a kind piece, created by the artist.")
     expect(html).toContain(
+      "Our partners are responsible for providing accurate classification information for all works."
+    )
+  })
+
+  it("renders the classifications without disclaimer", () => {
+    const { getWrapper } = getWrapperWithResponsibilityMessage(false)
+    const wrapper = getWrapper({
+      AttributionClass: () => ({
+        name: "Unique",
+        longDescription: "One of a kind piece, created by the artist.",
+      }),
+    })
+
+    const html = wrapper.html()
+
+    expect(html).toContain("Unique")
+    expect(html).toContain("One of a kind piece, created by the artist.")
+    expect(html).not.toContain(
       "Our partners are responsible for providing accurate classification information for all works."
     )
   })
