9924f4fd26d87ec7282e584c4aa5ccc7064b9bb3
Alican Akyuz <alicakyz@gmail.com>
2021-10-25 14:16:10 +0300

    begin testing

--- src/v2/Apps/Settings/Routes/Auctions/__tests__/SettingsAuctionsRoute.jest.tsx
+++ src/v2/Apps/Settings/Routes/Auctions/__tests__/SettingsAuctionsRoute.jest.tsx
@@ -1,3 +1,93 @@
+import { graphql } from "react-relay"
+import { SettingsAuctionsRouteFragmentContainer } from "../SettingsAuctionsRoute"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
+
+jest.unmock("react-relay")
+
+const { getWrapper } = setupTestWrapper({
+  Component: SettingsAuctionsRouteFragmentContainer,
+  query: graphql`
+    query SettingsAuctionsRouteTestQuery {
+      me {
+        ...SettingsAuctionsRoute_me
+      }
+    }
+  `,
+})
+
+const data = {
+  name: "test",
+  lotStandings: [
+    {
+      isLeadingBidder: true,
+      activeBid: {
+        id: "activeBid-id",
+      },
+      saleArtwork: {
+        lotLabel: "xxx",
+        highestBid: {
+          display: "$1000",
+        },
+        counts: {
+          bidderPositions: 5,
+        },
+        artwork: {
+          title: "mist",
+          href: "/mist",
+          image: {
+            url: "someMockImageUrl",
+          },
+          artist: {
+            name: "Caspar",
+          },
+        },
+      },
+    },
+  ],
+  myBids: {
+    closed: [
+      {
+        sale: {
+          name: "mockSaleName",
+          href: "/mockSaleName",
+        },
+      },
+    ],
+  },
+  saleRegistrationsConnection: {
+    edges: [
+      {
+        node: {
+          isRegistered: false,
+          sale: {
+            name: "saleName",
+            href: "/saleName",
+            id: "1234",
+            isClosed: false,
+            startAt: "yyy",
+          },
+        },
+      },
+    ],
+  },
+}
+
 describe("SettingsAuctionsRoute", () => {
-  // TODO
+  it("renders correctly with 3 sections", () => {
+    const wrapper = getWrapper({
+      Me: () => data,
+    })
+
+    expect(wrapper.isEmptyRender()).toBe(false)
+  })
+
+  it("renders with 3 section titles", () => {
+    const wrapper = getWrapper({
+      Me: () => data,
+    })
+
+    expect(wrapper.html()).toContain("Active Bids")
+    expect(wrapper.html()).toContain("Bid History")
+    expect(wrapper.html()).toContain("Registration for Upcoming Auctions")
+  })
 })
---
+++ src/v2/__generated__/SettingsAuctionsRouteTestQuery.graphql.ts
@@ -0,0 +1,410 @@
+/* tslint:disable */
+/* eslint-disable */
+
+import { ConcreteRequest } from "relay-runtime";
+import { FragmentRefs } from "relay-runtime";
+export type SettingsAuctionsRouteTestQueryVariables = {};
+export type SettingsAuctionsRouteTestQueryResponse = {
+    readonly me: {
+        readonly " $fragmentRefs": FragmentRefs<"SettingsAuctionsRoute_me">;
+    } | null;
+};
+export type SettingsAuctionsRouteTestQuery = {
+    readonly response: SettingsAuctionsRouteTestQueryResponse;
+    readonly variables: SettingsAuctionsRouteTestQueryVariables;
+};
+
+
+
+/*
+query SettingsAuctionsRouteTestQuery {
+  me {
+    ...SettingsAuctionsRoute_me
+    id
+  }
+}
+
+fragment SettingsAuctionsRoute_me on Me {
+  name
+  lotStandings {
+    isLeadingBidder
+    activeBid {
+      id
+    }
+    saleArtwork {
+      lotLabel
+      highestBid {
+        display
+      }
+      counts {
+        bidderPositions
+      }
+      artwork {
+        title
+        href
+        image {
+          url
+        }
+        artist {
+          name
+          id
+        }
+        id
+      }
+      id
+    }
+  }
+  myBids {
+    closed {
+      sale {
+        name
+        href
+        id
+      }
+    }
+  }
+  saleRegistrationsConnection(registered: false) {
+    edges {
+      node {
+        isRegistered
+        sale {
+          name
+          href
+          id
+          isClosed
+          startAt
+        }
+        id
+      }
+    }
+  }
+}
+*/
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "name",
+  "storageKey": null
+},
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "href",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "SettingsAuctionsRouteTestQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Me",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "SettingsAuctionsRoute_me"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query"
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "SettingsAuctionsRouteTestQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Me",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "LotStanding",
+            "kind": "LinkedField",
+            "name": "lotStandings",
+            "plural": true,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "isLeadingBidder",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "BidderPosition",
+                "kind": "LinkedField",
+                "name": "activeBid",
+                "plural": false,
+                "selections": [
+                  (v1/*: any*/)
+                ],
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "SaleArtwork",
+                "kind": "LinkedField",
+                "name": "saleArtwork",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "lotLabel",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "SaleArtworkHighestBid",
+                    "kind": "LinkedField",
+                    "name": "highestBid",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "display",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "SaleArtworkCounts",
+                    "kind": "LinkedField",
+                    "name": "counts",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "bidderPositions",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Artwork",
+                    "kind": "LinkedField",
+                    "name": "artwork",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "title",
+                        "storageKey": null
+                      },
+                      (v2/*: any*/),
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Image",
+                        "kind": "LinkedField",
+                        "name": "image",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "url",
+                            "storageKey": null
+                          }
+                        ],
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Artist",
+                        "kind": "LinkedField",
+                        "name": "artist",
+                        "plural": false,
+                        "selections": [
+                          (v0/*: any*/),
+                          (v1/*: any*/)
+                        ],
+                        "storageKey": null
+                      },
+                      (v1/*: any*/)
+                    ],
+                    "storageKey": null
+                  },
+                  (v1/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "MyBids",
+            "kind": "LinkedField",
+            "name": "myBids",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "MyBid",
+                "kind": "LinkedField",
+                "name": "closed",
+                "plural": true,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Sale",
+                    "kind": "LinkedField",
+                    "name": "sale",
+                    "plural": false,
+                    "selections": [
+                      (v0/*: any*/),
+                      (v2/*: any*/),
+                      (v1/*: any*/)
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": [
+              {
+                "kind": "Literal",
+                "name": "registered",
+                "value": false
+              }
+            ],
+            "concreteType": "SaleRegistrationConnection",
+            "kind": "LinkedField",
+            "name": "saleRegistrationsConnection",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "SaleRegistrationEdge",
+                "kind": "LinkedField",
+                "name": "edges",
+                "plural": true,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "SaleRegistration",
+                    "kind": "LinkedField",
+                    "name": "node",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "isRegistered",
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Sale",
+                        "kind": "LinkedField",
+                        "name": "sale",
+                        "plural": false,
+                        "selections": [
+                          (v0/*: any*/),
+                          (v2/*: any*/),
+                          (v1/*: any*/),
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "isClosed",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "startAt",
+                            "storageKey": null
+                          }
+                        ],
+                        "storageKey": null
+                      },
+                      (v1/*: any*/)
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": "saleRegistrationsConnection(registered:false)"
+          },
+          (v1/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "id": null,
+    "metadata": {},
+    "name": "SettingsAuctionsRouteTestQuery",
+    "operationKind": "query",
+    "text": "query SettingsAuctionsRouteTestQuery {\n  me {\n    ...SettingsAuctionsRoute_me\n    id\n  }\n}\n\nfragment SettingsAuctionsRoute_me on Me {\n  name\n  lotStandings {\n    isLeadingBidder\n    activeBid {\n      id\n    }\n    saleArtwork {\n      lotLabel\n      highestBid {\n        display\n      }\n      counts {\n        bidderPositions\n      }\n      artwork {\n        title\n        href\n        image {\n          url\n        }\n        artist {\n          name\n          id\n        }\n        id\n      }\n      id\n    }\n  }\n  myBids {\n    closed {\n      sale {\n        name\n        href\n        id\n      }\n    }\n  }\n  saleRegistrationsConnection(registered: false) {\n    edges {\n      node {\n        isRegistered\n        sale {\n          name\n          href\n          id\n          isClosed\n          startAt\n        }\n        id\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+(node as any).hash = '0dba8539019df214549b3d5d9558871a';
+export default node;
