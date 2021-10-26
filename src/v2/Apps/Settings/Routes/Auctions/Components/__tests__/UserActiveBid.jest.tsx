2d5ad949976651abdd53fc5208997db57095a54e
Alican Akyuz <alicakyz@gmail.com>
2021-10-26 17:44:52 +0300

    test UserActiveBid comp

--- src/v2/Apps/Settings/Routes/Auctions/Components/__tests__/UserActiveBid.jest.tsx
+++ src/v2/Apps/Settings/Routes/Auctions/Components/__tests__/UserActiveBid.jest.tsx
@@ -2,36 +2,36 @@ import React from "react"
 import { UserActiveBid } from "../UserActiveBid"
 import { mount } from "enzyme"
 
-const data = {
-  isLeadingBidder: true,
-  activeBid: {
-    id: "activeBid-id",
-  },
-  saleArtwork: {
-    lotLabel: "xxx",
-    highestBid: {
-      display: "$1000",
+const data = [
+  {
+    isLeadingBidder: true,
+    activeBid: {
+      id: "activeBid-id",
     },
-    counts: {
-      bidderPositions: 5,
-    },
-    artwork: {
-      title: "mist",
-      href: "/mist",
-      image: {
-        url: "someMockImageUrl",
+    saleArtwork: {
+      lotLabel: "xxx",
+      highestBid: {
+        display: "$1000",
       },
-      artist: {
-        name: "Caspar",
+      counts: {
+        bidderPositions: 5,
+      },
+      artwork: {
+        title: "mist",
+        href: "/mist",
+        image: {
+          url: "someMockImageUrl",
+        },
+        artist: {
+          name: "Caspar",
+        },
       },
     },
   },
-}
+]
 
-describe("UserActiveBid component", () => {
-  let wrapper = mount(
-    <UserActiveBid lot={data} shouldDisplayBorderBottom={false} />
-  )
+describe("UserActiveBid", () => {
+  let wrapper = mount(<UserActiveBid lotStandings={data} />)
 
   afterAll(() => {
     wrapper.unmount()
@@ -41,7 +41,24 @@ describe("UserActiveBid component", () => {
     expect(wrapper.isEmptyRender()).toBe(false)
   })
 
-  it("renders correct lot number", () => {
-    expect(wrapper.find("Text").first().html()).toContain("Lot xxx")
+  it("renders active bids title", () => {
+    expect(wrapper.find("Text").first().html()).toContain("Active Bids")
+  })
+
+  it("renders correct artwork link", () => {
+    expect(wrapper.find("RouterLink").first().props().to).toEqual(
+      data[0].saleArtwork.artwork.href
+    )
+  })
+
+  it("renders the passed image url", () => {
+    expect(wrapper.find("Image").props().src).toEqual(
+      data[0].saleArtwork.artwork.image.url
+    )
+  })
+
+  it("renders nothing message when no data found", () => {
+    wrapper = mount(<UserActiveBid lotStandings={[]} />)
+    expect(wrapper.html()).toContain("Nothing to Show")
   })
 })
