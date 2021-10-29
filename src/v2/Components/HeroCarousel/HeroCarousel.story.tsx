b6535176c4d7b968952896fda3c050ced93ad500
dzucconi <mail@damonzucconi.com>
2021-10-29 16:50:21 -0400

    feat: extracts hero carousel component

---
+++ src/v2/Components/HeroCarousel/HeroCarousel.story.tsx
@@ -0,0 +1,40 @@
+import { Text } from "@artsy/palette"
+import { FC } from "react"
+import { States } from "storybook-states"
+import { HeroCarousel, HeroCarouselProps } from "./HeroCarousel"
+
+export default {
+  title: "Components/HeroCarousel",
+}
+
+export const Default = () => {
+  return (
+    <States<Partial<HeroCarouselProps>>
+      states={[
+        {},
+        { children: <ExampleCell>Single cell</ExampleCell> },
+        { fullBleed: false },
+      ]}
+    >
+      <HeroCarousel>
+        <ExampleCell>1</ExampleCell>
+        <ExampleCell>2</ExampleCell>
+        <ExampleCell>3</ExampleCell>
+      </HeroCarousel>
+    </States>
+  )
+}
+
+const ExampleCell: FC = ({ children }) => (
+  <Text
+    variant="xl"
+    bg="black10"
+    width="100%"
+    height={500}
+    display="flex"
+    alignItems="center"
+    justifyContent="center"
+  >
+    {children}
+  </Text>
+)
