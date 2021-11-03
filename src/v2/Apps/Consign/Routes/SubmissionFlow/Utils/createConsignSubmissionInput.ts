49e17ae740f8520875cdab2d5c5c710756c97c00
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-11-03 15:36:02 +0300

    returned Contact step

--- src/v2/Apps/Consign/Routes/SubmissionFlow/Utils/createConsignSubmissionInput.ts
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/Utils/createConsignSubmissionInput.ts
@@ -5,8 +5,7 @@ import {
 import { SubmissionModel } from "./useSubmission"
 
 export const createConsignSubmissionInput = (
-  submission: SubmissionModel,
-  user: User
+  submission: SubmissionModel
 ): CreateSubmissionMutationInput => {
   return {
     artistID: submission.artworkDetailsForm.artistId,
@@ -24,9 +23,9 @@ export const createConsignSubmissionInput = (
     dimensionsMetric: submission.artworkDetailsForm.units,
     provenance: submission.artworkDetailsForm.provenance,
     state: "SUBMITTED",
-    userEmail: user?.email,
-    userName: user?.name,
-    userPhone: user?.phone,
+    userEmail: submission.contactInformationForm?.email,
+    userName: submission.contactInformationForm?.name,
+    userPhone: submission.contactInformationForm?.phone,
     utmMedium: submission?.utmParams?.utmMedium,
     utmSource: submission?.utmParams?.utmSource,
     utmTerm: submission?.utmParams?.utmTerm,
