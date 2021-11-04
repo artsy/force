78c24262d03fb1fb897b6376ff5aa430355d9a09
Dima Tretyak <itretyak@ya.ru>
2021-11-04 20:05:24 +0300

    tests: rename and update user/alerts

--- cypress/integration/alerts.spec.ts
+++
@@ -1,8 +0,0 @@
-import { visitWithStatusRetries } from "../helpers/visitWithStatusRetries"
-
-describe("Alerts", () => {
-  it("/user/alerts - all alerts", () => {
-    visitWithStatusRetries("user/alerts")
-    cy.title().should("eq", "Saved Alerts | Artsy")
-  })
-})
---
+++ cypress/integration/userAlerts.spec.ts
@@ -0,0 +1,8 @@
+describe("user/alerts", () => {
+  describe("unauthenticated", () => {
+    it("redirects to the login page", () => {
+      cy.visit("user/alerts")
+      cy.contains("Login to Artsy")
+    })
+  })
+})
