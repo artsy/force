77112fcac43df6f3b8d3ac88fad7bf5cb9558d66
Hanna Trapachka <trepachko.ann@gmail.com>
2021-11-12 13:42:19 +0300

    feat(FX-3443): use palette button in CreateAlertButton

--- src/v2/Components/ArtworkFilter/SavedSearch/Components/CreateAlertButton.tsx
+++ src/v2/Components/ArtworkFilter/SavedSearch/Components/CreateAlertButton.tsx
@@ -1,31 +1,7 @@
 import React, { useState } from "react"
-import styled from "styled-components"
-import { themeGet } from "@styled-system/theme-get"
-import { BellIcon, Button, Flex, Spacer, Text } from "@artsy/palette"
+import { BellIcon, Button } from "@artsy/palette"
 import { CreateSavedSearchAlert } from "v2/Components/SavedSearchAlert/CreateSavedSearchAlert"
 import { SavedSearchAlertFormPropsBase } from "v2/Components/SavedSearchAlert/SavedSearchAlertModel"
-
-const StyledCreateAlertButton = styled(Button).attrs({
-  variant: "secondaryOutline",
-  size: "small",
-})`
-  padding: 0 10px;
-  border-color: ${themeGet("colors.blue100")};
-  div {
-    color: ${themeGet("colors.blue100")};
-  }
-  svg {
-    fill: ${themeGet("colors.blue100")};
-  }
-  &:hover {
-    div {
-      color: ${themeGet("colors.white100")};
-    }
-    svg {
-      fill: ${themeGet("colors.white100")};
-    }
-  }
-`
 
 export const CreateAlertButton: React.FC<SavedSearchAlertFormPropsBase> = ({
   artistId,
@@ -38,13 +14,10 @@ export const CreateAlertButton: React.FC<SavedSearchAlertFormPropsBase> = ({
 
   return (
     <>
-      <StyledCreateAlertButton onClick={handleOpenForm}>
-        <Flex justifyContent="space-between" alignItems="center">
-          <BellIcon width="16px" height="16px" />
-          <Spacer mr={0.5} />
-          <Text>Create an Alert</Text>
-        </Flex>
-      </StyledCreateAlertButton>
+      <Button onClick={handleOpenForm} variant="secondaryOutline" size="small">
+        <BellIcon mr={0.5} color="currentColor" />
+        Create an Alert
+      </Button>
       <CreateSavedSearchAlert
         artistId={artistId}
         artistName={artistName}
