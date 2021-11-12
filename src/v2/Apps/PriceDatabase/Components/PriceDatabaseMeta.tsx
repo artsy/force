e6ac19700ad6bf6eb22e03036310b2f5fd070db7
Jacob Herrington <jacobherringtondeveloper@gmail.com>
2021-11-12 15:49:54 -0600

    chore(GRO-660): Change metadata for Price Database

--- src/v2/Apps/PriceDatabase/Components/PriceDatabaseMeta.tsx
+++ src/v2/Apps/PriceDatabase/Components/PriceDatabaseMeta.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from "react"
 import { MetaTags } from "v2/Components/MetaTags"
 
 export const PriceDatabaseMeta: React.FC = () => {
   return (
     <MetaTags
-      title="Artsy | Artsy Price Database"
-      description="Search auction results for more than 340,000 artists for free."
+      title="Search Auction Results for Free | Artsy Price Database"
+      description="Unlimited access to auction results and art market data—for free. Browse millions of auction records from top auction houses, from 1986 to today."
       pathname="/price-database"
       imageURL="" // TODO: replace with image
     />
