d24cb1de627c9d61abe85cf00b0739bab7a520f4
Anastasia <chizhikpyzhik2015@gmail.com>
2021-11-15 15:26:13 +0300

    FX-3543 Use Pill from palette

--- src/v2/Components/ArtworkFilter/SavedSearch/Components/FiltersPills.tsx
+++ src/v2/Components/ArtworkFilter/SavedSearch/Components/FiltersPills.tsx
@@ -1,11 +1,13 @@
 import React, { FC, useEffect, useState } from "react"
 import styled from "styled-components"
-import { Box, Clickable, CloseIcon, Flex, Spacer, Text } from "@artsy/palette"
+import { Box, CloseIcon, Flex, Pill, Spacer } from "@artsy/palette"
 import { growAndFadeIn, shrinkAndFadeOut } from "v2/Assets/Animations"
 import { CreateAlertButton } from "./CreateAlertButton"
 import { SavedSearchAttributes } from "../types"
 
 const WRAPPER_HEIGHT = 70
+const PILL_HORIZONTAL_MARGIN_SIZE = 0.5
+const CLOSE_ICON_SIZE = 15
 
 interface FilterPill {
   name: string
@@ -18,36 +20,7 @@ interface FiltersPillsProps {
   savedSearchAttributes: SavedSearchAttributes
 }
 
-const Pill: FC<{ enableRemoveButton: boolean }> = ({
-  children,
-  enableRemoveButton,
-}) => {
-  return (
-    <Flex
-      alignItems="center"
-      justifyContent="center"
-      m={0.5}
-      px={1}
-      py={0.5}
-      border="1px solid"
-      borderColor="black60"
-    >
-      <Text variant="xs" overflowEllipsis>
-        {children}
-      </Text>
-      {enableRemoveButton && (
-        <>
-          <Spacer ml={0.5} />
-          <Clickable display="flex">
-            <CloseIcon />
-          </Clickable>
-        </>
-      )}
-    </Flex>
-  )
-}
-
-export const FiltersPills: React.FC<FiltersPillsProps> = ({
+export const FiltersPills: FC<FiltersPillsProps> = ({
   show,
   pills,
   savedSearchAttributes,
@@ -64,18 +37,28 @@ export const FiltersPills: React.FC<FiltersPillsProps> = ({
     <>
       {showBlock && (
         <AnimatedBox show={show}>
-          <Flex flexWrap="wrap">
+          <Flex flexWrap="wrap" mx={-PILL_HORIZONTAL_MARGIN_SIZE}>
             {pills.map((pill, index) => (
               <Pill
                 key={`filter-label-${index}`}
-                enableRemoveButton={!pill.isDefault}
+                variant="textSquare"
+                mx={PILL_HORIZONTAL_MARGIN_SIZE}
+                mb={1}
               >
                 {pill.name}
+                {!pill.isDefault && (
+                  <CloseIcon
+                    fill="currentColor"
+                    width={CLOSE_ICON_SIZE}
+                    height={CLOSE_ICON_SIZE}
+                    ml={0.5}
+                  />
+                )}
               </Pill>
             ))}
             <CreateAlertButton
               savedSearchAttributes={savedSearchAttributes}
-              m={0.5}
+              ml={PILL_HORIZONTAL_MARGIN_SIZE}
             />
           </Flex>
           <Spacer mt={4} />
