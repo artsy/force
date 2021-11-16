fcb6f27dea2d39d2cb5dc5488262cad90795e1de
Alican Akyuz <alicakyz@gmail.com>
2021-11-16 15:31:06 +0100

    feat: replace meta title for /consign

--- src/v2/Apps/Consign/Routes/MarketingLanding/Components/ConsignMeta.tsx
+++ src/v2/Apps/Consign/Routes/MarketingLanding/Components/ConsignMeta.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import * as React from "react"
 import { MetaTags } from "v2/Components/MetaTags"
 
 export const ConsignMeta: React.FC = () => {
   return (
     <MetaTags
       pathname="/consign"
-      title="Sell Artwork with Artsy | Art Consignment | Artsy"
+      title="Sell Artwork with Artsy"
       description="Sell and consign your art with Artsy. Get competitive offers from the world’s top auction houses and galleries to sell art from your collection. Submit today at no cost."
     />
   )
