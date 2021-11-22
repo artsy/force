01f2ef8d4318fd9414c243424df7e83fff0ee8cb
Alican Akyuz <alicakyz@gmail.com>
2021-11-22 11:48:27 +0100

    use error modal in artwork details and contact info steps
    (cherry picked from commit 88dc5d8e7fc60ec3e5c0cbba02f1ebb2f97ccd2e)

--- src/v2/Apps/Consign/Routes/SubmissionFlow/ArtworkDetails/Components/ArtworkDetailsForm.tsx
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/ArtworkDetails/Components/ArtworkDetailsForm.tsx
@@ -70,7 +70,6 @@ export const ArtworkDetailsForm: React.FC = () => {
 
   const { openErrorModal } = useErrorModal()
 
-  // const [isAutosuggestError] = useState(false)
   const [isRarityModalOpen, setIsRarityModalOpen] = useState(false)
   const [isProvenanceModalOpen, setIsProvenanceModalOpen] = useState(false)
 
@@ -96,17 +95,17 @@ export const ArtworkDetailsForm: React.FC = () => {
   }, [submission])
 
   const handleAutosuggestError = (isError: boolean) => {
-    openErrorModal(true, "header", "")
-
-    if (!isError) {
-      setFieldValue("artistName", "")
-      setFieldValue("artistId", "")
+    if (isError) {
+      openErrorModal()
+      return
     }
+
+    setFieldValue("artistName", "")
+    setFieldValue("artistId", "")
   }
 
   return (
     <>
-      <Button onClick={() => handleAutosuggestError(true)}>Open it</Button>
       <ArtworkSidebarClassificationsModalQueryRenderer
         onClose={() => setIsRarityModalOpen(false)}
         show={isRarityModalOpen}
