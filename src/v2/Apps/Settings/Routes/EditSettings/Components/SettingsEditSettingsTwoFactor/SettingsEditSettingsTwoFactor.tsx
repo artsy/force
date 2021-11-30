47b3e98f4092947a2516ecc3feac81a2d900f188
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2021-11-30 09:26:31 -0800

    Displaying as expected for 2FA

--- src/v2/Apps/Settings/Routes/EditSettings/Components/SettingsEditSettingsTwoFactor/SettingsEditSettingsTwoFactor.tsx
+++ src/v2/Apps/Settings/Routes/EditSettings/Components/SettingsEditSettingsTwoFactor/SettingsEditSettingsTwoFactor.tsx
@@ -1,5 +1,5 @@
-import { Box, Flex, Sans, Serif } from "@artsy/palette"
-import * as React from "react";
+import { Box, Flex, Text } from "@artsy/palette"
+import * as React from "react"
 import { RelayRefetchProp, createRefetchContainer, graphql } from "react-relay"
 
 import { SystemContextProps, useSystemContext } from "v2/System"
@@ -8,9 +8,9 @@ import { SystemQueryRenderer } from "v2/System/Relay/SystemQueryRenderer"
 import { SettingsEditSettingsTwoFactor_me } from "v2/__generated__/SettingsEditSettingsTwoFactor_me.graphql"
 import { SettingsEditSettingsTwoFactorQuery } from "v2/__generated__/SettingsEditSettingsTwoFactorQuery.graphql"
 
-import { SmsSecondFactorFragmentContainer } from "v2/Components/UserSettings/TwoFactorAuthentication/Components/SmsSecondFactor";
-import { BackupSecondFactorFragmentContainer } from "v2/Components/UserSettings/TwoFactorAuthentication/Components/BackupSecondFactor";
-import { AppSecondFactorFragmentContainer } from "v2/Components/UserSettings/TwoFactorAuthentication/Components/AppSecondFactor";
+import { SmsSecondFactorFragmentContainer } from "v2/Components/UserSettings/TwoFactorAuthentication/Components/SmsSecondFactor"
+import { BackupSecondFactorFragmentContainer } from "v2/Components/UserSettings/TwoFactorAuthentication/Components/BackupSecondFactor"
+import { AppSecondFactorFragmentContainer } from "v2/Components/UserSettings/TwoFactorAuthentication/Components/AppSecondFactor"
 
 export interface SettingsEditSettingsTwoFactorProps extends SystemContextProps {
   me: SettingsEditSettingsTwoFactor_me
@@ -20,28 +20,48 @@ export interface SettingsEditSettingsTwoFactorProps extends SystemContextProps {
 export const SettingsEditSettingsTwoFactor: React.FC<SettingsEditSettingsTwoFactorProps> = props => {
   const { me, relay } = props
 
+  const secondFactorFragments = () => {
+    if (typeof window !== "undefined") {
+      return (
+        <>
+          <AppSecondFactorFragmentContainer
+            mt={3}
+            me={me}
+            relayRefetch={relay}
+          />
+          <SmsSecondFactorFragmentContainer
+            mt={2}
+            me={me}
+            relayRefetch={relay}
+          />
+        </>
+      )
+    }
+  }
+
   return (
     <Box>
       <Flex
         flexDirection={["column", "row"]}
         alignItems={["flex-start", "flex-end"]}
       >
-        <Serif size="6" color="black100">
+        <Text variant="lg" color="black100">
           Two-factor Authentication
-        </Serif>
+        </Text>
         {me.hasSecondFactorEnabled && (
-          <Sans ml={[0, 1]} weight="medium" size="4" color="green100">
+          <Text ml={[0, 1]} variant="lg" color="green100">
             Enabled
-          </Sans>
+          </Text>
         )}
       </Flex>
-      <Serif mt={1} size="3t" maxWidth="515px" color="black60">
+      <Text mt={2} mb={4} variant="md" maxWidth="515px" color="black60">
         Set up an additional layer of security by requiring a security code in
         addition to your password to log in to your Artsy account.
-      </Serif>
-      <AppSecondFactorFragmentContainer mt={3} me={me} relayRefetch={relay} />
-      <SmsSecondFactorFragmentContainer mt={2} me={me} relayRefetch={relay} />
-      {me.hasSecondFactorEnabled && <BackupSecondFactorFragmentContainer mt={2} me={me} />}
+      </Text>
+      {secondFactorFragments()}
+      {me.hasSecondFactorEnabled && (
+        <BackupSecondFactorFragmentContainer mt={2} me={me} />
+      )}
     </Box>
   )
 }
@@ -85,7 +105,9 @@ export const SettingsEditSettingsTwoFactorQueryRenderer = () => {
           }
         }
       `}
-      render={renderWithLoadProgress(SettingsEditSettingsTwoFactorRefetchContainer)}
+      render={renderWithLoadProgress(
+        SettingsEditSettingsTwoFactorRefetchContainer
+      )}
     />
   )
 }
