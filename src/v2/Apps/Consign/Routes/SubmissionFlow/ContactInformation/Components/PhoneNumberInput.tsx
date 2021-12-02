541853555781bfff0484c20d44804a716aa5ef62
Kirill Zubarau <kiry.zubarau@artsymail.com>
2021-12-02 16:38:30 +0300

    Fix border

--- src/v2/Apps/Consign/Routes/SubmissionFlow/ContactInformation/Components/PhoneNumberInput.tsx
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/ContactInformation/Components/PhoneNumberInput.tsx
@@ -7,6 +7,7 @@ import {
   Select,
   InputProps,
   Text,
+  SelectProps,
 } from "@artsy/palette"
 import { useEffect, useState } from "react"
 
@@ -19,7 +20,8 @@ export interface PhoneNumber {
 export interface PhoneNumberInputProps extends BoxProps {
   phoneNumber: PhoneNumber
   onChange?: (regionCode: string, number?: string) => void
-  inputProps?: InputProps
+  inputProps?: Partial<InputProps>
+  selectProps?: Partial<SelectProps>
   error?: string
 }
 
@@ -27,10 +29,13 @@ export const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
   phoneNumber: { isValid, national, regionCode },
   onChange,
   inputProps,
+  selectProps,
   error,
   ...props
 }) => {
   const [number, setNumber] = useState(isValid && national ? national : "")
+  const [focus, setFocus] = useState(false)
+  const [hover, setHover] = useState(false)
   const [region, setRegion] = useState(
     isValid && regionCode ? regionCode : "us"
   )
@@ -46,6 +51,27 @@ export const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
     }
   }, [national])
 
+  const handleFocus = <T extends Element>(
+    value: boolean,
+    handler?: React.FocusEventHandler<T>
+  ) => (e: React.FocusEvent<T>) => {
+    handler?.(e)
+    setFocus(value)
+  }
+
+  const handleHover = <T extends Element>(
+    value: boolean,
+    handler?: React.MouseEventHandler<T>
+  ) => (e: React.MouseEvent<T>) => {
+    handler?.(e)
+    setHover(value)
+  }
+
+  const handleNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    inputProps?.onChange?.(e)
+    setNumber(e.target.value)
+  }
+
   return (
     <Flex flexDirection="column" {...props}>
       <Flex justifyContent="space-between">
@@ -57,10 +83,17 @@ export const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
       <Flex>
         <Box minWidth={120} maxWidth="35%">
           <Select
+            {...(selectProps || {})}
             name="countryCode"
             options={countries}
             selected={region}
+            focus={focus}
+            hover={hover}
             onSelect={setRegion}
+            onBlur={handleFocus(false, selectProps?.onBlur)}
+            onFocus={handleFocus(true, selectProps?.onFocus)}
+            onMouseOver={handleHover(true, selectProps?.onMouseOver)}
+            onMouseOut={handleHover(false, selectProps?.onMouseOut)}
             style={{
               borderRight: "none",
               letterSpacing: "1px",
@@ -73,8 +106,14 @@ export const PhoneNumberInput: React.FC<PhoneNumberInputProps> = ({
             {...(inputProps || {})}
             name="phone"
             type="tel"
-            onChange={e => setNumber(e.target.value)}
             value={number}
+            focus={focus}
+            hover={hover}
+            onBlur={handleFocus(false, inputProps?.onBlur)}
+            onFocus={handleFocus(true, inputProps?.onFocus)}
+            onMouseOver={handleHover(true, inputProps?.onMouseOver)}
+            onMouseOut={handleHover(false, inputProps?.onMouseOut)}
+            onChange={handleNumberChange}
             style={{ borderLeft: "none" }}
           />
         </Flex>
