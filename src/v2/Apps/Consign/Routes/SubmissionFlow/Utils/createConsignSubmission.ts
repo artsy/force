aa54b2f17c83955651698a7e94b851f8dfd755a2
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-12-07 12:03:50 +0300

    create-submission mutation moved to Artwork Details

--- src/v2/Apps/Consign/Routes/SubmissionFlow/Utils/createConsignSubmission.ts
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/Utils/createConsignSubmission.ts
@@ -1,78 +1,67 @@
 import { Environment } from "relay-runtime"
 import { createConsignSubmissionInput } from "./createConsignSubmissionInput"
 import {
-  addAssetToConsignment,
+  // addAssetToConsignment,
   createConsignSubmissionMutation,
-  createGeminiAssetWithS3Credentials,
-  getConvectionGeminiKey,
+  // createGeminiAssetWithS3Credentials,
+  // getConvectionGeminiKey,
 } from "../Mutations"
-import createLogger from "v2/Utils/logger"
+// import createLogger from "v2/Utils/logger"
 import { SubmissionModel } from "./useSubmission"
-import { ActionType } from "@artsy/cohesion"
-import { trackEvent } from "lib/analytics/helpers"
+// import { ActionType } from "@artsy/cohesion"
+// import { trackEvent } from "lib/analytics/helpers"
+// import { useState } from "react"
 
-const logger = createLogger("createConsignSubmission.ts")
+// const logger = createLogger("createConsignSubmission.ts")
 
-export const createConsignSubmission = async (
+export const createOrUpdateConsignSubmission = async (
   relayEnvironment: Environment,
   submission: SubmissionModel,
-  userId?: string,
+  user?: User,
   sessionId?: string
 ) => {
-  if (
-    !submission ||
-    !submission.uploadPhotosForm ||
-    !submission.contactInformationForm
-  ) {
-    return
+  if (submission.id) {
+    // TODO: updateConsignSubmissionMutation
+  } else {
+    const input = createConsignSubmissionInput(submission, sessionId)
+    const submissionId = await createConsignSubmissionMutation(
+      relayEnvironment,
+      input
+    )
+
+    return submissionId
   }
 
-  const input = createConsignSubmissionInput(submission, sessionId)
+  // const convectionKey = await getConvectionGeminiKey(relayEnvironment)
 
-  const submissionId = await createConsignSubmissionMutation(
-    relayEnvironment,
-    input
-  )
+  // await Promise.all(
+  //   submission.uploadPhotosForm.photos
+  //     .filter(photo => photo.s3Key && photo.bucket)
+  //     .map(async photo => {
+  //       try {
+  //         // Let Gemini know that this file exists and should be processed
+  //         const geminiToken = await createGeminiAssetWithS3Credentials(
+  //           relayEnvironment,
+  //           {
+  //             sourceKey: photo.s3Key!,
+  //             sourceBucket: photo.bucket!,
+  //             templateKey: convectionKey,
+  //             metadata: {
+  //               id: submissionId,
+  //               _type: "Consignment",
+  //             },
+  //           }
+  //         )
 
-  trackEvent({
-    action: ActionType.consignmentSubmitted,
-    submission_id: submissionId,
-    user_id: userId,
-    user_email: submission.contactInformationForm.email,
-  })
-
-  const convectionKey = await getConvectionGeminiKey(relayEnvironment)
-
-  await Promise.all(
-    submission.uploadPhotosForm.photos
-      .filter(photo => photo.s3Key && photo.bucket)
-      .map(async photo => {
-        try {
-          // Let Gemini know that this file exists and should be processed
-          const geminiToken = await createGeminiAssetWithS3Credentials(
-            relayEnvironment,
-            {
-              sourceKey: photo.s3Key!,
-              sourceBucket: photo.bucket!,
-              templateKey: convectionKey,
-              metadata: {
-                id: submissionId,
-                _type: "Consignment",
-              },
-            }
-          )
-
-          await addAssetToConsignment(relayEnvironment, {
-            assetType: "image",
-            geminiToken,
-            submissionID: submissionId,
-            sessionID: sessionId,
-          })
-        } catch (error) {
-          logger.error("Consign submission: add asset error", error)
-        }
-      })
-  )
-
-  return submissionId
+  //         await addAssetToConsignment(relayEnvironment, {
+  //           assetType: "image",
+  //           geminiToken,
+  //           submissionID: submissionId,
+  //           sessionID: sessionId,
+  //         })
+  //       } catch (error) {
+  //         logger.error("Consign submission: add asset error", error)
+  //       }
+  //     })
+  // )
 }
