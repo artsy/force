7552d82dfddb19f31c033731a05362685d07452c
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-12-09 11:55:28 +0300

    update submission mutation

--- src/v2/Apps/Consign/Routes/SubmissionFlow/Mutations/index.ts
+++ src/v2/Apps/Consign/Routes/SubmissionFlow/Mutations/index.ts
@@ -1,4 +1,5 @@
 export * from "./CreateConsignSubmissionMutation"
+export * from "./UpdateConsignSubmissionMutation"
 export * from "./addAssetToConsignment"
 export * from "./Gemini/createGeminiAssetWithS3Credentials"
 export * from "./Gemini/getConvectionGeminiKey"
