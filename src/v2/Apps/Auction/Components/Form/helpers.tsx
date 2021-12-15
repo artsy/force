9bd8bfa7651cc22061b570a213a6df6ad329fcb4
Nicholas Spinazze <nspinazz89@gmail.com>
2021-12-15 16:29:40 -0600

    chore:update copy on reserve not met error to improve clarity

--- src/v2/Apps/Auction/Components/Form/helpers.tsx
+++ src/v2/Apps/Auction/Components/Form/helpers.tsx
@@ -35,7 +35,7 @@ const BIDDING_STATE_TO_MESSAGE_MAPPING = {
 
   LIVE_BIDDING_STARTED: "Continue to the live sale to place your bid.",
   OUTBID: "Your bid wasn't high enough. Please select a higher bid.",
-  RESERVE_NOT_MET: "Your bid wasn't high enough. Please select a higher bid.",
+  RESERVE_NOT_MET: "Your bid is below the reserve price. Please select a higher bid.",
   SALE_CLOSED: "This sale had been closed. Please browse other open sales.",
 }
 
