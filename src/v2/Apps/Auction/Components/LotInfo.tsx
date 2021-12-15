b99053ce453dc9d936c0fcf2756b77b8a3cfacb2
Anna Carey <anna.carey@artsymail.com>
2021-12-15 17:01:51 -0500

    chore: Wrap lot image and title in a link back to the artwork page
    Co-authored-by: nick <nick.spinazze@artsymail.com>

--- src/v2/Apps/Auction/Components/LotInfo.tsx
+++ src/v2/Apps/Auction/Components/LotInfo.tsx
@@ -3,6 +3,7 @@ import { LotInfo_artwork } from "v2/__generated__/LotInfo_artwork.graphql"
 import { LotInfo_saleArtwork } from "v2/__generated__/LotInfo_saleArtwork.graphql"
 import * as React from "react"
 import { RelayProp, createFragmentContainer, graphql } from "react-relay"
+import { RouterLink } from "v2/System/Router/RouterLink"
 
 interface Props {
   artwork: LotInfo_artwork
@@ -16,28 +17,32 @@ export const LotInfo: React.FC<Props> = ({ artwork, saleArtwork }) => {
     counts: { bidderPositions: bidCount },
   } = saleArtwork
   return (
-    <Flex py={4}>
+    <Flex id="our-parent-flex-box" py={4}>
       <Box maxWidth="150px" width="100%" height="auto" p={0}>
-        <div
-          style={{
-            width: "100%",
-            paddingBottom: "100%",
-            backgroundImage: `url(${artwork.imageUrl})`,
-            backgroundSize: "contain",
-          }}
-        />
+        <RouterLink to={`/artwork/${artwork.slug}`}>
+          <div
+            style={{
+              width: "100%",
+              paddingBottom: "100%",
+              backgroundImage: `url(${artwork.imageUrl})`,
+              backgroundSize: "contain",
+            }}
+          />
+        </RouterLink>
       </Box>
       <Flex pl={2} pt={1} flexDirection="column">
-        <Text variant="md" color="black100">
-          Lot {saleArtwork.lotLabel}
-        </Text>
-        <Text variant="md" color="black100">
-          <i>{artwork.title}</i>
-          {artwork.date && `, ${artwork.date}`}
-        </Text>
-        <Text variant="md" color="black100">
-          {artwork.artistNames}
-        </Text>
+        <RouterLink to={`/artwork/${artwork.slug}`} textDecoration="none">
+          <Text variant="md" color="black100">
+            Lot {saleArtwork.lotLabel}
+          </Text>
+          <Text variant="md" color="black100">
+            <i>{artwork.title}</i>
+            {artwork.date && `, ${artwork.date}`}
+          </Text>
+          <Text variant="md" color="black100">
+            {artwork.artistNames}
+          </Text>
+        </RouterLink>
         <br />
         <Text variant="md" color="black100" fontWeight="bold">
           Current Bid: {saleArtwork.minimumNextBid?.display}
@@ -60,6 +65,7 @@ export const LotInfoFragmentContainer = createFragmentContainer(LotInfo, {
       title
       imageUrl
       artistNames
+      slug
     }
   `,
   saleArtwork: graphql`
