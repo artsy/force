103427f4153cc15e4aac88173b02c774841e5d3d
Joel Rosenblatt <7117670+jo-rs@users.noreply.github.com>
2021-12-22 00:54:16 +0100

    fix: more comprehensive test structure

--- src/v2/Apps/Order/Components/__tests__/PriceOptions.jest.tsx
+++ src/v2/Apps/Order/Components/__tests__/PriceOptions.jest.tsx
@@ -48,133 +48,138 @@ const getTrackingObject = (
   amount,
 })
 
-describe("PriceOptions - Range", () => {
+describe("PriceOptions", () => {
   beforeEach(() => {
     mockUseTracking.mockImplementation(() => ({
       trackEvent,
     }))
-    renderWithRelay({
-      Artwork: () => ({
-        priceCurrency: "USD",
-        isPriceRange: true,
-        listPrice: {
-          __typename: "PriceRange",
-          maxPrice: {
-            major: 200,
+  })
+  describe("Range", () => {
+    beforeEach(() => {
+      renderWithRelay({
+        Artwork: () => ({
+          priceCurrency: "USD",
+          isPriceRange: true,
+          listPrice: {
+            __typename: "PriceRange",
+            maxPrice: {
+              major: 200,
+            },
+            minPrice: {
+              major: 100,
+            },
           },
-          minPrice: {
+        }),
+      })
+      radios = screen.getAllByRole("radio")
+    })
+    it("renders all radio options", () => {
+      expect(radios).toHaveLength(4)
+    })
+    it("correctly formats values", () => {
+      expect(radios[0]).toHaveTextContent("US$100.00")
+      expect(radios[1]).toHaveTextContent("US$150.00")
+      expect(radios[2]).toHaveTextContent("US$200.00")
+      expect(radios[3]).toHaveTextContent("Different amount")
+    })
+    it("fires click event with correct value", () => {
+      fireEvent.click(radios[1])
+      expect(radios[1]).toBeChecked()
+      expect(setValue).toHaveBeenLastCalledWith(150)
+    })
+    it("conditionally displays input field", async () => {
+      expect(within(radios[3]).queryByRole("textbox")).not.toBeInTheDocument()
+      fireEvent.click(radios[3])
+      const input = await within(radios[3]).findByRole("textbox")
+      expect(input).toBeInTheDocument()
+    })
+    it("sets the correct custom value on input", async () => {
+      fireEvent.click(radios[3])
+      const input = await within(radios[3]).findByRole("textbox")
+      fireEvent.change(input, { target: { value: 1000 } })
+      expect(setValue).toHaveBeenLastCalledWith(1000)
+    })
+    it("correctly tracks the clicking of an option", () => {
+      fireEvent.click(radios[0])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(
+          getTrackingObject("Low-end of range", 100, "USD")
+        )
+      )
+      fireEvent.click(radios[1])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(getTrackingObject("Midpoint", 150, "USD"))
+      )
+      fireEvent.click(radios[2])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(
+          getTrackingObject("Top-end of range", 200, "USD")
+        )
+      )
+      fireEvent.click(radios[3])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(getTrackingObject("Different amount", 0, "USD"))
+      )
+    })
+    it("tracks the offer too low notice", async () => {
+      fireEvent.click(radios[3])
+      const input = await within(radios[3]).findByRole("textbox")
+      fireEvent.change(input, { target: { value: 50 } })
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          action_type: "Viewed offer too low",
+          flow: "Make offer",
+        })
+      )
+    })
+  })
+
+  describe("Exact", () => {
+    beforeEach(() => {
+      renderWithRelay({
+        Artwork: () => ({
+          priceCurrency: "EUR",
+          isPriceRange: false,
+          listPrice: {
+            __typename: "Money",
             major: 100,
           },
-        },
-      }),
-    })
-    radios = screen.getAllByRole("radio")
-  })
-  it("renders all radio options", () => {
-    expect(radios).toHaveLength(4)
-  })
-  it("correctly formats values", () => {
-    expect(radios[0]).toHaveTextContent("US$100.00")
-    expect(radios[1]).toHaveTextContent("US$150.00")
-    expect(radios[2]).toHaveTextContent("US$200.00")
-    expect(radios[3]).toHaveTextContent("Different amount")
-  })
-  it("fires click event with correct value", () => {
-    fireEvent.click(radios[1])
-    expect(radios[1]).toBeChecked()
-    expect(setValue).toHaveBeenLastCalledWith(150)
-  })
-  it("conditionally displays input field", async () => {
-    expect(within(radios[3]).queryByRole("textbox")).not.toBeInTheDocument()
-    fireEvent.click(radios[3])
-    const input = await within(radios[3]).findByRole("textbox")
-    expect(input).toBeInTheDocument()
-  })
-  it("sets the correct custom value on input", async () => {
-    fireEvent.click(radios[3])
-    const input = await within(radios[3]).findByRole("textbox")
-    fireEvent.change(input, { target: { value: 1000 } })
-    expect(setValue).toHaveBeenLastCalledWith(1000)
-  })
-  it("correctly tracks the clicking of an option", () => {
-    fireEvent.click(radios[0])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(getTrackingObject("Low-end of range", 100, "USD"))
-    )
-    fireEvent.click(radios[1])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(getTrackingObject("Midpoint", 150, "USD"))
-    )
-    fireEvent.click(radios[2])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(getTrackingObject("Top-end of range", 200, "USD"))
-    )
-    fireEvent.click(radios[3])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(getTrackingObject("Different amount", 0, "USD"))
-    )
-  })
-  it("tracks the offer too low notice", async () => {
-    fireEvent.click(radios[3])
-    const input = await within(radios[3]).findByRole("textbox")
-    fireEvent.change(input, { target: { value: 50 } })
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining({
-        action_type: "Viewed offer too low",
-        flow: "Make offer",
+        }),
       })
-    )
-  })
-})
-
-describe("PriceOptions - Exact", () => {
-  beforeEach(() => {
-    mockUseTracking.mockImplementation(() => ({
-      trackEvent,
-    }))
-    renderWithRelay({
-      Artwork: () => ({
-        priceCurrency: "EUR",
-        isPriceRange: false,
-        listPrice: {
-          __typename: "Money",
-          major: 100,
-        },
-      }),
+      radios = screen.getAllByRole("radio")
     })
-    radios = screen.getAllByRole("radio")
-  })
-  it("renders all radio options", () => {
-    expect(radios).toHaveLength(4)
-  })
-  it("correctly formats values", () => {
-    expect(radios[0]).toHaveTextContent("€80.00")
-    expect(radios[1]).toHaveTextContent("€85.00")
-    expect(radios[2]).toHaveTextContent("€90.00")
-    expect(radios[3]).toHaveTextContent("Different amount")
-  })
-  it("correctly tracks the clicking of an option", () => {
-    fireEvent.click(radios[0])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(
-        getTrackingObject("20% below the list price", 80, "EUR")
+    it("renders all radio options", () => {
+      expect(radios).toHaveLength(4)
+    })
+    it("correctly formats values", () => {
+      expect(radios[0]).toHaveTextContent("€80.00")
+      expect(radios[1]).toHaveTextContent("€85.00")
+      expect(radios[2]).toHaveTextContent("€90.00")
+      expect(radios[3]).toHaveTextContent("Different amount")
+    })
+    it("correctly tracks the clicking of an option", () => {
+      fireEvent.click(radios[0])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(
+          getTrackingObject("20% below the list price", 80, "EUR")
+        )
       )
-    )
-    fireEvent.click(radios[1])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(
-        getTrackingObject("15% below the list price", 85, "EUR")
+      fireEvent.click(radios[1])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(
+          getTrackingObject("15% below the list price", 85, "EUR")
+        )
       )
-    )
-    fireEvent.click(radios[2])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(
-        getTrackingObject("10% below the list price", 90, "EUR")
+      fireEvent.click(radios[2])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(
+          getTrackingObject("10% below the list price", 90, "EUR")
+        )
       )
-    )
-    fireEvent.click(radios[3])
-    expect(trackEvent).toHaveBeenLastCalledWith(
-      expect.objectContaining(getTrackingObject("Different amount", 0, "EUR"))
-    )
+      fireEvent.click(radios[3])
+      expect(trackEvent).toHaveBeenLastCalledWith(
+        expect.objectContaining(getTrackingObject("Different amount", 0, "EUR"))
+      )
+    })
   })
 })
