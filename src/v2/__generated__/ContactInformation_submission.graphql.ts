3ec2905ff80a7a9c6b2548e2b647d5cd09d0aea1
Sergey Kravchyonok <siarhei.krauchonak@artsymail.com>
2021-12-21 12:39:53 +0300

    update mutation on contact step

--- src/v2/__generated__/ContactInformation_submission.graphql.ts
+++ src/v2/__generated__/ContactInformation_submission.graphql.ts
@@ -6,7 +6,6 @@ import { ReaderFragment } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type ContactInformation_submission = {
     readonly id: string;
-    readonly artistId: string;
     readonly " $refType": "ContactInformation_submission";
 };
 export type ContactInformation_submission$data = ContactInformation_submission;
@@ -29,17 +28,10 @@ const node: ReaderFragment = {
       "kind": "ScalarField",
       "name": "id",
       "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "artistId",
-      "storageKey": null
     }
   ],
   "type": "ConsignmentSubmission",
   "abstractKey": null
 };
-(node as any).hash = '74e392c0edc71a47c8bd9c7b5bfdfb9f';
+(node as any).hash = '5ae005fd76817564634de5eb1fe5353c';
 export default node;
