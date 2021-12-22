de3622a611b1e7bfccd48f4c96ec3c088d898fe1
Dima Tretyak <itretyak@ya.ru>
2021-12-22 15:53:24 +0300

    chore: add basic layout for results

--- src/v2/Apps/Algolia/Routes/AlgoliaResults.tsx
+++ src/v2/Apps/Algolia/Routes/AlgoliaResults.tsx
@@ -1,8 +1,26 @@
 import * as React from "react"
 import { Box } from "@artsy/palette"
 import { Hits, Pagination } from "react-instantsearch-dom"
+import { GenericSearchResultItem } from "v2/Apps/Search/Components/GenericSearchResultItem"
+import { useRouter } from "v2/System/Router/useRouter"
 
-const Hit = ({ hit }) => <p>{hit.name}</p>
+const Hit = ({ hit }) => {
+  const { match } = useRouter()
+
+  return (
+    <Box>
+      <GenericSearchResultItem
+        name={hit.name}
+        imageUrl={hit.image_url}
+        entityType="Artist"
+        href={hit.href}
+        index={hit.__position - 1}
+        term={match.location.query?.query}
+        id={hit.objectID}
+      />
+    </Box>
+  )
+}
 
 export const AlgoliaResults = () => {
   return (
