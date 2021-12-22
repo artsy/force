46b5371a58931cc0657d9f56c72a45f52976618a
Dima Tretyak <itretyak@ya.ru>
2021-12-22 16:40:28 +0300

    feat: render all indices

---
+++ src/v2/Apps/Algolia/Components/AlgoliaIndices.tsx
@@ -0,0 +1,45 @@
+import { createFragmentContainer, graphql } from "react-relay"
+import { AlgoliaIndices_algolia } from "v2/__generated__/AlgoliaIndices_algolia.graphql"
+import { Box, Pill } from "@artsy/palette"
+
+interface AlgoliaIndicesProps {
+  algolia: AlgoliaIndices_algolia | null
+  onClick: (index: number) => void
+}
+
+export const AlgoliaIndices: React.FC<AlgoliaIndicesProps> = ({
+  algolia,
+  onClick,
+}) => {
+  return (
+    <Box my={2} mx={-0.5}>
+      {algolia?.indices.map((indice, index) => {
+        return (
+          <Pill
+            variant="filter"
+            onClick={() => onClick(index)}
+            key={indice.key}
+            mx={0.5}
+          >
+            {indice.displayName}
+          </Pill>
+        )
+      })}
+    </Box>
+  )
+}
+
+export const AlgoliaIndicesFragmentContainer = createFragmentContainer(
+  AlgoliaIndices,
+  {
+    algolia: graphql`
+      fragment AlgoliaIndices_algolia on Algolia {
+        indices {
+          displayName
+          key
+          name
+        }
+      }
+    `,
+  }
+)
