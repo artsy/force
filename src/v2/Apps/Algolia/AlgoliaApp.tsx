ab0bd8c7e9bd1d8cbe4ca6d0f391a00928b79327
Dima Tretyak <itretyak@ya.ru>
2021-12-22 15:19:08 +0300

    feat: render Algolia results

--- src/v2/Apps/Algolia/AlgoliaApp.tsx
+++ src/v2/Apps/Algolia/AlgoliaApp.tsx
@@ -1,19 +1,32 @@
-import { Box, Text } from "@artsy/palette"
 import * as React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { AlgoliaApp_system } from "v2/__generated__/AlgoliaApp_system.graphql"
+import algoliasearch from "algoliasearch"
+import { InstantSearch } from "react-instantsearch-dom"
 
 interface AlgoliaAppProps {
   system: AlgoliaApp_system
 }
 
-export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ children }) => {
-  return (
-    <Box>
-      <Text>AlgoliaApp</Text>
-      {children}
-    </Box>
-  )
+export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ system, children }) => {
+  const { algolia } = system
+  const searchClient = React.useMemo(() => {
+    if (algolia?.appID && algolia.apiKey) {
+      return algoliasearch(algolia.appID, algolia.apiKey)
+    }
+
+    return null
+  }, [algolia?.appID, algolia?.apiKey])
+
+  if (searchClient) {
+    return (
+      <InstantSearch searchClient={searchClient} indexName="Artist_staging">
+        {children}
+      </InstantSearch>
+    )
+  }
+
+  return null
 }
 
 export const AlgoliaAppFragmentContainer = createFragmentContainer(AlgoliaApp, {
