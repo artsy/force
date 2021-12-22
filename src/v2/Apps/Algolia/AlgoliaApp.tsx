34761fb7fa758ef299caeb04f58bd19b03235d75
Dima Tretyak <itretyak@ya.ru>
2021-12-22 14:13:50 +0300

    feat: add algolia routes

---
+++ src/v2/Apps/Algolia/AlgoliaApp.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@artsy/palette"
+import * as React from "react"
+
+export const AlgoliaApp: React.FC = ({ children }) => {
+  return (
+    <Box>
+      <Text>AlgoliaApp</Text>
+      {children}
+    </Box>
+  )
+}
---
+++ src/v2/Apps/Algolia/Routes/AlgoliaResults.tsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+import { Box, Text } from "@artsy/palette"
+
+export const AlgoliaResults = () => {
+  return (
+    <Box>
+      <Text>AlgoliaResults</Text>
+    </Box>
+  )
+}
