6eea5a576d92121d2772c730535a7808217fa5b1
Dima Tretyak <itretyak@ya.ru>
2021-12-22 15:40:23 +0300

    feat: filter results by query param

--- src/v2/Apps/Algolia/AlgoliaApp.tsx
+++ src/v2/Apps/Algolia/AlgoliaApp.tsx
@@ -2,7 +2,8 @@ import * as React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { AlgoliaApp_system } from "v2/__generated__/AlgoliaApp_system.graphql"
 import algoliasearch from "algoliasearch"
-import { InstantSearch } from "react-instantsearch-dom"
+import { InstantSearch, Configure } from "react-instantsearch-dom"
+import { useRouter } from "v2/System/Router/useRouter"
 
 interface AlgoliaAppProps {
   system: AlgoliaApp_system
@@ -10,6 +11,9 @@ interface AlgoliaAppProps {
 
 export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ system, children }) => {
   const { algolia } = system
+  const {
+    match: { location },
+  } = useRouter()
   const searchClient = React.useMemo(() => {
     if (algolia?.appID && algolia.apiKey) {
       return algoliasearch(algolia.appID, algolia.apiKey)
@@ -21,6 +25,7 @@ export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ system, children }) => {
   if (searchClient) {
     return (
       <InstantSearch searchClient={searchClient} indexName="Artist_staging">
+        <Configure query={location.query?.query} />
         {children}
       </InstantSearch>
     )
