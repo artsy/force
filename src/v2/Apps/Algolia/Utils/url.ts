d6e2fbd542209e39c6da03f81fd9925dc0a081b4
Dima Tretyak <itretyak@ya.ru>
2021-12-23 12:30:53 +0300

    feat: sync search state with url

---
+++ src/v2/Apps/Algolia/Utils/url.ts
@@ -0,0 +1,12 @@
+import qs from "qs"
+import { omit } from "lodash"
+import { DENIED_SEACH_STATE_KEYS } from "../constants"
+
+export const createURL = state => `?${qs.stringify(state)}`
+
+export const urlToSearchState = ({ search }) => qs.parse(search.slice(1))
+
+export const searchStateToUrl = searchState => {
+  const clearedSearchState = omit(searchState, DENIED_SEACH_STATE_KEYS)
+  return createURL(clearedSearchState)
+}
