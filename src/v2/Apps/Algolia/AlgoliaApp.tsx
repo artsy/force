67ef42900b30471d70f16538c8cfd7d7817e591a
Dima Tretyak <itretyak@ya.ru>
2021-12-23 12:30:53 +0300

    feat: sync search state with url

--- src/v2/Apps/Algolia/AlgoliaApp.tsx
+++ src/v2/Apps/Algolia/AlgoliaApp.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react"
+import { useState, useMemo, useRef, useEffect } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { AlgoliaApp_system } from "v2/__generated__/AlgoliaApp_system.graphql"
 import algoliasearch from "algoliasearch"
@@ -12,8 +12,9 @@ import { useRouter } from "v2/System/Router/useRouter"
 import { AlgoliaIndicesFragmentContainer } from "./Components/AlgoliaIndices"
 import { AlgoliaResultItem } from "./Components/AlgoliaResultItem"
 import { AlgoliaPagination } from "./Components/AlgoliaPagination"
-import { ANCHOR_CONTAINER_ID, HITS_PER_PAGE } from "./constants"
+import { ANCHOR_CONTAINER_ID, HITS_PER_PAGE, DEBOUNCE_TIME } from "./constants"
 import { Box } from "@artsy/palette"
+import { createURL, searchStateToUrl, urlToSearchState } from "./Utils/url"
 
 interface AlgoliaAppProps {
   system: AlgoliaApp_system
@@ -23,10 +24,12 @@ const ConnectedAlgoliaPagination = connectPagination(AlgoliaPagination)
 
 export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ system, children }) => {
   const { algolia } = system
-  const {
-    match: { location },
-  } = useRouter()
+  const { match } = useRouter()
+  const [searchState, setSearchState] = useState(
+    urlToSearchState(match.location)
+  )
   const [selectedTabIndex, setSelectedTabIndex] = useState(0)
+  const debouncedSetStateRef = useRef<NodeJS.Timeout | null>(null)
   const searchClient = useMemo(() => {
     if (algolia?.appID && algolia.apiKey) {
       return algoliasearch(algolia.appID, algolia.apiKey)
@@ -35,6 +38,25 @@ export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ system, children }) => {
     return null
   }, [algolia?.appID, algolia?.apiKey])
 
+  const onSearchStateChange = updatedSearchState => {
+    if (debouncedSetStateRef.current) {
+      clearTimeout(debouncedSetStateRef.current)
+    }
+
+    debouncedSetStateRef.current = setTimeout(() => {
+      const params = searchStateToUrl(updatedSearchState)
+      const url = `${match.location.pathname}${params}`
+
+      window.history.pushState(null, "", url)
+    }, DEBOUNCE_TIME)
+
+    setSearchState(updatedSearchState)
+  }
+
+  useEffect(() => {
+    setSearchState(urlToSearchState(match.location))
+  }, [match.location])
+
   if (searchClient) {
     const selectedIndice = algolia?.indices[selectedTabIndex]!
 
@@ -42,8 +64,11 @@ export const AlgoliaApp: React.FC<AlgoliaAppProps> = ({ system, children }) => {
       <InstantSearch
         searchClient={searchClient}
         indexName={selectedIndice.name}
+        searchState={searchState}
+        onSearchStateChange={onSearchStateChange}
+        createURL={createURL}
       >
-        <Configure query={location.query?.query} hitsPerPage={HITS_PER_PAGE} />
+        <Configure hitsPerPage={HITS_PER_PAGE} />
         <Box id={ANCHOR_CONTAINER_ID} />
         <AlgoliaIndicesFragmentContainer
           algolia={algolia}
--- src/v2/Apps/Algolia/constants.ts
+++ src/v2/Apps/Algolia/constants.ts
@@ -1,2 +1,4 @@
 export const HITS_PER_PAGE = 10
 export const ANCHOR_CONTAINER_ID = "jump--algoliaResults"
+export const DEBOUNCE_TIME = 400
+export const DENIED_SEACH_STATE_KEYS = ["configure"]
