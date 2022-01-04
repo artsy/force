af4e5863bbd12f7c0e81e532d0d2d2b77dd3519e
Dima Tretyak <itretyak@ya.ru>
2022-01-04 16:39:07 +0300

    feat: add banner to create saved search modal

--- src/v2/Components/SavedSearchAlert/SavedSearchAlertModal.tsx
+++ src/v2/Components/SavedSearchAlert/SavedSearchAlertModal.tsx
@@ -28,6 +28,8 @@ import { SavedSearchAttributes } from "../ArtworkFilter/SavedSearch/types"
 import { FilterPill } from "../ArtworkFilter/SavedSearch/Utils/FilterPillsContext"
 import { extractPills } from "./Utils/extractPills"
 import { Pills } from "../ArtworkFilter/SavedSearch/Components/Pills"
+import { RouterLink } from "v2/System/Router/RouterLink"
+import { Device, DOWNLOAD_APP_URLS } from "v2/Utils/Hooks/useDeviceDetection"
 
 interface SavedSearchAlertFormProps {
   savedSearchAttributes: SavedSearchAttributes
@@ -180,6 +182,42 @@ export const SavedSearchAlertModal: React.FC<SavedSearchAlertFormProps> = ({
                   selected={formik.values.push}
                 />
               </Box>
+            </Box>
+
+            <Box p={2} backgroundColor="black5">
+              <Text variant="md" mb={2} textAlign="center">
+                Download the app to stay current as new artworks are available
+                on Artsy.
+              </Text>
+              <Flex justifyContent="center" flexWrap="wrap">
+                <RouterLink
+                  to={DOWNLOAD_APP_URLS[Device.iPhone]}
+                  mx={2}
+                  textDecoration="none"
+                >
+                  <img
+                    src="https://files.artsy.net/images/download-ios-app.svg"
+                    width={120}
+                    height={40}
+                    alt="Download on the App Store"
+                    loading="lazy"
+                  />
+                </RouterLink>
+
+                <RouterLink
+                  to={DOWNLOAD_APP_URLS[Device.Android]}
+                  mx={2}
+                  textDecoration="none"
+                >
+                  <img
+                    src="https://files.artsy.net/images/download-android-app.svg"
+                    width={136}
+                    height={40}
+                    alt="Get it on Google Play"
+                    loading="lazy"
+                  />
+                </RouterLink>
+              </Flex>
             </Box>
           </Join>
         </Modal>
