e7228277c30fe8a6abb36cb98218b333aef41a39
Dima Tretyak <itretyak@ya.ru>
2022-01-06 12:25:17 +0300

    fix: typo in constant name

--- src/v2/Apps/Search2/Utils/url.ts
+++ src/v2/Apps/Search2/Utils/url.ts
@@ -1,12 +1,12 @@
 import qs from "qs"
 import { omit } from "lodash"
-import { DENIED_SEACH_STATE_KEYS } from "../constants"
+import { DENIED_SEARCH_STATE_KEYS } from "../constants"
 
 export const createURL = state => `?${qs.stringify(state)}`
 
 export const urlToSearchState = (search: string) => qs.parse(search.slice(1))
 
 export const searchStateToUrl = searchState => {
-  const clearedSearchState = omit(searchState, DENIED_SEACH_STATE_KEYS)
+  const clearedSearchState = omit(searchState, DENIED_SEARCH_STATE_KEYS)
   return createURL(clearedSearchState)
 }
--- src/v2/Apps/Search2/constants.ts
+++ src/v2/Apps/Search2/constants.ts
@@ -1,4 +1,4 @@
 export const HITS_PER_PAGE = 10
 export const ANCHOR_CONTAINER_ID = "jump--searchResults"
 export const DEBOUNCE_TIME = 400
-export const DENIED_SEACH_STATE_KEYS = ["configure"]
+export const DENIED_SEARCH_STATE_KEYS = ["configure"]
