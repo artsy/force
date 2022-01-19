104f428e67ae398d797c3816c3d660b42ee43295
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2022-01-19 10:31:53 -0800

    Basic header styling in progress, alignement correct

--- src/v2/Apps/_Preferences2/PreferencesApp.tsx
+++ src/v2/Apps/_Preferences2/PreferencesApp.tsx
@@ -1,9 +1,40 @@
-import { Text } from "@artsy/palette"
+import { Box, Column, Flex, Separator, Text } from "@artsy/palette"
 
 export const PreferencesApp: React.FC = () => {
   return (
     <>
-      <Text variant="xl">Preferences App</Text>
+      <Text variant="xl" mt={4}>Preferences Center</Text>
+      <Column
+        span={12}
+        pb={2}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            mt={12}
+            flexBasis={"50%"}
+          >
+            <Text>Subscribe to all</Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            mt={12}
+            flexBasis={"50%"}
+          >
+            <Text>Checkbox</Text>
+          </Box>
+        </Flex>
+      </Column>
+      
+      <Separator mt={4} />
     </>
-  )
+  ) 
 }
