6d8eb3de67d37f981e42e95b56e5959dac517847
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2022-01-19 11:03:36 -0800

    Preferences Center preferences visually styled and present

--- src/v2/Apps/_Preferences2/PreferencesApp.tsx
+++ src/v2/Apps/_Preferences2/PreferencesApp.tsx
@@ -1,4 +1,13 @@
-import { Box, Column, Flex, Separator, Text } from "@artsy/palette"
+import { 
+  Box, 
+  Button, 
+  Checkbox, 
+  Column, 
+  Flex, 
+  Separator, 
+  Spacer, 
+  Text 
+} from "@artsy/palette"
 
 export const PreferencesApp: React.FC = () => {
   return (
@@ -7,6 +16,8 @@ export const PreferencesApp: React.FC = () => {
       <Column
         span={12}
         pb={2}
+        pr={12}
+        mt={6}
         display="flex"
         justifyContent="space-between"
       >
@@ -15,10 +26,9 @@ export const PreferencesApp: React.FC = () => {
           flexDirection="column"
         >
           <Box
-            mt={12}
             flexBasis={"50%"}
           >
-            <Text>Subscribe to all</Text>
+            <Text variant={"md"}>Subscribe to all</Text>
           </Box>
         </Flex>
         <Flex 
@@ -26,15 +36,246 @@ export const PreferencesApp: React.FC = () => {
           flexDirection={"column"}
         >
           <Box
-            mt={12}
             flexBasis={"50%"}
           >
-            <Text>Checkbox</Text>
+            <Checkbox>Email</Checkbox>
           </Box>
         </Flex>
       </Column>
       
-      <Separator mt={4} />
+      <Separator mt={2} />
+
+      <Column
+        span={12}
+        pb={2}
+        pr={12}
+        mt={2}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Text variant="md">Recommended By Artsy</Text>
+            <Text 
+              variant="sm" 
+              color="black60"
+              >
+                Artworks, shows, fairs, auctions, and collections we think you'll love
+              </Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Checkbox>Email</Checkbox>
+          </Box>
+        </Flex>
+      </Column>
+
+      <Column
+        span={12}
+        pb={2}
+        pr={12}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Text variant="md">Art World Insights</Text>
+            <Text 
+              variant="sm" 
+              color="black60"
+              >
+                Market stories, artist profiles, exhibition reviews, and more art world insights
+              </Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Checkbox>Email</Checkbox>
+          </Box>
+        </Flex>
+      </Column>
+
+      <Column
+        span={12}
+        pb={2}
+        pr={12}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Text variant="md">Product Updates</Text>
+            <Text 
+              variant="sm" 
+              color="black60"
+              >
+                Announcements of new features on Artsy.net and the mobile app
+              </Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Checkbox>Email</Checkbox>
+          </Box>
+        </Flex>
+      </Column>
+
+      <Column
+        span={12}
+        pb={2}
+        pr={12}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Text variant="md">Guidance on Collecting</Text>
+            <Text 
+              variant="sm" 
+              color="black60"
+              >
+                Expert advice on buying and selling art, directly from an Artsy specialist
+              </Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Checkbox>Email</Checkbox>
+          </Box>
+        </Flex>
+      </Column>
+
+      <Column
+        span={12}
+        pb={2}
+        pr={12}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Text variant="md">Custom Alerts</Text>
+            <Text 
+              variant="sm" 
+              color="black60"
+              >
+                A round up of updates on your favorite artists, chosen by you
+              </Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Checkbox>Email</Checkbox>
+          </Box>
+        </Flex>
+      </Column>
+
+      <Separator mt={2} />
+
+      <Column
+        span={12}
+        pb={2}
+        pr={12}
+        pt={2}
+        display="flex"
+        justifyContent="space-between"
+      >
+        <Flex 
+          p={2}
+          flexDirection="column"
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Text variant="md">Unsubscribe from all</Text>
+          </Box>
+        </Flex>
+        <Flex 
+          p={2}
+          flexDirection={"column"}
+        >
+          <Box
+            flexBasis={"50%"}
+          >
+            <Checkbox>Email</Checkbox>
+          </Box>
+        </Flex>
+      </Column>
+
+      <Flex 
+        flexBasis="50%" 
+        alignItems="center" 
+        justifyContent="flex-end"
+        pt={2}
+        pr={4}
+      >
+        <>
+        <Button
+          width={["100%", "auto"]}
+        >
+          Cancel
+        </Button>
+
+        <Spacer ml={1} />
+
+        <Button
+          width={["100%", "auto"]}
+        >
+          Save
+        </Button>
+        </>
+      </Flex>
     </>
   ) 
 }
