ca4a70d639fddc8c82c07c59f96b19a64c133d0f
George Kartalis <g.kartalis7@gmail.com>
2022-01-26 12:25:18 +0100

    feat: add empty alert screen

--- src/v2/Apps/SavedSearchAlerts/SavedSearchAlertsList.tsx
+++ src/v2/Apps/SavedSearchAlerts/SavedSearchAlertsList.tsx
@@ -1,5 +1,14 @@
-import { Box, Clickable, Flex, Separator, Spacer, Text } from "@artsy/palette"
+import {
+  Box,
+  Button,
+  Clickable,
+  Flex,
+  Separator,
+  Spacer,
+  Text,
+} from "@artsy/palette"
 import { createPaginationContainer, graphql } from "react-relay"
+import { RouterLink } from "v2/System/Router/RouterLink"
 import { extractNodes } from "v2/Utils/extractNodes"
 import { SavedSearchAlertsList_me } from "v2/__generated__/SavedSearchAlertsList_me.graphql"
 
@@ -11,9 +20,10 @@ const SavedSearchAlertsList: React.FC<SavedSearchAlertsListProps> = ({
   me,
 }) => {
   const alerts = extractNodes(me.savedSearchesConnection)
+  const hasAlerts = alerts.length !== 0
   return (
     <>
-      {alerts.length !== 0 ? (
+      {hasAlerts ? (
         alerts.map((edge, i) => (
           <Box key={edge.internalID} px={[2, 4]} pt={4}>
             <Flex
@@ -38,7 +48,33 @@ const SavedSearchAlertsList: React.FC<SavedSearchAlertsListProps> = ({
           </Box>
         ))
       ) : (
-        <Text variant="sm">no results</Text>
+        <Box
+          mx="auto"
+          px={[2, 0, 0, 0]}
+          justifyContent="center"
+          maxWidth="540px"
+          pt={6}
+          pb={100}
+        >
+          <Flex flexDirection="column" alignItems="center">
+            <Text textAlign="center" variant="xl">
+              You haven't created any Alerts yet.
+            </Text>
+            <Text py={2} variant="sm" color="black60" textAlign="center">
+              Filter for the artworks you love on an Artist Page and tap ‘Create
+              Alert’ to be notified when new works are added to Artsy.
+            </Text>
+            <Button
+              // @ts-ignore
+              as={RouterLink}
+              to="/artists"
+              width="100%"
+              size="medium"
+            >
+              Explore Artists
+            </Button>
+          </Flex>
+        </Box>
       )}
     </>
   )
