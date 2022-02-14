68bff26cca9c1b18e09bb854661399cf51b62a27
Tanjie McMeans <tanjmcmeans01@gmail.com>
2022-02-14 16:52:04 -0600

    add helper function to smoke test

--- cypress/integration/about2.spec.ts
+++ cypress/integration/about2.spec.ts
@@ -1,5 +1,8 @@
+import { visitWithStatusRetries } from "../helpers/visitWithStatusRetries"
+
 describe("About", () => {
   it("renders page content", () => {
+    visitWithStatusRetries("about")
     cy.get("h1").should("contain", "For the Love of Art")
     cy.title().should("eq", "About | Artsy")
   })
