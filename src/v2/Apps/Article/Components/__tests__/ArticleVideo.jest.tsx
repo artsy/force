5ad312c88e630e615073b0532a3cc28b8f8988a1
dzucconi <mail@damonzucconi.com>
2022-02-14 10:25:57 -0500

    feat(article): implements video layout

---
+++ src/v2/Apps/Article/Components/__tests__/ArticleVideo.jest.tsx
@@ -0,0 +1,35 @@
+import { screen } from "@testing-library/react"
+import { graphql } from "react-relay"
+import { setupTestWrapperTL } from "v2/DevTools/setupTestWrapper"
+import { ArticleVideoFragmentContainer } from "../ArticleVideo"
+
+jest.unmock("react-relay")
+
+const { renderWithRelay } = setupTestWrapperTL({
+  Component: ArticleVideoFragmentContainer,
+  query: graphql`
+    query ArticleVideo_test_Query @relay_test_operation {
+      article(id: "example") {
+        ...ArticleVideo_article
+      }
+    }
+  `,
+})
+
+describe("ArticleVideo", () => {
+  it("renders the article", () => {
+    renderWithRelay({
+      Article: () => ({
+        media: {
+          description: "Example description",
+        },
+      }),
+    })
+
+    expect(screen.getByText("About the Film")).toBeInTheDocument()
+    expect(screen.getByText("Example description")).toBeInTheDocument()
+    expect(
+      screen.getByText("Presented in Partnership with")
+    ).toBeInTheDocument()
+  })
+})
