66f4a1428ebb4b0dca604d4c6655498aa1ebeb35
Tanjie McMeans <tanjmcmeans01@gmail.com>
2022-02-15 12:00:39 -0600

    (typo fix) pass in about2 instead of about

--- cypress/integration/about2.spec.ts
+++ cypress/integration/about2.spec.ts
@@ -2,7 +2,7 @@ import { visitWithStatusRetries } from "../helpers/visitWithStatusRetries"
 
 describe("About", () => {
   it("renders page content", () => {
-    visitWithStatusRetries("about")
+    visitWithStatusRetries("about2")
     cy.get("h1").should("contain", "For the Love of Art")
     cy.title().should("eq", "About | Artsy")
   })
