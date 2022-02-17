86784976a6d6fbf96dbf1dd318c696fe7289bccf
Tanjie McMeans <tanjmcmeans01@gmail.com>
2022-02-17 16:35:28 -0600

    update smoke test

--- cypress/integration/about2.spec.ts
+++ cypress/integration/about2.spec.ts
@@ -2,7 +2,7 @@ import { visitWithStatusRetries } from "../helpers/visitWithStatusRetries"
 
 describe("About", () => {
   it("renders page content", () => {
-    visitWithStatusRetries("about2")
+    visitWithStatusRetries("about")
     cy.get("h1").should("contain", "For the Love of Art")
     cy.title().should("eq", "About | Artsy")
   })
