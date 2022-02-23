6cd15b47713048f0fc3a2e50c8513253204e087b
Matt Jones <matt.jones@artsymail.com>
2022-02-23 14:49:55 -0500

    fix: router index import

--- src/v2/System/Router/index.ts
+++ src/v2/System/Router/index.ts
@@ -2,7 +2,7 @@ import { RelaySSREnvironment } from "v2/System/Relay/createRelaySSREnvironment"
 import { FarceCreateRouterArgs } from "found"
 import { SystemContextProps } from "../SystemContext"
 import { AppRouteConfig } from "./Route"
-
+import { FeatureFlags } from "lib/featureFlags/featureFlagService"
 export { Link } from "found"
 export { Boot } from "./Boot"
 export { SystemContextProvider, SystemContextConsumer } from "v2/System"
