9ba3b01f944c2158248ceb59ae648c483776c13c
Matt Jones <matt.jones@artsymail.com>
2022-02-25 16:50:16 -0500

    refactor: remove useVariant from provider

--- src/lib/featureFlags/FeatureFlagProvider.tsx
+++ src/lib/featureFlags/FeatureFlagProvider.tsx
@@ -30,25 +30,3 @@ export function useFeatureFlag(flagName) {
 
   return Object.keys(context).indexOf(flagName) !== -1 && context[flagName]
 }
-
-export function useFeatureFlagVariant(flagName) {
-  const context = React.useContext(FeatureFlagContext)
-
-  if (context === undefined) {
-    throw new Error(
-      "useFeatureVariant must be used within a FeatureFlagProvider"
-    )
-  }
-
-  if (!flagName) {
-    return null
-  }
-
-  if (!context[flagName]) {
-    throw new Error(
-      "the argument for flagName is not in the FeatureFlagContext"
-    )
-  }
-
-  return context[flagName]
-}
