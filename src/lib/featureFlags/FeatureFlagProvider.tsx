8c59c13122ef03300b6e44d8d6aeb5db2f086a54
Matt Jones <matt.jones@artsymail.com>
2022-02-28 14:36:27 -0500

    refactor: useFeatureFlag error handling

--- src/lib/featureFlags/FeatureFlagProvider.tsx
+++ src/lib/featureFlags/FeatureFlagProvider.tsx
@@ -5,12 +5,16 @@ export type EnabledFeatureFlags = Record<string, boolean>
 export function useFeatureFlag(flagName) {
   const { featureFlags } = useSystemContext()
   if (featureFlags === undefined) {
-    throw new Error("useFeatureFlag must be used within a FeatureFlagProvider")
-  }
-
-  if (!flagName) {
+    console.error("[Force] Error: featureFlags is undefined in SystemContext")
     return null
   }
 
-  return Object.keys(featureFlags).indexOf(flagName) !== -1 && featureFlags[flagName]
+  if (!flagName) {
+    console.error("[Force] Error: cannot find flagName in featureFlags")
+    return null
+  }
+
+  return (
+    Object.keys(featureFlags).indexOf(flagName) !== -1 && featureFlags[flagName]
+  )
 }
