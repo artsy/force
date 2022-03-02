963b35849007069fe7ce02fa46e2facc1dc23e50
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2022-03-02 12:38:19 +0300

    feat(FX-3759): add slider for price filter (#9486)
    * feat: add Range component
    
    * feat: add theme styles for Range
    
    * feat: use Range for PriceRangeFilter
    
    * chore: set max price value for slider
    
    * feat: update slider, inputs logic
    
    * chore: styles for Range
    
    * chore: add labels for custom inputs
    
    * chore: add offset for dots
    
    * chore: render labels nder slider
    
    * refactor: small updates
    
    * chore: update range styles
    
    * feat: add feature flag
    
    * chore: remove debug component
    
    * test: price filter
    
    * refactor: split price filter
    
    * chore: rename helper method
    
    Co-authored-by: George  <g.kartalis7@gmail.com>
    
    * chore: add tests for helper methods
    
    * chore: small updates
    
    Co-authored-by: George  <g.kartalis7@gmail.com>

---
+++ src/v2/Components/ArtworkFilter/ArtworkFilters/__tests__/PriceRangeFilterNew.jest.tsx
@@ -0,0 +1,210 @@
+import { fireEvent, render, screen } from "@testing-library/react"
+import {
+  ArtworkFilterContextProps,
+  ArtworkFilterContextProvider,
+  useArtworkFilterContext,
+} from "../../ArtworkFilterContext"
+import { getENV } from "v2/Utils/getENV"
+import {
+  convertToFilterFormatRange,
+  getValue,
+  parseRange,
+  PriceRangeFilterNew,
+  PriceRangeFilterNewProps,
+} from "../PriceRangeFilterNew"
+
+jest.mock("v2/Utils/Hooks/useMatchMedia", () => ({
+  __internal__useMatchMedia: () => ({}),
+}))
+jest.mock("v2/Utils/getENV")
+jest.mock("lodash/debounce", () => jest.fn(e => e))
+
+describe("PriceRangeFilterNew", () => {
+  let context: ArtworkFilterContextProps
+
+  const renderPriceRangeFilter = (
+    props: PriceRangeFilterNewProps = { expanded: true }
+  ) => {
+    return render(
+      <ArtworkFilterContextProvider>
+        <PriceRangeFilterTest {...props} />
+      </ArtworkFilterContextProvider>
+    )
+  }
+
+  const PriceRangeFilterTest = (props: PriceRangeFilterNewProps) => {
+    context = useArtworkFilterContext()
+    return <PriceRangeFilterNew {...props} />
+  }
+
+  const mockGetENV = getENV as jest.Mock
+
+  beforeEach(() => {
+    mockGetENV.mockImplementation(() => {
+      return true
+    })
+  })
+
+  it("updates the filter and min slider when the min custom input is changed", () => {
+    renderPriceRangeFilter()
+
+    const minSliderHandle = screen.queryByLabelText("Min price slider handle")
+
+    fireEvent.change(screen.getByLabelText("Min price"), {
+      target: { value: "10000" },
+    })
+
+    expect(context.filters?.priceRange).toEqual("10000-*")
+    expect(minSliderHandle).toHaveAttribute("aria-valuenow", "10000")
+  })
+
+  it("updates the filter and max slider when the max custom input is changed", () => {
+    renderPriceRangeFilter()
+
+    const maxSliderHandle = screen.queryByLabelText("Max price slider handle")
+
+    fireEvent.change(screen.getByLabelText("Max price"), {
+      target: { value: "35000" },
+    })
+
+    expect(context.filters?.priceRange).toEqual("*-35000")
+    expect(maxSliderHandle).toHaveAttribute("aria-valuenow", "35000")
+  })
+
+  it("updates the filter and sliders when the custom inputs are changed", () => {
+    renderPriceRangeFilter()
+
+    const minSliderHandle = screen.queryByLabelText("Min price slider handle")
+    const maxSliderHandle = screen.queryByLabelText("Max price slider handle")
+
+    fireEvent.change(screen.getByLabelText("Min price"), {
+      target: { value: "10000" },
+    })
+    fireEvent.change(screen.getByLabelText("Max price"), {
+      target: { value: "35000" },
+    })
+
+    expect(context.filters?.priceRange).toEqual("10000-35000")
+    expect(minSliderHandle).toHaveAttribute("aria-valuenow", "10000")
+    expect(maxSliderHandle).toHaveAttribute("aria-valuenow", "35000")
+  })
+
+  it("do not apply price restrictions when the custom inputs are empty", () => {
+    renderPriceRangeFilter()
+
+    fireEvent.change(screen.getByLabelText("Min price"), {
+      target: { value: "10000" },
+    })
+    fireEvent.change(screen.getByLabelText("Max price"), {
+      target: { value: "35000" },
+    })
+
+    expect(context.filters?.priceRange).toEqual("10000-35000")
+
+    fireEvent.change(screen.getByLabelText("Min price"), {
+      target: { value: "" },
+    })
+    fireEvent.change(screen.getByLabelText("Max price"), {
+      target: { value: "" },
+    })
+
+    expect(context.filters?.priceRange).toEqual("*-*")
+  })
+
+  describe("the `expanded` prop", () => {
+    it("hides the filter controls when not set", () => {
+      renderPriceRangeFilter({})
+
+      const minSliderHandle = screen.queryByLabelText("Min price slider handle")
+      const maxSliderHandle = screen.queryByLabelText("Min price slider handle")
+
+      expect(screen.queryByLabelText("Min price")).not.toBeInTheDocument()
+      expect(screen.queryByLabelText("Max price")).not.toBeInTheDocument()
+
+      expect(minSliderHandle).not.toBeInTheDocument()
+      expect(maxSliderHandle).not.toBeInTheDocument()
+    })
+
+    it("hides the filter controls when `false`", () => {
+      renderPriceRangeFilter({
+        expanded: false,
+      })
+
+      const minSliderHandle = screen.queryByLabelText("Min price slider handle")
+      const maxSliderHandle = screen.queryByLabelText("Min price slider handle")
+
+      expect(screen.queryByLabelText("Min price")).not.toBeInTheDocument()
+      expect(screen.queryByLabelText("Max price")).not.toBeInTheDocument()
+
+      expect(minSliderHandle).not.toBeInTheDocument()
+      expect(maxSliderHandle).not.toBeInTheDocument()
+    })
+
+    it("shows the filter controls when `true`", () => {
+      renderPriceRangeFilter({
+        expanded: true,
+      })
+
+      const minSliderHandle = screen.queryByLabelText("Min price slider handle")
+      const maxSliderHandle = screen.queryByLabelText("Min price slider handle")
+
+      expect(screen.queryByLabelText("Min price")).toBeInTheDocument()
+      expect(screen.queryByLabelText("Max price")).toBeInTheDocument()
+
+      expect(minSliderHandle).toBeInTheDocument()
+      expect(maxSliderHandle).toBeInTheDocument()
+    })
+  })
+})
+
+describe("convertToFilterFormatRange", () => {
+  it("should return passed values", () => {
+    expect(convertToFilterFormatRange([10, 20])).toEqual([10, 20])
+    expect(convertToFilterFormatRange([1000, 2000])).toEqual([1000, 2000])
+    expect(convertToFilterFormatRange([35000, 60000])).toEqual([35000, 60000])
+  })
+
+  it("should return default range value for min if min slider value is passed", () => {
+    expect(convertToFilterFormatRange([0, 20])).toEqual(["*", 20])
+  })
+
+  it("should return default range value for max if max slider value is passed", () => {
+    expect(convertToFilterFormatRange([10, 50000])).toEqual([10, "*"])
+  })
+
+  it("should return default range value if min and max slider values are passed", () => {
+    expect(convertToFilterFormatRange([0, 50000])).toEqual(["*", "*"])
+  })
+})
+
+describe("getValue", () => {
+  it("should return passed values", () => {
+    expect(getValue(10)).toBe(10)
+    expect(getValue(15000)).toBe(15000)
+    expect(getValue(60000)).toBe(60000)
+  })
+
+  it("should return empty string when default range value is passed", () => {
+    expect(getValue("*")).toBe("")
+  })
+
+  it("should return empty string when 0 is passed", () => {
+    expect(getValue(0)).toBe("")
+  })
+})
+
+describe("parseRange", () => {
+  it("should correctly parse range when valid range is passed", () => {
+    expect(parseRange("5-10")).toEqual([5, 10])
+  })
+
+  it("should return numeric values", () => {
+    expect(parseRange("5.5-10.789")).toEqual([5, 10])
+  })
+
+  it("should correctly parse range when default range values are passed", () => {
+    expect(parseRange("*-5")).toEqual(["*", 5])
+    expect(parseRange("5-*")).toEqual([5, "*"])
+    expect(parseRange("*-*")).toEqual(["*", "*"])
+  })
+})
---
+++ src/v2/Components/Range.tsx
@@ -0,0 +1,132 @@
+import { themeGet } from "@styled-system/theme-get"
+import { Range as RCRange, RangeProps } from "rc-slider"
+import styled from "styled-components"
+
+const BORDER_RADIUS_BASE = 6
+export const RANGE_DOT_SIZE = 16
+
+// TODO: Add styles for vertical mode, tooltip
+export const Range: React.FC<RangeProps> = props => {
+  return <StyledRange {...props} />
+}
+
+const StyledRange = styled(RCRange)`
+  position: relative;
+  height: ${RANGE_DOT_SIZE}px;
+  width: 100%;
+  touch-action: none;
+
+  .rc-slider-rail {
+    width: 100%;
+    background-color: ${themeGet("colors.black30")};
+    border-radius: ${BORDER_RADIUS_BASE}px;
+  }
+
+  .rc-slider-track {
+    left: 0;
+    border-radius: ${BORDER_RADIUS_BASE}px;
+    background-color: ${themeGet("colors.blue100")};
+  }
+
+  .rc-slider-rail,
+  .rc-slider-track,
+  .rc-slider-step {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    height: 2px;
+  }
+
+  .rc-slider-handle {
+    position: absolute;
+    width: ${RANGE_DOT_SIZE}px;
+    height: ${RANGE_DOT_SIZE}px;
+    cursor: pointer;
+    cursor: -webkit-grab;
+    cursor: grab;
+    border-radius: 50%;
+    background-color: ${themeGet("colors.white100")};
+    touch-action: pan-x;
+    box-shadow: 0px 2px 12px rgba(0, 0, 0, 0.1);
+    border: solid 1px ${themeGet("colors.black10")};
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  .rc-slider-mark {
+    position: absolute;
+    top: 18px;
+    left: 0;
+    width: 100%;
+    font-size: 12px;
+  }
+
+  .rc-slider-mark-text {
+    position: absolute;
+    display: inline-block;
+    vertical-align: middle;
+    text-align: center;
+    cursor: pointer;
+    color: ${themeGet("colors.black60")};
+  }
+
+  .rc-slider-step {
+    position: absolute;
+    width: 100%;
+    height: 2px;
+    background: transparent;
+  }
+
+  .rc-slider-dot {
+    position: absolute;
+    top: 50%;
+    transform: translateY(-50%);
+    margin-left: -4px;
+    width: 8px;
+    height: 8px;
+    background-color: ${themeGet("colors.black30")};
+    cursor: pointer;
+    border-radius: 50%;
+    vertical-align: middle;
+  }
+
+  .rc-slider-dot-active {
+    background-color: ${themeGet("colors.blue100")};
+  }
+
+  .rc-slider-dot-reverse {
+    margin-right: -4px;
+  }
+
+  &.rc-slider-disabled {
+    .rc-slider-track,
+    .rc-slider-handle,
+    .rc-slider-dot {
+      background-color: ${themeGet("colors.black30")};
+    }
+
+    .rc-slider-handle,
+    .rc-slider-dot {
+      box-shadow: none;
+      cursor: not-allowed;
+    }
+
+    .rc-slider-mark-text,
+    .rc-slider-dot {
+      cursor: not-allowed !important;
+    }
+  }
+
+  &,
+  & * {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(
+      0,
+      0,
+      0,
+      0
+    ); //  remove tap highlight color for mobile safari
+  }
+`
