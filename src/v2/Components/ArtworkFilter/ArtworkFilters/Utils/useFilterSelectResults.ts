f1c75bc975e1672f49c299fda3f3da3859ec6ced
Dima Tretyak <itretyak@ya.ru>
2022-03-02 16:03:23 +0300

    chore: updates for useFilterSelectResults

--- src/v2/Components/ArtworkFilter/ArtworkFilters/Utils/useFilterSelectResults.ts
+++ src/v2/Components/ArtworkFilter/ArtworkFilters/Utils/useFilterSelectResults.ts
@@ -23,20 +23,17 @@ export const useFilterSelectResults = ({
 }: UseFilterSelectResultsProps) => {
   const { aggregations, setFilter } = useArtworkFilterContext()
   const selectedFilters = useCurrentlySelectedFilters()
-
   const filtersCount = useFilterLabelCountByKey(filtersCountKey)
   const labelWithCount = `${label}${filtersCount}`
+  const filtersByFaceName = selectedFilters[facetName] ?? []
 
   const items = aggregations
     ?.find(aggregation => aggregation.slice === slice)
     ?.counts.map(item => ({ label: item.name, ...item })) as FilterSelectItems
 
-  const selectedItems = (selectedFilters[facetName] ?? []).map(
-    selectedFacetName => {
-      const selectedItem = items.find(item => item.value === selectedFacetName)
-      return selectedItem
-    }
-  ) as FilterSelectItems
+  const selectedItems = filtersByFaceName.map(selectedFacetName => {
+    return items.find(item => item.value === selectedFacetName)
+  }) as FilterSelectItems
 
   const handleFilterSelectChange = (state: FilterSelectChangeState) => {
     const selectedNationalities = state.selectedItems.map(item => item.value)
