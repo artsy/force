c72fb1077157dd76159b7cf72ae09df4f1b033c5
Matt Jones <matt.jones@artsymail.com>
2022-03-04 16:41:45 -0500

    refactor: useFeatureFlag and useFeatureVariant

--- src/v2/System/__tests__/useFeatureFlag.jest.tsx
+++ src/v2/System/__tests__/useFeatureFlag.jest.tsx
@@ -4,9 +4,10 @@ import { useSystemContext } from "v2/System/useSystemContext"
 jest.mock("v2/System/useSystemContext")
 
 const mockUseSystemContext = useSystemContext as jest.Mock
+let mockFeatureFlags
 
-beforeEach(() => {
-  mockUseSystemContext.mockImplementation(() => ({
+beforeAll(() => {
+  mockFeatureFlags = {
     featureFlags: {
       "feature-a": {
         flagEnabled: true,
@@ -27,37 +28,25 @@ beforeEach(() => {
         },
       },
     },
-  }))
+  }
+
+  mockUseSystemContext.mockImplementation(() => mockFeatureFlags)
 })
 
 describe("useFeatureFlag", () => {
-  it("returns true when the feature flag is present", () => {
+  it("returns true when the feature is enabled", () => {
     const result = useFeatureFlag("feature-a")
     expect(result).toBe(true)
   })
 
-  it("returns false when the feature flag is not present", () => {
-    const result = useFeatureFlag("notAvailable")
+  it("returns false when the feature is is NOT enabled", () => {
+    const result = useFeatureFlag("feature-b")
     expect(result).toBe(false)
   })
 
-  it("returns null when we don't pass in an argument", () => {
-    // @ts-ignore
-    const result = useFeatureFlag()
-    expect(result).toBe(null)
-    expect(console.error).toHaveBeenLastCalledWith(
-      `[Force] Error: no argument passed into useFeatureFlag`
-    )
-  })
-
-  it("returns null when we don't have featureFlags", () => {
-    mockUseSystemContext.mockImplementation(() => ({}))
-
+  it("returns null when the feature isn't in featureFlags", () => {
     const result = useFeatureFlag("feature")
     expect(result).toBe(null)
-    expect(console.error).toHaveBeenLastCalledWith(
-      "[Force] Error: featureFlags is undefined in SystemContext"
-    )
   })
 })
 
@@ -67,25 +56,13 @@ describe("useFeatureVariant", () => {
     expect(variant!.enabled).toBe(true)
   })
 
-  it("return false when the variant is not enabled", () => {
+  it("return false when the variant is NOT enabled", () => {
     const variant = useFeatureVariant("feature-b")
     expect(variant!.enabled).toBe(false)
   })
 
-  it("returns false for the enabled property when the variant is not passed in as an argument", () => {
-    // @ts-ignore
-    const variant = useFeatureVariant()
-    expect(variant!.enabled).toBe(false)
-    expect(console.error).toHaveBeenCalledWith(
-      "[Force] Error: no argument passed into useFeatureVariant"
-    )
-  })
-
   it("returns false when the variant isn't present", () => {
     const variant = useFeatureVariant("feature-x")
-    expect(variant!.enabled).toBe(false)
-    expect(console.error).toHaveBeenLastCalledWith(
-      "[Force] Error: the variant can't be found on featureFlags"
-    )
+    expect(variant).toBe(null)
   })
 })
--- src/v2/System/useFeatureFlag.tsx
+++ src/v2/System/useFeatureFlag.tsx
@@ -8,48 +8,32 @@ interface FeatureFlagDetails {
   variant: Variant
 }
 
-export function useFeatureFlag(flagName: string): boolean | null {
+export function useFeatureFlag(featureName: string): boolean | null {
   const { featureFlags } = useSystemContext()
+  const flagEnabled = featureFlags?.[featureName]?.flagEnabled
 
-  if (featureFlags === undefined) {
-    console.error("[Force] Error: featureFlags is undefined in SystemContext")
+  if (flagEnabled === undefined) {
+    console.error(
+      "[Force] Error: cannot find flagName in featureFlags: ",
+      featureFlags
+    )
     return null
   }
 
-  if (!flagName) {
-    console.error("[Force] Error: no argument passed into useFeatureFlag")
-    return null
-  }
-
-  return (
-    Object.keys(featureFlags).indexOf(flagName) !== -1 &&
-    featureFlags[flagName].flagEnabled
-  )
+  return flagEnabled
 }
 
-export function useFeatureVariant(featureName: string): Variant {
+export function useFeatureVariant(featureName: string): Variant | null {
   const { featureFlags } = useSystemContext()
-  const errorObject = {
-    enabled: false,
-    name: "false",
-  }
+  const variant = featureFlags?.[featureName]?.variant
 
-  if (featureFlags === undefined) {
+  if (variant === undefined) {
     console.error(
-      "[Force] Error: featureFlags is undefined in SystemContext in UseFeatureVariant"
+      "[Force] Error: cannot find variant on featureFlags: ",
+      featureFlags
     )
-    return errorObject
+    return null
   }
 
-  if (!featureName) {
-    console.error("[Force] Error: no argument passed into useFeatureVariant")
-    return errorObject
-  }
-
-  if (!featureFlags[featureName]) {
-    console.error("[Force] Error: the variant can't be found on featureFlags")
-    return errorObject
-  }
-
-  return featureFlags[featureName].variant
+  return variant
 }
