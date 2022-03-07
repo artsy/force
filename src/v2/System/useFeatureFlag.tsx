b218b38c40a30272dffeb6cb333b8f9c0907f0f4
Matt Jones <matt.jones@artsymail.com>
2022-03-07 14:37:19 -0500

    feat: add tracking for Experiment Viewed

--- src/v2/System/useFeatureFlag.tsx
+++ src/v2/System/useFeatureFlag.tsx
@@ -1,5 +1,7 @@
+import * as Schema from "v2/System/Analytics"
 import { useSystemContext } from "v2/System"
 import { Variant } from "unleash-client"
+import { useTracking } from "react-tracking"
 
 export type FeatureFlags = Record<string, FeatureFlagDetails>
 
@@ -25,6 +27,7 @@ export function useFeatureFlag(featureName: string): boolean | null {
 
 export function useFeatureVariant(featureName: string): Variant | null {
   const { featureFlags } = useSystemContext()
+  const { trackEvent } = useTracking()
   const variant = featureFlags?.[featureName]?.variant
 
   if (!variant) {
@@ -35,5 +38,47 @@ export function useFeatureVariant(featureName: string): Variant | null {
     return null
   }
 
+  // detremine if we should track the experimentView
+  if (typeof window !== "undefined") {
+    let trackFeatureView = maybeTrackFeatureView(featureName, variant.name)
+
+    if (trackFeatureView) {
+      trackEvent({
+        action: Schema.ActionType.ExperimentViewed,
+        service: "unleash",
+        experiment_name: featureName,
+        variant_name: variant.name,
+        payload: variant.payload,
+      })
+    }
+  }
+
   return variant
+}
+
+function maybeTrackFeatureView(featureName, variantName) {
+  const experimentName = `${featureName}+${variantName}`
+  let viewedExperiments = getExperimentsViewed()
+
+  if (viewedExperiments.includes(experimentName)) {
+    return false
+  }
+
+  viewedExperiments.push(experimentName)
+  setExperimentsViewed(viewedExperiments)
+
+  return true
+}
+
+function getExperimentsViewed() {
+  let experimentsViewed = window.localStorage.getItem("experimentsViewed")
+
+  return experimentsViewed === null ? [] : JSON.parse(experimentsViewed)
+}
+
+function setExperimentsViewed(_viewedExperiments) {
+  window.localStorage.setItem(
+    "experimentsViewed",
+    JSON.stringify(_viewedExperiments)
+  )
 }
