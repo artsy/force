5132d32c57548d8fadd5b11e1ac03c700c1a0afc
dzucconi <mail@damonzucconi.com>
2022-03-08 15:20:38 -0500

    fix(article): cleans up bylines

--- src/v2/Apps/Article/Components/ArticleByline.tsx
+++ src/v2/Apps/Article/Components/ArticleByline.tsx
@@ -1,4 +1,12 @@
-import { EntityHeader, Join, Spacer, Text } from "@artsy/palette"
+import {
+  Box,
+  Column,
+  EntityHeader,
+  GridColumns,
+  Join,
+  Spacer,
+  Text,
+} from "@artsy/palette"
 import { FC } from "react"
 import { createFragmentContainer, graphql } from "react-relay"
 import { ArticleByline_article } from "v2/__generated__/ArticleByline_article.graphql"
@@ -11,27 +19,47 @@ const ArticleByline: FC<ArticleBylineProps> = ({ article }) => {
   if (article.authors.length === 0) {
     return (
       <Text variant="md" fontWeight="bold">
-        —{article.byline}
+        {article.byline}
       </Text>
     )
   }
 
   return (
-    <Join separator={<Spacer mt={2} />}>
-      {article.authors.map(author => {
-        const image = author.image?.cropped
+    <GridColumns>
+      <Column span={9}>
+        <Join separator={<Spacer mt={2} />}>
+          {article.authors.map(author => {
+            const image = author.image?.cropped
 
-        return (
-          <EntityHeader
-            key={author.internalID}
-            name={author.name || "Artsy Editorial"}
-            initials={author.initials || "A"}
-            meta={author.bio!}
-            image={image!}
-          />
-        )
-      })}
-    </Join>
+            if (image) {
+              return (
+                <EntityHeader
+                  key={author.internalID}
+                  name={author.name || "Artsy Editorial"}
+                  initials={author.initials || "A"}
+                  meta={author.bio!}
+                  image={image!}
+                />
+              )
+            }
+
+            return (
+              <Box>
+                <Text variant="md" fontWeight="bold">
+                  {author.name || "Artsy Editorial"}
+                </Text>
+
+                {author.bio && (
+                  <Text variant="xs" color="black60">
+                    {author.bio}
+                  </Text>
+                )}
+              </Box>
+            )
+          })}
+        </Join>
+      </Column>
+    </GridColumns>
   )
 }
 
