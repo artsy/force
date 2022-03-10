e0ea30ffe3f21d36db1a83bcf1022fd711046e86
Dima Tretyak <itretyak@ya.ru>
2022-03-10 19:35:20 +0300

    chore: use getInitialFilterState

--- src/v2/Apps/Artist/Routes/WorksForSale/Utils/getWorksForSaleRouteVariables.ts
+++ src/v2/Apps/Artist/Routes/WorksForSale/Utils/getWorksForSaleRouteVariables.ts
@@ -1,18 +1,17 @@
-import { paramsToCamelCase } from "v2/Components/ArtworkFilter/Utils/urlBuilder"
 import { initialArtworkFilterState } from "v2/Components/ArtworkFilter/ArtworkFilterContext"
-import { allowedFilters } from "v2/Components/ArtworkFilter/Utils/allowedFilters"
 import { getDefaultSortValueByVariant } from "v2/Utils/merchandisingTrial"
+import { getInitialFilterState } from "v2/Components/ArtworkFilter/Utils/getInitialFilterState"
 
 export function getWorksForSaleRouteVariables({ artistID }, props) {
   // FIXME: The initial render includes `location` in props, but subsequent
   // renders (such as tabbing back to this route in your browser) will not.
-  const filterStateFromUrl = props.location ? props.location.query : {}
+  const filterStateFromUrl = props.location?.query ?? {}
   const defaultSort = getDefaultSortValueByVariant(artistID)
 
   const filterParams = {
     ...initialArtworkFilterState,
     sort: defaultSort,
-    ...paramsToCamelCase(filterStateFromUrl),
+    ...getInitialFilterState(filterStateFromUrl),
   }
 
   const aggregations = [
@@ -25,7 +24,7 @@ export function getWorksForSaleRouteVariables({ artistID }, props) {
   ]
 
   return {
-    input: allowedFilters(filterParams),
+    input: filterParams,
     aggregations,
     artistID,
   }
