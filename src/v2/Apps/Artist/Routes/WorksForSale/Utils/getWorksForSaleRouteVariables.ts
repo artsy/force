94ad714f8e5d1e4226a7a831b5a8a3ca2066ddb1
Dima Tretyak <itretyak@ya.ru>
2022-03-11 12:56:27 +0300

    chore: remove initialArtworkFilterState

--- src/v2/Apps/Artist/Routes/WorksForSale/Utils/getWorksForSaleRouteVariables.ts
+++ src/v2/Apps/Artist/Routes/WorksForSale/Utils/getWorksForSaleRouteVariables.ts
@@ -1,17 +1,15 @@
-import { initialArtworkFilterState } from "v2/Components/ArtworkFilter/ArtworkFilterContext"
 import { getDefaultSortValueByVariant } from "v2/Utils/merchandisingTrial"
 import { getInitialFilterState } from "v2/Components/ArtworkFilter/Utils/getInitialFilterState"
 
 export function getWorksForSaleRouteVariables({ artistID }, props) {
   // FIXME: The initial render includes `location` in props, but subsequent
   // renders (such as tabbing back to this route in your browser) will not.
-  const filterStateFromUrl = props.location?.query ?? {}
+  const initialFilterState = getInitialFilterState(props.location?.query ?? {})
   const defaultSort = getDefaultSortValueByVariant(artistID)
 
   const filterParams = {
-    ...initialArtworkFilterState,
     sort: defaultSort,
-    ...getInitialFilterState(filterStateFromUrl),
+    ...initialFilterState,
   }
 
   const aggregations = [
