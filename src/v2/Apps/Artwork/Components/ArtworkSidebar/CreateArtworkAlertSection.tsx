c259237df92329abdd4a59023dd38148f4495b97
Dima Tretyak <itretyak@ya.ru>
2022-03-16 15:16:56 +0300

    refactor: reuse SavedSearchCreateAlertButton

--- src/v2/Apps/Artwork/Components/ArtworkSidebar/CreateArtworkAlertSection.tsx
+++ src/v2/Apps/Artwork/Components/ArtworkSidebar/CreateArtworkAlertSection.tsx
@@ -1,17 +1,5 @@
-import React, { useState } from "react"
-import {
-  Separator,
-  Spacer,
-  Flex,
-  Button,
-  BellIcon,
-  Text,
-  Box,
-} from "@artsy/palette"
-import {
-  SavedSearchAlertContextProvider,
-  useSavedSearchAlertContext,
-} from "v2/Components/SavedSearchAlert/SavedSearchAlertContext"
+import React from "react"
+import { Separator, Spacer, Flex, Text, Box } from "@artsy/palette"
 import { createFragmentContainer, graphql } from "react-relay"
 import { CreateArtworkAlertSection_artwork } from "v2/__generated__/CreateArtworkAlertSection_artwork.graphql"
 import {
@@ -21,52 +9,14 @@ import {
 import { compact } from "lodash"
 import { checkboxValues } from "v2/Components/ArtworkFilter/ArtworkFilters/AttributionClassFilter"
 import { getAllowedSearchCriteria } from "v2/Components/SavedSearchAlert/Utils/savedSearchCriteria"
-import { SavedSearchAlertModal } from "v2/Components/SavedSearchAlert/SavedSearchAlertModal"
 import { OwnerType } from "@artsy/cohesion"
+import { SavedSearchCreateAlertButton } from "v2/Components/SavedSearchAlert/Components/SavedSearchCreateAlertButton"
 
 interface CreateArtworkAlertSectionProps {
   artwork: CreateArtworkAlertSection_artwork
 }
 
-export const CreateArtworkAlertSection: React.FC = () => {
-  const [visible, setVisible] = useState(false)
-  const { entity } = useSavedSearchAlertContext()
-
-  const openModal = () => setVisible(true)
-  const closeModal = () => setVisible(false)
-
-  return (
-    <Box my={2}>
-      <Separator />
-      <Spacer m={2} />
-      <Flex
-        flexDirection="row"
-        py={1}
-        alignItems="center"
-        justifyContent="space-between"
-      >
-        <Text variant="xs" mr={2}>
-          Be notified when a similar piece is available
-        </Text>
-        <Button variant="secondaryOutline" size="small" onClick={openModal}>
-          <BellIcon mr={0.5} fill="currentColor" />
-          <Text variant="xs">Create Alert</Text>
-        </Button>
-      </Flex>
-
-      {visible ? (
-        <SavedSearchAlertModal
-          initialValues={{ name: "", email: true, push: false }}
-          entity={entity}
-          onClose={closeModal}
-          onComplete={closeModal}
-        />
-      ) : null}
-    </Box>
-  )
-}
-
-export const CreateArtworkAlertSectionContainer: React.FC<CreateArtworkAlertSectionProps> = ({
+export const CreateArtworkAlertSection: React.FC<CreateArtworkAlertSectionProps> = ({
   artwork,
 }) => {
   const artists = compact(artwork.artists)
@@ -94,14 +44,29 @@ export const CreateArtworkAlertSectionContainer: React.FC<CreateArtworkAlertSect
   const allowedCriteria = getAllowedSearchCriteria(criteria)
 
   return (
-    <SavedSearchAlertContextProvider criteria={allowedCriteria} entity={entity}>
-      <CreateArtworkAlertSection />
-    </SavedSearchAlertContextProvider>
+    <Box my={2}>
+      <Separator />
+      <Spacer m={2} />
+      <Flex
+        flexDirection="row"
+        py={1}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Text variant="xs" mr={2}>
+          Be notified when a similar piece is available
+        </Text>
+        <SavedSearchCreateAlertButton
+          entity={entity}
+          criteria={allowedCriteria}
+        />
+      </Flex>
+    </Box>
   )
 }
 
 export const CreateArtworkAlertSectionFragmentContainer = createFragmentContainer(
-  CreateArtworkAlertSectionContainer,
+  CreateArtworkAlertSection,
   {
     artwork: graphql`
       fragment CreateArtworkAlertSection_artwork on Artwork {
