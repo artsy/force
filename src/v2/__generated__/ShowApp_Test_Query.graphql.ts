d20a92416bc073b0411e6127fe99cfb9fe71a25f
artsyit <it@artsymail.com>
2022-03-16 17:43:47 +0000

    Update metaphysics schema

--- src/v2/__generated__/ShowApp_Test_Query.graphql.ts
+++ src/v2/__generated__/ShowApp_Test_Query.graphql.ts
@@ -1989,6 +1989,7 @@ return {
             "PARTNER_CITY",
             "PERIOD",
             "PRICE_RANGE",
+            "SIMPLE_PRICE_HISTOGRAM",
             "TOTAL"
           ],
           "nullable": true,
