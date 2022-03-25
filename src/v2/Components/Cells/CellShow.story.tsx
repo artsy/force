ca870adb8a51990d5d5a2a3db388b0fed752db89
dzucconi <mail@damonzucconi.com>
2022-03-25 17:09:46 -0400

    feat(cells): adds cell for shows

---
+++ src/v2/Components/Cells/CellShow.story.tsx
@@ -0,0 +1,75 @@
+import { graphql } from "relay-runtime"
+import { States } from "storybook-states"
+import { SystemQueryRenderer } from "v2/System/Relay/SystemQueryRenderer"
+import {
+  CellShowFragmentContainer,
+  CellShowProps,
+  CellShowPlaceholder,
+} from "./CellShow"
+import { CellShowStoryQuery } from "v2/__generated__/CellShowStoryQuery.graphql"
+
+export default {
+  title: "Components/Cell",
+}
+
+export const CellShow = () => {
+  return (
+    <States<{ id: string } & Partial<Omit<CellShowProps, "show">>>
+      states={[
+        { id: "whitney-museum-of-american-art-1-whitney-biennial-2019-1" },
+        {
+          id: "whitney-museum-of-american-art-1-whitney-biennial-2019-1",
+          displayStatus: true,
+        },
+        {
+          id: "whitney-museum-of-american-art-1-whitney-biennial-2019-1",
+          displayKind: true,
+        },
+        {
+          id: "whitney-museum-of-american-art-1-whitney-biennial-2019-1",
+          displayPartner: false,
+        },
+        {
+          id: "whitney-museum-of-american-art-1-whitney-biennial-2019-1",
+          mode: "GRID",
+        },
+        {
+          id: "whitney-museum-of-american-art-1-laura-owens",
+        },
+        { id: "whitney-museum-of-american-art-1-laura-owens", mode: "GRID" },
+      ]}
+    >
+      {({ id, ...rest }) => {
+        return (
+          <SystemQueryRenderer<CellShowStoryQuery>
+            variables={{ id }}
+            placeholder={<CellShowPlaceholder {...rest} />}
+            query={graphql`
+              query CellShowStoryQuery($id: String!) {
+                show(id: $id) {
+                  ...CellShow_show
+                }
+              }
+            `}
+            render={({ error, props }) => {
+              if (error) {
+                console.error(error)
+                return null
+              }
+
+              if (!props?.show) {
+                return <CellShowPlaceholder {...rest} />
+              }
+
+              return <CellShowFragmentContainer show={props.show} {...rest} />
+            }}
+          />
+        )
+      }}
+    </States>
+  )
+}
+
+CellShow.story = {
+  name: "CellShow",
+}
