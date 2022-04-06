c66ebf5f45fc13856026d1b846591da855b0615d
dzucconi <mail@damonzucconi.com>
2022-04-06 17:53:26 -0400

    refactor(buyer-guarantee): cleans up buyer-guarantee page

---
+++ src/v2/Apps/BuyerGuarantee/Components/BuyerGuaranteeTables.tsx
@@ -0,0 +1,216 @@
+import { GridColumns, Column, Text, CheckIcon } from "@artsy/palette"
+import { themeGet } from "@styled-system/theme-get"
+import { FC } from "react"
+import styled from "styled-components"
+
+export const BuyerGuaranteeTableDesktop: FC = () => {
+  return (
+    <>
+      <Row>
+        <Td span={2}></Td>
+
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Vetted Sellers
+          </Text>
+        </Td>
+
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Dedicated Support
+          </Text>
+        </Td>
+
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Authenticity Guarantee
+          </Text>
+        </Td>
+
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Money-Back Guarantee
+          </Text>
+        </Td>
+
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Secure Payment
+          </Text>
+        </Td>
+      </Row>
+
+      <Row>
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Making an Inquiry
+          </Text>
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={2}></Td>
+
+        <Td span={2}></Td>
+
+        <Td span={2}></Td>
+      </Row>
+
+      <Row bg="white100">
+        <Td span={2}>
+          <Text variant="md" fontWeight="bold">
+            Purchasing with Artsy’s Secure Checkout
+          </Text>
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={2}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+      </Row>
+    </>
+  )
+}
+
+export const BuyerGuaranteeTableMobile: FC = () => {
+  return (
+    <>
+      <Row>
+        <Td span={[4]}></Td>
+
+        <Td span={[4]}>
+          <Text variant="md" fontWeight="bold">
+            Making an Inquiry
+          </Text>
+        </Td>
+
+        <Td span={[4]} bg="white100">
+          <Text variant="md" fontWeight="bold">
+            Purchasing with Artsy’s Secure Checkout
+          </Text>
+        </Td>
+      </Row>
+
+      <Row>
+        <Td span={[4]}>
+          <Text variant="md" fontWeight="bold">
+            Vetted Sellers
+          </Text>
+        </Td>
+
+        <Td span={[4]}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={[4]} bg="white100">
+          <CheckIcon width={40} height={40} />
+        </Td>
+      </Row>
+
+      <Row>
+        <Td span={[4]}>
+          <Text variant="md" fontWeight="bold">
+            Dedicated Support
+          </Text>
+        </Td>
+
+        <Td span={[4]}>
+          <CheckIcon width={40} height={40} />
+        </Td>
+
+        <Td span={[4]} bg="white100">
+          <CheckIcon width={40} height={40} />
+        </Td>
+      </Row>
+
+      <Row>
+        <Td span={[4]}>
+          <Text variant="md" fontWeight="bold">
+            Authenticity Guarantee
+          </Text>
+        </Td>
+
+        <Td span={[4]}></Td>
+
+        <Td span={[4]} bg="white100">
+          <CheckIcon width={40} height={40} />
+        </Td>
+      </Row>
+
+      <Row>
+        <Td span={[4]}>
+          <Text variant="md" fontWeight="bold">
+            Money-Back Guarantee
+          </Text>
+        </Td>
+
+        <Td span={[4]}></Td>
+
+        <Td span={[4]} bg="white100">
+          <CheckIcon width={40} height={40} />
+        </Td>
+      </Row>
+
+      <Row>
+        <Td span={[4]}>
+          <Text variant="md" fontWeight="bold">
+            Secure Payment
+          </Text>
+        </Td>
+
+        <Td span={[4]}></Td>
+
+        <Td span={[4]} bg="white100">
+          <CheckIcon width={40} height={40} />
+        </Td>
+      </Row>
+    </>
+  )
+}
+
+const Td = styled(Column)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  padding: ${themeGet("space.2")} ${themeGet("space.1")};
+  border-right: 1px solid ${themeGet("colors.black100")};
+
+  &:last-of-type {
+    border-right: 0;
+  }
+`
+
+const Row = styled(GridColumns)`
+  border-bottom: 1px solid ${themeGet("colors.black100")};
+
+  &:last-of-type {
+    border-bottom: 0;
+  }
+`
+
+Row.defaultProps = {
+  gridRowGap: 0,
+  gridColumnGap: 0,
+}
