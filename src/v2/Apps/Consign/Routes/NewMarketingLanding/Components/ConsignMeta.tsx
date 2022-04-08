3cc33033ae7cb7414d8069602476b3f6affab16a
kiryl-zubarau <79979820+kiryl-zubarau@users.noreply.github.com>
2022-04-08 15:46:52 +0300

    Add sell2 route. Update PromoSpace component. (#9847)
    Co-authored-by: Kirill Zubarau <kiry.zubarau@artsymail.com>

---
+++ src/v2/Apps/Consign/Routes/NewMarketingLanding/Components/ConsignMeta.tsx
@@ -0,0 +1,11 @@
+import { MetaTags } from "v2/Components/MetaTags"
+
+export const ConsignMeta: React.FC = () => {
+  return (
+    <MetaTags
+      pathname="/sell"
+      title="Sell Artwork with Artsy | Art Consignment | Artsy"
+      description="Sell and consign your art with Artsy. Get competitive offers from the world’s top auction houses and galleries to sell art from your collection. Submit today at no cost."
+    />
+  )
+}
