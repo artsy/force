c5d155f7785c3dbbed659b83758f479ba63edc2e
Alican Akyuz <alicakyz@gmail.com>
2022-04-28 14:06:18 +0200

    feat: prevent call before mounting

--- src/v2/Apps/Consign/Hooks/useConnectUserToSubmission.ts
+++ src/v2/Apps/Consign/Hooks/useConnectUserToSubmission.ts
@@ -4,18 +4,18 @@ import { useSystemContext } from "v2/System"
 import { useAddUserToSubmission } from "v2/Apps/Consign/Routes/SubmissionFlow/Mutations"
 import createLogger from "v2/Utils/logger"
 import { MutationParameters } from "relay-runtime"
+import { useDidMount } from "v2/Utils/Hooks/useDidMount"
 
 const logger = createLogger("Utils/Hooks/useConnectUserToSubmission.tsx")
 
 export const connectUserToSubmission = async (
-  user: User,
   addUser: (
     variables: MutationParameters["variables"]
   ) => Promise<MutationParameters["response"]>
 ) => {
   const submissionId = await Cookies.get("submissionId")
 
-  if (!submissionId || !user) return
+  if (!submissionId) return
 
   try {
     await addUser({
@@ -33,10 +33,13 @@ export const connectUserToSubmission = async (
 }
 
 export const useConnectUserToSubmission = () => {
+  const isMounted = useDidMount()
   const { user } = useSystemContext()
   const { submitMutation: addUser } = useAddUserToSubmission()
 
   useEffect(() => {
-    connectUserToSubmission(user, addUser)
-  }, [user, addUser])
+    if (!user || !isMounted) return
+
+    connectUserToSubmission(addUser)
+  }, [user, isMounted, addUser])
 }
