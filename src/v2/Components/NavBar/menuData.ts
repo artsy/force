127a1e62341a158e41f75d0a56d08f136a9ceeef
George Kartalis <g.kartalis7@gmail.com>
2022-05-04 11:35:07 +0200

    chore: update navigation links for artist tab

--- src/v2/Components/NavBar/menuData.ts
+++ src/v2/Components/NavBar/menuData.ts
@@ -32,34 +32,34 @@ export const ARTISTS_SUBMENU_DATA: MenuLinkData = {
     title: "Artists",
     links: [
       {
-        text: "Trending on Artsy",
-        href: "/collection/highlights-this-month",
+        text: "Trending This Week",
+        href: "/gene/trending-this-week",
       },
       {
-        text: "The Artsy Vanguard Artists",
-        href: "/collection/artsy-vanguard-artists",
+        text: "Black Painters On Our Radar",
+        href: "/gene/black-painters-on-our-radar",
       },
       {
-        text: "Limited-Edition Prints by Leading Artists",
-        href: "/collection/limited-edition-prints-trending-artists",
+        text: "Our Top Auction Lots",
+        href: "/gene/our-top-auction-lots",
         dividerBelow: true,
       },
       {
-        text: "New From",
+        text: "Curator's Picks",
         menu: {
-          title: "New From",
+          title: "Curator's Picks",
           links: [
             {
-              text: "Emerging Artists",
-              href: "/collection/new-from-emerging-artists",
+              text: "Artists On The Rise",
+              href: "/gene/artists-on-the-rise",
             },
             {
-              text: "Established Artists",
-              href: "/collection/new-from-established-artists",
+              text: "Emerging Photographers to Watch",
+              href: "/gene/emerging-photographers-to-watch",
             },
             {
-              text: "Street Artists",
-              href: "/collection/street-art-highlights",
+              text: "Street Art Now",
+              href: "/gene/street-art-now-1",
             },
           ],
         },
