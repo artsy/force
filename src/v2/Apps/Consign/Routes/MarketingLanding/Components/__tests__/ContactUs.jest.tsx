c30f14d0747d16231478a30e128a9031edba437a
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2022-05-04 11:31:29 -0700

    Test converted to react-library tests, IP

--- src/v2/Apps/Consign/Routes/MarketingLanding/Components/__tests__/ContactUs.jest.tsx
+++ src/v2/Apps/Consign/Routes/MarketingLanding/Components/__tests__/ContactUs.jest.tsx
@@ -1,32 +1,27 @@
-import { mount } from "enzyme"
 import { ContactUs } from "../ContactUs"
 import { MockBoot } from "v2/DevTools"
 import { Breakpoint } from "@artsy/palette"
+import { render, screen, within } from "@testing-library/react"
+import { debug } from "console"
 
 jest.mock("react-tracking")
 
 describe("ContactUs", () => {
   const getWrapper = (breakpoint = "md") => {
-    return mount(
+    return render(
       <MockBoot breakpoint={breakpoint as Breakpoint}>
         <ContactUs />
       </MockBoot>
     )
   }
 
-  it("contains correct email in body", () => {
-    const wrapper = getWrapper()
-    expect(wrapper.text()).toContain(
-      "Email us at sell@artsy.net or call +1-646-797-3423 for more information on how Artsy can sell your artwork."
-    ) // pull text minus divs
-  })
-
-  it("has correct email link", () => {
-    const wrapper = getWrapper()
-    expect(wrapper.find("a").first().props().href).toBe("mailto:sell@artsy.net")
-    const wrapperMobile = getWrapper("xs")
-    expect(wrapperMobile.find("a").first().props().href).toBe(
-      "mailto:sell@artsy.net"
-    )
+  it("contains correct email in body with, correct email link and phone number", () => {
+    getWrapper()
+    // console.log("*****************************")
+    // const text = screen.getByTestId("contactUsBody")
+    // debug(text)
+    // expect(within(text).getByText("Email us at")).toBeInTheDocument()
+    expect(screen.getByText("sell@artsy.net")).toBeInTheDocument()
+    expect(screen.getByText("+1-646-797-3423")).toBeInTheDocument()
   })
 })
