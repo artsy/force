e22dc1c57723fdb67a357a8f4266d96994739e8f
Ana Lisa Sutherland <sutherlandanalisa@gmail.com>
2022-05-05 13:29:57 -0700

    Added New for You page title and header copy

--- src/v2/Apps/NewForYou/NewForYouApp.tsx
+++ src/v2/Apps/NewForYou/NewForYouApp.tsx
@@ -1,6 +1,8 @@
 import React, { FC, useEffect } from "react"
+import { MetaTags } from "v2/Components/MetaTags"
 import { useRouter } from "v2/System/Router/useRouter"
 import { useFeatureFlag } from "v2/System/useFeatureFlag"
+import { Column, GridColumns, Spacer, Text } from "@artsy/palette"
 
 export const NewForYouApp: FC = () => {
   const featureFlagEnabled = useFeatureFlag("new_for_you")
@@ -12,5 +14,23 @@ export const NewForYouApp: FC = () => {
     }
   })
 
-  return <>NewForYouApp</>
+  return (
+    <>
+      <Spacer mt={2} />
+      <MetaTags title="New For You" />
+      <GridColumns>
+        <Column span={6}>
+          <Text variant="xl" mt={4}>
+            New Works For You
+          </Text>
+        </Column>
+        <Column span={6}>
+          <Text variant={"md"} mt={6}>
+            Description Here Explaining Stuff
+          </Text>
+        </Column>
+      </GridColumns>
+      <Spacer mt={4} />
+    </>
+  )
 }
