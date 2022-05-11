ad472eb4d284417ecc659c991a1001d56d4e9775
George Kartalis <g.kartalis7@gmail.com>
2022-05-11 11:17:19 +0200

    chore: updated copy on artists with no works

--- src/v2/Apps/Artist/Routes/WorksForSale/Components/ZeroState.tsx
+++ src/v2/Apps/Artist/Routes/WorksForSale/Components/ZeroState.tsx
@@ -1,31 +1,12 @@
-import { ContextModule } from "@artsy/cohesion"
-import { Clickable, Flex, Message, Text } from "@artsy/palette"
-import { FollowArtistButtonFragmentContainer as FollowArtistButton } from "v2/Components/FollowButton/FollowArtistButton"
+import { Message, Text } from "@artsy/palette"
 
 export const ZeroState = props => {
-  const { isFollowed, artist } = props
-
   return (
     <Message>
-      There aren’t any works available by the artist at this time.{" "}
-      {!isFollowed && (
-        <Flex>
-          <FollowArtistButton
-            artist={artist}
-            contextModule={ContextModule.worksForSaleRail}
-            render={({ name }) => {
-              return (
-                <Clickable cursor="pointer" textDecoration="underline">
-                  <Text>Follow {name}</Text>
-                </Clickable>
-              )
-            }}
-          />{" "}
-          <Text pl={0.5}>
-            to receive notifications when new works are added.
-          </Text>
-        </Flex>
-      )}
+      <Text>No works available by the artist at this time</Text>
+      <Text textColor="black60">
+        Create an Alert to receive notifications when new works are added
+      </Text>
     </Message>
   )
 }
