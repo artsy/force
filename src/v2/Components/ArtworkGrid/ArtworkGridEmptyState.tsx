8e1d06654a9c7b98c6f69008768bea4ce8c14a60
Christopher Pappas <damassi.pappas@gmail.com>
2022-05-12 10:37:00 -0700

    feat(artwork-filter): Making loading/zero states sticky

--- src/v2/Components/ArtworkGrid/ArtworkGridEmptyState.tsx
+++ src/v2/Components/ArtworkGrid/ArtworkGridEmptyState.tsx
@@ -1,5 +1,6 @@
-import { Clickable, Message } from "@artsy/palette"
-import * as React from "react";
+import { Sticky } from "../Sticky"
+import { Box, Clickable, Message } from "@artsy/palette"
+import * as React from "react"
 import styled from "styled-components"
 
 interface ArtworkGridEmptyStateProps {
@@ -7,25 +8,34 @@ interface ArtworkGridEmptyStateProps {
 }
 
 export const ArtworkGridEmptyState: React.FC<ArtworkGridEmptyStateProps> = props => (
-  <Message width="100%">
-    <>
-      There aren't any works available that meet the following criteria at this
-      time.
-    </>
-    {props.onClearFilters && (
-      <>
-        {" "}
-        Change your filter criteria to view more works.{" "}
-        <ResetFilterLink
-          textDecoration="underline"
-          onClick={props.onClearFilters}
-        >
-          Clear all filters
-        </ResetFilterLink>
-        .
-      </>
-    )}
-  </Message>
+  <Box width="100%" my={1}>
+    <Sticky>
+      {({ stuck }) => {
+        return (
+          <Box pt={stuck ? 1 : 0}>
+            <Message width="100%">
+              <>
+                There aren't any works available that meet the following
+                criteria at this time.
+              </>
+              {props.onClearFilters && (
+                <>
+                  Change your filter criteria to view more works.{" "}
+                  <ResetFilterLink
+                    textDecoration="underline"
+                    onClick={props.onClearFilters}
+                  >
+                    Clear all filters
+                  </ResetFilterLink>
+                  .
+                </>
+              )}
+            </Message>
+          </Box>
+        )
+      }}
+    </Sticky>
+  </Box>
 )
 
 export const ResetFilterLink = styled(Clickable)``
