d6cef500cf125273ac418c4d00d658001b0a09f5
George Kartalis <g.kartalis7@gmail.com>
2022-05-18 12:03:09 +0200

    feat: make ZeroState responsive

--- src/v2/Apps/Artist/Routes/WorksForSale/Components/ZeroState.tsx
+++ src/v2/Apps/Artist/Routes/WorksForSale/Components/ZeroState.tsx
@@ -1,12 +1,24 @@
-import { Message, Text } from "@artsy/palette"
+import { Box, Message, Text } from "@artsy/palette"
+import { Sticky } from "v2/Components/Sticky"
 
 export const ZeroState: React.FC = () => {
   return (
-    <Message>
-      <Text>No works available by the artist at this time</Text>
-      <Text textColor="black60">
-        Create an Alert to receive notifications when new works are added
-      </Text>
-    </Message>
+    <Box width="100%" my={1}>
+      <Sticky>
+        {({ stuck }) => {
+          return (
+            <Box pt={stuck ? 1 : 0}>
+              <Message>
+                <Text>No works available by the artist at this time</Text>
+                <Text textColor="black60">
+                  Create an Alert to receive notifications when new works are
+                  added
+                </Text>
+              </Message>
+            </Box>
+          )
+        }}
+      </Sticky>
+    </Box>
   )
 }
