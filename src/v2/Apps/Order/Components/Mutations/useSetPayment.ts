b5cdec00ffd658f9dd74cb03f0d1e13412b6bae1
rquartararo <rquartararo@gmail.com>
2022-05-19 13:59:37 +0200

    fix: fix my fix

--- src/v2/Apps/Order/Components/Mutations/useSetPayment.ts
+++ src/v2/Apps/Order/Components/Mutations/useSetPayment.ts
@@ -1,6 +1,6 @@
 import { useMutation } from "v2/Utils/Hooks/useMutation"
 import { graphql } from "react-relay"
-import { useSetPaymentMutation } from "/Users/rachel/Artsy/force/src/v2/__generated__/useSetPaymentMutation.graphql"
+import { useSetPaymentMutation } from "v2/__generated__/useSetPaymentMutation.graphql"
 
 export const useSetPayment = () => {
   return useMutation<useSetPaymentMutation>({
