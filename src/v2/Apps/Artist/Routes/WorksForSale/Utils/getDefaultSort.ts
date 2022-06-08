592e0c42a94a8933c628ac383dabfe04ef3f5784
Dima Tretyak <itretyak@ya.ru>
2022-06-08 12:59:22 +0300

    chore: use control and experiment variants

--- src/v2/Apps/Artist/Routes/WorksForSale/Utils/getDefaultSort.ts
+++ src/v2/Apps/Artist/Routes/WorksForSale/Utils/getDefaultSort.ts
@@ -1,9 +1,13 @@
 import { Variant } from "unleash-client"
 
-export const getDefaultSort = (sort: string, variant: Variant | null, defaultSort = '-decayed_merch') => {
-  if (variant?.name === 'trending-sort' && sort === defaultSort) {
-    return '-default_trending_score'
+export const getDefaultSort = (
+  sort: string,
+  variant: Variant | null,
+  defaultSort = "-decayed_merch"
+) => {
+  if (variant?.name === "experiment" && sort === defaultSort) {
+    return "-default_trending_score"
   }
 
   return sort
-};
+}
