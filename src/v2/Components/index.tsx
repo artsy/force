1f76517d76e639a616285c8122887b887cf9e83f
dzucconi <mail@damonzucconi.com>
2022-06-10 11:39:09 -0400

    refactor(v2): begins removing v2 from app (2)

--- src/v2/Components/index.tsx
+++ src/v2/Components/index.tsx
@@ -1,33 +1,7 @@
-import Artwork from "./Artwork"
 import ArtworkGrid from "./ArtworkGrid/ArtworkGrid"
-import Buttons from "./Buttons"
-import Checkbox from "./Checkbox"
 import Modal from "./Modal/Modal"
-import Text from "./Text"
-
-export interface InitOptions {
-  user: User
-  component: any
-  domID: string
-  queryConfig: any
-}
-
-// TODO: Fix Force integration post Relay Modern migration
-export function init(options: InitOptions) {
-  // Relay.injectNetworkLayer(artsyNetworkLayer(options.user))
-  // const rootRoute = (
-  //   <Artsy.SystemContextProvider user={options.user}>
-  //     <Relay.RootContainer Component={options.component} route={options.queryConfig} />
-  //   </Artsy.SystemContextProvider>
-  // )
-  // ReactDOM.render(rootRoute, document.getElementById(options.domID))
-}
 
 export default {
-  Artwork,
-  Buttons,
   Modal,
   ArtworkGrid,
-  Checkbox,
-  Text,
 }
