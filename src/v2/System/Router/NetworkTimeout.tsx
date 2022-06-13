31b355e0452ac173b39083aa68ded310bec42980
dzucconi <mail@damonzucconi.com>
2022-06-13 10:53:23 -0400

    refactor(v2): refactors network timeout to use toasts

--- src/v2/System/Router/NetworkTimeout.tsx
+++ src/v2/System/Router/NetworkTimeout.tsx
@@ -1,45 +1,40 @@
-import { ErrorModal } from "v2/Components/Modal/ErrorModal"
-import { useEffect, useState } from "react";
-import * as React from "react";
+import { useEffect } from "react"
+import * as React from "react"
 import { ErrorWithMetadata } from "v2/Utils/errors"
 import createLogger from "v2/Utils/logger"
+import { useToasts } from "@artsy/palette"
 
 const logger = createLogger("Artsy/Router/NetworkTimeout")
 
 const NETWORK_TIMEOUT_MS = 15000
 
 export const NetworkTimeout: React.FC = () => {
-  const [showErrorModal, toggleErrorModal] = useState(false)
+  const { sendToast } = useToasts()
 
   useEffect(() => {
     const timeout = setTimeout(() => {
-      if (!showErrorModal) {
-        logger.error(
-          new ErrorWithMetadata(
-            `[Router/NetworkTimeout] Error: Network request timed out after ${NETWORK_TIMEOUT_MS}ms`
-          )
+      logger.error(
+        new ErrorWithMetadata(
+          `[Router/NetworkTimeout] Error: Network request timed out after ${NETWORK_TIMEOUT_MS}ms`
         )
-        toggleErrorModal(true)
-      }
+      )
+
+      sendToast({
+        variant: "error",
+        message: "Network request timed out",
+        action: {
+          label: "Retry",
+          onClick: () => {
+            window.location.reload()
+          },
+        },
+      })
     }, NETWORK_TIMEOUT_MS)
 
     return () => {
       clearTimeout(timeout)
     }
-  }, [showErrorModal])
+  }, [sendToast])
 
-  if (!showErrorModal) {
-    return null
-  }
-
-  return (
-    <ErrorModal
-      headerText="Network timeout"
-      show={showErrorModal}
-      closeText="Retry"
-      ctaAction={() => {
-        location.reload()
-      }}
-    />
-  )
+  return null
 }
