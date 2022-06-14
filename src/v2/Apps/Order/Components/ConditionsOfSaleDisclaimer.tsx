7a387c8906fe8c019f6b6d8a2c85c4daa878006c
dzucconi <mail@damonzucconi.com>
2022-06-14 14:22:27 -0400

    refactor(v2): removes sans

--- src/v2/Apps/Order/Components/ConditionsOfSaleDisclaimer.tsx
+++ src/v2/Apps/Order/Components/ConditionsOfSaleDisclaimer.tsx
@@ -1,13 +1,13 @@
-import { Link, Text, SansProps } from "@artsy/palette"
-import * as React from "react";
+import { Text, TextProps } from "@artsy/palette"
+import * as React from "react"
 
-export const ConditionsOfSaleDisclaimer: React.FC<Partial<SansProps>> = (
+export const ConditionsOfSaleDisclaimer: React.FC<Partial<TextProps>> = (
   props: any
 ) => (
   <Text variant="xs" color="black60" {...props}>
     By clicking Submit, I agree to Artsy’s{" "}
-    <Link href="/conditions-of-sale" target="_blank">
+    <a href="/conditions-of-sale" target="_blank">
       Conditions of Sale.
-    </Link>
+    </a>
   </Text>
 )
