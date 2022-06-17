acf70d928caf392ac0d5236da11bfd6b9a77a76e
araujobarret <araujobarret@gmail.com>
2022-06-17 17:39:41 +0200

    test: remove createTestEnv from Offer2

--- src/v2/Apps/Order/Routes/__tests__/Offer2.jest.tsx
+++ src/v2/Apps/Order/Routes/__tests__/Offer2.jest.tsx
@@ -1,13 +1,31 @@
 import { OfferTestQueryRawResponse } from "v2/__generated__/OfferTestQuery.graphql"
-import { createTestEnv } from "v2/DevTools/createTestEnv"
 import { graphql } from "react-relay"
 import { UntouchedOfferOrderPriceHidden } from "../../../__tests__/Fixtures/Order"
-import { initialOfferSuccess } from "../__fixtures__/MutationResults"
 import { OfferFragmentContainer } from "../Offer"
 import { OrderAppTestPage } from "./Utils/OrderAppTestPage"
+import { MockBoot } from "v2/DevTools"
+import { setupTestWrapper } from "v2/DevTools/setupTestWrapper"
+import { initialOfferSuccess } from "../__fixtures__/MutationResults"
 
 jest.unmock("react-tracking")
 jest.unmock("react-relay")
+
+const mockCommitMutation = jest.fn()
+jest.mock("v2/Apps/Order/Utils/commitMutation", () => ({
+  ...jest.requireActual("../../Utils/commitMutation"),
+  injectCommitMutation: Component => props => (
+    <Component {...props} commitMutation={mockCommitMutation} />
+  ),
+}))
+
+const mockShowErrorDialog = jest.fn()
+jest.mock("v2/Apps/Order/Dialogs", () => ({
+  ...jest.requireActual("../../Dialogs"),
+  injectDialog: Component => props => (
+    <Component {...props} dialog={{ showErrorDialog: mockShowErrorDialog }} />
+  ),
+}))
+
 const testOfferHiddenPrice: OfferTestQueryRawResponse["order"] = {
   ...UntouchedOfferOrderPriceHidden,
   internalID: "1234",
@@ -15,15 +33,14 @@ const testOfferHiddenPrice: OfferTestQueryRawResponse["order"] = {
 }
 
 describe("an offer on the work with price hidden", () => {
-  const { buildPage, mutations } = createTestEnv({
-    Component: OfferFragmentContainer,
-    TestPage: OrderAppTestPage,
-    defaultData: {
-      order: testOfferHiddenPrice,
-    },
-    defaultMutationResults: {
-      ...initialOfferSuccess,
-    },
+  let isCommittingMutation
+
+  const { getWrapper } = setupTestWrapper({
+    Component: (props: any) => (
+      <MockBoot>
+        <OfferFragmentContainer {...props} router={{ push: jest.fn() }} />
+      </MockBoot>
+    ),
     query: graphql`
       query Offer2TestQuery @raw_response_type @relay_test_operation {
         order: commerceOrder(id: "1234567") {
@@ -33,23 +50,24 @@ describe("an offer on the work with price hidden", () => {
     `,
   })
 
-  let page: OrderAppTestPage
   beforeEach(async () => {
-    page = await buildPage({
-      mockData: {
-        order: {
-          ...testOfferHiddenPrice,
-          price: "",
-        },
-      },
-    })
+    jest.clearAllMocks()
+    isCommittingMutation = false
   })
 
   it("shows no modal warning when an offer made on work with hidden price", async () => {
+    mockCommitMutation.mockResolvedValue(initialOfferSuccess)
+    let wrapper = getWrapper({
+      CommerceOrder: () => ({
+        ...testOfferHiddenPrice,
+        price: "",
+      }),
+    })
+    let page = new OrderAppTestPage(wrapper)
+
     await page.setOfferAmount(2)
     await page.clickSubmit()
-    await page.expectButtonSpinnerWhenSubmitting()
-    await page.expectNoModal()
-    expect(mutations.mockFetch).toHaveBeenCalled()
+    expect(mockCommitMutation).toHaveBeenCalledTimes(1)
+    expect(mockShowErrorDialog).not.toHaveBeenCalled()
   })
 })
