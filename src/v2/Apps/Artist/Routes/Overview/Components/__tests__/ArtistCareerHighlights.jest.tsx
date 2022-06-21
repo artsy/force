ca38a9de4efba8d56b98c8f299c2796f9617617f
tmcmeans <tanjmcmeans01@gmail.com>
2022-06-21 16:40:17 -0500

    add second test to artistcareerhighlights

--- src/v2/Apps/Artist/Routes/Overview/Components/__tests__/ArtistCareerHighlights.jest.tsx
+++ src/v2/Apps/Artist/Routes/Overview/Components/__tests__/ArtistCareerHighlights.jest.tsx
@@ -17,19 +17,44 @@ const { renderWithRelay } = setupTestWrapperTL({
 })
 
 describe("ArtistCareerHighlights", () => {
-  it("renders correctly", () => {
+  it("renders Career Highlights correctly", () => {
     renderWithRelay({
       Artist: () => ({
-        insights: [
+        insightsList: [
           {
-            type: "ACTIVE_SECONDARY_MARKET",
+            label: "Solo show at a major institution",
+            entities: ["Foo Museum"],
+            kind: "SOLO_SHOW",
           },
         ],
       }),
     })
 
     expect(screen.getByText("Career Highlights")).toBeInTheDocument()
+    expect(
+      screen.getByText("Solo show at a major institution")
+    ).toBeInTheDocument()
+    expect(screen.getByText("Foo Museum")).toBeInTheDocument()
+  })
+
+  it("renders Artist Badges correctly", () => {
+    renderWithRelay({
+      Artist: () => ({
+        insights: [
+          {
+            label: "Active Secondary Market",
+            entities: [],
+            description: "Recent auction results in the Artsy Price Database",
+            kind: "ACTIVE_SECONDARY_MARKET",
+          },
+        ],
+      }),
+    })
+
     expect(screen.getByText("Artist Badges")).toBeInTheDocument()
     expect(screen.getByText("Active Secondary Market")).toBeInTheDocument()
+    expect(
+      screen.getByText("Recent auction results in the Artsy Price Database")
+    ).toBeInTheDocument()
   })
 })
