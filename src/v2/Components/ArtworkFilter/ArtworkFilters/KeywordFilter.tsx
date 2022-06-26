c90aa90652f48c1ca99b94e9da0e09c3732fea27
Nikita Skalkin <nikskalkin@gmail.com>
2022-06-26 16:12:13 +0200

    feat(FX-4027): fixes in KeywordFitler component

--- src/v2/Components/ArtworkFilter/ArtworkFilters/KeywordFilter.tsx
+++ src/v2/Components/ArtworkFilter/ArtworkFilters/KeywordFilter.tsx
@@ -1,6 +1,6 @@
 import { LabeledInput, MagnifyingGlassIcon } from "@artsy/palette"
 import { FilterExpandable } from "./FilterExpandable"
-import React, { useEffect, useMemo } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import { debounce } from "lodash"
 import { useArtworkFilterContext } from "../ArtworkFilterContext"
 
@@ -9,29 +9,43 @@ const DEBOUNCE_DELAY = 300
 export const KeywordFilter: React.FC = () => {
   const filterContext = useArtworkFilterContext()
 
+  const [keyword, setKeyword] = useState(filterContext.filters!.keyword)
+
   const updateKeywordFilter = (text: string) => {
-    let textOrUndefined = !text ? undefined : text
+    let textOrUndefined = text.length === 0 ? undefined : text
     filterContext.setFilter("keyword", textOrUndefined)
   }
 
-  const handleChangeText = useMemo(
+  const handleDebounce = useMemo(
     () => debounce(updateKeywordFilter, DEBOUNCE_DELAY),
-    [filterContext]
+    []
   )
+
+  const handleChangeText = text => {
+    setKeyword(text)
+    handleDebounce(text)
+  }
 
   // Stop the invocation of the debounced function after unmounting
   useEffect(() => {
-    return () => handleChangeText.cancel()
+    return () => handleDebounce.cancel()
   }, [])
+
+  useEffect(() => {
+    if (filterContext.filters!.keyword === undefined) {
+      setKeyword("")
+    }
+  }, [filterContext.filters!.keyword])
 
   return (
     <FilterExpandable label="Keyword Search" expanded={true}>
       <LabeledInput
-        defaultValue={filterContext.filters!.keyword}
+        value={keyword}
         placeholder="Enter a search term"
         onChange={event => handleChangeText(event.currentTarget.value)}
         type="text"
         label={<MagnifyingGlassIcon />}
+        data-testid="keywordSearchInput"
       />
     </FilterExpandable>
   )
