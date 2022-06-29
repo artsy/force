1ca3fa66bab7853088eeabce03f686fcdd8c5cad
tmcmeans <tanjmcmeans01@gmail.com>
2022-06-29 15:42:58 -0500

    (onboarding): use OnboardingGene to render Trove collection and artists on the rise

--- src/v2/Apps/Onboarding/Views/OnboardingTopAuctionLots.tsx
+++ src/v2/Apps/Onboarding/Views/OnboardingTopAuctionLots.tsx
@@ -2,5 +2,16 @@ import { FC } from "react"
 import { OnboardingGeneQueryRenderer } from "v2/Apps/Onboarding/Components/OnboardingGene"
 
 export const OnboardingTopAuctionLots: FC = () => {
-  return <OnboardingGeneQueryRenderer id="our-top-auction-lots" />
+  return (
+    <OnboardingGeneQueryRenderer
+      id="our-top-auction-lots"
+      description={
+        <>
+          Click the heart to save artworks you love.
+          <br />
+          Follow the collection to stay up-to-date with the latest auction lots.
+        </>
+      }
+    />
+  )
 }
