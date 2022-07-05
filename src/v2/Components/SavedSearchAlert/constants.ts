954c54aaeafdbea0cd8e054f8f59bd763bc7b787
Dima Tretyak <itretyak@ya.ru>
2022-07-05 13:17:40 +0300

    chore: extract artists from aggregation

--- src/v2/Components/SavedSearchAlert/constants.ts
+++ src/v2/Components/SavedSearchAlert/constants.ts
@@ -5,6 +5,8 @@ export const shouldExtractValueNamesFromAggregation = [
   "materialsTerms",
   "additionalGeneIDs",
   "partnerIDs",
+  "artistIDs",
+  "artistNationalities",
 ]
 
 export const aggregationNameFromFilter: Record<string, Slice> = {
@@ -12,6 +14,8 @@ export const aggregationNameFromFilter: Record<string, Slice> = {
   materialsTerms: "MATERIALS_TERMS",
   additionalGeneIDs: "MEDIUM",
   partnerIDs: "PARTNER",
+  artistIDs: "ARTIST",
+  artistNationalities: "ARTIST_NATIONALITY",
 }
 
 export const allowedSearchCriteriaKeys = [
