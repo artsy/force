4e69909ffd3c7e095a26a31c7513a62563a07b84
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-07-18 17:06:53 +0200

    refactor my collection app

--- src/__generated__/MyCollectionApp_me.graphql.ts
+++ src/__generated__/MyCollectionApp_me.graphql.ts
@@ -5,7 +5,23 @@
 import { ReaderFragment } from "relay-runtime";
 import { FragmentRefs } from "relay-runtime";
 export type MyCollectionApp_me = {
-    readonly name: string | null;
+    readonly myCollectionConnection: {
+        readonly totalCount: number | null;
+        readonly pageInfo: {
+            readonly hasNextPage: boolean;
+            readonly startCursor: string | null;
+            readonly endCursor: string | null;
+        };
+        readonly pageCursors: {
+            readonly " $fragmentRefs": FragmentRefs<"Pagination_pageCursors">;
+        };
+        readonly edges: ReadonlyArray<{
+            readonly node: {
+                readonly internalID: string;
+                readonly " $fragmentRefs": FragmentRefs<"GridItem_artwork">;
+            } | null;
+        } | null> | null;
+    } | null;
     readonly " $refType": "MyCollectionApp_me";
 };
 export type MyCollectionApp_me$data = MyCollectionApp_me;
@@ -17,21 +33,145 @@ export type MyCollectionApp_me$key = {
 
 
 const node: ReaderFragment = {
-  "argumentDefinitions": [],
+  "argumentDefinitions": [
+    {
+      "defaultValue": 1,
+      "kind": "LocalArgument",
+      "name": "page"
+    }
+  ],
   "kind": "Fragment",
-  "metadata": null,
+  "metadata": {
+    "connection": [
+      {
+        "count": null,
+        "cursor": null,
+        "direction": "forward",
+        "path": [
+          "myCollectionConnection"
+        ]
+      }
+    ]
+  },
   "name": "MyCollectionApp_me",
   "selections": [
     {
-      "alias": null,
+      "alias": "myCollectionConnection",
       "args": null,
-      "kind": "ScalarField",
-      "name": "name",
+      "concreteType": "MyCollectionConnection",
+      "kind": "LinkedField",
+      "name": "__MyCollectionApp_myCollectionConnection_connection",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "totalCount",
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PageInfo",
+          "kind": "LinkedField",
+          "name": "pageInfo",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "hasNextPage",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "startCursor",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "endCursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "PageCursors",
+          "kind": "LinkedField",
+          "name": "pageCursors",
+          "plural": false,
+          "selections": [
+            {
+              "args": null,
+              "kind": "FragmentSpread",
+              "name": "Pagination_pageCursors"
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "MyCollectionEdge",
+          "kind": "LinkedField",
+          "name": "edges",
+          "plural": true,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Artwork",
+              "kind": "LinkedField",
+              "name": "node",
+              "plural": false,
+              "selections": [
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "internalID",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "__typename",
+                  "storageKey": null
+                },
+                {
+                  "args": null,
+                  "kind": "FragmentSpread",
+                  "name": "GridItem_artwork"
+                }
+              ],
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "cursor",
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
       "storageKey": null
     }
   ],
   "type": "Me",
   "abstractKey": null
 };
-(node as any).hash = 'c23b5017938e5dfe4976dc838cfded1d';
+(node as any).hash = '6eadd4d568892497287858031b76ec6e';
 export default node;
