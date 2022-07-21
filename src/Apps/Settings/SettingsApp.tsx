a440f7c4c7d3984c9c4e0e0d78f2144a448afc23
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-07-21 14:31:45 +0200

    add my-collection to settings tabs when FF is enabled

--- src/Apps/Settings/SettingsApp.tsx
+++ src/Apps/Settings/SettingsApp.tsx
@@ -4,6 +4,7 @@ import { SettingsApp_me } from "__generated__/SettingsApp_me.graphql"
 import { createFragmentContainer, graphql } from "react-relay"
 import { RouteTab, RouteTabs } from "Components/RouteTabs"
 import { MetaTags } from "Components/MetaTags"
+import { useFeatureFlag } from "System/useFeatureFlag"
 
 interface SettingsAppProps {
   me: SettingsApp_me
@@ -20,7 +21,23 @@ const TABS = [
   { name: "Shipping", url: "/settings/shipping" },
 ]
 
+// Tabs with My Collection (should be removed when the feature flag is enabled)
+const TABS_WITH_MYC = [
+  { name: "Edit Settings", url: "/settings/edit-settings" },
+  { name: "Collector Profile", url: "/settings/edit-profile" },
+  { name: "My Collection", url: "/settings/my-collection" },
+  { name: "Saves & Follows", url: "/settings/saves" },
+  { name: "Your Alerts", url: "/settings/alerts" },
+  { name: "Order History", url: "/settings/purchases" },
+  { name: "Bids", url: "/settings/auctions" },
+  { name: "Payments", url: "/settings/payments" },
+  { name: "Shipping", url: "/settings/shipping" },
+]
+
 const SettingsApp: React.FC<SettingsAppProps> = ({ me, children }) => {
+  const isMyCollectionEnabled = useFeatureFlag("my-collection-web")
+
+  const tabs = isMyCollectionEnabled ? TABS_WITH_MYC : TABS
   return (
     <>
       <MetaTags title="Settings | Artsy" />
@@ -30,7 +47,7 @@ const SettingsApp: React.FC<SettingsAppProps> = ({ me, children }) => {
       </Text>
 
       <RouteTabs my={4}>
-        {TABS.map(tab => {
+        {tabs.map(tab => {
           return (
             <RouteTab key={tab.url} to={tab.url}>
               {tab.name}
