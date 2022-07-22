4a75bdc8f6608b2b7647cadfd337e7f8168d8b0a
Lidiane Taquehara <lidi.mayra@gmail.com>
2022-07-22 15:34:14 -0300

    Include docs about i18n (#10588)
    * Include docs about i18n
    
    We setup react-i18next during
    [these changes](https://github.com/artsy/force/pull/9797), these docs
    contain instructions about the context, usage and next steps.
    
    Co-authored-by: Lidiane Taquehara <lidi.mayra@gmail.com>
    Co-authored-by: Leandro Linares <leandro.linares@artsymail.com>
    
    * Move locales to /i18n directory
    
    * Add comment about assets served on the server-side
    
    * camelCase instead of lowerCamelCase
    
    * Update docs/i18n.md
    
    Co-authored-by: Pavlos Vinieratos <pvinis@gmail.com>
    
    Co-authored-by: Leandro Linares <leandro.linares@artsymail.com>
    Co-authored-by: Pavlos Vinieratos <pvinis@gmail.com>

--- src/System/i18n/i18n.ts
+++ src/System/i18n/i18n.ts
@@ -2,7 +2,8 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import { getENV } from "Utils/getENV"
 
-const translations = require("System/locales/en-US/translation.json")
+// TODO: Serve static assets on the server-side
+const translations = require("System/i18n/locales/en-US/translation.json")
 
 i18n.use(initReactI18next).init({
   debug: getENV("ENABLE_I18N_DEBUG") === "true",
