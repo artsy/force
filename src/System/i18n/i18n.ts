43419bdc7aa1e5e2155ad9df9d7b8413f07138b9
Lidiane Taquehara <lidi.mayra@gmail.com>
2022-07-22 20:11:02 +0200

    Move locales to /i18n directory

--- src/System/i18n/i18n.ts
+++ src/System/i18n/i18n.ts
@@ -2,7 +2,7 @@ import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import { getENV } from "Utils/getENV"
 
-const translations = require("System/locales/en-US/translation.json")
+const translations = require("System/i18n/locales/en-US/translation.json")
 
 i18n.use(initReactI18next).init({
   debug: getENV("ENABLE_I18N_DEBUG") === "true",
