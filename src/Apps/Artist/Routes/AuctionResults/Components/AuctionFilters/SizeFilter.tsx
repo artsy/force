9488ddc70637031f05ef31e26b400e22b4c05e64
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2022-07-26 12:27:34 +0300

    feat(FX-4039): expand auto-selection of user's preferred measurement system for size filter on auction results grid (#10594)
    * feat: add metric for auction results
    
    * fix: typo

--- src/Apps/Artist/Routes/AuctionResults/Components/AuctionFilters/SizeFilter.tsx
+++ src/Apps/Artist/Routes/AuctionResults/Components/AuctionFilters/SizeFilter.tsx
@@ -1,16 +1,23 @@
 import * as React from "react"
-import { Checkbox, Flex, Text } from "@artsy/palette"
+import { Checkbox, Flex, Radio, RadioGroup, Text } from "@artsy/palette"
 import { FilterExpandable } from "Components/ArtworkFilter/ArtworkFilters/FilterExpandable"
 import { ShowMore } from "Components/ArtworkFilter/ArtworkFilters/ShowMore"
 import {
   useAuctionResultsFilterContext,
   useCurrentlySelectedFiltersForAuctionResults,
 } from "../../AuctionResultsFilterContext"
+import { Metric } from "Utils/metrics"
 
 export const sizeMap = [
   { displayName: "Small (under 40cm)", name: "SMALL" },
   { displayName: "Medium (40 – 100cm)", name: "MEDIUM" },
   { displayName: "Large (over 100cm)", name: "LARGE" },
+]
+
+export const sizeMapInInches = [
+  { displayName: "Small (under 16in)", name: "SMALL" },
+  { displayName: "Medium (16 – 40in)", name: "MEDIUM" },
+  { displayName: "Large (over 40in)", name: "LARGE" },
 ]
 
 /**
@@ -19,7 +26,8 @@ export const sizeMap = [
  */
 export const SizeFilter: React.FC = () => {
   const { setFilter } = useAuctionResultsFilterContext()
-  const { sizes = [] } = useCurrentlySelectedFiltersForAuctionResults()
+  const { sizes = [], metric } = useCurrentlySelectedFiltersForAuctionResults()
+  const options = metric === "in" ? sizeMapInInches : sizeMap
 
   const toggleSelection = (selected: boolean, name: string) => {
     let updatedValues = sizes
@@ -33,14 +41,33 @@ export const SizeFilter: React.FC = () => {
     setFilter?.("sizes", updatedValues)
   }
 
+  const handleSelectMetric = (nextMetric: Metric) => {
+    if (metric === nextMetric) {
+      return
+    }
+
+    setFilter?.("metric", nextMetric)
+  }
+
   return (
     <FilterExpandable label="Size" expanded>
       <Flex flexDirection="column" alignItems="left">
         <Text variant="sm">
           This is based on the artwork’s average dimension.
         </Text>
+
+        <RadioGroup
+          defaultValue={metric}
+          onSelect={handleSelectMetric}
+          flexDirection="row"
+          my={2}
+        >
+          <Radio value="cm" label="cm" flex={1} />
+          <Radio value="in" label="in" flex={1} />
+        </RadioGroup>
+
         <ShowMore>
-          {sizeMap.map((checkbox, index) => {
+          {options.map((checkbox, index) => {
             const { name, displayName } = checkbox
             const props = {
               key: index,
