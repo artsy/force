64242955d14c59675466caef253bf135feaa24a5
Alican Akyuz <alicakyz@gmail.com>
2022-07-28 16:44:16 +0200

    clean up

--- src/Apps/Order/Components/SetPaymentByStripeIntent.tsx
+++ src/Apps/Order/Components/SetPaymentByStripeIntent.tsx
@@ -1,6 +1,5 @@
-import { FC, useEffect } from "react"
+import { FC } from "react"
 import { Payment_order } from "__generated__/Payment_order.graphql"
-import { useSetPaymentByStripeIntent } from "Apps/Order/Mutations/useSetPaymentByStripeIntent"
 import { ProcessingPayment } from "./ProcessingPayment"
 
 interface Props {
@@ -11,40 +10,40 @@ interface Props {
   onError: (object) => void
 }
 
-export const SetPaymentByStripeIntent: FC<Props> = props => {
-  const { setupIntentId, shouldSaveBankAccount } = props
-  const {
-    submitMutation: setPaymentByStripeIntentMutation,
-  } = useSetPaymentByStripeIntent()
+export const SetPaymentByStripeIntent: FC<Props> = () => {
+  // const { setupIntentId, shouldSaveBankAccount } = props
+  // const {
+  //   submitMutation: setPaymentByStripeIntentMutation,
+  // } = useSetPaymentByStripeIntent()
 
-  useEffect(() => {
-    const setPaymentByStripeIntent = async () => {
-      try {
-        const orderOrError = (
-          await setPaymentByStripeIntentMutation({
-            variables: {
-              input: {
-                id: props.order.internalID,
-                setupIntentId: setupIntentId,
-                oneTimeUse: !shouldSaveBankAccount,
-              },
-            },
-          })
-        ).commerceSetPaymentByStripeIntent?.orderOrError
+  // useEffect(() => {
+  //   const setPaymentByStripeIntent = async () => {
+  //     try {
+  //       const orderOrError = (
+  //         await setPaymentByStripeIntentMutation({
+  //           variables: {
+  //             input: {
+  //               id: props.order.internalID,
+  //               setupIntentId: setupIntentId,
+  //               oneTimeUse: !shouldSaveBankAccount,
+  //             },
+  //           },
+  //         })
+  //       ).commerceSetPaymentByStripeIntent?.orderOrError
 
-        if (orderOrError?.error) {
-          throw orderOrError.error
-        }
+  //       if (orderOrError?.error) {
+  //         throw orderOrError.error
+  //       }
 
-        props.onSuccess()
-      } catch (error) {
-        props.onError(error)
-      }
-    }
+  //       props.onSuccess()
+  //     } catch (error) {
+  //       props.onError(error)
+  //     }
+  //   }
 
-    setPaymentByStripeIntent()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
+  //   setPaymentByStripeIntent()
+  //   // eslint-disable-next-line react-hooks/exhaustive-deps
+  // }, [])
 
   return <ProcessingPayment />
 }
