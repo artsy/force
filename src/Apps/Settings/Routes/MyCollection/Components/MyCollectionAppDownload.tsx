82ebbf106229f53f218be78bd8a4e25d9dca69bd
Kizito Egeonu <kizito.egeonu@gmail.com>
2022-08-01 12:44:02 +0200

    Reverse direction of layout on mobile web; reduce font on mobile web

--- src/Apps/Settings/Routes/MyCollection/Components/MyCollectionAppDownload.tsx
+++ src/Apps/Settings/Routes/MyCollection/Components/MyCollectionAppDownload.tsx
@@ -9,16 +9,59 @@ import {
   Text,
 } from "@artsy/palette"
 import { resized } from "Utils/resized"
+import { Media } from "Utils/Responsive"
 
-export const MyCollectionAppDownload: React.FC = () => {
-  const image = resized(
-    "https://files.artsy.net/images/my-collection-get-app-img.jpg",
-    {
-      width: 910,
-      height: 652,
-    }
-  )
+const image = resized(
+  "https://files.artsy.net/images/my-collection-get-app-img.jpg",
+  {
+    width: 910,
+    height: 652,
+  }
+)
 
+const AppStoreBadges = () => (
+  <Flex>
+    <Box
+      width={120}
+      height={40}
+      as="a"
+      // @ts-ignore
+      href="https://apps.apple.com/us/app/artsy-buy-sell-original-art/id703796080"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        src="https://files.artsy.net/images/download-ios-app.svg"
+        width="100%"
+        height="100%"
+        alt="ios app store button"
+        loading="lazy"
+      />
+    </Box>
+
+    <Spacer ml={2} />
+
+    <Box
+      width={120}
+      height={40}
+      as="a"
+      // @ts-ignore
+      href="https://play.google.com/store/apps/details?id=net.artsy.app"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Image
+        src="https://files.artsy.net/images/download-android-app.svg"
+        width="100%"
+        height="100%"
+        alt="android play store button"
+        loading="lazy"
+      />
+    </Box>
+  </Flex>
+)
+
+const DesktopLayout: React.FC = () => {
   return (
     <GridColumns gridRowGap={4} alignItems="center">
       <Column span={6}>
@@ -32,45 +75,7 @@ export const MyCollectionAppDownload: React.FC = () => {
           website.
         </Text>
 
-        <Flex>
-          <Box
-            width={120}
-            height={40}
-            as="a"
-            // @ts-ignore
-            href="https://apps.apple.com/us/app/artsy-buy-sell-original-art/id703796080"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="https://files.artsy.net/images/download-ios-app.svg"
-              width="100%"
-              height="100%"
-              alt="ios app store button"
-              loading="lazy"
-            />
-          </Box>
-
-          <Spacer ml={2} />
-
-          <Box
-            width={120}
-            height={40}
-            as="a"
-            // @ts-ignore
-            href="https://play.google.com/store/apps/details?id=net.artsy.app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="https://files.artsy.net/images/download-android-app.svg"
-              width="100%"
-              height="100%"
-              alt="android play store button"
-              loading="lazy"
-            />
-          </Box>
-        </Flex>
+        <AppStoreBadges />
       </Column>
 
       <Column span={6}>
@@ -86,5 +91,49 @@ export const MyCollectionAppDownload: React.FC = () => {
         </ResponsiveBox>
       </Column>
     </GridColumns>
+  )
+}
+
+const MobileLayout: React.FC = () => {
+  return (
+    <GridColumns gridRowGap={4} alignItems="center">
+      <Column span={6}>
+        <ResponsiveBox aspectHeight={652} aspectWidth={910} maxWidth="100%">
+          <Image
+            src={image.src}
+            width="100%"
+            height="100%"
+            srcSet={image.srcSet}
+            lazyLoad
+            alt="My Collection app hero image"
+          />
+        </ResponsiveBox>
+      </Column>
+      <Column span={6}>
+        <Text variant="lg" mt={1}>
+          Get More from My Collection on the App
+        </Text>
+
+        <Text variant="sm" mt={1} mb={4}>
+          Discover all the features of My Collection on the Artsy app. Coming
+          soon: The full range of features will also be available on the
+          website.
+        </Text>
+        <AppStoreBadges />
+      </Column>
+    </GridColumns>
+  )
+}
+
+export const MyCollectionAppDownload: React.FC = () => {
+  return (
+    <>
+      <Media at="xs">
+        <MobileLayout />
+      </Media>
+      <Media greaterThan="xs">
+        <DesktopLayout />
+      </Media>
+    </>
   )
 }
