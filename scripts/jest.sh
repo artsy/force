b5e45199aedca553bfb15108c67068c05a4f6b2f
Christopher Pappas <damassi.pappas@gmail.com>
2022-08-02 22:37:04 -0700

    ci(jest): Experiment with test sharding

--- scripts/jest.sh
+++ scripts/jest.sh
@@ -2,16 +2,14 @@
 
 set -ex
 
+
 NODE_ENV=test
 
-function runJest() {
-  node \
-    --expose-gc \
-    --max_old_space_size=4096 \
-    ./node_modules/.bin/jest \
-      --logHeapUsage \
-      --maxWorkers 3 \
-      --config "${1}"
-}
-
-runJest jest.config.js
+node \
+  --expose-gc \
+  --max_old_space_size=4096 \
+  ./node_modules/.bin/jest \
+    --logHeapUsage \
+    --maxWorkers 3 \
+    --config jest.config.js \
+    "$@"
