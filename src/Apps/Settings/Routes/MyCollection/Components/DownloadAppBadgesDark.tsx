401ade3ab1a303f1997694174d7919d2653ec798
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-08-04 11:54:56 +0200

    update download app badges logic

---
+++ src/Apps/Settings/Routes/MyCollection/Components/DownloadAppBadgesDark.tsx
@@ -0,0 +1,61 @@
+import { Box, Flex, Image, Spacer } from "@artsy/palette"
+import { RouterLink } from "System/Router/RouterLink"
+import {
+  useDeviceDetection,
+  Device,
+  DOWNLOAD_APP_URLS,
+} from "Utils/Hooks/useDeviceDetection"
+
+export const DownloadAppBadgesDark: React.FC = () => {
+  const { device } = useDeviceDetection()
+
+  if (device === Device.Android) {
+    return <PlayStoreBadge />
+  }
+
+  if (device === Device.iPhone) {
+    return <AppStoreBadge />
+  }
+
+  return (
+    <Flex>
+      <AppStoreBadge />
+
+      <Spacer ml={2} />
+
+      <PlayStoreBadge />
+    </Flex>
+  )
+}
+
+const PlayStoreBadge: React.FC = () => {
+  return (
+    <RouterLink to={DOWNLOAD_APP_URLS[Device.Android]} target="_blank">
+      <Box width={136} height={40}>
+        <Image
+          src="https://files.artsy.net/images/download-android-app.svg"
+          width="100%"
+          height="100%"
+          alt="android play store button"
+          loading="lazy"
+        />
+      </Box>
+    </RouterLink>
+  )
+}
+
+const AppStoreBadge: React.FC = () => {
+  return (
+    <RouterLink to={DOWNLOAD_APP_URLS[Device.iPhone]} target="_blank">
+      <Box width={120} height={40}>
+        <Image
+          src="https://files.artsy.net/images/download-ios-app.svg"
+          width="100%"
+          height="100%"
+          alt="ios app store button"
+          loading="lazy"
+        />
+      </Box>
+    </RouterLink>
+  )
+}
