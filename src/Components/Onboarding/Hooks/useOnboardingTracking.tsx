4c574a227ebf43f9f14b7915a8386afc6f99504f
tmcmeans <tanjmcmeans01@gmail.com>
2022-08-09 14:41:14 -0500

    feat(onboarding): update and clean up onboarding tracking functions

--- src/Components/Onboarding/Hooks/useOnboardingTracking.tsx
+++ src/Components/Onboarding/Hooks/useOnboardingTracking.tsx
@@ -1,119 +1,139 @@
 import { useTracking } from "react-tracking"
-import { useAnalyticsContext } from "System"
 import {
   ActionType,
   ContextModule,
   StartedOnboarding,
   CompletedOnboarding,
   OnboardingUserInputData,
-  // FollowedArtist,
-  // FollowedPartner,
-  // FollowedGene,
-  // followedPartner,
-  // unfollowedPartner,
-  // followedArtist,
-  // unfollowedArtist,
-  // followedGene,
-  // unfollowedGene,
+  OwnerType,
+  FollowedArtist,
+  FollowedPartner,
+  FollowedGene,
+  UnfollowedArtist,
+  UnfollowedPartner,
+  UnfollowedGene,
 } from "@artsy/cohesion"
+import { getContextPageFromClient } from "lib/getContextPage"
 
 export const useOnboardingTracking = () => {
   const { trackEvent } = useTracking()
-
-  const {
-    contextPageOwnerId,
-    contextPageOwnerType,
-    contextPageOwnerSlug,
-  } = useAnalyticsContext()
-
-  if (!contextPageOwnerId || !contextPageOwnerType || !contextPageOwnerSlug) {
-    console.warn("Missing analytics context")
-  }
-
-  const context = {
-    context_page_owner_id: contextPageOwnerId,
-    context_page_owner_type: contextPageOwnerType,
-    context_page_owner_slug: contextPageOwnerSlug,
-  }
 
   return {
     // user clicks get started
     userStartedOnboarding: () => {
       const payload: StartedOnboarding = {
         action: ActionType.startedOnboarding,
-        ...context,
       }
 
       trackEvent(payload)
     },
 
     // user clicks next after answering "have you bought art before"
-    userAnsweredCollectorQuestion: () => {
+    userAnsweredCollectorQuestion: response => {
       const payload: OnboardingUserInputData = {
         action: ActionType.onboardingUserInputData,
         context_module: ContextModule.onboardingCollectorLevel,
-        data_input: "[userCollectorLevel]",
-        ...context,
+        data_input: response,
       }
 
       trackEvent(payload)
     },
 
     // user clicks next after answering "what do you love most about art"
-    userAnsweredInterestQuestion: () => {
+    userAnsweredInterestQuestion: response => {
       const payload: OnboardingUserInputData = {
         action: ActionType.onboardingUserInputData,
         context_module: ContextModule.onboardingInterests,
-        data_input: "[userInterests]",
-        ...context,
+        data_input: response,
       }
 
       trackEvent(payload)
     },
 
     // user clicks next after answering "what would you like to see first"
-    userAnsweredActivityQuestion: () => {
+    userAnsweredActivityQuestion: response => {
       const payload: OnboardingUserInputData = {
         action: ActionType.onboardingUserInputData,
         context_module: ContextModule.onboardingActivity,
-        data_input: "[userActivity]",
-        ...context,
+        data_input: response,
       }
 
       trackEvent(payload)
     },
 
     // user follows or unfollows a artist
-    trackArtistFollows: isFollowed => {
-      // const payload: FollowedArtist = {
-      //   action: ActionType.followedArtist,
-      //   context_module: ContextModule.onboardingFlow,
-      //   ...context,
-      // }
-      // change payload based on follow or unfollow
-      // trackEvent(isFollowed ? unfollowedArtist() : followedArtist())
+    trackArtistFollows: (isFollowed, internalID) => {
+      const contextPage = getContextPageFromClient()
+
+      const followPayload: FollowedArtist = {
+        action: ActionType.followedArtist,
+        context_module: ContextModule.onboardingFlow,
+        context_owner_type: OwnerType.savesAndFollows,
+        owner_id: internalID,
+        owner_slug: contextPage?.pageSlug!,
+        owner_type: OwnerType.artist,
+      }
+
+      const unfollowPayload: UnfollowedArtist = {
+        action: ActionType.unfollowedArtist,
+        context_module: ContextModule.onboardingFlow,
+        context_owner_type: OwnerType.savesAndFollows,
+        owner_id: internalID,
+        owner_slug: contextPage?.pageSlug!,
+        owner_type: OwnerType.artist,
+      }
+
+      trackEvent(isFollowed ? followPayload : unfollowPayload)
     },
 
     // user follows or unfollows a gallery
-    trackGalleryFollows: isFollowed => {
-      // const payload: FollowedPartner = {
-      //   action: ActionType.followedPartner,
-      //   context_module: ContextModule.onboardingFlow,
-      //   ...context,
-      // }
-      // change payload based on follow or unfollow
-      // trackEvent(isFollowed ? unfollowedPartner() : followedPartner())
+    trackGalleryFollows: (isFollowed, internalID) => {
+      const contextPage = getContextPageFromClient()
+
+      const followedPayload: FollowedPartner = {
+        action: ActionType.followedPartner,
+        context_module: ContextModule.onboardingFlow,
+        context_owner_type: OwnerType.savesAndFollows,
+        owner_id: internalID,
+        owner_slug: contextPage?.pageSlug!,
+        owner_type: OwnerType.partner,
+      }
+
+      const unfollowedPayload: UnfollowedPartner = {
+        action: ActionType.unfollowedPartner,
+        context_module: ContextModule.onboardingFlow,
+        context_owner_type: OwnerType.savesAndFollows,
+        owner_id: internalID,
+        owner_slug: contextPage?.pageSlug!,
+        owner_type: OwnerType.partner,
+      }
+
+      trackEvent(isFollowed ? followedPayload : unfollowedPayload)
     },
 
     // user follows or unfollows a gene
-    trackGeneFollows: isFollowed => {
-      // const payload: FollowedGene = {
-      //   action: ActionType.followedGene,
-      //   context_module: ContextModule.onboardingFlow,
-      //   ...context,
-      // }
-      // change payload based on follow or unfollow
-      // trackEvent(isFollowed ? unfollowedGene() : followedGene())
+    trackGeneFollows: (isFollowed, internalID) => {
+      const contextPage = getContextPageFromClient()
+
+      const followedPayload: FollowedGene = {
+        action: ActionType.followedGene,
+        context_module: ContextModule.onboardingFlow,
+        context_owner_type: OwnerType.savesAndFollows,
+        owner_id: internalID,
+        owner_slug: contextPage?.pageSlug!,
+        owner_type: OwnerType.gene,
+      }
+
+      const unfollowedPayload: UnfollowedGene = {
+        action: ActionType.unfollowedGene,
+        context_module: ContextModule.onboardingFlow,
+        context_owner_type: OwnerType.savesAndFollows,
+        owner_id: internalID,
+        owner_slug: contextPage?.pageSlug!,
+        owner_type: OwnerType.gene,
+      }
+
+      trackEvent(isFollowed ? followedPayload : unfollowedPayload)
     },
 
     // whenever we decide onboarding is complete
