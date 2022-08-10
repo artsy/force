43912f7585a4650a0cea06af8e7702f049dad8f0
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-08-10 11:46:09 +0200

    fix: remove commented code
    Co-authored-by: Mounir Dhahri <soussouheros@gmail.com>

--- src/Apps/MyCollectionArtwork/myCollectionArtworkRoutes.tsx
+++ src/Apps/MyCollectionArtwork/myCollectionArtworkRoutes.tsx
@@ -19,24 +19,5 @@ export const myCollectionArtworkRoutes: AppRouteConfig[] = [
     onClientSideRender: () => {
       MyCollectionArtworkApp.preload()
     },
-    // TODO: use this query when working on my collection artwork page tasks [CX-2707 OR CX-2708].
-    // prepareVariables: ({ artworkSlug }, props) => {
-    //   return {
-    //     artworkSlug,
-    //   }
-    // },
-    // query: graphql`
-    //   query artworkRoutes_ArtworkQuery($artworkSlug: String!) {
-    //     artwork(id: $artworkSlug) @principalField {
-    //       ...ArtworkApp_artwork
-    //     }
-    //     me {
-    //       ...ArtworkApp_me
-    //     }
-    //   }
-    // `,
-    // cacheConfig: {
-    //   force: true,
-    // },
   },
 ]
