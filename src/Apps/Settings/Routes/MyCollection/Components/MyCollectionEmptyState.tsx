02a95b749acec605f8c9003f96c998778cbc3625
Daria Kozlova <daria.kozlova@artsymail.com>
2022-08-18 15:04:50 +0200

    feat(CX-2696): build empty state page replacing landing page for my collection (#10785)

---
+++ src/Apps/Settings/Routes/MyCollection/Components/MyCollectionEmptyState.tsx
@@ -0,0 +1,108 @@
+import {
+  Button,
+  Column,
+  GridColumns,
+  Image,
+  ResponsiveBox,
+  Text,
+} from "@artsy/palette"
+import { RouterLink } from "System/Router/RouterLink"
+import { resized } from "Utils/resized"
+import { Media } from "Utils/Responsive"
+
+const image = resized(
+  "https://files.artsy.net/images/my-coll-get-app-img.jpg",
+  {
+    width: 770,
+    height: 652,
+  }
+)
+
+const DesktopLayout: React.FC = () => {
+  return (
+    <GridColumns mb={12} gridRowGap={4} alignItems="center">
+      <Column span={6}>
+        <Text variant="xxl">Manage Your Art Collection Online</Text>
+
+        <Text variant="sm" mt={2} mb={6}>
+          Access price and market insights and build an online record of your
+          collection.
+        </Text>
+
+        <Button
+          // @ts-ignore
+          as={RouterLink}
+          variant="primaryBlack"
+          to="/my-collection/artworks/new"
+        >
+          Upload Artwork
+        </Button>
+      </Column>
+
+      <Column span={6}>
+        <ResponsiveBox aspectHeight={652} aspectWidth={770} maxWidth="100%">
+          <Image
+            src={image.src}
+            width="100%"
+            height="100%"
+            srcSet={image.srcSet}
+            lazyLoad
+            alt="Screenshot of My Collection sample screen on the App against a painting background"
+          />
+        </ResponsiveBox>
+      </Column>
+    </GridColumns>
+  )
+}
+
+const MobileLayout: React.FC = () => {
+  return (
+    <GridColumns gridRowGap={2} alignItems="center">
+      <Column span={6}>
+        <ResponsiveBox aspectHeight={652} aspectWidth={770} maxWidth="100%">
+          <Image
+            src={image.src}
+            width="100%"
+            height="100%"
+            srcSet={image.srcSet}
+            lazyLoad
+            alt="My Collection app hero image"
+          />
+        </ResponsiveBox>
+      </Column>
+
+      <Column span={6}>
+        <Text variant="xl" mt={2}>
+          Manage Your Art Collection Online
+        </Text>
+
+        <Text variant="sm" mt={0.5} mb={2}>
+          Access price and market insights and build an online record of your
+          collection.
+        </Text>
+        <Button
+          // @ts-ignore
+          as={RouterLink}
+          variant="primaryBlack"
+          to="/my-collection/artworks/new"
+          width="100%"
+        >
+          Upload Artwork
+        </Button>
+      </Column>
+    </GridColumns>
+  )
+}
+
+export const MyCollectionEmptyState: React.FC = () => {
+  return (
+    <>
+      <Media at="xs">
+        <MobileLayout />
+      </Media>
+      <Media greaterThan="xs">
+        <DesktopLayout />
+      </Media>
+    </>
+  )
+}
