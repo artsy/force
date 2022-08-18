ff688103a9b4c27b99481f81901d35d08e535745
dariakoko <nargle30@gmail.com>
2022-08-18 17:09:05 +0200

    chore: addressing review comments

--- src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworkComparables.tsx
+++ src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworkComparables.tsx
@@ -1,19 +1,19 @@
-import { Column, Flex, Join, Message, Spacer, Text } from "@artsy/palette"
+import { Column, Join, Spacer, Text } from "@artsy/palette"
 import { createFragmentContainer, graphql } from "react-relay"
 import { MetaTags } from "Components/MetaTags"
 import { ArtistAuctionResultItemFragmentContainer } from "Apps/Artist/Routes/AuctionResults/ArtistAuctionResultItem"
 import { extractNodes } from "Utils/extractNodes"
-import { __internal__useMatchMedia } from "Utils/Hooks/useMatchMedia"
 import { MyCollectionArtworkComparables_artwork } from "__generated__/MyCollectionArtworkComparables_artwork.graphql"
+
 interface MyCollectionArtworkComparablesProps {
   artwork: MyCollectionArtworkComparables_artwork
 }
 
-const ComparablesContainer: React.FC<MyCollectionArtworkComparablesProps> = ({
+const MyCollectionArtworkComparables: React.FC<MyCollectionArtworkComparablesProps> = ({
   artwork,
 }) => {
   if (!artwork.auctionResult) {
-    return <Flex />
+    return null
   }
   const results = extractNodes(artwork.auctionResult)
   const titleString = `${artwork.artist?.name!} - Comparable Works on Artsy`
@@ -30,25 +30,17 @@ const ComparablesContainer: React.FC<MyCollectionArtworkComparablesProps> = ({
 
       <Column span={9}>
         <Spacer mt={[2, 0]} />
-
-        {results.length > 0 ? (
-          <Join separator={<Spacer mt={2} />}>
-            {results.map((result, index) => {
-              return (
-                <ArtistAuctionResultItemFragmentContainer
-                  key={index}
-                  auctionResult={result}
-                  filtersAtDefault={false}
-                />
-              )
-            })}
-          </Join>
-        ) : (
-          <Message>
-            There aren’t any auction results available by the artist at this
-            time.
-          </Message>
-        )}
+        <Join separator={<Spacer mt={2} />}>
+          {results.map((result, index) => {
+            return (
+              <ArtistAuctionResultItemFragmentContainer
+                key={index}
+                auctionResult={result}
+                filtersAtDefault={false}
+              />
+            )
+          })}
+        </Join>
       </Column>
       <Spacer pb={6} />
     </>
@@ -56,7 +48,7 @@ const ComparablesContainer: React.FC<MyCollectionArtworkComparablesProps> = ({
 }
 
 export const MyCollectionArtworkComparablesFragmentContainer = createFragmentContainer(
-  ComparablesContainer,
+  MyCollectionArtworkComparables,
   {
     artwork: graphql`
       fragment MyCollectionArtworkComparables_artwork on Artwork {
