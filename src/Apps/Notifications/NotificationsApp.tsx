5b8c3f0593245681220af028473b8d8ebebfe2cc
Dima Tretyak <itretyak@ya.ru>
2022-08-25 11:24:20 +0300

    refactor: keep query renderer in Components

--- src/Apps/Notifications/NotificationsApp.tsx
+++ src/Apps/Notifications/NotificationsApp.tsx
@@ -1,5 +1,5 @@
-import { NotificationsQueryRenderer } from "./Components/Notifications"
 import { MetaTags } from "Components/MetaTags"
+import { NotificationsQueryRenderer } from "Components/Notifications/Notifications"
 
 export const NotificationsApp: React.FC = () => {
   return (
