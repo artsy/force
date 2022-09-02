b1492a8ec038cb2211076b865413c0b6af6972d4
tmcmeans <tanjmcmeans01@gmail.com>
2022-09-02 15:45:35 -0500

    feat(art-quiz) styling updates

--- src/Components/ArtQuiz/Views/ArtQuizWelcome.tsx
+++ src/Components/ArtQuiz/Views/ArtQuizWelcome.tsx
@@ -1,40 +1,64 @@
-import { Box, Flex, Text, Spacer, Button } from "@artsy/palette"
-import { OnboardingSplitLayout } from "Components/Onboarding/Components/OnboardingSplitLayout"
-import { ArtQuizLeftPanel } from "../Components/ArtQuizLeftPanel"
+import {
+  Box,
+  Flex,
+  Text,
+  Spacer,
+  Button,
+  ArtsyMarkBlackIcon,
+} from "@artsy/palette"
+import { SplitLayout } from "Components/SplitLayout"
 
 export const ArtQuizWelcome = () => {
   return (
-    <OnboardingSplitLayout
-      left={<ArtQuizLeftPanel />}
-      leftProps={{ display: "block" }}
-      right={
-        <Flex flexDirection="column" justifyContent="space-between" p={[2, 4]}>
-          {/* Vertically centers next Box */}
-          <Box />
+    <Box height="100%" width="100%" padding={0}>
+      <SplitLayout
+        hideLogo
+        left={
+          <Flex height="100%" alignItems="center" justifyContent="center">
+            <ArtsyMarkBlackIcon height="65px" width="65px" fill="white100" />
+          </Flex>
+        }
+        leftProps={{ display: ["none", "block"] }}
+        right={
+          <Flex
+            flexDirection="column"
+            justifyContent="space-between"
+            p={[2, 4]}
+          >
+            {/* Vertically centers next Box */}
+            <Box />
 
-          <Box width="100%">
-            <Text variant={["xl", "xxl"]}>Art Curator Quiz</Text>
+            <Box width="100%">
+              <Text variant={["xl", "xxl"]}>Art Curator Quiz</Text>
 
-            <Spacer mt={[2, 4]} />
+              <Spacer mt={[2, 4]} />
 
-            <Text variant={["md", "lg-display"]}>
-              See more of what you love.
-              <Spacer my={1} />
-              Rate artworks to discover your taste profile and get
-              recommendations tailored to you.
-            </Text>
-          </Box>
+              <Text variant={["md", "lg-display"]}>
+                See more of what you love.
+                <Spacer my={1} />
+                Rate artworks to discover your taste profile and get
+                recommendations tailored to you.
+              </Text>
+            </Box>
 
-          <Spacer my={1} />
+            <Spacer my={1} />
 
-          <Box width="100%">
-            <Button width="100%">Start the Quiz</Button>
-            <Button variant="tertiary" mt={1} width="100%">
-              Skip
-            </Button>
-          </Box>
-        </Flex>
-      }
-    />
+            <Box width="100%">
+              <Button width="100%">Start the Quiz</Button>
+              <Button
+                variant="tertiary"
+                mt={1}
+                width="100%"
+                onClick={() => {
+                  //redirect to home
+                }}
+              >
+                Skip
+              </Button>
+            </Box>
+          </Flex>
+        }
+      />
+    </Box>
   )
 }
