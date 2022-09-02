b3765a0037604700d0ca4828cf64facd4e7b1f8e
Dima Tretyak <itretyak@ya.ru>
2022-09-02 16:20:24 +0300

    fix: test

--- src/Components/Notifications/__tests__/Notifications.jest.tsx
+++ src/Components/Notifications/__tests__/Notifications.jest.tsx
@@ -1,9 +1,13 @@
 import { render, screen } from "@testing-library/react"
 import { Notifications } from "../Notifications"
 
+jest.mock("Utils/Hooks/useMatchMedia", () => ({
+  __internal__useMatchMedia: () => false,
+}))
+
 describe("Notifications", () => {
   it("should render tabs", () => {
-    render(<Notifications />)
+    render(<Notifications mode="page" />)
 
     expect(screen.getByText("All")).toBeInTheDocument()
     expect(screen.getByText("Alerts")).toBeInTheDocument()
