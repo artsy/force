a1c440236399ad29cfec9011e1c98ff7448146f6
Dima Tretyak (Dzmitry Tratsiak) <itretyak@ya.ru>
2022-09-09 13:13:50 +0300

    feat: unlock scroll when menu is closed (#10956)

--- src/Components/NavBar/Menus/NavBarNewNotifications.tsx
+++ src/Components/NavBar/Menus/NavBarNewNotifications.tsx
@@ -1,9 +1,20 @@
 import { Box } from "@artsy/palette"
 import { Notifications } from "Components/Notifications/Notifications"
+import { useEffect } from "react"
 import { useScrollLock } from "Utils/Hooks/useScrollLock"
 
 export const NavBarNewNotifications = () => {
   const { lockScroll, unlockScroll } = useScrollLock()
+
+  useEffect(() => {
+    return () => {
+      /**
+       * unlock parent scroll when component unmounted
+       * for example, when the user clicked on the notification and the dropdown menu was closed
+       */
+      unlockScroll()
+    }
+  }, [unlockScroll])
 
   return (
     <Box
