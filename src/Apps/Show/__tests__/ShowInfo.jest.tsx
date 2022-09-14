3f06b3c3e37650da4a7ea5853d0157c190cd0d15
Jon Allured <jon.allured@gmail.com>
2022-09-14 10:43:40 -0400

    User sees show events on the info page

--- src/Apps/Show/__tests__/ShowInfo.jest.tsx
+++ src/Apps/Show/__tests__/ShowInfo.jest.tsx
@@ -16,11 +16,80 @@ const { getWrapper } = setupTestWrapper({
 })
 
 describe("ShowInfo", () => {
-  it("renders the info page", () => {
-    const wrapper = getWrapper({ Partner: () => ({ type: "Gallery" }) })
+  describe("default rendering", () => {
+    it("renders the basic page", () => {
+      const events = []
 
-    expect(wrapper.find("h1")).toHaveLength(1)
-    expect(wrapper.find("h1").text()).toEqual("About")
-    expect(wrapper.find("h2").text()).toEqual("Gallery")
+      const wrapper = getWrapper({
+        Show: () => ({ events }),
+        Partner: () => ({ type: "Gallery" }),
+      })
+
+      expect(wrapper.find("h1")).toHaveLength(1)
+      expect(wrapper.find("h1").text()).toEqual("About")
+      expect(wrapper.find("EventList")).toHaveLength(0)
+      expect(wrapper.find("h2").text()).toEqual("Gallery")
+    })
+  })
+
+  describe("with an event", () => {
+    it("renders that event", () => {
+      const event = {
+        title: "Number one best event",
+        dateTimeRange: "noon to 1:00",
+        description: "This is going to be a fun event!",
+      }
+
+      const events = [event]
+
+      const wrapper = getWrapper({
+        Show: () => ({ events }),
+        Partner: () => ({ type: "Gallery" }),
+      })
+
+      expect(wrapper.find("EventList")).toHaveLength(1)
+      const eventList = wrapper.find("EventList")
+
+      expect(eventList.find("h3").text()).toEqual("Number one best event")
+      expect(eventList.text()).toContain("noon to 1:00")
+      expect(eventList.text()).toContain("This is going to be a fun event!")
+    })
+  })
+
+  describe("with an event that has no title", () => {
+    it("renders the event type instead", () => {
+      const event = {
+        title: null,
+        eventType: "Opening Ceremony",
+        dateTimeRange: "noon to 1:00",
+        description: "This is going to be a fun event!",
+      }
+
+      const events = [event]
+
+      const wrapper = getWrapper({
+        Show: () => ({ events }),
+        Partner: () => ({ type: "Gallery" }),
+      })
+
+      expect(wrapper.text()).toContain("Opening Ceremony")
+    })
+  })
+
+  describe("with a null event", () => {
+    it("skips rendering that event", () => {
+      const event = null
+
+      const events = [event]
+
+      const wrapper = getWrapper({
+        Show: () => ({ events }),
+        Partner: () => ({ type: "Gallery" }),
+      })
+
+      expect(wrapper.find("EventList")).toHaveLength(1)
+      const eventList = wrapper.find("EventList")
+      expect(eventList.find("h3")).toHaveLength(0)
+    })
   })
 })
