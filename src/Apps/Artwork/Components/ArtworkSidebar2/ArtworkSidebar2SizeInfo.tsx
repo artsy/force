acbd4c12365a51a6d023868875c49632d8bedb47
George Kartalis <g.kartalis7@gmail.com>
2022-09-14 15:44:33 +0200

    feat: add edition sets section without state functionality

---
+++ src/Apps/Artwork/Components/ArtworkSidebar2/ArtworkSidebar2SizeInfo.tsx
@@ -0,0 +1,65 @@
+import { Box, Text } from "@artsy/palette"
+import { createFragmentContainer, graphql } from "react-relay"
+import { Media } from "Utils/Responsive"
+import { ArtworkSidebar2SizeInfo_piece } from "__generated__/ArtworkSidebar2SizeInfo_piece.graphql"
+
+interface ArtworkSidebar2SizeInfoProps {
+  piece: ArtworkSidebar2SizeInfo_piece
+}
+
+const ArtworkSidebar2SizeInfo: React.FC<ArtworkSidebar2SizeInfoProps> = ({
+  piece,
+}) => {
+  const { dimensions, editionOf } = piece
+
+  const hasInDimensions = !!dimensions?.in
+  const hasCmDimensions = !!dimensions?.cm
+
+  const sizeInfoMissing = (dimensions, editionOf) => {
+    const dimensionsPresent =
+      /\d/.test(dimensions?.in) || /\d/.test(dimensions?.cm)
+
+    return !editionOf?.length && !dimensionsPresent
+  }
+
+  if (sizeInfoMissing(dimensions, editionOf)) {
+    return null
+  }
+
+  return (
+    <>
+      <Media greaterThan="sm">
+        <Box color="black60">
+          <Text variant="sm-display">
+            {hasInDimensions && dimensions.in}{" "}
+            {hasInDimensions && hasCmDimensions && "|"}{" "}
+            {hasCmDimensions && dimensions.cm}
+          </Text>
+          {editionOf && <Text variant="sm-display">{editionOf}</Text>}
+        </Box>
+      </Media>
+      <Media at="sm">
+        <Box color="black60">
+          {hasInDimensions && <Text variant="sm-display">{dimensions.in}</Text>}
+          {hasCmDimensions && <Text variant="sm-display">{dimensions.cm}</Text>}
+          {editionOf && <Text variant="sm-display">{editionOf}</Text>}
+        </Box>
+      </Media>
+    </>
+  )
+}
+
+export const ArtworkSidebar2SizeInfoFragmentContainer = createFragmentContainer(
+  ArtworkSidebar2SizeInfo,
+  {
+    piece: graphql`
+      fragment ArtworkSidebar2SizeInfo_piece on Sellable {
+        dimensions {
+          in
+          cm
+        }
+        editionOf
+      }
+    `,
+  }
+)
---
+++ src/__generated__/ArtworkSidebar2EditionSets_artwork.graphql.ts
@@ -0,0 +1,96 @@
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment } from "relay-runtime";
+import { FragmentRefs } from "relay-runtime";
+export type ArtworkSidebar2EditionSets_artwork = {
+    readonly isInquireable: boolean | null;
+    readonly isOfferable: boolean | null;
+    readonly isAcquireable: boolean | null;
+    readonly editionSets: ReadonlyArray<{
+        readonly id: string;
+        readonly isOfferable: boolean | null;
+        readonly isAcquireable: boolean | null;
+        readonly saleMessage: string | null;
+        readonly " $fragmentRefs": FragmentRefs<"ArtworkSidebar2SizeInfo_piece">;
+    } | null> | null;
+    readonly " $refType": "ArtworkSidebar2EditionSets_artwork";
+};
+export type ArtworkSidebar2EditionSets_artwork$data = ArtworkSidebar2EditionSets_artwork;
+export type ArtworkSidebar2EditionSets_artwork$key = {
+    readonly " $data"?: ArtworkSidebar2EditionSets_artwork$data | undefined;
+    readonly " $fragmentRefs": FragmentRefs<"ArtworkSidebar2EditionSets_artwork">;
+};
+
+
+
+const node: ReaderFragment = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "isOfferable",
+  "storageKey": null
+},
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "isAcquireable",
+  "storageKey": null
+};
+return {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "ArtworkSidebar2EditionSets_artwork",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "isInquireable",
+      "storageKey": null
+    },
+    (v0/*: any*/),
+    (v1/*: any*/),
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "EditionSet",
+      "kind": "LinkedField",
+      "name": "editionSets",
+      "plural": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "id",
+          "storageKey": null
+        },
+        (v0/*: any*/),
+        (v1/*: any*/),
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "saleMessage",
+          "storageKey": null
+        },
+        {
+          "args": null,
+          "kind": "FragmentSpread",
+          "name": "ArtworkSidebar2SizeInfo_piece"
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "Artwork",
+  "abstractKey": null
+};
+})();
+(node as any).hash = '7caa0d9ba122d91eaf4ccef5c6ec2741';
+export default node;
