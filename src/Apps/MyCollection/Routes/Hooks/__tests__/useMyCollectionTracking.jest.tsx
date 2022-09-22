a4b3b92ebb45cd51069da7122bac1ce95c963af8
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-09-22 15:45:16 +0200

    feat: add myCollectionTracking tests

---
+++ src/Apps/MyCollection/Routes/Hooks/__tests__/useMyCollectionTracking.jest.tsx
@@ -0,0 +1,66 @@
+import { renderHook } from "@testing-library/react-hooks"
+import { useTracking } from "react-tracking"
+import { useMyCollectionTracking } from "../useMyCollectionTracking"
+
+jest.mock("react-tracking")
+
+describe("useMyCollectionTracking", () => {
+  const mockUseTracking = useTracking as jest.Mock
+  const trackingSpy = jest.fn()
+
+  const setupHook = () => {
+    const { result } = renderHook(() => useMyCollectionTracking())
+
+    if (result.error) {
+      throw result.error
+    }
+
+    const tracking = result.current
+    return tracking
+  }
+
+  beforeAll(() => {
+    mockUseTracking.mockImplementation(() => ({ trackEvent: trackingSpy }))
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it("#deleteCollectedArtwork", () => {
+    setupHook().deleteCollectedArtwork("artwork-id", "artwork-slug")
+
+    expect(trackingSpy).toBeCalledWith({
+      action: "deleteCollectedArtwork",
+      context_module: "myCollectionArtwork",
+      context_owner_type: "myCollectionArtwork",
+      context_owner_id: "artwork-id",
+      context_owner_slug: "artwork-slug",
+      platform: "web",
+    })
+  })
+
+  it("#saveCollectedArtwork", () => {
+    setupHook().saveCollectedArtwork()
+
+    expect(trackingSpy).toBeCalledWith({
+      action: "saveCollectedArtwork",
+      context_module: "myCollectionHome",
+      context_owner_type: "myCollection",
+      platform: "web",
+    })
+  })
+
+  it("#editCollectedArtwork", () => {
+    setupHook().editCollectedArtwork("artwork-id", "artwork-slug")
+
+    expect(trackingSpy).toBeCalledWith({
+      action: "editCollectedArtwork",
+      context_module: "myCollectionArtwork",
+      context_owner_id: "artwork-id",
+      context_owner_slug: "artwork-slug",
+      context_owner_type: "myCollectionArtwork",
+      platform: "web",
+    })
+  })
+})
