79701f7877850819ae7141049a357ccd04f3a12e
Dima Tretyak <itretyak@ya.ru>
2022-09-22 15:29:35 +0300

    feat: execute mutation

--- src/Components/Notifications/MarkAllAsReadPanel.tsx
+++ src/Components/Notifications/MarkAllAsReadPanel.tsx
@@ -1,9 +1,34 @@
 import { Clickable, Flex, Text } from "@artsy/palette"
+import { useSystemContext } from "System"
+import createLogger from "Utils/logger"
+import { markAllNotificationsAsRead } from "./Mutations/markAllNotificationsAsRead"
 
 export const MARK_ALL_AS_READ_PANEL_HEIGHT = 40
 
+const logger = createLogger("MarkAllAsReadPanel")
+
 export const MarkAllAsReadPanel = () => {
-  const handleMarkAsRead = () => {}
+  const { relayEnvironment } = useSystemContext()
+
+  const markAllAsRead = async () => {
+    if (!relayEnvironment) {
+      return
+    }
+
+    try {
+      const response = await markAllNotificationsAsRead(relayEnvironment)
+      const errorMessage =
+        response.markAllNotificationsAsRead?.responseOrError?.mutationError
+          ?.message
+
+      if (errorMessage) {
+        throw new Error(errorMessage)
+      }
+    } catch (error) {
+      console.log("[debug] error", error)
+      logger.error(error)
+    }
+  }
 
   return (
     <Flex
@@ -14,7 +39,7 @@ export const MarkAllAsReadPanel = () => {
       borderBottomColor="black15"
       bg="white100"
     >
-      <Clickable onClick={handleMarkAsRead}>
+      <Clickable onClick={markAllAsRead}>
         <Text variant="xs" color="black60">
           Mark all as read
         </Text>
