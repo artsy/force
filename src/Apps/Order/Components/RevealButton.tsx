3607a5830ee3d9b24ec1686785c0d7f13efdad1e
Alican Akyuz <alicakyz@gmail.com>
2022-09-26 13:35:58 +0200

    refactor: RevealButton to FC

--- src/Apps/Order/Components/RevealButton.tsx
+++ src/Apps/Order/Components/RevealButton.tsx
@@ -1,53 +1,38 @@
 import { Button, Collapse, Flex } from "@artsy/palette"
-import { Component } from "react"
+import { FC, useState } from "react"
 
-interface RevealButtonProps {
+export const RevealButton: FC<{
   buttonLabel: string
   align?: "left" | "right"
-}
+}> = ({ align, children, buttonLabel }) => {
+  const [showing, setShowing] = useState(false)
 
-interface RevealButtonState {
-  showing: boolean
-}
-
-export class RevealButton extends Component<
-  RevealButtonProps,
-  RevealButtonState
-> {
-  state: RevealButtonState = { showing: false }
-
-  render() {
-    const { align, children, buttonLabel } = this.props
-    const { showing } = this.state
-    return (
+  return (
+    <Flex
+      flexDirection="column"
+      position="relative"
+      style={{ minHeight: "26px" }}
+    >
       <Flex
-        flexDirection="column"
-        position="relative"
-        style={{ minHeight: "26px" }}
+        position="absolute"
+        justifyContent={align === "left" ? "flex-start" : "flex-end"}
+        style={{
+          right: "0",
+          left: "0",
+          transition: "opacity 0.24s ease",
+          opacity: showing ? 0 : 1,
+          pointerEvents: showing ? "none" : "all",
+        }}
       >
-        <Flex
-          position="absolute"
-          justifyContent={align === "left" ? "flex-start" : "flex-end"}
-          style={{
-            right: "0",
-            left: "0",
-            transition: "opacity 0.24s ease",
-            opacity: showing ? 0 : 1,
-            pointerEvents: showing ? "none" : "all",
-          }}
+        <Button
+          variant="primaryGray"
+          size="small"
+          onClick={() => setShowing(true)}
         >
-          <Button
-            variant="primaryGray"
-            size="small"
-            onClick={() => {
-              this.setState({ showing: true })
-            }}
-          >
-            {buttonLabel}
-          </Button>
-        </Flex>
-        <Collapse open={this.state.showing}>{children}</Collapse>
+          {buttonLabel}
+        </Button>
       </Flex>
-    )
-  }
+      <Collapse open={showing}>{children}</Collapse>
+    </Flex>
+  )
 }
