0b771a21d163240f0d264625e45e12e6f0de0fa2
tmcmeans <tanjmcmeans01@gmail.com>
2022-09-27 12:32:50 -0500

    feat(art-quiz) hook up button to interface

--- src/Apps/ArtQuiz/ArtQuizApp.tsx
+++ src/Apps/ArtQuiz/ArtQuizApp.tsx
@@ -1,10 +1,22 @@
 import { ArtQuizContextProvider } from "Apps/ArtQuiz/ArtQuizContext/ArtQuizContext"
+import { ArtQuizWelcome } from "Components/ArtQuiz/Views/ArtQuizWelcome"
+import { useState } from "react"
 import { ArtQuizInterface } from "./ArtQuizInterface/ArtQuizInterface"
 
 export const ArtQuizApp = () => {
+  const [startQuiz, setStartQuiz] = useState(false)
+
+  const handleStartQuiz = () => {
+    setStartQuiz(true)
+  }
+
   return (
     <ArtQuizContextProvider>
-      <ArtQuizInterface />
+      {startQuiz ? (
+        <ArtQuizInterface />
+      ) : (
+        <ArtQuizWelcome onStartQuiz={handleStartQuiz} />
+      )}
     </ArtQuizContextProvider>
   )
 }
