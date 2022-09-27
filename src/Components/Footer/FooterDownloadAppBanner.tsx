53e220db2c4693419501d52d6b4d947262b1aac9
Christopher Pappas <damassi.pappas@gmail.com>
2022-09-27 13:33:20 -0700

    fix(footer): Fix mobile footer caption

--- src/Components/Footer/FooterDownloadAppBanner.tsx
+++ src/Components/Footer/FooterDownloadAppBanner.tsx
@@ -1,5 +1,5 @@
 import { ContextModule } from "@artsy/cohesion"
-import { Box, Column, GridColumns, Image, Spacer, Text } from "@artsy/palette"
+import { Column, GridColumns, Image, Spacer, Text } from "@artsy/palette"
 import { resized } from "Utils/resized"
 import { Media } from "Utils/Responsive"
 import { DownloadAppBadges } from "../DownloadAppBadges/DownloadAppBadges"
@@ -16,58 +16,62 @@ const MOBILE_COVER_IMAGE = resized(
 
 export const FooterDownloadAppBanner = () => {
   return (
-    <Box borderBottom="1px solid" borderColor="black10">
-      <GridColumns>
-        <Column
-          span={4}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          justifyContent="center"
-          order={[2, 1]}
-          px={2}
-          py={[6, 2]}
-        >
-          <Text variant="xl" textAlign="center" mb={1}>
-            Get the Artsy app
-          </Text>
+    <GridColumns
+      gridRowGap={1}
+      borderBottom="1px solid"
+      borderColor="black10"
+      pb={1}
+    >
+      <Column
+        span={4}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        order={[2, 1]}
+        px={2}
+        py={[6, 2]}
+      >
+        <Text variant="xl" textAlign="center" mb={1}>
+          Get the Artsy app
+        </Text>
 
-          <Spacer mt={2} />
+        <Spacer mt={2} />
 
-          <DownloadAppBadges contextModule={ContextModule.footer} />
-        </Column>
+        <DownloadAppBadges contextModule={ContextModule.footer} />
+      </Column>
 
-        <Column span={8} position="relative" order={[1, 2]}>
-          <Media at="xs">
-            <Image
-              src={MOBILE_COVER_IMAGE.src}
-              srcSet={MOBILE_COVER_IMAGE.srcSet}
-              height={320}
-              width="100%"
-              lazyLoad
-              alt=""
-              style={{ objectFit: "cover", objectPosition: "center top" }}
-            />
-          </Media>
+      <Column span={8} position="relative" order={[1, 2]}>
+        <Media at="xs">
+          <Image
+            src={MOBILE_COVER_IMAGE.src}
+            srcSet={MOBILE_COVER_IMAGE.srcSet}
+            height={320}
+            width="100%"
+            lazyLoad
+            alt=""
+            style={{ objectFit: "cover", objectPosition: "center top" }}
+          />
+        </Media>
 
-          <Media greaterThan="xs">
-            <Image
-              src={DESKTOP_COVER_IMAGE.src}
-              srcSet={DESKTOP_COVER_IMAGE.srcSet}
-              height={320}
-              width="100%"
-              lazyLoad
-              alt=""
-              style={{ objectFit: "cover", objectPosition: "center top" }}
-            />
-          </Media>
-        </Column>
-      </GridColumns>
-
-      <Text variant="xs" fontStyle="italic" textAlign="right" my={1}>
-        Nicolas Party, Trees, 2019. Damien Cifelli, May You Live in Interesting
-        Times, 2022. Anna Park, It's Good For You, 2020
-      </Text>
-    </Box>
+        <Media greaterThan="xs">
+          <Image
+            src={DESKTOP_COVER_IMAGE.src}
+            srcSet={DESKTOP_COVER_IMAGE.srcSet}
+            height={320}
+            width="100%"
+            lazyLoad
+            alt=""
+            style={{ objectFit: "cover", objectPosition: "center top" }}
+          />
+        </Media>
+      </Column>
+      <Column span={12} order={[1, 2]}>
+        <Text variant="xs" fontStyle="italic" textAlign="right">
+          Nicolas Party, Trees, 2019. Damien Cifelli, May You Live in
+          Interesting Times, 2022. Anna Park, It's Good For You, 2020
+        </Text>
+      </Column>
+    </GridColumns>
   )
 }
