288a3545186f281749087adc5c102aaf980a528c
Ole <ole.richter91@gmail.com>
2022-09-28 11:59:36 +0200

    fix: Decrease Consign FAQ page top spacing for mobile (#11069)
    * fix: Decrease Consign FAQ page top spacing for mobile
    
    * adjust only for eigen

--- src/Apps/Consign/Routes/MarketingLanding/FAQApp.tsx
+++ src/Apps/Consign/Routes/MarketingLanding/FAQApp.tsx
@@ -1,9 +1,14 @@
 import { Box } from "@artsy/palette"
+import { useSystemContext } from "System"
 import { FAQ } from "./Components"
 
 export const FAQApp = () => {
+  const { isEigen } = useSystemContext()
+
+  const marginTop = isEigen ? -2 : 4
+
   return (
-    <Box mt={2} mb={4}>
+    <Box mt={marginTop} mb={4}>
       <FAQ />
     </Box>
   )
