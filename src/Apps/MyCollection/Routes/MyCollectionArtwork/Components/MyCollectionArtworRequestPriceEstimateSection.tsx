21ab26c978254d49c2e3ed1cb8a43da763ed66cd
dariakoko <nargle30@gmail.com>
2022-10-05 07:17:23 +0200

    feat: add request price estimate sent section

--- src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworRequestPriceEstimateSection.tsx
+++ src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworRequestPriceEstimateSection.tsx
@@ -1,19 +1,41 @@
-import { Button, Separator, Text } from "@artsy/palette"
+import { Button, Flex, Separator, Text, WinningBidIcon } from "@artsy/palette"
+import { useState } from "react"
+import { RouterLink } from "System/Router/RouterLink"
 import { Media } from "Utils/Responsive"
 
 export const MyCollectionArtworkRequestPriceEstimateSection: React.FC<{}> = () => {
+  const [isPriceEstimteRequested, setIsPriceEstimteRequested] = useState<
+    boolean
+  >(false)
   return (
     <>
       <Separator my={2} />
-      <Text mb={0.5} variant="sm-display">
-        Get a Free Price Estimate
-      </Text>
-      <Text mb={2} color="black60">
-        This artwork is eligible for a free evaluation from an Artsy specialist.
-      </Text>
-      <Button variant="primaryBlack" width="100%">
-        Request a Price Estimate
-      </Button>
+      {isPriceEstimteRequested ? (
+        <Flex alignItems="center" flexDirection={"row"} mb={2} mt={2}>
+          <WinningBidIcon />
+          <Text variant="sm" ml={0.5}>
+            Price estimate request sent
+          </Text>
+        </Flex>
+      ) : (
+        <>
+          <Text mb={0.5} variant="sm-display">
+            Get a Free Price Estimate
+          </Text>
+          <Text mb={2} color="black60">
+            This artwork is eligible for a free evaluation from an Artsy
+            specialist.
+          </Text>
+          <RouterLink
+            onClick={() => setIsPriceEstimteRequested(true)}
+            to={null}
+          >
+            <Button variant="primaryBlack" width="100%">
+              Request a Price Estimate
+            </Button>
+          </RouterLink>
+        </>
+      )}
       <Media lessThan="sm">
         <Separator my={2} />
       </Media>
