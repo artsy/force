0dc11c31a701f9881513ce00c6bda0d15df92b99
Dima Tretyak <itretyak@ya.ru>
2022-10-11 15:30:56 +0300

    feat: add ability to pass rightContent prop

--- src/Components/TopContextBar.tsx
+++ src/Components/TopContextBar.tsx
@@ -7,17 +7,17 @@ import {
   Separator,
   Text,
   Image,
-  TextProps,
 } from "@artsy/palette"
 import { RouterLink } from "System/Router/RouterLink"
 import { cropped } from "Utils/resized"
 
-export interface TopContextBarProps extends TextProps {
+export interface TopContextBarProps {
   displayBackArrow?: boolean
   href?: string | null
   redirectTo?: string | undefined
   /** Should the biggest size image available */
   src?: string | null
+  rightContent?: React.ReactNode
   onClick?(): void
 }
 
@@ -27,8 +27,8 @@ export const TopContextBar: React.FC<TopContextBarProps> = ({
   href,
   redirectTo,
   src,
+  rightContent,
   onClick,
-  ...rest
 }) => {
   const image = src ? cropped(src, { width: 60, height: 60 }) : null
 
@@ -53,32 +53,36 @@ export const TopContextBar: React.FC<TopContextBarProps> = ({
             }
           : {})}
       >
-        {displayBackArrow && (
-          <ChevronIcon
-            direction="left"
-            height={14}
-            mr={1}
-            title=""
-            aria-hidden
-          />
-        )}
-
-        {image && (
-          <Box flexShrink={0} mr={1} borderRadius="50%" overflow="hidden">
-            <Image
-              src={image.src}
-              srcSet={image.srcSet}
-              width={30}
-              height={30}
-              alt=""
-              lazyLoad
+        <Flex flex={1} flexDirection="row" alignItems="center">
+          {displayBackArrow && (
+            <ChevronIcon
+              direction="left"
+              height={14}
+              mr={1}
+              title=""
+              aria-hidden
             />
-          </Box>
-        )}
+          )}
 
-        <Text variant="xs" lineHeight={1} lineClamp={2} {...rest}>
-          {children}
-        </Text>
+          {image && (
+            <Box flexShrink={0} mr={1} borderRadius="50%" overflow="hidden">
+              <Image
+                src={image.src}
+                srcSet={image.srcSet}
+                width={30}
+                height={30}
+                alt=""
+                lazyLoad
+              />
+            </Box>
+          )}
+
+          <Text variant="xs" lineHeight={1} lineClamp={2}>
+            {children}
+          </Text>
+        </Flex>
+
+        {rightContent}
       </Flex>
 
       <FullBleed>
