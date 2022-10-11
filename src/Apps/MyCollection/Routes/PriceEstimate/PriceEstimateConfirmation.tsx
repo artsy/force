446ac56accc4e5afaee2bf6bfe9cbacef866cd70
dariakoko <nargle30@gmail.com>
2022-10-11 12:19:48 +0200

    fix: add back button and header

--- src/Apps/MyCollection/Routes/PriceEstimate/PriceEstimateConfirmation.tsx
+++ src/Apps/MyCollection/Routes/PriceEstimate/PriceEstimateConfirmation.tsx
@@ -1,17 +1,44 @@
 import { MetaTags } from "Components/MetaTags"
 import { ConfirmationScreenComponent } from "Components/ConfirmationScreenComponent"
+import { ArtsyLogoBlackIcon, Flex, FullBleed } from "@artsy/palette"
+import { RouterLink } from "System/Router/RouterLink"
+import { AppContainer } from "Apps/Components/AppContainer"
+import { BackLink } from "Components/Links/BackLink"
+import { useRouter } from "System/Router/useRouter"
 
 export const PriceEstimateConfirmation = () => {
+  const { match } = useRouter()
+  const artworkId = match.params.artworkID
+
   return (
     <>
       <MetaTags title="Request a Price Estimate | Artsy" />
-      <ConfirmationScreenComponent
-        title="Price Estimate Request Sent"
-        subtitle="An Artsy Specialist will evaluate your artwork and contact you with a
+      <Flex my={4}>
+        <RouterLink to="/my-collection" display="block">
+          <ArtsyLogoBlackIcon display="block" />
+        </RouterLink>
+      </Flex>
+
+      <FullBleed border="1px solid" borderColor="black10" />
+
+      <AppContainer>
+        <BackLink
+          py={2}
+          mb={4}
+          width="min-content"
+          to={`/my-collection/artwork/${artworkId}`}
+        >
+          Back
+        </BackLink>
+
+        <ConfirmationScreenComponent
+          title="Price Estimate Request Sent"
+          subtitle="An Artsy Specialist will evaluate your artwork and contact you with a
       free price estimate."
-        buttonTitle="Back to My Collection"
-        routerLink="/settings/my-collection"
-      />
+          buttonTitle="Back to My Collection"
+          routerLink="/settings/my-collection"
+        />
+      </AppContainer>
     </>
   )
 }
