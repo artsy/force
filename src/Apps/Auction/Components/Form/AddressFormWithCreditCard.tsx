2a70bb3ee5d1634ac35446a32ae94701b6ccf51c
Erik <9088720+erikdstock@users.noreply.github.com>
2022-10-17 15:58:35 -0400

    update auction registration credit card note (#11181)

--- src/Apps/Auction/Components/Form/AddressFormWithCreditCard.tsx
+++ src/Apps/Auction/Components/Form/AddressFormWithCreditCard.tsx
@@ -15,14 +15,9 @@ export const AddressFormWithCreditCard: React.FC = () => {
   return (
     <Join separator={<Spacer my={2} />}>
       <Text variant="sm-display">
+        Registration is free. A valid credit card is required in order to bid.
         Please enter your credit card information below. The name on your Artsy
-        account must match the name on the card, and a valid credit card is
-        required in order to bid.
-      </Text>
-
-      <Text variant="sm-display">
-        Registration is free. Artsy will never charge this card without your
-        permission, and you are not required to use this card to pay if you win.
+        account must match the name on the card.
       </Text>
 
       <Text variant="sm-display">Credit Card</Text>
