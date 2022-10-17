800e2ee07f1f0c638d9d2993485ce8f9b968a987
dariakoko <nargle30@gmail.com>
2022-10-17 18:38:55 +0200

    fix: adjust the ui of the confirmation screen and component

--- src/Components/ConfirmationScreenComponent.tsx
+++ src/Components/ConfirmationScreenComponent.tsx
@@ -1,31 +1,49 @@
 import { Text, Flex, Button } from "@artsy/palette"
+import React, { ReactElement } from "react"
 import { RouterLink } from "System/Router/RouterLink"
 
 export const ConfirmationScreenComponent: React.FC<{
   title: string
-  subtitle: string
-  buttonText: string
-  routerLink: string
-}> = ({ title, subtitle, buttonText, routerLink }) => {
+  subtitle?: string
+  customSubtitle?: ReactElement
+  buttonText?: string
+  routerLink?: string
+  extraButtonText?: string
+  extraRouterLink?: string
+}> = ({ title, subtitle, customSubtitle, buttonText, routerLink }) => {
   return (
     <>
-      <Text variant="xl" mt={4}>
+      <Text variant="lg" mt={4}>
         {title}
       </Text>
-      <Text variant="md" color="black60" mt={1} mb={6}>
-        {subtitle}
-      </Text>
 
-      <Flex
-        flexDirection={["column", "row"]}
-        alignItems={["stretch", "center"]}
-      >
-        <RouterLink to={routerLink}>
-          <Button width={["100%", "auto"]} size="large" variant="primaryBlack">
-            {buttonText}
-          </Button>
-        </RouterLink>
-      </Flex>
+      {!!subtitle && (
+        <Text variant="sm-display" color="black60" mt={1} mb={6}>
+          {subtitle}
+        </Text>
+      )}
+      {!!customSubtitle && (
+        <Text variant="sm-display" color="black60" mt={1} mb={6} maxWidth={720}>
+          {customSubtitle}
+        </Text>
+      )}
+
+      {!!routerLink && (
+        <Flex
+          flexDirection={["column", "row"]}
+          alignItems={["stretch", "center"]}
+        >
+          <RouterLink to={routerLink}>
+            <Button
+              width={["100%", "auto"]}
+              size="large"
+              variant="primaryBlack"
+            >
+              {buttonText}
+            </Button>
+          </RouterLink>
+        </Flex>
+      )}
     </>
   )
 }
