d93e22ecc6c2e751744daf7e1350081960ab1e7f
dariakoko <nargle30@gmail.com>
2022-10-18 14:17:30 +0200

    fix: remove back button

--- src/Apps/MyCollection/Routes/PriceEstimate/PriceEstimateConfirmation.tsx
+++ src/Apps/MyCollection/Routes/PriceEstimate/PriceEstimateConfirmation.tsx
@@ -1,9 +1,8 @@
 import { MetaTags } from "Components/MetaTags"
 import { ConfirmationScreenComponent } from "Components/ConfirmationScreenComponent"
-import { ArtsyLogoBlackIcon, Flex, FullBleed } from "@artsy/palette"
+import { ArtsyLogoBlackIcon, Flex } from "@artsy/palette"
 import { RouterLink } from "System/Router/RouterLink"
 import { AppContainer } from "Apps/Components/AppContainer"
-import { BackLink } from "Components/Links/BackLink"
 import { useRouter } from "System/Router/useRouter"
 
 export const PriceEstimateConfirmation = () => {
@@ -19,18 +18,7 @@ export const PriceEstimateConfirmation = () => {
         </RouterLink>
       </Flex>
 
-      <FullBleed border="1px solid" borderColor="black10" />
-
       <AppContainer>
-        <BackLink
-          py={2}
-          mb={4}
-          width="min-content"
-          to={`/my-collection/artwork/${artworkId}`}
-        >
-          Back
-        </BackLink>
-
         <ConfirmationScreenComponent
           title="Price Estimate Request Sent"
           subtitle="An Artsy Specialist will evaluate your artwork and contact you with a
