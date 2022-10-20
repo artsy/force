e1367b09c4b52959378508d6fdc63ff789b03a52
Mounir Dhahri <soussouheros@gmail.com>
2022-10-20 10:13:36 +0200

    feat: add subtitle

--- src/Apps/MyCollectionInsights/Routes/MyCollectionInsightsMedianSalePriceAtAuction/MyCollectionInsightsMedianSalePriceAtAuction.tsx
+++ src/Apps/MyCollectionInsights/Routes/MyCollectionInsightsMedianSalePriceAtAuction/MyCollectionInsightsMedianSalePriceAtAuction.tsx
@@ -6,6 +6,9 @@ export const MyCollectionInsightsMedianSalePriceAtAuction = () => {
       <Text mt={2} variant={"lg-display"}>
         Median Auction Price
       </Text>
+      <Text variant={["sm-display", "xs"]} color="black60">
+        Track price stability or growth of your artists
+      </Text>
     </Box>
   )
 }
