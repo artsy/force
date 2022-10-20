c96988b3ba0cc9d674dde514e5e76c04d51d94b8
George Kartalis <g.kartalis7@gmail.com>
2022-10-20 17:56:21 +0200

    refactor: remove inlined render function

--- src/Apps/Artwork/Components/ArtworkSidebar2/ArtworkSidebar2CommercialButtons.tsx
+++ src/Apps/Artwork/Components/ArtworkSidebar2/ArtworkSidebar2CommercialButtons.tsx
@@ -278,20 +278,10 @@ const ArtworkSidebar2CommerialButtons: React.FC<ArtworkSidebar2CommercialButtons
   const shouldRenderButtons =
     artworkEcommerceAvailable || !!artwork.isInquireable
 
-  const createAlertAvailable =
+  const isCreateAlertAvailable =
     artwork.isSold && (artwork?.artists?.length ?? 0) > 0
   const isSecondaryContactGalleryButton =
-    artwork.isOfferable || createAlertAvailable
-
-  const renderCreateAlertButton = () => {
-    if (artwork.artists?.length === 0) {
-      return null
-    }
-
-    return (
-      <ArtworkSidebarCreateAlertButtonFragmentContainer artwork={artwork} />
-    )
-  }
+    artwork.isOfferable || isCreateAlertAvailable
 
   return (
     <>
@@ -300,7 +290,7 @@ const ArtworkSidebar2CommerialButtons: React.FC<ArtworkSidebar2CommercialButtons
       {(artwork?.editionSets?.length ?? 0) < 2 ? (
         <>
           <SaleMessage saleMessage={artwork.saleMessage} />
-          {createAlertAvailable && <Spacer mt={1} />}
+          {!!isCreateAlertAvailable && <Spacer mt={1} />}
         </>
       ) : (
         <>
@@ -321,15 +311,15 @@ const ArtworkSidebar2CommerialButtons: React.FC<ArtworkSidebar2CommercialButtons
         </>
       )}
 
-      {shouldRenderButtons && (
-        <>
-          <Spacer mt={2} />
-        </>
-      )}
+      {shouldRenderButtons && <Spacer mt={2} />}
 
       <Flex flexDirection={["column", "column", "column", "column", "row"]}>
         <Join separator={<Spacer ml={1} mt={1} />}>
-          {artwork.isSold && renderCreateAlertButton()}
+          {!!isCreateAlertAvailable && (
+            <ArtworkSidebarCreateAlertButtonFragmentContainer
+              artwork={artwork}
+            />
+          )}
           {artwork.isAcquireable && (
             <Button
               width="100%"
