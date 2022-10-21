153a9641cbbb665b2e44c51653af28286b7a16eb
Laura Bhayani <laura.bhayani@artsymail.com>
2022-10-21 13:58:54 -0400

    swap render

--- src/Apps/ArtQuiz/__tests__/ArtQuizResults.jest.tsx
+++ src/Apps/ArtQuiz/__tests__/ArtQuizResults.jest.tsx
@@ -1,39 +1,6 @@
 import { ArtQuizResults } from "Apps/ArtQuiz/ArtQuizResults"
-import { render, screen } from "@testing-library/react"
-
-const translationFileMock = {
-  artQuizPage: {
-    results: {
-      emailButton: "Email My Results",
-      title: "Explore Your Quiz Results",
-      subtitle:
-        "Explore these collections and artists recommended for you based on your saved works. Follow them to see their latest works on your Artsy home.",
-      tabs: {
-        worksYouLiked: "Works You Liked",
-        recommendedCollections: "Recommended Collections",
-        recommendedArtists: "Recommended Artists",
-      },
-    },
-  },
-}
-
-jest.mock("react-i18next", () => {
-  return {
-    useTranslation: () => ({
-      t: (dotNotation: string) => {
-        const [page, ...nestedKeys] = dotNotation.split(".")
-
-        let temp: Record<string, any> = { ...translationFileMock[page] }
-
-        nestedKeys.forEach(key => {
-          temp = temp[key]
-        })
-
-        return (temp as unknown) as string
-      },
-    }),
-  }
-})
+import { screen } from "@testing-library/react"
+import { render } from "DevTools/setupTestWrapper"
 
 describe("ArtQuizResults", () => {
   it.each([
