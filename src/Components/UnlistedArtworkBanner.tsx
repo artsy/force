2edc448bf198ef4d30a8b83a945a8d19e4000ecc
Nicholas Spinazze <nspinazz89@gmail.com>
2022-10-26 12:47:55 -0500

    WIP: add modal to unlisted banner

--- src/Components/UnlistedArtworkBanner.tsx
+++ src/Components/UnlistedArtworkBanner.tsx
@@ -1,41 +1,51 @@
 import { createFragmentContainer, graphql } from "react-relay"
-import { Button, ModalDialog, Text, Clickable } from "@artsy/palette"
+import { Text, Clickable } from "@artsy/palette"
 import { useState } from "react"
 import { FullBleedBanner } from "Components/FullBleedBanner"
 import { UnlistedArtworkBanner_partner$data } from "/__generated__/UnlistedArtworkBanner_partner.graphql"
+import { UnlistedBannerModal } from "Apps/Artwork/Components/UnlistedBannerModal"
 
 interface UnlistedArtworkBannerProps {
-  partner: UnlistedArtworkBanner_partner$data
-  show: boolean
-  onClose(): void
+  partner?: UnlistedArtworkBanner_partner$data
 }
 
 const UnlistedArtworkBanner: React.FC<UnlistedArtworkBannerProps> = ({
   partner,
-  show,
-  onClose,
 }) => {
   const [open, setOpen] = useState(false)
 
   return (
-    <FullBleedBanner variant="brand">
-      {partner.name ? (
-        <Text>
-          This is a{" "}
-          <Clickable
-            textDecoration="underline"
-            onClick={() => {
-              setOpen(true)
-            }}
-          >
-            private listing
-          </Clickable>{" "}
-          from {partner.name}
-        </Text>
-      ) : (
-        "This is a private listing"
-      )}
-    </FullBleedBanner>
+    <>
+      <FullBleedBanner variant="brand">
+        {partner?.name ? (
+          <Text>
+            This is a{" "}
+            <Clickable
+              textDecoration="underline"
+              onClick={() => {
+                setOpen(true)
+              }}
+            >
+              private listing
+            </Clickable>{" "}
+            from {partner.name}.
+          </Text>
+        ) : (
+          <Text>
+            This is a{" "}
+            <Clickable
+              textDecoration="underline"
+              onClick={() => {
+                setOpen(true)
+              }}
+            >
+              private listing.
+            </Clickable>
+          </Text>
+        )}
+      </FullBleedBanner>
+      <UnlistedBannerModal show={open} onClose={() => setOpen(false)} />
+    </>
   )
 }
 
