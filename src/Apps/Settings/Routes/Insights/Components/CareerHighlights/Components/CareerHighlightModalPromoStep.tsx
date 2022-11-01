efb8fc918b5b247e7ab13eb10492586a6ba69fb2
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-11-01 16:12:45 +0100

    fix: update promo card design

--- src/Apps/Settings/Routes/Insights/Components/CareerHighlights/Components/CareerHighlightModalPromoStep.tsx
+++ src/Apps/Settings/Routes/Insights/Components/CareerHighlights/Components/CareerHighlightModalPromoStep.tsx
@@ -5,11 +5,14 @@ import {
   Button,
   ResponsiveBox,
   Image,
+  ArtsyLogoBlackIcon,
+  Box,
 } from "@artsy/palette"
 import { RouterLink } from "System/Router/RouterLink"
 import { cropped } from "Utils/resized"
+import { Media } from "Utils/Responsive"
 
-const { src, srcSet } = cropped(
+const { src: mSrc, srcSet: mSrcSet } = cropped(
   "https://files.artsy.net/images/CareerHighlightModalPromoImageMobile.png",
   {
     height: 900,
@@ -17,10 +20,73 @@ const { src, srcSet } = cropped(
   }
 )
 
-export const CareerHighlightModalPromoStep = () => {
+const { src: dSrc, srcSet: dSrcSet } = cropped(
+  "https://files.artsy.net/images/CareerHighlightModalPromoImage.png",
+  {
+    height: 1238,
+    width: 798,
+  }
+)
+
+const CareerHighlightModalPromoStepDesktop = () => {
   return (
-    <Flex flex={1} flexDirection="column" mx={[0, 1]}>
-      <Text variant={["lg-display", "lg"]}>
+    <Flex flex={1} flexDirection="column" flexGrow={1}>
+      <Box mb={2}>
+        <ArtsyLogoBlackIcon />
+      </Box>
+
+      <Flex overflowY="auto">
+        <ResponsiveBox
+          aspectWidth={798}
+          aspectHeight={1238}
+          maxWidth="100%"
+          bg="black10"
+          mr={1}
+        >
+          <Image
+            lazyLoad
+            src={dSrc}
+            srcSet={dSrcSet}
+            alt="Artwork called '25 Cats Name Sam and One Blue Pussy' by Andy Warhol"
+          />
+        </ResponsiveBox>
+
+        <Spacer mr={4} />
+
+        <Flex flexDirection="column">
+          <Text variant={"lg-display"}>
+            Discover Career Highlights for Your Artists
+          </Text>
+
+          <Spacer mt={2} />
+
+          <Text variant="sm-display">
+            Add artworks to reveal career highlights for your artists.
+          </Text>
+
+          <Spacer mt={4} />
+
+          <Button
+            // @ts-ignore
+            as={RouterLink}
+            width="100%"
+            variant="primaryBlack"
+            size="large"
+            to="/my-collection/artworks/new"
+            py={1}
+          >
+            Upload Artwork
+          </Button>
+        </Flex>
+      </Flex>
+    </Flex>
+  )
+}
+
+const CareerHighlightModalPromoStepMobile = () => {
+  return (
+    <Flex flex={1} flexDirection="column">
+      <Text variant="lg-display">
         Discover Career Highlights for Your Artists
       </Text>
 
@@ -40,11 +106,12 @@ export const CareerHighlightModalPromoStep = () => {
         size="large"
         to="/my-collection/artworks/new"
         py={1}
+        mx={2}
       >
         Upload Artwork
       </Button>
 
-      <Flex mt={4} overflowY="auto">
+      <Flex flexGrow={1} mt={4} overflowY="auto">
         <ResponsiveBox
           aspectWidth={750}
           aspectHeight={900}
@@ -53,12 +120,26 @@ export const CareerHighlightModalPromoStep = () => {
         >
           <Image
             lazyLoad
-            src={src}
-            srcSet={srcSet}
+            src={mSrc}
+            srcSet={mSrcSet}
             alt="Artwork called '25 Cats Name Sam and One Blue Pussy' by Andy Warhol"
           />
         </ResponsiveBox>
       </Flex>
     </Flex>
+  )
+}
+
+export const CareerHighlightModalPromoStep = () => {
+  return (
+    <>
+      <Media greaterThanOrEqual="sm">
+        <CareerHighlightModalPromoStepDesktop />
+      </Media>
+
+      <Media lessThan="sm">
+        <CareerHighlightModalPromoStepMobile />
+      </Media>
+    </>
   )
 }
