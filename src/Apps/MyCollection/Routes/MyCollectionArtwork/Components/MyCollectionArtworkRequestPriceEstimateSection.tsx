48bbb690646be969f43d7ab73baf969848bf61e8
Ole <ole.richter91@gmail.com>
2022-11-01 15:49:49 +0100

    fix: Always show price estimate sent (#11279)

--- src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworkRequestPriceEstimateSection.tsx
+++ src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworkRequestPriceEstimateSection.tsx
@@ -11,36 +11,48 @@ interface MyCollectionArtworkRequestPriceEstimateSectionProps {
 export const MyCollectionArtworkRequestPriceEstimateSection: React.FC<MyCollectionArtworkRequestPriceEstimateSectionProps> = ({
   artwork,
 }) => {
-  return (
-    <>
-      <Separator my={2} />
-      {artwork.hasPriceEstimateRequest ? (
+  const isP1Artist = artwork.artist?.targetSupply?.isP1
+  const isAlreadySubmitted = artwork.submissionId
+
+  if (artwork.hasPriceEstimateRequest) {
+    return (
+      <>
+        <Separator my={2} />
         <Flex alignItems="center" flexDirection={"row"} mb={2} mt={2}>
           <WinningBidIcon />
           <Text variant="sm" ml={0.5}>
             Price estimate request sent
           </Text>
         </Flex>
-      ) : (
-        <>
-          <Text mb={0.5} variant="sm-display">
-            Get a Free Price Estimate
-          </Text>
-          <Text mb={2} color="black60" variant="xs">
-            This artwork is eligible for a free evaluation from an Artsy
-            specialist.
-          </Text>
-          <RouterLink
-            to={`/my-collection/artwork/${artwork.internalID}/price-estimate`}
-            textDecoration="none"
-            display="block"
-          >
-            <Button variant="primaryBlack" width="100%">
-              Request a Price Estimate
-            </Button>
-          </RouterLink>
-        </>
-      )}
+        <Media lessThan="sm">
+          <Separator my={2} />
+        </Media>
+      </>
+    )
+  }
+
+  if (!isP1Artist || isAlreadySubmitted) {
+    return null
+  }
+
+  return (
+    <>
+      <Separator my={2} />
+      <Text mb={0.5} variant="sm-display">
+        Get a Free Price Estimate
+      </Text>
+      <Text mb={2} color="black60" variant="xs">
+        This artwork is eligible for a free evaluation from an Artsy specialist.
+      </Text>
+      <RouterLink
+        to={`/my-collection/artwork/${artwork.internalID}/price-estimate`}
+        textDecoration="none"
+        display="block"
+      >
+        <Button variant="primaryBlack" width="100%">
+          Request a Price Estimate
+        </Button>
+      </RouterLink>
       <Media lessThan="sm">
         <Separator my={2} />
       </Media>
@@ -53,8 +65,14 @@ export const MyCollectionArtworkRequestPriceEstimateSectionFragmentContainer = c
   {
     artwork: graphql`
       fragment MyCollectionArtworkRequestPriceEstimateSection_artwork on Artwork {
-        internalID
+        artist {
+          targetSupply {
+            isP1
+          }
+        }
         hasPriceEstimateRequest
+        internalID
+        submissionId
       }
     `,
   }
