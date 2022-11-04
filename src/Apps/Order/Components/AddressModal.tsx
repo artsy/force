7a2428e7ec37ea9304fb584b1ac506ee788860c7
rquartararo <rquartararo@gmail.com>
2022-11-04 09:46:02 +0100

    feat: update address modal and delete dialog

--- src/Apps/Order/Components/AddressModal.tsx
+++ src/Apps/Order/Components/AddressModal.tsx
@@ -7,7 +7,7 @@ import {
   Dialog,
   Flex,
   Input,
-  Modal,
+  ModalDialog,
   ModalWidth,
   Spacer,
   Text,
@@ -18,19 +18,19 @@ import {
 import {
   SavedAddressType,
   convertShippingAddressToMutationInput,
-} from "../Utils/shippingUtils"
+} from "Apps/Order/Utils/shippingUtils"
 import { Formik, FormikHelpers, FormikProps } from "formik"
 import {
   removeEmptyKeys,
   validateAddress,
   validatePhoneNumber,
-} from "../Utils/formValidators"
-import { updateUserAddress } from "../Mutations/UpdateUserAddress"
+} from "Apps/Order/Utils/formValidators"
+import { updateUserAddress } from "Apps/Order/Mutations/UpdateUserAddress"
 import { createUserAddress } from "Apps/Order/Mutations/CreateUserAddress"
 import { SavedAddresses_me$data } from "__generated__/SavedAddresses_me.graphql"
 import { AddressModalFields } from "Components/Address/AddressModalFields"
 import { useSystemContext } from "System/SystemContext"
-import { updateUserDefaultAddress } from "../Mutations/UpdateUserDefaultAddress"
+import { updateUserDefaultAddress } from "Apps/Order/Mutations/UpdateUserDefaultAddress"
 import { UpdateUserAddressMutation$data } from "__generated__/UpdateUserAddressMutation.graphql"
 import { CreateUserAddressMutation$data } from "__generated__/CreateUserAddressMutation.graphql"
 import { countries } from "Utils/countries"
@@ -106,207 +106,220 @@ export const AddressModal: React.FC<Props> = ({
 
   return (
     <>
-      <Modal
-        title={title}
-        show={show}
-        onClose={handleModalClose}
-        modalWidth={ModalWidth.Wide}
-      >
-        <Formik
-          validateOnMount
-          initialValues={createMutation ? { country: "US" } : { ...address }}
-          validate={validator}
-          onSubmit={(
-            values: SavedAddressType,
-            actions: FormikHelpers<SavedAddressType>
-          ) => {
-            const handleError = message => {
-              const userMessage: Record<string, string> | null =
-                SERVER_ERROR_MAP[message]
-              if (userMessage) {
-                actions.setFieldError(userMessage.field, userMessage.message)
-              } else {
-                setCreateUpdateError(GENERIC_FAIL_MESSAGE)
-              }
-              actions?.setSubmitting(false)
-              onError && onError(message)
-            }
-
-            const handleSuccess = savedAddress => {
-              // update default address only if isDefault changed or new
-              // address marked ad default
-              if (
-                values?.isDefault &&
-                values?.isDefault !== address?.isDefault
-              ) {
-                updateUserDefaultAddress(
-                  relayEnvironment,
-                  savedAddress?.createUserAddress?.userAddressOrErrors
-                    ?.internalID || address?.internalID,
-                  () => {
-                    onSuccess(savedAddress)
-                  },
-                  onError
-                )
-              } else {
-                onSuccess && onSuccess(savedAddress)
-              }
-
-              setCreateUpdateError(null)
-            }
-            const addressInput = convertShippingAddressToMutationInput(values)
-            if (createMutation) {
-              createUserAddress(
-                relayEnvironment,
-                addressInput,
-                handleSuccess,
-                handleError,
-                me,
-                closeModal
-              )
-            } else {
-              if (address?.internalID) {
-                updateUserAddress(
-                  relayEnvironment,
-                  address.internalID,
-                  addressInput,
-                  closeModal,
-                  handleSuccess,
-                  handleError
-                )
-              }
-            }
-          }}
+      {show && (
+        <ModalDialog
+          title={title}
+          onClose={() => handleModalClose()}
+          width="900px"
         >
-          {(formik: FormikProps<SavedAddressType>) => (
-            <form onSubmit={formik.handleSubmit}>
-              {createUpdateError && (
-                <Banner my={2} data-test="credit-card-error" variant="error">
-                  {createUpdateError}
-                </Banner>
-              )}
-              <AddressModalFields />
-              <Spacer mb={2} />
-              {user && !userHasLabFeature(user, "Phone Number Validation") && (
-                <Input
-                  title="Phone number"
-                  description="Required for shipping logistics"
-                  placeholder="Add phone number"
-                  name="phoneNumber"
-                  type="tel"
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  error={
-                    formik.touched.phoneNumber && formik.errors.phoneNumber
-                  }
-                  value={formik.values?.phoneNumber || ""}
-                  data-test="phoneInputWithoutValidationFlag"
-                />
-              )}
-              {user && userHasLabFeature(user, "Phone Number Validation") && (
-                <Flex>
-                  <Box style={{ maxWidth: "35%" }}>
-                    <Select
-                      title="Phone number"
-                      description="Only used for shipping purposes"
-                      options={countries}
-                      onSelect={cc => {
-                        setCountryCode(cc)
-                      }}
-                      style={{
-                        letterSpacing: "1px",
-                        borderRight: "none",
-                      }}
-                      data-test="countryDropdown"
-                    />
-                  </Box>
-                  <Flex
-                    flexDirection="column"
-                    style={{
-                      width: "100%",
-                    }}
-                  >
-                    <Box height="100%"></Box>
+          <Formik
+            validateOnMount
+            initialValues={createMutation ? { country: "US" } : { ...address }}
+            validate={validator}
+            onSubmit={(
+              values: SavedAddressType,
+              actions: FormikHelpers<SavedAddressType>
+            ) => {
+              const handleError = message => {
+                const userMessage: Record<string, string> | null =
+                  SERVER_ERROR_MAP[message]
+                if (userMessage) {
+                  actions.setFieldError(userMessage.field, userMessage.message)
+                } else {
+                  setCreateUpdateError(GENERIC_FAIL_MESSAGE)
+                }
+                actions?.setSubmitting(false)
+                onError && onError(message)
+              }
+
+              const handleSuccess = savedAddress => {
+                // update default address only if isDefault changed or new
+                // address marked ad default
+                if (
+                  values?.isDefault &&
+                  values?.isDefault !== address?.isDefault
+                ) {
+                  updateUserDefaultAddress(
+                    relayEnvironment,
+                    savedAddress?.createUserAddress?.userAddressOrErrors
+                      ?.internalID || address?.internalID,
+                    () => {
+                      onSuccess(savedAddress)
+                    },
+                    onError
+                  )
+                } else {
+                  onSuccess && onSuccess(savedAddress)
+                }
+
+                setCreateUpdateError(null)
+              }
+              const addressInput = convertShippingAddressToMutationInput(values)
+              if (createMutation) {
+                createUserAddress(
+                  relayEnvironment,
+                  addressInput,
+                  handleSuccess,
+                  handleError,
+                  me,
+                  closeModal
+                )
+              } else {
+                if (address?.internalID) {
+                  updateUserAddress(
+                    relayEnvironment,
+                    address.internalID,
+                    addressInput,
+                    closeModal,
+                    handleSuccess,
+                    handleError
+                  )
+                }
+              }
+            }}
+          >
+            {(formik: FormikProps<SavedAddressType>) => (
+              <form onSubmit={formik.handleSubmit}>
+                {createUpdateError && (
+                  <Banner my={2} data-test="credit-card-error" variant="error">
+                    {createUpdateError}
+                  </Banner>
+                )}
+                <AddressModalFields />
+                <Spacer mb={2} />
+                {user &&
+                  !userHasLabFeature(user, "Phone Number Validation") && (
                     <Input
-                      title=""
-                      description=""
-                      placeholder={"Add phone number"}
+                      title="Phone number"
+                      description="Required for shipping logistics"
+                      placeholder="Add phone number"
                       name="phoneNumber"
                       type="tel"
                       onChange={formik.handleChange}
-                      onBlur={e => {
-                        formik.handleBlur
-                      }}
+                      onBlur={formik.handleBlur}
                       error={
                         formik.touched.phoneNumber && formik.errors.phoneNumber
                       }
-                      value={formik.values?.phoneNumber ?? ""}
-                      style={{ borderLeft: "none" }}
+                      value={formik.values?.phoneNumber || ""}
+                      data-test="phoneInputWithoutValidationFlag"
                     />
+                  )}
+                {user && userHasLabFeature(user, "Phone Number Validation") && (
+                  <Flex>
+                    <Box style={{ maxWidth: "35%" }}>
+                      <Select
+                        title="Phone number"
+                        description="Only used for shipping purposes"
+                        options={countries}
+                        onSelect={cc => {
+                          setCountryCode(cc)
+                        }}
+                        style={{
+                          letterSpacing: "1px",
+                          borderRight: "none",
+                        }}
+                        data-test="countryDropdown"
+                      />
+                    </Box>
+                    <Flex
+                      flexDirection="column"
+                      style={{
+                        width: "100%",
+                      }}
+                    >
+                      <Box height="100%"></Box>
+                      <Input
+                        title=""
+                        description=""
+                        placeholder={"Add phone number"}
+                        name="phoneNumber"
+                        type="tel"
+                        onChange={formik.handleChange}
+                        onBlur={e => {
+                          formik.handleBlur
+                        }}
+                        error={
+                          formik.touched.phoneNumber &&
+                          formik.errors.phoneNumber
+                        }
+                        value={formik.values?.phoneNumber ?? ""}
+                        style={{ borderLeft: "none" }}
+                      />
+                    </Flex>
                   </Flex>
-                </Flex>
-              )}
-              <Spacer mb={2} />
-              {(!address?.isDefault || createMutation) && (
-                <Checkbox
-                  onSelect={selected => {
-                    formik.setFieldValue("isDefault", selected)
-                  }}
-                  selected={formik.values?.isDefault}
-                  data-test="setAsDefault"
-                >
-                  Set as default
-                </Checkbox>
-              )}
-              {!createMutation && (
-                <Flex mt={2} flexDirection="column" alignItems="center">
-                  <Clickable
-                    data-test="deleteButton"
-                    onClick={() => setShowDialog(true)}
+                )}
+                <Spacer mb={2} />
+                {(!address?.isDefault || createMutation) && (
+                  <Checkbox
+                    onSelect={selected => {
+                      formik.setFieldValue("isDefault", selected)
+                    }}
+                    selected={formik.values?.isDefault}
+                    data-test="setAsDefault"
                   >
-                    <Text variant="xs" color="red100">
-                      Delete address
-                    </Text>
-                  </Clickable>
-                </Flex>
-              )}
-              <Button
-                data-test="saveButton"
-                type="submit"
-                variant="primaryBlack"
-                loading={formik.isSubmitting}
-                disabled={Object.keys(formik.errors).length > 0}
-                width="100%"
-                mt={2}
-              >
-                Save
-              </Button>
-            </form>
-          )}
-        </Formik>
-      </Modal>
-      <Dialog
-        title="Delete address?"
-        detail="This will remove this address from your saved addresses."
-        show={showDialog}
-        primaryCta={{
-          action: () => {
-            setShowDialog(false)
-            closeModal()
-            if (address?.internalID) {
-              onDeleteAddress(address.internalID)
-            }
-          },
-          text: "Delete",
-        }}
-        secondaryCta={{
-          action: () => {
-            setShowDialog(false)
-          },
-          text: "Cancel",
-        }}
-        onClose={() => setShowDialog(false)}
-      />
+                    Set as default
+                  </Checkbox>
+                )}
+                {!createMutation && (
+                  <Flex mt={2} flexDirection="column" alignItems="center">
+                    <Clickable
+                      data-test="deleteButton"
+                      onClick={() => setShowDialog(true)}
+                    >
+                      <Text variant="xs" color="red100">
+                        Delete address
+                      </Text>
+                    </Clickable>
+                  </Flex>
+                )}
+                <Button
+                  data-test="saveButton"
+                  type="submit"
+                  variant="primaryBlack"
+                  loading={formik.isSubmitting}
+                  disabled={Object.keys(formik.errors).length > 0}
+                  width="100%"
+                  mt={2}
+                >
+                  Save
+                </Button>
+              </form>
+            )}
+          </Formik>
+        </ModalDialog>
+      )}
+      {showDialog && (
+        <ModalDialog
+          title="Delete address?"
+          onClose={() => setShowDialog(false)}
+          width="350px"
+        >
+          <Text>This will remove this address from your saved addressess.</Text>
+          <Spacer mb={2} />
+          <Flex justifyContent="flex-end">
+            <Button
+              variant="secondaryNeutral"
+              size="small"
+              onClick={() => setShowDialog(false)}
+            >
+              Cancel
+            </Button>
+            <Spacer mr={1} />
+            <Button
+              size="small"
+              onClick={() => {
+                setShowDialog(false)
+                closeModal()
+                if (address?.internalID) {
+                  onDeleteAddress(address.internalID)
+                }
+              }}
+            >
+              Detete
+            </Button>
+          </Flex>
+        </ModalDialog>
+      )}
     </>
   )
 }
