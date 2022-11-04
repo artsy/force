ae9cb1c93f10646dedc67adb235cdbf57b487126
George Kartalis <g.kartalis7@gmail.com>
2022-11-04 11:00:54 +0100

    fix: copy changes on empty activity panel

--- src/Components/Notifications/__tests__/NotificationsEmptyStateByType.jest.tsx
+++ src/Components/Notifications/__tests__/NotificationsEmptyStateByType.jest.tsx
@@ -5,7 +5,7 @@ describe("NotificationsEmptyStateByType", () => {
   it("should render correct state when type is 'All'", () => {
     render(<NotificationsEmptyStateByType type="all" />)
 
-    const title = "You haven't followed any artists, galleries or fairs yet."
+    const title = "You haven’t followed any artists, galleries or fairs yet."
     const message =
       "Follow artists to keep track of their latest work and career highlights. Following artists helps Artsy to recommend works you might like."
 
@@ -16,9 +16,9 @@ describe("NotificationsEmptyStateByType", () => {
   it("should render correct state when type is 'Alerts'", () => {
     render(<NotificationsEmptyStateByType type="alerts" />)
 
-    const title = "Set alerts for artworks you're hunting for."
+    const title = "Set alerts for artworks you’re seeking."
     const message =
-      'Filter for the artworks you love on an artist page and tap "Create Alert". Get notifications here when there\'s a match.'
+      "Filter for the artworks you love on an artist page and tap “Create Alert.” Get notifications here when there’s a match."
 
     expect(screen.getByText(title)).toBeInTheDocument()
     expect(screen.getByText(message)).toBeInTheDocument()
