51b901d2b3851a2e2dd874f24c12838806d154bb
dzucconi <mail@damonzucconi.com>
2022-11-08 16:47:05 -0500

    fix(artist): temporarily removes sticky zero state while investigating infinite loop render bug

--- src/Apps/Artist/Routes/WorksForSale/Components/ZeroState.tsx
+++ src/Apps/Artist/Routes/WorksForSale/Components/ZeroState.tsx
@@ -2,7 +2,6 @@ import { Box, BoxProps, Message } from "@artsy/palette"
 import { isEmpty } from "lodash"
 import { useArtworkFilterContext } from "Components/ArtworkFilter/ArtworkFilterContext"
 import { ArtworkGridEmptyState } from "Components/ArtworkGrid/ArtworkGridEmptyState"
-import { Sticky } from "Components/Sticky"
 
 export const ZeroState: React.FC<BoxProps> = props => {
   const { selectedFiltersCounts, resetFilters } = useArtworkFilterContext()
@@ -14,18 +13,9 @@ export const ZeroState: React.FC<BoxProps> = props => {
 
   return (
     <Box width="100%" {...props}>
-      <Sticky>
-        {({ stuck }) => {
-          return (
-            <Box pt={stuck ? 1 : 0}>
-              <Message title="No works available by the artist at this time">
-                Create an Alert to receive notifications when new works are
-                added
-              </Message>
-            </Box>
-          )
-        }}
-      </Sticky>
+      <Message title="No works available by the artist at this time">
+        Create an Alert to receive notifications when new works are added
+      </Message>
     </Box>
   )
 }
