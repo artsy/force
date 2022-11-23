6e04b7e7afed9534128de26a8a9a68c01628ca60
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-11-23 17:22:56 +0100

    fix(CX-3129): Cleanup Inquiry flow (#11399)

--- src/Components/Inquiry/config.ts
+++ src/Components/Inquiry/config.ts
@@ -3,30 +3,14 @@ import { WorkflowEngine } from "Utils/WorkflowEngine"
 import { Context } from "./Hooks/useInquiryContext"
 import { Visited } from "./Visited"
 import { InquiryAccount } from "./Views/InquiryAccount"
-import { InquiryArtistsInCollection } from "./Views/InquiryArtistsInCollection"
-import { InquiryAuctionHousesYouWorkWith } from "./Views/InquiryAuctionHousesYouWorkWith"
 import { InquiryBasicInfoQueryRenderer } from "./Views/InquiryBasicInfo"
-import { InquiryCommercialInterest } from "./Views/InquiryCommercialInterest"
-import { InquiryConfirmation } from "./Views/InquiryConfirmation"
-import { InquiryDone } from "./Views/InquiryDone"
-import { InquiryFairsYouAttend } from "./Views/InquiryFairsYouAttend"
-import { InquiryGalleriesYouWorkWith } from "./Views/InquiryGalleriesYouWorkWith"
 import { InquiryInquiryQueryRenderer } from "./Views/InquiryInquiry"
-import { InquiryInstitutionalAffiliations } from "./Views/InquiryInstitutionalAffiliations"
 import { InquirySpecialist } from "./Views/InquirySpecialist"
 
 const VIEWS = {
   Account: InquiryAccount,
-  ArtistsInCollection: InquiryArtistsInCollection,
-  AuctionHousesYouWorkWith: InquiryAuctionHousesYouWorkWith,
   BasicInfo: InquiryBasicInfoQueryRenderer,
-  CommercialInterest: InquiryCommercialInterest,
-  Confirmation: InquiryConfirmation,
-  Done: InquiryDone,
-  FairsYouAttend: InquiryFairsYouAttend,
-  GalleriesYouWorkWith: InquiryGalleriesYouWorkWith,
   Inquiry: InquiryInquiryQueryRenderer,
-  InstitutionalAffiliations: InquiryInstitutionalAffiliations,
   Specialist: InquirySpecialist,
 }
 
@@ -61,58 +45,11 @@ export const useEngine = ({ context, onDone }: UseEngine) => {
                 },
               },
               {
-                hasCompletedProfile: {
-                  false: ["Confirmation"],
-                },
-              },
-              {
-                hasSeenCommercialInterest: {
-                  false: ["CommercialInterest"],
-                },
-              },
-              {
                 hasBasicInfo: {
                   false: ["BasicInfo"],
                 },
               },
-              {
-                isCollector: {
-                  true: [
-                    {
-                      hasSeenArtistsInCollection: {
-                        false: ["ArtistsInCollection"],
-                      },
-                    },
-                    {
-                      hasSeenGalleriesYouWorkWith: {
-                        false: ["GalleriesYouWorkWith"],
-                      },
-                    },
-                    {
-                      hasSeenAuctionHousesYouWorkWith: {
-                        false: ["AuctionHousesYouWorkWith"],
-                      },
-                    },
-                    {
-                      hasSeenFairsYouAttend: {
-                        false: ["FairsYouAttend"],
-                      },
-                    },
-                    {
-                      hasSeenInstitutionalAffiliations: {
-                        false: ["InstitutionalAffiliations"],
-                      },
-                    },
-                  ],
-                },
-              },
             ],
-          },
-        },
-        {
-          hasSeenConfirmationThisSession: {
-            true: ["Done"],
-            false: ["Confirmation"],
           },
         },
       ],
@@ -127,53 +64,6 @@ export const useEngine = ({ context, onDone }: UseEngine) => {
             !!context.current?.phone &&
             !!context.current?.shareFollows
           )
-        },
-        hasCompletedProfile: () => {
-          const hasBasicInfo =
-            !!context.current?.profession &&
-            !!context.current?.location?.city &&
-            !!context.current?.phone &&
-            !!context.current?.shareFollows
-
-          const isCollector = (context.current?.collectorLevel ?? 0) >= 3
-
-          return hasBasicInfo && isCollector
-            ? // If you're a collector then you should have seen all of these
-              // before "completing" your profile
-              visited.current.hasSeen(
-                "CommercialInterest",
-                "ArtistsInCollection",
-                "GalleriesYouWorkWith",
-                "AuctionHousesYouWorkWith",
-                "FairsYouAttend",
-                "InstitutionalAffiliations"
-              )
-            : // If you've never bought art then you only saw this step
-              visited.current.hasSeen("CommercialInterest")
-        },
-        hasSeenArtistsInCollection: () => {
-          return visited.current.hasSeen("ArtistsInCollection")
-        },
-        hasSeenAuctionHousesYouWorkWith: () => {
-          return visited.current.hasSeen("AuctionHousesYouWorkWith")
-        },
-        hasSeenCommercialInterest: () => {
-          return visited.current.hasSeen("CommercialInterest")
-        },
-        hasSeenConfirmationThisSession: () => {
-          return visited.current.hasSeenThisSession("Confirmation")
-        },
-        hasSeenFairsYouAttend: () => {
-          return visited.current.hasSeen("FairsYouAttend")
-        },
-        hasSeenGalleriesYouWorkWith: () => {
-          return visited.current.hasSeen("GalleriesYouWorkWith")
-        },
-        hasSeenInstitutionalAffiliations: () => {
-          return visited.current.hasSeen("InstitutionalAffiliations")
-        },
-        isCollector: () => {
-          return (context.current?.collectorLevel ?? 0) >= 3
         },
         isLoggedOut: () => {
           return !context.current?.isLoggedIn
