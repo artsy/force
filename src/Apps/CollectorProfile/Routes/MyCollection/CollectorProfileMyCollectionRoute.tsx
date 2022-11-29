0201c4ff7a469a4b548431b51dcf7d3c011e41f0
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-11-29 15:50:26 +0100

    fix: hide pages behind feature flag

--- src/Apps/CollectorProfile/Routes/MyCollection/CollectorProfileMyCollectionRoute.tsx
+++ src/Apps/CollectorProfile/Routes/MyCollection/CollectorProfileMyCollectionRoute.tsx
@@ -1,6 +1,13 @@
 import { Text } from "@artsy/palette"
+import { useFeatureFlag } from "System/useFeatureFlag"
 
 const CollectorProfileMyCollectionRoute: React.FC = () => {
+  const isCollectorProfileEnabled = useFeatureFlag("cx-collector-profile")
+
+  if (!isCollectorProfileEnabled) {
+    return null
+  }
+
   return <Text>Ja Hallo!</Text>
 }
 
--- src/Apps/CollectorProfile/Routes/SavesAndFollows/CollectorProfileSavesAndFollowsRoute.tsx
+++ src/Apps/CollectorProfile/Routes/SavesAndFollows/CollectorProfileSavesAndFollowsRoute.tsx
@@ -1,6 +1,13 @@
 import { Text } from "@artsy/palette"
+import { useFeatureFlag } from "System/useFeatureFlag"
 
 const CollectorProfileSavesAndFollowsRoute: React.FC = () => {
+  const isCollectorProfileEnabled = useFeatureFlag("cx-collector-profile")
+
+  if (!isCollectorProfileEnabled) {
+    return null
+  }
+
   return <Text>Yo!</Text>
 }
 
