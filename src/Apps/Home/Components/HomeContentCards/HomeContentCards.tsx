32b5af768bd2dfe495389bafdfa74bb3817a2023
Jon Allured <jon.allured@gmail.com>
2022-11-29 21:11:57 -0600

    Separate effect hooks

--- src/Apps/Home/Components/HomeContentCards/HomeContentCards.tsx
+++ src/Apps/Home/Components/HomeContentCards/HomeContentCards.tsx
@@ -12,23 +12,25 @@ export const HomeContentCards: React.FC = () => {
     window.analytics?.ready(() => {
       const appboy = (window as any).appboy
       setAppboy(appboy)
-
-      if (!appboy) return
-
-      appboy.subscribeToContentCardsUpdates(async () => {
-        const response = await appboy.getCachedContentCards()
-        const { cards: updatedCards } = response
-        const sortedCards = updatedCards.sort((lhs, rhs) => {
-          const lhsPosition = (lhs.extras || {}).position || lhs.id
-          const rhsPosition = (rhs.extras || {}).position || rhs.id
-          return lhsPosition > rhsPosition ? 1 : -1
-        })
-        setCards(sortedCards)
-      })
-
-      appboy.requestContentCardsRefresh()
     })
-  }, [])
+  })
+
+  useEffect(() => {
+    if (!appboy) return
+
+    appboy.subscribeToContentCardsUpdates(async () => {
+      const response = await appboy.getCachedContentCards()
+      const { cards: updatedCards } = response
+      const sortedCards = updatedCards.sort((lhs, rhs) => {
+        const lhsPosition = (lhs.extras || {}).position || lhs.id
+        const rhsPosition = (rhs.extras || {}).position || rhs.id
+        return lhsPosition > rhsPosition ? 1 : -1
+      })
+      setCards(sortedCards)
+    })
+
+    appboy.requestContentCardsRefresh()
+  }, [appboy])
 
   const hasBrazeCards = appboy && cards.length > 0
 
