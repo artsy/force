eb5d9d542cb65d2da2e5798a8b5d8b6c37ecd1de
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-12-02 17:59:15 +0100

    fix: update constants names

--- src/Apps/CollectorProfile/Components/CollectorProfileHeader/Components/CollectorProfileHeaderInfo.tsx
+++ src/Apps/CollectorProfile/Components/CollectorProfileHeader/Components/CollectorProfileHeaderInfo.tsx
@@ -11,8 +11,8 @@ import { createFragmentContainer, graphql } from "react-relay"
 import { Media } from "Utils/Responsive"
 import { CollectorProfileHeaderInfo_me$data } from "__generated__/CollectorProfileHeaderInfo_me.graphql"
 
-const M_ICON_SIZE = 14
-const D_ICON_SIZE = 18
+const MOBILE_ICON_SIZE = 14
+const DESKTOP_ICON_SIZE = 18
 
 interface CollectorProfileHeaderInfoProps {
   me: CollectorProfileHeaderInfo_me$data
@@ -78,11 +78,19 @@ const InfoField: FC<InfoFieldProps> = ({ type, value }) => {
     <>
       <Flex flexDirection="row" alignItems="center" pr={0.5} pb={0.5}>
         <Media style={{ display: "flex" }} lessThan="sm">
-          <Icon fill="black60" width={M_ICON_SIZE} height={M_ICON_SIZE} />
+          <Icon
+            fill="black60"
+            width={MOBILE_ICON_SIZE}
+            height={MOBILE_ICON_SIZE}
+          />
         </Media>
 
         <Media style={{ display: "flex" }} greaterThanOrEqual="sm">
-          <Icon fill="black60" width={D_ICON_SIZE} height={D_ICON_SIZE} />
+          <Icon
+            fill="black60"
+            width={DESKTOP_ICON_SIZE}
+            height={DESKTOP_ICON_SIZE}
+          />
         </Media>
 
         <Text variant={["xs", "sm-display"]} color="black60" px={0.5}>
