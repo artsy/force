abe6e77fbba983a4aeb1571ea9f9dc817ea2618c
dariakoko <nargle30@gmail.com>
2022-12-09 19:30:49 +0100

    add test to the settings button

--- src/Apps/CollectorProfile/Components/CollectorProfileHeader/__tests__/CollectorProfileHeader.jest.tsx
+++ src/Apps/CollectorProfile/Components/CollectorProfileHeader/__tests__/CollectorProfileHeader.jest.tsx
@@ -1,11 +1,12 @@
 import { Breakpoint } from "@artsy/palette"
-import { screen } from "@testing-library/react"
+import { fireEvent, screen } from "@testing-library/react"
 import { CollectorProfileHeaderFragmentContainer } from "Apps/CollectorProfile/Components/CollectorProfileHeader/CollectorProfileHeader"
 import { MockBoot } from "DevTools"
 import { setupTestWrapperTL } from "DevTools/setupTestWrapper"
 import { graphql } from "react-relay"
 
 jest.unmock("react-relay")
+const mockRouterPush = jest.fn()
 
 describe("CollectorProfileHeader", () => {
   const getWrapper = (breakpoint: Breakpoint = "lg") => {
@@ -44,12 +45,33 @@ describe("CollectorProfileHeader", () => {
       expect(screen.getByText("Settings")).toBeInTheDocument()
     })
 
+    it("navigates when the settings button is pressed", () => {
+      const { renderWithRelay } = getWrapper()
+      renderWithRelay(mockResolvers, false)
+
+      expect(
+        screen
+          .getAllByRole("link")
+          .find(c => c.textContent?.includes("Settings"))
+      ).toHaveAttribute("href", `/settings/edit-settings`)
+    })
+
     it("renders the settings icon in Mobile", () => {
       const { renderWithRelay } = getWrapper("xs")
 
       renderWithRelay(mockResolvers, false)
 
       expect(screen.getByTitle("Settings")).toBeInTheDocument()
+    })
+    it("navigates when the the settings icon in Mobile is pressed", () => {
+      const { renderWithRelay } = getWrapper("xs")
+      renderWithRelay(mockResolvers, false)
+
+      expect(
+        screen
+          .getAllByRole("link")
+          .find(c => c.textContent?.includes("Settings"))
+      ).toHaveAttribute("href", `/settings/edit-settings`)
     })
   })
 })
