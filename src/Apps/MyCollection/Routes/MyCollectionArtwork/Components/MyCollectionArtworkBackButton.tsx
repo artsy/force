a3a2124cafa79518cd0fbb7948df78d18ecbdb0f
dariakoko <nargle30@gmail.com>
2022-12-14 16:16:10 +0100

    feat: go to artwork page and back!

--- src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworkBackButton.tsx
+++ src/Apps/MyCollection/Routes/MyCollectionArtwork/Components/MyCollectionArtworkBackButton.tsx
@@ -1,10 +1,20 @@
 import { ChevronIcon, Flex, Text } from "@artsy/palette"
 import { RouterLink } from "System/Router/RouterLink"
+import { useFeatureFlag } from "System/useFeatureFlag"
 import { Media } from "Utils/Responsive"
 
 export const MyCollectionArtworkBackButton = () => {
+  const isCollectorProfileEnabled = useFeatureFlag("cx-collector-profile")
+
   return (
-    <RouterLink textDecoration="none" to="/settings/my-collection">
+    <RouterLink
+      textDecoration="none"
+      to={
+        isCollectorProfileEnabled
+          ? "/collector-profile/my-collection"
+          : "/settings/my-collection"
+      }
+    >
       <Flex alignItems="center">
         <ChevronIcon height={14} width={14} direction="left" />
         <Media greaterThanOrEqual="sm">
