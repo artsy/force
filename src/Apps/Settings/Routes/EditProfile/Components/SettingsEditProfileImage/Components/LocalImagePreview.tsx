b77463ceec94a8b4e314c93e4ffd9d93a6f5842c
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2022-12-15 16:46:58 +0100

    feat: use `objectFit: "cover"` for local image preview

--- src/Apps/Settings/Routes/EditProfile/Components/SettingsEditProfileImage/Components/LocalImagePreview.tsx
+++ src/Apps/Settings/Routes/EditProfile/Components/SettingsEditProfileImage/Components/LocalImagePreview.tsx
@@ -2,16 +2,28 @@ import { Avatar } from "@artsy/palette"
 import { Media } from "Utils/Responsive"
 
 export const LocalImagePreview: React.FC<{ imageUrl: string }> = ({
-  imageUrl: imageUrl,
+  imageUrl,
 }) => {
   return (
     <>
       <Media lessThan="sm">
-        <Avatar src={imageUrl} size="xs" lazyLoad mr={[1, 2]} />
+        <Avatar
+          style={{ objectFit: "cover" }}
+          src={imageUrl}
+          size="xs"
+          lazyLoad
+          mr={[1, 2]}
+        />
       </Media>
 
       <Media greaterThanOrEqual="sm">
-        <Avatar src={imageUrl} size="md" lazyLoad mr={[1, 2]} />
+        <Avatar
+          style={{ objectFit: "cover" }}
+          src={imageUrl}
+          size="md"
+          lazyLoad
+          mr={[1, 2]}
+        />
       </Media>
     </>
   )
