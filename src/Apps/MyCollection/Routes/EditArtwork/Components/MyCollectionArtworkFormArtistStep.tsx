afea4cc8df51f998afd9d8e52bccf1c038f5d35c
Ole <ole.richter91@gmail.com>
2022-12-16 15:52:47 +0100

    remove <></>

--- src/Apps/MyCollection/Routes/EditArtwork/Components/MyCollectionArtworkFormArtistStep.tsx
+++ src/Apps/MyCollection/Routes/EditArtwork/Components/MyCollectionArtworkFormArtistStep.tsx
@@ -9,39 +9,37 @@ export const MyCollectionArtworkFormArtistStep: React.FC<MyCollectionArtworkForm
   const { onBack, onNext, onSkip } = useMyCollectionArtworkFormContext()
 
   return (
-    <>
-      <AppContainer>
-        <MyCollectionArtworkFormHeader
-          onBackClick={() => onBack()}
-          NextButton={
-            <Button
-              width={[100, 300]}
-              data-testid="artist-select-skip-button"
-              onClick={() => onSkip?.()}
-              size={["small", "large"]}
-              variant="secondaryNeutral"
-            >
-              Skip
-            </Button>
-          }
-        />
+    <AppContainer>
+      <MyCollectionArtworkFormHeader
+        onBackClick={() => onBack()}
+        NextButton={
+          <Button
+            width={[100, 300]}
+            data-testid="artist-select-skip-button"
+            onClick={() => onSkip?.()}
+            size={["small", "large"]}
+            variant="secondaryNeutral"
+          >
+            Skip
+          </Button>
+        }
+      />
 
-        <Spacer y={4} />
+      <Spacer y={4} />
 
-        {/* TODO: Implement screen */}
+      {/* TODO: Implement screen */}
 
-        <Text variant={"xl"}>Select Artist</Text>
+      <Text variant={"xl"}>Select Artist</Text>
 
-        <Button
-          width={300}
-          onClick={() => onNext?.()}
-          size="large"
-          variant="secondaryNeutral"
-          mt={4}
-        >
-          Next
-        </Button>
-      </AppContainer>
-    </>
+      <Button
+        width={300}
+        onClick={() => onNext?.()}
+        size="large"
+        variant="secondaryNeutral"
+        mt={4}
+      >
+        Next
+      </Button>
+    </AppContainer>
   )
 }
