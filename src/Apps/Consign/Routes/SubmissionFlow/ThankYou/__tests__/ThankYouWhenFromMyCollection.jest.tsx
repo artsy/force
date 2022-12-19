fdb43d13dd9a9030e733aea2306997725e492d2d
dariakoko <nargle30@gmail.com>
2022-12-19 10:36:30 +0100

    chore: add allowlist secret

--- src/Apps/Consign/Routes/SubmissionFlow/ThankYou/__tests__/ThankYouWhenFromMyCollection.jest.tsx
+++ src/Apps/Consign/Routes/SubmissionFlow/ThankYou/__tests__/ThankYouWhenFromMyCollection.jest.tsx
@@ -1,5 +1,5 @@
 import { mount } from "enzyme"
-import { ThankYouWhenFromMyCollection } from "Apps/Consign/Routes/SubmissionFlow/ThankYou/ThankYouWhenFromMyCollection"
+import { ThankYouWhenFromMyCollection } from "Apps/Consign/Routes/SubmissionFlow/ThankYou/ThankYouWhenFromMyCollection" // pragma: allowlist secret
 
 describe("ThankYou page when artwork is submitted from My Collection", () => {
   const wrapper = mount(<ThankYouWhenFromMyCollection />)
