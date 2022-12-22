aa26eaf83695cebb412faa4848071ff660377c48
dzucconi <mail@damonzucconi.com>
2022-12-22 14:32:47 -0500

    refactor: moves removeindex type (breaks prettier)

--- src/Utils/typeSupport.ts
+++ src/Utils/typeSupport.ts
@@ -53,3 +53,7 @@ export type CleanRelayFragment<T> = Omit<
   T,
   "$refType" | " $fragmentRefs" | " $fragmentSpreads" | " $fragmentType"
 >
+
+export type RemoveIndex<T> = {
+  [P in keyof T as string extends P ? never : number extends P ? never : P]: T[P]
+};
