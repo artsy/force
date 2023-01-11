62b8872edb84efcddd67b439e2dc7f594197ca33
tmcmeans <tanjmcmeans01@gmail.com>
2023-01-11 10:41:47 -0600

    (feat)art-quiz: add email campaign mutation to quiz results page button

--- src/Apps/ArtQuiz/Components/ArtQuizResultsTabs.tsx
+++ src/Apps/ArtQuiz/Components/ArtQuizResultsTabs.tsx
@@ -4,7 +4,9 @@ import { ArtQuizRecommendedArtistsQueryRenderer } from "Apps/ArtQuiz/Components/
 import { ArtQuizResultsRecommendedArtworksQueryRenderer } from "Apps/ArtQuiz/Components/ArtQuizResultsRecommendedArtworks"
 import { FC } from "react"
 import { useTranslation } from "react-i18next"
+import { graphql } from "react-relay"
 import { useSystemContext } from "System"
+import { useMutation } from "Utils/Hooks/useMutation"
 
 export const ArtQuizResultsTabs: FC = () => {
   const { t } = useTranslation()
@@ -13,8 +15,22 @@ export const ArtQuizResultsTabs: FC = () => {
 
   const { sendToast } = useToasts()
 
+  const { submitMutation } = useMutation({
+    mutation: graphql`
+      mutation TriggerCampaign($input: TriggerCampaignInput!) {
+        clientMutationId
+      }
+    `,
+  })
+
   const handleClick = () => {
-    // TODO: Trigger email
+    submitMutation({
+      variables: {
+        input: {
+          campaignId: "ART_QUIZ",
+        },
+      },
+    })
 
     sendToast({
       variant: "success",
