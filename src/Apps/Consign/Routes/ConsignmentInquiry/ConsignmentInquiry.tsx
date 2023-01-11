174abbd0ad429d72e0197bf46309e67b4d6d2091
Kizito Egeonu <kizito.egeonu@gmail.com>
2023-01-11 11:09:23 +0100

    fix UI issues (#11683)

--- src/Apps/Consign/Routes/ConsignmentInquiry/ConsignmentInquiry.tsx
+++ src/Apps/Consign/Routes/ConsignmentInquiry/ConsignmentInquiry.tsx
@@ -108,7 +108,7 @@ export const ConsignmentInquiry: React.FC<ConsignmentInquiryProps> = ({
 
       if (consignmentInquiryId) {
         trackEvent(tracks.sentConsignmentInquiry(consignmentInquiryId))
-        router.replace({ pathname: "/sell/inquiry" })
+        router.replace({ pathname: "/sell" })
         router.push("/sell/inquiry/sent")
       }
     } catch (error) {
@@ -201,7 +201,7 @@ export const ConsignmentInquiry: React.FC<ConsignmentInquiryProps> = ({
               </Text>
               <Button
                 data-testid="consignment-inquiry-send-button"
-                width={["50%", "33%"]}
+                width={["100%", "20%"]}
                 disabled={!isValid || isSubmitting}
                 loading={isSubmitting}
                 type="submit"
