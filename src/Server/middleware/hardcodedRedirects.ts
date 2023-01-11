bd4c08b5556a246b00dd2d4f1f714e6144ed7b13
Sultan Al-Maari <sltun2oo9@gmail.com>
2023-01-11 11:40:59 +0100

    fix: update `/my-collection` redirection

--- src/Server/middleware/hardcodedRedirects.ts
+++ src/Server/middleware/hardcodedRedirects.ts
@@ -70,7 +70,7 @@ const PERMANENT_REDIRECTS = {
   "/user/purchases": "/settings/purchases",
   "/user/payments": "/settings/payments",
   "/user/alerts": "/settings/alerts",
-  "/my-collection": "/settings/my-collection",
+  "/my-collection": "/collector-profile/my-collection",
   "/page/collector-faqs-selling-on-artsy": "/sell",
   "/apply/gallery": "http://apply.artsy.net/galleries",
   "/apply/institution": "http://apply.artsy.net/institutions",
