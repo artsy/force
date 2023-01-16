dec77a501a37aa3aa97fbfa8b36cc8e7b58a6777
Alican Akyuz <alicakyz@gmail.com>
2023-01-16 14:13:07 +0100

    feat: redirect users to home page upon logo click

--- src/Apps/Order/__tests__/OrderApp.jest.tsx
+++ src/Apps/Order/__tests__/OrderApp.jest.tsx
@@ -664,4 +664,19 @@ describe("OrderApp", () => {
       "Sorry, the page you were looking for doesn’t exist at this URL."
     )
   })
+
+  it("redirects user to home page for private sale orders when Artsy logo is clicked", () => {
+    const props = getProps({
+      location: "/order/123/review",
+    })
+    const page = getWrapper({
+      context: { isEigen: false },
+      props: { ...props, order: PrivateSaleOrderWithShippingDetails },
+    })
+
+    const logo = page.find(`[data-test="logoLink"]`).first()
+    logo.simulate("click")
+
+    expect(window.location.pathname).toBe("/")
+  })
 })
