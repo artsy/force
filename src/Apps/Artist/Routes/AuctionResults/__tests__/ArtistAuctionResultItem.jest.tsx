106f38fd2647d66be70f810785155138ee9ecfb6
Sultan Al-Maari <sultan.al-maari@artsymail.com>
2023-01-26 17:54:10 +0100

    fix: add tests

--- src/Apps/Artist/Routes/AuctionResults/__tests__/ArtistAuctionResultItem.jest.tsx
+++ src/Apps/Artist/Routes/AuctionResults/__tests__/ArtistAuctionResultItem.jest.tsx
@@ -3,9 +3,11 @@ import { graphql } from "react-relay"
 import { ArtistAuctionResultItemFragmentContainer } from "Apps/Artist/Routes/AuctionResults/ArtistAuctionResultItem"
 import { ArtistAuctionResultItemTestQuery } from "__generated__/ArtistAuctionResultItemTestQuery.graphql"
 import { screen } from "@testing-library/react"
+import { useSystemContext } from "System"
 
 jest.unmock("react-relay")
 
+jest.mock("System/useSystemContext")
 jest.mock("System/Router/useRouter", () => ({
   useRouter: () => ({ match: { location: { pathname: "anything" } } }),
 }))
@@ -38,6 +40,10 @@ describe("ArtistAuctionResultItem", () => {
         }
       }
     `,
+  })
+
+  beforeEach(() => {
+    ;(useSystemContext as jest.Mock).mockImplementation(() => ({ user: null }))
   })
 
   it("renders the auction result data", () => {
@@ -81,23 +87,52 @@ describe("ArtistAuctionResultItem", () => {
       expect(screen.getByText("Lot 33")).toBeInTheDocument()
     })
   })
+
+  describe("Awaiting results", () => {
+    beforeEach(() => {
+      ;(useSystemContext as jest.Mock).mockImplementation(() => ({
+        user: { name: "Logged In", email: "loggedin@example.com" },
+      }))
+    })
+
+    it("renders awaiting results when the auction result is in last with no price", () => {
+      const lastMonth = new Date().setDate(new Date().getDate() - 1)
+      const lastMonthISO = new Date(lastMonth).toISOString()
+
+      renderWithRelay(
+        { AuctionResult: () => ({ ...auctionResult, saleDate: lastMonthISO }) },
+        false
+      )
+
+      expect(screen.getAllByText("Awaiting results")).toHaveLength(2)
+    })
+
+    it("renders price not available when the auction result is over a month with no price", () => {
+      renderWithRelay(mockResolver, false)
+
+      expect(screen.getAllByText("Price not available")).toHaveLength(2)
+    })
+  })
 })
 
-const mockResolver = {
-  AuctionResult: () => ({
-    title: "Neuschwanstein (Feldmann & Schellmann 372)",
-    dimension_text: "62.0 x 91.0 cm",
-    organization: "Bonhams",
-    artist: {
-      name: "Andy Warhol",
-    },
-    mediumText:
-      "Offset lithographic poster with screenprint in colours, on wove paper",
-    categoryText: "Print",
-    date_text: "1987",
-    saleDate: "2022-09-21T00:00:00.000Z",
-    saleTitle: "Contemporary Art Evening Sale",
-    location: "Athens",
-    lotNumber: "33",
-  }),
+const auctionResult = {
+  title: "Neuschwanstein (Feldmann & Schellmann 372)",
+  dimension_text: "62.0 x 91.0 cm",
+  organization: "Bonhams",
+  artist: {
+    name: "Andy Warhol",
+  },
+  mediumText:
+    "Offset lithographic poster with screenprint in colours, on wove paper",
+  categoryText: "Print",
+  date_text: "1987",
+  saleDate: "2022-09-21T00:00:00.000Z",
+  saleTitle: "Contemporary Art Evening Sale",
+  location: "Athens",
+  lotNumber: "33",
+  boughtIn: false,
+  isUpcoming: false,
+  price_realized: { display: null },
 }
+
+const mockResolver = { AuctionResult: () => auctionResult }
