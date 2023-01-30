8804b3b776bae46f764bab19389405843f8edc3e
Dima Tretyak <itretyak@ya.ru>
2023-01-30 19:58:34 +0300

    feat: render demo rail

--- src/Apps/CollectorProfile/Routes/Saves2/CollectorProfileSaves2Route.tsx
+++ src/Apps/CollectorProfile/Routes/Saves2/CollectorProfileSaves2Route.tsx
@@ -1,6 +1,21 @@
-import { Text } from "@artsy/palette"
+import { Shelf } from "@artsy/palette"
+import { SavesItem } from "Apps/CollectorProfile/Routes/Saves2/Components/SavesItem"
 import { FC } from "react"
 
+const items = Array.from(Array(10))
+
 export const CollectorProfileSaves2Route: FC = () => {
-  return <Text>/collector-profile/saves2</Text>
+  return (
+    <Shelf showProgress={false}>
+      {items.map((_, index) => (
+        <SavesItem
+          key={`saves-item-${index}`}
+          title="Saves"
+          artworksCount={36}
+          isSelected={index === 0}
+          imagesLayout={index === 0 ? "grid" : "stacked"}
+        />
+      ))}
+    </Shelf>
+  )
 }
