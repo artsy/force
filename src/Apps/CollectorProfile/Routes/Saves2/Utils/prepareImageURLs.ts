7e77ed6483f5566b85f2636c6f4b0b2ae2e473db
Dima Tretyak <itretyak@ya.ru>
2023-01-30 20:13:53 +0300

    refactor: dynamically create array

--- src/Apps/CollectorProfile/Routes/Saves2/Utils/prepareImageURLs.ts
+++ src/Apps/CollectorProfile/Routes/Saves2/Utils/prepareImageURLs.ts
@@ -1,6 +1,6 @@
 export const prepareImageURLs = (imageURLs: string[]) => {
   // Ensure we have an array of exactly 4 images
-  return [null, null, null, null].reduce(
+  return Array.from(Array(4)).reduce(
     (acc, _, i) => [...acc, imageURLs[i] ?? null],
     [] as string[]
   )
