41beefa08f110c3f09551448755d2b32aaccb318
Laura Bhayani <laura.bhayani@artsymail.com>
2023-01-31 11:04:36 -0500

    add a tooltip test

---
+++ src/Apps/ArtQuiz/__tests__/ArtQuizArtworks.jest.tsx
@@ -0,0 +1,27 @@
+import { setupTestWrapperTL } from "DevTools/setupTestWrapper"
+import { ArtQuizArtworksFragmentContainer } from "Apps/ArtQuiz/Routes/ArtQuizArtworks"
+import { graphql } from "react-relay"
+import { screen } from "@testing-library/react"
+
+jest.unmock("react-relay")
+jest.mock("react-tracking")
+
+describe("ArtQuizArtworks", () => {
+  const { renderWithRelay } = setupTestWrapperTL({
+    Component: ArtQuizArtworksFragmentContainer,
+    query: graphql`
+      query ArtQuizArtworks_Test_Query @relay_test_operation {
+        me {
+          ...ArtQuizArtworks_me
+        }
+      }
+    `,
+  })
+
+  it("displays a tooltip on the first artwork", () => {
+    renderWithRelay()
+
+    expect(screen.getByText("Like it? Hit the heart.")).toBeInTheDocument()
+    expect(screen.getByText("Not for you? Choose X.")).toBeInTheDocument()
+  })
+})
---
+++ src/__generated__/ArtQuizArtworks_Test_Query.graphql.ts
@@ -0,0 +1,361 @@
+/**
+ * @generated SignedSource<<3554b40954ca8d033af42f9bae2bb4b2>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type ArtQuizArtworks_Test_Query$variables = {};
+export type ArtQuizArtworks_Test_Query$data = {
+  readonly me: {
+    readonly " $fragmentSpreads": FragmentRefs<"ArtQuizArtworks_me">;
+  } | null;
+};
+export type ArtQuizArtworks_Test_Query = {
+  response: ArtQuizArtworks_Test_Query$data;
+  variables: ArtQuizArtworks_Test_Query$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v1 = {
+  "enumValues": null,
+  "nullable": false,
+  "plural": false,
+  "type": "ID"
+},
+v2 = {
+  "enumValues": null,
+  "nullable": true,
+  "plural": false,
+  "type": "String"
+},
+v3 = {
+  "enumValues": null,
+  "nullable": true,
+  "plural": false,
+  "type": "Int"
+},
+v4 = {
+  "enumValues": null,
+  "nullable": false,
+  "plural": false,
+  "type": "String"
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ArtQuizArtworks_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Me",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "ArtQuizArtworks_me"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "ArtQuizArtworks_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Me",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          (v0/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Quiz",
+            "kind": "LinkedField",
+            "name": "quiz",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": [
+                  {
+                    "kind": "Literal",
+                    "name": "first",
+                    "value": 16
+                  }
+                ],
+                "concreteType": "QuizArtworkConnection",
+                "kind": "LinkedField",
+                "name": "quizArtworkConnection",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "QuizArtworkEdge",
+                    "kind": "LinkedField",
+                    "name": "edges",
+                    "plural": true,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "interactedAt",
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "position",
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Artwork",
+                        "kind": "LinkedField",
+                        "name": "node",
+                        "plural": false,
+                        "selections": [
+                          (v0/*: any*/),
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "internalID",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "concreteType": "Image",
+                            "kind": "LinkedField",
+                            "name": "image",
+                            "plural": false,
+                            "selections": [
+                              {
+                                "alias": null,
+                                "args": [
+                                  {
+                                    "kind": "Literal",
+                                    "name": "height",
+                                    "value": 900
+                                  },
+                                  {
+                                    "kind": "Literal",
+                                    "name": "version",
+                                    "value": [
+                                      "normalized",
+                                      "larger",
+                                      "large"
+                                    ]
+                                  },
+                                  {
+                                    "kind": "Literal",
+                                    "name": "width",
+                                    "value": 900
+                                  }
+                                ],
+                                "concreteType": "ResizedImageUrl",
+                                "kind": "LinkedField",
+                                "name": "resized",
+                                "plural": false,
+                                "selections": [
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "src",
+                                    "storageKey": null
+                                  },
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "srcSet",
+                                    "storageKey": null
+                                  },
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "width",
+                                    "storageKey": null
+                                  },
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "height",
+                                    "storageKey": null
+                                  }
+                                ],
+                                "storageKey": "resized(height:900,version:[\"normalized\",\"larger\",\"large\"],width:900)"
+                              }
+                            ],
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "isDisliked",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "isSaved",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "slug",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "title",
+                            "storageKey": null
+                          }
+                        ],
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": "quizArtworkConnection(first:16)"
+              },
+              (v0/*: any*/)
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "630eef316218972e9e71e1d1b9c031dd",
+    "id": null,
+    "metadata": {
+      "relayTestingSelectionTypeInfo": {
+        "me": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Me"
+        },
+        "me.id": (v1/*: any*/),
+        "me.quiz": {
+          "enumValues": null,
+          "nullable": false,
+          "plural": false,
+          "type": "Quiz"
+        },
+        "me.quiz.id": (v1/*: any*/),
+        "me.quiz.quizArtworkConnection": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "QuizArtworkConnection"
+        },
+        "me.quiz.quizArtworkConnection.edges": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": true,
+          "type": "QuizArtworkEdge"
+        },
+        "me.quiz.quizArtworkConnection.edges.interactedAt": (v2/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Artwork"
+        },
+        "me.quiz.quizArtworkConnection.edges.node.id": (v1/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.image": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Image"
+        },
+        "me.quiz.quizArtworkConnection.edges.node.image.resized": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "ResizedImageUrl"
+        },
+        "me.quiz.quizArtworkConnection.edges.node.image.resized.height": (v3/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.image.resized.src": (v4/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.image.resized.srcSet": (v4/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.image.resized.width": (v3/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.internalID": (v1/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.isDisliked": {
+          "enumValues": null,
+          "nullable": false,
+          "plural": false,
+          "type": "Boolean"
+        },
+        "me.quiz.quizArtworkConnection.edges.node.isSaved": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Boolean"
+        },
+        "me.quiz.quizArtworkConnection.edges.node.slug": (v1/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.node.title": (v2/*: any*/),
+        "me.quiz.quizArtworkConnection.edges.position": {
+          "enumValues": null,
+          "nullable": false,
+          "plural": false,
+          "type": "Int"
+        }
+      }
+    },
+    "name": "ArtQuizArtworks_Test_Query",
+    "operationKind": "query",
+    "text": "query ArtQuizArtworks_Test_Query {\n  me {\n    ...ArtQuizArtworks_me\n    id\n  }\n}\n\nfragment ArtQuizArtworks_me on Me {\n  id\n  quiz {\n    quizArtworkConnection(first: 16) {\n      edges {\n        interactedAt\n        position\n        node {\n          id\n          internalID\n          image {\n            resized(width: 900, height: 900, version: [\"normalized\", \"larger\", \"large\"]) {\n              src\n              srcSet\n              width\n              height\n            }\n          }\n          isDisliked\n          isSaved\n          slug\n          title\n        }\n      }\n    }\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "f284f1e055d060048d333081794ba7b1";
+
+export default node;
