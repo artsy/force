f3d23d86293b6a5d5718f31661856e554417eff6
dzucconi <mail@damonzucconi.com>
2023-02-10 13:06:14 -0500

    feat(usecountrycode): skips query if logged in

--- src/Components/AuthDialog/Hooks/useCountryCode.ts
+++ src/Components/AuthDialog/Hooks/useCountryCode.ts
@@ -1,4 +1,5 @@
 import { graphql } from "react-relay"
+import { useSystemContext } from "System"
 import { getENV } from "Utils/getENV"
 import { useQuery } from "Utils/Hooks/useQuery"
 import { useCountryCodeQuery } from "__generated__/useCountryCodeQuery.graphql"
@@ -12,6 +13,8 @@ const USE_COUNTRY_CODE_QUERY = graphql`
 `
 
 export const useCountryCode = () => {
+  const { isLoggedIn } = useSystemContext()
+
   const { data, loading, error } = useQuery<useCountryCodeQuery>({
     query: USE_COUNTRY_CODE_QUERY,
     variables: {
@@ -20,6 +23,8 @@ export const useCountryCode = () => {
     cacheConfig: {
       fetchPolicy: "store-or-network",
     },
+    // If the user is logged in, we don't need the country code
+    skip: isLoggedIn,
   })
 
   return {
