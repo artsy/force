901a98666116e16bf877afbfbf42633355c069bc
Dima Tretyak <itretyak@ya.ru>
2023-02-15 21:43:41 +0300

    chore: pass  for header component

--- src/Apps/CollectorProfile/Routes/Saves2/Components/SelectListsForArtworkModal/SelectListsForArtworkHeader.tsx
+++ src/Apps/CollectorProfile/Routes/Saves2/Components/SelectListsForArtworkModal/SelectListsForArtworkHeader.tsx
@@ -3,7 +3,7 @@ import { Button, Flex, Spacer, Text } from "@artsy/palette"
 export const SelectListsForArtworkHeader = () => {
   return (
     <Flex flexDirection="row" alignItems="center" m={2}>
-      <Flex flexDirection="row" alignItems="center">
+      <Flex flex={1} flexDirection="row" alignItems="center">
         <Flex flexShrink={0} width={50} height={50} bg="black10" />
         <Spacer x={1} />
         <Text>Marie Pop, The NAR, 2016</Text>
