caae67513b93d3221b1fe5f8f87e66c1bf5e39da
Dima Tretyak <itretyak@ya.ru>
2023-03-03 20:45:39 +0300

    refactor: add entity image component

--- src/Apps/CollectorProfile/Routes/Saves2/Components/CreateNewListModal/CreateNewListModalHeader.tsx
+++ src/Apps/CollectorProfile/Routes/Saves2/Components/CreateNewListModal/CreateNewListModalHeader.tsx
@@ -1,5 +1,5 @@
 import { Flex, Spacer, Text } from "@artsy/palette"
-import { SelectListsForArtworkImage } from "Apps/CollectorProfile/Routes/Saves2/Components/SelectListsForArtworkModal/SelectListsForArtworkImage"
+import { SavesEntityImage } from "Apps/CollectorProfile/Routes/Saves2/Components/SavesEntityImage"
 import { FC } from "react"
 
 export interface ArtworkEntity {
@@ -16,7 +16,7 @@ export const CreateNewListModalHeader: FC<CreateNewListModalHeaderProps> = ({
 }) => {
   return (
     <Flex flex={1} flexDirection="row" alignItems="center" mb={2}>
-      <SelectListsForArtworkImage size={50} url={artwork.imageURL} />
+      <SavesEntityImage size={50} url={artwork.imageURL} />
       <Spacer x={1} />
       <Text lineClamp={2}>{artwork.title}</Text>
     </Flex>
