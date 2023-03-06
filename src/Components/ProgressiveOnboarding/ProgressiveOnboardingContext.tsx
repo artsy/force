2728ff2dfbc77384d145cc199376be722849df70
dzucconi <mail@damonzucconi.com>
2023-03-06 13:27:43 -0500

    fix(progressive-onboarding): partitions storage per user

--- src/Components/ProgressiveOnboarding/ProgressiveOnboardingContext.tsx
+++ src/Components/ProgressiveOnboarding/ProgressiveOnboardingContext.tsx
@@ -9,6 +9,7 @@ import {
 } from "react"
 import { uniq } from "lodash"
 import { useFeatureFlag } from "System/useFeatureFlag"
+import { useSystemContext } from "System/SystemContext"
 
 const ProgressiveOnboardingContext = createContext<{
   dismissed: ProgressiveOnboardingKey[]
@@ -21,19 +22,22 @@ const ProgressiveOnboardingContext = createContext<{
 })
 
 export const ProgressiveOnboardingProvider: FC = ({ children }) => {
+  const { user } = useSystemContext()
+  const id = user?.id ?? "user"
+
   const [dismissed, setDismissed] = useState<ProgressiveOnboardingKey[]>([])
 
   const dismiss = useCallback(
     (key: ProgressiveOnboardingKey) => {
-      __dismiss__(key)
+      __dismiss__(id, key)
       setDismissed([...dismissed, key])
     },
-    [dismissed]
+    [dismissed, id]
   )
 
   useEffect(() => {
-    setDismissed(get())
-  }, [])
+    setDismissed(get(id))
+  }, [id])
 
   const mounted = useDidMount()
 
@@ -70,7 +74,9 @@ export const useProgressiveOnboarding = () => {
   }
 }
 
-const LOCAL_STORAGE_KEY = "progressive-onboarding-dismissed"
+export const localStorageKey = (id: string) => {
+  return `progressive-onboarding.dismissed.${id}`
+}
 
 export const PROGRESSIVE_ONBOARDING_FOLLOW_ARTIST = "follow-artist"
 export const PROGRESSIVE_ONBOARDING_FIND_FOLLOWS = "find-follows"
@@ -90,7 +96,10 @@ const PROGRESSIVE_ONBOARDING_KEYS = [
 
 type ProgressiveOnboardingKey = typeof PROGRESSIVE_ONBOARDING_KEYS[number]
 
-export const parse = (value: string | null): ProgressiveOnboardingKey[] => {
+export const parse = (
+  id: string,
+  value: string | null
+): ProgressiveOnboardingKey[] => {
   if (!value) return []
 
   try {
@@ -104,22 +113,22 @@ export const parse = (value: string | null): ProgressiveOnboardingKey[] => {
   }
 }
 
-export const __dismiss__ = (key: ProgressiveOnboardingKey) => {
-  const item = localStorage.getItem(LOCAL_STORAGE_KEY)
-  const dismissed = parse(item)
+export const __dismiss__ = (id: string, key: ProgressiveOnboardingKey) => {
+  const item = localStorage.getItem(localStorageKey(id))
+  const dismissed = parse(id, item)
 
   localStorage.setItem(
-    LOCAL_STORAGE_KEY,
+    localStorageKey(id),
     JSON.stringify(uniq([...dismissed, key]))
   )
 }
 
-export const get = () => {
-  const item = localStorage.getItem(LOCAL_STORAGE_KEY)
+export const get = (id: string) => {
+  const item = localStorage.getItem(localStorageKey(id))
 
-  return parse(item)
+  return parse(id, item)
 }
 
-export const reset = () => {
-  localStorage.removeItem(LOCAL_STORAGE_KEY)
+export const reset = (id: string) => {
+  localStorage.removeItem(localStorageKey(id))
 }
