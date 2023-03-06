d3e0a73ff3592530765f5f2cd9e2f162e7409d2e
Dima Tretyak <itretyak@ya.ru>
2023-03-06 18:43:16 +0300

    refactor: improvements

--- src/Apps/CollectorProfile/Routes/Saves2/Components/SelectListsForArtworkModal/useUpdateCollectionsForArtwork.ts
+++ src/Apps/CollectorProfile/Routes/Saves2/Components/SelectListsForArtworkModal/useUpdateCollectionsForArtwork.ts
@@ -2,6 +2,11 @@ import { graphql } from "react-relay"
 import { useMutation } from "Utils/Hooks/useMutation"
 import { useUpdateCollectionsForArtworkMutation } from "__generated__/useUpdateCollectionsForArtworkMutation.graphql"
 
+interface Counts {
+  custom: number
+  default: number
+}
+
 export const useUpdateCollectionsForArtwork = (artworkID: string) => {
   return useMutation<useUpdateCollectionsForArtworkMutation>({
     mutation: graphql`
@@ -11,17 +16,14 @@ export const useUpdateCollectionsForArtwork = (artworkID: string) => {
         artworksCollectionsBatchUpdate(input: $input) {
           responseOrError {
             ... on ArtworksCollectionsBatchUpdateSuccess {
-              counts {
-                addedToCollections
-                removedFromCollections
-              }
-
               addedToCollections {
                 internalID
+                name
                 artworksCount
               }
               removedFromCollections {
                 internalID
+                name
                 artworksCount
               }
             }
@@ -35,13 +37,27 @@ export const useUpdateCollectionsForArtwork = (artworkID: string) => {
       }
     `,
     updater: (store, data) => {
-      const response = data.artworksCollectionsBatchUpdate?.responseOrError
-      const counts = response?.counts
-      const addedCounts = counts?.addedToCollections ?? 0
-      const removedCounts = counts?.removedFromCollections ?? 0
       const artwork = store.get(artworkID)
 
-      const entity = artwork?.getLinkedRecord("collectionsConnection", {
+      if (!artwork) {
+        return
+      }
+
+      const response = data.artworksCollectionsBatchUpdate?.responseOrError
+      const addedCounts = getCountsByLists(response?.addedToCollections as any)
+      const removedCounts = getCountsByLists(
+        response?.removedFromCollections as any
+      )
+
+      if (addedCounts.default > 0) {
+        artwork.setValue(true, "isSaved")
+      }
+
+      if (removedCounts.default > 0) {
+        artwork.setValue(false, "isSaved")
+      }
+
+      const entity = artwork.getLinkedRecord("collectionsConnection", {
         first: 0,
         default: false,
         saves: true,
@@ -52,9 +68,27 @@ export const useUpdateCollectionsForArtwork = (artworkID: string) => {
       }
 
       const prevValue = (entity.getValue("totalCount") ?? 0) as number
-      const newValue = prevValue + addedCounts - removedCounts
+      const newValue = prevValue + addedCounts.custom - removedCounts.custom
 
       entity.setValue(newValue, "totalCount")
     },
   })
+}
+
+const getCountsByLists = <T extends { name: string }>(lists: T[]) => {
+  const defaultCounts: Counts = {
+    custom: 0,
+    default: 0,
+  }
+
+  return lists.reduce((acc, list) => {
+    const isDefault = list.name === "Saved Artwork"
+    const key: keyof Counts = isDefault ? "default" : "custom"
+    const prevCount = acc[key]
+
+    return {
+      ...acc,
+      [key]: prevCount + 1,
+    }
+  }, defaultCounts)
 }
