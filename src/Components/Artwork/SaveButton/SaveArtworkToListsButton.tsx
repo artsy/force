c4874fad1d9a910f35d754178388d589d645eb65
Dima Tretyak <itretyak@ya.ru>
2023-03-17 20:09:00 +0300

    feat: do some "magic"

--- src/Components/Artwork/SaveButton/SaveArtworkToListsButton.tsx
+++ src/Components/Artwork/SaveButton/SaveArtworkToListsButton.tsx
@@ -3,10 +3,11 @@ import { useToasts } from "@artsy/palette"
 import { useManageArtworkForSavesContext } from "Components/Artwork/ManageArtworkForSaves"
 import { SaveButtonBase } from "Components/Artwork/SaveButton/SaveButton"
 import { useSaveArtworkToLists } from "Components/Artwork/SaveButton/useSaveArtworkToLists"
-import { FC } from "react"
+import { FC, useEffect } from "react"
 import { useTranslation } from "react-i18next"
 import { createFragmentContainer, graphql } from "react-relay"
 import { useTracking } from "react-tracking"
+import { useAuthIntent } from "Utils/Hooks/useAuthIntent"
 import { SaveArtworkToListsButton_artwork$data } from "__generated__/SaveArtworkToListsButton_artwork.graphql"
 
 interface SaveArtworkToListsButtonProps {
@@ -22,9 +23,12 @@ const SaveArtworkToListsButton: FC<SaveArtworkToListsButtonProps> = ({
   const { t } = useTranslation()
   const { sendToast } = useToasts()
   const { savedListId, isSavedToList } = useManageArtworkForSavesContext()
+  const { value, clearValue } = useAuthIntent()
 
+  const artworkID = artwork.internalID
   const customListsCount = artwork.customCollections?.totalCount ?? 0
-  const hasCustomLists = customListsCount > 0
+  const isSavedToDefaultList = !!artwork.is_saved
+  const isSavedToCustomLists = customListsCount > 0
 
   const {
     isSaved,
@@ -38,10 +42,30 @@ const SaveArtworkToListsButton: FC<SaveArtworkToListsButtonProps> = ({
       slug: artwork.slug,
       title: `${artwork.title}, ${artwork.date}`,
       imageURL: artwork.preview?.url ?? null,
-      isSavedToDefaultList: !!artwork.is_saved,
-      isSavedToCustomLists: hasCustomLists,
+      isSavedToDefaultList,
+      isSavedToCustomLists,
     },
   })
+
+  useEffect(() => {
+    if (value?.objectId !== artworkID) {
+      return
+    }
+
+    if (isSavedToCustomLists) {
+      // Display select lists for artwork modal if arwork is saved to the custom lists
+      openSelectListsForArtworkModal()
+    } else if (isSavedToDefaultList) {
+      // Display toast if artwork is already saved to the default list
+      showToastByAction("Saved Artwork")
+    } else {
+      // Save artwork to the default list
+      handleSave()
+    }
+
+    clearValue()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [value, clearValue])
 
   const showToastByAction = (action: string) => {
     if (action === "Saved Artwork") {
@@ -69,11 +93,7 @@ const SaveArtworkToListsButton: FC<SaveArtworkToListsButtonProps> = ({
     })
   }
 
-  const handleClick = async (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    event.preventDefault()
-
+  const handleSave = async () => {
     try {
       const response = await saveArtworkToLists()
 
@@ -97,6 +117,13 @@ const SaveArtworkToListsButton: FC<SaveArtworkToListsButtonProps> = ({
       // TODO: Log error
       console.log("[debug] error", error)
     }
+  }
+
+  const handleClick = async (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.preventDefault()
+    handleSave()
   }
 
   return (
