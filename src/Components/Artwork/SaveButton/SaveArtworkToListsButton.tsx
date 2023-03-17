9f2a7b4f52f615cf81e3b63135cd8bd6e55d20bb
Dima Tretyak <itretyak@ya.ru>
2023-03-17 19:21:55 +0300

    refactor: use new hook

--- src/Components/Artwork/SaveButton/SaveArtworkToListsButton.tsx
+++ src/Components/Artwork/SaveButton/SaveArtworkToListsButton.tsx
@@ -1,9 +1,12 @@
 import { AuthContextModule } from "@artsy/cohesion"
+import { useToasts } from "@artsy/palette"
 import { useManageArtworkForSavesContext } from "Components/Artwork/ManageArtworkForSaves"
 import { SaveButtonBase } from "Components/Artwork/SaveButton/SaveButton"
 import { useSaveArtworkToLists } from "Components/Artwork/SaveButton/useSaveArtworkToLists"
 import { FC } from "react"
+import { useTranslation } from "react-i18next"
 import { createFragmentContainer, graphql } from "react-relay"
+import { useTracking } from "react-tracking"
 import { SaveArtworkToListsButton_artwork$data } from "__generated__/SaveArtworkToListsButton_artwork.graphql"
 
 interface SaveArtworkToListsButtonProps {
@@ -15,19 +18,85 @@ const SaveArtworkToListsButton: FC<SaveArtworkToListsButtonProps> = ({
   artwork,
   contextModule,
 }) => {
-  const { isSavedToList, savedListId } = useManageArtworkForSavesContext()
-  const { isSaved, onSave } = useSaveArtworkToLists({
-    artwork,
+  const tracking = useTracking()
+  const { t } = useTranslation()
+  const { sendToast } = useToasts()
+  const { savedListId, isSavedToList } = useManageArtworkForSavesContext()
+
+  const customListsCount = artwork.customCollections?.totalCount ?? 0
+  const hasCustomLists = customListsCount > 0
+
+  const {
+    isSaved,
+    saveArtworkToLists,
+    openManageArtworkForSavesModal,
+  } = useSaveArtworkToLists({
     contextModule,
-    customListsCount: artwork.customCollections?.totalCount ?? 0,
-    isSaved: !!artwork.is_saved,
+    artwork: {
+      internalID: artwork.internalID,
+      id: artwork.id,
+      slug: artwork.slug,
+      title: `${artwork.title}, ${artwork.date}`,
+      imageURL: artwork.preview?.url ?? null,
+      isSavedToDefaultList: !!artwork.is_saved,
+      isSavedToCustomLists: hasCustomLists,
+    },
   })
 
-  const handleClick = (
+  const showToastByAction = (action: string) => {
+    if (action === "Saved Artwork") {
+      sendToast({
+        variant: "success",
+        message: t(
+          "collectorSaves.saveArtworkToListsButton.artworkSavedToast.message"
+        ),
+        action: {
+          label: t(
+            "collectorSaves.saveArtworkToListsButton.artworkSavedToast.button"
+          ),
+          onClick: openManageArtworkForSavesModal,
+        },
+      })
+
+      return
+    }
+
+    sendToast({
+      variant: "message",
+      message: t(
+        "collectorSaves.saveArtworkToListsButton.artworkRemovedToast.message"
+      ),
+    })
+  }
+
+  const handleClick = async (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) => {
     event.preventDefault()
-    onSave()
+
+    try {
+      const response = await saveArtworkToLists()
+
+      // "Select lists for artwork" modal has been opened
+      if (!response) {
+        return
+      }
+
+      const action = !!response.saveArtwork?.artwork?.is_saved
+        ? "Saved Artwork"
+        : "Removed Artwork"
+
+      showToastByAction(action)
+
+      tracking.trackEvent({
+        action,
+        entity_slug: artwork.slug,
+        entity_id: artwork.internalID,
+      })
+    } catch (error) {
+      // TOOD: Log error
+      console.log("[debug] error", error)
+    }
   }
 
   return (
