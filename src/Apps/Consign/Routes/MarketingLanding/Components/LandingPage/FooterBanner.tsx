1fc8a6eb5fd91af870a4ea50e13635e4f228d50f
Mounir Dhahri <soussouheros@gmail.com>
2023-03-20 15:43:28 +0100

    fix: address review comments

--- src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/FooterBanner.tsx
+++ src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/FooterBanner.tsx
@@ -1,19 +1,18 @@
-import { Box, Text } from "@artsy/palette"
+import { Box, Spacer } from "@artsy/palette"
 import { FullBleedBanner } from "Components/FullBleedBanner"
 import { RouterLink } from "System/Router/RouterLink"
 
 export const FooterBanner: React.FC = () => {
   return (
     <Box display={["none", "block"]}>
-      <FullBleedBanner dismissable={false} variant="defaultLight" mb={-4}>
-        <Text variant="xs">
-          Gallery or art dealer?{" "}
-          <RouterLink to="https://partners.artsy.net">
-            Become a partner
-          </RouterLink>{" "}
-          to access the world’s largest online marketplace.
-        </Text>
+      <FullBleedBanner dismissable={false} variant="defaultLight">
+        Gallery or art dealer?{" "}
+        <RouterLink to="https://partners.artsy.net">
+          Become a partner
+        </RouterLink>{" "}
+        to access the world’s largest online marketplace.
       </FullBleedBanner>
+      <Spacer y={-4} />
     </Box>
   )
 }
