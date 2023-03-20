0f8745825808530852cc1718b9f12af4007e17b6
Mounir Dhahri <soussouheros@gmail.com>
2023-03-20 17:41:48 +0100

    fix: address sell footer review comment (#12120)
    * fix: address sell footer review comment
    
    * fix: address review comments
    
    * fix: move negative spacing to parent component

--- src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/FooterBanner.tsx
+++ src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/FooterBanner.tsx
@@ -1,24 +1,17 @@
-import { RouterLink } from "System/Router/RouterLink"
+import { Box } from "@artsy/palette"
 import { FullBleedBanner } from "Components/FullBleedBanner"
-import { useWindowSize } from "Utils/Hooks/useWindowSize"
-import { breakpoints, Text } from "@artsy/palette"
+import { RouterLink } from "System/Router/RouterLink"
 
 export const FooterBanner: React.FC = () => {
-  const { width } = useWindowSize()
-
   return (
-    <>
-      {width > parseInt(breakpoints.sm, 10) && (
-        <FullBleedBanner dismissable={false} variant="defaultLight" mb={-4}>
-          <Text variant="xs">
-            Gallery or art dealer?{" "}
-            <RouterLink to="https://partners.artsy.net">
-              Become a partner
-            </RouterLink>{" "}
-            to access the world’s largest online marketplace.
-          </Text>
-        </FullBleedBanner>
-      )}
-    </>
+    <Box display={["none", "block"]}>
+      <FullBleedBanner dismissable={false} variant="defaultLight">
+        Gallery or art dealer?{" "}
+        <RouterLink to="https://partners.artsy.net">
+          Become a partner
+        </RouterLink>{" "}
+        to access the world’s largest online marketplace.
+      </FullBleedBanner>
+    </Box>
   )
 }
