b15dbeee3db998d8bb2150d6d52c3f83a90b707a
Dima Tretyak <itretyak@ya.ru>
2023-03-22 18:51:15 +0300

    chore: add test cases

---
+++ src/Apps/Artwork/Components/ArtworkImageBrowser/__tests__/ArtworkActionsSaveButtonV2.jest.tsx
@@ -0,0 +1,199 @@
+import { fireEvent, screen } from "@testing-library/react"
+import { ArtworkActionsSaveButtonV2FragmentContainer } from "Apps/Artwork/Components/ArtworkImageBrowser/ArtworkActionsSaveButtonV2"
+import { AppToasts } from "Apps/Components/AppToasts"
+import { ManageArtworkForSavesProvider } from "Components/Artwork/ManageArtworkForSaves"
+import { SaveArtwork } from "Components/Artwork/SaveButton/SaveArtworkMutation"
+import { MockBoot } from "DevTools/MockBoot"
+import { setupTestWrapperTL } from "DevTools/setupTestWrapper"
+import { graphql } from "react-relay"
+import { ArtworkActionsSaveButtonV2_Test_Query } from "__generated__/ArtworkActionsSaveButtonV2_Test_Query.graphql"
+
+jest.unmock("react-relay")
+jest.mock("Components/Artwork/SaveButton/SaveArtworkMutation")
+
+describe("ArtworkActionsSaveButtonV2", () => {
+  const mockSaveArtwork = SaveArtwork as jest.Mock
+
+  beforeEach(() => {
+    mockSaveArtwork.mockImplementation(() => ({}))
+  })
+
+  const { renderWithRelay } = setupTestWrapperTL<
+    ArtworkActionsSaveButtonV2_Test_Query
+  >({
+    Component: props => {
+      if (!props.artwork) {
+        return null
+      }
+
+      return (
+        <MockBoot context={{ user: { id: "percy-z" } }}>
+          <AppToasts accomodateNav={false} />
+
+          <ManageArtworkForSavesProvider>
+            <ArtworkActionsSaveButtonV2FragmentContainer
+              artwork={props.artwork}
+            />
+          </ManageArtworkForSavesProvider>
+        </MockBoot>
+      )
+    },
+    query: graphql`
+      query ArtworkActionsSaveButtonV2_Test_Query @relay_test_operation {
+        artwork(id: "artworkID") {
+          ...ArtworkActionsSaveButtonV2_artwork
+        }
+      }
+    `,
+  })
+
+  it("should display `Save` label", () => {
+    renderWithRelay({
+      Artwork: () => unsavedArtwork,
+    })
+
+    expect(screen.getByText("Save")).toBeInTheDocument()
+  })
+
+  describe("Save flow", () => {
+    beforeEach(() => {
+      mockSaveArtwork.mockImplementation(args => ({
+        saveArtwork: {
+          artwork: {
+            is_saved: true,
+          },
+        },
+      }))
+    })
+
+    it("should display a toast message", async () => {
+      renderWithRelay({
+        Artwork: () => unsavedArtwork,
+      })
+
+      fireEvent.click(screen.getByText("Save"))
+
+      expect(await screen.findByText("Artwork saved")).toBeInTheDocument()
+      expect(await screen.findByText("Add to a List")).toBeInTheDocument()
+    })
+
+    it("should open the modal when `Add to a List` button was pressed", async () => {
+      renderWithRelay({
+        Artwork: () => unsavedArtwork,
+      })
+
+      fireEvent.click(screen.getByText("Save"))
+      fireEvent.click(await screen.findByText("Add to a List"))
+
+      const modalTitle = "Select lists for this artwork"
+      expect(screen.getByText(modalTitle)).toBeInTheDocument()
+    })
+  })
+
+  describe("Unsave flow", () => {
+    beforeEach(() => {
+      mockSaveArtwork.mockImplementation(args => ({
+        saveArtwork: {
+          artwork: {
+            is_saved: false,
+          },
+        },
+      }))
+    })
+
+    describe("should display `Saved` label", () => {
+      it("if artwork was previously saved in `All Saves` list", () => {
+        renderWithRelay({
+          Artwork: () => ({
+            isSaved: true,
+          }),
+        })
+
+        expect(screen.getByText("Saved")).toBeInTheDocument()
+      })
+
+      it("if artwork was previously saved in custom lists", () => {
+        renderWithRelay({
+          Artwork: () => ({
+            isSaved: false,
+            customCollections: {
+              totalCount: 2,
+            },
+          }),
+        })
+
+        expect(screen.getByText("Saved")).toBeInTheDocument()
+      })
+
+      it("if artwork was previously saved in `All Saves` and custom lists", () => {
+        renderWithRelay({
+          Artwork: () => ({
+            isSaved: false,
+            customCollections: {
+              totalCount: 2,
+            },
+          }),
+        })
+
+        expect(screen.getByText("Saved")).toBeInTheDocument()
+      })
+    })
+
+    it("should display a toast message when artwork was unsaved", async () => {
+      renderWithRelay({
+        Artwork: () => savedArtwork,
+      })
+
+      fireEvent.click(screen.getByText("Saved"))
+
+      const element = await screen.findByText("Removed from All Saves")
+      expect(element).toBeInTheDocument()
+    })
+
+    it("should unsave artwork from `All Saves` list by default", async () => {
+      renderWithRelay({
+        Artwork: () => ({
+          isSaved: true,
+          customCollections: {
+            totalCount: 0,
+          },
+        }),
+      })
+
+      fireEvent.click(screen.getByText("Saved"))
+
+      const element = await screen.findByText("Removed from All Saves")
+      expect(element).toBeInTheDocument()
+    })
+
+    it("should open the modal when artwork was saved in custom lists", () => {
+      renderWithRelay({
+        Artwork: () => ({
+          isSaved: true,
+          customCollections: {
+            totalCount: 2,
+          },
+        }),
+      })
+
+      fireEvent.click(screen.getByText("Saved"))
+
+      const modalTitle = "Select lists for this artwork"
+      expect(screen.getByText(modalTitle)).toBeInTheDocument()
+    })
+  })
+})
+
+const unsavedArtwork = {
+  isSaved: false,
+  customCollections: {
+    totalCount: 0,
+  },
+}
+
+const savedArtwork = {
+  isSaved: true,
+  customCollections: {
+    totalCount: 0,
+  },
+}
---
+++ src/__generated__/ArtworkActionsSaveButtonV2_Test_Query.graphql.ts
@@ -0,0 +1,341 @@
+/**
+ * @generated SignedSource<<ac05be9aba3057ed59fb9f042b579e5d>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type ArtworkActionsSaveButtonV2_Test_Query$variables = {};
+export type ArtworkActionsSaveButtonV2_Test_Query$data = {
+  readonly artwork: {
+    readonly " $fragmentSpreads": FragmentRefs<"ArtworkActionsSaveButtonV2_artwork">;
+  } | null;
+};
+export type ArtworkActionsSaveButtonV2_Test_Query = {
+  response: ArtworkActionsSaveButtonV2_Test_Query$data;
+  variables: ArtworkActionsSaveButtonV2_Test_Query$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "id",
+    "value": "artworkID"
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "slug",
+  "storageKey": null
+},
+v3 = {
+  "enumValues": null,
+  "nullable": true,
+  "plural": false,
+  "type": "String"
+},
+v4 = {
+  "enumValues": null,
+  "nullable": false,
+  "plural": false,
+  "type": "ID"
+},
+v5 = {
+  "enumValues": null,
+  "nullable": true,
+  "plural": false,
+  "type": "Boolean"
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ArtworkActionsSaveButtonV2_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Artwork",
+        "kind": "LinkedField",
+        "name": "artwork",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "ArtworkActionsSaveButtonV2_artwork"
+          }
+        ],
+        "storageKey": "artwork(id:\"artworkID\")"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "ArtworkActionsSaveButtonV2_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": "Artwork",
+        "kind": "LinkedField",
+        "name": "artwork",
+        "plural": false,
+        "selections": [
+          (v1/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "internalID",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "isSaved",
+            "storageKey": null
+          },
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "title",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "date",
+            "storageKey": null
+          },
+          {
+            "alias": "preview",
+            "args": null,
+            "concreteType": "Image",
+            "kind": "LinkedField",
+            "name": "image",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": [
+                  {
+                    "kind": "Literal",
+                    "name": "version",
+                    "value": "square"
+                  }
+                ],
+                "kind": "ScalarField",
+                "name": "url",
+                "storageKey": "url(version:\"square\")"
+              }
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": "customCollections",
+            "args": [
+              {
+                "kind": "Literal",
+                "name": "default",
+                "value": false
+              },
+              {
+                "kind": "Literal",
+                "name": "first",
+                "value": 0
+              },
+              {
+                "kind": "Literal",
+                "name": "saves",
+                "value": true
+              }
+            ],
+            "concreteType": "CollectionsConnection",
+            "kind": "LinkedField",
+            "name": "collectionsConnection",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "totalCount",
+                "storageKey": null
+              }
+            ],
+            "storageKey": "collectionsConnection(default:false,first:0,saves:true)"
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "Sale",
+            "kind": "LinkedField",
+            "name": "sale",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "isAuction",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "isClosed",
+                "storageKey": null
+              },
+              (v1/*: any*/),
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "isLiveOpen",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "isRegistrationClosed",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "liveStartAt",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "Bidder",
+                "kind": "LinkedField",
+                "name": "registrationStatus",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "qualifiedForBidding",
+                    "storageKey": null
+                  },
+                  (v1/*: any*/)
+                ],
+                "storageKey": null
+              },
+              (v2/*: any*/),
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "registrationEndsAt",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          }
+        ],
+        "storageKey": "artwork(id:\"artworkID\")"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "ef01d6b0f97ef6b68b7bdbf0b5c2b2e3",
+    "id": null,
+    "metadata": {
+      "relayTestingSelectionTypeInfo": {
+        "artwork": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Artwork"
+        },
+        "artwork.customCollections": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "CollectionsConnection"
+        },
+        "artwork.customCollections.totalCount": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Int"
+        },
+        "artwork.date": (v3/*: any*/),
+        "artwork.id": (v4/*: any*/),
+        "artwork.internalID": (v4/*: any*/),
+        "artwork.isSaved": (v5/*: any*/),
+        "artwork.preview": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Image"
+        },
+        "artwork.preview.url": (v3/*: any*/),
+        "artwork.sale": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Sale"
+        },
+        "artwork.sale.id": (v4/*: any*/),
+        "artwork.sale.isAuction": (v5/*: any*/),
+        "artwork.sale.isClosed": (v5/*: any*/),
+        "artwork.sale.isLiveOpen": (v5/*: any*/),
+        "artwork.sale.isRegistrationClosed": (v5/*: any*/),
+        "artwork.sale.liveStartAt": (v3/*: any*/),
+        "artwork.sale.registrationEndsAt": (v3/*: any*/),
+        "artwork.sale.registrationStatus": {
+          "enumValues": null,
+          "nullable": true,
+          "plural": false,
+          "type": "Bidder"
+        },
+        "artwork.sale.registrationStatus.id": (v4/*: any*/),
+        "artwork.sale.registrationStatus.qualifiedForBidding": (v5/*: any*/),
+        "artwork.sale.slug": (v4/*: any*/),
+        "artwork.slug": (v4/*: any*/),
+        "artwork.title": (v3/*: any*/)
+      }
+    },
+    "name": "ArtworkActionsSaveButtonV2_Test_Query",
+    "operationKind": "query",
+    "text": "query ArtworkActionsSaveButtonV2_Test_Query {\n  artwork(id: \"artworkID\") {\n    ...ArtworkActionsSaveButtonV2_artwork\n    id\n  }\n}\n\nfragment ArtworkActionsSaveButtonV2_artwork on Artwork {\n  id\n  internalID\n  isSaved\n  slug\n  title\n  date\n  preview: image {\n    url(version: \"square\")\n  }\n  customCollections: collectionsConnection(first: 0, default: false, saves: true) {\n    totalCount\n  }\n  sale {\n    isAuction\n    isClosed\n    id\n  }\n  ...ArtworkActionsWatchLotButton_artwork\n}\n\nfragment ArtworkActionsWatchLotButton_artwork on Artwork {\n  sale {\n    isLiveOpen\n    isRegistrationClosed\n    liveStartAt\n    registrationStatus {\n      qualifiedForBidding\n      id\n    }\n    id\n  }\n  isSaved\n  ...ArtworkAuctionRegistrationPanel_artwork\n}\n\nfragment ArtworkAuctionRegistrationPanel_artwork on Artwork {\n  sale {\n    slug\n    registrationEndsAt\n    isRegistrationClosed\n    id\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "ac0317764eff3dbb503f31e06ebecf4c";
+
+export default node;
