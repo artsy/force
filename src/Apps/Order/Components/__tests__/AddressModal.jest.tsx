056edef24330e08c2533a2595f54388bee902d16
Alican Akyuz <42584148+AlicanAkyuz@users.noreply.github.com>
2023-04-14 11:17:58 +0200

    fix(TX-1019): Error display for invalid phone number submission via Add Address modal (#12238)
    * fix: add createUserAddress mutations returned error format to look up table
    
    * test: phone err in adding address validation
    
    * test: fix

--- src/Apps/Order/Components/__tests__/AddressModal.jest.tsx
+++ src/Apps/Order/Components/__tests__/AddressModal.jest.tsx
@@ -243,43 +243,43 @@ describe("AddressModal", () => {
 
       expect(wrapper.find(errorBoxQuery).text()).toContain(GENERIC_FAIL_MESSAGE)
     })
-  })
 
-  it("sets formik error when mutation returns phone validation error", async () => {
-    let wrapper = getWrapper(testAddressModalProps)
+    it("sets formik error when address mutation returns phone validation error", async () => {
+      let wrapper = getWrapper(testAddressModalProps)
 
-    commitMutation.mockImplementationOnce((_, { onCompleted }) =>
-      onCompleted({
-        updateUserAddress: {
-          userAddressOrErrors: {
-            errors: [
-              {
-                message:
-                  "Validation failed for phone: not a valid phone number",
-              },
-            ],
+      commitMutation.mockImplementationOnce((_, { onCompleted }) =>
+        onCompleted({
+          updateUserAddress: {
+            userAddressOrErrors: {
+              errors: [
+                {
+                  message:
+                    "Validation failed for phone: not a valid phone number",
+                },
+              ],
+            },
           },
-        },
+        })
+      )
+
+      const formik = wrapper.find("Formik").first()
+      const setFieldError = jest.fn()
+
+      const onSubmit = formik.props().onSubmit as any
+      onSubmit(validAddress as any, {
+        setFieldError: setFieldError,
+        setSubmitting: jest.fn(),
       })
-    )
 
-    const formik = wrapper.find("Formik").first()
-    const setFieldError = jest.fn()
+      await wrapper.update()
 
-    const onSubmit = formik.props().onSubmit as any
-    onSubmit(validAddress as any, {
-      setFieldError: setFieldError,
-      setSubmitting: jest.fn(),
+      await tick()
+
+      expect(setFieldError).toHaveBeenCalledWith(
+        "phoneNumber",
+        "Please enter a valid phone number"
+      )
     })
-
-    await wrapper.update()
-
-    await tick()
-
-    expect(setFieldError).toHaveBeenCalledWith(
-      "phoneNumber",
-      "Please enter a valid phone number"
-    )
   })
 })
 
