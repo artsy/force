8a99d65e0b3a08b9104ae548ee4e43f9100e7e33
Dima Tretyak <itretyak@ya.ru>
2023-04-14 17:52:39 +0300

    chore: pass `onlyVisible` arg for `artworksCount` (#12246)

--- src/Apps/CollectorProfile/Routes/Saves2/Components/Actions/Mutations/useCreateCollection.ts
+++ src/Apps/CollectorProfile/Routes/Saves2/Components/Actions/Mutations/useCreateCollection.ts
@@ -50,7 +50,7 @@ export const useCreateCollection = () => {
               collection {
                 internalID
                 name
-                artworksCount
+                artworksCount(onlyVisible: true)
               }
             }
 
