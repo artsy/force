a85ebab2986e8423a6cdc0753395f11d2fa2c6af
dzucconi <mail@damonzucconi.com>
2023-04-21 14:33:28 -0400

    feat(alerts): completes alerts onboarding tips

---
+++ src/Components/ProgressiveOnboarding/ProgressiveOnboardingAlertHighlight.tsx
@@ -0,0 +1,59 @@
+import {
+  PROGRESSIVE_ONBOARDING_ALERT_HIGHLIGHT,
+  PROGRESSIVE_ONBOARDING_ALERT_FIND,
+  useProgressiveOnboarding,
+} from "Components/ProgressiveOnboarding/ProgressiveOnboardingContext"
+import {
+  ProgressiveOnboardingHighlight,
+  ProgressiveOnboardingHighlightPosition,
+} from "Components/ProgressiveOnboarding/ProgressiveOnboardingHighlight"
+import { FC, useEffect } from "react"
+
+interface ProgressiveOnboardingAlertHighlightProps {
+  position: ProgressiveOnboardingHighlightPosition
+}
+
+export const ProgressiveOnboardingAlertHighlight: FC<ProgressiveOnboardingAlertHighlightProps> = ({
+  children,
+  position,
+}) => {
+  const { isDismissed, dismiss, isEnabledFor } = useProgressiveOnboarding()
+
+  const isDisplayable =
+    // If the feature is enabled
+    isEnabledFor("alerts") &&
+    // And you haven't already dismissed this
+    !isDismissed(PROGRESSIVE_ONBOARDING_ALERT_HIGHLIGHT).status &&
+    // And you've previously dismissed the previous onboarding tip
+    isDismissed(PROGRESSIVE_ONBOARDING_ALERT_FIND).status &&
+    // And you've dismissed the previous step within the last 20 seconds
+    isDismissed(PROGRESSIVE_ONBOARDING_ALERT_FIND).timestamp >
+      Date.now() - 20 * 1000
+
+  useEffect(() => {
+    if (!isDisplayable) return
+
+    const handleClick = () => {
+      dismiss(PROGRESSIVE_ONBOARDING_ALERT_HIGHLIGHT)
+    }
+
+    document.addEventListener("click", handleClick, { once: true })
+
+    return () => {
+      document.removeEventListener("click", handleClick)
+    }
+  }, [dismiss, isDisplayable])
+
+  if (!isDisplayable) {
+    return <>{children}</>
+  }
+
+  return (
+    <ProgressiveOnboardingHighlight
+      position={position}
+      name={PROGRESSIVE_ONBOARDING_ALERT_HIGHLIGHT}
+    >
+      {children}
+    </ProgressiveOnboardingHighlight>
+  )
+}
