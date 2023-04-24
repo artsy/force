cbb2c519dc078d4c883ea84a9af54fa5405eed56
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2023-04-24 11:48:54 -0400

    refactor: remove ? from useAuthValidation.ts as requested
    - https://github.com/artsy/force/pull/12243#discussion_r1175351969

--- src/Utils/Hooks/useAuthValidation.ts
+++ src/Utils/Hooks/useAuthValidation.ts
@@ -17,7 +17,7 @@ export const useAuthValidation = () => {
           }
         `,
         {}
-      )?.toPromise()
+      ).toPromise()
 
       if (data?.authenticationStatus === "INVALID") {
         try {
