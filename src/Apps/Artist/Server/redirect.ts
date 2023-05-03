5431bf01c586708b019fe76d8b4d15c0284070cf
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-05-03 18:16:58 -0400

    fix: 301 redirect invalid artist urls from trending score a/b

---
+++ src/Apps/Artist/Server/redirect.ts
@@ -0,0 +1,18 @@
+export function redirectWithCanonicalParams({ req, res }) {
+  if (hasCanonicalSort(req.query)) {
+    // all good
+    return
+  }
+
+  // else remove the offending sort, causing fallback to default sort
+  delete req.query.sort
+
+  const sanitizedQuerystring = new URLSearchParams(req.query).toString()
+  const sanitizedPath = [req.path, sanitizedQuerystring].join("?")
+  res.redirect(301, sanitizedPath)
+}
+
+function hasCanonicalSort(query: { sort: string }) {
+  // See https://artsyproduct.atlassian.net/browse/FX-4778
+  return query.sort != "-default_trending_score"
+}
