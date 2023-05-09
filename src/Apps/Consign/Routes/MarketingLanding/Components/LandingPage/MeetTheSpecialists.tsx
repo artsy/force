e50820394513e6bc32fdaa817b1829b8dbf97b63
Ole <ole.richter91@gmail.com>
2023-05-09 16:19:02 +0200

    fix: Adjust SWA Meet the Specialist section pill styling

--- src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/MeetTheSpecialists.tsx
+++ src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/MeetTheSpecialists.tsx
@@ -2,7 +2,7 @@ import { ActionType, ContextModule } from "@artsy/cohesion"
 import {
   Box,
   Button,
-  Flex,
+  HorizontalOverflow,
   Image,
   Pill,
   ReadMore,
@@ -96,7 +96,7 @@ export const MeetTheSpecialists: React.FC = () => {
       <Text mb={2} variant={["xs", "sm"]}>
         Our specialists span today’s most popular collecting categories.
       </Text>
-      <Flex overflowY="scroll">
+      <HorizontalOverflow>
         {filteredPills.map(pill => (
           <Pill
             mr={1}
@@ -109,20 +109,11 @@ export const MeetTheSpecialists: React.FC = () => {
                 SPECIALISTS.filter(i => i.specialty === pill.type)
               )
             }}
-            style={
-              selectedSpecialty === pill.type
-                ? {
-                    color: "white",
-                    backgroundColor: "black",
-                    borderColor: "black",
-                  }
-                : undefined
-            }
           >
             {pill.title}
           </Pill>
         ))}
-      </Flex>
+      </HorizontalOverflow>
       <Rail
         title=""
         getItems={() => {
