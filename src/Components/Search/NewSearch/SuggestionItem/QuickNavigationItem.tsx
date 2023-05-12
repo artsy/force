072e3156148ab91e2b223693a326f436773d93b1
Nikita Skalkin <nikskalkin@gmail.com>
2023-05-12 12:44:14 +0200

    feat(FX-4655): fixes new search bar quicklink navigation (#12394)

--- src/Components/Search/NewSearch/SuggestionItem/QuickNavigationItem.tsx
+++ src/Components/Search/NewSearch/SuggestionItem/QuickNavigationItem.tsx
@@ -1,26 +1,68 @@
 import { Pill, PillProps } from "@artsy/palette"
+import { RouterLink } from "System/Router/RouterLink"
 import { FC } from "react"
+import {
+  ActionType,
+  ContextModule,
+  SelectedSearchSuggestionQuickNavigationItem,
+} from "@artsy/cohesion"
+import { useTracking } from "react-tracking"
+
+type Label = SelectedSearchSuggestionQuickNavigationItem["label"]
 
 interface QuickNavigationItemProps {
+  label: Label
   to: string
   onClick?: (event: React.MouseEvent<HTMLAnchorElement>) => void
 }
 
 export const QuickNavigationItem: FC<QuickNavigationItemProps & PillProps> = ({
+  label,
   to,
   onClick,
+  onMouseDown,
   ...rest
 }) => {
-  const handleClick = event => {
-    // Stopping the event from propagating to prevent SearchBar from navigation to the main suggestion item url.
-    event.stopPropagation()
-    event.preventDefault()
-    onClick?.(event)
+  const { trackEvent } = useTracking()
 
-    // TODO: Use routerLink instead of manual navigation?
-    // FIXME: Using `window.location.assign(to)` instead of `router.push(to)` to prevent a bug where the search bar won't hide anymore.
-    window.location.assign(to)
+  const track = () => {
+    const event: SelectedSearchSuggestionQuickNavigationItem = {
+      context_module: ContextModule.header,
+      destination_path: to,
+      action: ActionType.selectedSearchSuggestionQuickNavigationItem,
+      label,
+    }
+
+    trackEvent(event)
   }
 
-  return <Pill onClick={handleClick} mt={1} mr={1} {...rest} />
+  const handleClick = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    track()
+    onClick?.(event)
+  }
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    event.stopPropagation()
+    onMouseDown?.(event)
+  }
+
+  return (
+    <Pill
+      as={RouterLink}
+      // @ts-ignore
+      to={to}
+      mt={1}
+      mr={1}
+      onClick={handleClick}
+      onMouseDown={handleMouseDown}
+      {...rest}
+    >
+      {label}
+    </Pill>
+  )
 }
