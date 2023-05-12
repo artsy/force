f699de2038800ac7ffd66e892b7b22c9c9b70c3c
Nikita Skalkin <nikskalkin@gmail.com>
2023-05-12 10:36:38 +0200

    feat(FX-4655): fixes new search bar quicklink navigation

--- src/Components/Search/NewSearch/SuggestionItem/QuickNavigation.tsx
+++ src/Components/Search/NewSearch/SuggestionItem/QuickNavigation.tsx
@@ -1,14 +1,8 @@
 import { FC } from "react"
 import { SuggionItemOptionProps } from "./NewSuggestionItem"
-import { useTracking } from "react-tracking"
 import { Flex } from "@artsy/palette"
 import GavelIcon from "@artsy/icons/GavelIcon"
 import ArtworkIcon from "@artsy/icons/ArtworkIcon"
-import {
-  ActionType,
-  ContextModule,
-  SelectedSearchSuggestionQuickNavigationItem,
-} from "@artsy/cohesion"
 import { QuickNavigationItem } from "./QuickNavigationItem"
 
 interface QuickNavigationProps {
@@ -16,26 +10,6 @@ interface QuickNavigationProps {
 }
 
 export const QuickNavigation: FC<QuickNavigationProps> = ({ option }) => {
-  const { trackEvent } = useTracking()
-
-  const handleArtworksItemClicked = () => {
-    trackEvent(
-      tracks.quickNavigationItemClicked({
-        destinationPath: `${option.href}/works-for-sale`,
-        label: "Artworks",
-      })
-    )
-  }
-
-  const handleAuctionResultsItemClicked = () => {
-    trackEvent(
-      tracks.quickNavigationItemClicked({
-        destinationPath: `${option.href}/auction-results`,
-        label: "Auction Results",
-      })
-    )
-  }
-
   if (!option.showArtworksButton && !option.showAuctionResultsButton)
     return null
 
@@ -43,37 +17,18 @@ export const QuickNavigation: FC<QuickNavigationProps> = ({ option }) => {
     <Flex flexWrap="wrap">
       {!!option.showArtworksButton && (
         <QuickNavigationItem
-          onClick={handleArtworksItemClicked}
           to={`${option.href}/works-for-sale`}
           Icon={ArtworkIcon}
-        >
-          Artworks
-        </QuickNavigationItem>
+          label="Artworks"
+        />
       )}
       {!!option.showAuctionResultsButton && (
         <QuickNavigationItem
-          onClick={handleAuctionResultsItemClicked}
           to={`${option.href}/auction-results`}
           Icon={GavelIcon}
-        >
-          Auction Results
-        </QuickNavigationItem>
+          label="Auction Results"
+        />
       )}
     </Flex>
   )
-}
-
-const tracks = {
-  quickNavigationItemClicked: ({
-    destinationPath,
-    label,
-  }: {
-    destinationPath: string
-    label: "Auction Results" | "Artworks"
-  }): SelectedSearchSuggestionQuickNavigationItem => ({
-    context_module: ContextModule.header,
-    destination_path: destinationPath,
-    action: ActionType.selectedSearchSuggestionQuickNavigationItem,
-    label,
-  }),
 }
