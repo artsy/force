70931e226817607e031b9e2a6cf4fd98c4306e70
kajatiger <ksantro@gmx.de>
2023-05-15 18:27:18 +0200

    center everything properly in the grid Co-authored-by: Tanjie <tanjmcmeans01@gmail.com> Co-authored-by: Chriss <damassi.pappas@gmail.com> Co-authored-by: Damon <mail@damonzucconi.com>

--- src/Apps/Artist/Routes/AuctionResults/Components/EmptyArtistAuctionResults.tsx
+++ src/Apps/Artist/Routes/AuctionResults/Components/EmptyArtistAuctionResults.tsx
@@ -4,13 +4,13 @@ import { RouterLink } from "System/Router/RouterLink"
 export const EmptyArtistAuctionResults: React.FC = () => {
   return (
     <Flex>
-      <GridColumns width="100%" gridRowGap={[4, 2]}>
-        <Column span={12} alignItems="center">
+      <GridColumns width="100%" gridRowGap={2}>
+        <Column span={6} start={4}>
           <Text variant="md" textAlign="center">
             There are no auction results for this artist at this time.
           </Text>
         </Column>
-        <Column span={12} alignItems="center">
+        <Column span={6} start={4}>
           <Text variant="md" color="black60" textAlign="center">
             The page will be updated once auction results are added. In the
             meantime, check out the millions of auction results and art market
@@ -18,7 +18,7 @@ export const EmptyArtistAuctionResults: React.FC = () => {
           </Text>
         </Column>
         <Column span={12} alignItems="center">
-          <Flex>
+          <Flex width="100%" justifyContent="center">
             <RouterLink to="/price-database">
               <Button variant="secondaryNeutral">
                 View Artsy’s Price Database
