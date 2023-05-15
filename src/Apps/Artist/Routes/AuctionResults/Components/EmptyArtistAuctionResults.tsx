269d882a97ff4a9b24875b1903cf507674ca10a2
kajatiger <ksantro@gmx.de>
2023-05-15 17:12:23 +0200

    add some styling to empty page

--- src/Apps/Artist/Routes/AuctionResults/Components/EmptyArtistAuctionResults.tsx
+++ src/Apps/Artist/Routes/AuctionResults/Components/EmptyArtistAuctionResults.tsx
@@ -1,13 +1,29 @@
-import { Flex, Text } from "@artsy/palette"
+import { Flex, Text, Button, GridColumns, Column } from "@artsy/palette"
 
 export const EmptyArtistAuctionResults: React.FC = () => {
   return (
     <Flex>
-      <Text>There are no auction results for this artist at this time.</Text>
-      <Text>
-        The page will be updated once auction results are added. In the
-        meantime, check out the millions of auction results and art market data.
-      </Text>
+      <GridColumns width="100%" gridRowGap={[4, 2]}>
+        <Column span={12} alignItems="center">
+          <Text variant="md" textAlign="center">
+            There are no auction results for this artist at this time.
+          </Text>
+        </Column>
+        <Column span={12} alignItems="center">
+          <Text variant="md" color="black60" textAlign="center">
+            The page will be updated once auction results are added. In the
+            meantime, check out the millions of auction results and art market
+            data.
+          </Text>
+        </Column>
+        <Column span={12} alignItems="center">
+          <Flex>
+            <Button variant="secondaryNeutral">
+              View Artsy’s Price Database
+            </Button>
+          </Flex>
+        </Column>
+      </GridColumns>
     </Flex>
   )
 }
