c6f8b5ea18defdcb5053f59293478e65ef7bbc84
Alican Akyuz <alicakyz@gmail.com>
2023-05-25 12:29:24 +0200

    test: form change

--- src/Apps/Order/Components/__tests__/CheckoutAddress.jest.tsx
+++ src/Apps/Order/Components/__tests__/CheckoutAddress.jest.tsx
@@ -1,4 +1,4 @@
-import { screen, render } from "@testing-library/react"
+import { screen, render, fireEvent } from "@testing-library/react"
 import { Formik } from "formik"
 import {
   CheckoutAddress,
@@ -28,6 +28,12 @@ const mockOnChange = jest.fn((address: Address, key: keyof Address) => null)
 const checkoutAddressProps = {
   userCountry: "",
   onChange: mockOnChange,
+}
+
+const fireChangeEvent = (input: HTMLElement, value: string) => {
+  fireEvent.change(input, {
+    target: { value: value },
+  })
 }
 
 describe("CheckoutAddress form", () => {
@@ -65,5 +71,111 @@ describe("CheckoutAddress form", () => {
     expect(
       screen.queryByPlaceholderText("Add state, province, or region")
     ).toBeInTheDocument()
+  })
+
+  describe("change handler fired correctly", () => {
+    it("for name", () => {
+      const nameInput = screen.getByLabelText("address-name-input")
+      fireChangeEvent(nameInput, "John Doe")
+      expect(mockOnChange).toHaveBeenCalledWith(
+        {
+          name: "John Doe",
+          country: "",
+          addressLine1: "",
+          addressLine2: "",
+          city: "",
+          postalCode: "",
+          region: "",
+        },
+        "name"
+      )
+    })
+
+    it("for 1st address line", () => {
+      const addressLineInput = screen.getByLabelText("address-street-input")
+      fireChangeEvent(addressLineInput, "Weserstr. 175")
+      expect(mockOnChange).toHaveBeenCalledWith(
+        {
+          name: "",
+          country: "",
+          addressLine1: "Weserstr. 175",
+          addressLine2: "",
+          city: "",
+          postalCode: "",
+          region: "",
+        },
+        "addressLine1"
+      )
+    })
+
+    it("for 2nd address line", () => {
+      const addressLineInput2nd = screen.getByLabelText(
+        "address-optional-second-line-input"
+      )
+      fireChangeEvent(addressLineInput2nd, "Hinterhaus, links")
+      expect(mockOnChange).toHaveBeenCalledWith(
+        {
+          name: "",
+          country: "",
+          addressLine1: "",
+          addressLine2: "Hinterhaus, links",
+          city: "",
+          postalCode: "",
+          region: "",
+        },
+        "addressLine2"
+      )
+    })
+
+    it("for postal code", () => {
+      const postalCodeInput = screen.getByLabelText("address-postal-code-input")
+      fireChangeEvent(postalCodeInput, "12045")
+      expect(mockOnChange).toHaveBeenCalledWith(
+        {
+          name: "",
+          country: "",
+          addressLine1: "",
+          addressLine2: "",
+          city: "",
+          postalCode: "12045",
+          region: "",
+        },
+        "postalCode"
+      )
+    })
+
+    it("for city", () => {
+      const cityInput = screen.getByLabelText("address-city-input")
+      fireChangeEvent(cityInput, "Berlin")
+      expect(mockOnChange).toHaveBeenCalledWith(
+        {
+          name: "",
+          country: "",
+          addressLine1: "",
+          addressLine2: "",
+          city: "Berlin",
+          postalCode: "",
+          region: "",
+        },
+        "city"
+      )
+    })
+
+    it("for region", () => {
+      const regionInput = screen.getByLabelText("address-state-or-region-input")
+      fireChangeEvent(regionInput, "Berlin")
+      expect(mockOnChange).toHaveBeenCalledWith(
+        {
+          name: "",
+          country: "",
+          addressLine1: "",
+          addressLine2: "",
+          city: "",
+          postalCode: "",
+          region: "Berlin",
+        },
+        "region"
+      )
+    })
   })
 })
