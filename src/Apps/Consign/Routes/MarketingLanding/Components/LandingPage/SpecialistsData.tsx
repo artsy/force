98567f256f5aab0006ebf371144cff93f2e8253e
Mounir Dhahri <soussouheros@gmail.com>
2023-06-28 14:36:10 +0200

    chore: update list of specialists (#12568)

--- src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/SpecialistsData.tsx
+++ src/Apps/Consign/Routes/MarketingLanding/Components/LandingPage/SpecialistsData.tsx
@@ -30,16 +30,6 @@ export const SPECIALISTS: SpecialistsData[] = [
     email: "shlomi.rabi@artsy.net",
   },
   {
-    specialty: "priveteSalesAndAdvisory",
-    name: "Akanksha Ballaney",
-    firstName: "Akanksha",
-    jobTitle: "Director, Private Sales",
-    bio:
-      "Akanksha leads Artsy’s private sales team, assisting our global audience to buy and sell artworks across today’s most popular collecting categories. With Artsy since 2018, she has 10+ years of experience in art fairs, galleries, auction houses, and art-tech platforms.",
-    imageUrl: "https://files.artsy.net/images/Akanksha-Mobile.png",
-    email: "akanksha@artsy.net",
-  },
-  {
     specialty: "auctions",
     name: "Christine Aschwald",
     firstName: "Christine",
@@ -58,16 +48,6 @@ export const SPECIALISTS: SpecialistsData[] = [
       "With 14+ years in the auction industry, Lauren works with primary and secondary markets. Her focus is ultra-contemporary and established post-war markets for auction and private sale. In 2020, she established the Artsy Auctions division. She has worked across multiple auction houses, including as a specialist in the modern & contemporary department at Heritage Auctions.",
     imageUrl: "https://files.artsy.net/images/Lauren-Mobile.png",
     email: "lauren.carpinelli@artsy.net",
-  },
-  {
-    specialty: "auctions",
-    name: "Celine Cunha",
-    firstName: "Celine",
-    jobTitle: "Specialist in Post-War & Contemporary Art",
-    bio:
-      "With 13+ years of experience, Celine specializes in emerging and post-war artist markets. Her background includes MoMA-PS1 and Christie’s, where she was a post-war and contemporary specialist focused on auction and private sales, and led corporate social responsibility initiatives. She holds a Contemporary Art Business degree from Christie’s Education, and a BA from Cornell University.",
-    imageUrl: "https://files.artsy.net/images/Celine-Mobile.png",
-    email: "celine.cunha@artsymail.com",
   },
   {
     specialty: "auctions",
@@ -98,16 +78,6 @@ export const SPECIALISTS: SpecialistsData[] = [
       "Caroline advises collectors on buying and selling hard-to-access unique artworks. Her passion and foremost expertise is emerging and ultra contemporary artist markets. With Artsy since 2016, she holds a Bachelor of Arts in Art History from Wake Forest University.",
     imageUrl: "https://files.artsy.net/images/Caroline-Mobile.png",
     email: "caroline.perkins@artsy.net",
-  },
-  {
-    specialty: "priveteSalesAndAdvisory",
-    name: "Robin Roche",
-    firstName: "Robin",
-    jobTitle: "Senior Private Sales Director",
-    bio:
-      "Robin has 20+ years experience in modern, contemporary, and emerging art, including at Christie's (10 years), Gerald Peters Gallery (5 years), Auctionata and Artnet. She was instrumental in developing Artsy’s private sales business, and brokered our first 7-figure private sale.",
-    imageUrl: "https://files.artsy.net/images/Robin-Mobile.png",
-    email: "robin.roche@artsy.net",
   },
   {
     specialty: "auctions",
