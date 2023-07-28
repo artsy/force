78aec5f0e94531c12af734d37c56fef715997993
Chung-Yi Chi <chung-yi@artsymail.com>
2023-07-28 16:40:22 -0400

    test: add a test to make sure basic form validation happens before address verification

--- src/Apps/Order/Routes/__tests__/Shipping.jest.tsx
+++ src/Apps/Order/Routes/__tests__/Shipping.jest.tsx
@@ -1029,6 +1029,38 @@ describe("Shipping", () => {
 
           expect(mockCommitMutation).toHaveBeenCalled()
         })
+
+        describe("with address verfication enabled", () => {
+          beforeAll(() => {
+            ;(useFeatureFlag as jest.Mock).mockImplementation(
+              (featureName: string) => featureName === "address_verification_us"
+            )
+          })
+
+          afterAll(() => {
+            ;(useFeatureFlag as jest.Mock).mockReset()
+          })
+
+          it("triggers basic form validation first", async () => {
+            const wrapper = getWrapper({
+              CommerceOrder: () => testOrder,
+              Me: () => emptyTestMe,
+            })
+
+            const page = new ShippingTestPage(wrapper)
+
+            await page.clickSubmit()
+
+            const input = page
+              .find(Input)
+              .filterWhere(wrapper => wrapper.props().title === "Full name")
+            expect(input.props().error).toEqual("This field is required")
+
+            expect(
+              page.find(`[data-testid="address-verification-flow"]`).exists()
+            ).toBe(false)
+          })
+        })
       })
 
       it("does submit the mutation with a non-ship order", async () => {
