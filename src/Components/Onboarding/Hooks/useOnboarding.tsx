615f5a378a091cda4d9d4ee23d6478e2c91f4510
dzucconi <mail@damonzucconi.com>
2023-07-31 13:53:49 -0400

    fix(onboarding): avoids extraneous re-renders

--- src/Components/Onboarding/Hooks/useOnboarding.tsx
+++ src/Components/Onboarding/Hooks/useOnboarding.tsx
@@ -1,24 +1,36 @@
-import { useDialog } from "Utils/Hooks/useDialog"
 import { OnboardingModal } from "Components/Onboarding/Components/OnboardingModal"
 import { OnboardingSteps } from "Components/Onboarding/Components/OnboardingSteps"
 import { OnboardingProvider } from "./useOnboardingContext"
+import { useMemo, useState } from "react"
 
 interface UseOnboarding {
   onClose(): void
 }
 
 export const useOnboarding = ({ onClose }: UseOnboarding) => {
-  const { isVisible, showDialog, hideDialog, dialogComponent } = useDialog({
-    Dialog: () => {
-      return (
-        <OnboardingProvider onClose={onClose}>
-          <OnboardingModal onClose={hideDialog}>
-            <OnboardingSteps />
-          </OnboardingModal>
-        </OnboardingProvider>
-      )
-    },
-  })
+  const [isVisible, setIsVisible] = useState(false)
+
+  const showDialog = () => {
+    setIsVisible(true)
+  }
+
+  const hideDialog = () => {
+    setIsVisible(false)
+  }
+
+  const dialogComponent = useMemo(() => {
+    return (
+      <>
+        {isVisible && (
+          <OnboardingProvider onClose={onClose}>
+            <OnboardingModal onClose={hideDialog}>
+              <OnboardingSteps />
+            </OnboardingModal>
+          </OnboardingProvider>
+        )}
+      </>
+    )
+  }, [isVisible, onClose])
 
   return {
     isVisible,
