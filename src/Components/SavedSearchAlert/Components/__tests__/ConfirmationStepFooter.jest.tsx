90d3454a8be75c646de5f05dddeed494c0413730
Nikita Skalkin <nikskalkin@gmail.com>
2023-08-01 11:52:57 +0200

    fix(onyx-158): Hide See all matching works button on alerts confirmation step when <= 10 artworks match criteria

--- src/Components/SavedSearchAlert/Components/__tests__/ConfirmationStepFooter.jest.tsx
+++ src/Components/SavedSearchAlert/Components/__tests__/ConfirmationStepFooter.jest.tsx
@@ -3,6 +3,7 @@ import { setupTestWrapperTL } from "DevTools/setupTestWrapper"
 import { graphql } from "react-relay"
 import { ConfirmationStepFooter_Test_Query } from "__generated__/ConfirmationStepFooter_Test_Query.graphql"
 import { screen } from "@testing-library/react"
+import { NUMBER_OF_ARTWORKS_TO_SHOW } from "Components/SavedSearchAlert/ConfirmationArtworksGrid"
 
 jest.unmock("react-relay")
 
@@ -24,35 +25,59 @@ describe("ConfirmationStepFooter", () => {
     `,
   })
 
-  const renderComponent = () => {
-    renderWithRelay({
-      Me: () => ({
-        internalID: "1234",
-        savedSearch: {
-          href:
-            "/artist/banksy?sort=-published_at&attribution_class%5B0%5D=unique&additional_gene_ids%5B0%5D=prints&for_sale=true&search_criteria_id=289e87de-97e7-4c36-87aa-9fcd479b46b7",
-        },
-      }),
-    })
+  const renderComponent = (artworksCount: number) => {
+    renderWithRelay(
+      {
+        Me: () => ({
+          internalID: "1234",
+          savedSearch: {
+            href:
+              "/artist/banksy?sort=-published_at&attribution_class%5B0%5D=unique&additional_gene_ids%5B0%5D=prints&for_sale=true&search_criteria_id=289e87de-97e7-4c36-87aa-9fcd479b46b7",
+          },
+        }),
+      },
+      false,
+      { artworksCount: artworksCount }
+    )
   }
 
-  it("renders buttons", () => {
-    renderComponent()
+  describe("buttons", () => {
+    describe("when there are no artworks", () => {
+      it("renders only manage your alerts button", () => {
+        renderComponent(0)
 
-    expect(screen.getByText("See all matching works")).toBeInTheDocument()
-    expect(screen.getByText("Manage your alerts")).toBeInTheDocument()
-  })
+        expect(screen.getByText("Manage your alerts")).toBeInTheDocument()
+      })
+    })
 
-  it("redirects to the alerts page", () => {
-    renderComponent()
+    describe("when there are <= 10 artworks", () => {
+      it("renders only manage your alerts button", () => {
+        renderComponent(NUMBER_OF_ARTWORKS_TO_SHOW)
 
-    expect(screen.getByTestId("seeAllMatchingWorksButton")).toHaveAttribute(
-      "href",
-      "/artist/banksy?sort=-published_at&attribution_class%5B0%5D=unique&additional_gene_ids%5B0%5D=prints&for_sale=true&search_criteria_id=289e87de-97e7-4c36-87aa-9fcd479b46b7"
-    )
-    expect(screen.getByTestId("manageYourAlertsButton")).toHaveAttribute(
-      "href",
-      "/settings/alerts"
-    )
+        expect(screen.getByText("Manage your alerts")).toBeInTheDocument()
+      })
+    })
+
+    describe("when there are > 10 artworks", () => {
+      it("renders both buttons", () => {
+        renderComponent(NUMBER_OF_ARTWORKS_TO_SHOW + 1)
+
+        expect(screen.getByText("See all matching works")).toBeInTheDocument()
+        expect(screen.getByText("Manage your alerts")).toBeInTheDocument()
+      })
+    })
+
+    it("buttons have correct redirect links", () => {
+      renderComponent(NUMBER_OF_ARTWORKS_TO_SHOW + 1)
+
+      expect(screen.getByTestId("seeAllMatchingWorksButton")).toHaveAttribute(
+        "href",
+        "/artist/banksy?sort=-published_at&attribution_class%5B0%5D=unique&additional_gene_ids%5B0%5D=prints&for_sale=true&search_criteria_id=289e87de-97e7-4c36-87aa-9fcd479b46b7"
+      )
+      expect(screen.getByTestId("manageYourAlertsButton")).toHaveAttribute(
+        "href",
+        "/settings/alerts"
+      )
+    })
   })
 })
