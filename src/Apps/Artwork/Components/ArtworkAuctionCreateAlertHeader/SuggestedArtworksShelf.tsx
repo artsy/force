d7cd893ef6f08edf42234dbe5c70cffcc15e5f75
tam-kis <kis.tamara91@gmail.com>
2023-08-25 11:37:03 +0200

    fix(ONYX-270): hide overflowing artworks

--- src/Apps/Artwork/Components/ArtworkAuctionCreateAlertHeader/SuggestedArtworksShelf.tsx
+++ src/Apps/Artwork/Components/ArtworkAuctionCreateAlertHeader/SuggestedArtworksShelf.tsx
@@ -26,7 +26,14 @@ export const SuggestedArtworksShelf: FC<SuggestedArtworksShelfProps> = ({
   const artworks = extractNodes(artworksConnection)
 
   return (
-    <Flex flexDirection="row" flexWrap="wrap" gap={2} justifyContent="center">
+    <Flex
+      flexDirection="row"
+      flexWrap="wrap"
+      gap={2}
+      justifyContent="center"
+      overflow="hidden"
+      height="340px"
+    >
       {artworks.map(artwork => (
         <ShelfArtworkFragmentContainer
           artwork={artwork}
