b0173db7a8321dc5ec5470f3d7b342c572c304cc
tam-kis <kis.tamara91@gmail.com>
2023-08-31 13:09:30 +0200

    fix(ONYX-274): remove unused wrapper

--- src/Apps/Artwork/Components/ArtworkAuctionCreateAlertHeader/SuggestedArtworksModalHeader.tsx
+++ src/Apps/Artwork/Components/ArtworkAuctionCreateAlertHeader/SuggestedArtworksModalHeader.tsx
@@ -6,28 +6,26 @@ export const SuggestedArtworksModalHeader: FC = () => {
   const { pills } = useSavedSearchAlertContext()
 
   return (
-    <>
-      <Flex flexDirection="column">
-        <Text variant="sm-display" textColor="black60" mb={2}>
-          Available works you may have missed based on similar filters listed
-          below.
-        </Text>
+    <Flex flexDirection="column">
+      <Text variant="sm-display" textColor="black60" mb={2}>
+        Available works you may have missed based on similar filters listed
+        below.
+      </Text>
 
-        <Flex flexWrap="wrap" gap={1}>
-          {pills.map(pill => {
-            return (
-              <Pill
-                key={`filter-label-${pill.field}-${pill.value}`}
-                variant="filter"
-                mb={1}
-                disabled
-              >
-                {pill.displayValue}
-              </Pill>
-            )
-          })}
-        </Flex>
+      <Flex flexWrap="wrap" gap={1}>
+        {pills.map(pill => {
+          return (
+            <Pill
+              key={`filter-label-${pill.field}-${pill.value}`}
+              variant="filter"
+              mb={1}
+              disabled
+            >
+              {pill.displayValue}
+            </Pill>
+          )
+        })}
       </Flex>
-    </>
+    </Flex>
   )
 }
