22d74055388bee162f8858ca53549672cb0dfca2
Adam Iskounen <adam.iskounen@artsymail.com>
2023-09-05 06:43:23 -0400

    feat: added test to assert that suggestions appear

--- src/Components/__tests__/AddressForm.jest.tsx
+++ src/Components/__tests__/AddressForm.jest.tsx
@@ -1,6 +1,19 @@
 import { screen, render, fireEvent } from "@testing-library/react"
 import { AddressForm } from "Components/AddressForm"
 import { FC, useState } from "react"
+import { useFeatureFlag } from "System/useFeatureFlag"
+
+jest.mock("System/useFeatureFlag", () => ({
+  useFeatureFlag: jest.fn(),
+}))
+
+jest.mock("Utils/getENV", () => ({
+  getENV: jest.fn().mockImplementation(() => {
+    return {
+      key: "smarty-api-key",
+    }
+  }),
+}))
 
 describe("AddressForm", () => {
   it("preserves updated value from props after user input", () => {
@@ -38,5 +51,50 @@ describe("AddressForm", () => {
     const city = screen.getByPlaceholderText("City")
     fireEvent.change(city, { target: { value: "New York" } })
     expect(line1.value).toBe("After update")
+  })
+
+  describe("address autocomplete is enabled", () => {
+    beforeAll(() => {
+      ;(useFeatureFlag as jest.Mock).mockImplementation(
+        featureName => featureName === "address_autocomplete_us"
+      )
+
+      const mockFetch = jest.fn().mockResolvedValue({
+        json: jest.fn().mockResolvedValue({
+          suggestions: [
+            {
+              city: "New York",
+              entries: 0,
+              secondary: "Fl 25",
+              state: "NY",
+              street_line: "401 Broadway",
+              zipcode: "10013",
+            },
+          ],
+        }),
+      })
+
+      // @ts-ignore
+      global.fetch = mockFetch
+    })
+
+    afterAll(() => {
+      jest.resetAllMocks()
+    })
+
+    describe("address line 1", () => {
+      it("shows suggestions", async () => {
+        render(<AddressForm onChange={jest.fn()} errors={{}} touched={{}} />)
+
+        const input = screen.getByPlaceholderText("Street address")
+        fireEvent.change(input, { target: { value: "401 Broadway" } })
+
+        const listbox = await screen.findByRole("listbox", { hidden: true })
+
+        expect(listbox).toHaveTextContent(
+          "401 Broadway Fl 25 New York, NY 10013"
+        )
+      })
+    })
   })
 })
