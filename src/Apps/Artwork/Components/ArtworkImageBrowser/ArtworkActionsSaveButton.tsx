d39fe6ecb4d80632808f38d88983761c57c52654
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2023-09-25 13:35:14 -0400

    chore(artwork lists): remove flag (and unreachable code) from image browser actions
    Ever since the feature flag was enabled in early August the
    deleted code in ArtworkImageBrowser/ArtworkActionsSaveButton
    has been unreachable.
    
    The equivalant functionality is covered by the remaining code
    path in ArtworkActionsSaveButtonV2FragmentContainer.

--- src/Apps/Artwork/Components/ArtworkImageBrowser/ArtworkActionsSaveButton.tsx
+++ src/Apps/Artwork/Components/ArtworkImageBrowser/ArtworkActionsSaveButton.tsx
@@ -1,13 +1,7 @@
-import { ContextModule } from "@artsy/cohesion"
 import { createFragmentContainer, graphql } from "react-relay"
-import { useSaveArtwork } from "Components/Artwork/SaveButton/useSaveArtwork"
 import { ArtworkActionsSaveButton_artwork$data } from "__generated__/ArtworkActionsSaveButton_artwork.graphql"
-import { ProgressiveOnboardingSaveArtwork } from "Components/ProgressiveOnboarding/ProgressiveOnboardingSaveArtwork"
-import { SaveUtilButton } from "Apps/Artwork/Components/ArtworkImageBrowser/SaveUtilButton"
-import { ArtworkActionsWatchLotButtonFragmentContainer } from "Apps/Artwork/Components/ArtworkImageBrowser/ArtworkActionsWatchLotButton"
 import { ArtworkActionsSaveButtonV2FragmentContainer } from "Apps/Artwork/Components/ArtworkImageBrowser/ArtworkActionsSaveButtonV2"
 import { ManageArtworkForSavesProvider } from "Components/Artwork/ManageArtworkForSaves"
-import { useCheckIfArtworkListsEnabled } from "Apps/CollectorProfile/Routes/Saves2/useCheckIfArtworkListsEnabled"
 
 interface ArtworkActionsSaveButtonProps {
   artwork: ArtworkActionsSaveButton_artwork$data
@@ -15,40 +9,10 @@ interface ArtworkActionsSaveButtonProps {
 const ArtworkActionsSaveButton: React.FC<ArtworkActionsSaveButtonProps> = ({
   artwork,
 }) => {
-  const isArtworksListEnabled = useCheckIfArtworkListsEnabled()
-  const { handleSave } = useSaveArtwork({
-    isSaved: !!artwork.isSaved,
-    artwork,
-    contextModule: ContextModule.artworkImage,
-  })
-
-  const { isAuction, isClosed } = artwork.sale ?? {}
-  const isOpenOrUpcomingSale = isAuction && !isClosed
-  const isSaved = !!artwork.isSaved
-
-  if (isArtworksListEnabled) {
-    return (
-      <ManageArtworkForSavesProvider>
-        <ArtworkActionsSaveButtonV2FragmentContainer artwork={artwork} />
-      </ManageArtworkForSavesProvider>
-    )
-  }
-
-  // If an Auction, use Bell (for notifications); if a standard artwork use Heart
-  if (isOpenOrUpcomingSale) {
-    return (
-      <ArtworkActionsWatchLotButtonFragmentContainer
-        isSaved={isSaved}
-        artwork={artwork}
-        onClick={handleSave}
-      />
-    )
-  }
-
   return (
-    <ProgressiveOnboardingSaveArtwork>
-      <SaveUtilButton isSaved={isSaved} onClick={handleSave} />
-    </ProgressiveOnboardingSaveArtwork>
+    <ManageArtworkForSavesProvider>
+      <ArtworkActionsSaveButtonV2FragmentContainer artwork={artwork} />
+    </ManageArtworkForSavesProvider>
   )
 }
 
@@ -65,8 +29,6 @@ export const ArtworkActionsSaveButtonFragmentContainer = createFragmentContainer
           isAuction
           isClosed
         }
-        ...ArtworkActionsWatchLotButton_artwork
-        ...ArtworkAuctionRegistrationPanel_artwork
         ...ArtworkActionsSaveButtonV2_artwork
       }
     `,
