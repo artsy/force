ecf64d107f321684482959bd270218fc6733e961
dzucconi <mail@damonzucconi.com>
2023-09-28 17:15:54 -0400

    feat(articles): adds news module to top of page

--- src/__generated__/ArticlesApp_viewer.graphql.ts
+++ src/__generated__/ArticlesApp_viewer.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<47a3c930d2ad4868596ce6b4e8a68f7e>>
+ * @generated SignedSource<<c7ff57b3d633acc2d9e3e3f84e414f29>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -11,7 +11,7 @@
 import { Fragment, ReaderFragment } from 'relay-runtime';
 import { FragmentRefs } from "relay-runtime";
 export type ArticlesApp_viewer$data = {
-  readonly " $fragmentSpreads": FragmentRefs<"ArticlesIndexArticles_viewer">;
+  readonly " $fragmentSpreads": FragmentRefs<"ArticlesIndexArticles_viewer" | "ArticlesIndexNews_viewer">;
   readonly " $fragmentType": "ArticlesApp_viewer";
 };
 export type ArticlesApp_viewer$key = {
@@ -28,6 +28,11 @@ const node: ReaderFragment = {
     {
       "args": null,
       "kind": "FragmentSpread",
+      "name": "ArticlesIndexNews_viewer"
+    },
+    {
+      "args": null,
+      "kind": "FragmentSpread",
       "name": "ArticlesIndexArticles_viewer"
     }
   ],
@@ -35,6 +40,6 @@ const node: ReaderFragment = {
   "abstractKey": null
 };
 
-(node as any).hash = "17f39e1201099c7627271106edd9a319";
+(node as any).hash = "e90c5b665eb010cc1ad4e60ecd4bac26";
 
 export default node;
