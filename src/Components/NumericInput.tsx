fa405b899d40d90899272539931df4de79e1d9be
dzucconi <mail@damonzucconi.com>
2023-10-18 09:09:10 -0400

    fix(filters): fixes range by disabling input padding in labeled inputs

--- src/Components/NumericInput.tsx
+++ src/Components/NumericInput.tsx
@@ -2,7 +2,13 @@ import { LabeledInput } from "@artsy/palette"
 import styled from "styled-components"
 
 // Disables arrows in numeric inputs
-export const NumericInput = styled(LabeledInput).attrs({ type: "number" })`
+export const NumericInput = styled(LabeledInput).attrs({
+  type: "number",
+  // HACK: Work around sizing issue in Drawer component by removing padding
+  // to accommodate labels. Extremely unlikely to cause an issue in practice
+  // because you'd have to insert prices into the quadrillions to encounter overlap.
+  style: {},
+})`
   /* Chrome, Safari, Edge, Opera */
   input::-webkit-outer-spin-button,
   input::-webkit-inner-spin-button {
