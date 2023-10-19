046dbeb93dd7f6ff9582856c5e54bda7cfc46df7
dzucconi <mail@damonzucconi.com>
2023-10-19 08:12:41 -0400

    fix(progressive-onboarding): removes lazyload to prevent layout shift

--- src/Components/ProgressiveOnboarding/withProgressiveOnboardingCounts.tsx
+++ src/Components/ProgressiveOnboarding/withProgressiveOnboardingCounts.tsx
@@ -35,7 +35,6 @@ export const withProgressiveOnboardingCounts = <
 
     return (
       <SystemQueryRenderer<withProgressiveOnboardingCountsQuery>
-        lazyLoad
         placeholder={
           <Component
             {...props}
