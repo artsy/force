ffb0d1313fd44a1b35ce72b68ad8d3a540f28d5e
dzucconi <mail@damonzucconi.com>
2023-11-21 10:26:45 -0500

    fix(sticky): supports proper stacking and release

--- src/Components/Sticky/__tests__/StickyProvider.jest.tsx
+++ src/Components/Sticky/__tests__/StickyProvider.jest.tsx
@@ -1,11 +1,14 @@
-import { getOffsetTopForSticky } from "../StickyProvider"
+import {
+  getOffsetTopForSticky,
+  TSticky,
+} from "Components/Sticky/StickyProvider"
 
-const EXAMPLE_STICKIES = [
-  { id: "example1", height: 80 },
-  { id: "example2", height: 200 },
-  { id: "example3", height: 15 },
-  { id: "example4", height: 333 },
-  { id: "example5", height: 66 },
+const EXAMPLE_STICKIES: TSticky[] = [
+  { id: "example1", height: 80, status: "ORIGINAL" },
+  { id: "example2", height: 200, status: "ORIGINAL" },
+  { id: "example3", height: 15, status: "ORIGINAL" },
+  { id: "example4", height: 333, status: "ORIGINAL" },
+  { id: "example5", height: 66, status: "ORIGINAL" },
 ]
 
 describe("getOffsetTopForSticky", () => {
