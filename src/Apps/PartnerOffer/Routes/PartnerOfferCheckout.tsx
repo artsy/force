e1a0ca09cdbe8d197fa36b575488edf4a4a9566a
Leandro Motta <leamotta@msn.com>
2023-12-04 11:55:39 +0100

    verify partnerOfferId to avoid double call

--- src/Apps/PartnerOffer/Routes/PartnerOfferCheckout.tsx
+++ src/Apps/PartnerOffer/Routes/PartnerOfferCheckout.tsx
@@ -14,38 +14,40 @@ export const PartnerOfferCheckout: FC = () => {
   } = usePartnerOfferCheckoutMutation()
 
   const handleRedirect = useCallback(async () => {
-    try {
-      const response = await partnerCheckoutMutation({
-        variables: {
-          input: {
-            partnerOfferId: partnerOfferId,
+    if (partnerOfferId) {
+      try {
+        const response = await partnerCheckoutMutation({
+          variables: {
+            input: {
+              partnerOfferId: partnerOfferId,
+            },
           },
-        },
-      })
+        })
 
-      let redirectUrl = "/"
-      let orderOrError = response.commerceCreatePartnerOfferOrder?.orderOrError
+        let redirectUrl = "/"
+        let orderOrError = response.commerceCreatePartnerOfferOrder?.orderOrError
 
-      if (orderOrError?.error) {
-        const errorCode = orderOrError.error.code
-        const errorData = JSON.parse(orderOrError.error.data?.toString() ?? "")
+        if (orderOrError?.error) {
+          const errorCode = orderOrError.error.code
+          const errorData = JSON.parse(orderOrError.error.data?.toString() ?? "")
 
-        switch (errorCode) {
-          case "expired_partner_offer":
-            // TODO: Show error message in artwork page
-            redirectUrl = `/artwork/${errorData.artwork_id}`
-            break
-          case "not_acquireable":
-            // TODO: Show error message in artwork page
-            redirectUrl = `/artwork/${errorData.artwork_id}`
-            break
+          switch (errorCode) {
+            case "expired_partner_offer":
+              // TODO: Show error message in artwork page
+              redirectUrl = `/artwork/${errorData.artwork_id}`
+              break
+            case "not_acquireable":
+              // TODO: Show error message in artwork page
+              redirectUrl = `/artwork/${errorData.artwork_id}`
+              break
+          }
+        } else {
+          redirectUrl = `/orders/${orderOrError?.order?.internalID}`
         }
-      } else {
-        redirectUrl = `/orders/${orderOrError?.order?.internalID}`
+        router?.push(redirectUrl)
+      } catch (error) {
+        console.error("Error creating partner offer order", error)
       }
-      router?.push(redirectUrl)
-    } catch (error) {
-      console.error("Error creating partner offer order", error)
     }
   }, [partnerCheckoutMutation, partnerOfferId, router])
 
