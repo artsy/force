ce607d28c625e1926b40c6d18d571378a51bc7ba
Kazunari Okuda <saori.cube@gmail.com>
2024-01-03 09:31:01 +0100

    chore: remove an unnecessary blank component

--- src/Apps/Order/Components/PartnerOfferTimerItem.tsx
+++ src/Apps/Order/Components/PartnerOfferTimerItem.tsx
@@ -34,28 +34,26 @@ export const PartnerOfferTimerItem: React.FC<{
   return (
     <>
       <Box flexDirection="row" textAlign="center">
-        <>
-          <Text variant="sm" color="blue100" mt={2}>
-            <StopwatchIcon
-              display="inline-block"
-              top="0.2rem"
-              width={14}
-              height={17}
-              fill="blue100"
-            />
-            {remainingTime} left
-          </Text>
-          <ProgressBar percentComplete={percentComplete} highlight="blue100" />
-          <Text variant="sm" fontWeight="bold">
-            Purchase by {actionDeadline}
-          </Text>
-          <Text variant="xs" color="black60">
-            Offer Expires after 72 hours.
-          </Text>
-          <Text variant="xs" color="black60">
-            Keep in mind the work can be sold to another buyer in the meantime.
-          </Text>
-        </>
+        <Text variant="sm" color="blue100" mt={2}>
+          <StopwatchIcon
+            display="inline-block"
+            top="0.2rem"
+            width={14}
+            height={17}
+            fill="blue100"
+          />
+          {remainingTime} left
+        </Text>
+        <ProgressBar percentComplete={percentComplete} highlight="blue100" />
+        <Text variant="sm" fontWeight="bold">
+          Purchase by {actionDeadline}
+        </Text>
+        <Text variant="xs" color="black60">
+          Offer Expires after 72 hours.
+        </Text>
+        <Text variant="xs" color="black60">
+          Keep in mind the work can be sold to another buyer in the meantime.
+        </Text>
       </Box>
       <Spacer y={2} />
     </>
