1ba6ff1e84c6e201fa4af60c39c34c7a29d03f02
tam-kis <kis.tamara91@gmail.com>
2024-01-19 15:25:55 +0100

    fix(ONYX-627): add tests with feature flag to display pills

--- src/Components/Notifications/__tests__/Notifications.jest.tsx
+++ src/Components/Notifications/__tests__/Notifications.jest.tsx
@@ -1,11 +1,21 @@
 import { render, screen } from "@testing-library/react"
 import { Notifications } from "Components/Notifications/Notifications"
+import { NotificationsWrapper } from "Components/Notifications/NotificationsWrapper"
+import { useFeatureFlag } from "System/useFeatureFlag"
+
+jest.mock("System/useFeatureFlag", () => ({
+  useFeatureFlag: jest.fn(),
+}))
 
 jest.mock("Utils/Hooks/useMatchMedia", () => ({
   __internal__useMatchMedia: () => false,
 }))
 
 describe("Notifications", () => {
+  beforeEach(() => {
+    ;(useFeatureFlag as jest.Mock).mockImplementation(() => false)
+  })
+
   it("should render tabs", () => {
     render(<Notifications mode="page" unreadCounts={0} />)
 
@@ -17,5 +27,19 @@ describe("Notifications", () => {
     render(<Notifications mode="page" unreadCounts={5} />)
 
     expect(screen.getByText("New notifications")).toBeInTheDocument()
+  })
+})
+
+describe("Notifications with pills", () => {
+  beforeEach(() => {
+    ;(useFeatureFlag as jest.Mock).mockImplementation(() => true)
+  })
+
+  it("should render pills", () => {
+    render(<NotificationsWrapper mode="dropdown" />)
+
+    expect(screen.getByText("All")).toBeInTheDocument()
+    expect(screen.getByText("Alerts")).toBeInTheDocument()
+    expect(screen.getByText("Following")).toBeInTheDocument()
   })
 })
