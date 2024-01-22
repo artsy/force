c9ab4b89610545f98ddee8dbdb95e16b56f5f88d
Janae Edwards <edwards.janae@gmail.com>
2024-01-22 11:23:15 -0500

    set up conditions modal

--- src/Apps/Artwork/Components/ArtworkDefinitionList.tsx
+++ src/Apps/Artwork/Components/ArtworkDefinitionList.tsx
@@ -1,9 +1,11 @@
-import * as React from "react";
-import { BoxProps, Flex, Text } from "@artsy/palette"
+import * as React from "react"
+import { BoxProps, Flex, Text, Clickable } from "@artsy/palette"
+// import { useState } from "react"
 
 interface ArtworkDefinitionListProps extends BoxProps {
   term: string
   children: React.ReactNode
+  // modalComponent?: React.ReactNode
 }
 
 export const ArtworkDefinitionList: React.FC<ArtworkDefinitionListProps> = ({
@@ -11,11 +13,15 @@ export const ArtworkDefinitionList: React.FC<ArtworkDefinitionListProps> = ({
   children,
   ...rest
 }) => {
+  // const [showModal, setShowModal] = useState(false)
+
   return (
     <Flex as="dl" flexDirection={["column", "row"]} {...rest}>
+      {/* <Clickable textDecoration="underline"> */}
       <Text as="dt" variant="xs" width={150} flexShrink={0} mr={2}>
         {term}
       </Text>
+      {/* </Clickable> */}
 
       <Text as="dd" variant="xs" color="black60" flex={1}>
         {children}
