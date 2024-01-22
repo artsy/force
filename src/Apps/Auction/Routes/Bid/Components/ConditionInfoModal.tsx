049e871c2897c6e9e3ade820965b80dee6c19732
Janae Edwards <edwards.janae@gmail.com>
2024-01-22 11:28:12 -0500

    do i need the useState here

--- src/Apps/Auction/Routes/Bid/Components/ConditionInfoModal.tsx
+++ src/Apps/Auction/Routes/Bid/Components/ConditionInfoModal.tsx
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { ModalDialog, Clickable, Text, Box } from "@artsy/palette"
+import { useState } from "react"
 
 interface ConditionInfoModalProps {
   onClose: () => void
