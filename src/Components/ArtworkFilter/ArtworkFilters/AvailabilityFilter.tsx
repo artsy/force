e2fcd3a8824150705c79915197a00bba7826e75b
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-02-02 14:05:28 -0500

    feat: add an AvailabilityFilter to toggle only for-sale works

---
+++ src/Components/ArtworkFilter/ArtworkFilters/AvailabilityFilter.tsx
@@ -0,0 +1,21 @@
+import { Checkbox } from "@artsy/palette"
+import { useArtworkFilterContext } from "Components/ArtworkFilter/ArtworkFilterContext"
+import { FilterExpandable } from "Components/ArtworkFilter/ArtworkFilters/FilterExpandable"
+
+export const AvailabilityFilter: React.FC = () => {
+  const { setFilter, unsetFilter, filters } = useArtworkFilterContext()
+
+  return (
+    <FilterExpandable label="Availability" expanded>
+      <Checkbox
+        selected={!!filters?.forSale}
+        onSelect={selected => {
+          selected ? setFilter("forSale", true) : unsetFilter("forSale")
+        }}
+        my={1}
+      >
+        Only works for sale
+      </Checkbox>
+    </FilterExpandable>
+  )
+}
