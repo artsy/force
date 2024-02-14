ae8a56e616796bcdbd124f0924ed487f87426b37
Tam <kis.tamara91@gmail.com>
2024-02-14 12:55:05 +0100

    feat(ONYX-742): notifications empty state redesign (#13487)
    * feat(ONYX-742): update emptystate with new copy and design
    
    * fix(ONYX-742): update empty state links
    
    * fix(ONYX-742): remove braces
    
    * fix(ONYX-742): alerts and follows filters visible and manage alerts link

--- src/Components/Notifications/NotificationsContextualMenu.tsx
+++ src/Components/Notifications/NotificationsContextualMenu.tsx
@@ -2,7 +2,6 @@ import { ContextualMenu, ContextualMenuItem } from "Components/ContextualMenu"
 import { MarkAllAsReadPanel } from "Components/Notifications/MarkAllAsReadPanel"
 import { Z } from "Apps/Components/constants"
 import { RouterLink } from "System/Router/RouterLink"
-import { BASE_SAVES_PATH } from "Apps/CollectorProfile/constants"
 
 interface NotificationsContextualMenuProps {
   onHide?: () => void
@@ -21,13 +20,13 @@ export const NotificationsContextualMenu: React.FC<NotificationsContextualMenuPr
       <ContextualMenuItem p={0}>
         <RouterLink
           onClick={onHide}
-          to={BASE_SAVES_PATH}
+          to={"/settings/alerts"}
           textDecoration="none"
           color="black100"
           display="block"
           p={2}
         >
-          Manage Saves
+          Manage Alerts
         </RouterLink>
       </ContextualMenuItem>
       <ContextualMenuItem p={0}>
