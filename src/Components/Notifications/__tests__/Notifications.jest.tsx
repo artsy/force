44f81d6b3e99b5d25910359224107b1f3d910d02
Ole <ole.richter91@gmail.com>
2024-02-14 11:12:37 +0100

    test

--- src/Components/Notifications/__tests__/Notifications.jest.tsx
+++ src/Components/Notifications/__tests__/Notifications.jest.tsx
@@ -1,81 +1,58 @@
-// import { screen } from "@testing-library/react"
-// import { Notifications } from "Components/Notifications/Notifications"
-// import { NotificationsWrapper } from "Components/Notifications/NotificationsWrapper"
-// import { MockBoot } from "DevTools/MockBoot"
-// import { flushPromiseQueue } from "DevTools/flushPromiseQueue"
-// import { render } from "DevTools/renderWithMockBoot"
-// import { setupTestWrapperTL } from "DevTools/setupTestWrapper"
-// import { useFeatureFlag } from "System/useFeatureFlag"
-// import { graphql } from "react-relay"
-// import { MockEnvironment, createMockEnvironment } from "relay-test-utils"
+import { screen } from "@testing-library/react"
+import { Notifications } from "Components/Notifications/Notifications"
+import { MockBoot } from "DevTools/MockBoot"
+import { flushPromiseQueue } from "DevTools/flushPromiseQueue"
+import { render } from "DevTools/renderWithMockBoot"
+import { useFeatureFlag } from "System/useFeatureFlag"
+import { createMockEnvironment } from "relay-test-utils"
 
-// jest.mock("System/useFeatureFlag", () => ({
-//   useFeatureFlag: jest.fn(),
-// }))
+jest.mock("System/useFeatureFlag", () => ({
+  useFeatureFlag: jest.fn(),
+}))
 
-// jest.mock("Utils/Hooks/useMatchMedia", () => ({
-//   __internal__useMatchMedia: () => false,
-// }))
-// jest.unmock("react-relay")
+jest.mock("Utils/Hooks/useMatchMedia", () => ({
+  __internal__useMatchMedia: () => false,
+}))
+jest.unmock("react-relay")
 
-// describe("Notifications", () => {
-//   beforeEach(() => {
-//     ;(useFeatureFlag as jest.Mock).mockImplementation(() => false)
-//   })
+describe("Notifications", () => {
+  beforeEach(() => {
+    ;(useFeatureFlag as jest.Mock).mockImplementation(() => false)
+  })
 
-//   it("should render tabs", () => {
-//     render(<Notifications mode="page" unreadCounts={0} />)
+  it("should render tabs", () => {
+    render(<Notifications mode="page" unreadCounts={0} />)
 
-//     expect(screen.getByText("All")).toBeInTheDocument()
-//     expect(screen.getByText("Alerts")).toBeInTheDocument()
-//   })
+    expect(screen.getByText("All")).toBeInTheDocument()
+    expect(screen.getByText("Alerts")).toBeInTheDocument()
+  })
 
-//   it("should display new notifications message", () => {
-//     render(<Notifications mode="page" unreadCounts={5} />)
+  it("should display new notifications message", () => {
+    render(<Notifications mode="page" unreadCounts={5} />)
 
-//     expect(screen.getByText("New notifications")).toBeInTheDocument()
-//   })
-// })
+    expect(screen.getByText("New notifications")).toBeInTheDocument()
+  })
+})
 
-// describe("Notifications with pills", () => {
-//   const environment = createMockEnvironment()
+describe("Notifications with pills", () => {
+  const environment = createMockEnvironment()
 
-//   // const { renderWithRelay } = setupTestWrapperTL({
-//   //   Component: (props: any) => (
-//   //     <MockBoot>
-//   //       <Notifications mode="page" unreadCounts={0} />
-//   //     </MockBoot>
-//   //   ),
-//   //   query: graphql`
-//   //     query ArtistAuctionResults_Test_Query($artistID: String!)
-//   //       @raw_response_type {
-//   //       artist(id: $artistID) {
-//   //         ...ArtistAuctionResultsRoute_artist
-//   //       }
-//   //     }
-//   //   `,
-//   //   variables: {
-//   //     artistID: "pablo-picasso",
-//   //   },
-//   // })
+  beforeEach(() => {
+    ;(useFeatureFlag as jest.Mock).mockImplementation(() => true)
+  })
 
-//   beforeEach(() => {
-//     ;(useFeatureFlag as jest.Mock).mockImplementation(() => true)
-//   })
+  // TODO: Bring back this test!
+  it.skip("should render pills", async () => {
+    render(
+      <MockBoot relayEnvironment={environment}>
+        <Notifications mode="page" unreadCounts={0} />
+      </MockBoot>
+    )
 
-//   it("should render pills", async () => {
-//     render(
-//       <MockBoot relayEnvironment={environment}>
-//         <Notifications mode="page" unreadCounts={0} />
-//       </MockBoot>
-//     )
+    await flushPromiseQueue()
 
-//     await flushPromiseQueue()
-
-//     await environment.mock.resolveMostRecentOperation({})
-
-//     expect(screen.getByText("All")).toBeInTheDocument()
-//     expect(screen.getByText("Alerts")).toBeInTheDocument()
-//     expect(screen.getByText("Following")).toBeInTheDocument()
-//   })
-// })
+    expect(screen.getByText("All")).toBeInTheDocument()
+    expect(screen.getByText("Alerts")).toBeInTheDocument()
+    expect(screen.getByText("Following")).toBeInTheDocument()
+  })
+})
