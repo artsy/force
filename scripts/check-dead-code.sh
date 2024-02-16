a00501fc2d17a3dc33abe2f45084ba547639ebf1
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-02-16 17:22:48 -0500

    feat: add a production mode to dead-code sweep

--- scripts/check-dead-code.sh
+++ scripts/check-dead-code.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-npx knip --no-exit-code --include files \
+if [ "$1" = "production" ]; then
+    IS_PRODUCTION_MODE="--production"
+else
+    IS_PRODUCTION_MODE=""
+fi
+
+npx knip --no-exit-code --include files $IS_PRODUCTION_MODE \
   | grep -v '__generated__' \
   | grep -v '__mocks__' \
   | grep -v '__stories__'
