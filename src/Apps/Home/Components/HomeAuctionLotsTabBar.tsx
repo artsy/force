9fc7f456d2d9939e60ef20e8ac4df3f3a2d8004d
tam-kis <kis.tamara91@gmail.com>
2024-03-28 13:55:54 +0100

    fix(ONYX-804): clean up imports

--- src/Apps/Home/Components/HomeAuctionLotsTabBar.tsx
+++ src/Apps/Home/Components/HomeAuctionLotsTabBar.tsx
@@ -1,7 +1,7 @@
+import * as React from "react"
 import { Tab, Tabs } from "@artsy/palette"
 import { HomeAuctionLotsForYouRailQueryRenderer } from "Apps/Home/Components/HomeAuctionLotsForYouRail"
 import { HomeAuctionLotsRailQueryRenderer } from "Apps/Home/Components/HomeAuctionLotsRail"
-import * as React from "react"
 import { useSystemContext } from "System/useSystemContext"
 
 export const HomeAuctionLotsTabBar: React.FC = () => {
