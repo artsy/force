26f4a5147a26146fb6c618788fc3cfa3ad518e7c
Adam Iskounen <adam.iskounen@artsymail.com>
2024-03-28 06:11:00 -0400

    feat: update footer links

--- src/Components/Footer/__tests__/Footer.jest.tsx
+++ src/Components/Footer/__tests__/Footer.jest.tsx
@@ -3,11 +3,16 @@ import { mount } from "enzyme"
 import { Footer } from "Components/Footer/Footer"
 import { Breakpoint } from "@artsy/palette/dist/themes/types"
 import { useRouter } from "System/Router/useRouter"
+import { useFeatureFlag } from "System/useFeatureFlag"
 
 jest.mock("System/Router/useRouter", () => ({
   useRouter: jest.fn().mockReturnValue({
     match: { location: { pathname: "/" } },
   }),
+}))
+
+jest.mock("System/useFeatureFlag", () => ({
+  useFeatureFlag: jest.fn(),
 }))
 
 describe("Footer", () => {
@@ -74,6 +79,61 @@ describe("Footer", () => {
       const wrapper = getWrapper("lg")
       expect(wrapper.text()).not.toContain("Meet your new art advisor.")
     })
+
+    it("renders footer links", () => {
+      const wrapper = getWrapper("lg")
+
+      expect(wrapper.text()).toContain("Terms of Use")
+      expect(wrapper.html()).toContain("/terms")
+
+      expect(wrapper.text()).toContain("Privacy Policy")
+      expect(wrapper.html()).toContain("/privacy")
+
+      expect(wrapper.text()).toContain("Security")
+      expect(wrapper.html()).toContain("/security")
+
+      expect(wrapper.text()).toContain("Conditions of Sale")
+      expect(wrapper.html()).toContain("/conditions-of-sale")
+
+      expect(wrapper.text()).toContain("ACA Seller’s Agreement")
+      expect(wrapper.html()).toContain(
+        "/page/artsy-curated-auctions-listing-agreement"
+      )
+
+      expect(wrapper.text()).toContain("Buyer Guarantee")
+      expect(wrapper.html()).toContain("/buyer-guarantee")
+    })
+
+    describe("when diamond_new-terms-and-conditions is enabled", () => {
+      beforeEach(() => {
+        ;(useFeatureFlag as jest.Mock).mockImplementation(
+          (f: string) => f === "diamond_new-terms-and-conditions"
+        )
+      })
+
+      afterEach(() => {
+        ;(useFeatureFlag as jest.Mock).mockReset()
+      })
+
+      it("renders the new footer links", () => {
+        const wrapper = getWrapper("lg")
+
+        expect(wrapper.text()).toContain("Terms and Conditions")
+        expect(wrapper.html()).toContain("/terms")
+
+        expect(wrapper.text()).toContain("Auction Supplement")
+        expect(wrapper.html()).toContain("/supplemental-COS")
+
+        expect(wrapper.text()).toContain("Buyer Guarantee")
+        expect(wrapper.html()).toContain("/buyer-guarantee")
+
+        expect(wrapper.text()).toContain("Privacy Policy")
+        expect(wrapper.html()).toContain("/privacy")
+
+        expect(wrapper.text()).toContain("Security")
+        expect(wrapper.html()).toContain("/security")
+      })
+    })
   })
 
   describe("small screen size", () => {
@@ -85,6 +145,61 @@ describe("Footer", () => {
     it("renders the CCPA request button", () => {
       const wrapper = getWrapper("xs")
       expect(wrapper.find("button").length).toEqual(1)
+    })
+
+    it("renders footer links", () => {
+      const wrapper = getWrapper("xs")
+
+      expect(wrapper.text()).toContain("Terms of Use")
+      expect(wrapper.html()).toContain("/terms")
+
+      expect(wrapper.text()).toContain("Privacy Policy")
+      expect(wrapper.html()).toContain("/privacy")
+
+      expect(wrapper.text()).toContain("Security")
+      expect(wrapper.html()).toContain("/security")
+
+      expect(wrapper.text()).toContain("Conditions of Sale")
+      expect(wrapper.html()).toContain("/conditions-of-sale")
+
+      expect(wrapper.text()).toContain("ACA Seller’s Agreement")
+      expect(wrapper.html()).toContain(
+        "/page/artsy-curated-auctions-listing-agreement"
+      )
+
+      expect(wrapper.text()).toContain("Buyer Guarantee")
+      expect(wrapper.html()).toContain("/buyer-guarantee")
+    })
+
+    describe("when diamond_new-terms-and-conditions is enabled", () => {
+      beforeEach(() => {
+        ;(useFeatureFlag as jest.Mock).mockImplementation(
+          (f: string) => f === "diamond_new-terms-and-conditions"
+        )
+      })
+
+      afterEach(() => {
+        ;(useFeatureFlag as jest.Mock).mockReset()
+      })
+
+      it("renders the new footer links", () => {
+        const wrapper = getWrapper("xs")
+
+        expect(wrapper.text()).toContain("Terms and Conditions")
+        expect(wrapper.html()).toContain("/terms")
+
+        expect(wrapper.text()).toContain("Auction Supplement")
+        expect(wrapper.html()).toContain("/supplemental-COS")
+
+        expect(wrapper.text()).toContain("Buyer Guarantee")
+        expect(wrapper.html()).toContain("/buyer-guarantee")
+
+        expect(wrapper.text()).toContain("Privacy Policy")
+        expect(wrapper.html()).toContain("/privacy")
+
+        expect(wrapper.text()).toContain("Security")
+        expect(wrapper.html()).toContain("/security")
+      })
     })
   })
 })
