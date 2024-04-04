2dde0acc07f2693c95420e2a37357de0a9312d96
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-04-04 13:54:08 -0400

    chore: remove ff from quick filters

--- src/Components/ArtworkFilter/ArtworkFiltersQuick/ArtistSeriesFilterQuick.tsx
+++ src/Components/ArtworkFilter/ArtworkFiltersQuick/ArtistSeriesFilterQuick.tsx
@@ -2,20 +2,15 @@ import { FC } from "react"
 import { useArtworkFilterContext } from "Components/ArtworkFilter/ArtworkFilterContext"
 import { DropdownProps } from "@artsy/palette"
 import { FilterQuick } from "Components/ArtworkFilter/ArtworkFiltersQuick/FilterQuick"
-import { useFeatureFlag } from "System/useFeatureFlag"
 
 export interface ArtistSeriesFilterQuickProps
   extends Omit<DropdownProps, "dropdown" | "children"> {}
 
 export const ArtistSeriesFilterQuick: FC<ArtistSeriesFilterQuickProps> = props => {
-  const isArtistSeriesFilterEnabled = useFeatureFlag(
-    "onyx_enable-artist-series-filter"
-  )
-
   const { aggregations } = useArtworkFilterContext()
   const aggregation = aggregations?.find(agg => agg.slice === "ARTIST_SERIES")
   const artistSeries = aggregation?.counts || []
-  const isDisplayable = isArtistSeriesFilterEnabled && artistSeries.length > 0
+  const isDisplayable = artistSeries.length > 0
 
   if (!isDisplayable) return null
 
--- src/Components/ArtworkFilter/ArtworkFiltersQuick/__tests__/ArtistSeriesFilterQuick.jest.tsx
+++ src/Components/ArtworkFilter/ArtworkFiltersQuick/__tests__/ArtistSeriesFilterQuick.jest.tsx
@@ -2,7 +2,6 @@ import { screen } from "@testing-library/react"
 import { createArtworkFilterTestRenderer } from "Components/ArtworkFilter/ArtworkFilters/__tests__/Utils"
 import { ArtworkFilterContextProps } from "Components/ArtworkFilter/ArtworkFilterContext"
 import { ArtistSeriesFilterQuick } from "Components/ArtworkFilter/ArtworkFiltersQuick/ArtistSeriesFilterQuick"
-import { useFeatureFlag } from "System/useFeatureFlag"
 
 jest.mock("System/useFeatureFlag", () => ({
   useFeatureFlag: jest.fn(() => true),
@@ -27,40 +26,23 @@ describe(ArtistSeriesFilterQuick, () => {
     render = createArtworkFilterTestRenderer(artworkFilterContext)
   })
 
-  describe("when onyx_enable-artist-series-filter flag is enabled", () => {
-    beforeEach(() => {
-      ;(useFeatureFlag as jest.Mock).mockImplementation(() => true)
-    })
-
-    describe("when artist series aggregations are present", () => {
-      it("renders the artist series quick filter", () => {
-        render(<ArtistSeriesFilterQuick />)
-        expect(screen.getByText("Artist Series")).toBeInTheDocument()
-      })
-    })
-
-    describe("when artist series aggregations are absent", () => {
-      it("does not render the artist series quick filter", () => {
-        render = createArtworkFilterTestRenderer({
-          aggregations: [
-            {
-              slice: "ARTIST_SERIES",
-              counts: [],
-            },
-          ],
-        })
-        render(<ArtistSeriesFilterQuick />)
-        expect(screen.queryByText("Artist Series")).not.toBeInTheDocument()
-      })
+  describe("when artist series aggregations are present", () => {
+    it("renders the artist series quick filter", () => {
+      render(<ArtistSeriesFilterQuick />)
+      expect(screen.getByText("Artist Series")).toBeInTheDocument()
     })
   })
 
-  describe("when onyx_enable-artist-series-filter flag is disabled", () => {
-    beforeEach(() => {
-      ;(useFeatureFlag as jest.Mock).mockImplementation(() => false)
-    })
-
+  describe("when artist series aggregations are absent", () => {
     it("does not render the artist series quick filter", () => {
+      render = createArtworkFilterTestRenderer({
+        aggregations: [
+          {
+            slice: "ARTIST_SERIES",
+            counts: [],
+          },
+        ],
+      })
       render(<ArtistSeriesFilterQuick />)
       expect(screen.queryByText("Artist Series")).not.toBeInTheDocument()
     })
