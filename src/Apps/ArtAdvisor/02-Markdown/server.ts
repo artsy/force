404a6c017ad620025f76ac793436069a3b99835d
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-05-01 15:07:45 -0400

    feat: wire up 02 locally
    This resulted in the loss of streaming the output, not sure why

--- src/Apps/ArtAdvisor/02-Markdown/server.ts
+++ src/Apps/ArtAdvisor/02-Markdown/server.ts
@@ -1,15 +1,55 @@
-import express from "express"
+import express, { Request, Response } from "express"
+import OpenAI from "openai"
+
+const openai = new OpenAI() // uses `OPENAI_API_KEY` from .env
+
+const handler = async (req: Request, res: Response) => {
+  try {
+    console.log(new Date())
+    console.log("Received request body:", req.body)
+
+    const stream = openai.beta.chat.completions.stream({
+      model: "gpt-3.5-turbo",
+      stream: true,
+      messages: [
+        {
+          role: "system",
+          content: `
+          You are Artsy's art advisor assistant.
+          `,
+        },
+        { role: "user", content: req.body },
+      ],
+    })
+
+    res.header("Content-Type", "text/plain")
+    // @ts-expect-error
+    for await (const chunk of stream.toReadableStream()) {
+      console.log(chunk)
+      res.write(chunk)
+    }
+
+    res.end()
+  } catch (e) {
+    console.error(e)
+  }
+}
+
+function parseTextPlain(req, res, next) {
+  if (req.headers["content-type"] === "text/plain") {
+    let data = ""
+    req.on("data", chunk => {
+      data += chunk
+    })
+    req.on("end", () => {
+      req.body = data
+      next()
+    })
+  } else {
+    next()
+  }
+}
 
 export const router = express.Router()
-
-router.get("/", (req, res) => {
-  res.send("Hello root under experiment 2")
-})
-
-router.get("/a", (req, res) => {
-  res.send("Hello experiment 2a")
-})
-
-router.get("/b", (req, res) => {
-  res.send("Hello experiment 2b")
-})
+router.use(parseTextPlain)
+router.post("/", handler)
