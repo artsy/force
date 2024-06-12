6c6ce52170b30923fae1684304bcf185241b0a7b
Christopher Pappas <damassi.pappas@gmail.com>
2024-06-12 11:10:24 -0700

    refactor(loading-bar): Polish loading states

---
+++ src/System/Router/PageLoadingBar.tsx
@@ -0,0 +1,76 @@
+import styled, { keyframes, css } from "styled-components"
+import { useState, useEffect, useRef } from "react"
+import { Box } from "@artsy/palette"
+
+interface PageLoadingBarProps {
+  loadingState: "resting" | "loading" | "complete"
+}
+
+export const PageLoadingBar: React.FC<PageLoadingBarProps> = ({
+  loadingState = "resting",
+}) => {
+  const [isComplete, setIsComplete] = useState(false)
+  const [loading, setLoading] = useState(loadingState)
+
+  const barRef = useRef<HTMLDivElement>(null)
+
+  useEffect(() => {
+    if (loadingState === "complete") {
+      setIsComplete(true)
+      setLoading("complete")
+    } else {
+      setIsComplete(false)
+      setLoading(loadingState)
+    }
+  }, [loadingState])
+
+  return (
+    <LoadingBar
+      ref={barRef as any}
+      loading={loading}
+      isComplete={isComplete}
+      backgroundColor="brand"
+      height={2}
+      top="1px"
+      left={0}
+      zIndex={100}
+      overflow="hidden"
+      position="fixed"
+    />
+  )
+}
+
+const easeOutExpo = "cubic-bezier(0.19, 1, 0.22, 1)"
+
+const loadingAnimation = keyframes`
+  from {
+    width: 0%;
+  }
+  to {
+    width: 30%;
+  }
+`
+
+const completeAnimation = keyframes`
+  from {
+    width: 30%;
+  }
+  to {
+    width: 100%;
+  }
+`
+
+const LoadingBar = styled(Box)<{ loading: string; isComplete: boolean }>`
+  ${({ loading }) =>
+    loading === "loading" &&
+    css`
+      animation: ${loadingAnimation} 1s ${easeOutExpo} forwards;
+    `}
+  ${({ isComplete }) =>
+    isComplete &&
+    css`
+      animation: ${completeAnimation} 1s ${easeOutExpo} forwards;
+    `}
+  opacity: ${({ isComplete }) => (isComplete ? 0 : 1)};
+  transition: opacity 0.4s;
+`
