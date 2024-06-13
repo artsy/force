1a5ec50b9357d03cf2b351773c632d0d70c2b3f4
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2024-06-13 15:41:50 -0400

    refactor: some variables in the server endpoint

--- src/Apps/ArtAdvisor/06-Discovery-Neartext/server.ts
+++ src/Apps/ArtAdvisor/06-Discovery-Neartext/server.ts
@@ -4,13 +4,11 @@ import _ from "lodash"
 import { WeaviateDB } from "./weaviate-db"
 import { generateUuid5 } from "weaviate-ts-client"
 
-const ROOP_TESTER_ID = "64724d9a-920f-4814-bf3f-bf1c45e02725"
-
-const w = new WeaviateDB()
+const weaviateDB = new WeaviateDB()
 
 const getArtworks = async (req: ArtsyRequest, res: ArtsyResponse) => {
   const { concepts, likedArtworkIds, dislikedArtworkIds, limit } = req.query
-  const artworks = await w.getArtworksNearConcepts({
+  const artworks = await weaviateDB.getArtworksNearConcepts({
     concepts: concepts as string[],
     likedArtworkIds: likedArtworkIds as string[],
     dislikedArtworkIds: dislikedArtworkIds as string[],
@@ -20,13 +18,13 @@ const getArtworks = async (req: ArtsyRequest, res: ArtsyResponse) => {
 }
 
 const getUsers = async (req: ArtsyRequest, res: ArtsyResponse) => {
-  const users = await w.getUsers()
+  const users = await weaviateDB.getUsers()
   res.json(users)
 }
 
 const getUser = async (req: ArtsyRequest, res: ArtsyResponse) => {
   const { id } = req.params // assumed to be UUID
-  const user = await w.getUser({ id })
+  const user = await weaviateDB.getUser({ id })
   res.json(user)
 }
 
@@ -37,7 +35,7 @@ const createArtworkLike = async (req: ArtsyRequest, res: ArtsyResponse) => {
   }
   if (!userId) throw new Error("Provide either userId (UUID) or userInternalID")
 
-  const result = await w.reactToArtwork({
+  const result = await weaviateDB.reactToArtwork({
     userId,
     artworkInternalID: artworkId,
     reaction: "like",
@@ -52,7 +50,7 @@ const createArtworkDislike = async (req: ArtsyRequest, res: ArtsyResponse) => {
   }
   if (!userId) throw new Error("Provide either userId (UUID) or userInternalID")
 
-  const result = await w.reactToArtwork({
+  const result = await weaviateDB.reactToArtwork({
     userId,
     artworkInternalID: artworkId,
     reaction: "dislike",
@@ -69,27 +67,29 @@ router.post("/artworks/likes", createArtworkLike)
 router.post("/artworks/dislikes", createArtworkDislike)
 
 router.get("/test", async (req: ArtsyRequest, res: ArtsyResponse) => {
-  const like = await w.reactToArtwork({
+  const TEST_ID = "64724d9a-920f-4814-bf3f-bf1c45e02725"
+
+  const like = await weaviateDB.reactToArtwork({
     artworkInternalID: "662fa0a76db981000e0725ff",
-    userId: ROOP_TESTER_ID,
+    userId: TEST_ID,
     reaction: "like",
   })
-  const dislike = await w.reactToArtwork({
+  const dislike = await weaviateDB.reactToArtwork({
     artworkInternalID: "66266a4b1275a600124aeba8",
-    userId: ROOP_TESTER_ID,
+    userId: TEST_ID,
     reaction: "dislike",
   })
 
-  const user = await w.getUser({ id: ROOP_TESTER_ID })
+  const user = await weaviateDB.getUser({ id: TEST_ID })
 
-  const artworks = await w.getArtworksNearConcepts({
+  const artworks = await weaviateDB.getArtworksNearConcepts({
     concepts: ["flower"],
     likedArtworkIds: user.likedArtworkIds,
     dislikedArtworkIds: user.dislikedArtworkIds,
     limit: 3,
   })
 
-  const users = await w.getUsers()
+  const users = await weaviateDB.getUsers()
 
   res.json({
     like,
