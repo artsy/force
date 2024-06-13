8a6a7c606b6333c8d38df8156fa0673c21472786
Matt Jones <38149304+mc-jones@users.noreply.github.com>
2024-06-13 14:54:20 -0400

    feat: add 05-near-object-rail experiment (#13982)
    * feat: add weaviate client
    
    * feat: add near object rail experiment server
    
    * feat: add near object rail client
    
    * feat: add 05-Near-Object-Rail to experiments menu

--- src/Apps/ArtAdvisor/ArtAdvisorServerRoutes.ts
+++ src/Apps/ArtAdvisor/ArtAdvisorServerRoutes.ts
@@ -3,6 +3,7 @@ import { router as router1 } from "./01-Spike/server"
 import { router as router2 } from "./02-Markdown/server"
 import { router as router3 } from "./03-Instruction-Experiments/server"
 import { router as router4 } from "./04-Bio-Follows-Alerts/server"
+import { router as router5 } from "./05-Near-Object-Rail/server"
 
 /*
  * Define API routes that can be collocated with each app experiment
@@ -14,3 +15,4 @@ artAdvisorServerRoutes.use("/api/advisor/1", router1)
 artAdvisorServerRoutes.use("/api/advisor/2", router2)
 artAdvisorServerRoutes.use("/api/advisor/3", router3)
 artAdvisorServerRoutes.use("/api/advisor/4", router4)
+artAdvisorServerRoutes.use("/api/advisor/5", router5)
