90e47494fa5634fb3e2ae8353badc833b55ee98e
tam-kis <kis.tamara91@gmail.com>
2024-06-14 18:38:49 +0200

    fix(ONYX-1015): update hooks route

--- src/Apps/Sell/Routes/__tests__/TitleRoute.jest.tsx
+++ src/Apps/Sell/Routes/__tests__/TitleRoute.jest.tsx
@@ -10,7 +10,7 @@ const mockUseRouter = useRouter as jest.Mock
 const mockPush = jest.fn()
 const mockReplace = jest.fn()
 
-jest.mock("System/Router/useRouter", () => ({
+jest.mock("System/Hooks/useRouter", () => ({
   useRouter: jest.fn(),
 }))
 
