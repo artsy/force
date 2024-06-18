dfee63b4b0dc7ab3b51b8181d3dc6696d3f5f4c7
tam-kis <kis.tamara91@gmail.com>
2024-06-18 20:02:24 +0200

    fix(ONYX-1013): update image urls, count and mWeb styling

--- src/Apps/Sell/Routes/IntroRoute.tsx
+++ src/Apps/Sell/Routes/IntroRoute.tsx
@@ -8,87 +8,101 @@ import {
   Image,
   Spacer,
   Text,
+  THEME,
 } from "@artsy/palette"
-import { useRouter } from "System/Hooks/useRouter"
 import { AppContainer } from "Apps/Components/AppContainer"
-import { SubmissionHeader } from "Apps/Sell/Components/SubmissionHeader"
+import { RouterLink } from "System/Components/RouterLink"
+import { SubmissionLayout } from "Apps/Sell/Components/SubmissionLayout"
+import { __internal__useMatchMedia } from "Utils/Hooks/useMatchMedia"
 
 export const introData = [
   {
-    number: "1",
     title: "Tell us about your work",
     description:
       "Start by adding an artist from our list of high demand artists. Include information such as year, medium, dimensions and materials.",
-    image: "https://files.artsy.net/images/01_submission_artwork.png",
+    image: "https://files.artsy.net/images/01_submission_intro_artwork.png",
   },
   {
-    number: "2",
     title: "Upload artwork images",
     description:
       "Improve your chances of selling by including photographs of the front, back, frame, signature, and other details.",
-    image: "https://files.artsy.net/images/02_submission_upload.png",
+    image: "https://files.artsy.net/images/02_submission_intro_upload.png",
   },
   {
-    number: "3",
     title: "Complete submission",
     description:
       "Your work will be submitted to an Artsy advisor who will assess whether your work is eligible and help guide you on next steps.",
-    image: "https://files.artsy.net/images/03_submission_advisor.png",
+    image: "https://files.artsy.net/images/03_submission_intro_advisor.png",
   },
 ]
 
 export const IntroRoute: React.FC = () => {
-  const { router } = useRouter()
-
-  const onCreateSubmission = () => {
-    router.push("/sell2/submissions/new")
-  }
-
-  //TODO: uncomment when ChooseFromMyCollection feature is ready
-  // const onChooseFromMyCollection = () => {
-  //   router.push("/sell2/submissions/new/collection")
-  // }
-
+  const isMobile = __internal__useMatchMedia(THEME.mediaQueries.xs)
   return (
     <AppContainer>
-      <SubmissionHeader />
-      <Flex py={4} flexDirection="column" alignItems="center">
-        <Box maxWidth={600}>
-          <Text variant="xl">It’s easy to sell on Artsy</Text>
+      <SubmissionLayout hideNavigation>
+        <Flex height="100%" flexDirection="column" overflowY="auto">
+          <Text variant={["lg-display", "xl"]}>It’s easy to sell on Artsy</Text>
+
           <Spacer y={4} />
+
           <Join separator={<Separator my={2} color="black5" />}>
             {introData.map((intro, index) => (
               <Flex key={index} justifyContent="space-between">
-                <Text variant="lg">{intro.number}</Text>
-                <Box pl={1}>
-                  <Text variant="lg">{intro.title}</Text>
-                  <Text variant="sm" color="black60">
+                <Text variant={["sm-display", "lg"]}>{index + 1}</Text>
+                <Box px={1}>
+                  <Text variant={["sm-display", "lg"]}>{intro.title}</Text>
+                  <Text variant={["xs", "sm"]} color="black60">
                     {intro.description}
                   </Text>
                 </Box>
-                <Image src={intro.image} alt="intro" width={100} height={100} />
+                <Image
+                  src={intro.image}
+                  srcSet={intro.image}
+                  alt={`Step ${index + 1}`}
+                  width={[70, 100]}
+                  height={[70, 100]}
+                  style={{ objectFit: "cover" }}
+                />
               </Flex>
             ))}
           </Join>
+
           <Spacer y={6} />
-          <Flex gap="2">
-            <Button onClick={onCreateSubmission} width="100%">
+
+          <Flex
+            gap="2"
+            width="100%"
+            bg="white100"
+            position={isMobile ? "fixed" : "relative"}
+            bottom={isMobile ? 0 : undefined}
+            left={isMobile ? 0 : undefined}
+            px={isMobile ? 2 : 0}
+            py={isMobile ? 4 : 0}
+          >
+            <Button
+              // @ts-ignore
+              as={RouterLink}
+              to="/sell2/submissions/new"
+              width="100%"
+            >
               Start New Submission
             </Button>
 
             {
               //TODO: uncomment when ChooseFromMyCollection feature is ready
               /* <Button
-              onClick={onChooseFromMyCollection}
-              variant={"secondaryBlack"}
-              width="100%"
-            >
-              New from My Collection
-            </Button> */
+                  // @ts-ignore
+                  as={RouterLink}
+                  to="/sell2/submissions/new/collection"
+                  width="100%"
+                >
+                  New from My Collection
+                </Button> */
             }
           </Flex>
-        </Box>
-      </Flex>
+        </Flex>
+      </SubmissionLayout>
     </AppContainer>
   )
 }
