a02908457c037b77b0ce275ab6b86cbaff553fbd
Ole <ole.richter91@gmail.com>
2024-06-20 14:54:45 +0200

    refactor

--- src/Apps/Sell/Routes/IntroRoute.tsx
+++ src/Apps/Sell/Routes/IntroRoute.tsx
@@ -1,21 +1,94 @@
-import * as React from "react"
 import {
   Box,
   Button,
   Flex,
+  Image,
   Join,
   Separator,
-  Image,
   Spacer,
-  Text,
   THEME,
+  Text,
 } from "@artsy/palette"
 import { AppContainer } from "Apps/Components/AppContainer"
-import { RouterLink } from "System/Components/RouterLink"
 import { SubmissionLayout } from "Apps/Sell/Components/SubmissionLayout"
+import { RouterLink } from "System/Components/RouterLink"
 import { __internal__useMatchMedia } from "Utils/Hooks/useMatchMedia"
+import { resized } from "Utils/resized"
+import * as React from "react"
 
-export const introData = [
+export const IntroRoute: React.FC = () => {
+  const isMobile = __internal__useMatchMedia(THEME.mediaQueries.xs)
+
+  return (
+    <AppContainer>
+      <SubmissionLayout hideNavigation>
+        <Flex
+          height={isMobile ? "100%" : undefined}
+          flexDirection="column"
+          overflowY="auto"
+        >
+          <Flex flex={1} overflowY="auto" mx="auto" flexDirection="column">
+            <Text variant={["lg-display", "xl"]}>
+              It’s easy to sell on Artsy
+            </Text>
+
+            <Spacer y={4} />
+
+            <Join separator={<Separator my={2} color="black5" />}>
+              {introData.map((intro, index) => (
+                <Flex key={index} justifyContent="space-between">
+                  <Text variant={["sm-display", "lg"]}>{index + 1}</Text>
+
+                  <Box px={1}>
+                    <Text variant={["sm-display", "lg"]}>{intro.title}</Text>
+
+                    <Text variant={["xs", "sm"]} color="black60">
+                      {intro.description}
+                    </Text>
+                  </Box>
+
+                  <Flex width={[70, 100]} height={[70, 100]}>
+                    <Image
+                      {...resized(intro.image, { width: 100, height: 100 })}
+                      alt={`Step ${index + 1}`}
+                      width={[70, 100]}
+                      height={[70, 100]}
+                      style={{ objectFit: "cover" }}
+                    />
+                  </Flex>
+                </Flex>
+              ))}
+            </Join>
+          </Flex>
+
+          <Flex gap="2" width="100%" pt={[2, 6]}>
+            <Button
+              // @ts-ignore
+              as={RouterLink}
+              to="/sell2/submissions/new"
+              width="100%"
+              data-testid="start-new-submission"
+            >
+              Start New Submission
+            </Button>
+
+            {/* TODO: uncomment when ChooseFromMyCollection feature is ready */}
+            {/* <Button
+                  // @ts-ignore
+                  as={RouterLink}
+                  to="/sell2/submissions/new/collection"
+                  width="100%"
+                >
+                  New from My Collection
+                </Button>  */}
+          </Flex>
+        </Flex>
+      </SubmissionLayout>
+    </AppContainer>
+  )
+}
+
+const introData = [
   {
     title: "Tell us about your work",
     description:
@@ -35,75 +108,3 @@ export const introData = [
     image: "https://files.artsy.net/images/03_submission_intro_advisor.png",
   },
 ]
-
-export const IntroRoute: React.FC = () => {
-  const isMobile = __internal__useMatchMedia(THEME.mediaQueries.xs)
-  return (
-    <AppContainer>
-      <SubmissionLayout hideNavigation>
-        <Flex height="100%" flexDirection="column" overflowY="auto">
-          <Text variant={["lg-display", "xl"]}>It’s easy to sell on Artsy</Text>
-
-          <Spacer y={4} />
-
-          <Join separator={<Separator my={2} color="black5" />}>
-            {introData.map((intro, index) => (
-              <Flex key={index} justifyContent="space-between">
-                <Text variant={["sm-display", "lg"]}>{index + 1}</Text>
-                <Box px={1}>
-                  <Text variant={["sm-display", "lg"]}>{intro.title}</Text>
-                  <Text variant={["xs", "sm"]} color="black60">
-                    {intro.description}
-                  </Text>
-                </Box>
-                <Image
-                  src={intro.image}
-                  srcSet={intro.image}
-                  alt={`Step ${index + 1}`}
-                  width={[70, 100]}
-                  height={[70, 100]}
-                  style={{ objectFit: "cover" }}
-                />
-              </Flex>
-            ))}
-          </Join>
-
-          <Spacer y={6} />
-
-          <Flex
-            gap="2"
-            width="100%"
-            bg="white100"
-            position={isMobile ? "fixed" : "relative"}
-            bottom={isMobile ? 0 : undefined}
-            left={isMobile ? 0 : undefined}
-            px={isMobile ? 2 : 0}
-            py={isMobile ? 4 : 0}
-          >
-            <Button
-              // @ts-ignore
-              as={RouterLink}
-              to="/sell2/submissions/new"
-              width="100%"
-              data-testid="start-new-submission"
-            >
-              Start New Submission
-            </Button>
-
-            {
-              //TODO: uncomment when ChooseFromMyCollection feature is ready
-              /* <Button
-                  // @ts-ignore
-                  as={RouterLink}
-                  to="/sell2/submissions/new/collection"
-                  width="100%"
-                >
-                  New from My Collection
-                </Button> */
-            }
-          </Flex>
-        </Flex>
-      </SubmissionLayout>
-    </AppContainer>
-  )
-}
