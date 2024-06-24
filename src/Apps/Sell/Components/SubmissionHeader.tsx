592ebf5a0c0578e2eb03528c5806e3c4a8dcd7b2
tam-kis <kis.tamara91@gmail.com>
2024-06-24 15:54:13 +0200

    fix(ONYX-1077): remove isMobile and use Media

--- src/Apps/Sell/Components/SubmissionHeader.tsx
+++ src/Apps/Sell/Components/SubmissionHeader.tsx
@@ -1,19 +1,19 @@
 import ArtsyLogoIcon from "@artsy/icons/ArtsyLogoIcon"
 import CloseIcon from "@artsy/icons/CloseIcon"
-import { Flex, FullBleed, THEME } from "@artsy/palette"
+import { Flex, FullBleed } from "@artsy/palette"
 import { AppContainer } from "Apps/Components/AppContainer"
 import { HorizontalPadding } from "Apps/Components/HorizontalPadding"
 import { useSellFlowContext } from "Apps/Sell/SellFlowContext"
 import { Sticky } from "Components/Sticky"
 import { RouterLink } from "System/Components/RouterLink"
 import { __internal__useMatchMedia } from "Utils/Hooks/useMatchMedia"
+import { Media } from "Utils/Responsive"
 
 export const SubmissionHeader: React.FC = () => {
   const context = useSellFlowContext()
   const isLastStep = context?.state?.isLastStep
   const submissionID = context?.state?.submissionID
-  const isMobile = __internal__useMatchMedia(THEME.mediaQueries.xs)
-  const saveAndExitMobile = submissionID && isMobile
+  const isSubmissionCompleted = isLastStep && submissionID
 
   return (
     <Sticky withoutHeaderOffset>
@@ -24,15 +24,18 @@ export const SubmissionHeader: React.FC = () => {
               <HorizontalPadding>
                 <Flex
                   flexDirection="row"
-                  justifyContent={saveAndExitMobile ? "end" : "space-between"}
+                  justifyContent={[
+                    submissionID ? "end" : "space-between",
+                    "space-between",
+                  ]}
                   alignItems="center"
                   py={[1, 4]}
                 >
-                  {!isMobile && (
+                  <Media greaterThan="xs">
                     <RouterLink to={"/sell"} display="block">
                       <ArtsyLogoIcon display="block" />
                     </RouterLink>
-                  )}
+                  </Media>
                   {submissionID ? (
                     <RouterLink
                       to={"/sell"}
@@ -47,7 +50,14 @@ export const SubmissionHeader: React.FC = () => {
                       display="block"
                       textDecoration={["none", "underline"]}
                     >
-                      {isMobile && !!isLastStep ? <CloseIcon /> : "Exit"}
+                      <Media at="xs">
+                        {isSubmissionCompleted && "Exit"}
+                        {!submissionID && <CloseIcon />}
+                      </Media>
+
+                      <Media greaterThan="xs">
+                        {isSubmissionCompleted || !submissionID ? "Exit" : ""}
+                      </Media>
                     </RouterLink>
                   )}
                 </Flex>
