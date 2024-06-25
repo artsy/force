fe88f2950f69bcb79a024b3b4e927178a642e275
Matt Jones <matt.jones@artsymail.com>
2024-06-25 16:56:13 -0400

    feat: add user related endpoints

--- src/Apps/ArtAdvisor/07-Curated-Discovery/server.ts
+++ src/Apps/ArtAdvisor/07-Curated-Discovery/server.ts
@@ -6,6 +6,54 @@ import { extractBudgetIntent } from "./llm/extractBudgetIntent"
 
 const weaviateDB = new WeaviateDB()
 
+const getUser = async (req: ArtsyRequest, res: ArtsyResponse) => {
+  const { id } = req.params
+  const user = await weaviateDB.getUser({ internalID: id })
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404).json("User not found")
+  }
+}
+
+const createUser = async (req: ArtsyRequest, res: ArtsyResponse) => {
+  const { name, userId } = req.body
+
+  if (!userId) throw new Error("Provide a userId")
+
+  const user = await weaviateDB.createUser({
+    internalID: userId,
+    name,
+  })
+  res.json(user)
+}
+
+const createArtworkLike = async (req: ArtsyRequest, res: ArtsyResponse) => {
+  let { userId, artworkId } = req.body
+
+  if (!userId) throw new Error("Provide a userId")
+
+  const result = await weaviateDB.reactToArtwork({
+    userInternalID: userId,
+    artworkInternalID: artworkId,
+    reaction: "like",
+  })
+  res.json(result)
+}
+
+const createArtworkDislike = async (req: ArtsyRequest, res: ArtsyResponse) => {
+  let { userId, artworkId } = req.body
+
+  if (!userId) throw new Error("Provide a userId")
+
+  const result = await weaviateDB.reactToArtwork({
+    userInternalID: userId,
+    artworkInternalID: artworkId,
+    reaction: "dislike",
+  })
+  res.json(result)
+}
+
 const getBudgetIntent = async (req: ArtsyRequest, res: ArtsyResponse) => {
   const { budget } = req.query
   const intent = await extractBudgetIntent(budget)
@@ -43,5 +91,9 @@ const getNearArticles = async (req: ArtsyRequest, res: ArtsyResponse) => {
 export const router = express.Router()
 
 router.get("/articles", getNearArticles)
+router.post("/artworks/dislikes", createArtworkDislike)
+router.post("/artworks/likes", createArtworkLike)
 router.get("/budget/intent", getBudgetIntent)
 router.get("/marketing_collections", getMarketingCollections)
+router.post("/users", createUser)
+router.get("/users/:id", getUser)
