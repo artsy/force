832f2efa64746f43e23a65143a14d4b5cea23389
Matt Jones <matt.jones@artsymail.com>
2024-06-27 14:45:37 -0400

    feat: update ArtworksRail and Artworks with user reactions

--- src/Apps/ArtAdvisor/07-Curated-Discovery/Components/Result/Artwork.tsx
+++ src/Apps/ArtAdvisor/07-Curated-Discovery/Components/Result/Artwork.tsx
@@ -1,23 +1,96 @@
 import { FC } from "react"
-import { Box, Image, Link, Text } from "@artsy/palette"
+import { Box, Clickable, Flex, Image, useToasts, Text } from "@artsy/palette"
 import { DiscoveryArtwork } from "Apps/ArtAdvisor/07-Curated-Discovery/Components/Result/ArtworksRail"
+import CheckmarkFillIcon from "@artsy/icons/CheckmarkFillIcon"
+import CloseFillIcon from "@artsy/icons/CloseFillIcon"
+import { useSystemContext } from "System/Hooks/useSystemContext"
+import _ from "lodash"
 
 interface ArtworkProps {
   artwork: DiscoveryArtwork
+  setExcludeArtworkIds: any
 }
 
 export const Artwork: FC<ArtworkProps> = props => {
-  const { artwork } = props
+  const { artwork, setExcludeArtworkIds } = props
+  const { sendToast } = useToasts()
+  const { user } = useSystemContext()
+
+  const handleClickLike = async () => {
+    try {
+      const like = await fetch("/api/advisor/7/artworks/likes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: user?.id,
+          artworkId: artwork.internalID,
+        }),
+      })
+
+      if (like.ok) {
+        setExcludeArtworkIds(prevState => [...prevState, artwork.internalID])
+      }
+
+      sendToast({
+        message: `Liked ${artwork.title}`,
+      })
+    } catch (error) {
+      console.error(error)
+      sendToast({
+        variant: "error",
+        message: `Something went wrong liking artwork: ${artwork.title}`,
+      })
+    }
+  }
+
+  const handleClickDislike = async () => {
+    try {
+      const dislike = await fetch("/api/advisor/7/artworks/dislikes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: user?.id,
+          artworkId: artwork.internalID,
+        }),
+      })
+
+      if (dislike.ok) {
+        setExcludeArtworkIds(prevState => [...prevState, artwork.internalID])
+      }
+
+      sendToast({
+        message: `Disliked ${artwork.title}`,
+      })
+    } catch (error) {
+      console.error(error)
+      sendToast({
+        variant: "error",
+        message: `Something went wrong disliking artwork: ${artwork.title}`,
+      })
+    }
+  }
 
   return (
-    <Link href={`/artwork/${artwork.slug}`} target="artwork">
-      <Box>
-        <Image src={artwork.imageUrl} height={300} />
-        <Box textAlign={"center"}>
-          <Text>{artwork.title}</Text>
-          <Text>{artwork.price}</Text>
-        </Box>
-      </Box>
-    </Link>
+    <Box>
+      <Image src={artwork.imageUrl} height={300} />
+      <Flex flexDirection="column" alignItems="center">
+        <a href={`/artwork/${artwork.slug}`} target="_blank">
+          {artwork.title}
+        </a>
+        <Text>{artwork.price}</Text>
+        <Flex py={1} gap={1} justifyContent={"center"}>
+          <Clickable onClick={handleClickLike}>
+            <CheckmarkFillIcon fill={"black60"} size={30} />
+          </Clickable>
+          <Clickable onClick={handleClickDislike}>
+            <CloseFillIcon fill={"black60"} size={30} />
+          </Clickable>
+        </Flex>
+      </Flex>
+    </Box>
   )
 }
--- src/Apps/ArtAdvisor/07-Curated-Discovery/Components/Result/ArtworksRail.tsx
+++ src/Apps/ArtAdvisor/07-Curated-Discovery/Components/Result/ArtworksRail.tsx
@@ -1,8 +1,9 @@
 import React, { FC, useEffect, useState } from "react"
-import { Box, SkeletonBox, Text } from "@artsy/palette"
+import { Box, SkeletonBox } from "@artsy/palette"
 import { Rail } from "Components/Rail/Rail"
 import { BudgetIntent, State } from "Apps/ArtAdvisor/07-Curated-Discovery/App"
 import { Artwork } from "Apps/ArtAdvisor/07-Curated-Discovery/Components/Result/Artwork"
+import { useSystemContext } from "System/Hooks/useSystemContext"
 
 interface ArtworksRailProps {
   state: State
@@ -39,31 +40,52 @@ export const ArtworksRail: FC<ArtworksRailProps> = props => {
   const { state } = props
   const [artworks, setArtworks] = useState<DiscoveryArtwork[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(true)
+  const [excludeArtworkIds, setExcludeArtworkIds] = useState<string[]>([])
+
+  const { user } = useSystemContext()
 
   useEffect(() => {
-    const params = new URLSearchParams()
-    state.interests.forEach(concept => {
-      params.append("concepts", concept)
-    })
+    const fetchArtworks = async (options: {
+      concepts: string[]
+      excludeArtworkIds: string[]
+    }) => {
+      setIsLoading(true)
 
-    const { priceMinUSD, priceMaxUSD } = getPriceRange(state.budgetIntent)
-    if (priceMinUSD) params.append("priceMinUSD", priceMinUSD.toString())
-    if (priceMaxUSD) params.append("priceMaxUSD", priceMaxUSD.toString())
+      const { concepts, excludeArtworkIds } = options
+      const { priceMinUSD, priceMaxUSD } = getPriceRange(state.budgetIntent)
 
-    const fetchArtworks = async () => {
+      const params = new URLSearchParams()
+
+      if (priceMinUSD) params.append("priceMinUSD", priceMinUSD.toString())
+      if (priceMaxUSD) params.append("priceMaxUSD", priceMaxUSD.toString())
+      concepts.forEach(concept => {
+        params.append("concepts", concept)
+      })
+      excludeArtworkIds.forEach(id => {
+        params.append("excludeArtworkIds", id)
+      })
+      params.append("userId", user?.id || "")
+      params.append("goal", state.goal)
+
       const response = await fetch(
         `/api/advisor/7/artworks?${params.toString()}`
       )
       const data = await response.json()
-      setArtworks(data)
       setIsLoading(false)
+      return data
     }
 
-    fetchArtworks()
-  }, [state.interests, state.goal, state.budgetIntent])
+    const options = {
+      concepts: state.interests,
+      userId: user?.id,
+      excludeArtworkIds,
+    }
+
+    fetchArtworks(options).then(setArtworks)
+  }, [state.interests, state.goal, state.budgetIntent, excludeArtworkIds, user])
 
   if (isLoading) {
-    return <Text>Loading...</Text>
+    return <SkeletonBox height="400px">Loading...</SkeletonBox>
   }
 
   return (
@@ -74,7 +96,13 @@ export const ArtworksRail: FC<ArtworksRailProps> = props => {
             title="Artworks"
             getItems={() => {
               return artworks.map((artwork: DiscoveryArtwork) => {
-                return <Artwork key={artwork.id} artwork={artwork} />
+                return (
+                  <Artwork
+                    key={artwork.id}
+                    artwork={artwork}
+                    setExcludeArtworkIds={setExcludeArtworkIds}
+                  />
+                )
               })
             }}
           />
