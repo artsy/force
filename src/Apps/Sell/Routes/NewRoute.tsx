1213b0054e4b5edff0e5ec85590f84171f7889ab
Ole <ole.richter91@gmail.com>
2024-07-01 10:45:55 +0200

    chore: Add invisible ReCaptcha to new Sell flow

--- src/Apps/Sell/Routes/NewRoute.tsx
+++ src/Apps/Sell/Routes/NewRoute.tsx
@@ -2,11 +2,14 @@ import { FullBleed } from "@artsy/palette"
 import { AppContainer } from "Apps/Components/AppContainer"
 import { ArtistRoute } from "Apps/Sell/Routes/ArtistRoute"
 import { SellFlowContextProvider } from "Apps/Sell/SellFlowContext"
+import { EnableRecaptcha } from "Utils/EnableRecaptcha"
 import * as React from "react"
 
 export const NewRoute: React.FC = () => {
   return (
     <FullBleed>
+      <EnableRecaptcha />
+
       <AppContainer>
         <SellFlowContextProvider>
           <ArtistRoute />
