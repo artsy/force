4d21d10761ef4616a3185c1a705c3318b43422b4
Ole <ole.richter91@gmail.com>
2024-07-01 11:41:13 +0200

    feat: New Sell flow meta tags (#14102)
    * feat: New Sell flow meta tags
    
    * update tests

--- src/Apps/Consign/Routes/MarketingLanding/Components/SellMeta.tsx
+++ src/Apps/Consign/Routes/MarketingLanding/Components/SellMeta.tsx
@@ -4,8 +4,8 @@ export const SellMeta: React.FC = () => {
   return (
     <MetaTags
       pathname="/sell"
-      title="Sell Artwork with Artsy | Art Consignment | Artsy"
-      description="Sell and consign your art with Artsy. Get competitive offers from the world’s top auction houses and galleries to sell art from your collection. Submit today at no cost."
+      title="Sell Art from Your Collection | Consignments | Artsy"
+      description="Get competitive offers from the world's top auction houses and galleries to sell art from your collection. Submit today at no cost."
     />
   )
 }
