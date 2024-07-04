c7a80bcfadc1686bd4ccd56135065807e13d299b
dariakoko <nargle30@gmail.com>
2024-07-04 16:45:38 +0200

    chore: address review comments

--- src/Apps/Sell/Utils/formUtils.ts
+++ src/Apps/Sell/Utils/formUtils.ts
@@ -1,15 +1,12 @@
 export const PROVENANCE_LIST = [
-  { value: "", text: "Select" },
-  {
-    value: "Purchased directly from gallery",
-    text: "Purchased directly from gallery",
-  },
-  {
-    value: "Purchased directly from artist",
-    text: "Purchased directly from artist",
-  },
-  { value: "Purchased at auction", text: "Purchased at auction" },
-  { value: "Gift from the artist", text: "Gift from the artist" },
-  { value: "Other", text: "Other" },
-  { value: "I don’t know", text: "I don’t know" },
-]
+  "",
+  "Purchased directly from gallery",
+  "Purchased directly from artist",
+  "Purchased at auction",
+  "Gift from the artist",
+  "Other",
+  "I don’t know",
+].map(provenance => ({
+  value: provenance,
+  text: provenance || "Select",
+}))
