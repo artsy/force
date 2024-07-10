09c4924d0a30cf799dd9111176a133c1a17601b4
Erik <9088720+erikdstock@users.noreply.github.com>
2024-07-10 16:34:27 -0500

    reorganize test cases, cleanup

--- src/Utils/__tests__/useCollectorSignals.jest.tsx
+++ src/Utils/__tests__/useCollectorSignals.jest.tsx
@@ -9,118 +9,134 @@ jest.unmock("react-relay")
 jest.mock("System/Hooks/useFeatureFlag", () => ({
   useFeatureFlag: jest.fn(),
 }))
-const mockUseFeatureFlag = useFeatureFlag as jest.Mock
-
-// We are testing a hook, but because we are using a Relay fragment we must
-// create masked fragments with our mocked environment and resolvers
-const TestApp = props => {
-  const result = useCollectorSignals(props)
-  return <div data-testid="result">{JSON.stringify(result)}</div>
-}
-
-const getResult = () => JSON.parse(screen.getByTestId("result").textContent!)
-
-const { renderWithRelay: renderWithArtworksConnection } = setupTestWrapperTL({
-  Component: TestApp,
-  query: graphql`
-    query useCollectorSignalsArtworksConnectionTestQuery {
-      artworksConnection(first: 3) {
-        ...useCollectorSignals_artworksConnection
-      }
-      me {
-        ...useCollectorSignals_me
-      }
-    }
-  `,
-})
-
-const { renderWithRelay: renderWithSingleArtwork } = setupTestWrapperTL({
-  Component: TestApp,
-  query: graphql`
-    query useCollectorSignalsSingleArtworkTestQuery {
-      artwork(id: "artwork-id") {
-        ...useCollectorSignals_artwork
-      }
-      me {
-        ...useCollectorSignals_me
-      }
-    }
-  `,
-})
 
 describe("useCollectorSignals", () => {
   beforeEach(() => {
     mockUseFeatureFlag.mockReturnValue(true)
   })
 
-  it("should process signals for multiple artworks", async () => {
-    await renderWithArtworksConnection({
-      FilterArtworksConnection: () => ({
-        edges: [
-          {
-            node: {
-              internalID: "acquireable-with-partner-offer",
-              isAcquireable: true,
-            },
-          },
-          { node: { internalID: "not-acquireable", isAcquireable: false } },
-          { node: { internalID: "no-partner-offer", isAcquireable: true } },
-        ],
-      }),
-      Me: () => ({
-        partnerOffersConnection: {
+  const mockUseFeatureFlag = useFeatureFlag as jest.Mock
+
+  // We are testing a hook, but because we are using a Relay fragment we must
+  // create masked fragments with our mocked environment and resolvers
+  const TestApp = props => {
+    const result = useCollectorSignals(props)
+    return <div data-testid="result">{JSON.stringify(result)}</div>
+  }
+
+  const getResult = () => JSON.parse(screen.getByTestId("result").textContent!)
+
+  describe("artworksConnection", () => {
+    const { renderWithRelay } = setupTestWrapperTL({
+      Component: TestApp,
+      query: graphql`
+        query useCollectorSignalsArtworksConnectionTestQuery {
+          artworksConnection(first: 3) {
+            ...useCollectorSignals_artworksConnection
+          }
+          me {
+            ...useCollectorSignals_me
+          }
+        }
+      `,
+    })
+
+    it("can handle empty arguments", async () => {
+      await renderWithRelay({
+        FilterArtworksConnection: () => null,
+        Me: () => null,
+      })
+
+      const result = getResult()
+
+      expect(result).toEqual({})
+    })
+
+    it("should process signals for multiple artworks", async () => {
+      await renderWithRelay({
+        FilterArtworksConnection: () => ({
           edges: [
             {
               node: {
-                artworkId: "acquireable-with-partner-offer",
-                endAt: "2042-01-01",
+                internalID: "acquireable-with-partner-offer",
+                isAcquireable: true,
               },
             },
-            { node: { artworkId: "not-acquireable", endAt: "2042-01-01" } },
+            { node: { internalID: "not-acquireable", isAcquireable: false } },
+            { node: { internalID: "no-partner-offer", isAcquireable: true } },
           ],
+        }),
+        Me: () => ({
+          partnerOffersConnection: {
+            edges: [
+              {
+                node: {
+                  artworkId: "acquireable-with-partner-offer",
+                  endAt: "2042-01-01",
+                },
+              },
+              { node: { artworkId: "not-acquireable", endAt: "2042-01-01" } },
+            ],
+          },
+        }),
+      })
+
+      const result = getResult()
+
+      expect(result["acquireable-with-partner-offer"]).toEqual({
+        partnerOffer: {
+          artworkId: "acquireable-with-partner-offer",
+          endAt: "2042-01-01",
         },
-      }),
+      })
+      expect(result["not-acquireable"]).toBeUndefined()
+      expect(result["no-partner-offer"]).toBeUndefined()
     })
-
-    const result = getResult()
-
-    expect(result["acquireable-with-partner-offer"]).toEqual({
-      partnerOffer: expect.objectContaining({
-        artworkId: "acquireable-with-partner-offer",
-        endAt: "2042-01-01",
-      }),
-    })
-    expect(result["not-acquireable"]).toBeUndefined()
-    expect(result["no-partner-offer"]).toBeUndefined()
   })
 
-  it("should process signals for a single artwork", async () => {
-    await renderWithSingleArtwork({
-      Artwork: () => ({
-        internalID: "acquireable-with-partner-offer",
-        isAcquireable: true,
-      }),
-      Me: () => ({
-        partnerOffersConnection: {
-          edges: [
-            {
-              node: {
-                artworkId: "acquireable-with-partner-offer",
-                endAt: "2042-01-01",
-              },
-            },
-          ],
-        },
-      }),
+  describe("single artwork", () => {
+    const { renderWithRelay: renderWithSingleArtwork } = setupTestWrapperTL({
+      Component: TestApp,
+      query: graphql`
+        query useCollectorSignalsSingleArtworkTestQuery {
+          artwork(id: "artwork-id") {
+            ...useCollectorSignals_artwork
+          }
+          me {
+            ...useCollectorSignals_me
+          }
+        }
+      `,
     })
 
-    const result = getResult()
+    it("should process signals for a single artwork", async () => {
+      await renderWithSingleArtwork({
+        Artwork: () => ({
+          internalID: "acquireable-with-partner-offer",
+          isAcquireable: true,
+        }),
+        Me: () => ({
+          partnerOffersConnection: {
+            edges: [
+              {
+                node: {
+                  artworkId: "acquireable-with-partner-offer",
+                  endAt: "2042-01-01",
+                },
+              },
+            ],
+          },
+        }),
+      })
 
-    expect(result).toEqual({
-      partnerOffer: expect.objectContaining({
-        artworkId: "acquireable-with-partner-offer",
-        endAt: "2042-01-01",
-      }),
+      const result = getResult()
+
+      expect(result).toEqual({
+        partnerOffer: {
+          artworkId: "acquireable-with-partner-offer",
+          endAt: "2042-01-01",
+        },
+      })
     })
   })
 })
