d47e3cc0065ebf7054886675a4ef79ecaedc8384
dzucconi <mail@damonzucconi.com>
2024-07-19 10:25:19 -0400

    feat: implements app download banner with story configurator

---
+++ src/Components/__stories__/AppDownloadBanner.story.tsx
@@ -0,0 +1,47 @@
+import { States } from "storybook-states"
+import {
+  AppDownloadBanner,
+  AppDownloadBannerProps,
+} from "Components/AppDownloadBanner"
+import { Input, Stack } from "@artsy/palette"
+import { useState } from "react"
+
+export default {
+  title: "Components/AppDownloadBanner",
+}
+
+export const Default = () => {
+  const [transitionDuration, setTransitionDuration] = useState(1000)
+  const [idleDuration, setIdleDuration] = useState(3000)
+
+  return (
+    <Stack gap={2}>
+      <Stack gap={1} flexDirection="row" maxWidth={400}>
+        <Input
+          title="Transition duration"
+          type="number"
+          value={transitionDuration}
+          onChange={event => {
+            setTransitionDuration(parseInt(event.target.value))
+          }}
+        />
+
+        <Input
+          title="Idle duration"
+          type="number"
+          value={idleDuration}
+          onChange={event => {
+            setIdleDuration(parseInt(event.target.value))
+          }}
+        />
+      </Stack>
+
+      <States<Partial<AppDownloadBannerProps>> states={[{}]}>
+        <AppDownloadBanner
+          transitionDuration={transitionDuration}
+          idleDuration={idleDuration}
+        />
+      </States>
+    </Stack>
+  )
+}
