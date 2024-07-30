04abac0e0afe3a48202d5575a625f651250373e3
Christopher Pappas <damassi.pappas@gmail.com>
2024-07-30 13:47:41 -0700

    fix(caching): for SSR pass, point to MP url

--- src/System/Relay/getMetaphysicsEndpoint.ts
+++ src/System/Relay/getMetaphysicsEndpoint.ts
@@ -1,11 +1,24 @@
+import { isServer } from "Server/isServer"
 import { getENV } from "Utils/getENV"
 
 export const getMetaphysicsEndpoint = () => {
   const APP_URL = getENV("APP_URL") ?? "http://localhost:4000"
 
-  const endpoint = getENV("ENABLE_GRAPHQL_PROXY")
-    ? `${APP_URL}/api/metaphysics`
-    : `${getENV("METAPHYSICS_ENDPOINT")}/v2`
+  const endpoint = (() => {
+    const metaphysicsEndpoint = `${getENV("METAPHYSICS_ENDPOINT")}/v2`
+
+    // Always return MP on the server.
+    if (isServer) {
+      return metaphysicsEndpoint
+    }
+
+    if (getENV("ENABLE_GRAPHQL_PROXY")) {
+      return `${APP_URL}/api/metaphysics`
+    }
+
+    // Fallback to MP as a default
+    return metaphysicsEndpoint
+  })()
 
   return endpoint
 }
