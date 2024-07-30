fe4238047d675659899b71140ac4bf8de0fd6560
Devon Blandin <dblandin@gmail.com>
2024-07-30 17:59:59 -0400

    feat: prompt for authentication for SWA > Add to MyC
    After selecting an artist ineligible for submission, we suggest that the user
    can add the artwork to My Collection instead. Since those surfaces require
    authentication, trigger the auth modal in response to that button click.

--- src/Apps/Sell/Routes/__tests__/ArtistNotEligibleRoute.jest.tsx
+++ src/Apps/Sell/Routes/__tests__/ArtistNotEligibleRoute.jest.tsx
@@ -1,11 +1,13 @@
-import { screen } from "@testing-library/react"
+import { screen, waitFor } from "@testing-library/react"
 import { ArtistNotEligibleRoute } from "Apps/Sell/Routes/ArtistNotEligibleRoute"
+import { useAuthDialog } from "Components/AuthDialog/useAuthDialog"
 import { setupTestWrapperTL } from "DevTools/setupTestWrapper"
 import { useRouter } from "System/Hooks/useRouter"
 import { useSystemContext } from "System/Hooks/useSystemContext"
 import { graphql } from "react-relay"
 
 const mockUseRouter = useRouter as jest.Mock
+const mockUseSystemContext = useSystemContext as jest.Mock
 const mockPush = jest.fn()
 const mockReplace = jest.fn()
 
@@ -20,6 +22,9 @@ jest.mock("react-relay", () => ({
 }))
 jest.mock("System/Hooks/useFeatureFlag", () => ({
   useFeatureFlag: jest.fn(() => true),
+}))
+jest.mock("Components/AuthDialog/useAuthDialog", () => ({
+  useAuthDialog: jest.fn().mockReturnValue({ showAuthDialog: jest.fn() }),
 }))
 
 let pathnameMock: string
@@ -67,11 +72,6 @@ describe("ArtistNotEligibleRoute", () => {
 
     expect(screen.getByText("Add Another Artist")).toBeInTheDocument()
 
-    expect(screen.getByTestId("add-to-collection")).toHaveAttribute(
-      "href",
-      "/collector-profile/my-collection/artworks/new"
-    )
-
     expect(screen.getByTestId("view-collection")).toHaveAttribute(
       "href",
       "/sell/submissions/new"
@@ -89,5 +89,39 @@ describe("ArtistNotEligibleRoute", () => {
     expect(
       screen.getByText("what our specialists are looking for")
     ).toHaveAttribute("href", "/sell/faq")
+  })
+
+  describe("while logged out", () => {
+    const mockUseAuthDialog = useAuthDialog as jest.Mock
+
+    beforeEach(() => {
+      mockUseSystemContext.mockImplementation(() => {
+        return { isLoggedIn: false }
+      })
+    })
+
+    it("prompts for authentication", async () => {
+      const showAuthDialog = jest.fn()
+      mockUseAuthDialog.mockImplementation(() => ({ showAuthDialog }))
+
+      renderWithRelay({})
+
+      screen.getByText("Add to My Collection").click()
+
+      await waitFor(() => {
+        expect(showAuthDialog).toBeCalledWith({
+          mode: "SignUp",
+          options: {
+            title: expect.any(Function),
+            redirectTo: "/collector-profile/my-collection/artworks/new",
+          },
+          analytics: {
+            contextModule: "sell",
+            intent: "addToMyCollection",
+            trigger: "click",
+          },
+        })
+      })
+    })
   })
 })
