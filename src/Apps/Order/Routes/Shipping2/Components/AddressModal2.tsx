bd4aa4a4ac6b7309b2d90a29076ea06e49b4e126
Erik <9088720+erikdstock@users.noreply.github.com>
2024-08-29 16:37:42 -0500

    dont render address modal until there is a reason to

--- src/Apps/Order/Routes/Shipping2/Components/AddressModal2.tsx
+++ src/Apps/Order/Routes/Shipping2/Components/AddressModal2.tsx
@@ -43,7 +43,7 @@ export type AddressModalAction =
 export interface AddressModalProps {
   closeModal: () => void
   onSuccess: (address: SavedAddressType) => void
-  addressModalAction: AddressModalAction | null
+  addressModalAction: AddressModalAction
 }
 
 interface FormValues {
@@ -71,34 +71,22 @@ export const AddressModal: FC<AddressModalProps> = ({
 
   const { executeUserAddressAction } = useUserAddressUpdates()
 
-  let initialValues: FormValues
+  const incomingAddress =
+    addressModalAction.type === "edit" ? addressModalAction.address : null
 
-  if (!addressModalAction) {
-    initialValues = {
-      attributes: {
-        isDefault: false,
-        ...addressWithFallbackValues({}),
-      },
-      setAsDefault: false,
-    }
-  } else {
-    const incomingAddress =
-      addressModalAction.type === "edit" ? addressModalAction.address : null
+  const initialValues: FormValues = {
+    attributes: {
+      isDefault: incomingAddress?.isDefault ?? false,
 
-    initialValues = {
-      attributes: {
-        isDefault: incomingAddress?.isDefault ?? false,
-
-        ...addressWithFallbackValues(
-          addressModalAction.type === "edit"
-            ? addressModalAction.address
-            : addressWithFallbackValues({
-                country: shippingContext.orderData.shipsFrom,
-              })
-        ),
-      },
-      setAsDefault: false,
-    }
+      ...addressWithFallbackValues(
+        addressModalAction.type === "edit"
+          ? addressModalAction.address
+          : {
+              country: shippingContext.orderData.shipsFrom,
+            }
+      ),
+    },
+    setAsDefault: false,
   }
 
   const handleSubmit = async (
