2fbcaa9e3e6ac572374708ea8c4620114d45c5f6
Christopher Pappas <damassi.pappas@gmail.com>
2024-09-16 12:45:00 -0700

    chore(proxy): Delete all MP proxy code

--- src/System/Relay/getMetaphysicsEndpoint.ts
+++ src/System/Relay/getMetaphysicsEndpoint.ts
@@ -1,13 +1,7 @@
 import { getENV } from "Utils/getENV"
 
 export const getMetaphysicsEndpoint = () => {
-  const APP_URL = getENV("APP_URL") ?? "http://localhost:4000"
-
-  const endpoint =
-    // Only use the proxy if logged out
-    getENV("ENABLE_GRAPHQL_PROXY")
-      ? `${APP_URL}/api/metaphysics`
-      : `${getENV("METAPHYSICS_ENDPOINT")}/v2`
+  const endpoint = `${getENV("METAPHYSICS_ENDPOINT")}/v2`
 
   return endpoint
 }
