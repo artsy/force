299302617b648e3e8ebd3e72f4ee45f22c4788ce
Matthew Zikherman <mzikherman@gmail.com>
2024-09-17 13:28:25 -0400

    feat: support ?nocache query param to skip CDN caching as an escape hatch / support preview functionality

--- src/System/Relay/isRequestCacheable.ts
+++ src/System/Relay/isRequestCacheable.ts
@@ -4,3 +4,14 @@ export const isRequestCacheable = req => {
 
   return queryText && CACHEABLE_DIRECTIVE_REGEX.test(queryText)
 }
+
+export const hasNoCacheParamPresent = url => {
+  const queryString = url?.split("?")[1]
+  const urlParams = new URLSearchParams(queryString)
+  const noCache = urlParams.get("nocache")
+  if (noCache) {
+    return true
+  }
+
+  return false
+}
