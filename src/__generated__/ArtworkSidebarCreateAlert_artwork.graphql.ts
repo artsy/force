d047d706bf644939386880d07c6beba825d6d70f
devon blandin <dblandin@gmail.com>
2024-09-20 20:33:41 +0200

    chore: remove overfetching in ArtworkSidebarCreateAlert (#14530)
    Since revising the alert flow, this querying just for this button is no longer
    necessary.

--- src/__generated__/ArtworkSidebarCreateAlert_artwork.graphql.ts
+++ src/__generated__/ArtworkSidebarCreateAlert_artwork.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<aacb3d6c478ada596b97ec98f51cc1bd>>
+ * @generated SignedSource<<f5a0bd9f91f8ab31eaff59623b51370e>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -11,24 +11,7 @@
 import { Fragment, ReaderFragment } from 'relay-runtime';
 import { FragmentRefs } from "relay-runtime";
 export type ArtworkSidebarCreateAlert_artwork$data = {
-  readonly artists: ReadonlyArray<{
-    readonly internalID: string;
-    readonly name: string | null | undefined;
-    readonly slug: string;
-  } | null | undefined> | null | undefined;
-  readonly attributionClass: {
-    readonly internalID: string;
-  } | null | undefined;
-  readonly internalID: string;
   readonly isEligibleToCreateAlert: boolean;
-  readonly mediumType: {
-    readonly filterGene: {
-      readonly name: string | null | undefined;
-      readonly slug: string;
-    } | null | undefined;
-  } | null | undefined;
-  readonly slug: string;
-  readonly title: string | null | undefined;
   readonly " $fragmentType": "ArtworkSidebarCreateAlert_artwork";
 };
 export type ArtworkSidebarCreateAlert_artwork$key = {
@@ -36,106 +19,24 @@ export type ArtworkSidebarCreateAlert_artwork$key = {
   readonly " $fragmentSpreads": FragmentRefs<"ArtworkSidebarCreateAlert_artwork">;
 };
 
-const node: ReaderFragment = (function(){
-var v0 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "internalID",
-  "storageKey": null
-},
-v1 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "slug",
-  "storageKey": null
-},
-v2 = {
-  "alias": null,
-  "args": null,
-  "kind": "ScalarField",
-  "name": "name",
-  "storageKey": null
-};
-return {
+const node: ReaderFragment = {
   "argumentDefinitions": [],
   "kind": "Fragment",
   "metadata": null,
   "name": "ArtworkSidebarCreateAlert_artwork",
   "selections": [
-    (v0/*: any*/),
-    {
-      "alias": null,
-      "args": null,
-      "kind": "ScalarField",
-      "name": "title",
-      "storageKey": null
-    },
-    (v1/*: any*/),
     {
       "alias": null,
       "args": null,
       "kind": "ScalarField",
       "name": "isEligibleToCreateAlert",
       "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "Artist",
-      "kind": "LinkedField",
-      "name": "artists",
-      "plural": true,
-      "selections": [
-        (v0/*: any*/),
-        (v2/*: any*/),
-        (v1/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "AttributionClass",
-      "kind": "LinkedField",
-      "name": "attributionClass",
-      "plural": false,
-      "selections": [
-        (v0/*: any*/)
-      ],
-      "storageKey": null
-    },
-    {
-      "alias": null,
-      "args": null,
-      "concreteType": "ArtworkMedium",
-      "kind": "LinkedField",
-      "name": "mediumType",
-      "plural": false,
-      "selections": [
-        {
-          "alias": null,
-          "args": null,
-          "concreteType": "Gene",
-          "kind": "LinkedField",
-          "name": "filterGene",
-          "plural": false,
-          "selections": [
-            (v1/*: any*/),
-            (v2/*: any*/)
-          ],
-          "storageKey": null
-        }
-      ],
-      "storageKey": null
     }
   ],
   "type": "Artwork",
   "abstractKey": null
 };
-})();
 
-(node as any).hash = "83ea48adca40a35e3c0cb5d210612df2";
+(node as any).hash = "285afab1fd1739e8437c6e165884bb7f";
 
 export default node;
