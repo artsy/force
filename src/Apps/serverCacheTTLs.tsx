d4bac8087e3b74f573695be3864a0ccdfb025215
Matthew Zikherman <mzikherman@gmail.com>
2024-09-29 13:10:56 -0400

    feat: add explicit 5 minute caching to article routes

--- src/Apps/serverCacheTTLs.tsx
+++ src/Apps/serverCacheTTLs.tsx
@@ -1,5 +1,6 @@
 const NO_CACHE = 0
 const HOURS_24 = 86400 // In seconds
+const MINUTES_5 = 300 // In seconds
 
 /**
  * This defines non-default cache TTLs for specific routes. Default is typically
@@ -8,8 +9,8 @@ const HOURS_24 = 86400 // In seconds
  */
 export const serverCacheTTLs = {
   artist: HOURS_24,
-  article: NO_CACHE,
-  articles: NO_CACHE,
+  article: MINUTES_5,
+  articles: MINUTES_5,
   artists: HOURS_24,
   artistSeries: HOURS_24,
   auction: NO_CACHE,
