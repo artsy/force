e680e085707a9f7c2b2b4c2fbcc4011917303b7f
Chung-Yi Chi <chung-yi@artsymail.com>
2024-10-17 09:34:41 -0400

    feat: overwrite public pricing after fetching offers

---
+++ src/__generated__/PartnerOfferedPriceQuery.graphql.ts
@@ -0,0 +1,156 @@
+/**
+ * @generated SignedSource<<c15893aacbb7161b42ba9c8e1db0fbac>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest, Query } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type PartnerOfferedPriceQuery$variables = {
+  id: string;
+};
+export type PartnerOfferedPriceQuery$data = {
+  readonly artwork: {
+    readonly " $fragmentSpreads": FragmentRefs<"PartnerOfferedPrice_artwork">;
+  } | null | undefined;
+};
+export type PartnerOfferedPriceQuery = {
+  response: PartnerOfferedPriceQuery$data;
+  variables: PartnerOfferedPriceQuery$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "defaultValue": null,
+    "kind": "LocalArgument",
+    "name": "id"
+  }
+],
+v1 = [
+  {
+    "kind": "Variable",
+    "name": "id",
+    "variableName": "id"
+  }
+],
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "PartnerOfferedPriceQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "Artwork",
+        "kind": "LinkedField",
+        "name": "artwork",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "PartnerOfferedPrice_artwork"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": (v0/*: any*/),
+    "kind": "Operation",
+    "name": "PartnerOfferedPriceQuery",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v1/*: any*/),
+        "concreteType": "Artwork",
+        "kind": "LinkedField",
+        "name": "artwork",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "CollectorSignals",
+            "kind": "LinkedField",
+            "name": "collectorSignals",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "PartnerOfferToCollector",
+                "kind": "LinkedField",
+                "name": "partnerOffer",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "endAt",
+                    "storageKey": null
+                  },
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "Money",
+                    "kind": "LinkedField",
+                    "name": "priceWithDiscount",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "kind": "ScalarField",
+                        "name": "display",
+                        "storageKey": null
+                      }
+                    ],
+                    "storageKey": null
+                  },
+                  (v2/*: any*/)
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          (v2/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "94df35b04390a18848aba366c104f4cf",
+    "id": null,
+    "metadata": {},
+    "name": "PartnerOfferedPriceQuery",
+    "operationKind": "query",
+    "text": "query PartnerOfferedPriceQuery(\n  $id: String!\n) {\n  artwork(id: $id) {\n    ...PartnerOfferedPrice_artwork\n    id\n  }\n}\n\nfragment PartnerOfferedPrice_artwork on Artwork {\n  collectorSignals {\n    partnerOffer {\n      endAt\n      priceWithDiscount {\n        display\n      }\n      id\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "f6ab5ba13662f712cb9ffb1e19f786af";
+
+export default node;
