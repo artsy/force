635607febebd87834ccbf38b21be679b66784c1f
Christopher Pappas <damassi.pappas@gmail.com>
2024-10-30 12:12:14 -0700

    refactor(tests): Migrate legacy enzyme tests to .jest.enzyme extension

---
+++ src/DevTools/__tests__/MockRelayRenderer.mocked.jest.enzyme.tsx
@@ -0,0 +1,21 @@
+import { mount } from "enzyme"
+import { MockRelayRenderer } from "DevTools/MockRelayRenderer"
+
+describe("MockRelayRenderer", () => {
+  const consoleError = console.error
+
+  beforeAll(() => {
+    console.error = jest.fn()
+  })
+
+  afterAll(() => {
+    console.error = consoleError
+  })
+
+  it("throws when react-relay is mocked", () => {
+    expect(() => {
+      // @ts-expect-error PLEASE_FIX_ME_STRICT_NULL_CHECK_MIGRATION
+      mount(<MockRelayRenderer Component={null} query={null} />)
+    }).toThrowError('jest.unmock("react-relay")')
+  })
+})
