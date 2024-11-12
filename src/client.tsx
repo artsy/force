bcf6a86b9d65280578d3158cd17e97f43991b156
Christopher Pappas <damassi.pappas@gmail.com>
2024-11-11 22:04:52 -0800

    fix: broken rehydration

--- src/client.tsx
+++ src/client.tsx
@@ -5,27 +5,25 @@ import { setupAnalytics } from "Server/analytics/helpers"
 import { setupClientRouter } from "System/Router/clientRouter"
 import { setupSentryClient } from "System/Utils/setupSentryClient"
 import { setupWebVitals } from "System/Utils/setupWebVitals"
-// import { hydrateRoot } from "react-dom/client"
-// import { createRoot } from "react-dom/client"
-import { createRoot, hydrateRoot } from "react-dom/client"
+import { hydrateRoot } from "react-dom/client"
 
 setupAnalytics()
 setupSentryClient()
 setupWebVitals()
 
-const { ClientRouter } = setupClientRouter({
-  routes: getAppRoutes(),
-})
+// Rehydrate app
+;(async () => {
+  const { ClientRouter } = await setupClientRouter({
+    routes: getAppRoutes(),
+  })
 
-// const root = createRoot(document.getElementById("react-root") as HTMLElement)
-// const root = createRoot(document.getElementById("react-root") as HTMLElement)
-
-loadableReady().then(() => {
-  hydrateRoot(
-    document.getElementById("react-root") as HTMLElement,
-    <ClientRouter />
-  )
-})
+  loadableReady().then(() => {
+    hydrateRoot(
+      document.getElementById("react-root") as HTMLElement,
+      <ClientRouter />
+    )
+  })
+})()
 
 // Enable hot-reloading if available.
 if (module.hot) {
