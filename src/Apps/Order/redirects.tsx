18ca1c4f5cef7e7f99d3a856c6f6ea7b43e9ec02
Leandro Motta <leamotta@msn.com>
2025-01-08 16:56:04 +0100

    invert if condition

--- src/Apps/Order/redirects.tsx
+++ src/Apps/Order/redirects.tsx
@@ -161,15 +161,15 @@ const goToRespondIfMyLastOfferIsNotMostRecentOffer: OrderPredicate = ({
 
 const goToRespondIfAwaitingBuyerResponse: OrderPredicate = ({ order }) => {
   if (order.awaitingResponseFrom === "BUYER")
-    if (order.displayState !== "PAYMENT_FAILED") {
-      return {
-        path: `/orders/${order.internalID}/respond`,
-        reason: "Still awaiting buyer response",
-      }
-    } else {
+    if (order.displayState === "PAYMENT_FAILED") {
       return {
         path: `/orders/${order.internalID}/payment/new`,
         reason: "Payment failed",
+      }
+    } else {
+      return {
+        path: `/orders/${order.internalID}/respond`,
+        reason: "Still awaiting buyer response",
       }
     }
 }
