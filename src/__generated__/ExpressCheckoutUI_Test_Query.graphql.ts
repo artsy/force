939d94492e834137589d48575017bf57b93cc2d3
Rachel Quartararo <50849237+rquartararo@users.noreply.github.com>
2025-03-04 11:44:39 -0500

    chore(EMI-2310): express checkout element tracking (#15269)

---
+++ src/__generated__/ExpressCheckoutUI_Test_Query.graphql.ts
@@ -0,0 +1,356 @@
+/**
+ * @generated SignedSource<<a8d44725048b022e858bd0f60bcdc2e3>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type CommerceOrderModeEnum = "BUY" | "OFFER" | "%future added value";
+export type CommerceOrderSourceEnum = "artwork_page" | "inquiry" | "partner_offer" | "private_sale" | "%future added value";
+export type ExpressCheckoutUI_Test_Query$variables = Record<PropertyKey, never>;
+export type ExpressCheckoutUI_Test_Query$data = {
+  readonly commerceOrder: {
+    readonly " $fragmentSpreads": FragmentRefs<"ExpressCheckoutUI_order">;
+  } | null | undefined;
+};
+export type ExpressCheckoutUI_Test_Query$rawResponse = {
+  readonly commerceOrder: {
+    readonly __typename: string;
+    readonly __isCommerceOrder: string;
+    readonly buyerTotalCents: number | null | undefined;
+    readonly currencyCode: string;
+    readonly id: string;
+    readonly internalID: string;
+    readonly lineItems: {
+      readonly edges: ReadonlyArray<{
+        readonly node: {
+          readonly artwork: {
+            readonly artsyShippingInternational: boolean | null | undefined;
+            readonly euShippingOrigin: boolean | null | undefined;
+            readonly id: string;
+            readonly internalID: string;
+            readonly onlyShipsDomestically: boolean | null | undefined;
+            readonly processWithArtsyShippingDomestic: boolean | null | undefined;
+            readonly shippingCountry: string | null | undefined;
+            readonly slug: string;
+          } | null | undefined;
+          readonly id: string;
+          readonly shippingQuoteOptions: {
+            readonly edges: ReadonlyArray<{
+              readonly node: {
+                readonly id: string;
+                readonly name: string | null | undefined;
+                readonly priceCents: number;
+                readonly tier: string;
+              } | null | undefined;
+            } | null | undefined> | null | undefined;
+          } | null | undefined;
+        } | null | undefined;
+      } | null | undefined> | null | undefined;
+    } | null | undefined;
+    readonly mode: CommerceOrderModeEnum | null | undefined;
+    readonly requestedFulfillment: {
+      readonly __typename: string;
+    } | null | undefined;
+    readonly shippingTotalCents: number | null | undefined;
+    readonly source: CommerceOrderSourceEnum;
+  } | null | undefined;
+};
+export type ExpressCheckoutUI_Test_Query = {
+  rawResponse: ExpressCheckoutUI_Test_Query$rawResponse;
+  response: ExpressCheckoutUI_Test_Query$data;
+  variables: ExpressCheckoutUI_Test_Query$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "id",
+    "value": "123"
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "__typename",
+  "storageKey": null
+},
+v2 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "internalID",
+  "storageKey": null
+},
+v3 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "ExpressCheckoutUI_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "commerceOrder",
+        "plural": false,
+        "selections": [
+          {
+            "args": null,
+            "kind": "FragmentSpread",
+            "name": "ExpressCheckoutUI_order"
+          }
+        ],
+        "storageKey": "commerceOrder(id:\"123\")"
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "ExpressCheckoutUI_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": (v0/*: any*/),
+        "concreteType": null,
+        "kind": "LinkedField",
+        "name": "commerceOrder",
+        "plural": false,
+        "selections": [
+          (v1/*: any*/),
+          {
+            "kind": "TypeDiscriminator",
+            "abstractKey": "__isCommerceOrder"
+          },
+          (v2/*: any*/),
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "mode",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "buyerTotalCents",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": null,
+            "kind": "LinkedField",
+            "name": "requestedFulfillment",
+            "plural": false,
+            "selections": [
+              (v1/*: any*/)
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "currencyCode",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "shippingTotalCents",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "kind": "ScalarField",
+            "name": "source",
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
+            "concreteType": "CommerceLineItemConnection",
+            "kind": "LinkedField",
+            "name": "lineItems",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "CommerceLineItemEdge",
+                "kind": "LinkedField",
+                "name": "edges",
+                "plural": true,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "concreteType": "CommerceLineItem",
+                    "kind": "LinkedField",
+                    "name": "node",
+                    "plural": false,
+                    "selections": [
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "Artwork",
+                        "kind": "LinkedField",
+                        "name": "artwork",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "slug",
+                            "storageKey": null
+                          },
+                          (v2/*: any*/),
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "processWithArtsyShippingDomestic",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "artsyShippingInternational",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "euShippingOrigin",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "shippingCountry",
+                            "storageKey": null
+                          },
+                          {
+                            "alias": null,
+                            "args": null,
+                            "kind": "ScalarField",
+                            "name": "onlyShipsDomestically",
+                            "storageKey": null
+                          },
+                          (v3/*: any*/)
+                        ],
+                        "storageKey": null
+                      },
+                      {
+                        "alias": null,
+                        "args": null,
+                        "concreteType": "CommerceShippingQuoteConnection",
+                        "kind": "LinkedField",
+                        "name": "shippingQuoteOptions",
+                        "plural": false,
+                        "selections": [
+                          {
+                            "alias": null,
+                            "args": null,
+                            "concreteType": "CommerceShippingQuoteEdge",
+                            "kind": "LinkedField",
+                            "name": "edges",
+                            "plural": true,
+                            "selections": [
+                              {
+                                "alias": null,
+                                "args": null,
+                                "concreteType": "CommerceShippingQuote",
+                                "kind": "LinkedField",
+                                "name": "node",
+                                "plural": false,
+                                "selections": [
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "name",
+                                    "storageKey": null
+                                  },
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "priceCents",
+                                    "storageKey": null
+                                  },
+                                  {
+                                    "alias": null,
+                                    "args": null,
+                                    "kind": "ScalarField",
+                                    "name": "tier",
+                                    "storageKey": null
+                                  },
+                                  (v3/*: any*/)
+                                ],
+                                "storageKey": null
+                              }
+                            ],
+                            "storageKey": null
+                          }
+                        ],
+                        "storageKey": null
+                      },
+                      (v3/*: any*/)
+                    ],
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          (v3/*: any*/)
+        ],
+        "storageKey": "commerceOrder(id:\"123\")"
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "8838cfb38f20f1485ccc19841d9d3a9b",
+    "id": null,
+    "metadata": {},
+    "name": "ExpressCheckoutUI_Test_Query",
+    "operationKind": "query",
+    "text": "query ExpressCheckoutUI_Test_Query {\n  commerceOrder(id: \"123\") {\n    __typename\n    ...ExpressCheckoutUI_order\n    id\n  }\n}\n\nfragment ExpressCheckoutUI_order on CommerceOrder {\n  __isCommerceOrder: __typename\n  internalID\n  mode\n  buyerTotalCents\n  requestedFulfillment {\n    __typename\n  }\n  currencyCode\n  shippingTotalCents\n  source\n  lineItems {\n    edges {\n      node {\n        artwork {\n          slug\n          internalID\n          processWithArtsyShippingDomestic\n          artsyShippingInternational\n          euShippingOrigin\n          shippingCountry\n          onlyShipsDomestically\n          id\n        }\n        shippingQuoteOptions {\n          edges {\n            node {\n              name\n              priceCents\n              tier\n              id\n            }\n          }\n        }\n        id\n      }\n    }\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "f3b9979c2d9dc1a0045e21a7cdfbd2f6";
+
+export default node;
