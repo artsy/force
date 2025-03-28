fa3884d900e6a0d72e0e51b9fba56c95f957fa8d
Chung-Yi Chi <chung-yi@artsymail.com>
2025-03-27 22:49:09 -0400

    test: add test

--- src/Apps/Order/Components/ExpressCheckout/__tests__/ExpressCheckoutUI.jest.tsx
+++ src/Apps/Order/Components/ExpressCheckout/__tests__/ExpressCheckoutUI.jest.tsx
@@ -21,44 +21,83 @@ jest.mock("System/Hooks/useAnalyticsContext", () => ({
 
 const mockExpressCheckoutElement = ExpressCheckoutElement as jest.Mock
 const mockResolveOnClick = jest.fn()
+const mockCreateConfirmationToken = jest.fn(() => {
+  return {
+    confirmationToken: {
+      id: "ctoken_123",
+    },
+  }
+})
 
 jest.mock("@stripe/react-stripe-js", () => {
   return {
-    useStripe: jest.fn(() => ({})),
-    useElements: jest.fn(() => ({})),
-    ExpressCheckoutElement: jest.fn(({ onClick, onCancel, onReady }) => {
-      useEffect(() => {
-        if (onReady) {
-          onReady({
-            availablePaymentMethods: { applePay: true },
-          })
-        }
-      }, [onReady])
+    useStripe: jest.fn(() => ({
+      createConfirmationToken: mockCreateConfirmationToken,
+    })),
+    useElements: jest.fn(() => ({
+      submit: jest.fn(async () => {
+        return {}
+      }),
+    })),
+    ExpressCheckoutElement: jest.fn(
+      ({ onClick, onCancel, onReady, onConfirm }) => {
+        useEffect(() => {
+          if (onReady) {
+            onReady({
+              availablePaymentMethods: { applePay: true },
+            })
+          }
+        }, [onReady])
 
-      return (
-        <div>
-          <button
-            type="button"
-            data-testid="express-checkout-button"
-            onClick={() =>
-              onClick({
-                expressPaymentType: "apple_pay",
-                resolve: mockResolveOnClick,
-              })
-            }
-          >
-            Apple Pay
-          </button>
-          <button
-            type="button"
-            data-testid="express-checkout-cancel"
-            onClick={() => onCancel()}
-          >
-            Cancel
-          </button>
-        </div>
-      )
-    }),
+        return (
+          <div>
+            <button
+              type="button"
+              data-testid="express-checkout-button"
+              onClick={() =>
+                onClick({
+                  expressPaymentType: "apple_pay",
+                  resolve: mockResolveOnClick,
+                })
+              }
+            >
+              Apple Pay
+            </button>
+            <button
+              type="button"
+              data-testid="express-checkout-confirm"
+              onClick={() =>
+                onConfirm({
+                  shippingAddress: {
+                    name: "Buyer Name",
+                    address: {
+                      line1: "401 Broadway",
+                      line2: null,
+                      city: "New York",
+                      state: "NY",
+                      postal_code: "10013",
+                      country: "US",
+                    },
+                  },
+                  billingDetails: {
+                    phone: "1234567890",
+                  },
+                })
+              }
+            >
+              Confirm
+            </button>
+            <button
+              type="button"
+              data-testid="express-checkout-cancel"
+              onClick={() => onCancel()}
+            >
+              Cancel
+            </button>
+          </div>
+        )
+      },
+    ),
   }
 })
 
@@ -96,6 +135,62 @@ describe("ExpressCheckoutUI", () => {
       buttonTheme: { applePay: "white-outline" },
       buttonHeight: 50,
     })
+  })
+
+  it("updates the order and submits it on confirm", async () => {
+    const { mockResolveLastOperation, env } = renderWithRelay({
+      Order: () => ({
+        ...orderData,
+        fulfillmentOptions: [{ type: "SHIPPING_TBD", amount: null }],
+      }),
+    })
+
+    fireEvent.click(screen.getByTestId("express-checkout-confirm"))
+
+    const { operationName, operationVariables } =
+      await mockResolveLastOperation({
+        updateOrderPayload: () => ({
+          orderOrError: {
+            __typename: "OrderMutationSuccess",
+            order: orderData,
+          },
+        }),
+      })
+    expect(operationName).toBe("useUpdateOrderMutation")
+    expect(operationVariables.input).toEqual({
+      id: "a5aaa8b0-93ff-4f2a-8bb3-9589f378d229",
+      buyerPhoneNumber: "1234567890",
+      buyerPhoneNumberCountryCode: null,
+      creditCardWalletType: "APPLE_PAY",
+      paymentMethod: "CREDIT_CARD",
+      shippingAddressLine1: "401 Broadway",
+      shippingAddressLine2: null,
+      shippingCity: "New York",
+      shippingCountry: "US",
+      shippingName: "Buyer Name",
+      shippingPostalCode: "10013",
+      shippingRegion: "NY",
+    })
+
+    await flushPromiseQueue()
+    expect(mockCreateConfirmationToken).toHaveBeenCalled()
+
+    const mutation = await mockResolveLastOperation({
+      submitOrderPayload: () => ({
+        orderOrError: {
+          __typename: "OrderMutationSuccess",
+          order: orderData,
+        },
+      }),
+    })
+    expect(mutation.operationName).toBe("useSubmitOrderMutation")
+    expect(mutation.operationVariables.input).toEqual({
+      id: "a5aaa8b0-93ff-4f2a-8bb3-9589f378d229",
+      confirmationToken: "ctoken_123",
+    })
+
+    await flushPromiseQueue()
+    expect(env.mock.getAllOperations()).toHaveLength(0)
   })
 
   it("unsets the order fulfillment details and uses the result for initial values on load", async () => {
