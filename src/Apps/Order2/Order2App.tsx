93e6780352d20a65c33b0f633ace78318d71b7dc
Oksana <oksana@artsymail.com>
2025-04-25 11:39:33 -0400

    chore: updating static code for details page to add help and update colors. (#15490)
    * chore: updating some static code for details page
    
    * more tweaks
    
    * unmerging reverted changes

--- src/Apps/Order2/Order2App.tsx
+++ src/Apps/Order2/Order2App.tsx
@@ -2,9 +2,5 @@ import { Box, breakpoints } from "@artsy/palette"
 import type * as React from "react"
 
 export const Order2App: React.FC<React.PropsWithChildren> = ({ children }) => {
-  return (
-    <>
-      <Box maxWidth={breakpoints.lg}>{children}</Box>
-    </>
-  )
+  return <Box maxWidth={breakpoints.lg}>{children}</Box>
 }
