6ba6e2f46bc78da5766e638186cf2ad6cc6f8a99
Erik <9088720+erikdstock@users.noreply.github.com>
2025-05-09 16:25:38 -0500

    chore: load order summary lines from MP (#15541)

--- src/Apps/Order2/Routes/Checkout/Components/Order2CollapsibleOrderSummary.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/Order2CollapsibleOrderSummary.tsx
@@ -1,5 +1,6 @@
 import ChevronDownIcon from "@artsy/icons/ChevronDownIcon"
 import { Box, Clickable, Flex, Image, Spacer, Text } from "@artsy/palette"
+import { Order2PricingBreakdown } from "Apps/Order2/Routes/Checkout/Components/Order2PricingBreakdown"
 import { RouterLink } from "System/Components/RouterLink"
 import type { Order2CollapsibleOrderSummary_order$key } from "__generated__/Order2CollapsibleOrderSummary_order.graphql"
 import type * as React from "react"
@@ -9,9 +10,6 @@ import { graphql, useFragment } from "react-relay"
 interface Order2CollapsibleOrderSummaryProps {
   order: Order2CollapsibleOrderSummary_order$key
 }
-
-const TAX_CALCULATION_ARTICLE =
-  "https://support.artsy.net/s/article/How-are-taxes-and-customs-fees-calculated"
 
 export const Order2CollapsibleOrderSummary: React.FC<
   Order2CollapsibleOrderSummaryProps
@@ -70,61 +68,7 @@ export const Order2CollapsibleOrderSummary: React.FC<
       >
         <Spacer y={1} />
         <Box mb={2}>
-          <Flex>
-            <Text flexGrow={1} variant="sm" color="mono60">
-              Price
-            </Text>
-            <Text flexGrow={0} variant="sm" color="mono60">
-              {orderData.itemsTotal?.display}
-            </Text>
-          </Flex>
-          <Flex>
-            <Text flexGrow={1} variant="sm" color="mono60">
-              Shipping
-            </Text>
-            <Text flexGrow={0} variant="sm" color="mono60">
-              {orderData.shippingTotal?.display || "Calculated in next steps"}
-            </Text>
-          </Flex>
-          <Flex>
-            <Text flexGrow={1} variant="sm" color="mono60">
-              Tax*
-            </Text>
-            <Text flexGrow={0} variant="sm" color="mono60">
-              {orderData.shippingTotal?.display || "Calculated in next steps"}
-            </Text>
-          </Flex>
-          <Spacer y={0.5} />
-          <Flex>
-            <Text
-              flexGrow={1}
-              variant="sm-display"
-              color="mono100"
-              fontWeight="medium"
-            >
-              Total
-            </Text>
-            <Text
-              flexGrow={0}
-              variant="sm-display"
-              color="mono100"
-              fontWeight="medium"
-            >
-              Waiting for final cost
-            </Text>
-          </Flex>
-          <Text variant="xs" color="mono60" textAlign="left" mt={2}>
-            *Additional duties and taxes{" "}
-            <RouterLink
-              inline
-              to={TAX_CALCULATION_ARTICLE}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              may apply at import
-            </RouterLink>
-            .
-          </Text>
+          <Order2PricingBreakdown order={orderData} />
         </Box>
         <Spacer y={1} />
       </Box>
@@ -134,6 +78,7 @@ export const Order2CollapsibleOrderSummary: React.FC<
 
 const FRAGMENT = graphql`
   fragment Order2CollapsibleOrderSummary_order on Order {
+    ...Order2PricingBreakdown_order
     mode
     source
     buyerTotal {
---
+++ src/Apps/Order2/Routes/Checkout/Components/__tests__/Order2PricingBreakdown.jest.tsx
@@ -0,0 +1,134 @@
+import { screen } from "@testing-library/react"
+import { setupTestWrapperTL } from "DevTools/setupTestWrapperTL"
+import type { Order2PricingBreakdownTestQuery } from "__generated__/Order2PricingBreakdownTestQuery.graphql"
+import { graphql } from "react-relay"
+import { Order2PricingBreakdown } from "../Order2PricingBreakdown"
+
+jest.unmock("react-relay")
+
+const { renderWithRelay } = setupTestWrapperTL<Order2PricingBreakdownTestQuery>(
+  {
+    Component: props => {
+      const order = props.me!.order!
+      return <Order2PricingBreakdown order={order} />
+    },
+    query: graphql`
+      query Order2PricingBreakdownTestQuery @relay_test_operation {
+        me {
+          order(id: "test-order") {
+            ...Order2PricingBreakdown_order
+          }
+        }
+      }
+    `,
+  },
+)
+
+describe("Order2PricingBreakdown", () => {
+  it("renders pricing breakdown lines with fallback text if amount is null", () => {
+    renderWithRelay({
+      Me: () => ({
+        order: {
+          pricingBreakdownLines: [
+            {
+              __typename: "SubtotalLine",
+              displayName: "Subtotal",
+              amount: { amount: 1000, currencySymbol: "$" },
+            },
+            {
+              __typename: "ShippingLine",
+              displayName: "Shipping",
+              amountFallbackText: "Calculated at checkout",
+              amount: null,
+            },
+            {
+              __typename: "TaxLine",
+              displayName: "Tax",
+              amountFallbackText: "Calculated at checkout",
+              amount: null,
+            },
+            {
+              __typename: "TotalLine",
+              displayName: "Total",
+              amountFallbackText: "Waiting for final totals",
+              amount: null,
+            },
+          ],
+        },
+      }),
+    })
+
+    // Subtotal
+    const subtotalRow = screen.getByText("Subtotal").parentElement
+    expect(subtotalRow).toHaveTextContent("$1000")
+
+    // Shipping
+    const shippingRow = screen.getByText("Shipping").parentElement
+    expect(shippingRow).toHaveTextContent("Calculated at checkout")
+
+    // Tax
+    const taxRow = screen.getByText("Tax*").parentElement
+    expect(taxRow).toHaveTextContent("Calculated at checkout")
+
+    // Total
+    const totalRow = screen.getByText("Total").parentElement
+    expect(totalRow).toHaveTextContent("Waiting for final totals")
+  })
+
+  it("renders pricing breakdown lines with prices", () => {
+    renderWithRelay({
+      Me: () => ({
+        order: {
+          pricingBreakdownLines: [
+            {
+              __typename: "SubtotalLine",
+              displayName: "Subtotal",
+              amount: {
+                amount: "1000",
+                currencySymbol: "$",
+              },
+            },
+            {
+              __typename: "ShippingLine",
+              displayName: "Shipping",
+              amount: {
+                amount: "42.99",
+                currencySymbol: "$",
+              },
+            },
+            {
+              __typename: "TaxLine",
+              displayName: "Tax",
+              amountFallbackText: null,
+
+              amount: { amount: "99.58", currencySymbol: "$" },
+            },
+            {
+              __typename: "TotalLine",
+              displayName: "Total",
+              amount: {
+                display: "$US 1052.57",
+              },
+            },
+          ],
+        },
+      }),
+    })
+
+    // Subtotal
+    const subtotalRow = screen.getByText("Subtotal").parentElement
+    expect(subtotalRow).toHaveTextContent("$1000")
+
+    // Shipping
+    const shippingRow = screen.getByText("Shipping").parentElement
+    expect(shippingRow).toHaveTextContent("$42.99")
+
+    // Tax
+    const taxRow = screen.getByText("Tax*").parentElement
+    expect(taxRow).toHaveTextContent("$99.58")
+
+    // Total
+    const totalRow = screen.getByText("Total").parentElement
+    expect(totalRow).toHaveTextContent("$US 1052.57")
+  })
+})
