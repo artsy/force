3b3c9875ed97c53447fb99c6105c77effa18bd83
oxaudo <oksana@artsymail.com>
2025-05-20 09:37:04 -0400

    adding spec

---
+++ src/__generated__/Order2DetailsMessage_Test_Query.graphql.ts
@@ -0,0 +1,179 @@
+/**
+ * @generated SignedSource<<5b3a620d3a9122c22edd96794ff87218>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ConcreteRequest } from 'relay-runtime';
+import { FragmentRefs } from "relay-runtime";
+export type DisplayTextsMessageTypeEnum = "APPROVED_PICKUP" | "APPROVED_SHIP" | "APPROVED_SHIP_EXPRESS" | "APPROVED_SHIP_STANDARD" | "APPROVED_SHIP_WHITE_GLOVE" | "CANCELLED_ORDER" | "COMPLETED_PICKUP" | "COMPLETED_SHIP" | "PAYMENT_FAILED" | "PROCESSING_PAYMENT_PICKUP" | "PROCESSING_PAYMENT_SHIP" | "PROCESSING_WIRE" | "SHIPPED" | "SUBMITTED_OFFER" | "SUBMITTED_ORDER" | "UNKNOWN" | "%future added value";
+export type Order2DetailsMessage_Test_Query$variables = Record<PropertyKey, never>;
+export type Order2DetailsMessage_Test_Query$data = {
+  readonly me: {
+    readonly order: {
+      readonly " $fragmentSpreads": FragmentRefs<"Order2DetailsMessage_order">;
+    } | null | undefined;
+  } | null | undefined;
+};
+export type Order2DetailsMessage_Test_Query$rawResponse = {
+  readonly me: {
+    readonly id: string;
+    readonly order: {
+      readonly buyerStateExpiresAt: string | null | undefined;
+      readonly code: string;
+      readonly displayTexts: {
+        readonly messageType: DisplayTextsMessageTypeEnum;
+      };
+      readonly id: string;
+      readonly internalID: string;
+    } | null | undefined;
+  } | null | undefined;
+};
+export type Order2DetailsMessage_Test_Query = {
+  rawResponse: Order2DetailsMessage_Test_Query$rawResponse;
+  response: Order2DetailsMessage_Test_Query$data;
+  variables: Order2DetailsMessage_Test_Query$variables;
+};
+
+const node: ConcreteRequest = (function(){
+var v0 = [
+  {
+    "kind": "Literal",
+    "name": "id",
+    "value": "123"
+  }
+],
+v1 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "id",
+  "storageKey": null
+};
+return {
+  "fragment": {
+    "argumentDefinitions": [],
+    "kind": "Fragment",
+    "metadata": null,
+    "name": "Order2DetailsMessage_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Me",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": (v0/*: any*/),
+            "concreteType": "Order",
+            "kind": "LinkedField",
+            "name": "order",
+            "plural": false,
+            "selections": [
+              {
+                "args": null,
+                "kind": "FragmentSpread",
+                "name": "Order2DetailsMessage_order"
+              }
+            ],
+            "storageKey": "order(id:\"123\")"
+          }
+        ],
+        "storageKey": null
+      }
+    ],
+    "type": "Query",
+    "abstractKey": null
+  },
+  "kind": "Request",
+  "operation": {
+    "argumentDefinitions": [],
+    "kind": "Operation",
+    "name": "Order2DetailsMessage_Test_Query",
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "concreteType": "Me",
+        "kind": "LinkedField",
+        "name": "me",
+        "plural": false,
+        "selections": [
+          {
+            "alias": null,
+            "args": (v0/*: any*/),
+            "concreteType": "Order",
+            "kind": "LinkedField",
+            "name": "order",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "buyerStateExpiresAt",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "code",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "internalID",
+                "storageKey": null
+              },
+              {
+                "alias": null,
+                "args": null,
+                "concreteType": "DisplayTexts",
+                "kind": "LinkedField",
+                "name": "displayTexts",
+                "plural": false,
+                "selections": [
+                  {
+                    "alias": null,
+                    "args": null,
+                    "kind": "ScalarField",
+                    "name": "messageType",
+                    "storageKey": null
+                  }
+                ],
+                "storageKey": null
+              },
+              (v1/*: any*/)
+            ],
+            "storageKey": "order(id:\"123\")"
+          },
+          (v1/*: any*/)
+        ],
+        "storageKey": null
+      }
+    ]
+  },
+  "params": {
+    "cacheID": "026726dfaec107b3bf6992676e78181e",
+    "id": null,
+    "metadata": {},
+    "name": "Order2DetailsMessage_Test_Query",
+    "operationKind": "query",
+    "text": "query Order2DetailsMessage_Test_Query {\n  me {\n    order(id: \"123\") {\n      ...Order2DetailsMessage_order\n      id\n    }\n    id\n  }\n}\n\nfragment Order2DetailsMessage_order on Order {\n  buyerStateExpiresAt\n  code\n  internalID\n  displayTexts {\n    messageType\n  }\n}\n"
+  }
+};
+})();
+
+(node as any).hash = "de1d599ef2e4b64a3e9b78a1d42fdaa8";
+
+export default node;
