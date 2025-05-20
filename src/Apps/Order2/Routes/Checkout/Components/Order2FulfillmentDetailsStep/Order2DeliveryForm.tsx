7f8f504d2006737f7e10e90680813d2a248a3512
Erik <9088720+erikdstock@users.noreply.github.com>
2025-05-20 15:21:08 -0500

    remove save address checkbox from no-saved-address form

--- src/Apps/Order2/Routes/Checkout/Components/Order2FulfillmentDetailsStep/Order2DeliveryForm.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/Order2FulfillmentDetailsStep/Order2DeliveryForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Flex, Spacer, Text } from "@artsy/palette"
+import { Button, Flex, Spacer, Text } from "@artsy/palette"
 import {
   AddressFormFields,
   addressFormFieldsValidator,
@@ -11,10 +11,9 @@ interface Order2DeliveryFormProps {
   order: any
 }
 
-const validationSchema = yup.object().shape({
-  ...addressFormFieldsValidator({ withPhoneNumber: true }),
-  saveAddress: yup.boolean(),
-})
+const validationSchema = yup
+  .object()
+  .shape(addressFormFieldsValidator({ withPhoneNumber: true }))
 
 export const Order2DeliveryForm: React.FC<Order2DeliveryFormProps> = () => {
   // Placeholders
@@ -49,15 +48,6 @@ export const Order2DeliveryForm: React.FC<Order2DeliveryFormProps> = () => {
         {formikContext => (
           <Flex flexDirection={"column"} mb={2}>
             <AddressFormFields withPhoneNumber />
-            <Spacer y={2} />
-            <Checkbox
-              onSelect={selected => {
-                formikContext.setFieldValue("saveAddress", selected)
-              }}
-              selected={formikContext.values.saveAddress}
-            >
-              <Text variant="xs">Save shipping address for later use</Text>
-            </Checkbox>
             <Spacer y={4} />
             <Button type="submit" onClick={() => formikContext.handleSubmit()}>
               See Shipping Methods
