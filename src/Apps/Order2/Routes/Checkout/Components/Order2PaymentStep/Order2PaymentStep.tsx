5bda31cd3b2975b413f7cfed9f8c3fa215a013a3
Fladson Gomes <fladsonthiago@gmail.com>
2025-05-22 15:14:44 +0200

    chore: remove not needed code

--- src/Apps/Order2/Routes/Checkout/Components/Order2PaymentStep/Order2PaymentStep.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/Order2PaymentStep/Order2PaymentStep.tsx
@@ -1,11 +1,7 @@
-import CheckmarkIcon from "@artsy/icons/CheckmarkIcon"
-import { Box, Clickable, Flex, Spacer, Text } from "@artsy/palette"
+import { Box, Flex, Spacer, Text } from "@artsy/palette"
 import { Order2PaymentForm } from "Apps/Order2/Routes/Checkout/Components/Order2PaymentStep/Order2PaymentForm"
-import { Brand, BrandCreditCardIcon } from "Components/BrandCreditCardIcon"
 import type { Order2PaymentStep_order$key } from "__generated__/Order2PaymentStep_order.graphql"
-import { SystemQueryRenderer } from "System/Relay/SystemQueryRenderer"
 import { graphql, useFragment } from "react-relay"
-import { Order2PaymentStepConfirmationTokenQuery } from "__generated__/Order2PaymentStepConfirmationTokenQuery.graphql"
 import { useCheckoutContext } from "Apps/Order2/Routes/Checkout/Hooks/useCheckoutContext"
 import {
   CheckoutStepName,
@@ -67,81 +63,6 @@ export const Order2PaymentStep: React.FC<Order2PaymentStepProps> = ({
         </Flex>
       </Box>
     </Flex>
-  )
-}
-
-interface MeConfirmationTokenQueryRendererProps {
-  tokenID: string
-  setConfirmationTokenID: (id: string | null) => void
-}
-
-export const MeConfirmationTokenQueryRenderer: React.FC<
-  MeConfirmationTokenQueryRendererProps
-> = ({ tokenID, setConfirmationTokenID }) => {
-  return (
-    <SystemQueryRenderer<Order2PaymentStepConfirmationTokenQuery>
-      query={graphql`
-        query Order2PaymentStepConfirmationTokenQuery($tokenID: String!) {
-          me {
-            confirmationToken(id: $tokenID) {
-              paymentMethodPreview {
-                card {
-                  displayBrand
-                  last4
-                }
-              }
-            }
-          }
-        }
-      `}
-      variables={{ tokenID }}
-      render={({ props }) => {
-        if (
-          props?.me?.confirmationToken?.paymentMethodPreview?.card?.displayBrand
-        ) {
-          return (
-            <Flex flexDirection="column" backgroundColor="mono0" p={2}>
-              <Flex justifyContent="space-between">
-                <Flex alignItems="center">
-                  <CheckmarkIcon fill="mono100" />
-                  <Spacer x={1} />
-                  <Text variant="sm-display" fontWeight={500} color="mono100">
-                    Payment
-                  </Text>
-                </Flex>
-                <Clickable
-                  textDecoration="underline"
-                  cursor="pointer"
-                  type="button"
-                  onClick={() => setConfirmationTokenID(null)}
-                >
-                  Edit
-                </Clickable>
-              </Flex>
-              <Flex alignItems="center" ml="30px" mt={1}>
-                <BrandCreditCardIcon
-                  mr={1}
-                  type={
-                    props?.me?.confirmationToken?.paymentMethodPreview?.card
-                      ?.displayBrand as Brand
-                  }
-                  width="26px"
-                  height="26px"
-                />
-                <Text variant="sm-display">
-                  ••••{" "}
-                  {
-                    props?.me?.confirmationToken?.paymentMethodPreview?.card
-                      ?.last4
-                  }
-                </Text>
-              </Flex>
-            </Flex>
-          )
-        }
-        return null
-      }}
-    />
   )
 }
 
