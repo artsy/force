dbdd238d3aaec59e33e0a623026f585915ba0308
oxaudo <oksana@artsymail.com>
2025-05-30 12:02:18 -0400

    update buyer protection to buyer guarantee text

--- src/Apps/Order/Routes/Shipping/Components/ShippingQuotes.tsx
+++ src/Apps/Order/Routes/Shipping/Components/ShippingQuotes.tsx
@@ -74,12 +74,12 @@ export const ShippingQuotes: React.FC<
         {orderData.isOffer ? (
           <>
             Please note that these are estimates and may change once offer is
-            finalized. All options are eligible for Artsy’s Buyer Protection
+            finalized. All options are eligible for Artsy’s Buyer Guarantee
             policy, which protects against damage and loss.
           </>
         ) : (
           <>
-            All options are eligible for Artsy’s Buyer Protection policy, which
+            All options are eligible for Artsy’s Buyer Guarantee policy, which
             protects against damage and loss.
           </>
         )}
