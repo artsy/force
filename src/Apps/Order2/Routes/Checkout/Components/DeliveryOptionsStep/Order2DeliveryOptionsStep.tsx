849e2278573d99bd5f941a92c171c757a3c25970
Fladson Gomes <fladsonthiago@gmail.com>
2025-06-10 15:40:42 +0200

    chore: address review comments

--- src/Apps/Order2/Routes/Checkout/Components/DeliveryOptionsStep/Order2DeliveryOptionsStep.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/DeliveryOptionsStep/Order2DeliveryOptionsStep.tsx
@@ -10,28 +10,28 @@ interface Order2DeliveryOptionsStepProps {}
 export const Order2DeliveryOptionsStep: React.FC<
   Order2DeliveryOptionsStepProps
 > = () => {
-  const { steps } = useCheckoutContext()!
+  const { steps } = useCheckoutContext()
 
   const stepState = steps?.find(
     step => step.name === CheckoutStepName.DELIVERY_OPTION,
   )?.state
 
   return (
-    <Flex flexDirection="column" backgroundColor="mono0">
-      <Box
-        p={2}
-        hidden={stepState !== CheckoutStepState.UPCOMING}
-        data-testid="DeliveryOptionsStep"
-      >
-        <Flex flexDirection="column">
-          <Text variant="sm-display" fontWeight="bold" color="mono100">
-            Shipping method
-          </Text>
-          <Text variant={["xs", "xs", "sm"]} color="mono60">
-            Options vary based on address and artwork size
-          </Text>
+    <>
+      {stepState === CheckoutStepState.UPCOMING && (
+        <Flex flexDirection="column" backgroundColor="mono0">
+          <Box p={2} data-testid="DeliveryOptionsStep">
+            <Flex flexDirection="column">
+              <Text variant="sm-display" fontWeight="bold" color="mono100">
+                Shipping method
+              </Text>
+              <Text variant={["xs", "xs", "sm"]} color="mono60">
+                Options vary based on address and artwork size
+              </Text>
+            </Flex>
+          </Box>
         </Flex>
-      </Box>
-    </Flex>
+      )}
+    </>
   )
 }
