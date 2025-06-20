1f10c9b54d91e91552beb7b0b1878f3039acdbaa
dzucconi <mail@damonzucconi.com>
2025-06-20 09:51:21 -0400

    feat(about2): implements scroll nav highlighting

---
+++ src/Apps/About2/Components/AboutMissionAndVision.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@artsy/palette"
+import { AboutSection } from "Apps/About2/Components/AboutSection"
+
+export const AboutMissionAndVision = () => {
+  return (
+    <AboutSection id="mission-and-vision">
+      <Text variant="lg" height={600} bg="mono10">
+        Mission and vision
+      </Text>
+    </AboutSection>
+  )
+}
