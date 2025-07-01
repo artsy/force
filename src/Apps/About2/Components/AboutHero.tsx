50055fa8b5edfbaafbe8a987a10ba037fab860b2
dzucconi <mail@damonzucconi.com>
2025-07-01 07:57:52 -0400

    feat(about2): adds hero images

--- src/Apps/About2/Components/AboutHero.tsx
+++ src/Apps/About2/Components/AboutHero.tsx
@@ -1,4 +1,5 @@
-import { Column, GridColumns, ResponsiveBox } from "@artsy/palette"
+import { Column, GridColumns, Image, ResponsiveBox } from "@artsy/palette"
+import { resized } from "Utils/resized"
 
 export const AboutHero = () => {
   return (
@@ -6,28 +7,59 @@ export const AboutHero = () => {
       <Column span={[6, 4]}>
         <ResponsiveBox
           bg="mono10"
-          aspectWidth={4}
-          aspectHeight={5}
+          aspectWidth={1280}
+          aspectHeight={1500}
           maxWidth="100%"
-        />
+        >
+          <Image
+            {...resized("https://files.artsy.net/images/about2-header-1.jpg", {
+              width: 500,
+            })}
+            width="100%"
+            height="100%"
+            alt=""
+          />
+        </ResponsiveBox>
       </Column>
 
       <Column span={[6, 4]} pt={[40, 100]}>
         <ResponsiveBox
           bg="mono15"
-          aspectWidth={4}
-          aspectHeight={5}
+          aspectWidth={1280}
+          aspectHeight={1500}
           maxWidth="100%"
-        />
+        >
+          <Image
+            {...resized(
+              "https://files.artsy.net/images/about2-header-2-2.jpg",
+              {
+                width: 500,
+              },
+            )}
+            width="100%"
+            height="100%"
+            alt=""
+          />
+        </ResponsiveBox>
       </Column>
 
       <Column span={[6, 4]} display={["none", "block"]}>
         <ResponsiveBox
           bg="mono10"
-          aspectWidth={4}
-          aspectHeight={5}
+          aspectWidth={1280}
+          aspectHeight={1500}
           maxWidth="100%"
-        />
+        >
+          <Image
+            {...resized(
+              "https://files.artsy.net/images/about2-header-3-2.jpg",
+              { width: 500 },
+            )}
+            width="100%"
+            height="100%"
+            alt=""
+          />
+        </ResponsiveBox>
       </Column>
     </GridColumns>
   )
