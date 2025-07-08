71e23a9a1014e0f22c81064599bef28c7898de21
dzucconi <mail@damonzucconi.com>
2025-07-08 06:43:23 -0400

    fix(about2): adjusts stats spacing

--- src/Apps/About2/Components/AboutStats.tsx
+++ src/Apps/About2/Components/AboutStats.tsx
@@ -2,13 +2,13 @@ import { Column, GridColumns, Text } from "@artsy/palette"
 
 export const AboutStats = () => {
   return (
-    <GridColumns textAlign={["left", "center"]}>
+    <GridColumns textAlign={["left", "center"]} gridRowGap={[4, 2]}>
       <Column
         span={[12, 3]}
         start={[1, 2]}
         display="flex"
         flexDirection="column"
-        gap={2}
+        gap={[1, 2]}
       >
         <Text variant={["xxl", "xxxl"]}>94K</Text>
         <Text variant="sm-display" color="mono60">
@@ -16,14 +16,14 @@ export const AboutStats = () => {
         </Text>
       </Column>
 
-      <Column span={[12, 4]} display="flex" flexDirection="column" gap={2}>
+      <Column span={[12, 4]} display="flex" flexDirection="column" gap={[1, 2]}>
         <Text variant={["xxl", "xxxl"]}>1M+</Text>
         <Text variant="sm-display" color="mono60">
           Artworks available
         </Text>
       </Column>
 
-      <Column span={[12, 3]} display="flex" flexDirection="column" gap={2}>
+      <Column span={[12, 3]} display="flex" flexDirection="column" gap={[1, 2]}>
         <Text variant={["xxl", "xxxl"]}>3K</Text>
         <Text variant="sm-display" color="mono60">
           Partner galleries and auction houses
