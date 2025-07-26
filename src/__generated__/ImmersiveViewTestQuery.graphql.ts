0fce85e9d4c239964f8fe79999e29acc8a853cb8
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2025-07-25 22:48:47 -0400

    feat: add automatic prev/next page navigation

--- src/__generated__/ImmersiveViewTestQuery.graphql.ts
+++ src/__generated__/ImmersiveViewTestQuery.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<0ed739a2e83eedac4be2347a747ed06e>>
+ * @generated SignedSource<<73bf14f0512546e8cdf7cacff776185a>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -85,6 +85,24 @@ return {
           {
             "alias": null,
             "args": null,
+            "concreteType": "PageInfo",
+            "kind": "LinkedField",
+            "name": "pageInfo",
+            "plural": false,
+            "selections": [
+              {
+                "alias": null,
+                "args": null,
+                "kind": "ScalarField",
+                "name": "hasNextPage",
+                "storageKey": null
+              }
+            ],
+            "storageKey": null
+          },
+          {
+            "alias": null,
+            "args": null,
             "concreteType": "FilterArtworksEdge",
             "kind": "LinkedField",
             "name": "edges",
@@ -153,7 +171,7 @@ return {
     ]
   },
   "params": {
-    "cacheID": "d2895c3d84f82540c76cce1b39989934",
+    "cacheID": "b7e14444c6005a957afc705afe01b1ae",
     "id": null,
     "metadata": {
       "relayTestingSelectionTypeInfo": {
@@ -185,12 +203,24 @@ return {
         },
         "filtered_artworks.edges.immersiveArtworkNode.image.url": (v1/*: any*/),
         "filtered_artworks.edges.immersiveArtworkNode.slug": (v2/*: any*/),
-        "filtered_artworks.id": (v2/*: any*/)
+        "filtered_artworks.id": (v2/*: any*/),
+        "filtered_artworks.pageInfo": {
+          "enumValues": null,
+          "nullable": false,
+          "plural": false,
+          "type": "PageInfo"
+        },
+        "filtered_artworks.pageInfo.hasNextPage": {
+          "enumValues": null,
+          "nullable": false,
+          "plural": false,
+          "type": "Boolean"
+        }
       }
     },
     "name": "ImmersiveViewTestQuery",
     "operationKind": "query",
-    "text": "query ImmersiveViewTestQuery {\n  filtered_artworks: artworksConnection {\n    ...ImmersiveView_filtered_artworks\n    id\n  }\n}\n\nfragment ImmersiveView_filtered_artworks on FilterArtworksConnection {\n  edges {\n    immersiveArtworkNode: node {\n      slug\n      formattedMetadata\n      image {\n        url(version: [\"larger\", \"large\"])\n      }\n      id\n    }\n  }\n}\n"
+    "text": "query ImmersiveViewTestQuery {\n  filtered_artworks: artworksConnection {\n    ...ImmersiveView_filtered_artworks\n    id\n  }\n}\n\nfragment ImmersiveView_filtered_artworks on FilterArtworksConnection {\n  pageInfo {\n    hasNextPage\n  }\n  edges {\n    immersiveArtworkNode: node {\n      slug\n      formattedMetadata\n      image {\n        url(version: [\"larger\", \"large\"])\n      }\n      id\n    }\n  }\n}\n"
   }
 };
 })();
