b0ea5c43aa6efad45b86469d1f9ecaf8319b3d0d
araujobarret <araujobarret@gmail.com>
2025-07-30 12:01:04 +0200

    feat: add playwright

---
+++ playwright.config.ts
@@ -0,0 +1,43 @@
+import { defineConfig, devices } from "@playwright/test"
+
+export default defineConfig({
+  testDir: "./playwright/e2e",
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 5 : 0,
+  workers: process.env.CI ? 1 : undefined,
+  reporter: "html",
+  use: {
+    baseURL: "http://localhost:4000",
+    trace: "on-first-retry",
+  },
+
+  expect: {
+    timeout: 10000, // 10 seconds for assertions
+  },
+
+  // Start local dev server before running tests
+  webServer: {
+    command: "yarn start",
+    port: 4000,
+    reuseExistingServer: !process.env.CI,
+    timeout: 120000, // 2 minutes to start the server
+  },
+
+  projects: [
+    {
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
+
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+  ],
+})
