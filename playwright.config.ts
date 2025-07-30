8f1fa3cdc7d15d05bafbf55912b3e9e65dc0248b
araujobarret <araujobarret@gmail.com>
2025-07-30 13:33:49 +0200

    feat: use orb for playwright, execute only in one brower

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,20 +24,27 @@ export default defineConfig({
     timeout: 120000, // 2 minutes to start the server
   },
 
-  projects: [
-    {
-      name: "chromium",
-      use: { ...devices["Desktop Chrome"] },
-    },
+  projects: process.env.CI
+    ? [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
+      ]
+    : [
+        {
+          name: "chromium",
+          use: { ...devices["Desktop Chrome"] },
+        },
 
-    {
-      name: "firefox",
-      use: { ...devices["Desktop Firefox"] },
-    },
+        {
+          name: "firefox",
+          use: { ...devices["Desktop Firefox"] },
+        },
 
-    {
-      name: "webkit",
-      use: { ...devices["Desktop Safari"] },
-    },
-  ],
+        {
+          name: "webkit",
+          use: { ...devices["Desktop Safari"] },
+        },
+      ],
 })
