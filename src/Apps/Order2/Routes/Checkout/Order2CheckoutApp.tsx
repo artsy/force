4373f4d6d60b883aae002e4abe3c328f9f65db8e
Christian Tatu <chr.tatu@gmail.com>
2025-08-13 16:42:58 +0200

    fix: fixing checkout for loading and express checkout submitting state

--- src/Apps/Order2/Routes/Checkout/Order2CheckoutApp.tsx
+++ src/Apps/Order2/Routes/Checkout/Order2CheckoutApp.tsx
@@ -103,31 +103,37 @@ export const Order2CheckoutApp: React.FC<Order2CheckoutAppProps> = ({
             : "width=device-width, initial-scale=1, maximum-scale=5 viewport-fit=cover"
         }
       />
-      {isLoading && <Order2CheckoutLoadingSkeleton order={orderData} />}
-      {expressCheckoutSubmitting && <SubmittingOrderSpinner />}
-      {!expressCheckoutSubmitting && (
+      {isLoading ? (
+        <Order2CheckoutLoadingSkeleton order={orderData} />
+      ) : (
         <GridColumns py={[0, 4]} px={[0, 4]}>
           <Column span={[12, 7, 6, 5]} start={[1, 1, 2, 3]}>
-            <Stack gap={1}>
-              <Box display={["block", "none"]}>
-                <Order2CollapsibleOrderSummary order={orderData} />
-              </Box>
-              {isExpressCheckoutEligible && (
-                <Order2ExpressCheckout order={orderData} />
-              )}
-              <Order2FulfillmentDetailsStep order={orderData} />
-              <Order2DeliveryOptionsStep order={orderData} />
-              <Order2PaymentStep order={orderData} me={meData} />
-            </Stack>
-            <Box display={["block", "none"]}>
-              <Spacer y={1} />
-              <Order2ReviewStep order={orderData} />
-              <Order2HelpLinksWithInquiry
-                order={orderData}
-                artworkID={artworkSlug as string}
-                contextModule={ContextModule.ordersCheckout}
-              />
-            </Box>
+            {expressCheckoutSubmitting ? (
+              <SubmittingOrderSpinner />
+            ) : (
+              <>
+                <Stack gap={1}>
+                  <Box display={["block", "none"]}>
+                    <Order2CollapsibleOrderSummary order={orderData} />
+                  </Box>
+                  {isExpressCheckoutEligible && (
+                    <Order2ExpressCheckout order={orderData} />
+                  )}
+                  <Order2FulfillmentDetailsStep order={orderData} />
+                  <Order2DeliveryOptionsStep order={orderData} />
+                  <Order2PaymentStep order={orderData} me={meData} />
+                </Stack>
+                <Box display={["block", "none"]}>
+                  <Spacer y={1} />
+                  <Order2ReviewStep order={orderData} />
+                  <Order2HelpLinksWithInquiry
+                    order={orderData}
+                    artworkID={artworkSlug as string}
+                    contextModule={ContextModule.ordersCheckout}
+                  />
+                </Box>
+              </>
+            )}
           </Column>
 
           <Column
