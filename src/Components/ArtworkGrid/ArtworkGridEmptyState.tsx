a0b0180d3453a949d678a5afd5af5e994c589732
dzucconi <mail@damonzucconi.com>
2025-10-02 09:04:48 -0400

    fix(filters): removes sticky empty state leftover from sidebar

--- src/Components/ArtworkGrid/ArtworkGridEmptyState.tsx
+++ src/Components/ArtworkGrid/ArtworkGridEmptyState.tsx
@@ -1,5 +1,4 @@
 import { Box, type BoxProps, Clickable, Message } from "@artsy/palette"
-import { Sticky } from "Components/Sticky"
 import type * as React from "react"
 import styled from "styled-components"
 
@@ -11,30 +10,19 @@ export const ArtworkGridEmptyState: React.FC<
   React.PropsWithChildren<ArtworkGridEmptyStateProps>
 > = ({ onClearFilters, ...rest }) => (
   <Box width="100%" {...rest}>
-    <Sticky>
-      {({ stuck }) => {
-        return (
-          <Box pt={stuck ? 1 : 0}>
-            <Message width="100%">
-              There aren‘t any works available that meet the following criteria
-              at this time.{" "}
-              {onClearFilters && (
-                <>
-                  Change your filter criteria to view more works.{" "}
-                  <ResetFilterLink
-                    textDecoration="underline"
-                    onClick={onClearFilters}
-                  >
-                    Clear all filters
-                  </ResetFilterLink>
-                  .
-                </>
-              )}
-            </Message>
-          </Box>
-        )
-      }}
-    </Sticky>
+    <Message width="100%">
+      There aren‘t any works available that meet the following criteria at this
+      time.{" "}
+      {onClearFilters && (
+        <>
+          Change your filter criteria to view more works.{" "}
+          <ResetFilterLink textDecoration="underline" onClick={onClearFilters}>
+            Clear all filters
+          </ResetFilterLink>
+          .
+        </>
+      )}
+    </Message>
   </Box>
 )
 
