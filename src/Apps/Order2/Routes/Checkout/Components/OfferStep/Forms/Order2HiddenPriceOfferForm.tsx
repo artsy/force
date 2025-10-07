36d90139aa8b989fbee80838e6b8c19f022a8672
Rachel Quartararo <rquartararo@gmail.com>
2025-10-07 18:18:13 -0400

    styling

--- src/Apps/Order2/Routes/Checkout/Components/OfferStep/Forms/Order2HiddenPriceOfferForm.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/OfferStep/Forms/Order2HiddenPriceOfferForm.tsx
@@ -17,7 +17,7 @@ export const Order2HiddenPriceOfferForm: React.FC<
   onContinueButtonPressed,
 }) => {
   return (
-    <>
+    <Box pb={2} px={[2, 4]}>
       {/* Offer Amount Section */}
 
       <Flex flexDirection="column">
@@ -31,41 +31,38 @@ export const Order2HiddenPriceOfferForm: React.FC<
       </Flex>
 
       <Spacer y={2} />
-
       {/* Offer Note Section */}
-      <Box py={2} px={[2, 4]}>
-        <Flex flexDirection="column">
-          <Text
-            variant={["sm-display", "md"]}
-            fontWeight={["bold", "normal"]}
-            color="mono100"
-          >
-            Offer note
-          </Text>
-          <Text variant={["xs", "xs", "sm"]} color="mono100">
-            Additional context to help the gallery evaluate your offer.
-          </Text>
+      <Flex flexDirection="column">
+        <Text
+          variant={["sm-display", "md"]}
+          fontWeight={["bold", "normal"]}
+          color="mono100"
+        >
+          Offer note
+        </Text>
+        <Text variant={["xs", "xs", "sm"]} color="mono100">
+          Additional context to help the gallery evaluate your offer.
+        </Text>
 
-          <TextArea
-            title="Note (recommended)"
-            maxLength={1000}
-            placeholder="Share what draws you to this work or artist, or add any context about your offer"
-            onChange={onOfferNoteChange}
-            value={offerNoteValue.value}
-          />
+        <TextArea
+          title="Note (recommended)"
+          maxLength={1000}
+          placeholder="Share what draws you to this work or artist, or add any context about your offer"
+          onChange={onOfferNoteChange}
+          value={offerNoteValue.value}
+        />
 
-          <Spacer y={4} />
-          <Button
-            variant="primaryBlack"
-            width="100%"
-            onClick={onContinueButtonPressed}
-            loading={isSubmittingOffer}
-            disabled={isSubmittingOffer}
-          >
-            Save and Continue
-          </Button>
-        </Flex>
-      </Box>
-    </>
+        <Spacer y={4} />
+        <Button
+          variant="primaryBlack"
+          width="100%"
+          onClick={onContinueButtonPressed}
+          loading={isSubmittingOffer}
+          disabled={isSubmittingOffer}
+        >
+          Save and Continue
+        </Button>
+      </Flex>
+    </Box>
   )
 }
