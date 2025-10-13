6b06327dac7ebc9dd6879e8cac53139d0ac54fb7
Oksana <oksana@artsymail.com>
2025-10-13 14:47:53 -0400

    feat: add new settings/orders route that will operate on Order type (behind feature flag) (#16202)
    * feat: add new settings/orders route that will operate on Order type
    
    * adding more todos
    
    * use createdAt to display proper date on settings/orders

---
+++ src/Apps/Settings/Routes/Orders/SettingsOrdersRoute.tsx
@@ -0,0 +1,25 @@
+import type { SettingsOrdersRoute_me$data } from "__generated__/SettingsOrdersRoute_me.graphql"
+import type React from "react"
+import { createFragmentContainer, graphql } from "react-relay"
+import { SettingsOrdersFragmentContainer } from "./Components/SettingsOrders"
+
+interface SettingsOrdersRouteProps {
+  me: SettingsOrdersRoute_me$data
+}
+
+const SettingsOrdersRoute: React.FC<
+  React.PropsWithChildren<SettingsOrdersRouteProps>
+> = ({ me }) => {
+  return <SettingsOrdersFragmentContainer me={me} />
+}
+
+export const SettingsOrdersRouteFragmentContainer = createFragmentContainer(
+  SettingsOrdersRoute,
+  {
+    me: graphql`
+      fragment SettingsOrdersRoute_me on Me {
+        ...SettingsOrders_me
+      }
+    `,
+  },
+)
---
+++ src/__generated__/SettingsOrdersRow_order.graphql.ts
@@ -0,0 +1,397 @@
+/**
+ * @generated SignedSource<<0977de3b705ba061d373c6e0fb87aa1e>>
+ * @lightSyntaxTransform
+ * @nogrep
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+// @ts-nocheck
+
+import { ReaderFragment } from 'relay-runtime';
+export type OrderBuyerStateEnum = "APPROVED" | "CANCELLED" | "COMPLETED" | "DECLINED_BY_BUYER" | "DECLINED_BY_SELLER" | "INCOMPLETE" | "PAYMENT_FAILED" | "PROCESSING_OFFLINE_PAYMENT" | "PROCESSING_PAYMENT" | "REFUNDED" | "SHIPPED" | "SUBMITTED" | "UNKNOWN" | "%future added value";
+export type OrderCreditCardWalletTypeEnum = "APPLE_PAY" | "GOOGLE_PAY" | "%future added value";
+export type OrderSourceEnum = "ARTWORK_PAGE" | "INQUIRY" | "PARTNER_OFFER" | "PRIVATE_SALE" | "%future added value";
+import { FragmentRefs } from "relay-runtime";
+export type SettingsOrdersRow_order$data = {
+  readonly buyerState: OrderBuyerStateEnum | null | undefined;
+  readonly buyerTotal: {
+    readonly display: string | null | undefined;
+  } | null | undefined;
+  readonly code: string;
+  readonly createdAt: string | null | undefined;
+  readonly creditCardWalletType: OrderCreditCardWalletTypeEnum | null | undefined;
+  readonly currencyCode: string;
+  readonly internalID: string;
+  readonly lineItems: ReadonlyArray<{
+    readonly artwork: {
+      readonly artistNames: string | null | undefined;
+      readonly artists: ReadonlyArray<{
+        readonly href: string | null | undefined;
+      } | null | undefined> | null | undefined;
+      readonly href: string | null | undefined;
+      readonly partner: {
+        readonly href: string | null | undefined;
+        readonly initials: string | null | undefined;
+        readonly name: string | null | undefined;
+        readonly profile: {
+          readonly icon: {
+            readonly cropped: {
+              readonly src: string;
+              readonly srcSet: string;
+            } | null | undefined;
+          } | null | undefined;
+        } | null | undefined;
+      } | null | undefined;
+      readonly shippingOrigin: string | null | undefined;
+      readonly title: string | null | undefined;
+    } | null | undefined;
+    readonly artworkVersion: {
+      readonly image: {
+        readonly cropped: {
+          readonly src: string;
+          readonly srcSet: string;
+        } | null | undefined;
+      } | null | undefined;
+    } | null | undefined;
+  } | null | undefined>;
+  readonly paymentMethodDetails: {
+    readonly __typename: "BankAccount";
+    readonly last4: string;
+  } | {
+    readonly __typename: "CreditCard";
+    readonly lastDigits: string;
+  } | {
+    readonly __typename: "WireTransfer";
+    readonly isManualPayment: boolean;
+  } | {
+    // This will never be '%other', but we need some
+    // value in case none of the concrete values match.
+    readonly __typename: "%other";
+  } | null | undefined;
+  readonly source: OrderSourceEnum;
+  readonly " $fragmentType": "SettingsOrdersRow_order";
+};
+export type SettingsOrdersRow_order$key = {
+  readonly " $data"?: SettingsOrdersRow_order$data;
+  readonly " $fragmentSpreads": FragmentRefs<"SettingsOrdersRow_order">;
+};
+
+const node: ReaderFragment = (function(){
+var v0 = {
+  "alias": null,
+  "args": null,
+  "kind": "ScalarField",
+  "name": "href",
+  "storageKey": null
+},
+v1 = [
+  {
+    "alias": null,
+    "args": [
+      {
+        "kind": "Literal",
+        "name": "height",
+        "value": 45
+      },
+      {
+        "kind": "Literal",
+        "name": "width",
+        "value": 45
+      }
+    ],
+    "concreteType": "CroppedImageUrl",
+    "kind": "LinkedField",
+    "name": "cropped",
+    "plural": false,
+    "selections": [
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "src",
+        "storageKey": null
+      },
+      {
+        "alias": null,
+        "args": null,
+        "kind": "ScalarField",
+        "name": "srcSet",
+        "storageKey": null
+      }
+    ],
+    "storageKey": "cropped(height:45,width:45)"
+  }
+];
+return {
+  "argumentDefinitions": [],
+  "kind": "Fragment",
+  "metadata": null,
+  "name": "SettingsOrdersRow_order",
+  "selections": [
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "source",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "internalID",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "code",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "buyerState",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "createdAt",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "creditCardWalletType",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": null,
+      "kind": "LinkedField",
+      "name": "paymentMethodDetails",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "__typename",
+          "storageKey": null
+        },
+        {
+          "kind": "InlineFragment",
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "lastDigits",
+              "storageKey": null
+            }
+          ],
+          "type": "CreditCard",
+          "abstractKey": null
+        },
+        {
+          "kind": "InlineFragment",
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "last4",
+              "storageKey": null
+            }
+          ],
+          "type": "BankAccount",
+          "abstractKey": null
+        },
+        {
+          "kind": "InlineFragment",
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "isManualPayment",
+              "storageKey": null
+            }
+          ],
+          "type": "WireTransfer",
+          "abstractKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "Money",
+      "kind": "LinkedField",
+      "name": "buyerTotal",
+      "plural": false,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "kind": "ScalarField",
+          "name": "display",
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "kind": "ScalarField",
+      "name": "currencyCode",
+      "storageKey": null
+    },
+    {
+      "alias": null,
+      "args": null,
+      "concreteType": "LineItem",
+      "kind": "LinkedField",
+      "name": "lineItems",
+      "plural": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "Artwork",
+          "kind": "LinkedField",
+          "name": "artwork",
+          "plural": false,
+          "selections": [
+            (v0/*: any*/),
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Partner",
+              "kind": "LinkedField",
+              "name": "partner",
+              "plural": false,
+              "selections": [
+                (v0/*: any*/),
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "initials",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "kind": "ScalarField",
+                  "name": "name",
+                  "storageKey": null
+                },
+                {
+                  "alias": null,
+                  "args": null,
+                  "concreteType": "Profile",
+                  "kind": "LinkedField",
+                  "name": "profile",
+                  "plural": false,
+                  "selections": [
+                    {
+                      "alias": null,
+                      "args": null,
+                      "concreteType": "Image",
+                      "kind": "LinkedField",
+                      "name": "icon",
+                      "plural": false,
+                      "selections": (v1/*: any*/),
+                      "storageKey": null
+                    }
+                  ],
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "shippingOrigin",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "title",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "artistNames",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": [
+                {
+                  "kind": "Literal",
+                  "name": "shallow",
+                  "value": true
+                }
+              ],
+              "concreteType": "Artist",
+              "kind": "LinkedField",
+              "name": "artists",
+              "plural": true,
+              "selections": [
+                (v0/*: any*/)
+              ],
+              "storageKey": "artists(shallow:true)"
+            }
+          ],
+          "storageKey": null
+        },
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "ArtworkVersion",
+          "kind": "LinkedField",
+          "name": "artworkVersion",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Image",
+              "kind": "LinkedField",
+              "name": "image",
+              "plural": false,
+              "selections": (v1/*: any*/),
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
+      "storageKey": null
+    }
+  ],
+  "type": "Order",
+  "abstractKey": null
+};
+})();
+
+(node as any).hash = "88c3bff01ede9fa25c63422cac008709";
+
+export default node;
