ffe7c1d9f0413802a96909c506a2ecebc855669a
Rachel Quartararo <rquartararo@gmail.com>
2025-11-10 12:47:50 -0500

    refactor offer step components

--- src/Apps/Order2/Routes/Checkout/Components/OfferStep/Forms/Order2HiddenPriceOfferForm.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/OfferStep/Forms/Order2HiddenPriceOfferForm.tsx
@@ -1,68 +1,23 @@
-import { Box, Button, Flex, Spacer, Text, TextArea } from "@artsy/palette"
 import { OfferInput } from "Apps/Order/Components/OfferInput"
-import type { OfferFormComponentProps } from "Apps/Order2/Routes/Checkout/Components/OfferStep/types"
+import type { OfferFormProps } from "Apps/Order2/Routes/Checkout/Components/OfferStep/types"
 
-export const Order2HiddenPriceOfferForm: React.FC<OfferFormComponentProps> = ({
+export const Order2HiddenPriceOfferForm: React.FC<OfferFormProps> = ({
   offerValue,
-  offerNoteValue,
   formIsDirty,
-  isSubmittingOffer,
   onOfferValueChange,
   onOfferOptionSelected,
-  onOfferNoteChange,
-  onContinueButtonPressed,
 }) => {
   return (
-    <Box pb={2} px={[2, 4]}>
-      {/* Offer Amount Section */}
-      <Flex flexDirection="column">
-        <OfferInput
-          id="OfferForm_offerValue"
-          showError={formIsDirty && offerValue <= 0}
-          onChange={onOfferValueChange}
-          onBlur={() => {
-            if (offerValue > 0) {
-              onOfferOptionSelected(offerValue)
-            }
-          }}
-          value={offerValue}
-        />
-      </Flex>
-
-      <Spacer y={4} />
-
-      {/* Offer Note Section */}
-      <Flex flexDirection="column">
-        <Text
-          variant={["sm-display", "md"]}
-          fontWeight={["bold", "normal"]}
-          color="mono100"
-        >
-          Offer note
-        </Text>
-        <Text variant={["xs", "xs", "sm"]} color="mono100">
-          Additional context to help the gallery evaluate your offer.
-        </Text>
-
-        <TextArea
-          title="Note (recommended)"
-          maxLength={1000}
-          placeholder="Share what draws you to this work or artist, or add any context about your offer"
-          onChange={onOfferNoteChange}
-          value={offerNoteValue.value}
-        />
-
-        <Spacer y={4} />
-        <Button
-          variant="primaryBlack"
-          width="100%"
-          onClick={onContinueButtonPressed}
-          loading={isSubmittingOffer}
-          disabled={isSubmittingOffer}
-        >
-          Save and Continue
-        </Button>
-      </Flex>
-    </Box>
+    <OfferInput
+      id="OfferForm_offerValue"
+      showError={formIsDirty && offerValue <= 0}
+      onChange={onOfferValueChange}
+      onBlur={() => {
+        if (offerValue > 0) {
+          onOfferOptionSelected(offerValue)
+        }
+      }}
+      value={offerValue}
+    />
   )
 }
--- src/Apps/Order2/Routes/Checkout/Components/OfferStep/types.ts
+++ src/Apps/Order2/Routes/Checkout/Components/OfferStep/types.ts
@@ -6,18 +6,9 @@ export interface OfferNoteValue {
 export interface OfferFormProps {
   order: any
   offerValue: number
-  offerNoteValue: OfferNoteValue
   formIsDirty: boolean
-  isSubmittingOffer: boolean
   onOfferValueChange: (value: number) => void
   onOfferOptionSelected: (value: number, description?: string) => void
-  onOfferNoteChange: (note: OfferNoteValue) => void
-  onSubmit: () => Promise<void>
-}
-
-export interface OfferFormComponentProps
-  extends Omit<OfferFormProps, "onSubmit"> {
-  onContinueButtonPressed: () => Promise<void>
 }
 
 export interface OfferSubmissionData {
