e869511cf987cad2a436fb0de2f916c168fd4f57
dzucconi <mail@damonzucconi.com>
2025-12-02 08:51:07 -0500

    <back>: prototypes stuck sub-nav

--- src/Apps/Artist/Routes/ArtistABTestRoute.tsx
+++ src/Apps/Artist/Routes/ArtistABTestRoute.tsx
@@ -1,14 +1,8 @@
-import { BaseTab, Clickable, Skeleton, Spacer, Text } from "@artsy/palette"
+import { BaseTab, Clickable, Skeleton, Spacer } from "@artsy/palette"
 import { useVariant } from "@unleash/proxy-client-react"
 import { useFlagsStatus } from "@unleash/proxy-client-react"
-import { ArtistTabs } from "Apps/Artist/Components/ArtistTabs"
-import { ArtistAuctionResultsQueryRenderer } from "Apps/Artist/Routes/AuctionResults/ArtistAuctionResults"
-import { MarketStatsQueryRenderer } from "Apps/Artist/Routes/AuctionResults/Components/MarketStats"
 import { ArtistCombinedRouteFragmentContainer } from "Apps/Artist/Routes/Combined/ArtistCombinedRoute"
-import { ArtistOverviewQueryRenderer } from "Apps/Artist/Routes/Overview/Components/ArtistOverview"
-import { ArtistArtworkFilterQueryRenderer } from "Apps/Artist/Routes/WorksForSale/Components/ArtistArtworkFilter"
 import { RouteTabs } from "Components/RouteTabs"
-import { useRouter } from "System/Hooks/useRouter"
 import { useTrackFeatureVariantOnMount } from "System/Hooks/useTrackFeatureVariant"
 import type { ArtistABTestRoute_artist$data } from "__generated__/ArtistABTestRoute_artist.graphql"
 import type { FC } from "react"
@@ -53,22 +47,7 @@ export const ArtistABTestRoute: FC<
     )
   }
 
-  if (variant.name === "experiment") {
-    return <ArtistCombinedRouteFragmentContainer artist={artist} />
-  }
-
-  // "control" or "disabled"
-  return (
-    <>
-      <Spacer y={[2, 4]} />
-
-      <ArtistTabs slug={artist.slug} />
-
-      <Spacer y={[2, 4]} />
-
-      <ArtistABTestRouteControl artist={artist} />
-    </>
-  )
+  return <ArtistCombinedRouteFragmentContainer artist={artist} />
 }
 
 export const ArtistABTestRouteFragmentContainer = createFragmentContainer(
@@ -83,51 +62,3 @@ export const ArtistABTestRouteFragmentContainer = createFragmentContainer(
     `,
   },
 )
-
-const ArtistABTestRouteControl: FC<
-  React.PropsWithChildren<ArtistABTestRouteProps>
-> = ({ artist }) => {
-  const {
-    match: { location },
-  } = useRouter()
-
-  const page = location.pathname.split("/").pop()
-
-  switch (page) {
-    case "auction-results": {
-      return (
-        <>
-          <MarketStatsQueryRenderer id={artist.internalID} />
-
-          <Spacer y={6} />
-
-          <Text variant="lg-display" as="h2">
-            Auction Results
-          </Text>
-
-          <Spacer y={2} />
-
-          <ArtistAuctionResultsQueryRenderer id={artist.internalID} />
-        </>
-      )
-    }
-
-    case "about": {
-      return <ArtistOverviewQueryRenderer id={artist.internalID} />
-    }
-
-    default: {
-      return (
-        <>
-          <Text variant="lg-display" as="h2">
-            Artworks
-          </Text>
-
-          <Spacer y={2} />
-
-          <ArtistArtworkFilterQueryRenderer id={artist.internalID} />
-        </>
-      )
-    }
-  }
-}
