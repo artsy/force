eba52ae3ed4fa2e84e655232c86b02af9c5a8da9
Anson Wang <ansor4@gmail.com>
2025-12-02 14:06:20 -0800

    try again

--- src/Components/__tests__/ErrorPage.jest.tsx
+++ src/Components/__tests__/ErrorPage.jest.tsx
@@ -4,11 +4,11 @@ import { ErrorPage } from "Components/ErrorPage"
 
 describe("ErrorPage", () => {
   describe("unit", () => {
-    it("renders an error page with no stack trace if it's a 404", () => {
+    it("renders an error page with a stack trace if it's a 404", () => {
       const { container } = render(
         <ErrorPage code={404} message="Custom error message" />,
       )
-      expect(container.textContent).not.toMatch("Custom error message")
+      expect(container.textContent).toMatch("Custom error message")
     })
 
     it("renders an error page with a stack trace if it's not a 404", () => {
