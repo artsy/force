24e397240b343c421e75966adbc22e570b3d037e
brainbicycle <brian.beckerle@artsymail.com>
2026-02-12 15:13:20 -0500

    remove log

--- src/Server/analytics/brazeMessagingIntegration.ts
+++ src/Server/analytics/brazeMessagingIntegration.ts
@@ -35,11 +35,6 @@ export const subscribeToInAppMessagesByPath = () => {
 
   if (!analytics || !braze || !location) return
 
-  console.log(
-    "[Braze] Subscribing to Braze In-App Messages for path:",
-    location.pathname,
-  )
-
   braze.subscribeToInAppMessage(inAppMessage => {
     if (!isMatchingRoute(location.pathname)) return
 
