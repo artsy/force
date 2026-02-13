dbba3b1d04b4cf8481974a1e98d73dc05fc943dc
dzucconi <mail@damonzucconi.com>
2026-02-13 09:37:02 -0500

    fix(partner): adjusts marker color and map resolution

--- src/Apps/Partner/Components/PartnerContacts/PartnerContactMap.tsx
+++ src/Apps/Partner/Components/PartnerContacts/PartnerContactMap.tsx
@@ -3,7 +3,6 @@ import { RouterLink } from "System/Components/RouterLink"
 import type { PartnerContactMap_location$data } from "__generated__/PartnerContactMap_location.graphql"
 import type * as React from "react"
 import { createFragmentContainer, graphql } from "react-relay"
-import styled from "styled-components"
 import {
   getGoogleMapUrl,
   getGoogleStaticMapImageUrl,
@@ -13,12 +12,6 @@ export interface PartnerContactMapProps {
   location: PartnerContactMap_location$data
 }
 
-const StaticMapImage = styled(Image)`
-  display: block;
-  width: 100%;
-  height: 100%;
-`
-
 const aspectWidth = 2
 const imageHeight = 300
 
@@ -26,6 +19,7 @@ export const PartnerContactMap: React.FC<
   React.PropsWithChildren<PartnerContactMapProps>
 > = ({ location }) => {
   const mapUrl = getGoogleMapUrl(location)
+
   const mapImageUrl = getGoogleStaticMapImageUrl(
     location,
     imageHeight * aspectWidth,
@@ -37,7 +31,7 @@ export const PartnerContactMap: React.FC<
   return (
     <RouterLink to={mapUrl} target="_blank">
       <ResponsiveBox aspectWidth={aspectWidth} aspectHeight={1} maxWidth="100%">
-        <StaticMapImage src={mapImageUrl} alt="" lazyLoad />
+        <Image src={mapImageUrl} alt="" lazyLoad width="100%" height="100%" />
       </ResponsiveBox>
     </RouterLink>
   )
