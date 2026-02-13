f90ffc82ada0be42e2fd9aa8716843b5016a6e0e
Fladson Gomes <fladsonthiago@gmail.com>
2026-02-13 15:58:55 +0100

    Revert "Update src/Components/Inquiry/Views/InquiryInquiry.tsx"
    This reverts commit fad3f21f3ed4caccbbca1054a48910e4485fe5ff.

--- src/Components/Inquiry/Views/InquiryInquiry.tsx
+++ src/Components/Inquiry/Views/InquiryInquiry.tsx
@@ -122,7 +122,7 @@ const InquiryInquiry: React.FC<
       label: "Certificate of Authenticity",
       value: artwork.certificateOfAuthenticity?.details,
     },
-  ].filter(Boolean)
+  ].filter(item => item.value != null && item.value !== "")
 
   return (
     <Box as="form" onSubmit={handleSubmit}>
