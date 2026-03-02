a8abe5c1e7cffa93de64125f07cd102c0537c0b5
Janae Edwards <edwards.janae@gmail.com>
2026-03-02 10:48:54 -0500

    implemented a/b test for create alert modal

--- src/Components/ArtworkFilter/ArtworkFilterCreateAlert.tsx
+++ src/Components/ArtworkFilter/ArtworkFilterCreateAlert.tsx
@@ -3,6 +3,8 @@ import { ProgressiveOnboardingAlertCreate } from "Components/ProgressiveOnboardi
 import { useSavedSearchAlertContext } from "Components/SavedSearchAlert/SavedSearchAlertContext"
 import { isEmpty } from "lodash"
 import type { FC, ReactNode } from "react"
+import { useVariant } from "@unleash/proxy-client-react"
+import { useTrackFeatureVariantOnMount } from "System/Hooks/useTrackFeatureVariant"
 
 interface ArtworkFilterCreateAlertProps {
   renderButton: (props: { onClick: () => void }) => JSX.Element
@@ -13,29 +15,49 @@ export const ArtworkFilterCreateAlert: FC<
   React.PropsWithChildren<ArtworkFilterCreateAlertProps>
 > = ({ renderButton, children }) => {
   const { entity } = useSavedSearchAlertContext()
+  const variant = useVariant("diamond_remove_tooltip_experiment")
+
+  useTrackFeatureVariantOnMount({
+    experimentName: "diamond_remove_tooltip_experiment",
+    variantName: variant?.name,
+  })
+
+  const showTooltip = variant.name !== "experiment"
 
   // If there is no entity then we don't want to create an alert
   if (isEmpty(entity)) return null
 
   return (
     <>
-      <CreateAlertButton
-        renderButton={({ onClick }) => (
-          <ProgressiveOnboardingAlertCreate>
-            {/*
+      {showTooltip ? (
+        <CreateAlertButton
+          renderButton={({ onClick }) => (
+            <ProgressiveOnboardingAlertCreate>
+              {/*
               FIXME: REACT_18_UPGRADE
               @ts-ignore */}
-            {({ onSkip: createSkip }) =>
-              renderButton({
-                onClick: () => {
-                  createSkip()
-                  onClick()
-                },
-              })
-            }
-          </ProgressiveOnboardingAlertCreate>
-        )}
-      />
+              {({ onSkip: createSkip }) =>
+                renderButton({
+                  onClick: () => {
+                    createSkip()
+                    onClick()
+                  },
+                })
+              }
+            </ProgressiveOnboardingAlertCreate>
+          )}
+        />
+      ) : (
+        <CreateAlertButton
+          renderButton={({ onClick }) =>
+            renderButton({
+              onClick: () => {
+                onClick()
+              },
+            })
+          }
+        />
+      )}
 
       {children}
     </>
