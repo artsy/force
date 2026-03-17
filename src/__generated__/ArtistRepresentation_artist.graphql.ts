5570adfc37721456f7a1da55bcf30579edd50ffe
dzucconi <mail@damonzucconi.com>
2026-03-17 10:06:16 -0400

    feat(artist): adds representation component

--- src/__generated__/ArtistRepresentation_artist.graphql.ts
+++ src/__generated__/ArtistRepresentation_artist.graphql.ts
@@ -1,5 +1,5 @@
 /**
- * @generated SignedSource<<6be47506b40edd6c68956602ffc26bf0>>
+ * @generated SignedSource<<8d0b6d458eb384a9622f6524b0ada6af>>
  * @lightSyntaxTransform
  * @nogrep
  */
@@ -11,7 +11,23 @@
 import { ReaderFragment } from 'relay-runtime';
 import { FragmentRefs } from "relay-runtime";
 export type ArtistRepresentation_artist$data = {
-  readonly internalID: string;
+  readonly verifiedRepresentatives: ReadonlyArray<{
+    readonly partner: {
+      readonly href: string | null | undefined;
+      readonly internalID: string;
+      readonly name: string | null | undefined;
+      readonly profile: {
+        readonly icon: {
+          readonly _1x: {
+            readonly src: string;
+          } | null | undefined;
+          readonly _2x: {
+            readonly src: string;
+          } | null | undefined;
+        } | null | undefined;
+      } | null | undefined;
+    };
+  }>;
   readonly " $fragmentType": "ArtistRepresentation_artist";
 };
 export type ArtistRepresentation_artist$key = {
@@ -19,7 +35,17 @@ export type ArtistRepresentation_artist$key = {
   readonly " $fragmentSpreads": FragmentRefs<"ArtistRepresentation_artist">;
 };
 
-const node: ReaderFragment = {
+const node: ReaderFragment = (function(){
+var v0 = [
+  {
+    "alias": null,
+    "args": null,
+    "kind": "ScalarField",
+    "name": "src",
+    "storageKey": null
+  }
+];
+return {
   "argumentDefinitions": [],
   "kind": "Fragment",
   "metadata": null,
@@ -28,15 +54,116 @@ const node: ReaderFragment = {
     {
       "alias": null,
       "args": null,
-      "kind": "ScalarField",
-      "name": "internalID",
+      "concreteType": "VerifiedRepresentative",
+      "kind": "LinkedField",
+      "name": "verifiedRepresentatives",
+      "plural": true,
+      "selections": [
+        {
+          "alias": null,
+          "args": null,
+          "concreteType": "Partner",
+          "kind": "LinkedField",
+          "name": "partner",
+          "plural": false,
+          "selections": [
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "internalID",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "name",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "kind": "ScalarField",
+              "name": "href",
+              "storageKey": null
+            },
+            {
+              "alias": null,
+              "args": null,
+              "concreteType": "Profile",
+              "kind": "LinkedField",
+              "name": "profile",
+              "plural": false,
+              "selections": [
+                {
+                  "alias": null,
+                  "args": null,
+                  "concreteType": "Image",
+                  "kind": "LinkedField",
+                  "name": "icon",
+                  "plural": false,
+                  "selections": [
+                    {
+                      "alias": "_1x",
+                      "args": [
+                        {
+                          "kind": "Literal",
+                          "name": "height",
+                          "value": 45
+                        },
+                        {
+                          "kind": "Literal",
+                          "name": "width",
+                          "value": 45
+                        }
+                      ],
+                      "concreteType": "CroppedImageUrl",
+                      "kind": "LinkedField",
+                      "name": "cropped",
+                      "plural": false,
+                      "selections": (v0/*: any*/),
+                      "storageKey": "cropped(height:45,width:45)"
+                    },
+                    {
+                      "alias": "_2x",
+                      "args": [
+                        {
+                          "kind": "Literal",
+                          "name": "height",
+                          "value": 90
+                        },
+                        {
+                          "kind": "Literal",
+                          "name": "width",
+                          "value": 90
+                        }
+                      ],
+                      "concreteType": "CroppedImageUrl",
+                      "kind": "LinkedField",
+                      "name": "cropped",
+                      "plural": false,
+                      "selections": (v0/*: any*/),
+                      "storageKey": "cropped(height:90,width:90)"
+                    }
+                  ],
+                  "storageKey": null
+                }
+              ],
+              "storageKey": null
+            }
+          ],
+          "storageKey": null
+        }
+      ],
       "storageKey": null
     }
   ],
   "type": "Artist",
   "abstractKey": null
 };
+})();
 
-(node as any).hash = "a7b13711bd3fad6b2f64e4d9e494394c";
+(node as any).hash = "19f0b6583f5e0f5ebe45539b80b2879f";
 
 export default node;
