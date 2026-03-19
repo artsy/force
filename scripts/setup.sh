3709400649726fd51f6b414777837d6ab2c745b9
Ozzie Vasdi <29984068+ovasdi@users.noreply.github.com>
2026-03-19 09:22:45 -0400

    chore: remove Artsy extension pack installation from setup

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -15,8 +15,6 @@ fi
 
 if command -v code >/dev/null; then
   echo "Installing Artsy VSCode extensions..."
-  code --install-extension Artsy.artsy-studio-extension-pack
-  # TODO: Move this into artsy-studio-extension-pack
   code --install-extension biomejs.biome
 else
   echo "Skipping VSCode extensions installation (VSCode not found)"
