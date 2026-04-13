b1a5d12857be8353f61f5be70f4cac1f7edcaa96
Janae Edwards <edwards.janae@gmail.com>
2026-04-13 14:35:49 -0400

    removing hardcoded maxWidth

--- src/Apps/Authentication/Routes/SignupLanding/Components/SignupStats.tsx
+++ src/Apps/Authentication/Routes/SignupLanding/Components/SignupStats.tsx
@@ -1,22 +1,19 @@
 import { Box, GridColumns, Column, Text } from "@artsy/palette"
-import { AppContainer } from "Apps/Components/AppContainer"
 
 export const SignupStats = () => {
   return (
-    <AppContainer>
-      <Box mx="auto" maxWidth="1200px">
-        <GridColumns textAlign="center" gridRowGap={[4, 2]}>
-          <Column span={[12, 4]}>
-            <Text variant="sm"> Stat 1 (DI-91) </Text>
-          </Column>
-          <Column span={[12, 4]}>
-            <Text variant="sm"> Stat 2 (DI-91)</Text>
-          </Column>
-          <Column span={[12, 4]}>
-            <Text variant="sm"> Stat 3 (DI-91) </Text>
-          </Column>
-        </GridColumns>
-      </Box>
-    </AppContainer>
+    <Box mx="auto">
+      <GridColumns textAlign="center" gridRowGap={[4, 2]}>
+        <Column span={[12, 4]}>
+          <Text variant="sm"> Stat 1 (DI-91) </Text>
+        </Column>
+        <Column span={[12, 4]}>
+          <Text variant="sm"> Stat 2 (DI-91)</Text>
+        </Column>
+        <Column span={[12, 4]}>
+          <Text variant="sm"> Stat 3 (DI-91) </Text>
+        </Column>
+      </GridColumns>
+    </Box>
   )
 }
--- src/Apps/Authentication/Routes/SignupLanding/Components/SignupValueProps.tsx
+++ src/Apps/Authentication/Routes/SignupLanding/Components/SignupValueProps.tsx
@@ -7,28 +7,31 @@ import {
   FullBleed,
 } from "@artsy/palette"
 import { AppContainer } from "Apps/Components/AppContainer"
+import { HorizontalPadding } from "Apps/Components/HorizontalPadding"
 
 export const SignupValueProps = () => {
   return (
     <FullBleed bg="mono5" py={[6, 12]}>
       <AppContainer>
-        <Text variant={["xl", "xxl"]} textAlign="center">
-          Why Choose Artsy
-        </Text>
-        <Spacer y={4} />
-        <Box mx="auto" maxWidth="1200px">
-          <GridColumns textAlign="center" gridRowGap={4}>
-            <Column span={[12, 4]}>
-              <Text variant="sm"> Feature card 1 (DI-91)</Text>
-            </Column>
-            <Column span={[12, 4]}>
-              <Text variant="sm"> Feature card 2 (DI-91)</Text>
-            </Column>
-            <Column span={[12, 4]}>
-              <Text variant="sm"> Feature card 3 (DI-91)</Text>
-            </Column>
-          </GridColumns>
-        </Box>
+        <HorizontalPadding>
+          <Text variant={["xl", "xxl"]} textAlign="center">
+            Why Choose Artsy
+          </Text>
+          <Spacer y={4} />
+          <Box mx="auto">
+            <GridColumns textAlign="center" gridRowGap={4}>
+              <Column span={[12, 4]}>
+                <Text variant="sm"> Feature card 1 (DI-91)</Text>
+              </Column>
+              <Column span={[12, 4]}>
+                <Text variant="sm"> Feature card 2 (DI-91)</Text>
+              </Column>
+              <Column span={[12, 4]}>
+                <Text variant="sm"> Feature card 3 (DI-91)</Text>
+              </Column>
+            </GridColumns>
+          </Box>
+        </HorizontalPadding>
       </AppContainer>
     </FullBleed>
   )
