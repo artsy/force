aa3b3fdcdd200ffeaee6f8a8026e844b4ac5a68a
Janae Edwards <edwards.janae@gmail.com>
2026-04-17 12:06:47 -0400

    fixed viewport issues for mobile web view

--- src/Apps/Authentication/Routes/SignupLanding/Components/SignupHero.tsx
+++ src/Apps/Authentication/Routes/SignupLanding/Components/SignupHero.tsx
@@ -1,21 +1,19 @@
-import { GridColumns, Column, Box } from "@artsy/palette"
+import { GridColumns, Column } from "@artsy/palette"
 import { SignupForm } from "Apps/Authentication/Routes/SignupLanding/Components/SignupForm"
 import { SignupHeroContent } from "Apps/Authentication/Routes/SignupLanding/Components/SignupHeroContent"
 import { HorizontalPadding } from "Apps/Components/HorizontalPadding"
 
 export const SignupHero = () => {
   return (
-    <HorizontalPadding>
-      <Box mx="auto" px={[2, 4]} py={[4, 6, 12]}>
-        <GridColumns gridRowGap={[6, 4]} gridColumnGap={[4, 6, 8]}>
-          <Column span={[12, 12, 7]} pr={[0, 2, 4]}>
-            <SignupHeroContent />
-          </Column>
-          <Column span={[12, 12, 5]} pl={[0, 2, 4]}>
-            <SignupForm />
-          </Column>
-        </GridColumns>
-      </Box>
+    <HorizontalPadding py={[4, 6, 12]}>
+      <GridColumns gridRowGap={[6, 4]}>
+        <Column span={[12, 12, 7]} pr={[0, 0, 4]}>
+          <SignupHeroContent />
+        </Column>
+        <Column span={[12, 12, 5]} pl={[0, 0, 4]}>
+          <SignupForm />
+        </Column>
+      </GridColumns>
     </HorizontalPadding>
   )
 }
