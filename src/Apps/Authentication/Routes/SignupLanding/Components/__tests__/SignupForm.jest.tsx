2cc59ef5280b783951d2570b8058cc200ab50d48
Janae Edwards <edwards.janae@gmail.com>
2026-04-17 12:35:06 -0400

    comments

--- src/Apps/Authentication/Routes/SignupLanding/Components/__tests__/SignupForm.jest.tsx
+++ src/Apps/Authentication/Routes/SignupLanding/Components/__tests__/SignupForm.jest.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react"
-// import { setupTestWrapperTL } from "DevTools/setupTestWrapperTL"
 import { SignupForm } from "../SignupForm"
 import { signUp } from "Utils/auth"
 
