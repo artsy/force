c1a4fb40f32028d69a7c4720f15b868ee3226150
Janae Edwards <edwards.janae@gmail.com>
2026-04-20 08:42:16 -0400

    adjusting text and icon sizes for mobile first view

--- src/Apps/Authentication/Routes/SignupLanding/Components/SignupHeroContent.tsx
+++ src/Apps/Authentication/Routes/SignupLanding/Components/SignupHeroContent.tsx
@@ -4,17 +4,17 @@ import CheckmarkFillIcon from "@artsy/icons/CheckmarkFillIcon"
 export const SignupHeroContent = () => {
   return (
     <Box>
-      <Text variant={["xl", "xxl"]} as="h1">
+      <Text variant={["lg", "xl", "xxl"]} as="h1">
         Discover and Buy Art That Moves You.
       </Text>
       <Spacer y={2} />
-      <Text variant="lg-display" color="mono60">
+      <Text variant={["md", "lg-display"]} color="mono60">
         Welcome to Artsy, the world's largest online art marketplace. We give
         you the tools to make art part of your daily routine, whether you're a
         seasoned art collector or looking for your first artwork.
       </Text>
       <Spacer y={4} />
-      <Stack gap={2}>
+      <Stack gap={1}>
         <ValuePropItem text="Find the art you love with 1.6M+ original artworks for sale" />
         <ValuePropItem text="Get art recommendations that match your taste" />
         <ValuePropItem text="Build and manage your collection" />
@@ -27,8 +27,8 @@ export const SignupHeroContent = () => {
 const ValuePropItem = ({ text }: { text: string }) => {
   return (
     <Flex alignItems="flex-start" gap={1}>
-      <CheckmarkFillIcon fill="black100" />
-      <Text variant="md">{text}</Text>
+      <CheckmarkFillIcon fill="black100" flexShrink={0} />
+      <Text variant="sm">{text}</Text>
     </Flex>
   )
 }
