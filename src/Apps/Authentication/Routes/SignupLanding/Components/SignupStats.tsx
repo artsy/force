caf0d9fe5daa015805aa2f19c63ea33903cb6d63
Janae Edwards <edwards.janae@gmail.com>
2026-04-21 13:23:55 -0400

    using format of /aboutus page stats

--- src/Apps/Authentication/Routes/SignupLanding/Components/SignupStats.tsx
+++ src/Apps/Authentication/Routes/SignupLanding/Components/SignupStats.tsx
@@ -5,16 +5,25 @@ export const SignupStats = () => {
     <Box mx="auto">
       <GridColumns textAlign="center" gridRowGap={[4, 2]}>
         <Column span={[12, 4]}>
-          <Text variant="xl"> 1.6M</Text>
-          <Text variant="md"> Artworks available</Text>
+          <Text variant={["xl", "xxl"]}>1.6M</Text>
+          <Text variant="sm-display" color="mono60">
+            {" "}
+            Artworks available
+          </Text>
         </Column>
         <Column span={[12, 4]}>
-          <Text variant="xl"> 3.1K</Text>
-          <Text variant="md"> Gallery partners</Text>
+          <Text variant={["xl", "xxl"]}>3.1K</Text>
+          <Text variant="sm-display" color="mono60">
+            {" "}
+            Gallery partners
+          </Text>
         </Column>
         <Column span={[12, 4]}>
-          <Text variant="xl"> 3.4M</Text>
-          <Text variant="md"> Artsy members</Text>
+          <Text variant={["xl", "xxl"]}>3.4M</Text>
+          <Text variant="sm-display" color="mono60">
+            {" "}
+            Artsy members
+          </Text>
         </Column>
       </GridColumns>
     </Box>
