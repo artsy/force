55fafd8af60fe6eb10db9ddb328a1afa0edadb88
dzucconi <mail@damonzucconi.com>
2026-05-05 07:39:32 -0400

    fix(passport): displays 2fa errors

--- src/Apps/Authentication/Components/AuthenticationInlineDialog.tsx
+++ src/Apps/Authentication/Components/AuthenticationInlineDialog.tsx
@@ -87,10 +87,16 @@ export const AuthenticationInlineDialog: FC<
 }
 
 const ERROR_CODES = {
-  ALREADY_EXISTS: `A user with this email address already exists. Log in to Artsy via email and password and link {provider} in your settings instead.`,
-  PREVIOUSLY_LINKED_SETTINGS: `{provider} account previously linked to Artsy. Log in to your Artsy account via email and password and link {provider} in your settings instead.`,
-  PREVIOUSLY_LINKED: `{provider} account previously linked to Artsy.`,
+  ALREADY_EXISTS:
+    "A user with this email address already exists. Log in to Artsy via email and password and link {provider} in your settings instead.",
+  PREVIOUSLY_LINKED_SETTINGS:
+    "{provider} account previously linked to Artsy. Log in to your Artsy account via email and password and link {provider} in your settings instead.",
+  PREVIOUSLY_LINKED: "{provider} account previously linked to Artsy.",
   IP_BLOCKED: "Your IP address was blocked by {provider}.",
+  TWO_FACTOR_AUTHENTICATION_REQUIRED:
+    "Please log in with email and password to use two-factor authentication.",
+  TWO_FACTOR_AUTHENTICATION_ENABLED:
+    "Social account linking is not available while two-factor authentication is enabled on your Artsy account.",
   UNKNOWN: "An unknown error occurred. Please try again.",
 }
 
