fc4b7e563a65a072b4934f289f2cac72a75b9d9a
dariakoko <nargle30@gmail.com>
2026-05-11 18:05:55 +0200

    fix: fix loading skeleton

--- src/Apps/Order2/Routes/Checkout/Components/Order2CheckoutLoadingSkeleton.tsx
+++ src/Apps/Order2/Routes/Checkout/Components/Order2CheckoutLoadingSkeleton.tsx
@@ -97,18 +97,22 @@ const Order2CollapsibleOrderSummarySkeleton: React.FC<
                 {/* Artwork details */}
                 <Box ml={1} flexGrow={1}>
                   <Flex>
-                    <SkeletonText variant="sm" flexGrow={1}>
+                    <SkeletonText variant="xs" flexGrow={1}>
                       {props.artworkArtistNames}
                     </SkeletonText>
                     {/* Price and chevron */}
-                    <Flex flexGrow={0} justifyContent={"flex-end"}>
-                      <SkeletonText variant="sm" mr={0.5}>
+                    <Flex
+                      flexGrow={0}
+                      justifyContent={"flex-end"}
+                      display={["none", "block", "none"]}
+                    >
+                      <SkeletonText variant="xs" mr={0.5}>
                         {props.artworkPrice}
                       </SkeletonText>
                       <SkeletonBox width={18} height={16} mt="2px" />
                     </Flex>
                   </Flex>
-                  <SkeletonText variant="sm">
+                  <SkeletonText variant="xs">
                     {props.artworkTitle}, {props.artworkDate}
                   </SkeletonText>
                 </Box>
@@ -156,7 +160,11 @@ const StepsSkeleton = () => {
           First step title
         </SkeletonText>
 
-        <SkeletonText flex={0} variant="sm-display">
+        <SkeletonText
+          flex={0}
+          variant="sm-display"
+          display={["none", "block", "none"]}
+        >
           Loading...
         </SkeletonText>
       </Flex>
