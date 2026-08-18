779b87da5be90ae3994c5edb3151f61fe6fd4a41
Rachel Quartararo <rquartararo@gmail.com>
2026-08-18 09:01:50 -0400

    remove mock data

--- src/Apps/Artist/Routes/Overview/Components/ArtistInstagramRail.tsx
+++ src/Apps/Artist/Routes/Overview/Components/ArtistInstagramRail.tsx
@@ -3,16 +3,6 @@ import { SystemQueryRenderer } from "System/Relay/SystemQueryRenderer"
 import type { ArtistInstagramRailQuery } from "__generated__/ArtistInstagramRailQuery.graphql"
 import type { ArtistInstagramRail_artist$data } from "__generated__/ArtistInstagramRail_artist.graphql"
 import { createFragmentContainer, graphql } from "react-relay"
-import mockInstagram1 from "./mockInstagramImages/mock-instagram-1.png"
-import mockInstagram2 from "./mockInstagramImages/mock-instagram-2.png"
-import mockInstagram3 from "./mockInstagramImages/mock-instagram-3.png"
-import mockInstagram4 from "./mockInstagramImages/mock-instagram-4.png"
-import mockInstagram5 from "./mockInstagramImages/mock-instagram-5.png"
-import mockInstagram6 from "./mockInstagramImages/mock-instagram-6.png"
-import mockInstagram7 from "./mockInstagramImages/mock-instagram-7.png"
-import mockInstagram8 from "./mockInstagramImages/mock-instagram-8.png"
-import mockInstagram9 from "./mockInstagramImages/mock-instagram-9.png"
-import mockInstagram10 from "./mockInstagramImages/mock-instagram-10.png"
 
 interface ArtistInstagramRailProps {
   artist: ArtistInstagramRail_artist$data
@@ -26,37 +16,10 @@ interface InstagramRailTile {
   srcSet?: string
 }
 
-// TODO: Remove once the Instagram integration passes Facebook app review and
-// `instagramMedia` returns live posts again. Until then we fall back to these
-// colocated mock images so the rail still renders.
-const MOCK_INSTAGRAM_IMAGES: string[] = [
-  mockInstagram1,
-  mockInstagram2,
-  mockInstagram3,
-  mockInstagram4,
-  mockInstagram5,
-  mockInstagram6,
-  mockInstagram7,
-  mockInstagram8,
-  mockInstagram9,
-  mockInstagram10,
-]
-
-const MOCK_INSTAGRAM_TILES: InstagramRailTile[] = MOCK_INSTAGRAM_IMAGES.map(
-  (src, index) => {
-    return {
-      internalID: `mock-instagram-${index}`,
-      permalink: "https://www.instagram.com/p/Da3Wck_oG7r/?hl=en&img_index=1",
-      caption: "Artsy on Instagram",
-      src,
-    }
-  },
-)
-
 const ArtistInstagramRail: React.FC<
   React.PropsWithChildren<ArtistInstagramRailProps>
 > = ({ artist }) => {
-  const liveMedia: InstagramRailTile[] = (artist.instagramMedia ?? [])
+  const media: InstagramRailTile[] = (artist.instagramMedia ?? [])
     .filter((item): item is NonNullable<typeof item> => !!item?.image?.cropped)
     .map(item => {
       return {
@@ -67,8 +30,6 @@ const ArtistInstagramRail: React.FC<
         srcSet: item.image?.cropped?.srcSet,
       }
     })
-
-  const media = liveMedia.length ? liveMedia : MOCK_INSTAGRAM_TILES
 
   if (!media.length) {
     return null
@@ -168,8 +129,12 @@ export const ArtistInstagramRailQueryRenderer: React.FC<
           return null
         }
 
-        if (!props || !props.artist) {
+        if (!props) {
           return PLACEHOLDER
+        }
+
+        if (!props.artist) {
+          return null
         }
 
         return <ArtistInstagramRailFragmentContainer artist={props.artist} />
