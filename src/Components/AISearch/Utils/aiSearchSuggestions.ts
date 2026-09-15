e36b5b065597db4e002829d0b50636979dd3b9ac
Nikita Skalkin <nikskalkin@gmail.com>
2026-09-15 12:56:00 +0200

    fix: typo

--- src/Components/AISearch/Utils/aiSearchSuggestions.ts
+++ src/Components/AISearch/Utils/aiSearchSuggestions.ts
@@ -1,7 +1,7 @@
 /** Starter prompts shown in the empty state; they populate the composer, not submit. */
 export const AI_SEARCH_SUGGESTIONS = [
   "Large blue abstract paintings under $20k",
-  "Something similar to my tase, check out my saves for inspo",
+  "Something similar to my taste, check out my saves for inspo",
   "Polaroids by Warhol",
   "What's trending now?",
 ]
