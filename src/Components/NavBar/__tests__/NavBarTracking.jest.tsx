1507eda5c5dba22c7072aa0a82385e3b22116021
Janae Edwards <edwards.janae@gmail.com>
2026-09-24 17:19:58 -0400

    fixing stray tests for nav bar tracking

--- src/Components/NavBar/__tests__/NavBarTracking.jest.tsx
+++ src/Components/NavBar/__tests__/NavBarTracking.jest.tsx
@@ -35,6 +35,7 @@ jest.mock("System/Hooks/useRouter", () => ({
   useRouter: jest.fn(() => ({
     match: { location: { pathname: "/" } },
   })),
+  useIsRouteActive: jest.fn(() => false),
 }))
 
 describe("NavBarTracking", () => {
